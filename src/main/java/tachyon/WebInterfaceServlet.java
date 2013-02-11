dc01e85438b8118aa5443eb80f5445e34cbb110d
Calvin Jia <calvin.jia@berkeley.edu>
2013-02-10 19:26:59 -0800

    Embedded Jetty with servlets.

--- src/main/java/tachyon/WebInterfaceServlet.java
+++ src/main/java/tachyon/WebInterfaceServlet.java
@@ -1,13 +1,21 @@
 package tachyon;
 
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class WebInterfaceServlet extends HttpServlet{
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		request.getRequestDispatcher("/index.jsp");
+public class WebInterfaceServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException{
+		request.setAttribute("data", 10);
+    request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 		return;
 	}
