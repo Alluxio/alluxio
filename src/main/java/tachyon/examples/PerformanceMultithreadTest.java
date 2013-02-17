96af751d0b373b67655fd6d5f63d09f0e66d5d61
haoyuan <haoyuan@eecs.berkeley.edu>
2013-02-16 19:37:54 -0800

    fix exapmles.

--- src/main/java/tachyon/examples/PerformanceMultithreadTest.java
+++ src/main/java/tachyon/examples/PerformanceMultithreadTest.java
@@ -14,32 +14,33 @@ import org.slf4j.LoggerFactory;
 
 import tachyon.Config;
 import tachyon.CommonUtils;
-import tachyon.client.Partition;
-import tachyon.client.Dataset;
 import tachyon.client.TachyonClient;
-import tachyon.thrift.OutOfMemoryForPinDatasetException;
-import tachyon.thrift.SuspectedPartitionSizeException;
+import tachyon.client.TachyonFile;
+import tachyon.thrift.OutOfMemoryForPinFileException;
+import tachyon.thrift.SuspectedFileSizeException;
 
 public class PerformanceMultithreadTest {
   private static Logger LOG = LoggerFactory.getLogger(PerformanceMultithreadTest.class);
 
   private static TachyonClient MTC = null;
   private static String MASTER_HOST = null;
-  private static String Dataset_NAME = null;
+  private static String FILE_NAME = null;
   private static int WRITE_BLOCK_SIZE_BYTES = -1;
-  private static int BLOCKS_PER_PARTITION = -1;
+  private static int BLOCKS_PER_FILE = -1;
   private static int THREADS = -1;
-  private static int PARTITIONS = -1;
+  private static int FILES = -1;
   private static boolean DEBUG_MODE = false;
-  private static int PARTITION_BYTES = -1;
-  private static long Dataset_BYTES = -1;
+  private static int FILE_BYTES = -1;
+  private static long FILES_BYTES = -1;
   private static int Tachyon_WRITE_BUFFER_SIZE_BYTES = -1;
   private static String RESULT_PREFIX = null;
 
-  public static void createDataset() {
+  public static void createFiles() {
     long startTimeMs = CommonUtils.getCurrentMs();
-    int datasetId = MTC.createDataset(Dataset_NAME, PARTITIONS);
-    CommonUtils.printTimeTakenMs(startTimeMs, LOG, "user_createDataset with datasetId " + datasetId);
+    for (int k = 0; k < FILES; k ++) {
+      int fileId = MTC.createFile(FILE_NAME + k);
+      CommonUtils.printTimeTakenMs(startTimeMs, LOG, "user_createFiles with fileId " + fileId);
+    }
   }
 
   public static class MemoryCopyWorker extends Thread {
@@ -70,15 +71,15 @@ public class PerformanceMultithreadTest {
           long startTimeMs = System.currentTimeMillis();
 
           if (times == mLeft) {
-            dst = ByteBuffer.allocateDirect(PARTITION_BYTES);
+            dst = ByteBuffer.allocateDirect(FILE_BYTES);
             dst.order(ByteOrder.nativeOrder());
           }
-          for (int k = 0; k < BLOCKS_PER_PARTITION; k ++) {
+          for (int k = 0; k < BLOCKS_PER_FILE; k ++) {
             mBuf.array()[0] = (byte) (k + mWorkerId);
             dst.put(mBuf.array());
           }
           long takenTimeMs = System.currentTimeMillis() - startTimeMs;
-          double result = 1000L * PARTITION_BYTES / takenTimeMs / 1024 / 1024;
+          double result = 1000L * FILE_BYTES / takenTimeMs / 1024 / 1024;
           LOG.info(times + "th MemCopy @ Worker " + mWorkerId + " : " + result + " Mb/sec. Took " +
               takenTimeMs + " ms. Is One To Many: " + mOneToMany);
           dst.clear();
@@ -88,14 +89,14 @@ public class PerformanceMultithreadTest {
           long startTimeMs = System.currentTimeMillis();
           RandomAccessFile file =
               new RandomAccessFile(Config.WORKER_DATA_FOLDER + "1-" + times, "r");
-          ByteBuffer dst = file.getChannel().map(MapMode.READ_ONLY, 0, PARTITION_BYTES);
+          ByteBuffer dst = file.getChannel().map(MapMode.READ_ONLY, 0, FILE_BYTES);
           dst.order(ByteOrder.nativeOrder());
 
-          for (int k = 0; k < BLOCKS_PER_PARTITION; k ++) {
+          for (int k = 0; k < BLOCKS_PER_FILE; k ++) {
             dst.get(mBuf.array());
           }
           long takenTimeMs = System.currentTimeMillis() - startTimeMs;
-          double result = 1000L * PARTITION_BYTES / takenTimeMs / 1024 / 1024;
+          double result = 1000L * FILE_BYTES / takenTimeMs / 1024 / 1024;
           LOG.info(times + "th MemCopy @ Worker " + mWorkerId + " : " + result + " Mb/sec. Took " +
               takenTimeMs + " ms. Is One To Many: " + mOneToMany);
           dst.clear();
@@ -130,9 +131,7 @@ public class PerformanceMultithreadTest {
           new InetSocketAddress(MASTER_HOST, Config.MASTER_PORT));
     }
 
-    public void writeParition() throws IOException, SuspectedPartitionSizeException, TException {
-      Dataset dataset = mTC.getDataset(Dataset_NAME);
-
+    public void writeParition() throws IOException, SuspectedFileSizeException, TException {
       if (DEBUG_MODE) {
         mBuf.flip();
         CommonUtils.printByteBuffer(LOG, mBuf);
@@ -140,20 +139,20 @@ public class PerformanceMultithreadTest {
 
       mBuf.flip();
       for (int pId = mLeft; pId < mRight; pId ++) {
-        Partition partition = dataset.getPartition(pId);
-        partition.open("w");
+        TachyonFile file =mTC.getFile(FILE_NAME + pId);
+        file.open("w");
         long startTimeMs = System.currentTimeMillis();
-        for (int k = 0; k < BLOCKS_PER_PARTITION; k ++) {
+        for (int k = 0; k < BLOCKS_PER_FILE; k ++) {
           mBuf.array()[0] = (byte) (k + mWorkerId);
           try {
-            partition.append(mBuf);
-          } catch (OutOfMemoryForPinDatasetException e) {
+            file.append(mBuf);
+          } catch (OutOfMemoryForPinFileException e) {
             CommonUtils.runtimeException(e);
           }
         }
-        partition.close();
+        file.close();
         long takenTimeMs = System.currentTimeMillis() - startTimeMs + 1;
-        double result = WRITE_BLOCK_SIZE_BYTES * 1000L * BLOCKS_PER_PARTITION / takenTimeMs / 1024 / 1024;
+        double result = WRITE_BLOCK_SIZE_BYTES * 1000L * BLOCKS_PER_FILE / takenTimeMs / 1024 / 1024;
         LOG.info(pId + "th Write @ Worker " + mWorkerId + ": " + result + " Mb/sec.");
       }
     }
@@ -164,7 +163,7 @@ public class PerformanceMultithreadTest {
         writeParition();
       } catch (IOException e) {
         CommonUtils.runtimeException(e);
-      } catch (SuspectedPartitionSizeException e) {
+      } catch (SuspectedFileSizeException e) {
         CommonUtils.runtimeException(e);
       } catch (TException e) {
         CommonUtils.runtimeException(e);
@@ -187,23 +186,21 @@ public class PerformanceMultithreadTest {
           new InetSocketAddress(MASTER_HOST, Config.MASTER_PORT));
     }
 
-    public void readPartition() throws IOException, SuspectedPartitionSizeException, TException {
-      Dataset dataset = mTC.getDataset(Dataset_NAME);
-
+    public void readPartition() throws IOException, SuspectedFileSizeException, TException {
       ByteBuffer buf;
       if (DEBUG_MODE) {
         LOG.info("Verifying the reading data...");
 
         for (int pId = mLeft; pId < mRight; pId ++) {
-          Partition partition = dataset.getPartition(pId);
-          partition.open("r");
+          TachyonFile file = mTC.getFile(FILE_NAME + pId);
+          file.open("r");
 
           long startTimeMs = System.currentTimeMillis();
-          buf = partition.readByteBuffer();
+          buf = file.readByteBuffer();
           IntBuffer intBuf;
           intBuf = buf.asIntBuffer();
           int tmp;
-          for (int i = 0; i < BLOCKS_PER_PARTITION; i ++) {
+          for (int i = 0; i < BLOCKS_PER_FILE; i ++) {
             for (int k = 0; k < WRITE_BLOCK_SIZE_BYTES / 4; k ++) {
               tmp = intBuf.get();
               if ((k == 0 && tmp == (i + mWorkerId)) || (k != 0 && tmp == k)) {
@@ -216,29 +213,29 @@ public class PerformanceMultithreadTest {
           }
 
           long takenTimeMs = System.currentTimeMillis() - startTimeMs + 1;
-          double result = WRITE_BLOCK_SIZE_BYTES * 1000L * BLOCKS_PER_PARTITION / takenTimeMs / 1024 / 1024;
+          double result = WRITE_BLOCK_SIZE_BYTES * 1000L * BLOCKS_PER_FILE / takenTimeMs / 1024 / 1024;
           LOG.info("Read Performance: " + result + " Mb/sec");
           LOG.info("Verifying read data: " + buf + " took " + takenTimeMs + " ms");
         }
       }
 
       for (int pId = mLeft; pId < mRight; pId ++) {
-        Partition partition = dataset.getPartition(pId);
-        partition.open("r");
+        TachyonFile file = mTC.getFile(FILE_NAME + pId);
+        file.open("r");
 
         int[] readArray = new int[WRITE_BLOCK_SIZE_BYTES / 4];
         long startTimeMs = System.currentTimeMillis();
-        buf = partition.readByteBuffer();
+        buf = file.readByteBuffer();
         IntBuffer intBuf;
         intBuf = buf.asIntBuffer();
         int tmp = 0;
-        for (int i = 0; i < BLOCKS_PER_PARTITION; i ++) {
+        for (int i = 0; i < BLOCKS_PER_FILE; i ++) {
           intBuf.get(readArray);
           tmp = readArray[0];
         }
-        partition.close();
+        file.close();
         long takenTimeMs = System.currentTimeMillis() - startTimeMs + 1;
-        double result = 1000L * WRITE_BLOCK_SIZE_BYTES * BLOCKS_PER_PARTITION / takenTimeMs / 1024 / 1024;
+        double result = 1000L * WRITE_BLOCK_SIZE_BYTES * BLOCKS_PER_FILE / takenTimeMs / 1024 / 1024;
 
         LOG.info(pId + "th Read @ Worker " + mWorkerId + " : " + result + 
             " Mb/sec, took " + takenTimeMs + " ms. " + tmp);
@@ -255,7 +252,7 @@ public class PerformanceMultithreadTest {
         readPartition();
       } catch (IOException e) {
         CommonUtils.runtimeException(e);
-      } catch (SuspectedPartitionSizeException e) {
+      } catch (SuspectedFileSizeException e) {
         CommonUtils.runtimeException(e);
       } catch (TException e) {
         CommonUtils.runtimeException(e);
@@ -277,7 +274,7 @@ public class PerformanceMultithreadTest {
     }
 
     MemoryCopyWorker[] WWs = new MemoryCopyWorker[THREADS];
-    int t = PARTITIONS / THREADS;
+    int t = FILES / THREADS;
     for (int thread = 0; thread < THREADS; thread ++) {
       WWs[thread] = new MemoryCopyWorker(
           thread, t * thread, t * (thread + 1), bufs[thread], OneToMany);
@@ -295,7 +292,7 @@ public class PerformanceMultithreadTest {
       }
     }
     long takenTimeMs = System.currentTimeMillis() - startTimeMs;
-    double result = 1000L * Dataset_BYTES / takenTimeMs / 1024 / 1024;
+    double result = 1000L * FILES_BYTES / takenTimeMs / 1024 / 1024;
     LOG.info(RESULT_PREFIX + "Entire memoryCopyTest: " + result + " Mb/sec. Took " + 
         takenTimeMs + " ms.");
   }
@@ -313,7 +310,7 @@ public class PerformanceMultithreadTest {
     }
 
     WriterWorker[] WWs = new WriterWorker[THREADS];
-    int t = PARTITIONS / THREADS;
+    int t = FILES / THREADS;
     for (int thread = 0; thread < THREADS; thread ++) {
       WWs[thread] = new WriterWorker(thread, t * thread, t * (thread + 1), bufs[thread]);
     }
@@ -330,13 +327,13 @@ public class PerformanceMultithreadTest {
       }
     }
     long takenTimeMs = System.currentTimeMillis() - startTimeMs;
-    double result = Dataset_BYTES * 1000L / takenTimeMs / 1024 / 1024;
+    double result = FILES_BYTES * 1000L / takenTimeMs / 1024 / 1024;
     LOG.info(RESULT_PREFIX + "Entire Write: " + result + " Mb/sec. Took " + takenTimeMs + " ms.");
   }
 
   private static void readTest() {
     ReadWorker[] WWs = new ReadWorker[THREADS];
-    int t = PARTITIONS / THREADS;
+    int t = FILES / THREADS;
     for (int thread = 0; thread < THREADS; thread ++) {
       WWs[thread] = new ReadWorker(thread, t * thread, t * (thread + 1));
     }
@@ -353,61 +350,59 @@ public class PerformanceMultithreadTest {
       }
     }
     long takenTimeMs = System.currentTimeMillis() - startTimeMs;
-    double result = Dataset_BYTES * 1000L / takenTimeMs / 1024 / 1024;
+    double result = FILES_BYTES * 1000L / takenTimeMs / 1024 / 1024;
     LOG.info(RESULT_PREFIX + "Entire Read: " + result + " Mb/sec. Took " + takenTimeMs + " ms.");
   }
 
   public static void main(String[] args) throws IOException {
     if (args.length != 9) {
       System.out.println("java -cp target/tachyon-1.0-SNAPSHOT-jar-with-dependencies.jar " +
-          "tachyon.examples.PerformanceTest " + " <MasterIp> <DatasetName> " +
-          "<WriteBlockSizeInBytes> <BlocksPerPartition> <TachyonWriteBufferSize> " +
-          "<DebugMode:true/false> <Threads> <PartitionsPerThread> <Test Case Number>\n" +
-          "1: Dataset Write Test\n" +
-          "2: Dataset Read Test\n" + 
+          "tachyon.examples.PerformanceTest " + " <MasterIp> <FileName> " +
+          "<WriteBlockSizeInBytes> <BlocksPerFile> <TachyonWriteBufferSize> " +
+          "<DebugMode:true/false> <Threads> <FilesPerThread> <Test Case Number>\n" +
+          "1: Files Write Test\n" +
+          "2: Files Read Test\n" + 
           "3: ByteBuffer Memory Copy Test One to Many \n" +
           "4: ByteBuffer Memory Copy Test Many to One \n");
       System.exit(-1);
     }
 
     MASTER_HOST = args[0];
-    Dataset_NAME = args[1];
+    FILE_NAME = args[1];
     WRITE_BLOCK_SIZE_BYTES = Integer.parseInt(args[2]);
-    BLOCKS_PER_PARTITION = Integer.parseInt(args[3]);
+    BLOCKS_PER_FILE = Integer.parseInt(args[3]);
     Tachyon_WRITE_BUFFER_SIZE_BYTES = Integer.parseInt(args[4]);
     if (Tachyon_WRITE_BUFFER_SIZE_BYTES != -1) {
       // TODO 
-//      Config.USER_BUFFER_PER_PARTITION_BYTES = Tachyon_WRITE_BUFFER_SIZE_BYTES;
+      //      Config.USER_BUFFER_PER_PARTITION_BYTES = Tachyon_WRITE_BUFFER_SIZE_BYTES;
     } else {
       Tachyon_WRITE_BUFFER_SIZE_BYTES = Config.USER_BUFFER_PER_PARTITION_BYTES;
     }
     DEBUG_MODE = ("true".equals(args[5]));
     THREADS = Integer.parseInt(args[6]);
-    PARTITIONS = Integer.parseInt(args[7]) * THREADS;
+    FILES = Integer.parseInt(args[7]) * THREADS;
     int testCaseNumber = Integer.parseInt(args[8]);
 
-    PARTITION_BYTES = BLOCKS_PER_PARTITION * WRITE_BLOCK_SIZE_BYTES;
-    Dataset_BYTES = 1L * PARTITION_BYTES * PARTITIONS;
+    FILE_BYTES = BLOCKS_PER_FILE * WRITE_BLOCK_SIZE_BYTES;
+    FILES_BYTES = 1L * FILE_BYTES * FILES;
 
-    RESULT_PREFIX = String.format("Threads %d PartitionsPerThread %d TotalPartitions %d " +
-        "BLOCK_SIZE_KB %d BLOCKS_PER_PARTITION %d PARTITION_SIZE_MB %d Dataset_SIZE_MB %d " +
+    RESULT_PREFIX = String.format("Threads %d FilesPerThread %d TotalFiles %d " +
+        "BLOCK_SIZE_KB %d BLOCKS_PER_FILE %d FILE_SIZE_MB %d FILES_SIZE_MB %d " +
         "Tachyon_WRITE_BUFFER_SIZE_KB %d : ",
-        THREADS, PARTITIONS / THREADS, PARTITIONS, WRITE_BLOCK_SIZE_BYTES / 1024, 
-        BLOCKS_PER_PARTITION, CommonUtils.getMB(PARTITION_BYTES), CommonUtils.getMB(Dataset_BYTES),
+        THREADS, FILES / THREADS, FILES, WRITE_BLOCK_SIZE_BYTES / 1024, 
+        BLOCKS_PER_FILE, CommonUtils.getMB(FILE_BYTES), CommonUtils.getMB(FILES_BYTES),
         Tachyon_WRITE_BUFFER_SIZE_BYTES / 1024);
 
     if (testCaseNumber == 1) {
-      RESULT_PREFIX = "DatasetWriteTest " + RESULT_PREFIX;
+      RESULT_PREFIX = "FilesWriteTest " + RESULT_PREFIX;
       LOG.info(RESULT_PREFIX);
-      MTC = TachyonClient.getClient(
-          new InetSocketAddress(MASTER_HOST, Config.MASTER_PORT));
-      createDataset();
+      MTC = TachyonClient.getClient(new InetSocketAddress(MASTER_HOST, Config.MASTER_PORT));
+      createFiles();
       writeTest();
     } else if (testCaseNumber == 2) {
-      RESULT_PREFIX = "DatasetReadTest " + RESULT_PREFIX;
+      RESULT_PREFIX = "FilesReadTest " + RESULT_PREFIX;
       LOG.info(RESULT_PREFIX);
-      MTC = TachyonClient.getClient(
-          new InetSocketAddress(MASTER_HOST, Config.MASTER_PORT));
+      MTC = TachyonClient.getClient(new InetSocketAddress(MASTER_HOST, Config.MASTER_PORT));
       readTest();
     } else if (testCaseNumber == 3) {
       RESULT_PREFIX = "MemoryCopyTestOneToMany " + RESULT_PREFIX;
