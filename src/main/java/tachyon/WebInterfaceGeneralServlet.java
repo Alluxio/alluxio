dd5035239a03e49acd00f3719ea6beaa49f473f9
haoyuan <haoyuan@eecs.berkeley.edu>
2013-02-21 07:59:24 -0800

    Format the code a bit.

--- src/main/java/tachyon/WebInterfaceGeneralServlet.java
+++ src/main/java/tachyon/WebInterfaceGeneralServlet.java
@@ -11,32 +11,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class WebInterfaceGeneralServlet extends HttpServlet {
-
   MasterInfo mMasterInfo;
 
-	public WebInterfaceGeneralServlet(MasterInfo MI) {
-		mMasterInfo = MI;
-	}
+  public WebInterfaceGeneralServlet(MasterInfo MI) {
+    mMasterInfo = MI;
+  }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-    throws ServletException, IOException{
-      populateValues(request);
-      getServletContext().getRequestDispatcher("/general.jsp").forward(request, response);
-	}
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException{
+    populateValues(request);
+    getServletContext().getRequestDispatcher("/general.jsp").forward(request, response);
+  }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-	  return;
-	}
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    return;
+  }
 
-	// Populates key, value pairs for UI display
-	private void populateValues(HttpServletRequest request) {
-	  int upMillis = (int) (System.currentTimeMillis() - mMasterInfo.getStarttimeMs());
-    String uptime = (upMillis/84600000) + " d "
-    				        + (upMillis/3600000 % 24) + " h "
-    				        + (upMillis/60000 % 60) + " m "
-    				        + (upMillis/1000 % 60) + " s ";
-    request.setAttribute("uptime", uptime);
-      
+  // Populates key, value pairs for UI display
+  private void populateValues(HttpServletRequest request) {
+    long upMillis = System.currentTimeMillis() - mMasterInfo.getStarttimeMs();
+    request.setAttribute("uptime", CommonUtils.convertMillis(upMillis));
+
     DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
     Calendar cal = Calendar.getInstance();
     cal.setTimeInMillis(mMasterInfo.getStarttimeMs());
@@ -46,15 +41,13 @@ public class WebInterfaceGeneralServlet extends HttpServlet {
     String version = Config.VERSION;
     request.setAttribute("version", version);
 
-    String capacity = Double.toString(mMasterInfo.getCapacityBytes()/Math.pow(2,30));
+    String capacity = CommonUtils.getSizeFromBytes(mMasterInfo.getCapacityBytes());
     request.setAttribute("capacity", capacity);
 
-    String usedCapacity = Double.toString(mMasterInfo.getUsedCapacityBytes()/Math.pow(2,30));
+    String usedCapacity = CommonUtils.getSizeFromBytes(mMasterInfo.getUsedCapacityBytes());
     request.setAttribute("usedCapacity", usedCapacity);
 
     String liveWorkerNodes = Integer.toString(mMasterInfo.getWorkerCount());
     request.setAttribute("liveWorkerNodes", liveWorkerNodes);
-
-
-	}
+  }
 }
