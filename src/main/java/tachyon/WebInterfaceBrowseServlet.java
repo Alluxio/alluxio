ffdc35bf59bdaf0091290cd9630ed025c1af7b4a
haoyuan <haoyuan@eecs.berkeley.edu>
2013-02-21 07:59:24 -0800

    Format the code a bit.

--- src/main/java/tachyon/WebInterfaceBrowseServlet.java
+++ src/main/java/tachyon/WebInterfaceBrowseServlet.java
@@ -13,25 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class WebInterfaceBrowseServlet extends HttpServlet {
-
   MasterInfo mMasterInfo;
 
-	public WebInterfaceBrowseServlet(MasterInfo MI) {
-		mMasterInfo = MI;
-	}
+  public WebInterfaceBrowseServlet(MasterInfo MI) {
+    mMasterInfo = MI;
+  }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-    throws ServletException, IOException{
-      String currentPath = request.getParameter("path");
-      if (currentPath == null) {
-      	currentPath = "/";
-      }
-      request.setAttribute("currentPath", currentPath);
-      getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
-	}
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException{
+    String currentPath = request.getParameter("path");
+    if (currentPath == null) {
+      currentPath = "/";
+    }
+    request.setAttribute("currentPath", currentPath);
+    getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
+  }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-	  return;
-	}
-
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    return;
+  }
 }
