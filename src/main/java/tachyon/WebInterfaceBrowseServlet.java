4bf5263a54095cbd69eaff2a10ed1f9ec90a7c97
Calvin Jia <calvin.jia@berkeley.edu>
2013-02-22 02:42:35 -0800

    Changed jetty version to 8.1.9, added JSTL libraries, completed basic UI features

--- src/main/java/tachyon/WebInterfaceBrowseServlet.java
+++ src/main/java/tachyon/WebInterfaceBrowseServlet.java
@@ -1,8 +1,6 @@
 package tachyon;
 
-import java.util.Calendar;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
@@ -12,24 +10,122 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+import tachyon.thrift.ClientFileInfo;
+import tachyon.thrift.FileDoesNotExistException;
+import tachyon.thrift.InvalidPathException;
+
+
 public class WebInterfaceBrowseServlet extends HttpServlet {
   MasterInfo mMasterInfo;
 
   public WebInterfaceBrowseServlet(MasterInfo MI) {
-    mMasterInfo = MI;
+    this.mMasterInfo = MI;
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws ServletException, IOException{
+      throws ServletException, IOException {
+
+    request.setAttribute("invalidPathError", "");
+
+    List<ClientFileInfo> filesInfo = null;
+
     String currentPath = request.getParameter("path");
-    if (currentPath == null) {
+    
+    if (currentPath.isEmpty()) {
       currentPath = "/";
     }
     request.setAttribute("currentPath", currentPath);
+
+    try {
+      this.setPathDirectories(currentPath, request);
+      filesInfo = this.mMasterInfo.getFilesInfo(currentPath);
+    } catch (FileDoesNotExistException fdne) {
+      request.setAttribute("invalidPathError", "Error: not a valid directory");
+      getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
+      return; // For clarity
+    } catch (InvalidPathException ipe) {
+      request.setAttribute("invalidPathError", "Error: not a valid directory");
+      getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
+      return; // For clarity
+    }
+    int index = 0;
+    FileInfo[] fileInfos = new FileInfo[filesInfo.size()];
+    for (ClientFileInfo fileInfo : filesInfo) {
+      fileInfos[index++] = new FileInfo(fileInfo);
+    }
+    request.setAttribute("fileInfos", fileInfos);
+
     getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
   }
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     return;
+  }
+
+  // This function sets the fileinfos for folders that are in the path to the current directory
+
+  private void setPathDirectories(String path, HttpServletRequest request) 
+      throws FileDoesNotExistException, InvalidPathException {
+    String[] splitPath = path.split("/");
+    if (splitPath.length == 0) {
+      request.setAttribute("currentDirectory", new FileInfo(this.mMasterInfo.getFileInfo("/")));
+    }
+    String currentPath = new String("");
+    FileInfo[] pathInfos = new FileInfo[splitPath.length];
+    for (int i = 0; i < splitPath.length; i++) {
+      currentPath = currentPath + splitPath[i] + "/";
+      if (i == splitPath.length - 1) {
+        request.setAttribute("currentDirectory", 
+                            new FileInfo(this.mMasterInfo.getFileInfo(currentPath)));
+      } else {
+        pathInfos[i] = new FileInfo(this.mMasterInfo.getFileInfo(currentPath));
+      }
+    }
+    request.setAttribute("pathInfos", pathInfos);
+  }
+
+  // Class to make referencing file objects more intuitive. Mainly to avoid implicit association
+  // by array indexes.
+
+  public class FileInfo {
+    private String name;
+    private String absolutePath;
+    private long size;
+    private boolean inMemory;
+    private boolean isDirectory;
+
+    private FileInfo(ClientFileInfo fileInfo) {
+      this.name = fileInfo.getName();
+      this.absolutePath = fileInfo.getPath();
+      this.size = fileInfo.getSizeBytes();
+      this.inMemory = fileInfo.inMemory;
+      this.isDirectory = fileInfo.isFolder;
+    }
+
+    public String getName() {
+      if (this.isDirectory) {
+        return this.name + "/";
+      } else {
+        return this.name;
+      }
+    }
+
+    public String getAbsolutePath() {
+      return this.absolutePath;
+    }
+
+    public long getSize() {
+      return this.size;
+    }
+
+    public boolean getInMemory() {
+      return this.inMemory;
+    }
+
+    public boolean getIsDirectory() {
+      return this.isDirectory;
+    }
+
   }
 }
--- src/main/java/tachyon/WebInterfaceGeneralServlet.java
+++ src/main/java/tachyon/WebInterfaceGeneralServlet.java
@@ -1,5 +1,7 @@
 package tachyon;
 
+import java.lang.Comparable;
+import java.util.List;
 import java.util.Calendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,16 +12,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import tachyon.thrift.ClientWorkerInfo;
+
 public class WebInterfaceGeneralServlet extends HttpServlet {
-  MasterInfo mMasterInfo;
+  private MasterInfo mMasterInfo;
 
   public WebInterfaceGeneralServlet(MasterInfo MI) {
-    mMasterInfo = MI;
+    this.mMasterInfo = MI;
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws ServletException, IOException{
-    populateValues(request);
+      throws ServletException, IOException {
+    this.populateValues(request);
     getServletContext().getRequestDispatcher("/general.jsp").forward(request, response);
   }
 
@@ -29,25 +33,79 @@ public class WebInterfaceGeneralServlet extends HttpServlet {
 
   // Populates key, value pairs for UI display
   private void populateValues(HttpServletRequest request) {
-    long upMillis = System.currentTimeMillis() - mMasterInfo.getStarttimeMs();
+    long upMillis = System.currentTimeMillis() - this.mMasterInfo.getStarttimeMs();
     request.setAttribute("uptime", CommonUtils.convertMillis(upMillis));
 
     DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
     Calendar cal = Calendar.getInstance();
-    cal.setTimeInMillis(mMasterInfo.getStarttimeMs());
+    cal.setTimeInMillis(this.mMasterInfo.getStarttimeMs());
     String startTime = formatter.format(cal.getTime());
     request.setAttribute("startTime", startTime);
 
     String version = Config.VERSION;
     request.setAttribute("version", version);
 
-    String capacity = CommonUtils.getSizeFromBytes(mMasterInfo.getCapacityBytes());
+    String capacity = CommonUtils.getSizeFromBytes(this.mMasterInfo.getCapacityBytes());
     request.setAttribute("capacity", capacity);
 
-    String usedCapacity = CommonUtils.getSizeFromBytes(mMasterInfo.getUsedCapacityBytes());
+    String usedCapacity = CommonUtils.getSizeFromBytes(this.mMasterInfo.getUsedCapacityBytes());
     request.setAttribute("usedCapacity", usedCapacity);
 
-    String liveWorkerNodes = Integer.toString(mMasterInfo.getWorkerCount());
+    String liveWorkerNodes = Integer.toString(this.mMasterInfo.getWorkerCount());
     request.setAttribute("liveWorkerNodes", liveWorkerNodes);
+
+    List<ClientWorkerInfo> workerInfos = this.mMasterInfo.getWorkersInfo();
+    int index = 0;
+    NodeInfo[] nodeInfos = new NodeInfo[workerInfos.size()];
+    for (ClientWorkerInfo workerInfo : workerInfos) {
+      nodeInfos[index++] = new NodeInfo(workerInfo);
+    }
+    request.setAttribute("nodeInfos", nodeInfos);
+
+    request.setAttribute("pinlist", this.mMasterInfo.getPinList());
+
+    request.setAttribute("whitelist", this.mMasterInfo.getWhiteList());
+    
+  }
+
+  // Class to make referencing worker nodes more intuitive. Mainly to avoid implicit association
+  // by array indexes.
+
+  public class NodeInfo {
+    private String name;
+    private String lastHeartbeat;
+    private String state;
+    private int percentFreeSpace;
+    private int percentUsedSpace;
+
+    private NodeInfo(ClientWorkerInfo workerInfo) {
+      this.name = workerInfo.getAddress().getMHost();
+      this.lastHeartbeat = Integer.toString(workerInfo.getLastContactSec());
+      this.state = workerInfo.getState();
+      Long usedSpace = 100*workerInfo.getUsedBytes()/workerInfo.getCapacityBytes();
+      this.percentUsedSpace = usedSpace.intValue();
+      this.percentFreeSpace = 100 - percentUsedSpace;
+    }
+
+    public String getName() {
+      return this.name;
+    }
+
+    public String getLastHeartbeat() {
+      return this.lastHeartbeat;
+    }
+
+    public String getState() {
+      return this.state;
+    }
+
+    public int getPercentFreeSpace() {
+      return this.percentFreeSpace;
+    }
+
+    public int getPercentUsedSpace() {
+      return this.percentUsedSpace;
+    }
+
   }
 }
