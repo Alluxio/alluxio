8f44fcab6c4ce92161e313b4170fcb7120e60763
Calvin Jia <calvin.jia@berkeley.edu>
2013-02-22 11:53:27 -0800

    Changed mortbay library to the eclipse version and added glassfish, code styling changes

--- src/main/java/tachyon/WebInterfaceBrowseServlet.java
+++ src/main/java/tachyon/WebInterfaceBrowseServlet.java
@@ -1,37 +1,30 @@
 package tachyon;
-
 import java.util.List;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 
 import tachyon.thrift.ClientFileInfo;
 import tachyon.thrift.FileDoesNotExistException;
 import tachyon.thrift.InvalidPathException;
 
-
 public class WebInterfaceBrowseServlet extends HttpServlet {
   MasterInfo mMasterInfo;
 
   public WebInterfaceBrowseServlet(MasterInfo MI) {
-    this.mMasterInfo = MI;
+    mMasterInfo = MI;
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws ServletException, IOException {
-
     request.setAttribute("invalidPathError", "");
-
     List<ClientFileInfo> filesInfo = null;
-
     String currentPath = request.getParameter("path");
-    
+
     if (currentPath.isEmpty()) {
       currentPath = "/";
     }
@@ -39,7 +32,7 @@ public class WebInterfaceBrowseServlet extends HttpServlet {
 
     try {
       this.setPathDirectories(currentPath, request);
-      filesInfo = this.mMasterInfo.getFilesInfo(currentPath);
+      filesInfo = mMasterInfo.getFilesInfo(currentPath);
     } catch (FileDoesNotExistException fdne) {
       request.setAttribute("invalidPathError", "Error: not a valid directory");
       getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
@@ -49,6 +42,7 @@ public class WebInterfaceBrowseServlet extends HttpServlet {
       getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
       return; // For clarity
     }
+
     int index = 0;
     FileInfo[] fileInfos = new FileInfo[filesInfo.size()];
     for (ClientFileInfo fileInfo : filesInfo) {
@@ -59,17 +53,18 @@ public class WebInterfaceBrowseServlet extends HttpServlet {
     getServletContext().getRequestDispatcher("/browse.jsp").forward(request, response);
   }
 
+
+
   protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     return;
   }
 
   // This function sets the fileinfos for folders that are in the path to the current directory
-
   private void setPathDirectories(String path, HttpServletRequest request) 
       throws FileDoesNotExistException, InvalidPathException {
     String[] splitPath = path.split("/");
     if (splitPath.length == 0) {
-      request.setAttribute("currentDirectory", new FileInfo(this.mMasterInfo.getFileInfo("/")));
+      request.setAttribute("currentDirectory", new FileInfo(mMasterInfo.getFileInfo("/")));
     }
     String currentPath = new String("");
     FileInfo[] pathInfos = new FileInfo[splitPath.length];
@@ -77,9 +72,9 @@ public class WebInterfaceBrowseServlet extends HttpServlet {
       currentPath = currentPath + splitPath[i] + "/";
       if (i == splitPath.length - 1) {
         request.setAttribute("currentDirectory", 
-                            new FileInfo(this.mMasterInfo.getFileInfo(currentPath)));
+                            new FileInfo(mMasterInfo.getFileInfo(currentPath)));
       } else {
-        pathInfos[i] = new FileInfo(this.mMasterInfo.getFileInfo(currentPath));
+        pathInfos[i] = new FileInfo(mMasterInfo.getFileInfo(currentPath));
       }
     }
     request.setAttribute("pathInfos", pathInfos);
@@ -87,45 +82,43 @@ public class WebInterfaceBrowseServlet extends HttpServlet {
 
   // Class to make referencing file objects more intuitive. Mainly to avoid implicit association
   // by array indexes.
-
   public class FileInfo {
-    private String name;
-    private String absolutePath;
-    private long size;
-    private boolean inMemory;
-    private boolean isDirectory;
+    private String mName;
+    private String mAbsolutePath;
+    private long mSize;
+    private boolean mInMemory;
+    private boolean mIsDirectory;
 
     private FileInfo(ClientFileInfo fileInfo) {
-      this.name = fileInfo.getName();
-      this.absolutePath = fileInfo.getPath();
-      this.size = fileInfo.getSizeBytes();
-      this.inMemory = fileInfo.inMemory;
-      this.isDirectory = fileInfo.isFolder;
+      mName = fileInfo.getName();
+      mAbsolutePath = fileInfo.getPath();
+      mSize = fileInfo.getSizeBytes();
+      mInMemory = fileInfo.inMemory;
+      mIsDirectory = fileInfo.isFolder;
     }
 
     public String getName() {
-      if (this.isDirectory) {
-        return this.name + "/";
+      if (mIsDirectory) {
+        return mName + "/";
       } else {
-        return this.name;
+        return mName;
       }
     }
 
     public String getAbsolutePath() {
-      return this.absolutePath;
+      return mAbsolutePath;
     }
 
     public long getSize() {
-      return this.size;
+      return mSize;
     }
 
     public boolean getInMemory() {
-      return this.inMemory;
+      return mInMemory;
     }
 
     public boolean getIsDirectory() {
-      return this.isDirectory;
+      return mIsDirectory;
     }
-
   }
 }
