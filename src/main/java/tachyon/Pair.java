4816f12b07f4c2552451109531dce4961620cb64
Vamsi Chitters <vamsi.kc@gmail.com>
2013-02-23 15:44:26 -0800

    Added javadoc documentation to Pair.java as a sample for documentation procedures

--- src/main/java/tachyon/Pair.java
+++ src/main/java/tachyon/Pair.java
@@ -1,19 +1,38 @@
 package tachyon;
 
+/**
+ * A pair representation defined by two elements (of type First and type Second).  
+ * @author Vamsi
+ * 
+ * 
+ * @param <First> the first element of the Pair. 
+ * @param <Second> the second element of the Pair.
+ */
 public class Pair<First, Second> {
-  private First mFirst;
-  private Second mSecond;
+	private First mFirst;
+	private Second mSecond;
 
-  public Pair(First first, Second second) {
-    mFirst = first;
-    mSecond = second;
-  }
+	/**
+	 * Constructs and initializes a Pair specified by the two input elements.
+	 * @param first the first element of the pair (of type First)
+	 * @param second the second element of the pair (of type Second)
+	 */
+	public Pair(First first, Second second) {
+		mFirst = first;
+		mSecond = second;
+	}
 
-  public First getFirst() {
-    return mFirst;
-  }
+	/**
+	 * @return the first element of the pair.
+	 */
+	public First getFirst() {
+		return mFirst;
+	}
 
-  public Second getSecond() {
-    return mSecond;
-  }
+	/**
+	 * @return the second element of the pair.
+	 */
+	public Second getSecond() {
+		return mSecond;
+	}
 }
