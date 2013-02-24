4de4d88b9986996ce9c82987e8b978f48bd2e660
haoyuan <haoyuan@eecs.berkeley.edu>
2013-02-23 16:18:06 -0800

    Clean the code.

--- src/main/java/tachyon/Pair.java
+++ src/main/java/tachyon/Pair.java
@@ -1,38 +1,38 @@
 package tachyon;
 
 /**
- * A pair representation defined by two elements (of type First and type Second).  
+ * A pair representation defined by two elements (of type First and type Second).
+ *   
  * @author Vamsi
- * 
  * 
  * @param <First> the first element of the Pair. 
  * @param <Second> the second element of the Pair.
  */
 public class Pair<First, Second> {
-	private First mFirst;
-	private Second mSecond;
+  private First mFirst;
+  private Second mSecond;
 
-	/**
-	 * Constructs and initializes a Pair specified by the two input elements.
-	 * @param first the first element of the pair (of type First)
-	 * @param second the second element of the pair (of type Second)
-	 */
-	public Pair(First first, Second second) {
-		mFirst = first;
-		mSecond = second;
-	}
+  /**
+   * Constructs and initializes a Pair specified by the two input elements.
+   * @param first the first element of the pair (of type First)
+   * @param second the second element of the pair (of type Second)
+   */
+  public Pair(First first, Second second) {
+    mFirst = first;
+    mSecond = second;
+  }
 
-	/**
-	 * @return the first element of the pair.
-	 */
-	public First getFirst() {
-		return mFirst;
-	}
+  /**
+   * @return the first element of the pair.
+   */
+  public First getFirst() {
+    return mFirst;
+  }
 
-	/**
-	 * @return the second element of the pair.
-	 */
-	public Second getSecond() {
-		return mSecond;
-	}
+  /**
+   * @return the second element of the pair.
+   */
+  public Second getSecond() {
+    return mSecond;
+  }
 }
