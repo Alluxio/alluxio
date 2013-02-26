cf3f5a87c87915d538bf96055cc5d037e8b101f5
haoyuan <haoyuan@eecs.berkeley.edu>
2013-02-25 17:00:54 -0800

    commit the missing file in the previous commit

---
+++ src/main/java/tachyon/UIWebServer.java
@@ -0,0 +1,61 @@
+package tachyon;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UIWebServer {
+  private final Logger LOG = LoggerFactory.getLogger(UIWebServer.class);
+
+  private Server mServer;
+  private String mServerName;
+  private InetSocketAddress mAddress;
+
+  public UIWebServer(String serverName, InetSocketAddress address, MasterInfo masterInfo) {
+    mAddress = address;
+    mServerName = serverName;
+    mServer = new Server(mAddress);
+
+    WebAppContext webappcontext = new WebAppContext();
+
+    webappcontext.setContextPath("/");
+    File warPath = new File(new File("").getAbsolutePath(), "src/main/java/tachyon/webapps");
+    webappcontext.setWar(warPath.getAbsolutePath());
+    HandlerList handlers = new HandlerList();
+    webappcontext.addServlet(
+        new ServletHolder(new WebInterfaceGeneralServlet(masterInfo)), "/home");
+    webappcontext.addServlet(
+        new ServletHolder(new WebInterfaceBrowseServlet(masterInfo)), "/browse");
+    webappcontext.addServlet(
+        new ServletHolder(new WebInterfaceMemoryServlet(masterInfo)), "/memory");
+
+    handlers.setHandlers(new Handler[] { webappcontext, new DefaultHandler() });
+    mServer.setHandler(handlers);
+  }
+
+  public void setHandler(AbstractHandler handler) {
+    mServer.setHandler(handler);
+  }
+
+  public void startWebServer() {
+    try {
+      mServer.start();
+      LOG.info(mServerName + " started @ " + mAddress);
+    } catch (Exception e) {
+      CommonUtils.runtimeException(e);
+    }
+  }
+
+  public void shutdownWebServer() throws Exception {
+    mServer.stop();
+  }
+}
