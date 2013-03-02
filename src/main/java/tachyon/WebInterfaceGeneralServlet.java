8a4200a796e62ded12d1e8074b2b1c195693d0d0
Calvin Jia <calvin.jia@berkeley.edu>
2013-03-01 16:34:52 -0800

    added uptime to worker nodes, removed size from directories

--- src/main/java/tachyon/WebInterfaceGeneralServlet.java
+++ src/main/java/tachyon/WebInterfaceGeneralServlet.java
@@ -25,6 +25,7 @@ public class WebInterfaceGeneralServlet extends HttpServlet {
     private final String STATE;
     private final int FREE_SPACE_PERCENT;
     private final int USED_SPACE_PERCENT;
+    private final String UPTIME;
 
     private NodeInfo(ClientWorkerInfo workerInfo) {
       NAME = workerInfo.getAddress().getMHost();
@@ -32,6 +33,8 @@ public class WebInterfaceGeneralServlet extends HttpServlet {
       STATE = workerInfo.getState();
       USED_SPACE_PERCENT = (int) (100L * workerInfo.getUsedBytes() / workerInfo.getCapacityBytes());
       FREE_SPACE_PERCENT = 100 - USED_SPACE_PERCENT;
+      UPTIME = CommonUtils.convertMsToClockTime(
+          System.currentTimeMillis() - workerInfo.getStarttimeMs());
     }
 
     public String getName() {
@@ -53,6 +56,10 @@ public class WebInterfaceGeneralServlet extends HttpServlet {
     public int getUsedSpacePercent() {
       return USED_SPACE_PERCENT;
     }
+
+    public String getUptime() {
+      return UPTIME;
+    } 
   }
 
   public WebInterfaceGeneralServlet(MasterInfo masterInfo) {
