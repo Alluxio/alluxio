964fd368f519d68780219a63b6f956218f60d545
haoyuan <haoyuan@eecs.berkeley.edu>
2013-04-05 10:50:20 -0700

    format code

--- src/test/java/tachyon/client/TachyonClientTest.java
+++ src/test/java/tachyon/client/TachyonClientTest.java
@@ -15,61 +15,61 @@ import tachyon.thrift.InvalidPathException;
  * Unit tests on TachyonClient.
  */
 public class TachyonClientTest {
-  LocalTachyonCluster ltc = null;
-  TachyonClient client = null;
+  LocalTachyonCluster mLocalTachyonCluster = null;
+  TachyonClient mClient = null;
 
   @Before
   public final void before() throws IOException {
     System.setProperty("tachyon.user.quota.unit.bytes", "1000");
-    ltc = new LocalTachyonCluster(5555, 6666, 1000);
-    ltc.start();
-    client = ltc.getClient();
+    mLocalTachyonCluster = new LocalTachyonCluster(5555, 6666, 1000);
+    mLocalTachyonCluster.start();
+    mClient = mLocalTachyonCluster.getClient();
   }
 
   @After
   public final void after() throws Exception {
-    ltc.stop();
+    mLocalTachyonCluster.stop();
     System.clearProperty("tachyon.user.quota.unit.bytes");
   }
 
   @Test
   public void createFileTest() throws Exception {
-    int fileId = client.createFile("/root/testFile1");
+    int fileId = mClient.createFile("/root/testFile1");
     Assert.assertEquals(3, fileId);
-    fileId = client.createFile("/root/testFile2");
+    fileId = mClient.createFile("/root/testFile2");
     Assert.assertEquals(4, fileId);
-    fileId = client.createFile("/root/testFile3");
+    fileId = mClient.createFile("/root/testFile3");
     Assert.assertEquals(5, fileId);
   }
 
   @Test(expected = InvalidPathException.class)
   public void createFileWithInvalidPathExceptionTest() 
       throws InvalidPathException, FileAlreadyExistException {
-    client.createFile("root/testFile1");
+    mClient.createFile("root/testFile1");
   }
 
   @Test(expected = FileAlreadyExistException.class)
   public void createFileWithFileAlreadyExistExceptionTest() 
       throws InvalidPathException, FileAlreadyExistException {
-    int fileId = client.createFile("/root/testFile1");
+    int fileId = mClient.createFile("/root/testFile1");
     Assert.assertEquals(3, fileId);
-    fileId = client.createFile("/root/testFile1");
+    fileId = mClient.createFile("/root/testFile1");
   }
 
   @Test
   public void deleteFileTest() 
       throws InvalidPathException, FileAlreadyExistException {
-    int fileId = client.createFile("/root/testFile1");
-    client.deleteFile(fileId);
-    Assert.assertFalse(client.existFile("/root/testFile1"));
+    int fileId = mClient.createFile("/root/testFile1");
+    mClient.deleteFile(fileId);
+    Assert.assertFalse(mClient.existFile("/root/testFile1"));
   }
 
   @Test
   public void renameFileTest()
       throws InvalidPathException, FileAlreadyExistException {
-    int fileId = client.createFile("/root/testFile1");
-    client.renameFile("/root/testFile1", "/root/testFile2");
-    Assert.assertEquals(fileId, client.getFileId("/root/testFile2"));
-    Assert.assertFalse(client.existFile("/root/testFile1"));
+    int fileId = mClient.createFile("/root/testFile1");
+    mClient.renameFile("/root/testFile1", "/root/testFile2");
+    Assert.assertEquals(fileId, mClient.getFileId("/root/testFile2"));
+    Assert.assertFalse(mClient.existFile("/root/testFile1"));
   }
 }
--- src/test/java/tachyon/command/TFsShellTest.java
+++ src/test/java/tachyon/command/TFsShellTest.java
@@ -1,6 +1,8 @@
 package tachyon.command;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -18,28 +20,29 @@ import tachyon.thrift.InvalidPathException;
  * Unit tests on TFsShell.
  */
 public class TFsShellTest {
-  LocalTachyonCluster ltc = null;
-  TachyonClient client = null;
-  TFsShell tFsShell = null;
+  LocalTachyonCluster mLocalTachyonCluster = null;
+  TachyonClient mClient = null;
+  TFsShell mFsShell = null;
 
   @Before
   public final void before() throws IOException {
     System.setProperty("tachyon.user.quota.unit.bytes", "1000");
-    ltc = new LocalTachyonCluster(5555, 6666, 1000);
-    ltc.start();
-    client = ltc.getClient();
-    tFsShell = new TFsShell();
+    mLocalTachyonCluster = new LocalTachyonCluster(5555, 6666, 1000);
+    mLocalTachyonCluster.start();
+    mClient = mLocalTachyonCluster.getClient();
+    mFsShell = new TFsShell();
   }
 
   @After
   public final void after() throws Exception {
-    ltc.stop();
+    mLocalTachyonCluster.stop();
     System.clearProperty("tachyon.user.quota.unit.bytes");
   }
 
   @Test
-  public void mkdirTest() throws InvalidPathException, FileAlreadyExistException, TException {
-    tFsShell.mkdir(new String[]{"mkdir", "tachyon://saasbook:5555/root/testFile1"});
-    Assert.assertTrue(client.getFile("/root/testFile1") != null);
+  public void mkdirTest() throws InvalidPathException, FileAlreadyExistException, TException, UnknownHostException {
+    mFsShell.mkdir(new String[]{"mkdir", "tachyon://" + 
+        InetAddress.getLocalHost().getCanonicalHostName() + ":5555/root/testFile1"});
+    Assert.assertTrue(mClient.getFile("/root/testFile1") != null);
   }
 }
