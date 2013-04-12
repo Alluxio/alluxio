e526986a2655dd494b22df902ae1013f3f9d2a80
haoyuan <haoyuan@eecs.berkeley.edu>
2013-04-12 13:32:34 -0700

    Autofill version number in each compile

--- src/main/java/tachyon/Constants.java
+++ src/main/java/tachyon/Constants.java
@@ -8,7 +8,6 @@ public class Constants {
   public static final int MB = KB * 1024;
   public static final long GB = MB * 1024L;
   public static final long TB = GB * 1024L;
-  public static final long TWO_32 = 1L << 32;
 
   public static final long SECOND_MS = 1000;
   public static final long MINUTE_MS = SECOND_MS * 60;
