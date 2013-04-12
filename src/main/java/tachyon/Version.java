c0300b48ff3d71bb15022badaa40301e2c367244
haoyuan <haoyuan@eecs.berkeley.edu>
2013-04-12 13:44:38 -0700

    only the version is auto-filled by mvn.

--- src/main/java/tachyon/Version.java
+++ src/main/java/tachyon/Version.java
@@ -4,5 +4,5 @@ package tachyon;
  * This class is auto generated in each compile.
  */
 public class Version {
-  public static final String VERSION = "@pomversion@";
+  public static final String VERSION = "0.2.1-SNAPSHOT";
 }
