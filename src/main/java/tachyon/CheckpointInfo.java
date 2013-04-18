1d29981f60a4760eb90f6546481d19c5615642f8
haoyuan <haoyuan@eecs.berkeley.edu>
2013-04-18 14:09:22 -0700

    1. Change name Partition to File in DataServerMessage. 2. Add equal method to checkpointinfo. 3. fix a bug in dataserver.

--- src/main/java/tachyon/CheckpointInfo.java
+++ src/main/java/tachyon/CheckpointInfo.java
@@ -5,12 +5,30 @@ import java.io.Serializable;
 /**
  * Class to store global counter in master write head log.
  */
-public class CheckpointInfo implements Serializable {
+public class CheckpointInfo implements Serializable, Comparable<CheckpointInfo> {
   private static final long serialVersionUID = -8873733429025713755L;
 
   public final int COUNTER_INODE;
 
   public CheckpointInfo(int inodeCounter) {
     COUNTER_INODE = inodeCounter;
+  }
+
+  @Override
+  public synchronized int compareTo(CheckpointInfo o) {
+    return COUNTER_INODE - o.COUNTER_INODE;
+  }
+
+  @Override
+  public synchronized boolean equals(Object o) {
+    if (!(o instanceof CheckpointInfo)) {
+      return false;
+    }
+    return COUNTER_INODE == ((CheckpointInfo)o).COUNTER_INODE;
+  }
+
+  @Override
+  public synchronized int hashCode() {
+    return COUNTER_INODE;
   }
 }
