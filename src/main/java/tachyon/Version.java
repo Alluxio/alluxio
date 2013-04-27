0560761e01ae58c4d4448e2cf8acba0557702d54
haoyuan <haoyuan@eecs.berkeley.edu>
2013-04-26 20:01:57 -0700

    bump the version for next development iteration

--- src/main/java/tachyon/Version.java
+++ src/main/java/tachyon/Version.java
@@ -4,5 +4,5 @@ package tachyon;
  * The version of the current build. This class is auto generated in each compile.
  */
 public class Version {
-  public static final String VERSION = "0.2.1-SNAPSHOT";
+  public static final String VERSION = "0.2.2-SNAPSHOT";
 }
