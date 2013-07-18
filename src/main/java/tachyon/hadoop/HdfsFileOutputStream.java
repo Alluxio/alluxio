a48089a4062e1b0b402d34a7dae69ca6988e1057
haoyuan <haoyuan@eecs.berkeley.edu>
2013-07-17 18:14:46 -0700

    fix compile for hadoop 2

--- src/main/java/tachyon/hadoop/HdfsFileOutputStream.java
+++ src/main/java/tachyon/hadoop/HdfsFileOutputStream.java
@@ -3,22 +3,22 @@ package tachyon.hadoop;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import org.apache.hadoop.fs.Syncable;
+//import org.apache.hadoop.fs.Syncable;
 
 import tachyon.client.TachyonFile;
 import tachyon.client.WriteType;
 
-public class HdfsFileOutputStream extends OutputStream implements Syncable {
+public class HdfsFileOutputStream extends OutputStream { // implements Syncable {
 
   HdfsFileOutputStream(TachyonFile file, WriteType writeType) {
     // TODO Auto-generated constructor stub
   }
 
-  @Override
-  public void sync() throws IOException {
-    // TODO Auto-generated method stub
-    
-  }
+//  @Override
+//  public void sync() throws IOException {
+//    // TODO Auto-generated method stub
+//    
+//  }
 
   @Override
   public void write(int b) throws IOException {
