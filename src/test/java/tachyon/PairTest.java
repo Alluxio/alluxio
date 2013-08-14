ef41de8c23e5f81275a31f210f89dde386a26e73
Bill Zhao <tauneutrino@gmail.com>
2013-08-14 16:02:51 -0700

    test all combination of defined pairs

--- src/test/java/tachyon/PairTest.java
+++ src/test/java/tachyon/PairTest.java
@@ -24,12 +24,14 @@ public class PairTest {
     obj[4] = boolType;
     obj[5] = charType;
 
-    for (int k = 0; k < 5; k ++) {
-      Pair tPair = new Pair(obj[k], obj[k + 1]);
-      Assert.assertEquals(obj[k], tPair.getFirst());
-      Assert.assertEquals(obj[k + 1], tPair.getSecond());
-      Assert.assertNotSame(obj[k + 1], tPair.getFirst());
-      Assert.assertNotSame(obj[k], tPair.getSecond());
+    for (int j = 0; j < obj.length - 1; j ++) {
+      for (int k = j + 1; k < obj.length; k ++) {
+        Pair tPair = new Pair(obj[j], obj[k]);
+        Assert.assertEquals(obj[j], tPair.getFirst());
+        Assert.assertEquals(obj[k], tPair.getSecond());
+        Assert.assertNotSame(obj[k], tPair.getFirst());
+        Assert.assertNotSame(obj[j], tPair.getSecond());
+      }
     }
   }
 }
