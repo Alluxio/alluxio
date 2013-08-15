ec0bb389d400bbf84a866bb32a75b27f0dc5c3bc
haoyuan <haoyuan@eecs.berkeley.edu>
2013-08-15 10:07:36 -0700

    add util to i/o buf.

--- src/main/java/tachyon/io/Utils.java
+++ src/main/java/tachyon/io/Utils.java
@@ -3,6 +3,7 @@ package tachyon.io;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class Utils {
   public static void writeString(String str, DataOutputStream os) throws IOException {
@@ -29,5 +30,29 @@ public class Utils {
     }
     return new String(chars);
 
+  }
+
+  public static void writeByteBuffer(ByteBuffer buf, DataOutputStream os) throws IOException {
+    if (buf == null) {
+      os.writeInt(-1);
+      return;
+    }
+    int len = buf.limit() - buf.position();
+    os.writeInt(len);
+    os.write(buf.array(), buf.position(), len);
+  }
+
+  public static ByteBuffer ReadByteBuffer(DataInputStream is) throws IOException {
+    int len = is.readInt();
+    if (len == -1) {
+      return null;
+    }
+
+    byte[] arr = new byte[len];
+    for (int k = 0; k < len ; k ++) {
+      arr[k] = is.readByte();
+    }
+
+    return ByteBuffer.wrap(arr);
   }
 }
