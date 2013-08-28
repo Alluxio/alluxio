fd624b65f8f52865f786b3d34b4b49166d4bad28
Haoyuan Li <haoyuan@cs.berkeley.edu>
2013-08-28 13:59:28 -0700

    initial unsafe

---
+++ src/main/java/tachyon/io/UnsafeByteBufferWriter.java
@@ -0,0 +1,66 @@
+package tachyon.io;
+
+import java.nio.ByteBuffer;
+
+public class UnsafeByteBufferWriter extends ByteBufferWriter {
+
+  public UnsafeByteBufferWriter(ByteBuffer buf) {
+    super(buf);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public ByteBuffer put(Byte b) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer put(byte[] src) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer put(byte[] src, int offset, int length) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer putChar(char value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer putDouble(double value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer putFloat(float value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer putInt(int value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer putLong(long value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ByteBuffer putShort(short value) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
