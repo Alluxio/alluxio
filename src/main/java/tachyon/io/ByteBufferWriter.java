f559eb8dd0e2f70424b118fddcfd347fbbf0ebd6
Haoyuan Li <haoyuan@cs.berkeley.edu>
2013-08-27 18:05:27 -0700

    java reader/writer

---
+++ src/main/java/tachyon/io/ByteBufferWriter.java
@@ -0,0 +1,43 @@
+package tachyon.io;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public abstract class ByteBufferWriter {
+  protected ByteBuffer mBuf;
+
+  public ByteBufferWriter(ByteBuffer buf) {
+    mBuf = buf;
+  }
+
+  public abstract ByteBuffer put(Byte b);
+
+  public abstract ByteBuffer put(byte[] src);
+
+  public abstract ByteBuffer put(byte[] src, int offset, int length);
+
+  public abstract ByteBuffer putChar(char value);
+
+  public abstract ByteBuffer putDouble(double value);
+
+  public abstract ByteBuffer putFloat(float value);
+
+  public abstract ByteBuffer putInt(int value);
+
+  public abstract ByteBuffer putLong(long value);
+
+  public abstract ByteBuffer putShort(short value);
+
+  public ByteOrder order() {
+    return mBuf.order();
+  }
+
+  public ByteBuffer order(ByteOrder bo) {
+    return mBuf.order(bo);
+  }
+  
+  public ByteBuffer generateByteBuffer() {
+    mBuf.flip();
+    return mBuf;
+  }
+}
---
+++ src/main/java/tachyon/io/JavaByteBufferWriter.java
@@ -0,0 +1,55 @@
+package tachyon.io;
+
+import java.nio.ByteBuffer;
+
+public class JavaByteBufferWriter extends ByteBufferWriter {
+
+  public JavaByteBufferWriter(ByteBuffer buf) {
+    super(buf);
+  }
+
+  @Override
+  public ByteBuffer put(Byte b) {
+    return mBuf.put(b);
+  }
+
+  @Override
+  public ByteBuffer put(byte[] src) {
+    return mBuf.put(src);
+  }
+
+  @Override
+  public ByteBuffer put(byte[] src, int offset, int length) {
+    return mBuf.put(src, offset, length);
+  }
+
+  @Override
+  public ByteBuffer putChar(char value) {
+    return mBuf.putChar(value);
+  }
+
+  @Override
+  public ByteBuffer putDouble(double value) {
+    return mBuf.putDouble(value);
+  }
+
+  @Override
+  public ByteBuffer putFloat(float value) {
+    return mBuf.putFloat(value);
+  }
+
+  @Override
+  public ByteBuffer putInt(int value) {
+    return mBuf.putInt(value);
+  }
+
+  @Override
+  public ByteBuffer putLong(long value) {
+    return mBuf.putLong(value);
+  }
+
+  @Override
+  public ByteBuffer putShort(short value) {
+    return mBuf.putShort(value);
+  }
+}
