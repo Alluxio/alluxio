db85c1a4068188353363cb79b504abccb3f8183a
Haoyuan Li <haoyuan@cs.berkeley.edu>
2013-11-13 18:56:25 -0800

    fix scripts in linux.

--- libexec/tachyon-config.sh
+++ libexec/tachyon-config.sh
@@ -17,9 +17,8 @@
 # limitations under the License.
 #
 
-# Included in all the Tachyon scripts with source command
-# should not be executable directly
-# also should not be passed any arguments, since we need original $*
+# Included in all the Tachyon scripts with source command should not be executable directly also
+# should not be passed any arguments, since we need original $*
 
 # resolve links - $0 may be a softlink
 this="${BASH_SOURCE-$0}"
@@ -33,13 +32,11 @@ script=`basename "$this"`
 config_bin=`cd "$config_bin"; pwd`
 this="$config_bin/$script"
 
-# Allow for a script which overrides the default settings for system
-# integration folks.
+# Allow for a script which overrides the default settings for system integration folks.
 [ -f "$common_bin/tachyon-layout.sh" ] && . "$common_bin/tachyon-layout.sh"
 
-# this will set the default installation for a tarball installation
-# while os distributors can create their own tachyon-layout.sh file
-# to set system installation locations.
+# This will set the default installation for a tarball installation while os distributors can create
+# their own tachyon-layout.sh file to set system installation locations.
 if [ -z "$TACHYON_SYSTEM_INSTALLATION" ]; then
   export TACHYON_PREFIX=`dirname "$this"`/..
   export TACHYON_HOME=${TACHYON_PREFIX}
