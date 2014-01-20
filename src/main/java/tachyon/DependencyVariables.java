6c0c7dfd5b966633148e0380bdfc4e58b9c0968d
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-01-20 07:00:31 -0800

    improve ck example.

--- src/main/java/tachyon/DependencyVariables.java
+++ src/main/java/tachyon/DependencyVariables.java
@@ -1,7 +1,6 @@
 package tachyon;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Hashtable;
 
 import tachyon.conf.MasterConf;
 
@@ -9,5 +8,5 @@ public class DependencyVariables {
   public static final String MASTER_HOSTNAME = MasterConf.get().HOSTNAME;
   public static final int MASTER_PORT = MasterConf.get().PORT;
 
-  public static Map<String, String> sVariables = new HashMap<String, String>();
+  public static Hashtable<String, String> sVariables = new Hashtable<String, String>();
 }
