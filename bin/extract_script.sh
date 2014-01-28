35a3414ff061896fce92dd6522d427da6721cc41
Anurag Khandelwal <anuragk@berkeley.edu>
2014-01-27 17:39:55 -0800

    Cleaned master, bug fixes to RemoteBlockInStream.java:seek

--- bin/extract_script.sh
+++
@@ -1,5 +0,0 @@
-for (( i=512; i<=1048576; i*=2 ))
-do
-  echo "i = $1"
-  cat log_$i | grep 'Throughput' | awk '{ print($3) }' >> thputs
-done
--- bin/read_script.sh
+++
@@ -1,6 +0,0 @@
-for (( i=512; i<=1048576; i*=2 ))
-do
-  NumReads=$(((209715200/$i)))
-  echo "NumReads = $NumReads"
-  $TACHYON_HOME/bin/succinct runTest ReadData /data $NumReads $i > log_$i
-done
