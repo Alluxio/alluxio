cdf09affa490e617190060402eade8daa292f812
Timothy St. Clair <tstclair@redhat.com>
2014-01-30 21:18:06 -0600

    Fix for a null pointer exception

--- src/main/java/tachyon/client/FileOutStream.java
+++ src/main/java/tachyon/client/FileOutStream.java
@@ -134,10 +134,10 @@ public class FileOutStream extends OutStream {
         int tLen = len;
         int tOff = off;
         while (tLen > 0) {
-          if (mCurrentBlockLeftByte == 0) {
+          if (mCurrentBlockLeftByte <= 0 || mCurrentBlockOutStream == null ) {
             getNextBlock();
           }
-          if (mCurrentBlockLeftByte > tLen) {
+          if (mCurrentBlockLeftByte >= tLen) {
             mCurrentBlockOutStream.write(b, tOff, tLen);
             mCurrentBlockLeftByte -= tLen;
             mCachedBytes += tLen;
