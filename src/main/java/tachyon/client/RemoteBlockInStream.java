5616b7a6badcd2ce625580db4667b0b12bf03aea
Anurag Khandelwal <anuragk@berkeley.edu>
2014-02-02 01:00:36 -0800

    Modified fix to RemoteBlockInStream.java:seek

--- src/main/java/tachyon/client/RemoteBlockInStream.java
+++ src/main/java/tachyon/client/RemoteBlockInStream.java
@@ -352,13 +352,13 @@ public class RemoteBlockInStream extends BlockInStream {
     }
     mRecache = false;
     if (mCurrentBuffer != null) {
-      long length = BUFFER_SIZE;
       mReadByte = pos;
-      if(!(pos < mBufferStartPosition + length && pos >= mBufferStartPosition)) {
-        mBufferStartPosition = ((int)pos / length) * length;
-        mCurrentBuffer = readRemoteByteBuffer(mBlockInfo, mBufferStartPosition, length);
+      if(mBufferStartPosition <= pos && pos < mBufferStartPosition + mCurrentBuffer.limit()) {
+        mCurrentBuffer.position((int) (pos - mBufferStartPosition));
+      } else {
+        mBufferStartPosition = pos;
+        updateCurrentBuffer();
       }
-      mCurrentBuffer.position((int) (pos % length));
     } else {
       if (mCheckpointInputStream != null) {
         mCheckpointInputStream.close();
