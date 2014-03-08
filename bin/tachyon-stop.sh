b59562b40a8d3738ff5f9018da91d19226db3a01
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-03-08 13:07:18 -0800

    rename tachyon master and worker

--- bin/tachyon-stop.sh
+++ bin/tachyon-stop.sh
@@ -26,7 +26,7 @@ fi
 
 bin=`cd "$( dirname "$0" )"; pwd`
 
-$bin/tachyon killAll tachyon.master.Master
-$bin/tachyon killAll tachyon.worker.Worker
+$bin/tachyon killAll tachyon.master.TachyonMaster
+$bin/tachyon killAll tachyon.worker.TachyonWorker
 
-$bin/tachyon-slaves.sh $bin/tachyon killAll tachyon.worker.Worker
+$bin/tachyon-slaves.sh $bin/tachyon killAll tachyon.worker.TachyonWorker
