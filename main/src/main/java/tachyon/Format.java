a2863d9cfa293c32097a30c8b8e4988a9623d1d1
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-03-25 11:14:29 -0700

    only touch the file if format successfully

--- main/src/main/java/tachyon/Format.java
+++ main/src/main/java/tachyon/Format.java
@@ -1,13 +1,11 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
+ * contributor license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,7 +45,6 @@ public class Format {
       if (!ufs.mkdirs(folder, true)) {
         System.out.println("Failed to create JOURNAL_FOLDER: " + folder);
       }
-      CommonUtils.touch(folder + "/" + masterConf.FORMAT_FILE_PREFIX + System.currentTimeMillis());
 
       CommonConf commonConf = CommonConf.get();
       folder = commonConf.UNDERFS_DATA_FOLDER;
@@ -64,6 +61,8 @@ public class Format {
       if (!ufs.mkdirs(folder, true)) {
         System.out.println("Failed to create UNDERFS_WORKERS_FOLDER: " + folder);
       }
+
+      CommonUtils.touch(folder + "/" + masterConf.FORMAT_FILE_PREFIX + System.currentTimeMillis());
     } else if (args[0].toUpperCase().equals("WORKER")) {
       WorkerConf workerConf = WorkerConf.get();
       String localFolder = workerConf.DATA_FOLDER;
