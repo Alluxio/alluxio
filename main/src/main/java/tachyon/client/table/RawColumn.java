ba4727403d2f16d2caf467f5eb3881f2e8cfd966
Manu Goyal <manu.goyal2013@gmail.com>
2014-04-26 21:07:38 -0700

    Added methods to CommonUtils
    They come in handy across the code base

--- main/src/main/java/tachyon/client/table/RawColumn.java
+++ main/src/main/java/tachyon/client/table/RawColumn.java
@@ -20,6 +20,7 @@ import tachyon.Constants;
 import tachyon.client.TachyonFS;
 import tachyon.client.TachyonFile;
 import tachyon.master.MasterInfo;
+import tachyon.util.CommonUtils;
 
 /**
  * The column of a <code>RawTable</code>.
@@ -37,8 +38,8 @@ public class RawColumn {
 
   // TODO creating file here should be based on id.
   public boolean createPartition(int pId) throws IOException {
-    return TFS.createFile(RAW_TABLE.getPath() + Constants.PATH_SEPARATOR + MasterInfo.COL
-        + COLUMN_INDEX + Constants.PATH_SEPARATOR + pId) > 0;
+    return TFS.createFile(CommonUtils.concat(
+        CommonUtils.concat(RAW_TABLE.getPath(), MasterInfo.COL + COLUMN_INDEX), pId)) > 0;
   }
 
   // TODO creating file here should be based on id.
@@ -48,13 +49,14 @@ public class RawColumn {
 
   // TODO creating file here should be based on id.
   public TachyonFile getPartition(int pId, boolean cachedMetadata) throws IOException {
-    return TFS.getFile(RAW_TABLE.getPath() + Constants.PATH_SEPARATOR + MasterInfo.COL
-        + COLUMN_INDEX + Constants.PATH_SEPARATOR + pId, cachedMetadata);
+    return TFS.getFile(CommonUtils.concat(
+        CommonUtils.concat(RAW_TABLE.getPath(), MasterInfo.COL + COLUMN_INDEX), pId),
+        cachedMetadata);
   }
 
   // TODO creating file here should be based on id.
   public int partitions() throws IOException {
-    return TFS.getNumberOfFiles(RAW_TABLE.getPath() + Constants.PATH_SEPARATOR + MasterInfo.COL
-        + COLUMN_INDEX);
+    return TFS.getNumberOfFiles(CommonUtils.concat(RAW_TABLE.getPath(), MasterInfo.COL
+        + COLUMN_INDEX));
   }
 }
