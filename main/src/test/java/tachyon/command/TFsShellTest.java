9266211487666e72c990b1ae6782ae2c380a9589
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-05-11 13:20:04 -0700

    format and make rename return boolean

--- main/src/test/java/tachyon/command/TFsShellTest.java
+++ main/src/test/java/tachyon/command/TFsShellTest.java
@@ -430,14 +430,15 @@ public class TFsShellTest {
     Assert.assertNull(mTfs.getFile("/testFolder1"));
   }
 
-  @Test(expected = IOException.class)
+  @Test
   public void renameToExistingFileTest() throws IOException {
     StringBuilder toCompare = new StringBuilder();
     mFsShell.mkdir(new String[] { "mkdir", "/testFolder" });
     toCompare.append(getCommandOutput(new String[] { "mkdir", "/testFolder" }));
     mFsShell.mkdir(new String[] { "mkdir", "/testFolder1" });
     toCompare.append(getCommandOutput(new String[] { "mkdir", "/testFolder1" }));
-    mFsShell.rename(new String[] { "rename", "/testFolder1", "/testFolder" });
+    Assert.assertEquals(0,
+        mFsShell.rename(new String[] { "rename", "/testFolder1", "/testFolder" }));
   }
 
   @Test
