467d8d4079404616aa694419501d1e6c62001e63
Aaron Davidson <aaron@databricks.com>
2014-05-20 11:19:10 -0700

    Re-throw fatal exception

--- main/src/main/java/tachyon/worker/TachyonWorker.java
+++ main/src/main/java/tachyon/worker/TachyonWorker.java
@@ -93,6 +93,7 @@ public class TachyonWorker implements Runnable {
       worker.start();
     } catch (Exception e) {
       LOG.error("Uncaught exception terminating worker", e);
+      throw e
     }
   }
 
