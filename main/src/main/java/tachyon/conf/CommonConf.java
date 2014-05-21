61ffe1ce23072fd21b3b831d20692f2567859430
rootfs <hchen@redhat.com>
2014-05-21 12:07:44 -0400

    fix indentation

--- main/src/main/java/tachyon/conf/CommonConf.java
+++ main/src/main/java/tachyon/conf/CommonConf.java
@@ -86,11 +86,11 @@ public class CommonConf extends Utils {
     UNDERFS_GLUSTERFS_IMPL =
         getProperty("tachyon.underfs.glusterfs.impl", "org.apache.hadoop.fs.glusterfs.GlusterFileSystem");
     UNDERFS_GLUSTERFS_VOLUMES = 
-    		getProperty("tachyon.underfs.glusterfs.volumes", null);
+        getProperty("tachyon.underfs.glusterfs.volumes", null);
     UNDERFS_GLUSTERFS_MOUNTS = 
-    		getProperty("tachyon.underfs.glusterfs.mounts", null);
+        getProperty("tachyon.underfs.glusterfs.mounts", null);
     UNDERFS_GLUSTERFS_MR_DIR = 
-    		getProperty("tachyon.underfs.glusterfs.mapred.system.dir", "glusterfs:///mapred/system");
+        getProperty("tachyon.underfs.glusterfs.mapred.system.dir", "glusterfs:///mapred/system");
     USE_ZOOKEEPER = getBooleanProperty("tachyon.usezookeeper", false);
     if (USE_ZOOKEEPER) {
       ZOOKEEPER_ADDRESS = getProperty("tachyon.zookeeper.address");
