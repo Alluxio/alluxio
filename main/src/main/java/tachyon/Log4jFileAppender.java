c088a893d3bc31eb5299afcb93b1d2b4f8e641f7
Colin Patrick McCabe <cmccabe@cloudera.com>
2014-06-03 14:57:53 -0700

    Remove the dependency on ant.
    The only thing Tachyon uses from ant is LazyFileOutputStream.  Since
    this is under the Apache license, it's easier to just include that one
    file than to depend on ant.  Depending on ant means that (for example)
    Spark jobs get ant jars in their CLASSPATH when using Tachyon, which
    they may not want.

--- main/src/main/java/tachyon/Log4jFileAppender.java
+++ main/src/main/java/tachyon/Log4jFileAppender.java
@@ -25,7 +25,6 @@ import java.net.UnknownHostException;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.spi.ErrorCode;
 import org.apache.log4j.spi.LoggingEvent;
-import org.apache.tools.ant.util.LazyFileOutputStream;
 import tachyon.util.CommonUtils;
 
 /**
