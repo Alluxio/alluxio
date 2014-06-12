c043b11a16640745e69641fbf80c6e90abecbef5
chenh <hchen@redhat.com>
2014-06-12 08:55:25 -0400

    Revert "remove gfs"
    This reverts commit ee07614ffbc7127dc9dd4f57b072a412e4d75b16.

--- main/src/main/java/tachyon/UnderFileSystemHdfs.java
+++ main/src/main/java/tachyon/UnderFileSystemHdfs.java
@@ -66,13 +66,22 @@ public class UnderFileSystemHdfs extends UnderFileSystem {
         tConf = new Configuration();
       }
       tConf.set("fs.defaultFS", fsDefaultName);
-      tConf.set("fs.hdfs.impl", CommonConf.get().UNDERFS_HDFS_IMPL);
+      String glusterfsPrefix = "glusterfs:///";
+      if (fsDefaultName.startsWith(glusterfsPrefix)) {
+        tConf.set("fs.glusterfs.impl", CommonConf.get().UNDERFS_GLUSTERFS_IMPL);
+        tConf.set("mapred.system.dir", CommonConf.get().UNDERFS_GLUSTERFS_MR_DIR);
+        tConf.set("fs.glusterfs.volumes", CommonConf.get().UNDERFS_GLUSTERFS_VOLUMES);
+        tConf.set("fs.glusterfs.volume.fuse." + CommonConf.get().UNDERFS_GLUSTERFS_VOLUMES,
+            CommonConf.get().UNDERFS_GLUSTERFS_MOUNTS);
+      } else {
+        tConf.set("fs.hdfs.impl", CommonConf.get().UNDERFS_HDFS_IMPL);
 
-      // To disable the instance cache for hdfs client, otherwise it causes the
-      // FileSystem closed exception. Being configurable for unit/integration
-      // test only, and not expose to the end-user currently.
-      tConf.set("fs.hdfs.impl.disable.cache",
-          System.getProperty("fs.hdfs.impl.disable.cache", "false"));
+        // To disable the instance cache for hdfs client, otherwise it causes the
+        // FileSystem closed exception. Being configurable for unit/integration
+        // test only, and not expose to the end-user currently.
+        tConf.set("fs.hdfs.impl.disable.cache",
+            System.getProperty("fs.hdfs.impl.disable.cache", "false"));
+      }
 
       Utils.addS3Credentials(tConf);
 
