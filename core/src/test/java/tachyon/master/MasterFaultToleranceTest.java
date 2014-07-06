fe1d0c75c8e182f2998cc3f6357effc309e0ffa7
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-07-06 14:39:28 -0700

    improve unit test for master ft; add more comments; fix minor bugs

--- core/src/test/java/tachyon/master/MasterFaultToleranceTest.java
+++ core/src/test/java/tachyon/master/MasterFaultToleranceTest.java
@@ -15,6 +15,7 @@
 package tachyon.master;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.Assert;
@@ -24,6 +25,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import tachyon.Constants;
+import tachyon.Pair;
 import tachyon.client.TachyonFS;
 import tachyon.util.CommonUtils;
 
@@ -32,6 +34,7 @@ import tachyon.util.CommonUtils;
  */
 public class MasterFaultToleranceTest {
   private final int BLOCK_SIZE = 30;
+  private final int MASTERS = 5;
 
   private LocalTachyonClusterMultiMaster mLocalTachyonClusterMultiMaster = null;
   private TachyonFS mTfs = null;
@@ -49,36 +52,76 @@ public class MasterFaultToleranceTest {
     System.setProperty("tachyon.user.quota.unit.bytes", "1000");
     System.setProperty("tachyon.user.default.block.size.byte", String.valueOf(BLOCK_SIZE));
     System.setProperty("fs.hdfs.impl.disable.cache", "true");
-    mLocalTachyonClusterMultiMaster = new LocalTachyonClusterMultiMaster(10000, 5);
+    mLocalTachyonClusterMultiMaster = new LocalTachyonClusterMultiMaster(10000, MASTERS);
     mLocalTachyonClusterMultiMaster.start();
     mTfs = mLocalTachyonClusterMultiMaster.getClient();
+  }
+
+  /**
+   * Create 10 files in the folder
+   * 
+   * @param foldername
+   *          the folder name to create
+   * @param answer
+   *          the results, the mapping from file id to file path
+   * @throws IOException
+   */
+  private void faultTestDataCreation(String folderName, List<Pair<Integer, String>> answer)
+      throws IOException {
+    TachyonFS tfs = mLocalTachyonClusterMultiMaster.getClient();
+    if (!tfs.exist(folderName)) {
+      tfs.mkdir(folderName);
+      answer.add(new Pair<Integer, String>(tfs.getFileId(folderName), folderName));
+    }
+
+    for (int k = 0; k < 10; k ++) {
+      String path = folderName + Constants.PATH_SEPARATOR + folderName.substring(1) + k;
+      answer.add(new Pair<Integer, String>(tfs.createFile(path), path));
+    }
+  }
+
+  /**
+   * Tells if the results can match the answer
+   * 
+   * @param answer
+   *          the correct results
+   * @throws IOException
+   */
+  private void faultTestDataCheck(List<Pair<Integer, String>> answer) throws IOException {
+    TachyonFS tfs = mLocalTachyonClusterMultiMaster.getClient();
+    List<String> files = mTfs.ls(Constants.PATH_SEPARATOR, true);
+    Assert.assertEquals(answer.size(), files.size());
+    for (int k = 0; k < answer.size(); k ++) {
+      Assert.assertEquals(answer.get(k).getSecond(), tfs.getFile(answer.get(k).getFirst())
+          .getPath());
+      Assert.assertEquals(answer.get(k).getFirst().intValue(),
+          tfs.getFileId(answer.get(k).getSecond()));
+    }
   }
 
   @Test
   public void faultTest() throws IOException {
     int clients = 10;
-    mTfs.createFile("/0", 1024);
-    for (int k = 1; k < clients; k ++) {
-      TachyonFS tfs = mLocalTachyonClusterMultiMaster.getClient();
-      tfs.createFile(Constants.PATH_SEPARATOR + k, 1024);
-    }
-    List<String> files = mTfs.ls(Constants.PATH_SEPARATOR, true);
-    Assert.assertEquals(clients + 1, files.size());
-    Assert.assertEquals(Constants.PATH_SEPARATOR, files.get(0));
+    List<Pair<Integer, String>> answer = new ArrayList<Pair<Integer, String>>();
+    answer.add(new Pair<Integer, String>(1, Constants.PATH_SEPARATOR));
+//    faultTestDataCreation("/", answer);
     for (int k = 0; k < clients; k ++) {
-      Assert.assertEquals(Constants.PATH_SEPARATOR + k, files.get(k + 1));
+      faultTestDataCreation("/data" + k, answer);
     }
 
-    for (int kills = 1; kills <= 3; kills ++) {
+    faultTestDataCheck(answer);
+
+    for (int kills = 0; kills < 1; kills ++) {
       Assert.assertTrue(mLocalTachyonClusterMultiMaster.killLeader());
-      CommonUtils.sleepMs(null, 1500);
-      mTfs = mLocalTachyonClusterMultiMaster.getClient();
-      files = mTfs.ls(Constants.PATH_SEPARATOR, true);
-      Assert.assertEquals(clients + 1, files.size());
-      Assert.assertEquals(Constants.PATH_SEPARATOR, files.get(0));
-      for (int k = 0; k < clients; k ++) {
-        Assert.assertEquals(Constants.PATH_SEPARATOR + k, files.get(k + 1));
-      }
+      CommonUtils.sleepMs(null, Constants.SECOND_MS);
+      faultTestDataCheck(answer);
+    }
+
+    for (int kills = 1; kills < MASTERS - 1; kills ++) {
+      Assert.assertTrue(mLocalTachyonClusterMultiMaster.killLeader());
+      CommonUtils.sleepMs(null, Constants.SECOND_MS);
+      faultTestDataCheck(answer);
+      faultTestDataCreation("/data" + (clients + kills + 1), answer);
     }
   }
 
