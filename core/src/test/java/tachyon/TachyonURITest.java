612be9a3a04307637799a991c9aa80934452accf
cheng chang <myairia@gmail.com>
2014-07-08 01:01:16 +0800

    modify TachyonURI for this unit test

--- core/src/test/java/tachyon/TachyonURITest.java
+++ core/src/test/java/tachyon/TachyonURITest.java
@@ -34,8 +34,8 @@ public class TachyonURITest {
   public void basicTests() {
     String[] strs =
         new String[] { "tachyon://localhost:19998/xyz/abc", "hdfs://localhost:19998/xyz/abc",
-        "s3://localhost:19998/xyz/abc", "tachyon://localhost:19998/xy z/a b c",
-        "hdfs://localhost:19998/xy z/a b c", "s3://localhost:19998/xy z/a b c" };
+            "s3://localhost:19998/xyz/abc", "tachyon://localhost:19998/xy z/a b c",
+            "hdfs://localhost:19998/xy z/a b c", "s3://localhost:19998/xy z/a b c" };
     for (String str : strs) {
       TachyonURI uri = new TachyonURI(str);
       Assert.assertEquals(str, uri.toString());
@@ -43,5 +43,10 @@ public class TachyonURITest {
       Assert.assertEquals("localhost", uri.getHost());
       Assert.assertEquals(19998, uri.getPort());
     }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructFromEmptyPathTest1() {
+    TachyonURI uri = new TachyonURI("foo://bar:80");
   }
 }
