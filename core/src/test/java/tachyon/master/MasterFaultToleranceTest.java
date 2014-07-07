f207bc7996e970fb75d00fd340bcfbc2efd44bfd
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-07-07 00:27:37 -0700

    fix unit test

--- core/src/test/java/tachyon/master/MasterFaultToleranceTest.java
+++ core/src/test/java/tachyon/master/MasterFaultToleranceTest.java
@@ -89,7 +89,7 @@ public class MasterFaultToleranceTest {
    */
   private void faultTestDataCheck(List<Pair<Integer, String>> answer) throws IOException {
     TachyonFS tfs = mLocalTachyonClusterMultiMaster.getClient();
-    List<String> files = mTfs.ls(Constants.PATH_SEPARATOR, true);
+    List<String> files = tfs.ls(Constants.PATH_SEPARATOR, true);
     Assert.assertEquals(answer.size(), files.size());
     for (int k = 0; k < answer.size(); k ++) {
       Assert.assertEquals(answer.get(k).getSecond(), tfs.getFile(answer.get(k).getFirst())
