1558bbab3d09f3492257138d94bae075b20c9b10
David Capwell <dcapwell@David-Capwell-MacBook-Pro.local>
2014-07-11 17:24:27 -0700

    fixed import start

--- core/src/main/java/tachyon/hadoop/AbstractTFS.java
+++ core/src/main/java/tachyon/hadoop/AbstractTFS.java
@@ -7,7 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.*;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.apache.hadoop.util.Progressable;
 import org.apache.log4j.Logger;
