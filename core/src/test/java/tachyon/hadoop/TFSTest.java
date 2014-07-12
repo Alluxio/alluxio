878369e3472b41dc59c6a42a1f789c6f0e4f9602
David Capwell <dcapwell@davidcapwellmbp.att.net>
2014-07-11 20:59:17 -0700

    added tests for FS loading against 1x and 2x

--- core/src/test/java/tachyon/hadoop/TFSTest.java
+++ core/src/test/java/tachyon/hadoop/TFSTest.java
@@ -22,14 +22,22 @@ import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+import junit.framework.Assert;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.MockClassLoader;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -40,8 +48,10 @@ import tachyon.client.TachyonFS;
  * Unit tests for TFS
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(TachyonFS.class)
+@PrepareForTest({TachyonFS.class, UserGroupInformation.class})
 public class TFSTest {
+
+  private final Logger LOG = Logger.getLogger(TFSTest.class.getName());
 
   private TFSFT mTfs;
 
@@ -51,9 +61,25 @@ public class TFSTest {
     when(TachyonFS.get(anyString(), anyInt(), anyBoolean())).thenReturn(tachyonFS);
   }
 
+  private void mockUserGroupInformation() throws IOException {
+    // need to mock out since FileSystem.get calls UGI, which some times has issues on some systems
+    mockStatic(UserGroupInformation.class);
+    final UserGroupInformation ugi = mock(UserGroupInformation.class);
+    when(ugi.getCurrentUser()).thenReturn(ugi);
+  }
+
   @Before
   public void setup() throws Exception {
     mTfs = new TFSFT();
+    mockUserGroupInformation();
+
+    if(isHadoop1x()) {
+      LOG.debug("Running TFS tests against hadoop 1x");
+    } else if(isHadoop2x()) {
+      LOG.debug("Running TFS tests against hadoop 2x");
+    } else {
+      LOG.warn("Running TFS tests against untargeted Hadoop version: " + getHadoopVersion());
+    }
   }
 
   @Test
@@ -75,5 +101,81 @@ public class TFSTest {
     // then
     PowerMockito.verifyStatic();
     TachyonFS.get("stanley", 19998, true);
+  }
+
+  @Test
+  public void hadoopShouldLoadTfsWhenConfigured() throws IOException {
+    mockTachyonFSGet();
+
+    final Configuration conf = new Configuration();
+    if(isHadoop1x()) {
+      conf.set("fs."+Constants.SCHEME+".impl", TFS.class.getName());
+    }
+
+    // when
+    final URI uri = URI.create(Constants.HEADER + "stanley:19998/tmp/path.txt");
+    final FileSystem fs = FileSystem.get(uri, conf);
+
+    Assert.assertTrue(fs instanceof TFS);
+
+    PowerMockito.verifyStatic();
+    TachyonFS.get("stanley", 19998, false);
+  }
+
+  @Test
+  public void hadoopShouldLoadTfsFtWhenConfigured() throws IOException {
+    mockTachyonFSGet();
+
+    final Configuration conf = new Configuration();
+    if(isHadoop1x()) {
+      conf.set("fs."+Constants.SCHEME_FT+".impl", TFSFT.class.getName());
+    }
+
+    // when
+    final URI uri = URI.create(Constants.HEADER_FT + "stanley:19998/tmp/path.txt");
+    final FileSystem fs = FileSystem.get(uri, conf);
+
+    Assert.assertTrue(fs instanceof TFSFT);
+
+    PowerMockito.verifyStatic();
+    TachyonFS.get("stanley", 19998, true);
+  }
+
+  private boolean isHadoop1x() {
+    return getHadoopVersion().startsWith("1");
+  }
+
+  private boolean isHadoop2x() {
+    return getHadoopVersion().startsWith("2");
+  }
+
+  private String getHadoopVersion() {
+    try {
+      final URL url = getSourcePath(FileSystem.class);
+      final File path = new File(url.toURI());
+      final String[] splits = path.getName().split("-");
+      final String last = splits[splits.length - 1];
+      return last.substring(0, last.lastIndexOf("."));
+    } catch (URISyntaxException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  private URL getSourcePath(Class<?> clazz) {
+    try {
+      clazz = getClassLoader(clazz).loadClass(clazz.getName());
+      return clazz.getProtectionDomain().getCodeSource().getLocation();
+    } catch (ClassNotFoundException e) {
+      throw new AssertionError("Unable to find class " + clazz.getName());
+    }
+  }
+
+  private ClassLoader getClassLoader(Class<?> clazz) {
+    // Power Mock makes this hard, so try to hack it
+    ClassLoader cl = clazz.getClassLoader();
+    if(cl instanceof MockClassLoader) {
+      cl = cl.getParent();
+    }
+    return cl;
   }
 }
