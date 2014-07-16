818898f748fea37a78e8db9b128cc0a6ba2c6603
David Capwell <dcapwell@office-5-242.pa.gopivotal.com>
2014-07-16 13:30:37 -0700

    updated docs based off feedback

--- core/src/main/java/tachyon/hadoop/AbstractTFS.java
+++ core/src/main/java/tachyon/hadoop/AbstractTFS.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package tachyon.hadoop;
 
 import java.io.FileNotFoundException;
@@ -30,6 +44,13 @@ import tachyon.thrift.NetAddress;
 import tachyon.util.CommonUtils;
 import tachyon.util.UnderfsUtils;
 
+/**
+ * Base class for Apache Hadoop based Tachyon {@link FileSystem}.  This class really just delegates
+ * to {@link tachyon.client.TachyonFS} for most operations.
+ *
+ * All implementing classes must define {@link #isZookeeperMode()} which states if fault tolerant
+ * is used and {@link #getScheme()} for Hadoop's {@link java.util.ServiceLoader} support.
+ */
 public abstract class AbstractTFS extends FileSystem {
   public static final String FIRST_COM_PATH = "tachyon_dep/";
   public static final String RECOMPUTE_PATH = "tachyon_recompute/";
