e4a2f097595f9852ad2b4ad506714ffb9607ce9f
rootfs <hchen@redhat.com>
2014-08-12 15:01:18 -0400

    use latest cephfs-hadoop
    Signed-off-by: rootfs <hchen@redhat.com>

--- vagrant/ceph-install.sh
+++ vagrant/ceph-install.sh
@@ -5,7 +5,6 @@ yum install -y ceph-release
 yum install -y ceph-deploy
 yum install -y ceph-common
 yum install -y ceph-devel
-yum install -y ceph-fuse
 yum install -y ceph-fuse libcephfs1.x86_64 rbd-fuse 
 
 echo "Now installing the java bindings" 
