f8c83199460a6462209c7b2160b1f2386e8d05eb
David Capwell <dcapwell@gmail.com>
2014-08-19 15:26:21 -0700

    moved file region logic to response encoder

--- core/src/main/java/tachyon/worker/netty/DataServerHandler.java
+++ core/src/main/java/tachyon/worker/netty/DataServerHandler.java
@@ -14,10 +14,7 @@
  */
 package tachyon.worker.netty;
 
-import java.io.Closeable;
-import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 import org.apache.log4j.Logger;
@@ -32,9 +29,11 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.DefaultFileRegion;
-import io.netty.channel.socket.nio.NioSocketChannel;
 
+/**
+ * Main logic for the read path.  This class consumes {@link tachyon.worker.netty.BlockRequest}
+ * messages and returns {@link tachyon.worker.netty.BlockResponse} messages.
+ */
 public final class DataServerHandler extends ChannelInboundHandlerAdapter {
   private static final Logger LOG = Logger.getLogger(DataServerHandler.class);
 
@@ -53,61 +52,31 @@ public final class DataServerHandler extends ChannelInboundHandlerAdapter {
     final long offset = req.getOffset();
     final long len = req.getLength();
 
-    int lockId = LOCKER.lock(blockId);
+    final int lockId = LOCKER.lock(blockId);
 
     RandomAccessFile file = null;
     try {
-      if (offset < 0) {
-        throw new IOException("Offset can not be negative: " + offset);
-      }
-      if (len < 0 && len != -1) {
-        throw new IOException("Length can not be negative except -1: " + len);
-      }
+      validateInput(req);
 
       String filePath = CommonUtils.concat(WorkerConf.get().DATA_FOLDER, blockId);
       LOG.info("Try to response remote request by reading from " + filePath);
 
       file = new RandomAccessFile(filePath, "r");
       long fileLength = file.length();
-      String error = null;
-      if (offset > fileLength) {
-        error = String.format("Offset(%d) is larger than file length(%d)", offset, fileLength);
-      }
-      if (error == null && len != -1 && offset + len > fileLength) {
-        error =
-            String.format("Offset(%d) plus length(%d) is larger than file length(%d)", offset,
-                len, fileLength);
-      }
-      if (error != null) {
-        throw new IOException(error);
-      }
+      validateBounds(req, fileLength);
 
-      final long readLength;
-      if (len == -1) {
-        readLength = fileLength - offset;
-      } else {
-        readLength = len;
-      }
+      final long readLength = returnLength(offset, len, fileLength);
 
-      ChannelFuture future = null;
       FileChannel channel = file.getChannel();
-//      if (ctx.channel().getClass().equals(NioSocketChannel.class)) {
-//        ctx.write(new BlockResponse(blockId, offset, readLength));
-//        future = ctx.writeAndFlush(new DefaultFileRegion(channel, offset, readLength));
-//      } else {
-//        // only nio supports FileRegion, so read data here
-//        MappedByteBuffer data = channel.map(FileChannel.MapMode.READ_ONLY, offset, readLength);
-//        future = ctx.writeAndFlush(new BlockResponse(blockId, offset, readLength, data));
-//      }
-      ctx.write(new BlockResponse(blockId, offset, readLength));
-      future = ctx.writeAndFlush(new DefaultFileRegion(channel, offset, readLength));
+      ChannelFuture future =
+          ctx.writeAndFlush(new BlockResponse(blockId, offset, readLength, channel));
       future.addListener(ChannelFutureListener.CLOSE);
       future.addListener(new ClosableResourceChannelListener(file));
       LOG.info("Response remote request by reading from " + filePath + " preparation done.");
     } catch (Exception e) {
       // TODO This is a trick for now. The data may have been removed before remote retrieving.
       LOG.error("The file is not here : " + e.getMessage(), e);
-      BlockResponse resp = new BlockResponse(-blockId, 0, 0);
+      BlockResponse resp = BlockResponse.createErrorResponse(blockId);
       ChannelFuture future = ctx.writeAndFlush(resp);
       future.addListener(ChannelFutureListener.CLOSE);
       if (file != null) {
@@ -118,24 +87,45 @@ public final class DataServerHandler extends ChannelInboundHandlerAdapter {
     }
   }
 
-  private static final class ClosableResourceChannelListener implements ChannelFutureListener {
-    private final Closeable resource;
-
-    private ClosableResourceChannelListener(Closeable resource) {
-      this.resource = resource;
-    }
-
-    @Override
-    public void operationComplete(ChannelFuture future) throws Exception {
-      if (resource != null) {
-        resource.close();
-      }
-    }
-  }
-
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
     LOG.warn("Exception thrown while processing request", cause);
     ctx.close();
+  }
+
+  /**
+   * Returns how much of a file to read. When {@code len} is {@code -1}, then
+   * {@code fileLength - offset} is used.
+   */
+  private static long returnLength(final long offset, final long len, final long fileLength) {
+    if (len == -1) {
+      return fileLength - offset;
+    } else {
+      return len;
+    }
+  }
+
+  private static void validateInput(final BlockRequest req) {
+    if (req.getOffset() < 0) {
+      throw new IllegalArgumentException("Offset can not be negative: " + req.getOffset());
+    }
+    if (req.getLength() < 0 && req.getLength() != -1) {
+      String msg = "Length can not be negative except -1: " + req.getLength();
+      throw new IllegalArgumentException(msg);
+    }
+  }
+
+  private static void validateBounds(final BlockRequest req, final long fileLength) {
+    if (req.getOffset() > fileLength) {
+      String msg =
+          String.format("Offset(%d) is larger than file length(%d)", req.getOffset(), fileLength);
+      throw new IllegalArgumentException(msg);
+    }
+    if (req.getLength() != -1 && req.getOffset() + req.getLength() > fileLength) {
+      String msg =
+          String.format("Offset(%d) plus length(%d) is larger than file length(%d)",
+              req.getOffset(), req.getLength(), fileLength);
+      throw new IllegalArgumentException(msg);
+    }
   }
 }
