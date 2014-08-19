0a36e92c071033c44f95d15d74261813a86d190b
rootfs <hchen@redhat.com>
2014-08-19 11:09:12 -0400

    remove ceph specific handling, make possible to add other HCFS
    Signed-off-by: rootfs <hchen@redhat.com>

--- core/src/main/java/tachyon/UnderFileSystemHdfs.java
+++ core/src/main/java/tachyon/UnderFileSystemHdfs.java
@@ -65,36 +65,30 @@ public class UnderFileSystemHdfs extends UnderFileSystem {
     } else {
       tConf = new Configuration();
     }
-    final String glusterfsPrefix = "glusterfs:///";
-    final String cephPrefix = "ceph://";
+
     // Setting Hadoop compatible filesystem (HCFS) properties in command argument is not scalable
     // for some HCFS e.g. ceph, that has numerous parameters.
     // Providing a hadoop core-site.xml that is required for this type of underfs.
-    if (fsDefaultName.startsWith(cephPrefix)) {
-      if (null != CommonConf.get().HADOOP_CORE_SITE_PATH && !CommonConf.get().HADOOP_CORE_SITE_PATH.equals("")){
-        tConf.addResource(new Path(CommonConf.get().HADOOP_CORE_SITE_PATH));
-        tConf.set("fs.ceph.impl.disable.cache", "true");
-      }else {
-        throw Throwables.propagate(new IOException("No core-site.xml provided"));
-      }
+    if (null != CommonConf.get().HADOOP_CORE_SITE_PATH && !CommonConf.get().HADOOP_CORE_SITE_PATH.equals("")){
+      tConf.addResource(new Path(CommonConf.get().HADOOP_CORE_SITE_PATH));
     }else {
-     tConf.set("fs.defaultFS", fsDefaultName);
-    }
+      final String glusterfsPrefix = "glusterfs:///";
+      tConf.set("fs.defaultFS", fsDefaultName);
+      if (fsDefaultName.startsWith(glusterfsPrefix)) {
+        tConf.set("fs.glusterfs.impl", CommonConf.get().UNDERFS_GLUSTERFS_IMPL);
+        tConf.set("mapred.system.dir", CommonConf.get().UNDERFS_GLUSTERFS_MR_DIR);
+        tConf.set("fs.glusterfs.volumes", CommonConf.get().UNDERFS_GLUSTERFS_VOLUMES);
+        tConf.set("fs.glusterfs.volume.fuse." + CommonConf.get().UNDERFS_GLUSTERFS_VOLUMES,
+            CommonConf.get().UNDERFS_GLUSTERFS_MOUNTS);
+      } else {
+        tConf.set("fs.hdfs.impl", CommonConf.get().UNDERFS_HDFS_IMPL);
 
-    if (fsDefaultName.startsWith(glusterfsPrefix)) {
-      tConf.set("fs.glusterfs.impl", CommonConf.get().UNDERFS_GLUSTERFS_IMPL);
-      tConf.set("mapred.system.dir", CommonConf.get().UNDERFS_GLUSTERFS_MR_DIR);
-      tConf.set("fs.glusterfs.volumes", CommonConf.get().UNDERFS_GLUSTERFS_VOLUMES);
-      tConf.set("fs.glusterfs.volume.fuse." + CommonConf.get().UNDERFS_GLUSTERFS_VOLUMES,
-          CommonConf.get().UNDERFS_GLUSTERFS_MOUNTS);
-    } else {
-      tConf.set("fs.hdfs.impl", CommonConf.get().UNDERFS_HDFS_IMPL);
-
-      // To disable the instance cache for hdfs client, otherwise it causes the
-      // FileSystem closed exception. Being configurable for unit/integration
-      // test only, and not expose to the end-user currently.
-      tConf.set("fs.hdfs.impl.disable.cache", 
-          System.getProperty("fs.hdfs.impl.disable.cache", "false"));
+        // To disable the instance cache for hdfs client, otherwise it causes the
+        // FileSystem closed exception. Being configurable for unit/integration
+        // test only, and not expose to the end-user currently.
+        tConf.set("fs.hdfs.impl.disable.cache", 
+            System.getProperty("fs.hdfs.impl.disable.cache", "false"));
+      }
     }
 
     Utils.addS3Credentials(tConf);
