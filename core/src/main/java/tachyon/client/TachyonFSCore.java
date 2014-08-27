4c430afa7fdc2e39626d4792f25dd06919ec59de
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-08-27 01:35:23 -0700

    explict label methods public for interface.

--- core/src/main/java/tachyon/client/TachyonFSCore.java
+++ core/src/main/java/tachyon/client/TachyonFSCore.java
@@ -18,68 +18,83 @@ interface TachyonFSCore extends Closeable {
   /**
    * Creates a new file in the file system.
    *
-   * @param path The path of the file
-   * @param ufsPath The path of the file in the under file system. If this is empty, the file does
-   *        not exist in the under file system yet.
-   * @param blockSizeByte The size of the block in bytes. It is -1 iff ufsPath is non-empty.
-   * @param recursive Creates necessary parent folders if true, not otherwise.
+   * @param path
+   *          The path of the file
+   * @param ufsPath
+   *          The path of the file in the under file system. If this is empty, the file does
+   *          not exist in the under file system yet.
+   * @param blockSizeByte
+   *          The size of the block in bytes. It is -1 iff ufsPath is non-empty.
+   * @param recursive
+   *          Creates necessary parent folders if true, not otherwise.
    * @return The file id, which is globally unique.
    */
-  int createFile(String path, String ufsPath, long blockSizeByte, boolean recursive)
+  public int createFile(String path, String ufsPath, long blockSizeByte, boolean recursive)
       throws IOException;
 
   /**
    * Deletes a file or folder
    *
-   * @param fileId The id of the file / folder. If it is not -1, path parameter is ignored.
-   *        Otherwise, the method uses the path parameter.
-   * @param path The path of the file / folder. It could be empty iff id is not -1.
-   * @param recursive If fileId or path represents a non-empty folder, delete the folder recursively
-   *        or not
+   * @param fileId
+   *          The id of the file / folder. If it is not -1, path parameter is ignored.
+   *          Otherwise, the method uses the path parameter.
+   * @param path
+   *          The path of the file / folder. It could be empty iff id is not -1.
+   * @param recursive
+   *          If fileId or path represents a non-empty folder, delete the folder recursively
+   *          or not
    * @return true if deletes successfully, false otherwise.
    * @throws IOException
    */
-  boolean delete(int fileId, String path, boolean recursive) throws IOException;
+  public boolean delete(int fileId, String path, boolean recursive) throws IOException;
 
   /**
    * Gets the ClientFileInfo object that represents the fileId, or the path if fileId is -1.
    *
-   * @param fileId the file id of the file or folder.
-   * @param path the path of the file or folder. valid iff fileId is -1.
+   * @param fileId
+   *          the file id of the file or folder.
+   * @param path
+   *          the path of the file or folder. valid iff fileId is -1.
    * @return the ClientFileInfo of the file or folder, null if the file or folder does not exist.
    * @throws IOException
    */
-  ClientFileInfo getFileStatus(int fileId, String path) throws IOException;
+  public ClientFileInfo getFileStatus(int fileId, String path) throws IOException;
 
   /**
    * If the <code>path</code> is a directory, return all the direct entries in it. If the
    * <code>path</code> is a file, return its ClientFileInfo.
    *
-   * @param path the target directory/file path
+   * @param path
+   *          the target directory/file path
    * @return A list of ClientFileInfo, null if the file or folder does not exist.
    * @throws IOException
    */
-  List<ClientFileInfo> listStatus(String path) throws IOException;
+  public List<ClientFileInfo> listStatus(String path) throws IOException;
 
   /**
    * Creates a folder.
    *
-   * @param path the path of the folder to be created
-   * @param recursive Creates necessary parent folders if true, not otherwise.
+   * @param path
+   *          the path of the folder to be created
+   * @param recursive
+   *          Creates necessary parent folders if true, not otherwise.
    * @return true if the folder is created successfully or already existing. false otherwise.
    * @throws IOException
    */
-  boolean mkdirs(String path, boolean recursive) throws IOException;
+  public boolean mkdirs(String path, boolean recursive) throws IOException;
 
   /**
    * Renames a file or folder to another path.
    *
-   * @param fileId The id of the source file / folder. If it is not -1, path parameter is ignored.
-   *        Otherwise, the method uses the srcPath parameter.
-   * @param srcPath The path of the source file / folder. It could be empty iff id is not -1.
-   * @param dstPath The path of the destination file / folder. It could be empty iff id is not -1.
+   * @param fileId
+   *          The id of the source file / folder. If it is not -1, path parameter is ignored.
+   *          Otherwise, the method uses the srcPath parameter.
+   * @param srcPath
+   *          The path of the source file / folder. It could be empty iff id is not -1.
+   * @param dstPath
+   *          The path of the destination file / folder. It could be empty iff id is not -1.
    * @return true if renames successfully, false otherwise.
    * @throws IOException
    */
-  boolean rename(int fileId, String srcPath, String dstPath) throws IOException;
+  public boolean rename(int fileId, String srcPath, String dstPath) throws IOException;
 }
