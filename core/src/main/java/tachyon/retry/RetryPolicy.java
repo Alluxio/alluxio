5fba387297ce22a6992d57879b2bdb5bdcc91d08
David Capwell <dcapwell@gmail.com>
2014-08-29 09:57:29 -0700

    docs

--- core/src/main/java/tachyon/retry/RetryPolicy.java
+++ core/src/main/java/tachyon/retry/RetryPolicy.java
@@ -1,13 +1,14 @@
 package tachyon.retry;
 
 /**
- * Attempts to retry code from a do/while loop.  The way that this interface works is that the
- * logic for delayed retries (retries that sleep) can delay the caller of {@link #attemptRetry()}.
- * Because of this, its best to put retries in do/while loops to avoid the first wait.
+ * Attempts to retry code from a do/while loop. The way that this interface works is that the logic
+ * for delayed retries (retries that sleep) can delay the caller of {@link #attemptRetry()}. Because
+ * of this, its best to put retries in do/while loops to avoid the first wait.
  * <p />
  * This interface is not thread safe and as such should only ever be used from one thread.
  */
 public interface RetryPolicy {
   int getRetryCount();
+
   boolean attemptRetry();
 }
