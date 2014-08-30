04e7d728d9175abddb80c3188cf7f2a6a99fdf72
Henry Saputra <henry.saputra@gmail.com>
2014-08-29 17:12:06 -0700

    Add the assert tests to verify content parsed.

--- core/src/test/java/tachyon/master/EditLogOperationTest.java
+++ core/src/test/java/tachyon/master/EditLogOperationTest.java
@@ -1,22 +1,25 @@
 package tachyon.master;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import junit.framework.Assert;
+import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * Unit Test for EditLogOperation
  */
 public class EditLogOperationTest {
   private static final String CREATE_DEPENDENCY_TYPE = "{\"type\":\"CREATE_DEPENDENCY\"," +
-      "\"parameters\":{\"parents\":[],\"commandPrefix\":\"fake command\"," +
+      "\"parameters\":{\"parents\":[1,2,3],\"commandPrefix\":\"fake command\"," +
       "\"dependencyId\":1,\"frameworkVersion\":\"0.3\",\"data\":[\"AAAAAAAAAAAAAA==\"]," +
-      "\"children\":[251,252,253,254,255,256,257,258,259,260]," +
-      "\"comment\":\"BasicCheckpoint Dependency\",\"creationTimeMs\":1409349750338," +
+      "\"children\":[4,5,6,7]," +
+      "\"comment\":\"Comment Test\",\"creationTimeMs\":1409349750338," +
       "\"dependencyType\":\"Narrow\",\"framework\":\"Tachyon Examples\"}}";
 
   private static final ObjectMapper OBJECT_MAPPER = JsonObject.createObjectMapper();
@@ -28,16 +31,37 @@ public class EditLogOperationTest {
         EditLogOperation.class);
 
     // get all parameters for "CREATE_DEPENDENCY"
-    editLogOperation.get("parents", new TypeReference<List<Integer>>() {});
-    editLogOperation.get("children", new TypeReference<List<Integer>>() {});
-    editLogOperation.getString("commandPrefix");
-    editLogOperation.getByteBufferList("data");
-    editLogOperation.getString("comment");
-    editLogOperation.getString("framework");
-    editLogOperation.getString("frameworkVersion");
-    editLogOperation.get("dependencyType", DependencyType.class);
-    editLogOperation.getInt("dependencyId");
-    editLogOperation.getLong("creationTimeMs");
+    List<Integer> parents = editLogOperation.get("parents", new TypeReference<List<Integer>>() {});
+    Assert.assertEquals(3, parents.size());
+
+    List<Integer> children = editLogOperation.get("children", new TypeReference<List<Integer>>() {});
+    Assert.assertEquals(4, children.size());
+
+    String commandPrefix = editLogOperation.getString("commandPrefix");
+    Assert.assertEquals("fake command", commandPrefix);
+
+    List<ByteBuffer> data = editLogOperation.getByteBufferList("data");
+    Assert.assertEquals(1, data.size());
+    String decodedBase64 = new String(data.get(0).array(), "UTF-8");
+    Assert.assertEquals(new String (Base64.decodeBase64("AAAAAAAAAAAAAA==")), decodedBase64);
+
+    String comment = editLogOperation.getString("comment");
+    Assert.assertEquals("Comment Test", comment);
+
+    String framework = editLogOperation.getString("framework");
+    Assert.assertEquals("Tachyon Examples", framework);
+
+    String frameworkVersion = editLogOperation.getString("frameworkVersion");
+    Assert.assertEquals("0.3", frameworkVersion);
+
+    DependencyType dependencyType = editLogOperation.get("dependencyType", DependencyType.class);
+    Assert.assertEquals(DependencyType.Narrow, dependencyType);
+
+    Integer depId = editLogOperation.getInt("dependencyId");
+    Assert.assertEquals(1, depId.intValue());
+
+    Long creationTimeMs = editLogOperation.getLong("creationTimeMs");
+    Assert.assertEquals(1409349750338L, creationTimeMs.longValue());
   }
 
 }
