989c971fb7858086c9b3199ad9468c810e741f43
cheng chang <myairia@gmail.com>
2014-09-04 05:21:40 +0800

    update servlet to support pagination

--- core/src/main/java/tachyon/web/WebInterfaceMemoryServlet.java
+++ core/src/main/java/tachyon/web/WebInterfaceMemoryServlet.java
@@ -37,10 +37,10 @@ public class WebInterfaceMemoryServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     request.setAttribute("masterNodeAddress", mMasterInfo.getMasterAddress().toString());
+    request.setAttribute("fatalError", "");
 
     List<String> inMemoryFiles = mMasterInfo.getInMemoryFiles();
     Collections.sort(inMemoryFiles);
-    request.setAttribute("inMemoryFiles", inMemoryFiles);
 
     List<UiFileInfo> fileInfos = new ArrayList<UiFileInfo>(inMemoryFiles.size());
     for (String file : inMemoryFiles) {
@@ -50,13 +50,40 @@ public class WebInterfaceMemoryServlet extends HttpServlet {
           fileInfos.add(new UiFileInfo(fileInfo));
         }
       } catch (InvalidPathException ipe) {
-        request.setAttribute("invalidPathError",
-            "Error: Invalid Path " + ipe.getLocalizedMessage());
+        request.setAttribute("fatalError", "Error: Invalid Path " + ipe.getLocalizedMessage());
         getServletContext().getRequestDispatcher("/memory.jsp").forward(request, response);
         return;
       }
     }
-    request.setAttribute("fileInfos", fileInfos);
+    request.setAttribute("inMemoryFileNum", new Integer(fileInfos.size()));
+
+    // URL is "./memory", can not determine offset and limit, let javascript in jsp determine
+    // and redirect to "./memory?offset=xxx&limit=xxx"
+    if (request.getParameter("offset") == null && request.getParameter("limit") == null) {
+      getServletContext().getRequestDispatcher("/memory.jsp").forward(request, response);
+      return;
+    }
+
+    try {
+      int offset = Integer.parseInt(request.getParameter("offset"));
+      int limit = Integer.parseInt(request.getParameter("limit"));
+      List<UiFileInfo> sub = fileInfos.subList(offset, offset + limit);
+      request.setAttribute("fileInfos", sub);
+    } catch (NumberFormatException nfe) {
+      request.setAttribute("fatalError",
+          "Error: offset or limit parse error, " + nfe.getLocalizedMessage());
+      getServletContext().getRequestDispatcher("/memory.jsp").forward(request, response);
+      return;
+    } catch (IndexOutOfBoundsException iobe) {
+      request.setAttribute("fatalError", "Error: offset or offset + limit is out of bound, "
+          + iobe.getLocalizedMessage());
+      getServletContext().getRequestDispatcher("/memory.jsp").forward(request, response);
+      return;
+    } catch (IllegalArgumentException iae) {
+      request.setAttribute("fatalError", iae.getLocalizedMessage());
+      getServletContext().getRequestDispatcher("/memory.jsp").forward(request, response);
+      return;
+    }
 
     getServletContext().getRequestDispatcher("/memory.jsp").forward(request, response);
   }
