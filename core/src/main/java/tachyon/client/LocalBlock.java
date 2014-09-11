65d7aee0e3c1a237b84dfb8030811fa781159160
David Capwell <dcapwell@gmail.com>
2014-09-11 10:26:04 -0700

    style

--- core/src/main/java/tachyon/client/LocalBlock.java
+++ core/src/main/java/tachyon/client/LocalBlock.java
@@ -3,6 +3,8 @@ package tachyon.client;
 import java.io.IOException;
 
 final class LocalBlock implements Block {
+  private static final long DEFAULT_BUFFER_SIZE = 4096; // 4k
+
   private final TachyonFS mTachyonFS;
   private final long mBlockId;
   private final long mMaxBlockSize;
@@ -15,7 +17,7 @@ final class LocalBlock implements Block {
 
   @Override
   public WritableBlockChannel write() throws IOException {
-    int bufferSize = (int) Math.min(mMaxBlockSize, 0x1000); // 4k
+    int bufferSize = (int) Math.min(mMaxBlockSize, DEFAULT_BUFFER_SIZE);
     LocalWritableBlockChannel local = new LocalWritableBlockChannel(mTachyonFS, mBlockId);
     BufferedWritableBlockChannel buffered = new BufferedWritableBlockChannel(bufferSize, local);
     BoundedWritableBlockChannel bounded = new BoundedWritableBlockChannel(mMaxBlockSize, buffered);
