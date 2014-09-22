ca4df3cd3596f2ad4a120ba6c45270d9c32b3bfb
rootfs <hchen@redhat.com>
2014-09-22 09:58:39 -0400

    multi provider, multi vm support
    Signed-off-by: rootfs <hchen@redhat.com>

--- deploy/vagrant/config_vb.rb
+++ deploy/vagrant/config_vb.rb
@@ -1,10 +1,11 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-def config_vb(v, i)
-  addr = @cmd['addresses']
+def config_vb(config, i)
+  addr = @cmd['Addresses']
   puts "starting " 
   puts addr[i - 1]
-  v.vm.network "private_network", ip: addr[i - 1]
-  v.vm.host_name =  "tachyon#{i}"
+  #v.customize ["modifyvm", :id, "--memory", "3024"]
+  config.vm.network "private_network", ip: addr[i - 1]
+  config.vm.host_name =  "tachyon#{i}"
 end
