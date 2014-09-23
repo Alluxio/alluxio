57cdd2ff24ebae4fdbb446fb56596d6a7b221988
rootfs <hchen@redhat.com>
2014-09-23 10:09:04 -0400

    configure virtualbox tachyon cluster
    Signed-off-by: rootfs <hchen@redhat.com>

--- deploy/vagrant/config_vb.rb
+++ deploy/vagrant/config_vb.rb
@@ -1,11 +1,12 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-def config_vb(config, i)
-  addr = @cmd['Addresses']
+def config_vb(config, i, total)
   puts "starting " 
-  puts addr[i - 1]
-  #v.customize ["modifyvm", :id, "--memory", "3024"]
-  config.vm.network "private_network", ip: addr[i - 1]
+  puts Addr[i - 1]
+  config.vm.network "private_network", ip: Addr[i - 1]
   config.vm.host_name =  "tachyon#{i}"
+  if i == total # last VM starts tachyon
+      config.vm.provision "shell", path: "start_tachyon_cluster.sh"
+  end
 end
