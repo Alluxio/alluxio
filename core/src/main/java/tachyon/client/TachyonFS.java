e8be6b3287a86c3caa153b4adbb84a2d9d006547
Manu Goyal <manu.goyal2013@gmail.com>
2014-10-21 11:41:23 -0700

    Documentation fixes

--- core/src/main/java/tachyon/client/TachyonFS.java
+++ core/src/main/java/tachyon/client/TachyonFS.java
@@ -597,7 +597,12 @@ public class TachyonFS extends AbstractTachyonFS {
     return new TachyonURI(scheme, authority, TachyonURI.SEPARATOR);
   }
   
-  synchronized long getUserId() throws IOException {
+  /**
+   * Returns the userId of the master client. This is only used for testing.
+   * @return the userId of the master client
+   * @throws IOException
+   */
+  long getUserId() throws IOException {
     return mMasterClient.getUserId();
   }
 
