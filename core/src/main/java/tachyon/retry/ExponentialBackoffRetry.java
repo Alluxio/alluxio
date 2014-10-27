d47422c1e6a2e7639c1497bdf59f924f080b0cbb
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-10-27 11:12:10 -0700

    tweak retry code

--- core/src/main/java/tachyon/retry/ExponentialBackoffRetry.java
+++ core/src/main/java/tachyon/retry/ExponentialBackoffRetry.java
@@ -5,9 +5,9 @@ import java.util.Random;
 import com.google.common.base.Preconditions;
 
 /**
- * Each retry will cause a sleep to happen. This sleep will grow over time exponentially
- * so each sleep gets much larger than the last. To make sure that this growth does not
- * grow out of control, a max sleep is used as a bounding.
+ * Each retry will cause a sleep to happen. This sleep will grow over time exponentially so each
+ * sleep gets much larger than the last. To make sure that this growth does not grow out of control,
+ * a max sleep is used as a bounding.
  */
 public final class ExponentialBackoffRetry extends SleepingRetry {
   private final Random mRandom = new Random();
@@ -25,11 +25,7 @@ public final class ExponentialBackoffRetry extends SleepingRetry {
 
   @Override
   protected long getSleepTime() {
-    // copied from Hadoop's RetryPolicies.java
     int sleepMs = mBaseSleepTimeMs * Math.max(1, mRandom.nextInt(1 << (getRetryCount() + 1)));
-    if (sleepMs > mMaxSleepMs) {
-      sleepMs = mMaxSleepMs;
-    }
-    return sleepMs;
+    return Math.min(sleepMs, mMaxSleepMs);
   }
 }
--- core/src/main/java/tachyon/retry/package-info.java
+++ core/src/main/java/tachyon/retry/package-info.java
@@ -3,8 +3,10 @@
  * {@link tachyon.retry.RetryPolicy} which is designed to work with do/while loops.
  * <p />
  * Example
+ * 
  * <pre>
- * {@code
+ * {
+ *   &#064;code
  *   RetryPolicy retry = new ExponentialBackoffRetry(50, Constants.SECOND_MS, MAX_CONNECT_TRY);
  *   do {
  *     // work to retry
@@ -13,3 +15,4 @@
  * </pre>
  */
 package tachyon.retry;
+
