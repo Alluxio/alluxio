ac7df14a67cb9daa51a7bc05c3e1433c35e02182
RongGu <gurongwalker@gmail.com>
2014-10-30 16:17:29 +0800

    Add abstract classes of a task

---
+++ perf/src/main/java/tachyon/perf/basic/PerfThread.java
@@ -0,0 +1,41 @@
+package tachyon.perf.basic;
+
+import org.apache.log4j.Logger;
+
+import tachyon.perf.PerfConstants;
+
+/**
+ * The abstract class for the thread, which contains the executing logic of the test. For new test,
+ * you should create a new class which extends this.
+ */
+public abstract class PerfThread implements Runnable {
+  protected static final Logger LOG = Logger.getLogger(PerfConstants.PERF_LOGGER_TYPE);
+
+  protected int mId;
+  protected int mTaskId;
+  protected String mNodeName;
+  protected String mTaskType;
+
+  public void initialSet(int threadId, int taskId, String nodeName, String taskType) {
+    mId = threadId;
+    mTaskId = taskId;
+    mNodeName = nodeName;
+    mTaskType = taskType;
+  }
+
+  /**
+   * Cleanup the thread. Do some following work.
+   * 
+   * @param taskConf
+   * @return true if cleanup successfully, false otherwise
+   */
+  public abstract boolean cleanupThread(TaskConfiguration taskConf);
+
+  /**
+   * Setup the thread. Do some preparations.
+   * 
+   * @param taskConf
+   * @return true if setup successfully, false otherwise
+   */
+  public abstract boolean setupThread(TaskConfiguration taskConf);
+}
