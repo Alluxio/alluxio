e9a0fb0a0992a2b4ad788c8559be332b93b5e43d
rootfs <hchen@redhat.com>
2014-10-31 14:57:17 -0400

    vagrant deploy: various bug fixes. - Change Hadoop download site to archive.apache.org for better locality - fix hostname issue in /etc/hosts that makes HDFS NN binds to 127.0.0.1 and other cannot connect
    Signed-off-by: rootfs <hchen@redhat.com>

--- deploy/vagrant/init.sh
+++ deploy/vagrant/init.sh
@@ -44,6 +44,12 @@ Host *
    StrictHostKeyChecking no
    UserKnownHostsFile=/dev/null
 EOF
+    # patch /etc/hosts
+    # HDFS NN binds to the first IP that resolves the hostname. 
+    # And thus make the default address to 127.0.0.1 
+    # This makes other DN unable to connect to NN
+    # Solution is to use fully qualified domain name
+    cat ${src}/hosts >> /etc/hosts
 
     chmod 600 ~/.ssh/*
     # install java
