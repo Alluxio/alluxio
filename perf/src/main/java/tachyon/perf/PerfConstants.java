a12bc6531272796165241f02bff5ca95ec85d1ec
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-11-25 21:34:01 -0800

    remove perf

--- perf/src/main/java/tachyon/perf/PerfConstants.java
+++
@@ -1,9 +0,0 @@
-package tachyon.perf;
-
-/**
- * Tachyon-Perf constants
- */
-public class PerfConstants {
-  public static final String PERF_LOGGER_TYPE = System.getProperty("tachyon.perf.logger.type", "");
-  public static final String PERF_CONTEXT_FILE_NAME_PREFIX = "context";
-}
