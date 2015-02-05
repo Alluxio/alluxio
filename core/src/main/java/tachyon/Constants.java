5b5c472ef36b433e5ccfd12fd8754de58defd7fd
Haoyuan Li <haoyuan@cs.berkeley.edu>
2015-02-04 23:29:54 -0800

    tweak TFsShell copies buf sizes.

--- core/src/main/java/tachyon/Constants.java
+++ core/src/main/java/tachyon/Constants.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -50,8 +50,6 @@ public class Constants {
   public static final int DEFAULT_MASTER_WEB_PORT = DEFAULT_MASTER_PORT + 1;
   public static final int DEFAULT_WORKER_PORT = 29998;
   public static final int DEFAULT_WORKER_DATA_SERVER_PORT = DEFAULT_WORKER_PORT + 1;
-  
-  public static final int DEFAULT_BUFFER_SIZE_BYTE = 64 * MB;
 
   public static final int DEFAULT_BLOCK_SIZE_BYTE = 512 * MB;
 
