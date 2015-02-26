aaed899f4ae5d8737a87b91ea2e386d408c289cd
Calvin Jia <jia.calvin@gmail.com>
2015-02-26 13:48:35 -0800

    Clean up.

--- core/src/main/java/tachyon/web/WebInterfaceConfigurationServlet.java
+++ core/src/main/java/tachyon/web/WebInterfaceConfigurationServlet.java
@@ -29,6 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import tachyon.Constants;
 import tachyon.conf.TachyonConf;
@@ -68,8 +69,8 @@ public final class WebInterfaceConfigurationServlet extends HttpServlet {
     getServletContext().getRequestDispatcher("/configuration.jsp").forward(request, response);
   }
 
-  private SortedSet<ImmutablePair<String, String>> getSortedProperties() {
-    TreeSet<ImmutablePair<String, String>> rtn = new TreeSet<ImmutablePair<String, String>>();
+  private SortedSet<Pair<String, String>> getSortedProperties() {
+    TreeSet<Pair<String, String>> rtn = new TreeSet<Pair<String, String>>();
     for (Map.Entry<Object, Object> entry : mTachyonConf.getInternalProperties().entrySet()) {
       String key = entry.getKey().toString();
       if (key.startsWith(TACHYON_CONF_PREFIX) && !TACHYON_CONF_EXCLUDES.contains(key)) {
