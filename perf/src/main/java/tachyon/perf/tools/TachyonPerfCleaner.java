fff16e3b121a6127bb8a0c6d9ae33e41a3402aa1
dongqianhao <09122SWAT@gmail.com>
2015-03-05 14:05:37 +0800

    update code with tachyon-0.6.0

--- perf/src/main/java/tachyon/perf/tools/TachyonPerfCleaner.java
+++ perf/src/main/java/tachyon/perf/tools/TachyonPerfCleaner.java
@@ -1,8 +1,23 @@
+/*
+ * Licensed to the University of California, Berkeley under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package tachyon.perf.tools;
 
 import java.io.IOException;
 
-import tachyon.conf.MasterConf;
+import tachyon.conf.TachyonConf;
 import tachyon.perf.conf.PerfConf;
 import tachyon.perf.fs.PerfFileSystem;
 
@@ -11,7 +26,7 @@ import tachyon.perf.fs.PerfFileSystem;
  */
 public class TachyonPerfCleaner {
   public static void main(String[] args) {
-    String tachyonAddress = MasterConf.get().MASTER_ADDRESS;
+    String tachyonAddress = new TachyonConf().get(tachyon.Constants.MASTER_ADDRESS, "");
     try {
       PerfFileSystem fs = PerfFileSystem.get();
       fs.delete(PerfConf.get().WORK_DIR, true);
