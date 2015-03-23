995fffab2c5cea2d04dc6ef8f3423776eebf45d5
Calvin Jia <jia.calvin@gmail.com>
2015-03-23 14:57:59 -0700

    Add create/rename contracts.

--- core/src/test/java/tachyon/hadoop/contract/TestTachyonContractRename.java
+++ core/src/test/java/tachyon/hadoop/contract/TestTachyonContractRename.java
@@ -15,10 +15,10 @@
 package tachyon.hadoop.contract;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.contract.AbstractContractCreateTest;
+import org.apache.hadoop.fs.contract.AbstractContractRenameTest;
 import org.apache.hadoop.fs.contract.AbstractFSContract;
 
-public class TestTachyonContractCreate extends AbstractContractCreateTest {
+public class TestTachyonContractRename extends AbstractContractRenameTest {
   @Override
   protected AbstractFSContract createContract(Configuration conf) {
     return new TachyonFSContract(conf);
