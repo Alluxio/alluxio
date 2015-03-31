caf04e58ebcb3be7d433630d999f84362d8725b8
Shaoshan Liu <liushaoshan@baidu.com>
2015-03-31 16:07:02 -0700

    populate UnderFileSystemDummyTest

--- core/src/test/java/tachyon/UnderFileSystemDummyTest.java
+++ core/src/test/java/tachyon/UnderFileSystemDummyTest.java
@@ -19,19 +19,97 @@ import org.junit.Before;
 import org.junit.Test;
 import tachyon.conf.TachyonConf;
 
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+
 /**
- * Unit tests for {@link UnderFileSystem}
+ * Unit tests for {@link UnderFileSystemDummy}
  */
 public final class UnderFileSystemDummyTest {
   private TachyonConf mTachyonConf;
-
+  private UnderFileSystemDummy mUFSD;
   @Before
   public void before() {
     mTachyonConf = new TachyonConf();
+    mUFSD = new UnderFileSystemDummy(mTachyonConf);
   }
 
   @Test
-  public void parseTest() {
- 
+  public void createTest() throws IOException {
+    Assert.assertTrue(mUFSD.create("") instanceof DummyFileOutputStream);
+    Assert.assertTrue(mUFSD.create("",0) instanceof DummyFileOutputStream);
+    Assert.assertTrue(mUFSD.create("",(short)0,0) instanceof DummyFileOutputStream);
+  }
+
+  @Test
+  public void deleteTest() throws IOException {
+    Assert.assertTrue(mUFSD.delete("", true));
+    Assert.assertTrue(mUFSD.delete("", false));
+  }
+
+  @Test
+  public void existsTest() throws IOException {
+    Assert.assertTrue(mUFSD.exists(""));
+  }
+
+  @Test
+  public void getBlockSizeByteTest() throws IOException {
+    Assert.assertEquals(0, mUFSD.getBlockSizeByte(""));
+  }
+
+  @Test
+  public void getConfTest() throws IOException {
+    Assert.assertTrue(mUFSD.getConf() instanceof Object);
+  }
+
+  @Test
+  public void getFileLocationsTest() throws IOException {
+    Assert.assertTrue(mUFSD.getFileLocations("").isEmpty());
+    Assert.assertTrue(mUFSD.getFileLocations("", 0).isEmpty());
+  }
+
+  @Test
+  public void getFileSizeTest() throws IOException {
+    Assert.assertEquals(0, mUFSD.getFileSize(""));
+  }
+
+  @Test
+  public void getModificationTimeMsTest() throws IOException {
+    Assert.assertEquals(0, mUFSD.getModificationTimeMs(""));
+  }
+
+  @Test
+  public void getSpaceTest() throws IOException {
+    Assert.assertEquals(0, mUFSD.getSpace("", null));
+  }
+
+  @Test
+  public void isFileTest() throws IOException {
+    Assert.assertTrue(mUFSD.isFile(""));
+  }
+
+  @Test
+  public void listTest() throws IOException {
+    Assert.assertEquals(0, mUFSD.list("").length);
+  }
+
+  @Test
+  public void mkdirsTest() throws IOException {
+    Assert.assertTrue(mUFSD.mkdirs("", true));
+    Assert.assertTrue(mUFSD.mkdirs("", false));
+  }
+
+  @Test
+  public void openTest() throws IOException {
+    Assert.assertTrue(mUFSD.open("") instanceof DummyFileInputStream);
+  }
+
+  @Test
+  public void renameTest() throws IOException {
+    Assert.assertTrue(mUFSD.rename("",""));
   }
 }
