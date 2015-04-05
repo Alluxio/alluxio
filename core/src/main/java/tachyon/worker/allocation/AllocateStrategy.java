6b8f6c56d2ed1151f57df5a507fc9d94a72067db
Bin Fan <fanbin103@gmail.com>
2015-04-04 22:10:00 -0700

    Cleanup in worker/allocation

--- core/src/main/java/tachyon/worker/allocation/AllocateStrategy.java
+++ core/src/main/java/tachyon/worker/allocation/AllocateStrategy.java
@@ -24,8 +24,8 @@ public interface AllocateStrategy {
   /**
    * Check whether it is possible to get enough space by evicting some blocks
    *
-   * @param storageDirs candidates of StorageDirs which space will be allocated in
-   * @param requestSizeBytes requested space in bytes
+   * @param storageDirs A list of candidate StorageDir to allocate space
+   * @param requestSizeBytes The size of requested space in bytes
    * @return true if possible, false otherwise
    */
   public boolean fitInPossible(StorageDir[] storageDirs, long requestSizeBytes);
@@ -34,10 +34,10 @@ public interface AllocateStrategy {
    * Allocate space on StorageDirs. It returns the affordable StorageDir according to
    * AllocateStrategy, null if no enough space available on StorageDirs.
    *
-   * @param storageDirs candidates of StorageDirs that space will be allocated in
-   * @param userId id of user
-   * @param requestSizeBytes size to request in bytes
-   * @return StorageDir assigned, null if no StorageDir is assigned
+   * @param storageDirs A list of candidate StorageDir to allocate space
+   * @param userId The id of user
+   * @param requestSizeBytes The size of requested space in bytes
+   * @return StorageDir assigned, null if no StorageDir is found
    */
   public StorageDir getStorageDir(StorageDir[] storageDirs, long userId, long requestSizeBytes);
 }
