36708d6d411737fb0a9c01777db4baaddac3df10
Calvin Jia <jia.calvin@gmail.com>
2015-04-22 17:49:09 -0700

    Resolve variable in configuration ui.

--- core/src/main/java/tachyon/web/WebInterfaceConfigurationServlet.java
+++ core/src/main/java/tachyon/web/WebInterfaceConfigurationServlet.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -54,7 +54,7 @@ public final class WebInterfaceConfigurationServlet extends HttpServlet {
 
   /**
    * Populates attributes before redirecting to a jsp.
-   * 
+   *
    * @param request The HttpServletRequest object
    * @param response The HttpServletReponse object
    * @throws ServletException
@@ -74,7 +74,7 @@ public final class WebInterfaceConfigurationServlet extends HttpServlet {
     for (Map.Entry<Object, Object> entry : mTachyonConf.getInternalProperties().entrySet()) {
       String key = entry.getKey().toString();
       if (key.startsWith(TACHYON_CONF_PREFIX) && !TACHYON_CONF_EXCLUDES.contains(key)) {
-        rtn.add(new ImmutablePair<String, String>(key, entry.getValue().toString()));
+        rtn.add(new ImmutablePair<String, String>(key, mTachyonConf.get(key, null)));
       }
     }
     return rtn;
