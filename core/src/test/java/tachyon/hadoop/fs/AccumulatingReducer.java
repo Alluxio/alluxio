416e33a23a6b412e08876de36e18fac2f8e830d9
Calvin Jia <jia.calvin@gmail.com>
2015-05-05 11:58:32 -0700

    Use slf4j logger in hadoop tests to be consistent.

--- core/src/test/java/tachyon/hadoop/fs/AccumulatingReducer.java
+++ core/src/test/java/tachyon/hadoop/fs/AccumulatingReducer.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,13 +17,15 @@ package tachyon.hadoop.fs;
 import java.io.IOException;
 import java.util.Iterator;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tachyon.Constants;
 
 /**
  * Reducer that accumulates values based on their type.
@@ -39,14 +41,14 @@ import org.apache.hadoop.mapred.Reporter;
  * <li><tt>f:</tt> - float, summ</li>
  * <li><tt>l:</tt> - long, summ</li>
  * </ul>
- * 
+ *
  */
 @SuppressWarnings("deprecation")
 public class AccumulatingReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
   static final String VALUE_TYPE_LONG = "l:";
   static final String VALUE_TYPE_FLOAT = "f:";
   static final String VALUE_TYPE_STRING = "s:";
-  private static final Log LOG = LogFactory.getLog(AccumulatingReducer.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
 
   protected String hostName;
 
