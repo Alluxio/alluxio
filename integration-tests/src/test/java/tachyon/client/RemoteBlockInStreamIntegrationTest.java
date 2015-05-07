2d1e6233f0c7f98468a168bf434a58e618f097b8
Bin Fan <fanbin103@gmail.com>
2015-05-07 12:36:04 -0700

    Update javadoc so we know they are for integration tests, not for unit tests any more

--- integration-tests/src/test/java/tachyon/client/RemoteBlockInStreamIntegrationTest.java
+++ integration-tests/src/test/java/tachyon/client/RemoteBlockInStreamIntegrationTest.java
@@ -6,7 +6,7 @@
  * copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -36,7 +36,7 @@ import tachyon.conf.TachyonConf;
 import tachyon.master.LocalTachyonCluster;
 
 /**
- * Unit tests for <code>tachyon.client.RemoteBlockInStream</code>.
+ * Integration tests for <code>tachyon.client.RemoteBlockInStream</code>.
  */
 @RunWith(Parameterized.class)
 public class RemoteBlockInStreamIntegrationTest {
@@ -437,7 +437,7 @@ public class RemoteBlockInStreamIntegrationTest {
 
   /**
    * Test <code>void seek(long pos)</code>.
-   * 
+   *
    * @throws IOException
    */
   @Test
