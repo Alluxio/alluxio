ce83b0ac747eda26550282b9a07345da4d9fa9d8
cc <myairia@gmail.com>
2015-05-08 10:56:55 +0800

    fix glusterfs download, configure errors

--- deploy/vagrant/provision/roles/ufs_glusterfs/files/compile_tachyon.sh
+++ deploy/vagrant/provision/roles/ufs_glusterfs/files/compile_tachyon.sh
@@ -3,4 +3,4 @@
 # build tachyon pkg
 cd /tachyon
 echo "compiling Tachyon..."
-mvn -q install -Dtest.profile=glusterfs -Dhadoop.version=2.3.0  -DskipTests -Dmaven.javadoc.skip=true -Dlicense.skip=true -Dcheckstyle.skip=true
+mvn -q clean install -Dtest.profile=glusterfs -Dhadoop.version=2.3.0 -DskipTests -Dmaven.javadoc.skip=true -Dlicense.skip=true -Dcheckstyle.skip=true
