71cd70c2f35486d9dec6e47c5ad3d636464294d4
cc <myairia@gmail.com>
2015-05-08 10:57:16 +0800

    make sure wget is installed

--- deploy/vagrant/provision/roles/ufs_hadoop1/files/download_release.sh
+++ deploy/vagrant/provision/roles/ufs_hadoop1/files/download_release.sh
@@ -3,6 +3,7 @@ HADOOP_VERSION="1.0.4"
 if [ ! -f /vagrant/shared/hadoop-${HADOOP_VERSION}-bin.tar.gz ]; then
     # download hadoop
     echo "Downloading hadoop ${HADOOP_VERSION} ..." 
+    sudo yum install -y -q wget
     wget -q https://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}-bin.tar.gz -P /vagrant/shared
 fi
 
