1f1d2be2ea7bc28feeb286f16ba802eacf2143fd
Calvin Jia <jia.calvin@gmail.com>
2015-05-11 11:04:31 -0700

    Add negative tests to under file system factory tests.

--- underfs/glusterfs/src/test/java/tachyon/underfs/glusterfs/GlusterFSUnderFileSystemTest.java
+++ underfs/glusterfs/src/test/java/tachyon/underfs/glusterfs/GlusterFSUnderFileSystemTest.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -55,10 +55,14 @@ public class GlusterFSUnderFileSystemTest {
     mGfs = UnderFileSystem.get("glusterfs:///", mTachyonConf);
     Assert.assertNotNull(mGfs.create("tachyon_test"));
   }
-  
+
   @Test
   public void factoryTest() {
     UnderFileSystemFactory factory = UnderFileSystemRegistry.find("glusterfs://localhost/test/path", mTachyonConf);
     Assert.assertNotNull("A UnderFileSystemFactory should exist for Gluster FS paths when using this module", factory);
+
+    factory = UnderFileSystemRegistry.find("tachyon://localhost/test/path", mTachyonConf);
+    Assert.assertNull("A UnderFileSystemFactory should not exist for unsupported paths when using" +
+        " this module.", factory);
   }
 }
