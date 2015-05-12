7e81675029b7acb86e5ad238ef84fd6c4fba8187
Bin Fan <fanbin103@gmail.com>
2015-05-12 10:55:30 -0700

    Fix various issues in pom.xml of each module

--- common/src/main/java/tachyon/worker/DataServerMessage.java
+++ common/src/main/java/tachyon/worker/DataServerMessage.java
@@ -232,7 +232,7 @@ public class DataServerMessage {
    *
    * @return The id of the block's locker
    */
-  int getLockId() {
+  public int getLockId() {
     return mLockId;
   }
 
@@ -343,7 +343,7 @@ public class DataServerMessage {
    *
    * @param lockId The id of the block's locker
    */
-  void setLockId(int lockId) {
+  public void setLockId(int lockId) {
     mLockId = lockId;
   }
 }
