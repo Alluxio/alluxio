dcf353b95e81592773ed1e13a40586707e4fa05e
cc <myairia@gmail.com>
2015-05-12 10:50:03 +0800

    more appropriate prompt in case spark is not started

--- deploy/vagrant/run_vb.sh
+++ deploy/vagrant/run_vb.sh
@@ -10,6 +10,6 @@ if [[ "$?" == "0" ]]; then
  master=`tail -n 1 $HERE/files/hosts | cut -d' ' -f1`
  purple='\033[1;35m'
  no_color='\033[0m'
- echo -e ">>> ${purple}visit $master:19999 for Tachyon Web Console${no_color} <<<"
- echo -e ">>> ${purple}visit $master:8080  for Spark   Web Console${no_color} <<<"
+ echo -e ">>> ${purple}TachyonMaster public IP is ${master}${no_color} <<<"
+ echo -e ">>> ${purple}visit default port of the web UI of what you deployed${no_color} <<<"
 fi
