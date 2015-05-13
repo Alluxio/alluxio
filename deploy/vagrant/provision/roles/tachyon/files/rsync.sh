9b072db6055204d46388bb143301440c8c17fc00
Calvin Jia <jia.calvin@gmail.com>
2015-05-13 15:11:27 -0700

    Modify deploy module to correctly handle assemblies jar.

--- deploy/vagrant/provision/roles/tachyon/files/rsync.sh
+++ deploy/vagrant/provision/roles/tachyon/files/rsync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p /tachyon/core/target
-rsync -avz TachyonMaster.local:/tachyon/core/target/tachyon-*-jar-with-dependencies.jar /tachyon/core/target
+rsync -avz TachyonMaster.local:/tachyon/assembly/target/tachyon-assmeblies-*-jar-with-dependencies.jar /tachyon/core/target
 rsync -avz TachyonMaster.local:'/tachyon/bin /tachyon/conf /tachyon/libexec' /tachyon
