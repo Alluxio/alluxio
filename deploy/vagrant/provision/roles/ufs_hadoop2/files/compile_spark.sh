fbcb5533ed6c0ebfa64227c1a04dd00b0cb69dbb
cc <myairia@gmail.com>
2015-05-15 02:06:38 +0800

    make hadoop version configurable when compiling tachyon/spark

--- deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_spark.sh
+++ deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_spark.sh
@@ -1,2 +1,8 @@
 cd /spark
-./make-distribution.sh -Phadoop-2.4 -Dhadoop.version=2.4.1 >/spark/make-distribution.log 2>&1
+# for version 2.4.1, major version is 2.4
+HADOOP_MAJOR_VERSION=`echo $HADOOP_VERSION | cut -d'.' -f1-2`
+if [[ "$SPARK_PROFILE" == "" ]]; then
+  ./make-distribution.sh -Dhadoop.version=${HADOOP_VERSION} >/spark/make-distribution.log 2>&1
+else
+  ./make-distribution.sh -Phadoop-${SPARK_PROFILE} -Dhadoop.version=${HADOOP_VERSION} >/spark/make-distribution.log 2>&1
+fi
