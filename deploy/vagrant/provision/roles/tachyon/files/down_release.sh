011437b669ddebcb8132aea27dc198041fcc173e
cc <myairia@gmail.com>
2015-05-15 02:03:25 +0800

    pass ansible variable through ansible's environment feature

--- deploy/vagrant/provision/roles/tachyon/files/down_release.sh
+++ deploy/vagrant/provision/roles/tachyon/files/down_release.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# ANSIBLE_TACHYON_DIST is set to variable {{ tachyon_dist }} in this file
-source /tmp/tachyon_dist.sh
-
 mkdir -p /vagrant/shared
 
-DIST=/vagrant/shared/$ANSIBLE_TACHYON_DIST
+DIST=/vagrant/shared/$TACHYON_DIST
 
 if [ ! -f $DIST ]; then
  version=`echo $DIST | cut -d'-' -f2`
  sudo yum install -y -q wget
- wget -q https://github.com/amplab/tachyon/releases/download/v${version}/${ANSIBLE_TACHYON_DIST} -P /vagrant/shared
+ wget -q https://github.com/amplab/tachyon/releases/download/v${version}/${TACHYON_DIST} -P /vagrant/shared
 fi
 
 tar xzf $DIST -C /tachyon --strip-components 1
