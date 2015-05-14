7694186bc6e1baa7907c378336b5dc307afc761c
cc <myairia@gmail.com>
2015-05-15 02:06:38 +0800

    make hadoop version configurable when compiling tachyon/spark

--- deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_tachyon.sh
+++ deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_tachyon.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-set -e
 
-HADOOP_VERSION="2.4.1"
-
-# setup tachyon
 cd /tachyon
-echo "compiling Tachyon..."
-mvn -q clean install -Dmaven.javadoc.skip=true -DskipTests -Dhadoop.version=${HADOOP_VERSION} -Dlicense.skip=true -Dcheckstyle.skip=true
+mvn clean install -Dmaven.javadoc.skip=true -DskipTests -Dhadoop.version=${HADOOP_VERSION} -Dlicense.skip=true -Dcheckstyle.skip=true >mvn.log 2>&1
