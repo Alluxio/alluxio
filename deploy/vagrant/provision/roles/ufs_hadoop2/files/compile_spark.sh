603c18ef9f870cd3e155f642ce4998deb21a3625
cc <myairia@gmail.com>
2015-05-15 12:28:29 +0800

    HADOOP_MAJOR_VERSION is not used

--- deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_spark.sh
+++ deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_spark.sh
@@ -1,6 +1,4 @@
 cd /spark
-# for version 2.4.1, major version is 2.4
-HADOOP_MAJOR_VERSION=`echo $HADOOP_VERSION | cut -d'.' -f1-2`
 if [[ "$SPARK_PROFILE" == "" ]]; then
   ./make-distribution.sh -Dhadoop.version=${HADOOP_VERSION} >/spark/make-distribution.log 2>&1
 else
