34f9f2c99132fef09b373670268c3cadd512a4fd
cc <myairia@gmail.com>
2015-05-16 02:22:43 +0800

    remove s3n from prefixes instead of limiting to hdfs

--- deploy/vagrant/provision/roles/ufs_s3/files/config_tachyon.sh
+++ deploy/vagrant/provision/roles/ufs_s3/files/config_tachyon.sh
@@ -17,7 +17,8 @@ sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
   -Dfs.s3n.awsAccessKeyId=${S3_ID} 
 " /tachyon/conf/tachyon-env.sh
 
+PREFIXES=`grep tachyon.underfs.hadoop.prefixes /tachyon/common/src/main/resources/tachyon-default.properties | sed "s|s3n://,||g"`
 # After this change, only S3UnderFileSystem will support s3n://
 sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
-  -Dtachyon.underfs.hadoop.prefixes=hdfs:// 
+  -D${PREFIXES}
 " /tachyon/conf/tachyon-env.sh
