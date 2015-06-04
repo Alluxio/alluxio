db7268ca7e45631df373a7e9d511510f9774b5af
Gene Pang <gene.pang@gmail.com>
2015-06-04 09:22:56 -0700

    move NettyUtils to common

--- servers/src/main/java/tachyon/worker/netty/NettyDataServer.java
+++ servers/src/main/java/tachyon/worker/netty/NettyDataServer.java
@@ -30,6 +30,7 @@ import com.google.common.base.Throwables;
 
 import tachyon.Constants;
 import tachyon.conf.TachyonConf;
+import tachyon.network.NettyUtils;
 import tachyon.worker.BlocksLocker;
 import tachyon.worker.DataServer;
 
