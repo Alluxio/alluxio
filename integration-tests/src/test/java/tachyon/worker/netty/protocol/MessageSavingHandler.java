b7d0f4a5934c077778c340967c3318e632165adb
Gene Pang <gene.pang@gmail.com>
2015-06-04 10:23:46 -0700

    fix import styles

--- integration-tests/src/test/java/tachyon/worker/netty/protocol/MessageSavingHandler.java
+++ integration-tests/src/test/java/tachyon/worker/netty/protocol/MessageSavingHandler.java
@@ -23,6 +23,7 @@ import org.junit.Assert;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
 import tachyon.network.protocol.RPCMessage;
 
 /**
