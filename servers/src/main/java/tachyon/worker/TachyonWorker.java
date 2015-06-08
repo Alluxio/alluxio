785e92ff435dabbabeeec799c85c5cbed5d8579e
Calvin Jia <jia.calvin@gmail.com>
2015-06-08 15:02:12 -0700

    Specify worker service handler as block worker specific.

--- servers/src/main/java/tachyon/worker/TachyonWorker.java
+++ servers/src/main/java/tachyon/worker/TachyonWorker.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -46,6 +46,7 @@ import tachyon.util.NetworkUtils;
 import tachyon.util.ThreadFactoryUtils;
 import tachyon.web.UIWebServer;
 import tachyon.web.WorkerUIWebServer;
+import tachyon.worker.block.WorkerServiceHandler;
 
 /**
  * Entry point for a worker daemon.
