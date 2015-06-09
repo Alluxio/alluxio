fff83a7c134307bfeb9a9da1f7f6246cf345b78b
Bin Fan <fanbin103@gmail.com>
2015-06-08 17:59:52 -0700

    Remove unused lockId field from BlockLock

--- servers/src/main/java/tachyon/worker/block/BlockLock.java
+++ servers/src/main/java/tachyon/worker/block/BlockLock.java
@@ -14,26 +14,18 @@ public class BlockLock implements ReadWriteLock {
     WRITE,  // A write lock
   }
 
-  static final AtomicLong mBlockLockId = new AtomicLong(0);
-
   private final ReentrantReadWriteLock mLock;
   /** The block Id this lock guards **/
   private final long mBlockId;
-  /** The unique id of each lock **/
-  private final long mLockId;
+
 
   public BlockLock(long blockId) {
     mBlockId = blockId;
-    mLockId = mBlockLockId.incrementAndGet();
     mLock = new ReentrantReadWriteLock();
   }
 
   public long getBlockId() {
     return mBlockId;
-  }
-
-  public long getLockId() {
-    return mLockId;
   }
 
   @Override
