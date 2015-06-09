8ec86692aad7aa9a04b2e0c060f93c0c844cf7f7
Bin Fan <fanbin103@gmail.com>
2015-06-08 18:10:22 -0700

    Update BlockLockManager to assign new lockId each time

--- servers/src/main/java/tachyon/worker/block/BlockLockManager.java
+++ servers/src/main/java/tachyon/worker/block/BlockLockManager.java
@@ -17,15 +17,17 @@ package tachyon.worker;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
+
 import tachyon.Constants;
+import tachyon.Pair;
 import tachyon.worker.block.BlockLock;
 
 /**
@@ -35,36 +37,69 @@ import tachyon.worker.block.BlockLock;
  */
 public class BlockLockManager {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
+  /** The unique id of each lock **/
+  static final AtomicLong mCurrentLockId = new AtomicLong(0);
 
-  /** A map from a block ID to its lock **/
+  /** A map from a block ID to its lock */
   private final Map<Long, BlockLock> mBlockIdToLockMap = new HashMap<Long, BlockLock>();
+  /** A map from a user ID to all the locks hold by this user */
+  private final Map<Long, Set<Long>> mUserIdToLockIdsMap = new HashMap<Long, Set<Long>>();
+  /** A map from a lock ID to the user ID holding this lock */
+  private final Map<Long, Pair<Long, Lock>> mLockIdToUserIdAndLockMap =
+      new HashMap<Long, Pair<Long, Lock>>();
 
   public BlockLockManager() {}
 
-  /**
-   * Get the lock for the given block id. If there is no such a lock yet, create one.
-   *
-   * @param blockId The id of the block
-   * @return the lock for this block
-   */
-  public synchronized Optional<BlockLock> getBlockLock(long blockId) {
+  public synchronized Optional<Long> lockBlock(long userId, long blockId,
+      BlockLock.BlockLockType blockLockType) {
     if (!mBlockIdToLockMap.containsKey(blockId)) {
       LOG.error("Cannot get lock for block {}: not exists", blockId);
       return Optional.absent();
     }
-    return Optional.of(mBlockIdToLockMap.get(blockId));
+    BlockLock blockLock = mBlockIdToLockMap.get(blockId);
+    Lock lock = null;
+    if (blockLockType == BlockLock.BlockLockType.READ) {
+      lock = blockLock.readLock();
+    } else if (blockLockType == BlockLock.BlockLockType.WRITE) {
+      lock = blockLock.writeLock();
+    }
+    lock.lock();
+    long lockId = createLockId(userId, lock);
+    return Optional.of(lockId);
   }
 
-  public synchronized Lock getBlockReadLock(long blockId) {
-    Preconditions.checkArgument(mBlockIdToLockMap.containsKey(blockId),
-        "Cannot get lock for block %s: not exists", blockId);
-    return mBlockIdToLockMap.get(blockId).readLock();
+  public synchronized boolean unlockBlock(long lockId) {
+    // TODO: implement me
+    // do unlock
+
+    cleanupLockId(lockId);
+    return true;
   }
 
-  public synchronized Lock getBlockWriteLock(long blockId) {
-    Preconditions.checkArgument(mBlockIdToLockMap.containsKey(blockId),
-        "Cannot get lock for block %s: not exists", blockId);
-    return mBlockIdToLockMap.get(blockId).writeLock();
+  private synchronized long createLockId(long userId, Lock lock) {
+    // TODO: implement me
+    long lockId = mCurrentLockId.getAndIncrement();
+    // mUserIdToAcquiredLockIdsMap.put(userId, lockID);
+    return lockId;
+  }
+
+  private synchronized boolean cleanupLockId(long lockId) {
+    // TODO: implement me
+    // mUserIdToAcquiredLockIdsMap.put(userId, lockID);
+    return true;
+  }
+
+  /**
+   * Validates the lock is hold by the given user for the given block.
+   *
+   * @param userId The ID of the user
+   * @param blockId The ID of the block
+   * @param lockId The ID of the lock
+   * @return true if validation succeeds, false otherwise
+   */
+  public synchronized boolean validateLockId(long userId, long blockId, long lockId) {
+    // TODO: implement me
+    return true;
   }
 
   /**
@@ -81,7 +116,6 @@ public class BlockLockManager {
     mBlockIdToLockMap.put(new BlockLock(blockId);
     return true;
   }
-
 
   /**
    * Remove a lock for the given block id.
