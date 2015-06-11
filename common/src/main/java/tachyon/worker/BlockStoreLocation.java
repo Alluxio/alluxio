48d65f82dc879020b65c0b179fdd6ef3b5dd3d1b
Bin Fan <fanbin103@gmail.com>
2015-06-11 10:45:43 -0700

    Add a helper function to derive StorageDirId from BlockLocation

--- common/src/main/java/tachyon/worker/BlockStoreLocation.java
+++ common/src/main/java/tachyon/worker/BlockStoreLocation.java
@@ -6,45 +6,54 @@ package tachyon.worker;
  */
 public class BlockStoreLocation {
   private static final int sAnyTier = -1;
-  private static final int sAnyDir = -2;
-  private final int mTier;
-  private final int mDir;
+  private static final int sAnyDir = -1;
+  private final int mTierAlias;
+  private final int mDirIndex;
 
   public static BlockStoreLocation anyTier() {
     return new BlockStoreLocation(sAnyTier, sAnyDir);
   }
 
-  public static BlockStoreLocation anyDirInTier(int tier) {
-    return new BlockStoreLocation(tier, sAnyDir);
+  public static BlockStoreLocation anyDirInTier(int tierAlias) {
+    return new BlockStoreLocation(tierAlias, sAnyDir);
   }
 
-  public BlockStoreLocation(int tier, int dir) {
-    mTier = tier;
-    mDir = dir;
+  public BlockStoreLocation(int tierAlias, int dirIndex) {
+    mTierAlias = tierAlias;
+    mDirIndex = dirIndex;
   }
+
+  // A helper function to derive StorageDirId from a BlockLocation.
+  // TODO: remove this method when master also understands BlockLocation
+  public long getStorageDirId() {
+    // TODO: double check if mTierAlias is really the level
+    return (mTierAlias << 24) + (mTierAlias << 16) + mDirIndex;
+  }
+
 
   public int tier() {
-    return mTier;
+    return mTierAlias;
   }
 
   public int dir() {
-    return mDir;
+    return mDirIndex;
   }
 
   @Override
   public String toString() {
     String result = "";
-    if (mDir == sAnyDir) {
+    if (mDirIndex == sAnyDir) {
       result += "any dir";
     } else {
-      result += "dir " + mDir;
+      result += "dir " + mDirIndex;
     }
 
-    if (mTier == sAnyTier) {
-      result += ", any tier"
+    if (mTierAlias == sAnyTier) {
+      result += ", any tier";
     } else {
-      result += ", tier " + mTier;
+      result += ", tier " + mTierAlias;
     }
     return result;
   }
+
 }
