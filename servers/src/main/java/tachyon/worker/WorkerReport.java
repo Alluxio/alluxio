d6d594ac0716ba346a8e649913932bafa2853a7f
Calvin Jia <jia.calvin@gmail.com>
2015-06-11 15:52:28 -0700

    Clean up block reporter code.

--- servers/src/main/java/tachyon/worker/WorkerReport.java
+++
@@ -1,8 +0,0 @@
-package tachyon.worker;
-
-/**
- * Represents a report from the worker to the master. Each worker type should implement its own
- * version of the report.
- */
-public class WorkerReport {
-}
