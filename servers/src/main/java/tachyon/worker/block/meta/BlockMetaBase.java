6d10621465c0e6ae81ad8d240d70a55c72caeea6
Bin Fan <fanbin103@gmail.com>
2015-06-10 17:21:22 -0700

    Update BlockMetaBase and its two subclasses, so blockSize is final in BlockMeta but not in TempBlockMeta

--- servers/src/main/java/tachyon/worker/block/meta/BlockMetaBase.java
+++ servers/src/main/java/tachyon/worker/block/meta/BlockMetaBase.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -23,21 +23,15 @@ import tachyon.worker.BlockStoreLocation;
  */
 public abstract class BlockMetaBase {
   protected final long mBlockId;
-  protected long mBlockSize;
-  protected StorageDir mDir;
+  protected final StorageDir mDir;
 
-  public BlockMetaBase(long blockId, long blockSize, StorageDir dir) {
+  public BlockMetaBase(long blockId, StorageDir dir) {
     mBlockId = blockId;
-    mBlockSize = blockSize;
     mDir = Preconditions.checkNotNull(dir);
   }
 
   public long getBlockId() {
     return mBlockId;
-  }
-
-  public long getBlockSize() {
-    return mBlockSize;
   }
 
   /**
@@ -53,4 +47,6 @@ public abstract class BlockMetaBase {
   }
 
   public abstract String getPath();
+
+  public abstract long getBlockSize();
 }
