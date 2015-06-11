2a76873f815c5a2296dbafde0bbe5cf085ab3fcf
Bin Fan <fanbin103@gmail.com>
2015-06-11 14:25:44 -0700

    Fix typo and other errors in TieredBlockStore.java

--- servers/src/main/java/tachyon/worker/block/TieredBlockStore.java
+++ servers/src/main/java/tachyon/worker/block/TieredBlockStore.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -281,10 +281,10 @@ public class TieredBlockStore implements BlockStore {
       }
     }
     // Step2: transfer blocks among tiers.
-    for (Pair<Long, Integer> entry : plan.toTransfer()) {
+    for (Pair<Long, BlockStoreLocation> entry : plan.toMove()) {
       long blockId = entry.getFirst();
-      int tierAlias = entry.getSecond();
-      if (!moveBlockNoLock(userId, blockId, tierAlias)) {
+      BlockStoreLocation newLocation = entry.getSecond();
+      if (!moveBlockNoLock(userId, blockId, newLocation)) {
         return false;
       }
     }
@@ -307,6 +307,11 @@ public class TieredBlockStore implements BlockStore {
 
   @Override
   public void registerMetaListener(BlockMetaEventListener listener) {
+    // TODO: implement me
+  }
 
+  @Override
+  public void registerAccessListener(BlockAccessEventListener listener) {
+    // TODO: implement me
   }
 }
