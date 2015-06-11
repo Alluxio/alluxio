5666cd366c92cd875939bbe17f010160b8cedc94
Calvin Jia <jia.calvin@gmail.com>
2015-06-11 14:42:20 -0700

    Implement generate report in block heartbeat reporter.

--- servers/src/main/java/tachyon/worker/block/BlockHeartbeatReport.java
+++ servers/src/main/java/tachyon/worker/block/BlockHeartbeatReport.java
@@ -8,15 +8,11 @@ import tachyon.worker.WorkerReport;
 /**
  * Container for the delta information in each worker to master heartbeat.
  */
-// TODO: This may be better as a thrift object
 public class BlockHeartbeatReport extends WorkerReport {
-  private final List<Long> mUsedBytesOnTiers;
   private final List<Long> mRemovedBlocks;
   private final Map<Long, List<Long>> mAddedBlocks;
 
-  public BlockHeartbeatReport(List<Long> usedBytesOnTier, List<Long> removedBlocks,
-      Map<Long, List<Long>> addedBlocks) {
-    mUsedBytesOnTiers = usedBytesOnTier;
+  public BlockHeartbeatReport(Map<Long, List<Long>> addedBlocks, List<Long> removedBlocks) {
     mRemovedBlocks = removedBlocks;
     mAddedBlocks = addedBlocks;
   }
@@ -27,9 +23,5 @@ public class BlockHeartbeatReport extends WorkerReport {
 
   public List<Long> getRemovedBlocks() {
     return mRemovedBlocks;
-  }
-
-  public List<Long> getUsedBytesOnTiers() {
-    return mUsedBytesOnTiers;
   }
 }
