0dd49ab748c9aefb13bb66ef565303cb5b5958e0
cc <myairia@gmail.com>
2015-06-12 22:29:28 +0800

    auto set security group

--- deploy/vagrant/bin/init.py
+++ deploy/vagrant/bin/init.py
@@ -4,6 +4,7 @@
 """
 1. Auto configure external python libraries like boto
 2. Check environment variables 
+3. Auto set up security group
 """
 
 from __future__ import print_function, with_statement
@@ -19,6 +20,28 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
+def get_or_make_group(conn, name):
+    groups = conn.get_all_security_groups()
+    group = [g for g in groups if g.name == name]
+    if len(group) > 0:
+        return group[0]
+    else:
+        print("Creating security group {name} in {region}".format(name=name, region=conn.region))
+        return conn.create_security_group(name, "Auto created by Tachyon deploy")
+
+
+def set_security_group(conn, name):
+    print("Setting up security group {} in {}".format(name, conn.region))
+    sg = get_or_make_group(conn, name)
+    if sg.rules != []:
+        print('security group {} in {} already has rules, no modification will happen then'.format(name, conn.region))
+        return
+    proto = ['tcp', 'udp']
+    authorized_ip = '0.0.0.0/0' # all IP
+    for p in proto:
+        sg.authorize(p, 0, 65535, authorized_ip)
+ 
 
 args = parse_args()
 if args.provider == 'aws':
@@ -41,3 +64,20 @@ if args.provider == 'aws':
             '[Credentials]', 
             'aws_access_key_id = ' + access_key, 
             'aws_secret_access_key = ' + secret_key]))
+
+    import boto
+    from boto import ec2
+    import yaml
+
+    ec2_conf = yaml.load(open('conf/ec2.yml'))
+    region = ec2_conf['Region']
+    sg = ec2_conf['Security_Group']
+
+    try:
+        conn = ec2.connect_to_region(region)
+        print(conn.region)
+    except Exception as e:
+        print((e), file=stderr)
+        sys.exit(1)
+    set_security_group(conn, sg)
+
