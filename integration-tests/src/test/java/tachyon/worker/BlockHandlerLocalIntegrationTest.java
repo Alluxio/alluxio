29a7046cc0f0f24524c398cd71191f9ed4c4eb37
Calvin Jia <jia.calvin@gmail.com>
2015-06-15 11:27:08 -0700

    Remove test dependencies on block handler.

--- integration-tests/src/test/java/tachyon/worker/BlockHandlerLocalIntegrationTest.java
+++ integration-tests/src/test/java/tachyon/worker/BlockHandlerLocalIntegrationTest.java
@@ -13,125 +13,126 @@
  * the License.
  */
 
-package tachyon.worker;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import tachyon.Constants;
-import tachyon.TachyonURI;
-import tachyon.TestUtils;
-import tachyon.client.TachyonFS;
-import tachyon.client.TachyonFSTestUtils;
-import tachyon.client.TachyonFile;
-import tachyon.client.WriteType;
-import tachyon.master.LocalTachyonCluster;
-import tachyon.worker.block.io.BlockHandler;
-
-/**
- * Integration tests for <code>tachyon.client.BlockHandlerLocal</code>.
- */
-public class BlockHandlerLocalIntegrationTest {
-  private static LocalTachyonCluster sLocalTachyonCluster = null;
-  private static TachyonFS sTFS = null;
-
-  @AfterClass
-  public static final void afterClass() throws Exception {
-    sLocalTachyonCluster.stop();
-  }
-
-  @BeforeClass
-  public static final void beforeClass() throws IOException {
-    sLocalTachyonCluster = new LocalTachyonCluster(10000, 1000, Constants.GB);
-    sLocalTachyonCluster.start();
-    sTFS = sLocalTachyonCluster.getClient();
-  }
-
-  @Test
-  public void directByteBufferWriteTest() throws IOException {
-    ByteBuffer buf = ByteBuffer.allocateDirect(100);
-    buf.put(TestUtils.getIncreasingByteArray(100));
-
-    int fileId = sTFS.createFile(new TachyonURI(TestUtils.uniqPath()));
-    long blockId = sTFS.getBlockId(fileId, 0);
-    String filename = sTFS.getLocalBlockTemporaryPath(blockId, 100);
-    BlockHandler handler = BlockHandler.get(filename);
-    try {
-      handler.append(0, buf);
-      sTFS.cacheBlock(blockId);
-      TachyonFile file = sTFS.getFile(fileId);
-      long fileLen = file.length();
-      Assert.assertEquals(100, fileLen);
-    } finally {
-      handler.close();
-    }
-  }
-
-  @Test
-  public void heapByteBufferwriteTest() throws IOException {
-    int fileId = sTFS.createFile(new TachyonURI(TestUtils.uniqPath()));
-    long blockId = sTFS.getBlockId(fileId, 0);
-    String filename = sTFS.getLocalBlockTemporaryPath(blockId, 100);
-    BlockHandler handler = BlockHandler.get(filename);
-    byte[] buf = TestUtils.getIncreasingByteArray(100);
-    try {
-      handler.append(0, ByteBuffer.wrap(buf));
-      sTFS.cacheBlock(blockId);
-      TachyonFile file = sTFS.getFile(fileId);
-      long fileLen = file.length();
-      Assert.assertEquals(100, fileLen);
-    } finally {
-      handler.close();
-    }
-  }
-
-  @Test
-  public void readExceptionTest() throws IOException {
-    int fileId =
-        TachyonFSTestUtils.createByteFile(sTFS, TestUtils.uniqPath(), WriteType.MUST_CACHE, 100);
-    TachyonFile file = sTFS.getFile(fileId);
-    String filename = file.getLocalFilename(0);
-    BlockHandler handler = BlockHandler.get(filename);
-    try {
-      Exception exception = null;
-      try {
-        handler.read(101, 10);
-      } catch (IOException e) {
-        exception = e;
-      }
-      Assert.assertEquals("offset(101) is larger than file length(100)",
-          exception.getMessage());
-      try {
-        handler.read(10, 100);
-      } catch (IOException e) {
-        exception = e;
-      }
-      Assert.assertEquals("offset(10) plus length(100) is larger than file length(100)",
-          exception.getMessage());
-    } finally {
-      handler.close();
-    }
-  }
-
-  @Test
-  public void readTest() throws IOException {
-    int fileId =
-        TachyonFSTestUtils.createByteFile(sTFS, TestUtils.uniqPath(), WriteType.MUST_CACHE, 100);
-    TachyonFile file = sTFS.getFile(fileId);
-    String filename = file.getLocalFilename(0);
-    BlockHandler handler = BlockHandler.get(filename);
-    try {
-      ByteBuffer buf = handler.read(0, 100);
-      Assert.assertEquals(TestUtils.getIncreasingByteBuffer(100), buf);
-      buf = handler.read(0, -1);
-      Assert.assertEquals(TestUtils.getIncreasingByteBuffer(100), buf);
-    } finally {
-      handler.close();
-    }
-  }
-}
+// TODO: Replace this integration test with one for the new block operators
+//package tachyon.worker;
+//
+//import java.io.IOException;
+//import java.nio.ByteBuffer;
+//
+//import org.junit.AfterClass;
+//import org.junit.Assert;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//import tachyon.Constants;
+//import tachyon.TachyonURI;
+//import tachyon.TestUtils;
+//import tachyon.client.TachyonFS;
+//import tachyon.client.TachyonFSTestUtils;
+//import tachyon.client.TachyonFile;
+//import tachyon.client.WriteType;
+//import tachyon.master.LocalTachyonCluster;
+//import tachyon.worker.block.io.BlockHandler;
+//
+///**
+// * Integration tests for <code>tachyon.client.BlockHandlerLocal</code>.
+// */
+//public class BlockHandlerLocalIntegrationTest {
+//  private static LocalTachyonCluster sLocalTachyonCluster = null;
+//  private static TachyonFS sTFS = null;
+//
+//  @AfterClass
+//  public static final void afterClass() throws Exception {
+//    sLocalTachyonCluster.stop();
+//  }
+//
+//  @BeforeClass
+//  public static final void beforeClass() throws IOException {
+//    sLocalTachyonCluster = new LocalTachyonCluster(10000, 1000, Constants.GB);
+//    sLocalTachyonCluster.start();
+//    sTFS = sLocalTachyonCluster.getClient();
+//  }
+//
+//  @Test
+//  public void directByteBufferWriteTest() throws IOException {
+//    ByteBuffer buf = ByteBuffer.allocateDirect(100);
+//    buf.put(TestUtils.getIncreasingByteArray(100));
+//
+//    int fileId = sTFS.createFile(new TachyonURI(TestUtils.uniqPath()));
+//    long blockId = sTFS.getBlockId(fileId, 0);
+//    String filename = sTFS.getLocalBlockTemporaryPath(blockId, 100);
+//    BlockHandler handler = BlockHandler.get(filename);
+//    try {
+//      handler.append(0, buf);
+//      sTFS.cacheBlock(blockId);
+//      TachyonFile file = sTFS.getFile(fileId);
+//      long fileLen = file.length();
+//      Assert.assertEquals(100, fileLen);
+//    } finally {
+//      handler.close();
+//    }
+//  }
+//
+//  @Test
+//  public void heapByteBufferwriteTest() throws IOException {
+//    int fileId = sTFS.createFile(new TachyonURI(TestUtils.uniqPath()));
+//    long blockId = sTFS.getBlockId(fileId, 0);
+//    String filename = sTFS.getLocalBlockTemporaryPath(blockId, 100);
+//    BlockHandler handler = BlockHandler.get(filename);
+//    byte[] buf = TestUtils.getIncreasingByteArray(100);
+//    try {
+//      handler.append(0, ByteBuffer.wrap(buf));
+//      sTFS.cacheBlock(blockId);
+//      TachyonFile file = sTFS.getFile(fileId);
+//      long fileLen = file.length();
+//      Assert.assertEquals(100, fileLen);
+//    } finally {
+//      handler.close();
+//    }
+//  }
+//
+//  @Test
+//  public void readExceptionTest() throws IOException {
+//    int fileId =
+//        TachyonFSTestUtils.createByteFile(sTFS, TestUtils.uniqPath(), WriteType.MUST_CACHE, 100);
+//    TachyonFile file = sTFS.getFile(fileId);
+//    String filename = file.getLocalFilename(0);
+//    BlockHandler handler = BlockHandler.get(filename);
+//    try {
+//      Exception exception = null;
+//      try {
+//        handler.read(101, 10);
+//      } catch (IOException e) {
+//        exception = e;
+//      }
+//      Assert.assertEquals("offset(101) is larger than file length(100)",
+//          exception.getMessage());
+//      try {
+//        handler.read(10, 100);
+//      } catch (IOException e) {
+//        exception = e;
+//      }
+//      Assert.assertEquals("offset(10) plus length(100) is larger than file length(100)",
+//          exception.getMessage());
+//    } finally {
+//      handler.close();
+//    }
+//  }
+//
+//  @Test
+//  public void readTest() throws IOException {
+//    int fileId =
+//        TachyonFSTestUtils.createByteFile(sTFS, TestUtils.uniqPath(), WriteType.MUST_CACHE, 100);
+//    TachyonFile file = sTFS.getFile(fileId);
+//    String filename = file.getLocalFilename(0);
+//    BlockHandler handler = BlockHandler.get(filename);
+//    try {
+//      ByteBuffer buf = handler.read(0, 100);
+//      Assert.assertEquals(TestUtils.getIncreasingByteBuffer(100), buf);
+//      buf = handler.read(0, -1);
+//      Assert.assertEquals(TestUtils.getIncreasingByteBuffer(100), buf);
+//    } finally {
+//      handler.close();
+//    }
+//  }
+//}
