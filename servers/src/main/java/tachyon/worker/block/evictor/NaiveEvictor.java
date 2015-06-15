20adc9bb5dc9e47da142f62935c5a6b3d32ad820
Bin Fan <fanbin103@gmail.com>
2015-06-15 11:23:01 -0700

    Update Naive Evictor

--- servers/src/main/java/tachyon/worker/block/evictor/NaiveEvictor.java
+++ servers/src/main/java/tachyon/worker/block/evictor/NaiveEvictor.java
@@ -15,25 +15,81 @@
 
 package tachyon.worker.block.evictor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+
+import tachyon.Pair;
 import tachyon.worker.BlockStoreLocation;
 import tachyon.worker.block.BlockAccessEventListener;
 import tachyon.worker.block.BlockMetadataManager;
+import tachyon.worker.block.meta.BlockMeta;
+import tachyon.worker.block.meta.StorageDir;
+import tachyon.worker.block.meta.StorageTier;
 
 /**
  * A simple evictor that evicts random blocks until the required size in the target tier is met.
  */
 public class NaiveEvictor implements Evictor, BlockAccessEventListener {
-  private final BlockMetadataManager mMetadata;
+  private final BlockMetadataManager mMetaManager;
 
   public NaiveEvictor(BlockMetadataManager metadata) {
-    mMetadata = metadata;
+    mMetaManager = Preconditions.checkNotNull(metadata);
   }
 
   @Override
   public EvictionPlan freeSpace(long bytes, BlockStoreLocation location) {
+    List<Pair<Long, BlockStoreLocation>> toMove = new ArrayList<Pair<Long, BlockStoreLocation>>();
+    List<Long> toEvict = new ArrayList<Long>();
+
+    long sizeFreed = 0;
+    if (location == BlockStoreLocation.anyTier()) {
+      for (StorageTier tier : mMetaManager.getTiers()) {
+        for (StorageDir dir : tier.getStorageDirs()) {
+          for (BlockMeta block : dir.getBlocks()) {
+            toEvict.add(block.getBlockId());
+            sizeFreed += block.getBlockSize();
+            if (sizeFreed >= bytes) {
+              return new EvictionPlan(toMove, toEvict);
+            }
+          }
+        }
+      }
+      return new EvictionPlan(toMove, toEvict);
+    }
+
+    int tierAlias = location.tier();
+    StorageTier tier = mMetaManager.getTier(tierAlias);
+    if (location == BlockStoreLocation.anyDirInTier(tierAlias)) {
+      // Loop over all dirs in the given tier
+      for (StorageDir dir : tier.getStorageDirs()) {
+        for (BlockMeta block : dir.getBlocks()) {
+          toEvict.add(block.getBlockId());
+          sizeFreed += block.getBlockSize();
+          if (sizeFreed >= bytes) {
+            return new EvictionPlan(toMove, toEvict);
+          }
+        }
+      }
+      return new EvictionPlan(toMove, toEvict);
+    }
+
+    int dirIndex = location.dir();
+    StorageDir dir = tier.getDir(dirIndex);
+    for (BlockMeta block : dir.getBlocks()) {
+      toEvict.add(block.getBlockId());
+      sizeFreed += block.getBlockSize();
+      if (sizeFreed >= bytes) {
+        return new EvictionPlan(toMove, toEvict);
+      }
+    }
     return new EvictionPlan(null, null);
   }
 
   @Override
-  public void onAccessBlock(long userId, long blockId) {}
+  public void onAccessBlock(long userId, long blockId) {
+    // do nothing
+  }
+
 }
