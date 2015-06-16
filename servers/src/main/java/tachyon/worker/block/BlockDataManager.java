f40573595f700160d4cbefb4ebeb230460ccf36b
Bin Fan <fanbin103@gmail.com>
2015-06-15 23:46:41 -0700

    Implement unlockBlock with blockId and userId, for debug only

--- servers/src/main/java/tachyon/worker/block/BlockDataManager.java
+++ servers/src/main/java/tachyon/worker/block/BlockDataManager.java
@@ -426,6 +426,11 @@ public class BlockDataManager {
     return mBlockStore.unlockBlock(lockId);
   }
 
+  // TODO: for debug only.
+  public boolean unlockBlock(long userId, long blockId) {
+    return mBlockStore.unlockBlock(userId, blockId);
+  }
+
   /**
    * Handles the heartbeat from a client.
    *
