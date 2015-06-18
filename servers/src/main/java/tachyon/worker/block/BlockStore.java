ad792e56c7cfba605c90ff67531a1e9edbe24215
Bin Fan <fanbin103@gmail.com>
2015-06-18 13:48:37 -0700

    Update javadoc of BlockStore interface

--- servers/src/main/java/tachyon/worker/block/BlockStore.java
+++ servers/src/main/java/tachyon/worker/block/BlockStore.java
@@ -26,16 +26,14 @@ import tachyon.worker.block.meta.BlockMeta;
 import tachyon.worker.block.meta.TempBlockMeta;
 
 /**
- * This interface represents a blob store that manages and serves all the blobs (i.e., blocks) in
- * the local storage.
- * <p>
- * TODO: rename this store to blob store.
+ * This interface represents a blob store that manages and serves all the blobs (although
+ * they are referred to as blocks) in the local storage.
  */
 public interface BlockStore {
 
   /**
-   * Locks a block for a given user to read it if it exists, and guards the subsequent
-   * READ operations on this block. In case the block doesn't exist, return absent.
+   * Locks a block for a given, and guards the subsequent READ operations on this block. In case the
+   * block doesn't exist, return absent.
    *
    * @param userId the ID of the user to lock this block
    * @param blockId the ID of the block to lock
@@ -44,7 +42,7 @@ public interface BlockStore {
   Optional<Long> lockBlock(long userId, long blockId);
 
   /**
-   * Release an acquired lock on a block previously by {@link #lockBlock}.
+   * Releases an acquired lock on a block based on a lockId returned by {@link #lockBlock}.
    *
    * @param lockId the ID of the lock returned by {@link #lockBlock}
    * @return true if the lock has been released, false otherwise
@@ -82,7 +80,7 @@ public interface BlockStore {
    * @param userId the ID of the user to get this file
    * @param blockId the ID of the block
    * @param lockId the ID of the lock
-   * @return the block meta, or {@link Optional#absent()} if the block can not be found.
+   * @return the block meta, or absent if the block can not be found.
    */
   Optional<BlockMeta> getBlockMeta(long userId, long blockId, long lockId);
 
@@ -94,7 +92,6 @@ public interface BlockStore {
    * @param userId the ID of the user
    * @param blockId the ID of a temp block
    * @return true if success, false otherwise
-   * @throws IOException
    */
   boolean commitBlock(long userId, long blockId);
 
@@ -106,25 +103,24 @@ public interface BlockStore {
    * @param userId the ID of the user
    * @param blockId the ID of a temp block
    * @return true if success, false otherwise
-   * @throws IOException
    */
   boolean abortBlock(long userId, long blockId);
 
   /**
-   * Requests to increase the size of a temp block. Since a temp block is "private" to the writer,
-   * this requires no proceeding lock acquired.
+   * Requests to increase the size of a temp block. Since a temp block is "private" to the writer
+   * client, this operation requires no proceeding lock acquired.
    *
    * @param userId the ID of the user to request space
    * @param blockId the ID of the temp block
-   * @param size the amount of more space to request in bytes
+   * @param moreBytes the amount of more space to request in bytes
    * @return true if success, false otherwise
    * @throws IOException
    */
-  boolean requestSpace(long userId, long blockId, long size) throws IOException;
+  boolean requestSpace(long userId, long blockId, long moreBytes) throws IOException;
 
   /**
    * Creates a writer to write data to a temp block. Since the temp block is "private" to the
-   * writer, this requires no proceeding lock acquired.
+   * writer, this operation requires no proceeding lock acquired.
    *
    * @param userId the ID of the user to get the writer
    * @param blockId the ID of the temp block
@@ -136,7 +132,7 @@ public interface BlockStore {
   /**
    * Creates a reader of an existing block to read data from this block.
    * <p>
-   * This method requires the lock ID returned by a proceeding {@link #lockBlock}.
+   * This operation requires the lock ID returned by a proceeding {@link #lockBlock}.
    *
    * @param userId the ID of the user to get the reader
    * @param blockId the ID of an existing block
@@ -158,8 +154,7 @@ public interface BlockStore {
   boolean moveBlock(long userId, long blockId, BlockStoreLocation newLocation) throws IOException;
 
   /**
-   * Removes an existing block from a specific location. If the block can not be found, return
-   * false.
+   * Removes an existing block. If the block can not be found in this store, return false.
    *
    * @param userId the ID of the user to remove a block
    * @param blockId the ID of an existing block
@@ -169,8 +164,8 @@ public interface BlockStore {
   boolean removeBlock(long userId, long blockId) throws IOException;
 
   /**
-   * Notifies the block store that a block was accessed (so the block store could update accordingly
-   * the evictor and allocator).
+   * Notifies the block store that a block was accessed so the block store could update accordingly
+   * the registered listeners such as evictor and allocator on block access.
    *
    * @param userId the ID of the user to access a block
    * @param blockId the ID of an accessed block
@@ -194,14 +189,15 @@ public interface BlockStore {
   boolean cleanupUser(long userId);
 
   /**
-   * Frees a certain amount of space in the specified location according to the eviction policy.
+   * Frees space to make a specific amount of bytes available in the location.
    *
    * @param userId the user ID
-   * @param size the amount of space to free in bytes
+   * @param availableBytes the amount of free space in bytes
    * @param location the location to free space
    * @return true if success, false otherwise
    */
-  boolean freeSpace(long userId, long size, BlockStoreLocation location) throws IOException;
+  boolean freeSpace(long userId, long availableBytes, BlockStoreLocation location) throws
+      IOException;
 
   /**
    * Registers a {@link BlockMetaEventListener} to this block store.
