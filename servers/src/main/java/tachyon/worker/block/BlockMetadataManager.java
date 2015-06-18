2a0a9c40a57ae07cdb2f32198a54967b7fa3a62b
Bin Fan <fanbin103@gmail.com>
2015-06-18 14:21:48 -0700

    Add getAvailableBytes for metadata manager given a location

--- servers/src/main/java/tachyon/worker/block/BlockMetadataManager.java
+++ servers/src/main/java/tachyon/worker/block/BlockMetadataManager.java
@@ -76,6 +76,33 @@ public class BlockMetadataManager {
   }
 
   /**
+   * Gets the amount of available space in given location in bytes.
+   *
+   * @param location location the check available bytes
+   * @return available bytes
+   */
+  public synchronized long getAvailableBytes(BlockStoreLocation location) {
+    long spaceAvailable = 0;
+
+    if (location.equals(BlockStoreLocation.anyTier())) {
+      for (StorageTier tier : getTiers()) {
+        spaceAvailable += tier.getAvailableBytes();
+      }
+      return spaceAvailable;
+    }
+
+    int tierAlias = location.tierAlias();
+    StorageTier tier = getTier(tierAlias);
+    if (location.equals(BlockStoreLocation.anyDirInTier(tierAlias))) {
+      return tier.getAvailableBytes();
+    }
+
+    int dirIndex = location.dir();
+    StorageDir dir = tier.getDir(dirIndex);
+    return dir.getAvailableBytes();
+  }
+
+  /**
    * Checks if the storage has a given block.
    *
    * @param blockId the block ID
