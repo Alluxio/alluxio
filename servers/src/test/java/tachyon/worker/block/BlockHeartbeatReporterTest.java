26bd3bf08c55a52742bf2d0877e3fcc6a942c344
Calvin Jia <jia.calvin@gmail.com>
2015-06-23 11:19:02 -0700

    Add unit tests for block heartbeat reporter.

---
+++ servers/src/test/java/tachyon/worker/block/BlockHeartbeatReporterTest.java
@@ -0,0 +1,83 @@
+package tachyon.worker.block;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tachyon.worker.BlockStoreLocation;
+
+import java.util.List;
+import java.util.Map;
+
+public class BlockHeartbeatReporterTest {
+  private static final int USER_ID = 1;
+  private static final BlockStoreLocation MEM_LOC = new BlockStoreLocation(1, 0);
+  private static final BlockStoreLocation SSD_LOC = new BlockStoreLocation(2, 0);
+  private static final BlockStoreLocation HDD_LOC = new BlockStoreLocation(3, 0);
+  BlockHeartbeatReporter mReporter;
+
+  @Before
+  public final void before() {
+    mReporter = new BlockHeartbeatReporter();
+  }
+
+  private void moveBlock(long blockId, BlockStoreLocation loc) {
+    mReporter.postMoveBlock(USER_ID, blockId, loc);
+  }
+
+  // Tests Empty Report
+  @Test
+  public void generateReportEmptyTest() {
+    BlockHeartbeatReport report = mReporter.generateReport();
+    Assert.assertTrue(report.getAddedBlocks().isEmpty());
+    Assert.assertTrue(report.getRemovedBlocks().isEmpty());
+  }
+
+  // Tests a report is correctly generated after moving blocks
+  @Test
+  public void generateReportSimpleMoveTest() {
+    Long block1 = 1L;
+    Long block2 = 2L;
+    Long block3 = 3L;
+    moveBlock(block1, MEM_LOC);
+    moveBlock(block2, SSD_LOC);
+    moveBlock(block3, HDD_LOC);
+    BlockHeartbeatReport report = mReporter.generateReport();
+    Map<Long, List<Long>> addedBlocks = report.getAddedBlocks();
+
+    // Block1 moved to memory
+    List<Long> addedBlocksMem = addedBlocks.get(MEM_LOC.getStorageDirId());
+    Assert.assertEquals(addedBlocksMem.size(), 1);
+    Assert.assertEquals(addedBlocksMem.get(0), block1);
+
+    // Block2 moved to ssd
+    List<Long> addedBlocksSsd = addedBlocks.get(SSD_LOC.getStorageDirId());
+    Assert.assertEquals(addedBlocksSsd.size(), 1);
+    Assert.assertEquals(addedBlocksSsd.get(0), block2);
+
+    // Block3 moved to hdd
+    List<Long> addedBlocksHdd = addedBlocks.get(HDD_LOC.getStorageDirId());
+    Assert.assertEquals(addedBlocksHdd.size(), 1);
+    Assert.assertEquals(addedBlocksHdd.get(0), block3);
+
+    // All blocks should be "removed" to force an update of their location in master
+    List<Long> removedBlocks = report.getRemovedBlocks();
+    Assert.assertEquals(removedBlocks.size(), 3);
+    Assert.assertTrue(removedBlocks.contains(block1));
+    Assert.assertTrue(removedBlocks.contains(block2));
+    Assert.assertTrue(removedBlocks.contains(block3));
+  }
+
+  // Tests generating a report clears the state of the reporter
+  @Test
+  public void generateReportStateClearTest() {
+    Long block1 = 1L;
+    moveBlock(block1, MEM_LOC);
+    BlockHeartbeatReport report = mReporter.generateReport();
+    Assert.assertFalse(report.getAddedBlocks().isEmpty());
+    Assert.assertFalse(report.getRemovedBlocks().isEmpty());
+
+    BlockHeartbeatReport nextReport = mReporter.generateReport();
+    Assert.assertTrue(nextReport.getAddedBlocks().isEmpty());
+    Assert.assertTrue(nextReport.getRemovedBlocks().isEmpty());
+  }
+}
