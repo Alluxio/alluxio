83ad637e49c7d2878a9129a16dc229d07efac6af
Gene Pang <gene.pang@gmail.com>
2015-06-24 12:05:38 -0700

    examine the local write config option

--- clients/unshaded/src/main/java/tachyon/client/BlockOutStream.java
+++ clients/unshaded/src/main/java/tachyon/client/BlockOutStream.java
@@ -58,7 +58,9 @@ public abstract class BlockOutStream extends OutStream {
   public static BlockOutStream get(TachyonFile tachyonFile, WriteType opType, int blockIndex,
       long initialBytes, TachyonConf tachyonConf) throws IOException {
 
-    if (tachyonFile.mTachyonFS.hasLocalWorker()) {
+    if (tachyonFile.mTachyonFS.hasLocalWorker()
+        && tachyonConf.getBoolean(Constants.USER_ENABLE_LOCAL_WRITE,
+            Constants.DEFAULT_USER_ENABLE_LOCAL_WRITE)) {
       LOG.info("Writing with local stream.");
       return new LocalBlockOutStream(tachyonFile, opType, blockIndex, initialBytes, tachyonConf);
     }
