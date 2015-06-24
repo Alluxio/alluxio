afe42a84f82bde0581db67448a24773ee6938886
Bin Fan <fanbin103@gmail.com>
2015-06-24 14:44:30 -0700

    Add Unit Test of StorageTier

--- servers/src/main/java/tachyon/worker/block/meta/StorageTier.java
+++ servers/src/main/java/tachyon/worker/block/meta/StorageTier.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -87,23 +87,8 @@ public class StorageTier {
     return mDirs.get(dirIndex);
   }
 
-  public Set<StorageDir> getStorageDirs() {
-    return new HashSet<StorageDir>(mDirs);
+  public List<StorageDir> getStorageDirs() {
+    return mDirs;
   }
 
-  /**
-   * Returns the BlockMeta for a given blockId.
-   *
-   * @param blockId the ID of the block
-   * @return BlockMeta
-   * @throws IOException if no BlockMeta for this blockId is found in this StorageDir
-   */
-  public BlockMeta getBlockMeta(long blockId) throws IOException {
-    for (StorageDir dir : mDirs) {
-      if (dir.hasBlockMeta(blockId)) {
-        return dir.getBlockMeta(blockId);
-      }
-    }
-    throw new IOException("Cannot get blockId " + blockId + " in tier " + this);
-  }
 }
