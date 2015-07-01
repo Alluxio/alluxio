287c2dcf3788abdc84d89b03c206bf13da0be12b
cc <myairia@gmail.com>
2015-07-01 12:18:11 +0800

    remove onPreloadBlock interface

--- servers/src/main/java/tachyon/worker/block/BlockStoreEventListenerBase.java
+++ servers/src/main/java/tachyon/worker/block/BlockStoreEventListenerBase.java
@@ -18,9 +18,6 @@ package tachyon.worker.block;
 public class BlockStoreEventListenerBase implements BlockStoreEventListener {
 
   @Override
-  public void onPreloadBlock(long blockId) {}
-
-  @Override
   public void onAccessBlock(long userId, long blockId) {}
 
   @Override
