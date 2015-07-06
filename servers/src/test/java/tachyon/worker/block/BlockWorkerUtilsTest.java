2b6d45508192c28f6b957a1ec18eb4fc075f9a97
Yours-shupeng <a051587782106@163.com>
2015-07-06 16:25:13 +0800

    fix assertEquals

--- servers/src/test/java/tachyon/worker/block/BlockWorkerUtilsTest.java
+++ servers/src/test/java/tachyon/worker/block/BlockWorkerUtilsTest.java
@@ -73,19 +73,19 @@ public class BlockWorkerUtilsTest {
 
     InetSocketAddress masterAddress = BlockWorkerUtils.getMasterAddress(mCustomPropsTachyonConf);
     Assert.assertNotNull(masterAddress);
-    Assert.assertEquals(masterAddress, new InetSocketAddress("RemoteMaster1", 10000));
+    Assert.assertEquals(new InetSocketAddress("RemoteMaster1", 10000), masterAddress);
 
     masterAddress = BlockWorkerUtils.getMasterAddress(mNullMasterHostNameTachyonConf);
     Assert.assertNotNull(masterAddress);
-    Assert.assertEquals(masterAddress, new InetSocketAddress(defaultHostname, 20000));
+    Assert.assertEquals(new InetSocketAddress(defaultHostname, 20000), masterAddress);
 
     masterAddress = BlockWorkerUtils.getMasterAddress(mNullMasterPortTachyonConf);
     Assert.assertNotNull(masterAddress);
-    Assert.assertEquals(masterAddress, new InetSocketAddress("RemoteMaster3", defaultPort));
+    Assert.assertEquals(new InetSocketAddress("RemoteMaster3", defaultPort), masterAddress);
 
     masterAddress = BlockWorkerUtils.getMasterAddress(mNullTachyonConf);
     Assert.assertNotNull(masterAddress);
-    Assert.assertEquals(masterAddress, new InetSocketAddress(defaultHostname, defaultPort));
+    Assert.assertEquals(new InetSocketAddress(defaultHostname, defaultPort), masterAddress);
   }
 
   @Test
@@ -95,10 +95,10 @@ public class BlockWorkerUtilsTest {
 
     InetSocketAddress workerAddress = BlockWorkerUtils.getWorkerAddress(mCustomPropsTachyonConf);
     Assert.assertNotNull(workerAddress);
-    Assert.assertEquals(workerAddress, new InetSocketAddress(defaultHostname, 10001));
+    Assert.assertEquals(new InetSocketAddress(defaultHostname, 10001), workerAddress);
 
     workerAddress = BlockWorkerUtils.getWorkerAddress(mNullTachyonConf);
     Assert.assertNotNull(workerAddress);
-    Assert.assertEquals(workerAddress, new InetSocketAddress(defaultHostname, defaultPort));
+    Assert.assertEquals(new InetSocketAddress(defaultHostname, defaultPort), workerAddress);
   }
 }
