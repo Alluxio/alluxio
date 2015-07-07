6a2fa71b6d090b59fe1a20b23a6831055a8242ae
dongche <dong1.chen@intel.com>
2015-07-07 07:17:53 -0400

    [TACHYON-601] Login Mechanism: Add OS_LOGIN_MODULE

---
+++ common/src/test/java/tachyon/security/LoginModuleTest.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the University of California, Berkeley under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package tachyon.security;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import java.security.Principal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for the login module defined in {@link tachyon.security.UserInformation}
+ */
+public class LoginModuleTest {
+
+  /**
+   * This test verify whether the OS login module works in JAAS framework.
+   * @throws Exception
+   */
+  @Test
+  public void osLoginModuleTest() throws Exception {
+    Class<? extends Principal> clazz = UserInformation.getOsPrincipalClass();
+    Subject subject = new Subject();
+
+    // login and add OS user into subject
+    LoginContext loginContext = new LoginContext("simple", subject, null,
+        new UserInformation.TachyonJaasConfiguration());
+    loginContext.login();
+
+    // verify whether OS user is fetched
+    Assert.assertFalse(subject.getPrincipals(clazz).isEmpty());
+  }
+}
