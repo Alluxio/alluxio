bc6c2f976266c467f4c654207497a4804ce06ef6
Qifan Pu <ooq@users.noreply.github.com>
2015-07-08 16:10:13 -0700

    minor fix in BlockMetadataManagerView.java

--- servers/src/main/java/tachyon/worker/block/BlockMetadataManagerView.java
+++ servers/src/main/java/tachyon/worker/block/BlockMetadataManagerView.java
@@ -55,7 +55,7 @@ public class BlockMetadataManagerView {
   public BlockMetadataManagerView(BlockMetadataManager manager, Set<Integer> pinnedInodes,
       Set<Long> lockedBlocks) throws IOException {
     mMetadataManager = Preconditions.checkNotNull(manager);
-    mPinnedInodes.addAll(Preconditions.checkNotNull(mPinnedInodes));
+    mPinnedInodes.addAll(Preconditions.checkNotNull(pinnedInodes));
     mLockedBlocks.addAll(Preconditions.checkNotNull(lockedBlocks));
 
     // iteratively create all StorageTierViews and StorageDirViews
