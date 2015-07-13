e9b1a22f2cee80c98e0005cd827958f357379926
cc <myairia@gmail.com>
2015-07-13 09:32:07 +0800

    clean up validation of eviction plan after meta data manager view is added
    fix javadoc

--- servers/src/main/java/tachyon/worker/block/evictor/LRUEvictor.java
+++ servers/src/main/java/tachyon/worker/block/evictor/LRUEvictor.java
@@ -180,7 +180,6 @@ public class LRUEvictor extends BlockStoreEventListenerBase implements Evictor {
     return candidateDirView;
   }
 
-
   @Override
   public EvictionPlan freeSpaceWithView(long bytesToBeAvailable, BlockStoreLocation location,
       BlockMetadataManagerView view) throws IOException {
@@ -194,31 +193,8 @@ public class LRUEvictor extends BlockStoreEventListenerBase implements Evictor {
     if (candidateDir == null) {
       return null;
     }
-    if (plan.isEmpty()) {
-      return plan;
-    }
 
-    // TODO: remove this check after implementation of metadata view
-    // assure all blocks are in the store, if not, remove from plan and lru cache
-    Iterator<Pair<Long, BlockStoreLocation>> moveIt = plan.toMove().iterator();
-    while (moveIt.hasNext()) {
-      long id = moveIt.next().getFirst();
-      if (null == mManagerView.getBlockMeta(id)) {
-        mLRUCache.remove(id);
-        moveIt.remove();
-      }
-    }
-    Iterator<Long> evictIt = plan.toEvict().iterator();
-    while (evictIt.hasNext()) {
-      long id = evictIt.next();
-      if (null == mManagerView.getBlockMeta(id)) {
-        mLRUCache.remove(id);
-        evictIt.remove();
-      }
-    }
-
-    return EvictorUtils.validCascadingPlan(bytesToBeAvailable, plan,
-        mManagerView.getMetadataManager()) ? plan : null;
+    return plan;
   }
 
   @Override
--- servers/src/test/java/tachyon/worker/block/evictor/EvictorTestUtils.java
+++ servers/src/test/java/tachyon/worker/block/evictor/EvictorTestUtils.java
@@ -18,8 +18,9 @@ package tachyon.worker.block.evictor;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -36,11 +37,11 @@ import tachyon.TestUtils;
 import tachyon.conf.TachyonConf;
 import tachyon.util.CommonUtils;
 import tachyon.worker.block.BlockMetadataManager;
-import tachyon.worker.block.BlockMetadataManagerView;
 import tachyon.worker.block.BlockStoreEventListener;
 import tachyon.worker.block.BlockStoreLocation;
 import tachyon.worker.block.io.BlockWriter;
 import tachyon.worker.block.io.LocalFileBlockWriter;
+import tachyon.worker.block.meta.BlockMeta;
 import tachyon.worker.block.meta.StorageDir;
 import tachyon.worker.block.meta.TempBlockMeta;
 
@@ -267,7 +268,95 @@ class EvictorTestUtils {
   }
 
   /**
-   * Only when plan is not null and at least one of {@link EvictorUtils#validCascadingPlan},
+   * Checks whether the plan of a cascading evictor is valid.
+   *
+   * A cascading evictor will try to free space by recursively moving blocks to next 1 tier and
+   * evict blocks only in the bottom tier.
+   *
+   * The plan is invalid when the requested space can not be satisfied or lower level of tiers do
+   * not have enough space to hold blocks moved from higher level of tiers.
+   *
+   * @param bytesToBeAvailable requested bytes to be available after eviction
+   * @param plan the eviction plan, should not be empty
+   * @param metaManager the meta data manager
+   * @return true if the above requirements are satisfied, otherwise false.
+   */
+  // TODO: unit test this method
+  public static boolean validCascadingPlan(long bytesToBeAvailable, EvictionPlan plan,
+      BlockMetadataManager metaManager) throws IOException {
+    // reassure the plan is feasible: enough free space to satisfy bytesToBeAvailable, and enough
+    // space in lower tier to move blocks in upper tier there
+
+    // Map from dir to a pair of bytes to be available in this dir and bytes to move into this dir
+    // after the plan taking action
+    Map<StorageDir, Pair<Long, Long>> spaceInfoInDir = new HashMap<StorageDir, Pair<Long, Long>>();
+
+    for (long blockId : plan.toEvict()) {
+      BlockMeta block = metaManager.getBlockMeta(blockId);
+      StorageDir dir = block.getParentDir();
+      if (spaceInfoInDir.containsKey(dir)) {
+        Pair<Long, Long> spaceInfo = spaceInfoInDir.get(dir);
+        spaceInfo.setFirst(spaceInfo.getFirst() + block.getBlockSize());
+      } else {
+        spaceInfoInDir.put(dir, new Pair<Long, Long>(
+            dir.getAvailableBytes() + block.getBlockSize(), 0L));
+      }
+    }
+
+    for (Pair<Long, BlockStoreLocation> move : plan.toMove()) {
+      long blockId = move.getFirst();
+      BlockMeta block = metaManager.getBlockMeta(blockId);
+      long blockSize = block.getBlockSize();
+      StorageDir srcDir = block.getParentDir();
+      StorageDir destDir = metaManager.getDir(move.getSecond());
+
+      if (spaceInfoInDir.containsKey(srcDir)) {
+        Pair<Long, Long> spaceInfo = spaceInfoInDir.get(srcDir);
+        spaceInfo.setFirst(spaceInfo.getFirst() + blockSize);
+      } else {
+        spaceInfoInDir
+            .put(srcDir, new Pair<Long, Long>(srcDir.getAvailableBytes() + blockSize, 0L));
+      }
+
+      if (spaceInfoInDir.containsKey(destDir)) {
+        Pair<Long, Long> spaceInfo = spaceInfoInDir.get(destDir);
+        spaceInfo.setSecond(spaceInfo.getSecond() + blockSize);
+      } else {
+        spaceInfoInDir.put(destDir, new Pair<Long, Long>(destDir.getAvailableBytes(), blockSize));
+      }
+    }
+
+    // the top tier among all tiers where blocks in the plan reside in
+    int topTierAlias = Integer.MAX_VALUE;
+    for (StorageDir dir : spaceInfoInDir.keySet()) {
+      topTierAlias = Math.min(topTierAlias, dir.getParentTier().getTierAlias());
+    }
+    long maxSpace = Long.MIN_VALUE; // maximum bytes to be available in a dir in the top tier
+    for (StorageDir dir : spaceInfoInDir.keySet()) {
+      if (dir.getParentTier().getTierAlias() == topTierAlias) {
+        Pair<Long, Long> space = spaceInfoInDir.get(dir);
+        maxSpace = Math.max(maxSpace, space.getFirst() - space.getSecond());
+      }
+    }
+    if (maxSpace < bytesToBeAvailable) {
+      // plan is invalid because requested space can not be satisfied in the top tier
+      return false;
+    }
+
+    for (StorageDir dir : spaceInfoInDir.keySet()) {
+      Pair<Long, Long> spaceInfo = spaceInfoInDir.get(dir);
+      if (spaceInfo.getFirst() < spaceInfo.getSecond()) {
+        // plan is invalid because there is not enough space in this dir to hold the blocks waiting
+        // to be moved into this dir
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Only when plan is not null and at least one of {@link EvictorTestUtils#validCascadingPlan},
    * {@link #validNonCascadingPlan} is true, the assertion will be passed, used in unit test.
    *
    * @param bytesToBeAvailable the requested bytes to be available
@@ -279,6 +368,6 @@ class EvictorTestUtils {
       BlockMetadataManager metaManager) throws IOException {
     Assert.assertNotNull(plan);
     Assert.assertTrue(validNonCascadingPlan(bytesToBeAvailable, plan, metaManager)
-        || EvictorUtils.validCascadingPlan(bytesToBeAvailable, plan, metaManager));
+        || EvictorTestUtils.validCascadingPlan(bytesToBeAvailable, plan, metaManager));
   }
 }
