0270e87b36db6e0c489bcb012cd0d16e8ae1ab57
Qifan Pu <qifan.pu@gmail.com>
2015-07-15 10:40:13 -0700

    Add another DataByteBuffer constructor to avoid copying from ByteBuf

--- common/src/main/java/tachyon/network/protocol/RPCBlockReadResponse.java
+++ common/src/main/java/tachyon/network/protocol/RPCBlockReadResponse.java
@@ -78,10 +78,7 @@ public class RPCBlockReadResponse extends RPCResponse {
     short status = in.readShort();
     DataBuffer data = null;
     if (length > 0) {
-      // TODO: look into accessing Netty ByteBuf directly, to avoid copying the data.
-      ByteBuffer buffer = ByteBuffer.allocate((int) length);
-      in.readBytes(buffer);
-      data = new DataByteBuffer(buffer, (int) length);
+      data = new DataByteBuffer(in, (int) length);
     }
     return new RPCBlockReadResponse(blockId, offset, length, data, Status.fromShort(status));
   }
