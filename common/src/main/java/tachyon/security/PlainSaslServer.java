98fbf9e8e06e0dbc35e1d079029dfa32f05b5848
Guoquan Shen <guoquan.shen@intel.com>
2015-07-16 13:27:45 +0800

    [TACHYON-622] Add a class PlainSaslServer that implements the SaslServer interface -- fix some exception problem,such as message and type

--- common/src/main/java/tachyon/security/PlainSaslServer.java
+++ common/src/main/java/tachyon/security/PlainSaslServer.java
@@ -72,7 +72,7 @@ public class PlainSaslServer implements SaslServer {
       String[] parts = payload.split("\u0000", 3);
       // validate response
       if (parts.length != 3) {
-        throw new IllegalArgumentException("Received corrupt response");
+        throw new IllegalArgumentException("Invalid message format, parts must contain 3 items");
       }
       String authzid = parts[0];
       mAuthcid = parts[1];
@@ -96,12 +96,12 @@ public class PlainSaslServer implements SaslServer {
       Callback[] cbList = {nameCallback, passwordCallback, authCallback};
       mHandler.handle(cbList);
       if (!authCallback.isAuthorized()) {
-        throw new SaslException("Authentication failed");
+        throw new SaslException("AuthorizeCallback authorized failure");
       }
     } catch (IOException ioe) {
-      throw new SaslException("Error validating the login", ioe);
+      throw new SaslException("Error validating the response", ioe);
     } catch (UnsupportedCallbackException uce) {
-      throw new SaslException("Error validating the login", uce);
+      throw new SaslException("Error validating the response", uce);
     }
     mCompleted = true;
     return null;
@@ -121,13 +121,13 @@ public class PlainSaslServer implements SaslServer {
   @Override
   public byte[] unwrap(byte[] incoming, int offset, int len) {
     throwIfNotComplete();
-    throw new IllegalStateException("PLAIN supports neither integrity nor privacy");
+    throw new UnsupportedOperationException("PLAIN doesn't support wrap or unwrap operation");
   }
 
   @Override
   public byte[] wrap(byte[] outgoing, int offset, int len) {
     throwIfNotComplete();
-    throw new IllegalStateException("PLAIN supports neither integrity nor privacy");
+    throw new UnsupportedOperationException("PLAIN doesn't support wrap or unwrap operation");
   }
 
   @Override
