0b4f75f0d01571f51d2c4550d4b432d877a84f61
dongche <dong1.chen@intel.com>
2015-07-23 06:07:08 -0400

    [TACHYON-634] Improve log messages in RPCBlockRequest

--- common/src/main/java/tachyon/network/protocol/RPCBlockReadRequest.java
+++ common/src/main/java/tachyon/network/protocol/RPCBlockReadRequest.java
@@ -72,7 +72,7 @@ public class RPCBlockReadRequest extends RPCRequest {
   public void validate() {
     Preconditions.checkState(mOffset >= 0, "Offset can not be negative: %s", mOffset);
     Preconditions.checkState(mLength >= 0 || mLength == -1,
-        "Length can not be negative except -1: %s", mLength);
+        "Length cannot be negative (except for -1): %s", mLength);
   }
 
   @Override
