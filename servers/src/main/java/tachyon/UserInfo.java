40a11694f8a8c00a09a73a3eb8a32994908a33b9
Qinghao Dai <rogerdai16@gmail.com>
2015-07-27 01:46:57 -0700

    Update UserInfo.java
    Override equals and hashCode to use mUserId to identify UserInfo object.

--- servers/src/main/java/tachyon/UserInfo.java
+++ servers/src/main/java/tachyon/UserInfo.java
@@ -54,4 +54,17 @@ public class UserInfo {
     sb.append(")");
     return sb.toString();
   }
+  
+  @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInfo userInfo = (UserInfo) o;
+        return mUserId == userInfo.mUserId;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (mUserId ^ (mUserId >>> 32));
+    }
 }
