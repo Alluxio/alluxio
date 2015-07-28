527e84f5b809005e789d9742f9ad4eda755752f8
Qinghao Dai <rogerdai16@gmail.com>
2015-07-27 23:04:47 -0700

    Update UserInfo.java
    add braces to if statements

--- servers/src/main/java/tachyon/UserInfo.java
+++ servers/src/main/java/tachyon/UserInfo.java
@@ -57,8 +57,12 @@ public class UserInfo {
   
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
     UserInfo userInfo = (UserInfo) o;
     return mUserId == userInfo.mUserId;
   }
