dd3565b1bb8d11c662e97081bd9fd232750abc11
Calvin Jia <jia.calvin@gmail.com>
2015-08-05 13:21:45 -0700

    Mark async through as experimental in 0.7.

--- clients/unshaded/src/main/java/tachyon/client/WriteType.java
+++ clients/unshaded/src/main/java/tachyon/client/WriteType.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -36,7 +36,8 @@ public enum WriteType {
    */
   THROUGH(4),
   /**
-   * Write the file asynchronously to the under fs (either must cache or must through).
+   * [Experimental] Write the file asynchronously to the under fs (either must cache or must
+   * through).
    */
   ASYNC_THROUGH(5);
 
@@ -48,7 +49,7 @@ public enum WriteType {
 
   /**
    * Return the value of the write type
-   * 
+   *
    * @return the value of the write type
    */
   public int getValue() {
