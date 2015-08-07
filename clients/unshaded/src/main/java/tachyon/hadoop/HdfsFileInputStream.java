09a8a727c0db2a91a9d533738d031b3ea6f8bb88
Qianhao Dong <qianhao.dong@intel.com>
2015-08-07 10:34:01 +0800

    enable FileSystem Counter

--- clients/unshaded/src/main/java/tachyon/hadoop/HdfsFileInputStream.java
+++ clients/unshaded/src/main/java/tachyon/hadoop/HdfsFileInputStream.java
@@ -45,6 +45,7 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
   private Path mHdfsPath;
   private Configuration mHadoopConf;
   private int mHadoopBufferSize;
+  private FileSystem.Statistics mStatistics;
   private TachyonFile mTachyonFile;
 
   private FSDataInputStream mHdfsInputStream = null;
@@ -60,9 +61,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
   private final TachyonConf mTachyonConf;
 
   public HdfsFileInputStream(TachyonFS tfs, int fileId, Path hdfsPath, Configuration conf,
-      int bufferSize, TachyonConf tachyonConf) throws IOException {
-    LOG.debug("PartitionInputStreamHdfs({}, {}, {}, {}, {})", tfs, fileId, hdfsPath, conf,
-        bufferSize);
+      int bufferSize, FileSystem.Statistics stats, TachyonConf tachyonConf) throws IOException {
+    LOG.debug("PartitionInputStreamHdfs({}, {}, {}, {}, {}, {})", tfs, fileId, hdfsPath, conf,
+        bufferSize, stats);
     mTachyonConf = tachyonConf;
     long bufferBytes = mTachyonConf.getBytes(Constants.USER_FILE_BUFFER_BYTES, 0);
     mBuffer = new byte[Ints.checkedCast(bufferBytes) * 4];
@@ -72,6 +73,7 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
     mHdfsPath = hdfsPath;
     mHadoopConf = conf;
     mHadoopBufferSize = bufferSize;
+    mStatistics = stats;
     mTachyonFile = mTFS.getFile(mFileId);
     if (mTachyonFile == null) {
       throw new FileNotFoundException("File " + hdfsPath + " with FID " + fileId
@@ -137,6 +139,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
       int ret = 0;
       try {
         ret = mTachyonFileInputStream.read();
+        if (mStatistics != null && ret != -1) {
+          mStatistics.incrementBytesRead(1);
+        }
         mCurrentPosition ++;
         return ret;
       } catch (IOException e) {
@@ -162,6 +167,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
       int ret = 0;
       try {
         ret = mTachyonFileInputStream.read(b, off, len);
+        if (mStatistics != null && ret != -1) {
+          mStatistics.incrementBytesRead(ret);
+        }
         mCurrentPosition += ret;
         return ret;
       } catch (IOException e) {
@@ -198,6 +206,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
       try {
         mTachyonFileInputStream.seek(position);
         ret = mTachyonFileInputStream.read(buffer, offset, length);
+        if (mStatistics != null && ret != -1) {
+          mStatistics.incrementBytesRead(ret);
+        }
         return ret;
       } finally {
         mTachyonFileInputStream.seek(oldPos);
@@ -207,6 +218,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
     try {
       getHdfsInputStream(position);
       ret = mHdfsInputStream.read(buffer, offset, length);
+      if (mStatistics != null && ret != -1) {
+        mStatistics.incrementBytesRead(ret);
+      }
       return ret;
     } finally {
       if (mHdfsInputStream != null) {
@@ -217,6 +231,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
 
   private int readFromHdfsBuffer() throws IOException {
     if (mBufferPosition < mBufferLimit) {
+      if (mStatistics != null) {
+        mStatistics.incrementBytesRead(1);
+      }
       return mBuffer[mBufferPosition ++];
     }
     LOG.error("Reading from HDFS directly");
@@ -227,6 +244,9 @@ public class HdfsFileInputStream extends InputStream implements Seekable, Positi
       return -1;
     }
     mBufferPosition = 0;
+    if (mStatistics != null) {
+      mStatistics.incrementBytesRead(1);
+    }
     return mBuffer[mBufferPosition ++];
   }
 
