16737d047612bba7d00fc697f83ffd8c2d387115
Daniel Yu <danielmethod@gmail.com>
2015-08-07 12:40:29 +0800

    make cleanerMethod and cleanMethod static members

--- common/src/main/java/tachyon/util/io/BufferUtils.java
+++ common/src/main/java/tachyon/util/io/BufferUtils.java
@@ -31,6 +31,8 @@ import tachyon.Constants;
  */
 public class BufferUtils {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
+  private static Method sCleanMethod;
+  private static Method sCleanerMethod;
 
   /**
    * Force to unmap direct buffer if the buffer is no longer used. It is unsafe operation and
@@ -44,11 +46,16 @@ public class BufferUtils {
     }
     if (buffer.isDirect()) {
       try {
-        final Method getCleanerMethod = buffer.getClass().getMethod("cleaner");
-        getCleanerMethod.setAccessible(true);
-        final Object cleaner = getCleanerMethod.invoke(buffer);
+        if (sCleanerMethod == null) {
+          sCleanerMethod = buffer.getClass().getMethod("cleaner");
+          sCleanerMethod.setAccessible(true);
+        }
+        final Object cleaner = sCleanerMethod.invoke(buffer);
         if (cleaner != null) {
-          cleaner.getClass().getMethod("clean").invoke(cleaner);
+          if (sCleanMethod == null) {
+            sCleanMethod = cleaner.getClass().getMethod("clean");
+          }
+          sCleanMethod.invoke(cleaner);
         }
       } catch (Exception e) {
         LOG.warn("Fail to unmap direct buffer due to ", e);
