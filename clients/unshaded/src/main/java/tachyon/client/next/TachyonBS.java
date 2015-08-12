4a5b842af01975c14b9271e22ad80824154609ff
Calvin Jia <jia.calvin@gmail.com>
2015-08-12 13:45:27 -0700

    Add initial skeleton for Tachyon Block Store client.

---
+++ clients/unshaded/src/main/java/tachyon/client/next/TachyonBS.java
@@ -0,0 +1,43 @@
+package tachyon.client.next;
+
+import tachyon.client.BlockInStream;
+import tachyon.client.BlockOutStream;
+
+public class TachyonBS {
+  public static TachyonBS get() {
+    // TODO: Implement me
+    return null;
+  }
+
+  public void close() {
+    // TODO: Implement me
+  }
+
+  public void deleteBlock(long blockId) {
+    // TODO: Implement me
+  }
+
+  public void freeBlock(long blockId) {
+    // TODO: Implement me
+  }
+
+  public BlockInfo getBlockInfo(long blockId) {
+    // TODO: Implement me
+    return null;
+  }
+
+  public BlockInStream getBlockInStream(long blockId, ClientOptions options) {
+    // TODO: Implement me
+    return null;
+  }
+
+  public BlockOutStream getBlockOutStream(long blockId, ClientOptions options) {
+    // TODO: Implement me
+    return null;
+  }
+
+  public boolean promote(long blockId) {
+    // TODO: Implement me
+    return false;
+  }
+}
