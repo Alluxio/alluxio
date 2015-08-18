b112a3a1a7a601736739aee0e73f91a7fb98e483
Chen Tian <tianc.tech@gmail.com>
2015-08-17 22:28:58 -0700

    use vagrant cli to create tachyon-dev box invoke build_box.sh in create, not Vagrantfile

--- deploy/vagrant/vbox/buildbox.sh
+++ deploy/vagrant/vbox/buildbox.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 OLD_BOX=$(vagrant box list | grep tachyon-dev | cut -d ' ' -f1)
 if [[ "$OLD_BOX" != '' ]]; then
  echo "$OLD_BOX exists"
@@ -12,9 +14,16 @@ if [ -f tachyon-dev.box ]; then
   rm -f tachyon-dev.box
 fi
 
-vagrant up
-vagrant package --output tachyon-dev.box default
-vagrant destroy -f
+BASE_BOX='chef/centos-7.0'
+AVAIL_BOX=$(vagrant box list | grep $BASE_BOX | grep '(virtualbox, 1.0.0)')
+
+if [ -z "$AVAIL_BOX" ]; then
+  vagrant box add chef/centos-7.0 --provider virtualbox
+fi
+
+echo "Generating 'tachyon-dev.box' based on box '$BASE_BOX' ..."
+vagrant box repackage $BASE_BOX virtualbox 1.0
+mv package.box tachyon-dev.box
 vagrant box add tachyon-dev tachyon-dev.box
 
 popd >/dev/null
