28e04cec5b5b92254f3bc138ea9f348fc7ae5eea
cc <myairia@gmail.com>
2015-08-21 20:39:03 +0800

    Add two concrete JournalEntry.

---
+++ servers/src/main/java/tachyon/master/next/filesystem/journal/AddCheckpointEntry.java
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the University of California, Berkeley under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package tachyon.master.next.filesystem.journal;
+
+import tachyon.master.next.journal.JournalEntry;
+import tachyon.master.next.journal.JournalEntryType;
+
+public class AddCheckpointEntry implements JournalEntry {
+  // TODO add necessary fields for serializing the addCheckpoint operation on FileSystemMaster
+
+  @Override
+  public JournalEntryType type() {
+    return JournalEntryType.ADD_CHECKPOINT;
+  }
+}
