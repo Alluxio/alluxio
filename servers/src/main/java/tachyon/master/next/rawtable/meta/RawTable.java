cad9105018ac3252e6f589849a50b9a82ce20a22
cc <myairia@gmail.com>
2015-08-21 10:43:01 +0800

    Remove usage of TachyonConf in rawtable/meta

--- servers/src/main/java/tachyon/master/next/rawtable/meta/RawTable.java
+++ servers/src/main/java/tachyon/master/next/rawtable/meta/RawTable.java
@@ -17,41 +17,52 @@ package tachyon.master.next.rawtable.meta;
 
 import java.nio.ByteBuffer;
 
-import tachyon.Constants;
-import tachyon.conf.TachyonConf;
 import tachyon.thrift.TachyonException;
 import tachyon.util.io.BufferUtils;
 
 public class RawTable {
-  private final TachyonConf mTachyonConf;
-
   /** Table ID */
   private final int mId;
+  /** Maximum number of columns */
+  private final long mMaxColumns;
   /** Number of columns */
   private final int mColumns;
   /** Table metadata */
   private ByteBuffer mMetadata;
 
-  private RawTable(TachyonConf tachyonConf, int id, int columns) {
-    mTachyonConf = tachyonConf;
+  private RawTable(int id, int columns, long maxColumns) {
     mId = id;
     mColumns = columns;
+    mMaxColumns = maxColumns;
     mMetadata = null;
   }
 
   /**
-   * Create a new RawTable.
+   * Create a new RawTable with metadata set to null. metadata can later be set via
+   * {@link #setMetadata(java.nio.ByteBuffer)}.
    *
-   * @param tachyonConf TachyonConf to use
    * @param id table id
    * @param columns number of columns
-   * @param metadata table metadata
+   * @param maxColumns maximum number of columns
+   */
+  public static RawTable newRawTable(int id, int columns, long maxColumns) {
+    return new RawTable(id, columns, maxColumns);
+  }
+
+  /**
+   * Create a new RawTable with metadata set to a ByteBuffer.
+   *
+   * @param id table id
+   * @param columns number of columns
+   * @param maxColumns maximum number of columns
+   * @param metadata table metadata, if is null, the internal metadata is set to an empty buffer,
+   *        otherwise, the provided buffer will be copied into the internal buffer.
    * @return the created RawTable
    * @throws TachyonException when metadata is larger than the configured maximum size
    */
-  public static RawTable newRawTable(TachyonConf tachyonConf, int id, int columns,
-      ByteBuffer metadata) throws TachyonException {
-    RawTable ret = new RawTable(tachyonConf, id, columns);
+  public static RawTable newRawTable(int id, int columns, long maxColumns, ByteBuffer metadata)
+      throws TachyonException {
+    RawTable ret = new RawTable(id, columns, maxColumns);
     ret.setMetadata(metadata);
     return ret;
   }
@@ -70,7 +81,7 @@ public class RawTable {
 
   /**
    * Set the table metadata. If the specified metadata is null, the internal metadata will be set to
-   * an empty byte buffer.
+   * an empty byte buffer, otherwise, the provided metadata will be copied into the internal buffer.
    *
    * @param metadata the metadata to be set
    * @throws TachyonException when metadata is larger than the configured maximum size
@@ -87,8 +98,7 @@ public class RawTable {
    * @throws TachyonException if the metadata is too large
    */
   private void validateMetadataSize(ByteBuffer metadata) throws TachyonException {
-    long maxSize = mTachyonConf.getBytes(Constants.MAX_TABLE_METADATA_BYTE);
-    if (metadata.limit() - metadata.position() >= maxSize) {
+    if (metadata.limit() - metadata.position() >= mMaxColumns) {
       throw new TachyonException("Too big table metadata: " + metadata.toString());
     }
   }
