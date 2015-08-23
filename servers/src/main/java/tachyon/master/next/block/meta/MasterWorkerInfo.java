f205beec3b09eeb3048ae9be491c6fea4644bfee
Yupeng Fu <yupeng.fu@gmail.com>
2015-08-22 22:21:28 -0700

    Added test case for MasterWorkerInfo

--- servers/src/main/java/tachyon/master/next/block/meta/MasterWorkerInfo.java
+++ servers/src/main/java/tachyon/master/next/block/meta/MasterWorkerInfo.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -27,11 +27,11 @@ import org.slf4j.LoggerFactory;
 import com.google.common.collect.Sets;
 
 import tachyon.Constants;
-import tachyon.thrift.WorkerInfo;
 import tachyon.thrift.NetAddress;
+import tachyon.thrift.WorkerInfo;
 import tachyon.util.CommonUtils;
 
-public class MasterWorkerInfo {
+public final class MasterWorkerInfo {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
   /** Worker's address */
   public final NetAddress mWorkerAddress;
@@ -265,7 +265,7 @@ public class MasterWorkerInfo {
 
   /**
    * Set the used space of the worker in bytes.
-   * 
+   *
    * @param usedBytesOnTiers used bytes on each storage tier
    */
   public synchronized void updateUsedBytes(List<Long> usedBytesOnTiers) {
@@ -278,7 +278,7 @@ public class MasterWorkerInfo {
 
   /**
    * Set the used space of the worker in bytes.
-   * 
+   *
    * @param aliasValue value of StorageLevelAlias
    * @param usedBytesOnTier used bytes on certain storage tier.
    */
