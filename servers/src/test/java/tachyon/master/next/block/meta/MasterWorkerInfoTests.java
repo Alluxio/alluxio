aea310b16b3b8ca9e2969b4b209b4c461b13e0c4
Yupeng Fu <yupeng.fu@gmail.com>
2015-08-24 12:59:37 -0700

    Added test case for register

--- servers/src/test/java/tachyon/master/next/block/meta/MasterWorkerInfoTests.java
+++ servers/src/test/java/tachyon/master/next/block/meta/MasterWorkerInfoTests.java
@@ -15,10 +15,43 @@
 
 package tachyon.master.next.block.meta;
 
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import tachyon.Constants;
+import tachyon.thrift.NetAddress;
+
 /**
  * Unit tests for MasterWorkerInfo.
  */
 public final class MasterWorkerInfoTests {
+  private MasterWorkerInfo mInfo;
+  private static final List<Long> TOTAL_BYTES_ON_TIERS =
+      Lists.newArrayList(Constants.KB * 2L, Constants.KB * 2L);
+  private static final List<Long> USED_BYTES_ON_TIERS = Lists.newArrayList(Constants.KB * 1L);
+  private static final Set<Long> NEW_BLOCKS = Sets.newHashSet(Constants.KB * 1L, Constants.KB * 1L);
 
+  @Before
+  public void before() {
+    // register
+    mInfo = new MasterWorkerInfo(0, new NetAddress());
+    mInfo.register(TOTAL_BYTES_ON_TIERS, USED_BYTES_ON_TIERS, NEW_BLOCKS);
+  }
+
+  @Test
+  public void registerTest() {
+    Assert.assertEquals(NEW_BLOCKS, mInfo.getBlocks());
+    Assert.assertEquals(TOTAL_BYTES_ON_TIERS, mInfo.getTotalBytesOnTiers());
+    Assert.assertEquals(Constants.KB * 4L, mInfo.getCapacityBytes());
+    Assert.assertEquals(USED_BYTES_ON_TIERS, mInfo.getUsedBytesOnTiers());
+    Assert.assertEquals(Constants.KB * 1L, mInfo.getUsedBytes());
+  }
 
 }
