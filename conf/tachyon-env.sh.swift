7d737a73020c1fd69d5d18d51ef6eca91295c4c0
Daniel Yu <danielmethod@gmail.com>
2015-08-24 09:34:30 +0800

    we don't need to define max tier level in tahcyon-env.sh

--- conf/tachyon-env.sh.swift
+++ conf/tachyon-env.sh.swift
@@ -63,7 +63,6 @@ CONF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export TACHYON_JAVA_OPTS+="
   -Dlog4j.configuration=file:$CONF_DIR/log4j.properties
-  -Dtachyon.worker.tieredstore.level.max=1
   -Dtachyon.worker.tieredstore.level0.alias=MEM
   -Dtachyon.worker.tieredstore.level0.dirs.path=$TACHYON_RAM_FOLDER
   -Dtachyon.worker.tieredstore.level0.dirs.quota=$TACHYON_WORKER_MEMORY_SIZE
