b5517c05dedf4bd2b79b046bf4c84923cc9a4dcc
Calvin Jia <jia.calvin@gmail.com>
2015-08-24 13:12:29 -0700

    Implement delete/free in TachyonFS and fix bug with TachyonFile.

--- clients/unshaded/src/main/java/tachyon/client/next/file/TachyonFS.java
+++ clients/unshaded/src/main/java/tachyon/client/next/file/TachyonFS.java
@@ -16,16 +16,19 @@
 package tachyon.client.next.file;
 
 import java.io.Closeable;
+import java.io.IOException;
 import java.util.List;
 
 import tachyon.TachyonURI;
 import tachyon.client.next.ClientOptions;
+import tachyon.master.MasterClient;
 import tachyon.thrift.FileInfo;
 
 /**
  * Tachyon File System client. This class is the entry point for all file level operations on
  * Tachyon files. An instance of this class can be obtained via {@link TachyonFS#get}. This class
- * is thread safe.
+ * is thread safe. The read/write interface provided by this client is similar to Java's
+ * input/output streams.
  */
 public class TachyonFS implements Closeable, TachyonFSCore {
   private static TachyonFS sCachedClient;
@@ -37,20 +40,33 @@ public class TachyonFS implements Closeable, TachyonFSCore {
     return sCachedClient;
   }
 
+  private FSContext mContext;
+
   private TachyonFS() {
-
+    mContext = FSContext.INSTANCE;
   }
 
-  public void close() {
-    // TODO: Implement me
+  // TODO: Evaluate the necessity of this method
+  public synchronized void close() {
+    sCachedClient = null;
   }
 
-  public void delete(TachyonFile file) {
-    // TODO: Implement me
+  public void delete(TachyonFile file) throws IOException {
+    MasterClient masterClient = mContext.acquireMasterClient();
+    try {
+      masterClient.user_delete(file.getFileId(), "", true);
+    } finally {
+      mContext.releaseMasterClient(masterClient);
+    }
   }
 
-  public void free(TachyonFile file) {
-    // TODO: Implement me
+  public void free(TachyonFile file) throws IOException {
+    MasterClient masterClient = mContext.acquireMasterClient();
+    try {
+      masterClient.user_freepath(file.getFileId(), "", true);
+    } finally {
+      mContext.releaseMasterClient(masterClient);
+    }
   }
 
   public FileInfo getInfo(TachyonFile file) {
@@ -63,7 +79,7 @@ public class TachyonFS implements Closeable, TachyonFSCore {
     return null;
   }
 
-  public FileOutStream getOutStream(TachyonURI path, ClientOptions options) {
+  public FileOutStream getOutStream(TachyonURI path, TachyonURI ufsPath, ClientOptions options) {
     // TODO: Implement me
     return null;
   }
