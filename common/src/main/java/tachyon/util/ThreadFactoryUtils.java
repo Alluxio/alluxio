da8d018add370eb168766b5490fab0dd18a06ebc
Jiri Simsa <jiri.simsa@gmail.com>
2015-08-25 09:18:07 -0700

    Applying the rearranger and formatter plugin to package tachyon.util.

--- common/src/main/java/tachyon/util/ThreadFactoryUtils.java
+++ common/src/main/java/tachyon/util/ThreadFactoryUtils.java
@@ -26,9 +26,9 @@ public final class ThreadFactoryUtils {
    * Creates a {@link java.util.concurrent.ThreadFactory} that spawns off threads.
    *
    * @param nameFormat name pattern for each thread. should contain '%d' to distinguish between
-   *                   threads.
-   * @param isDaemon if true, the {@link java.util.concurrent.ThreadFactory} will create
-   *                 daemon threads.
+   *        threads.
+   * @param isDaemon if true, the {@link java.util.concurrent.ThreadFactory} will create daemon
+   *        threads.
    */
   public static ThreadFactory build(final String nameFormat, boolean isDaemon) {
     return new ThreadFactoryBuilder().setDaemon(isDaemon).setNameFormat(nameFormat).build();
