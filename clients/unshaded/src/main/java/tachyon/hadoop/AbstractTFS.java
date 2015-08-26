e436a44d3173a04af97b323ddea3d2412c2f0e3f
Jiri Simsa <jiri.simsa@gmail.com>
2015-08-26 11:03:07 -0700

    Removing use of @link for classes with limited visibility.

--- clients/unshaded/src/main/java/tachyon/hadoop/AbstractTFS.java
+++ clients/unshaded/src/main/java/tachyon/hadoop/AbstractTFS.java
@@ -55,7 +55,7 @@ import tachyon.util.UnderFileSystemUtils;
  * All implementing classes must define {@link #isZookeeperMode()} which states if fault tolerant is
  * used and {@link #getScheme()} for Hadoop's {@link java.util.ServiceLoader} support.
  */
-public abstract class AbstractTFS extends FileSystem {
+abstract class AbstractTFS extends FileSystem {
   public static final String FIRST_COM_PATH = "tachyon_dep/";
   public static final String RECOMPUTE_PATH = "tachyon_recompute/";
 
