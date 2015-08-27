517b428e42f1045b0a6feb2ae344837324249ae8
Calvin Jia <jia.calvin@gmail.com>
2015-08-27 16:05:19 -0700

    Generate user id when creating a worker client.

--- clients/unshaded/src/main/java/tachyon/client/next/UserMasterClientPool.java
+++ clients/unshaded/src/main/java/tachyon/client/next/UserMasterClientPool.java
@@ -1,12 +1,27 @@
-package tachyon.client.next;
+/*
+ * Licensed to the University of California, Berkeley under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-import tachyon.client.UserMasterClient;
-import tachyon.conf.TachyonConf;
-import tachyon.util.ThreadFactoryUtils;
+package tachyon.client.next;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import tachyon.client.UserMasterClient;
+import tachyon.conf.TachyonConf;
+import tachyon.util.ThreadFactoryUtils;
 
 public class UserMasterClientPool extends ResourcePool<UserMasterClient> {
   private final ExecutorService mExecutorService;
