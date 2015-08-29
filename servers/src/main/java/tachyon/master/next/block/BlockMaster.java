ce43db82257b53625588a27da98cd698f9feef50
Calvin Jia <jia.calvin@gmail.com>
2015-08-28 17:26:49 -0700

    Implement some missing functions in the BlockMasterClient.

--- servers/src/main/java/tachyon/master/next/block/BlockMaster.java
+++ servers/src/main/java/tachyon/master/next/block/BlockMaster.java
@@ -45,6 +45,7 @@ import tachyon.master.next.journal.JournalEntry;
 import tachyon.master.next.journal.JournalInputStream;
 import tachyon.master.next.journal.JournalOutputStream;
 import tachyon.thrift.BlockInfo;
+import tachyon.thrift.BlockInfoException;
 import tachyon.thrift.BlockLocation;
 import tachyon.thrift.BlockMasterService;
 import tachyon.thrift.Command;
@@ -229,6 +230,26 @@ public class BlockMaster extends MasterBase implements ContainerIdGenerator {
       flushJournal();
     }
     masterBlockInfo.addWorker(workerId, tierAlias);
+  }
+
+  public BlockInfo getBlockInfo(long blockId) throws BlockInfoException {
+    MasterBlockInfo masterBlockInfo = mBlocks.get(blockId);
+    if (masterBlockInfo != null) {
+      // Construct the block info object to return.
+
+      // "Join" to get all the addresses of the workers.
+      List<BlockLocation> locations = new ArrayList<BlockLocation>();
+      for (MasterBlockLocation masterBlockLocation : masterBlockInfo.getBlockLocations()) {
+        MasterWorkerInfo workerInfo =
+            mWorkers.getFirstByField(mIdIndex, masterBlockLocation.getWorkerId());
+        if (workerInfo != null) {
+          locations.add(new BlockLocation(masterBlockLocation.getWorkerId(),
+              workerInfo.getAddress(), masterBlockLocation.getTier()));
+        }
+      }
+      return new BlockInfo(masterBlockInfo.getBlockId(), masterBlockInfo.getLength(), locations);
+    }
+    throw new BlockInfoException("Block info not found for " + blockId);
   }
 
   /**
