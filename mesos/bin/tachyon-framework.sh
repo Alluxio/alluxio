6bf6a049c4f1735bd144ee7a49d1955b75bea48b
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-02 08:53:26 -0700

    Minor fixes.

--- mesos/bin/tachyon-framework.sh
+++ mesos/bin/tachyon-framework.sh
@@ -14,4 +14,4 @@ MESOS_MASTER_ADDRESS="$1"
 
 mkdir -p "${TACHYON_LOGS_DIR}"
 
-(nohup "${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Djava.library.path="${MESOS_LIBRARY_PATH}" "${TACHYON_FRAMEWORK_JAVA_OPTS}" tachyon.mesos.TachyonFramework "${MESOS_MASTER_ADDRESS}" > "${TACHYON_LOGS_DIR}"/framework.out 2>&1)
+(nohup "${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Djava.library.path="${MESOS_LIBRARY_PATH}" ${TACHYON_FRAMEWORK_JAVA_OPTS} tachyon.mesos.TachyonFramework "${MESOS_MASTER_ADDRESS}" > "${TACHYON_LOGS_DIR}"/framework.out 2>&1)
