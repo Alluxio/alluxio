58452a6bce8edcb9cdfc6ab91ec29ffe9ffb1980
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-02 09:42:42 -0700

    Minor fixes.

--- mesos/bin/test-master.sh
+++ mesos/bin/test-master.sh
@@ -13,7 +13,7 @@ TACHYON_MASTER_JAVA_OPTS="${TACHYON_MASTER_JAVA_OPTS:-${TACHYON_JAVA_OPTS}}"
 mkdir -p "${TACHYON_LOGS_DIR}"
 
 # Format the master.
-"${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Dtachyon.master.hostname="${TACHYON_MASTER_ADDRESS}" -Dtachyon.logger.type="USER_LOGGER" "${TACHYON_JAVA_OPTS}" "tachyon.Format" "master"
+"${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Dtachyon.master.hostname="${TACHYON_MASTER_ADDRESS}" -Dtachyon.logger.type="USER_LOGGER" ${TACHYON_JAVA_OPTS} "tachyon.Format" "master"
 
 # Start the master.
 (nohup "${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Dtachyon.logger.type="MASTER_LOGGER" -Dtachyon.accesslogger.type="MASTER_ACCESS_LOGGER" -Dlog4j.configuration=file:"${TACHYON_CONF_DIR}/log4j.properties" ${TACHYON_MASTER_JAVA_OPTS} tachyon.mesos.TestMasterExecutor > "${TACHYON_LOGS_DIR}"/master.out 2>&1)
