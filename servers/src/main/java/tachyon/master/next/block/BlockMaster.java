417b97330e1e0defdde86cac2edd6746be1957bd
Calvin Jia <jia.calvin@gmail.com>
2015-09-01 17:35:08 -0700

    Have worker register with new master.

--- servers/src/main/java/tachyon/master/next/block/BlockMaster.java
+++ servers/src/main/java/tachyon/master/next/block/BlockMaster.java
@@ -373,9 +373,9 @@ public final class BlockMaster extends MasterBase implements ContainerIdGenerato
   public long workerRegister(long workerId, List<Long> totalBytesOnTiers,
       List<Long> usedBytesOnTiers, Map<Long, List<Long>> currentBlocksOnTiers) {
     synchronized (mWorkers) {
-      if (mWorkers.contains(mIdIndex, workerId)) {
+      if (!mWorkers.contains(mIdIndex, workerId)) {
         LOG.warn("Could not find worker id: " + workerId + " to register.");
-        return -1;
+        return -1L;
       }
       MasterWorkerInfo workerInfo = mWorkers.getFirstByField(mIdIndex, workerId);
       workerInfo.updateLastUpdatedTimeMs();
