fa620056b71e31327716cca33e11fc3179f7bc33
Ning Qu <quning@baidu.com>
2015-09-04 15:47:30 -0700

    [TACHYON-905] cleanup imports for PinListSync

--- servers/src/main/java/tachyon/worker/block/PinListSync.java
+++ servers/src/main/java/tachyon/worker/block/PinListSync.java
@@ -25,9 +25,6 @@ import tachyon.Constants;
 import tachyon.conf.TachyonConf;
 import tachyon.master.MasterClient;
 import tachyon.util.CommonUtils;
-import tachyon.util.ThreadFactoryUtils;
-import tachyon.util.network.NetworkAddressUtils;
-import tachyon.util.network.NetworkAddressUtils.ServiceType;
 
 /**
  * PinListSync periodically syncs the set of pinned inodes from master,
