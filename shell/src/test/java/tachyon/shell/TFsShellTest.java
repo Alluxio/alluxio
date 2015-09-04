a4112becc455baea8793c3308483190f18d387a7
cc <myairia@gmail.com>
2015-09-05 01:59:44 +0800

    Re-enable mkdirInvalidPathTest by throwing proper exception

--- shell/src/test/java/tachyon/shell/TFsShellTest.java
+++ shell/src/test/java/tachyon/shell/TFsShellTest.java
@@ -436,10 +436,10 @@ public class TFsShellTest {
     Assert.assertEquals(0, mFsShell.run(new String[] {"mkdir", "/testFile1"}));
   }
 
-//  @Test
-//  public void mkdirInvalidPathTest() throws IOException {
-//    Assert.assertEquals(-1, mFsShell.run(new String[] {"mkdir", "/test File Invalid Path"}));
-//  }
+  @Test
+  public void mkdirInvalidPathTest() throws IOException {
+    Assert.assertEquals(-1, mFsShell.run(new String[] {"mkdir", "/test File Invalid Path"}));
+  }
 
   @Test
   public void mkdirShortPathTest() throws IOException {
