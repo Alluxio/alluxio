6d97904e43f073d99e603fc17b42fb7b729d57a8
cc <myairia@gmail.com>
2015-09-05 00:03:40 +0800

    Should use cmd lsr in lsrTest

--- shell/src/test/java/tachyon/shell/TFsShellTest.java
+++ shell/src/test/java/tachyon/shell/TFsShellTest.java
@@ -370,16 +370,24 @@ public class TFsShellTest {
     TachyonFile fileC = TachyonFSTestUtils.createByteFile(mTfs, "/testRoot/testFileC",
         CacheType.NO_CACHE, UnderStorageType.PERSIST, 30);
     files[3] = mTfs.getInfo(fileC);
-    mFsShell.run(new String[] {"ls", "/testRoot"});
+    mFsShell.run(new String[] {"lsr", "/testRoot"});
     String expected = "";
     String format = "%-10s%-25s%-15s%-5s\n";
-    expected += String.format(format, FormatUtils.getSizeFromBytes(10),
-        TFsShell.convertMsToDate(files[0].getCreationTimeMs()), "In Memory", "/testRoot/testFileA");
-    expected += String.format(format, FormatUtils.getSizeFromBytes(0),
-        TFsShell.convertMsToDate(files[1].getCreationTimeMs()), "", "/testRoot/testDir");
-    expected += String.format(format, FormatUtils.getSizeFromBytes(30),
-        TFsShell.convertMsToDate(files[3].getCreationTimeMs()), "Not In Memory",
-        "/testRoot/testFileC");
+    expected +=
+        String.format(format, FormatUtils.getSizeFromBytes(10),
+            TFsShell.convertMsToDate(files[0].getCreationTimeMs()), "In Memory",
+            "/testRoot/testFileA");
+    expected +=
+        String.format(format, FormatUtils.getSizeFromBytes(0),
+            TFsShell.convertMsToDate(files[1].getCreationTimeMs()), "", "/testRoot/testDir");
+    expected +=
+        String.format(format, FormatUtils.getSizeFromBytes(20),
+            TFsShell.convertMsToDate(files[2].getCreationTimeMs()), "In Memory",
+            "/testRoot/testDir/testFileB");
+    expected +=
+        String.format(format, FormatUtils.getSizeFromBytes(30),
+            TFsShell.convertMsToDate(files[3].getCreationTimeMs()), "Not In Memory",
+            "/testRoot/testFileC");
     Assert.assertEquals(expected, mOutput.toString());
   }
 
