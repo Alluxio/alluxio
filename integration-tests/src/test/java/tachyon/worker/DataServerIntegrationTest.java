74f1f1e9f461f9854d2a2bd03ba277ca4efcad2f
Calvin Jia <jia.calvin@gmail.com>
2015-09-08 10:53:49 -0700

    Clean up integration tests.

--- integration-tests/src/test/java/tachyon/worker/DataServerIntegrationTest.java
+++ integration-tests/src/test/java/tachyon/worker/DataServerIntegrationTest.java
@@ -105,6 +105,9 @@ public class DataServerIntegrationTest {
     mTFS.close();
     mBlockMasterClient.close();
     mLocalTachyonCluster.stop();
+    System.clearProperty(Constants.WORKER_DATA_SERVER);
+    System.clearProperty(Constants.WORKER_NETTY_FILE_TRANSFER_TYPE);
+    System.clearProperty(Constants.USER_REMOTE_BLOCK_READER);
   }
 
   /**
