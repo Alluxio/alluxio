7858404bfd4a1b6b851f7862497869eeadeb82d0
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-08 16:29:21 -0700

    Addressing review comments.

--- deploy/vagrant/provision/roles/common/files/java.sh
+++ deploy/vagrant/provision/roles/common/files/java.sh
@@ -5,4 +5,4 @@
 # install java
 sudo yum install -y -q java-1.7.0-openjdk-devel.x86_64
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.85.x86_64" >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-openjdk" >> ~/.bashrc
