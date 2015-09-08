8b14ff3439f37d1340b81c3a6e5c949662d3e08c
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-08 16:29:21 -0700

    Addressing review comments.

--- deploy/vagrant/provision/roles/mesos/files/config_tachyon.sh
+++ deploy/vagrant/provision/roles/mesos/files/config_tachyon.sh
@@ -2,14 +2,18 @@
 
 set -e
 
-sed -i "s/# export TACHYON_MASTER_BIND_HOST/export TACHYON_MASTER_BIND_HOST/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_MASTER_WEB_BIND_HOST/export TACHYON_MASTER_WEB_BIND_HOST/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_WORKER_BIND_HOST/export TACHYON_WORKER_BIND_HOST/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_WORKER_DATA_BIND_HOST/export TACHYON_WORKER_DATA_BIND_HOST/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_WORKER_WEB_BIND_HOST/export TACHYON_WORKER_WEB_BIND_HOST/g" /tachyon/conf/tachyon-env.sh
+VARIABLES="\
+TACHYON_MASTER_BIND_HOST \
+TACHYON_MASTER_WEB_BIND_HOST \
+TACHYON_WORKER_BIND_HOST \
+TACHYON_WORKER_DATA_BIND_HOST \
+TACHYON_WORKER_WEB_BIND_HOST \
+TACHYON_MASTER_HOSTNAME \
+TACHYON_MASTER_WEB_HOSTNAME \
+TACHYON_WORKER_HOSTNAME \
+TACHYON_WORKER_DATA_HOSTNAME \
+TACHYON_WORKER_WEB_HOSTNAME"
 
-sed -i "s/# export TACHYON_MASTER_HOSTNAME/export TACHYON_MASTER_HOSTNAME/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_MASTER_WEB_HOSTNAME/export TACHYON_MASTER_WEB_HOSTNAME/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_WORKER_HOSTNAME/export TACHYON_WORKER_HOSTNAME/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_WORKER_DATA_HOSTNAME/export TACHYON_WORKER_DATA_HOSTNAME/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/# export TACHYON_WORKER_WEB_HOSTNAME/export TACHYON_WORKER_WEB_HOSTNAME/g" /tachyon/conf/tachyon-env.sh
+for var in ${VARIABLES}; do
+  sed -i "s/# ${var}/${var}/g" /tachyon/conf/tachyon-env.sh
+done
