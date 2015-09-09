02068e73ba159a6a86fbdec42bcde4f1d4591a5e
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-09 12:07:15 -0700

    Addressing design review comments.

--- mesos/bin/tachyon-worker.sh
+++ mesos/bin/tachyon-worker.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+#
+# Usage:
+#  tachyon-worker.sh <master-hostname> <memory-allocation>
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 source "${SCRIPT_DIR}/common.sh"
 TACHYON_WORKER_JAVA_OPTS="${TACHYON_WORKER_JAVA_OPTS:-${TACHYON_JAVA_OPTS}}"
 MESOS_LIBRARY_PATH="${MESOS_LIBRARY_PATH:-/usr/local/lib}"
+MASTER_HOSTNAME="$1"
+WORKER_MEMOORY_SIZE="$2"
 
 mkdir -p "${TACHYON_LOGS_DIR}"
 
-"${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Dtachyon.logger.type="WORKER_LOGGER" -Dtachyon.accesslogger.type="WORKER_ACCESS_LOGGER" -Djava.library.path="${MESOS_LIBRARY_PATH}" -Dlog4j.configuration=file:"${TACHYON_CONF_DIR}/log4j.properties" ${TACHYON_WORKER_JAVA_OPTS} tachyon.mesos.TachyonWorkerExecutor > "${TACHYON_LOGS_DIR}"/worker.out 2>&1
+"${JAVA}" -cp "${CLASSPATH}" \
+  -Djava.library.path="${MESOS_LIBRARY_PATH}" \
+  -Dlog4j.configuration=file:"${TACHYON_CONF_DIR}/log4j.properties" \
+  -Dtachyon.accesslogger.type="WORKER_ACCESS_LOGGER" \
+  -Dtachyon.home="${TACHYON_HOME}" \
+  -Dtachyon.logger.type="WORKER_LOGGER" \
+  -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" \
+  -Dtachyon.master.hostname="${MASTER_HOSTNAME}" \
+  -Dtachyon.worker.memory.size="${WORKER_MEMORY_SIZE}" \
+  ${TACHYON_WORKER_JAVA_OPTS} tachyon.mesos.TachyonWorkerExecutor > "${TACHYON_LOGS_DIR}"/worker.out 2>&1
--- mesos/bin/test-worker.sh
+++ mesos/bin/test-worker.sh
@@ -7,7 +7,18 @@ TACHYON_WORKER_JAVA_OPTS="${TACHYON_WORKER_JAVA_OPTS:-${TACHYON_JAVA_OPTS}}"
 mkdir -p "${TACHYON_LOGS_DIR}"
 
 # Format the worker.
-"${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Dtachyon.master.hostname="${TACHYON_MASTER_ADDRESS}" -Dtachyon.logger.type="USER_LOGGER" ${TACHYON_JAVA_OPTS} "tachyon.Format" "worker"
+"${JAVA}" -cp "${CLASSPATH}" \
+  -Dtachyon.home="${TACHYON_HOME}" \
+  -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" \
+  -Dtachyon.master.hostname="${TACHYON_MASTER_ADDRESS}" \
+  -Dtachyon.logger.type="USER_LOGGER" \
+  ${TACHYON_JAVA_OPTS} "tachyon.Format" "worker"
 
 # Start the worker.
-"${JAVA}" -cp "${CLASSPATH}" -Dtachyon.home="${TACHYON_HOME}" -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" -Dtachyon.logger.type="WORKER_LOGGER" -Dtachyon.accesslogger.type="WORKER_ACCESS_LOGGER" -Dlog4j.configuration=file:"${TACHYON_CONF_DIR}/log4j.properties" ${TACHYON_WORKER_JAVA_OPTS} tachyon.mesos.TestWorkerExecutor > "${TACHYON_LOGS_DIR}"/test-worker.out 2>&1
+"${JAVA}" -cp "${CLASSPATH}" \
+  -Dtachyon.home="${TACHYON_HOME}" \
+  -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" \
+  -Dtachyon.logger.type="WORKER_LOGGER" \
+  -Dtachyon.accesslogger.type="WORKER_ACCESS_LOGGER" \
+  -Dlog4j.configuration=file:"${TACHYON_CONF_DIR}/log4j.properties" \
+  ${TACHYON_WORKER_JAVA_OPTS} tachyon.mesos.TestWorkerExecutor > "${TACHYON_LOGS_DIR}"/test-worker.out 2>&1
