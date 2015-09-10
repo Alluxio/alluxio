c3dae263a4c6efa96bb13ce7e504b4ae761ecdf4
Bin Fan <fanbin103@gmail.com>
2015-09-09 22:12:01 -0700

    [SMALLFIX] Fix merge conflicts

--- common/src/main/java/tachyon/security/authorization/FsPermission.java
+++ common/src/main/java/tachyon/security/authorization/FsPermission.java
@@ -148,8 +148,7 @@ public class FsPermission {
   public static FsPermission getUMask(TachyonConf conf) {
     int umask = Constants.DEFAULT_TFS_PERMISSIONS_UMASK;
     if (conf != null) {
-      umask = conf.getInt(Constants.TFS_PERMISSIONS_UMASK_KEY,
-          Constants.DEFAULT_TFS_PERMISSIONS_UMASK);
+      umask = conf.getInt(Constants.TFS_PERMISSIONS_UMASK_KEY);
     }
     return new FsPermission((short)umask);
   }
