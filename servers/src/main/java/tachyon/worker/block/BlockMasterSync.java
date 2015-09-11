b35fb64881b36bc98c810333f311d279c37bb714
wangchao30 <wangchao30@baidu.com>
2015-09-11 15:06:43 -0700

    fix #941, change tachyon.master.MasterClient to tachyon.client.BlockMasterClient

--- servers/src/main/java/tachyon/worker/block/BlockMasterSync.java
+++ servers/src/main/java/tachyon/worker/block/BlockMasterSync.java
@@ -38,7 +38,7 @@ import tachyon.util.network.NetworkAddressUtils.ServiceType;
 
 /**
  * Task that carries out the necessary block worker to master communications, including register and
- * heartbeat. This class manages its own {@link tachyon.master.MasterClient}.
+ * heartbeat. This class manages its own {@link tachyon.client.BlockMasterClient}.
  *
  * When running, this task first requests a block report from the
  * {@link tachyon.worker.block.BlockDataManager}, then sends it to the master. The master may
