021507fd5d2eec8ba88491d344b681e9295bdef7
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-14 09:07:12 -0700

    Tagging TODOs in tachyon.shell.

--- shell/src/test/java/tachyon/shell/TFsShellTest.java
+++ shell/src/test/java/tachyon/shell/TFsShellTest.java
@@ -221,7 +221,7 @@ public class TFsShellTest {
     Assert.assertEquals(new String(read), dataString);
   }
 
-  // TODO: Investigate why this takes a long time? Around 7min on my Mac!
+  // TODO(cc): Investigate why this takes a long time? Around 7min on my Mac!
   @Test
   public void copyToLocalLargeTest() throws IOException {
     copyToLocalWithBytes(SIZE_BYTES);
