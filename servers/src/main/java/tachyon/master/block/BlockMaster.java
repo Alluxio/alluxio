c735df65d6e8fdae7cb1b300947445e9bd6a01b9
cc <myairia@gmail.com>
2015-09-15 21:48:54 +0800

    Fix signature and implementation of BlockMaster#workerRegister

--- servers/src/main/java/tachyon/master/block/BlockMaster.java
+++ servers/src/main/java/tachyon/master/block/BlockMaster.java
@@ -469,18 +469,20 @@ public final class BlockMaster extends MasterBase implements ContainerIdGenerabl
    * Updates metadata when a worker registers with the master. Called by workers via RPC.
    *
    * @param workerId the worker id of the worker registering
+   * @param workerAddress the worker NetAddress of the worker registering
    * @param totalBytesOnTiers list of total bytes on each tier
    * @param usedBytesOnTiers list of the used byes on each tier
    * @param currentBlocksOnTiers a mapping of each storage dir, to all the blocks on that storage
    * @return the worker id
    */
-  public long workerRegister(long workerId, List<Long> totalBytesOnTiers,
+  public long workerRegister(long workerId, NetAddress workerAddress, List<Long> totalBytesOnTiers,
       List<Long> usedBytesOnTiers, Map<Long, List<Long>> currentBlocksOnTiers) {
     synchronized (mBlocks) {
       synchronized (mWorkers) {
         if (!mWorkers.contains(mIdIndex, workerId)) {
-          LOG.warn("Could not find worker id: " + workerId + " to register.");
-          return -1L;
+          MasterWorkerInfo info = new MasterWorkerInfo(workerId, workerAddress);
+          LOG.info(info + " re-registers to master.");
+          mWorkers.add(info);
         }
         MasterWorkerInfo workerInfo = mWorkers.getFirstByField(mIdIndex, workerId);
         workerInfo.updateLastUpdatedTimeMs();
