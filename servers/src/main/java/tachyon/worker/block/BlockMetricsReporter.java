9eca5421fad54a48b1106731ee09b9d0215685e8
Jiri Simsa <jiri.simsa@gmail.com>
2015-09-15 10:43:33 -0700

    Undoing changes tracking down a memory leak.

--- servers/src/main/java/tachyon/worker/block/BlockMetricsReporter.java
+++ servers/src/main/java/tachyon/worker/block/BlockMetricsReporter.java
@@ -23,7 +23,7 @@ import tachyon.worker.WorkerSource;
 /**
  * This class listens on block events and increases the metrics counters.
  */
-public class BlockMetricsReporter extends BlockStoreEventListenerBase {
+public final class BlockMetricsReporter extends BlockStoreEventListenerBase {
   private final WorkerSource mWorkerSource;
 
   public BlockMetricsReporter(WorkerSource workerSource) {
