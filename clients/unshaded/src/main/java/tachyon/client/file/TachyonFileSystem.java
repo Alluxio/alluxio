98d5fbb24d8dac7fa5d3cfbd546e733cf95fab99
Calvin Jia <jia.calvin@gmail.com>
2015-09-15 17:40:02 -0700

    Make TachyonFileSystem an abstract class for TachyonFileSystemCore.

--- clients/unshaded/src/main/java/tachyon/client/file/TachyonFileSystem.java
+++ clients/unshaded/src/main/java/tachyon/client/file/TachyonFileSystem.java
@@ -15,11 +15,8 @@
 
 package tachyon.client.file;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.List;
-
-import com.google.common.base.Preconditions;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,37 +24,20 @@ import org.slf4j.LoggerFactory;
 import tachyon.Constants;
 import tachyon.TachyonURI;
 import tachyon.annotation.PublicApi;
-import tachyon.client.ClientOptions;
 import tachyon.client.FileSystemMasterClient;
-import tachyon.thrift.BlockInfoException;
-import tachyon.thrift.DependencyDoesNotExistException;
 import tachyon.thrift.FileAlreadyExistException;
 import tachyon.thrift.FileDoesNotExistException;
 import tachyon.thrift.FileInfo;
 import tachyon.thrift.InvalidPathException;
 
 /**
- * Tachyon File System client. This class is the entry point for all file level operations on
- * Tachyon files. An instance of this class can be obtained via {@link TachyonFileSystem#get}. This
- * class is thread safe. The read/write interface provided by this client is similar to Java's
- * input/output streams.
+ * Tachyon File System client. This class should be used to interface with the Tachyon File
+ * System master and supports all non IO operations. Implementing classes should provide their
+ * own IO methods separate from the {@link TachyonFileSystemCore} interface.
  */
 @PublicApi
-public class TachyonFileSystem implements Closeable, TachyonFSCore {
+public abstract class TachyonFileSystem implements TachyonFileSystemCore {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
-
-  /** A cached instance of the TachyonFileSystem */
-  private static TachyonFileSystem sClient;
-
-  /**
-   * @return a TachyonFileSystem instance, there is only one instance available at any time
-   */
-  public static synchronized TachyonFileSystem get() {
-    if (sClient == null) {
-      sClient = new TachyonFileSystem();
-    }
-    return sClient;
-  }
 
   /** The file system context which contains shared resources, such as the fs master client */
   private FileSystemContext mContext;
@@ -67,17 +47,6 @@ public class TachyonFileSystem implements Closeable, TachyonFSCore {
    */
   private TachyonFileSystem() {
     mContext = FileSystemContext.INSTANCE;
-  }
-
-  /**
-   * Closes this TachyonFS instance. The next call to get will create a new TachyonFS instance.
-   * Other references to the old client may still be used.
-   */
-  // TODO(calvin): Evaluate the necessity of this method.
-  @Override
-  public synchronized void close() {
-    LOG.info("Tachyon File System Client is closed");
-    sClient = null;
   }
 
   /**
@@ -117,7 +86,6 @@ public class TachyonFileSystem implements Closeable, TachyonFSCore {
    * {@inheritDoc} The file info is a snapshot of the file metadata, and the locations, last
    * modified time, and path are possibly inconsistent.
    */
-  // TODO(calvin): Consider FileInfo caching.
   @Override
   public FileInfo getInfo(TachyonFile file) throws IOException {
     FileSystemMasterClient masterClient = mContext.acquireMasterClient();
@@ -128,59 +96,6 @@ public class TachyonFileSystem implements Closeable, TachyonFSCore {
     } finally {
       mContext.releaseMasterClient(masterClient);
     }
-  }
-
-  /**
-   * Gets a {@link FileInStream} for the specified file. The stream's settings can be customized by
-   * setting the options parameter. The caller should close the stream after finishing the
-   * operations on it.
-   *
-   * @param file the handler for the file.
-   * @param options the set of options specific to this operation.
-   * @return an input stream to read the file
-   * @throws FileDoesNotExistException if the file does not exist
-   * @throws IOException if the stream cannot be opened for some other reason
-   */
-  public FileInStream getInStream(TachyonFile file, ClientOptions options) throws IOException,
-      FileDoesNotExistException {
-    FileSystemMasterClient masterClient = mContext.acquireMasterClient();
-    try {
-      FileInfo info = masterClient.getFileInfo(file.getFileId());
-      Preconditions.checkState(!info.isIsFolder(), "Cannot read from a folder");
-      return new FileInStream(info, options);
-    } finally {
-      mContext.releaseMasterClient(masterClient);
-    }
-  }
-
-  /**
-   * Creates a file and gets the {@link FileOutStream} for the specified file. This should only be
-   * called to write a file that does not exist. Once close is called on the output stream, the file
-   * will be completed. Append or update of a completed file is currently not supported.
-   *
-   * @param path the Tachyon path of the file
-   * @param options the set of options specific to this operation
-   * @return an output stream to write the file
-   * @throws InvalidPathException if the provided path is invalid
-   * @throws FileAlreadyExistException if the file being written to already exists
-   * @throws BlockInfoException if the provided block size is invalid
-   * @throws IOException if the file already exists or if the stream cannot be opened
-   */
-  public FileOutStream getOutStream(TachyonURI path, ClientOptions options) throws IOException,
-      InvalidPathException, FileAlreadyExistException, BlockInfoException {
-    FileSystemMasterClient masterClient = mContext.acquireMasterClient();
-    try {
-      long fileId = masterClient.createFile(path.getPath(), options.getBlockSize(), true);
-      return new FileOutStream(fileId, options);
-    } finally {
-      mContext.releaseMasterClient(masterClient);
-    }
-  }
-
-  // TODO(calvin): We should remove this when the TachyonFS code is fully deprecated.
-  @Deprecated
-  public FileOutStream getOutStream(long fileId, ClientOptions options) throws IOException {
-    return new FileOutStream(fileId, options);
   }
 
   /**
@@ -256,14 +171,9 @@ public class TachyonFileSystem implements Closeable, TachyonFSCore {
   }
 
   /**
-   * Sets the pin status of a file. A pinned file will never be evicted for any reason. The pin
-   * status is propagated asynchronously from this method call on the worker heartbeats.
-   *
-   * @param file the file handler for the file to pin
-   * @param pinned true to pin the file, false to unpin it
-   * @throws FileDoesNotExistException if the file does not exist
-   * @throws IOException if an error occurs during the pin operation
+   * {@inheritDoc}
    */
+  @Override
   public void setPin(TachyonFile file, boolean pinned) throws IOException,
       FileDoesNotExistException {
     FileSystemMasterClient masterClient = mContext.acquireMasterClient();
@@ -288,25 +198,6 @@ public class TachyonFileSystem implements Closeable, TachyonFSCore {
         LOG.info("Renamed file " + src.getFileId() + " to " + dst.getPath());
       }
       return result;
-    } finally {
-      mContext.releaseMasterClient(masterClient);
-    }
-  }
-
-  public void reportLostFile(TachyonFile file) throws IOException, FileDoesNotExistException {
-    FileSystemMasterClient masterClient = mContext.acquireMasterClient();
-    try {
-      masterClient.reportLostFile(file.getFileId());
-    } finally {
-      mContext.releaseMasterClient(masterClient);
-    }
-  }
-
-  public void requestFilesInDependency(int depId) throws IOException,
-      DependencyDoesNotExistException {
-    FileSystemMasterClient masterClient = mContext.acquireMasterClient();
-    try {
-      masterClient.requestFilesInDependency(depId);
     } finally {
       mContext.releaseMasterClient(masterClient);
     }
