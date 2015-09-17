5bb05448e2de022fc8a9fba233444135033dda47
Yupeng Fu <yupeng.fu@gmail.com>
2015-09-17 16:16:15 -0700

    Added addLineage in lineage master

--- servers/src/main/java/tachyon/master/lineage/meta/LineageStore.java
+++ servers/src/main/java/tachyon/master/lineage/meta/LineageStore.java
@@ -14,12 +14,15 @@
  */
 package tachyon.master.lineage.meta;
 
+import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import tachyon.client.file.TachyonFile;
 import tachyon.dag.DAG;
+import tachyon.job.Job;
 
 /**
  * A store of lineages.
@@ -28,17 +31,42 @@ public final class LineageStore {
   private DAG<Lineage> mLineageDAG;
 
   /** Indices for lineages */
-  private Map<TachyonFile, Lineage> mInputFileIndex;
+  /** Index of the output files of lineage to lineage */
   private Map<TachyonFile, Lineage> mOutputFileIndex;
   private Map<LineageState, Lineage> mStateIndex;
+  private Map<Long, Lineage> mIdIndex;
 
   public LineageStore() {
     mLineageDAG = new DAG<Lineage>();
-    mInputFileIndex = Maps.newHashMap();
     mOutputFileIndex = Maps.newHashMap();
     mStateIndex = Maps.newHashMap();
+    mIdIndex = Maps.newHashMap();
   }
 
+  public long addLineage(List<TachyonFile> inputFiles, List<TachyonFile> outputFiles, Job job) {
+    Lineage lineage  = new Lineage(inputFiles, outputFiles, job);
 
+    List<Lineage> parentLineages = Lists.newArrayList();
+    for(TachyonFile inputFile:inputFiles) {
+      if(mOutputFileIndex.containsKey(inputFile)) {
+        parentLineages.add(mOutputFileIndex.get(inputFile));
+      }
+    }
 
+    mLineageDAG.add(lineage, parentLineages);
+
+    // update index
+    for (TachyonFile outputFile : outputFiles) {
+      mOutputFileIndex.put(outputFile, lineage);
+    }
+    mStateIndex.put(lineage.getState(), lineage);
+    mIdIndex.put(lineage.getId(), lineage);
+
+    return lineage.getId();
+  }
+
+  private void updateState(Lineage lineage, LineageState newState) {
+    lineage.setState(newState);
+    mStateIndex.put(newState, lineage);
+  }
 }
