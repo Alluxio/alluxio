db84d79803ab9a24ad6b505fa9467d2685f518d2
Bin Fan <fanbin103@gmail.com>
2015-09-17 16:11:39 -0700

    [TACHYON-964] Add a script to launch tachyon on YARN

---
+++ integration/bin/tachyon-yarn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Usage:
+#  tachyon-yarn.sh
+
+APP_MASTER_JAR=hdfs://namenode:9000/tachyon-assemblies-0.8.0-SNAPSHOT-jar-with-dependencies.jar
+
+${HADOOP_HOME}/bin/hadoop fs -put -f ${TACHYON_HOME}/assembly/target/tachyon-assemblies-0.8.0-SNAPSHOT-jar-with-dependencies.jar ${APP_MASTER_JAR}
+
+${HADOOP_HOME}/bin/yarn jar ${TACHYON_HOME}/assembly/target/tachyon-assemblies-0.8.0-SNAPSHOT-jar-with-dependencies.jar tachyon.yarn.Client -jar ${APP_MASTER_JAR} \
+    -num_workers 1 -tachyon_home ${TACHYON_HOME}
