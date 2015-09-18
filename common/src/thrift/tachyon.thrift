926db4d7e123d6e79d9e917492f1e3640b0102da
cc <myairia@gmail.com>
2015-09-18 11:13:17 +0800

    Change signature of BlockMasterService#workerRegister
    fix

--- common/src/thrift/tachyon.thrift
+++ common/src/thrift/tachyon.thrift
@@ -144,9 +144,9 @@ exception DependencyDoesNotExistException {
 service BlockMasterService {
   i64 workerGetWorkerId(1: NetAddress workerNetAddress)
 
-  i64 workerRegister(1: i64 workerId, 2: list<i64> totalBytesOnTiers, 3: list<i64> usedBytesOnTiers,
-      4: map<i64, list<i64>> currentBlocksOnTiers)
-    throws (1: BlockInfoException bie)
+  void workerRegister(1: i64 workerId, 2: list<i64> totalBytesOnTiers,
+      3: list<i64> usedBytesOnTiers, 4: map<i64, list<i64>> currentBlocksOnTiers)
+    throws (1: TachyonException bie)
 
   Command workerHeartbeat(1: i64 workerId, 2: list<i64> usedBytesOnTiers,
       3: list<i64> removedBlockIds, 4: map<i64, list<i64>> addedBlocksOnTiers)
