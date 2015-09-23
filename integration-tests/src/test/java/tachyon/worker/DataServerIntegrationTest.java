06f94db95de800932d135238f6f52b313724bbbb
cc <myairia@gmail.com>
2015-09-23 17:43:03 +0800

    debug

--- integration-tests/src/test/java/tachyon/worker/DataServerIntegrationTest.java
+++ integration-tests/src/test/java/tachyon/worker/DataServerIntegrationTest.java
@@ -58,6 +58,7 @@ import tachyon.util.io.BufferUtils;
  */
 // let jenkins test again
 // let jenkins test again
+// let jenkins test again
 @RunWith(Parameterized.class)
 public class DataServerIntegrationTest {
   private static final int WORKER_CAPACITY_BYTES = 1000;
