2328b3075dd497c25307a274ac4c6e7093c1a40f
cc <myairia@gmail.com>
2015-09-23 17:28:13 +0800

    debug

--- integration-tests/src/test/java/tachyon/worker/DataServerIntegrationTest.java
+++ integration-tests/src/test/java/tachyon/worker/DataServerIntegrationTest.java
@@ -57,6 +57,7 @@ import tachyon.util.io.BufferUtils;
  * Integration tests for tachyon.worker.DataServer.
  */
 // let jenkins test again
+// let jenkins test again
 @RunWith(Parameterized.class)
 public class DataServerIntegrationTest {
   private static final int WORKER_CAPACITY_BYTES = 1000;
@@ -368,4 +369,3 @@ public class DataServerIntegrationTest {
     return mBlockMasterClient.getBlockInfo(fileInfo.blockIds.get(0));
   }
 }
-autogen to debug jenkins
