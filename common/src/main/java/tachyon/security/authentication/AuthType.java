cb609336889c41209c63bd8ceac6b63d4a8ce17d
Bin Fan <fanbin103@gmail.com>
2015-09-24 18:04:02 -0700

    [TACHYON-593] Remove redundant string value setting for an enum

--- common/src/main/java/tachyon/security/authentication/AuthType.java
+++ common/src/main/java/tachyon/security/authentication/AuthType.java
@@ -22,33 +22,27 @@ public enum AuthType {
   /**
    * Authentication is disabled. No user info in Tachyon.
    */
-  NOSASL("NOSASL"),
+  NOSASL,
 
   /**
    * User is aware in Tachyon. Login user is OS user. The verification of client user is disabled.
    */
-  SIMPLE("SIMPLE"),
+  SIMPLE,
 
   /**
    * User is aware in Tachyon. Login user is OS user. The user is verified by Custom
    * authentication provider (Use with property tachyon.security.authentication.custom.provider
    * .class).
    */
-  CUSTOM("CUSTOM"),
+  CUSTOM,
 
   /**
    * User is aware in Tachyon. The user is verified by Kerberos authentication. NOTE: this
    * authentication is not supported.
    */
-  KERBEROS("KERBEROS");
-
-  private final String mAuthType;
-
-  AuthType(String authType) {
-    mAuthType = authType;
-  }
+  KERBEROS;
 
   public String getAuthName() {
-    return mAuthType;
+    return name();
   }
 }
