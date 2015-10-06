1eedb5910221976e6b20542d69d90af5152f0d2c
cc <myairia@gmail.com>
2015-10-06 12:05:52 +0800

    Update the scripts for configuring tachyon-env.sh

--- deploy/vagrant/provision/roles/ufs_hadoop1/files/config_tachyon.sh
+++ deploy/vagrant/provision/roles/ufs_hadoop1/files/config_tachyon.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# last should be master
-namenode=`tail -n1 /tachyon/conf/workers`
-
-# create tachyon env
-/bin/cp /tachyon/conf/tachyon-env.sh.template /tachyon/conf/tachyon-env.sh
-sed -i "s/#export TACHYON_UNDERFS_ADDRESS=hdfs:\/\/localhost:9000/export TACHYON_UNDERFS_ADDRESS=hdfs:\/\/${namenode}:9000/g" /tachyon/conf/tachyon-env.sh
-sed -i "s/export TACHYON_MASTER_ADDRESS=localhost/export TACHYON_MASTER_ADDRESS=${namenode}/g" /tachyon/conf/tachyon-env.sh
+sed -i "s/^export TACHYON_UNDERFS_ADDRESS=.*/export TACHYON_UNDERFS_ADDRESS=hdfs:\/\/\$\{TACHYON_MASTER_ADDRESS\}:9000/g" /tachyon/conf/tachyon-env.sh
