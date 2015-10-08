5d9f1f708305eceaf63972b4abd4e9758cc07a35
cc <myairia@gmail.com>
2015-10-08 14:13:12 +0800

    Set longer worker heartbeat timeout in fault tolerant mode
    synctax typo

--- deploy/vagrant/provision/roles/tachyon/files/set_journal_folder.sh
+++ deploy/vagrant/provision/roles/tachyon/files/set_journal_folder.sh
@@ -3,5 +3,5 @@
 # For Tachyon version that is no less than 0.8
 echo 'tachyon.master.journal.folder=${tachyon.underfs.address}/tachyon/journal/' >> /tachyon/conf/tachyon-site.properties
 
-# For ealier Tachyon versions
+# For earlier Tachyon versions
 sed -i "s/tachyon.master.journal.folder=.*/tachyon.master.journal.folder=\$TACHYON_UNDERFS_ADDRESS\/tachyon\/journal\//g" /tachyon/conf/tachyon-env.sh
