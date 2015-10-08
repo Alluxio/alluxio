96024ffe090a884691aefd6980ea58465b762421
cc <myairia@gmail.com>
2015-10-08 12:55:34 +0800

    Back compatibility for configuring journal folder
    configure journal folder
    
    synctax typo

---
+++ deploy/vagrant/provision/roles/tachyon/files/set_journal_folder.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# For Tachyon version that is no less than 0.8
+echo 'tachyon.master.journal.folder=${tachyon.underfs.address}/tachyon/journal/' >> /tachyon/conf/tachyon-site.properties
+
+# For ealier Tachyon versions
+sed -i "s/tachyon.master.journal.folder=.*/tachyon.master.journal.folder=\$TACHYON_UNDERFS_ADDRESS\/tachyon\/journal\//g" /tachyon/conf/tachyon-env.sh
