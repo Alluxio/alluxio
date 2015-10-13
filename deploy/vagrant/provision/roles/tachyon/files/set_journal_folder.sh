060625091467ba0852ca18aa072c17222fcf4146
cc <myairia@gmail.com>
2015-10-13 12:25:43 +0800

    Do not set both tachyon-env.sh and tachyon-site.properties

--- deploy/vagrant/provision/roles/tachyon/files/set_journal_folder.sh
+++ deploy/vagrant/provision/roles/tachyon/files/set_journal_folder.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# For Tachyon version that is no less than 0.8
+# remove the property from tachyon-env.sh if it exists and set it in tachyon-site.properties
+sed -i "s/-Dtachyon.master.journal.folder=.*//g" /tachyon/conf/tachyon-env.sh
 echo 'tachyon.master.journal.folder=${tachyon.underfs.address}/tachyon/journal/' >> /tachyon/conf/tachyon-site.properties
-
-# For earlier Tachyon versions
-sed -i "s/tachyon.master.journal.folder=.*/tachyon.master.journal.folder=\$TACHYON_UNDERFS_ADDRESS\/tachyon\/journal\//g" /tachyon/conf/tachyon-env.sh
