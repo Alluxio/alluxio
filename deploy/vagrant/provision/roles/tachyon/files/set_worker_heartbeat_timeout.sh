f955f0264e1ea31037b9a9a2cdc9ce71b2111fab
cc <myairia@gmail.com>
2015-10-13 12:25:43 +0800

    Do not set both tachyon-env.sh and tachyon-site.properties

--- deploy/vagrant/provision/roles/tachyon/files/set_worker_heartbeat_timeout.sh
+++ deploy/vagrant/provision/roles/tachyon/files/set_worker_heartbeat_timeout.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-# For Tachyon version that is no less than 0.8
 echo 'tachyon.worker.block.heartbeat.timeout.ms=1000000' >> /tachyon/conf/tachyon-site.properties
-
-# For earlier Tachyon versions
-sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
-  -Dtachyon.worker.heartbeat.timeout.ms=1000000
-" /tachyon/conf/tachyon-env.sh
