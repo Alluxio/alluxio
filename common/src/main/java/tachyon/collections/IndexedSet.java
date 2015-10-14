7b15bd7c917b8027c205670f20db02f9d316d341
Andrew Audibert <aaudibert10@gmail.com>
2015-10-13 23:24:00 -0700

    [SMALLFIX] Remove unused import in IndexedSet

--- common/src/main/java/tachyon/collections/IndexedSet.java
+++ common/src/main/java/tachyon/collections/IndexedSet.java
@@ -28,7 +28,6 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import tachyon.Constants;
