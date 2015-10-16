7f91e1fc2441257a2901fe27c36cc834fdf818e3
cc <myairia@gmail.com>
2015-10-16 16:30:28 +0800

    Use ExceptionMessage in RawTableMaster

--- common/src/main/java/tachyon/exception/ExceptionMessage.java
+++ common/src/main/java/tachyon/exception/ExceptionMessage.java
@@ -62,7 +62,11 @@ public enum ExceptionMessage {
   UNEXPECETD_JOURNAL_ENTRY("Unexpected entry in journal: {0}"),
   FILEID_MUST_BE_FILE("File id {0} must be a file"),
   JOURNAL_WRITE_AFTER_CLOSE("Cannot write entry after closing the stream"),
+  RAW_TABLE_COLUMN_OVERRANGE("Number of column: {0} should range from 0 to {1} non-inclusive"),
   RAW_TABLE_ID_DOES_NOT_EXIST("Raw table with id {0} does not exist"),
+  RAW_TABLE_ID_DUPLICATED("There is already a raw table with id {0}"),
+  RAW_TABLE_METADATA_OVERSIZED("Size of raw table metadata {0} should be smaller than {1}"),
+  RAW_TABLE_PATH_DOES_NOT_EXIST("Raw table with path {0} does not exist"),
   UNKNOWN_ENTRY_TYPE("Unknown entry type: {0}"),
 
   // lineage
