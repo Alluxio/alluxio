9adf457999da8814883b3350ff4bd2f4c8da201c
Yupeng Fu <yupeng.fu@gmail.com>
2015-10-16 11:16:16 -0700

    Put the lineage master and worker behind the flag

--- servers/src/main/java/tachyon/master/TachyonMaster.java
+++ servers/src/main/java/tachyon/master/TachyonMaster.java
@@ -170,7 +170,10 @@ public class TachyonMaster {
       mBlockMaster = new BlockMaster(mBlockMasterJournal);
       mFileSystemMaster = new FileSystemMaster(mBlockMaster, mFileSystemMasterJournal);
       mRawTableMaster = new RawTableMaster(mFileSystemMaster, mRawTableMasterJournal);
-      mLineageMaster = new LineageMaster(mFileSystemMaster, mLineageMasterJournal);
+      boolean enableLineage = MasterContext.getConf().getBoolean(Constants.USER_LINEAGE_ENABLED);
+      if (enableLineage) {
+        mLineageMaster = new LineageMaster(mFileSystemMaster, mLineageMasterJournal);
+      }
 
       MasterContext.getMasterSource().registerGauges(this);
       mMasterMetricsSystem = new MetricsSystem("master", MasterContext.getConf());
@@ -279,7 +282,9 @@ public class TachyonMaster {
       mBlockMaster.start(isLeader);
       mFileSystemMaster.start(isLeader);
       mRawTableMaster.start(isLeader);
-      mLineageMaster.start(isLeader);
+      if (mLineageMaster != null) {
+        mLineageMaster.start(isLeader);
+      }
 
     } catch (IOException e) {
       LOG.error(e.getMessage(), e);
@@ -289,7 +294,9 @@ public class TachyonMaster {
 
   protected void stopMasters() {
     try {
-      mLineageMaster.stop();
+      if (mLineageMaster != null) {
+        mLineageMaster.stop();
+      }
       mBlockMaster.stop();
       mFileSystemMaster.stop();
       mRawTableMaster.stop();
@@ -331,7 +338,9 @@ public class TachyonMaster {
     processor.registerProcessor(mFileSystemMaster.getServiceName(),
         mFileSystemMaster.getProcessor());
     processor.registerProcessor(mRawTableMaster.getServiceName(), mRawTableMaster.getProcessor());
-    processor.registerProcessor(mLineageMaster.getServiceName(), mLineageMaster.getProcessor());
+    if (mLineageMaster != null) {
+      processor.registerProcessor(mLineageMaster.getServiceName(), mLineageMaster.getProcessor());
+    }
 
     // Return a TTransportFactory based on the authentication type
     TTransportFactory transportFactory;
