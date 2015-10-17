e53baf5c66b83790e6864a3a426fc7d5a64e8ec0
Jiri Simsa <jiri.simsa@gmail.com>
2015-10-16 18:29:09 -0700

    Using BlockSizeBytes instead of BlockSizeByte.

--- clients/unshaded/src/main/java/tachyon/client/file/FileOutStream.java
+++ clients/unshaded/src/main/java/tachyon/client/file/FileOutStream.java
@@ -80,7 +80,7 @@ public class FileOutStream extends OutputStream implements Cancelable {
   public FileOutStream(long fileId, OutStreamOptions options) throws IOException {
     mFileId = fileId;
     mNonce = ClientContext.getRandomNonNegativeLong();
-    mBlockSize = options.getBlockSize();
+    mBlockSize = options.getBlockSizeBytes();
     mTachyonStorageType = options.getTachyonStorageType();
     mUnderStorageType = options.getUnderStorageType();
     mContext = FileSystemContext.INSTANCE;
