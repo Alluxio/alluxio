8943516eaf8d595e9dbd3d478eccda2baacab0e2
Jiri Simsa <jiri.simsa@gmail.com>
2015-10-16 18:27:52 -0700

    Adding unit tests for server-side options.

---
+++ servers/src/test/java/tachyon/master/file/meta/options/CreatePathOptionsTest.java
@@ -0,0 +1,7 @@
+package tachyon.master.file.meta.options;
+
+/**
+ * Created by jsimsa on 10/16/15.
+ */
+public class CreatePathOptionsTest {
+}
---
+++ servers/src/test/java/tachyon/master/file/options/MkdirOptionsTest.java
@@ -0,0 +1,7 @@
+package tachyon.master.file.options;
+
+/**
+ * Created by jsimsa on 10/16/15.
+ */
+public class MkdirOptionsTest {
+}
