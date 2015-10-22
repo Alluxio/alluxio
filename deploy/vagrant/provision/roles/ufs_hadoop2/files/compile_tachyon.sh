6012146868cb507f44b200dd74330d9a1f12a334
Jiri Simsa <jiri.simsa@gmail.com>
2015-10-21 17:35:56 -0700

    Fixing Mesos integration.

--- deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_tachyon.sh
+++ deploy/vagrant/provision/roles/ufs_hadoop2/files/compile_tachyon.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cd /tachyon
-mvn clean install -Dmaven.javadoc.skip=true -DskipTests -Dhadoop.version=${HADOOP_VERSION} -Dlicense.skip=true -Dcheckstyle.skip=true >mvn.log 2>&1
+mvn clean install -Dmaven.javadoc.skip=true -DskipTests -Dhadoop.version=${HADOOP_VERSION} -Dlicense.skip=true -Dcheckstyle.skip=true -Pmesos -Pyarn >mvn.log 2>&1
