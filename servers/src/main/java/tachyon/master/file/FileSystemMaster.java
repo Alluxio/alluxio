ee5673fcbf05250e5734238481e06311f7ed6be5
Jiri Simsa <jiri.simsa@gmail.com>
2015-10-26 08:01:47 -0700

    Using build setters and regular setters consistently.

--- servers/src/main/java/tachyon/master/file/FileSystemMaster.java
+++ servers/src/main/java/tachyon/master/file/FileSystemMaster.java
@@ -1382,12 +1382,12 @@ public final class FileSystemMaster extends MasterBase {
    * @throws FileDoesNotExistException if the file doesn't exist
    */
   public void resetFile(long fileId) throws FileDoesNotExistException {
-    // TODO check the file is not persisted
+    // TODO(yupeng) check the file is not persisted
     synchronized (mInodeTree) {
       // free the file first
       free(fileId, false);
       InodeFile inodeFile = (InodeFile) mInodeTree.getInodeById(fileId);
-      inodeFile.reinit();
+      inodeFile.reset();
     }
   }
 
