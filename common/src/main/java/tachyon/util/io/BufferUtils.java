b85af61b4e54ddc408314385a400d369b53e039c
Andrew Audibert <aaudibert10@gmail.com>
2015-10-28 00:41:58 -0700

    Ensure that streams are closed

--- common/src/main/java/tachyon/util/io/BufferUtils.java
+++ common/src/main/java/tachyon/util/io/BufferUtils.java
@@ -283,8 +283,11 @@ public final class BufferUtils {
    */
   public static void writeBufferToFile(String path, byte[] buffer) throws IOException {
     FileOutputStream os = new FileOutputStream(path);
-    os.write(buffer);
-    os.close();
+    try {
+      os.write(buffer);
+    } finally {
+      os.close();
+    }
   }
 
   /**
