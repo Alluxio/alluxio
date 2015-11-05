9f5c3fe146567475ced6881bf5d0c1269040b7e0
cc <myairia@gmail.com>
2015-11-05 09:48:44 +0800

    Always search for a local block

--- clients/unshaded/src/main/java/tachyon/client/block/TachyonBlockStore.java
+++ clients/unshaded/src/main/java/tachyon/client/block/TachyonBlockStore.java
@@ -98,15 +98,15 @@ public final class TachyonBlockStore implements Closeable {
       // But when there is no local worker or there are no local blocks, we prefer the first
       // location in blockInfo.locations that is nearest to memory tier.
       // Assuming if there is no local worker, there are no local blocks in blockInfo.locations.
-      if (mContext.hasLocalWorker()) {
-        String localHostName = NetworkAddressUtils.getLocalHostName(ClientContext.getConf());
-        for (BlockLocation location : blockInfo.locations) {
-          NetAddress workerNetAddress = location.getWorkerAddress();
-          if (workerNetAddress.getHost().equals(localHostName)) {
-            // There is a local worker and the block is local.
-            return new LocalBlockInStream(blockId, blockInfo.getLength(),
-                new InetSocketAddress(workerNetAddress.getHost(), workerNetAddress.getDataPort()));
-          }
+      // TODO(cc): Check mContext.hasLocalWorker before finding for a local block when the TODO
+      // for hasLocalWorker is fixed.
+      String localHostName = NetworkAddressUtils.getLocalHostName(ClientContext.getConf());
+      for (BlockLocation location : blockInfo.locations) {
+        NetAddress workerNetAddress = location.getWorkerAddress();
+        if (workerNetAddress.getHost().equals(localHostName)) {
+          // There is a local worker and the block is local.
+          return new LocalBlockInStream(blockId, blockInfo.getLength(),
+              new InetSocketAddress(workerNetAddress.getHost(), workerNetAddress.getDataPort()));
         }
       }
       // No local worker/block, get the first location since it's nearest to memory tier.
