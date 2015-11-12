afdd55ab945e644b417db43f5bb0934905946988
Bin Fan <fanbin103@gmail.com>
2015-11-12 12:48:29 -0800

    Fix compile

--- shell/src/main/java/tachyon/shell/TfsShell.java
+++ shell/src/main/java/tachyon/shell/TfsShell.java
@@ -156,9 +156,8 @@ public class TfsShell implements Closeable {
       } else {
         if (fInfo.getInMemoryPercentage() == 100) {
           // The file has already been fully loaded into Tachyon memory.
-          return 0;
+          return;
         }
-
         Closer closer = Closer.create();
         try {
           InStreamOptions op = new InStreamOptions.Builder(mTachyonConf)
