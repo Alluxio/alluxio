e6af4bc49859ed0ff93bf9a825f3069bebb6dc8b
Yupeng Fu <yupeng.fu@gmail.com>
2015-11-16 22:53:02 -0800

    Added test for async persistence

--- integration-tests/src/test/java/tachyon/master/lineage/LineageMastertIntegrationsTest.java
+++ integration-tests/src/test/java/tachyon/master/lineage/LineageMastertIntegrationsTest.java
@@ -16,38 +16,75 @@
 package tachyon.master.lineage;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
 import tachyon.Constants;
+import tachyon.IntegrationTestConstants;
 import tachyon.LocalTachyonClusterResource;
+import tachyon.TachyonURI;
+import tachyon.client.TachyonStorageType;
+import tachyon.client.UnderStorageType;
+import tachyon.client.file.FileOutStream;
+import tachyon.client.file.options.OutStreamOptions;
 import tachyon.client.lineage.LineageMasterClient;
+import tachyon.client.lineage.TachyonLineageFileSystem;
+import tachyon.conf.TachyonConf;
+import tachyon.heartbeat.HeartbeatContext;
+import tachyon.heartbeat.HeartbeatScheduler;
 import tachyon.job.CommandLineJob;
 import tachyon.job.JobConf;
 import tachyon.master.lineage.meta.LineageFileState;
 import tachyon.thrift.LineageInfo;
 
 public final class LineageMastertIntegrationsTest {
+  private static final int BLOCK_SIZE_BYTES = 128;
+  private static final long WORKER_CAPACITY_BYTES = Constants.GB;
+  private static final int QUOTA_UNIT_BYTES = 128;
+  private static final int BUFFER_BYTES = 100;
+
   @Rule
   public LocalTachyonClusterResource mLocalTachyonClusterResource =
-      new LocalTachyonClusterResource(1000, 1000, Constants.GB);
+      new LocalTachyonClusterResource(WORKER_CAPACITY_BYTES, QUOTA_UNIT_BYTES, BLOCK_SIZE_BYTES,
+          Constants.USER_FILE_BUFFER_BYTES, String.valueOf(BUFFER_BYTES),
+          Constants.WORKER_DATA_SERVER, IntegrationTestConstants.NETTY_DATA_SERVER);
 
   private static final String OUT_FILE = "/test";
+  private TachyonLineageFileSystem mTFS;
+  private TachyonConf mTestConf;
+  private CommandLineJob mJob;
+
+
+  @BeforeClass
+  public static void beforeClass() {
+    HeartbeatContext.setTimerClass(HeartbeatContext.MASTER_CHECKPOINT_SCHEDULING,
+        HeartbeatContext.SCHEDULED_TIMER_CLASS);
+    HeartbeatContext.setTimerClass(HeartbeatContext.WORKER_LINEAGE_SYNC,
+        HeartbeatContext.SCHEDULED_TIMER_CLASS);
+  }
+
+  @Before
+  public void before() throws Exception {
+    mJob = new CommandLineJob("test", new JobConf("output"));
+    mTFS = (TachyonLineageFileSystem) mLocalTachyonClusterResource.get().getClient();
+    mTestConf = mLocalTachyonClusterResource.get().getMasterTachyonConf();
+  }
 
   @Test
   @LocalTachyonClusterResource.Config(tachyonConfParams = {Constants.USER_LINEAGE_ENABLED, "true"})
   public void lineageCreationTest() throws Exception {
-    LineageMasterClient lineageMasterClient =
-        new LineageMasterClient(mLocalTachyonClusterResource.get().getMaster().getAddress(),
-            mLocalTachyonClusterResource.get().getMasterTachyonConf());
+    LineageMasterClient lineageMasterClient = getLineageMasterClient();
+
     try {
-      CommandLineJob job = new CommandLineJob("test", new JobConf("output"));
       lineageMasterClient.createLineage(Lists.<String>newArrayList(), Lists.newArrayList(OUT_FILE),
-          job);
+          mJob);
 
       List<LineageInfo> infos = lineageMasterClient.getLineageInfoList();
       Assert.assertEquals(1, infos.size());
@@ -58,4 +95,63 @@ public final class LineageMastertIntegrationsTest {
     }
   }
 
+  @Test
+  @LocalTachyonClusterResource.Config(tachyonConfParams = {Constants.USER_LINEAGE_ENABLED, "true"})
+  public void lineageCompleteAndAsyncPersistTest() throws Exception {
+    LineageMasterClient lineageMasterClient = getLineageMasterClient();
+
+    Assert.assertTrue(HeartbeatScheduler.await(HeartbeatContext.MASTER_CHECKPOINT_SCHEDULING, 5,
+        TimeUnit.SECONDS));
+    Assert.assertTrue(
+        HeartbeatScheduler.await(HeartbeatContext.WORKER_LINEAGE_SYNC, 5, TimeUnit.SECONDS));
+
+    try {
+      lineageMasterClient.createLineage(Lists.<String>newArrayList(), Lists.newArrayList(OUT_FILE),
+          mJob);
+
+      OutStreamOptions options =
+          new OutStreamOptions.Builder(mTestConf).setTachyonStorageType(TachyonStorageType.STORE)
+              .setUnderStorageType(UnderStorageType.NO_PERSIST).setBlockSizeBytes(BLOCK_SIZE_BYTES)
+              .build();
+      FileOutStream outputStream = mTFS.getOutStream(new TachyonURI(OUT_FILE), options);
+      outputStream.write(1);
+      outputStream.close();
+
+      List<LineageInfo> infos = lineageMasterClient.getLineageInfoList();
+      Assert.assertEquals(LineageFileState.COMPLETED.toString(),
+          infos.get(0).outputFiles.get(0).state);
+
+      // Execute the checkpoint scheduler for async checkpoint
+      HeartbeatScheduler.schedule(HeartbeatContext.MASTER_CHECKPOINT_SCHEDULING);
+      HeartbeatScheduler.schedule(HeartbeatContext.WORKER_LINEAGE_SYNC);
+      Assert.assertTrue(HeartbeatScheduler.await(HeartbeatContext.MASTER_CHECKPOINT_SCHEDULING, 5,
+          TimeUnit.SECONDS));
+      Assert.assertTrue(
+          HeartbeatScheduler.await(HeartbeatContext.WORKER_LINEAGE_SYNC, 5, TimeUnit.SECONDS));
+
+      infos = lineageMasterClient.getLineageInfoList();
+      Assert.assertEquals(LineageFileState.PERSISENCE_REQUESTED.toString(),
+          infos.get(0).outputFiles.get(0).state);
+
+      // sleep and wait for worker to persist the file
+      Thread.sleep(5);
+
+      // worker notifies the master
+      HeartbeatScheduler.schedule(HeartbeatContext.WORKER_LINEAGE_SYNC);
+      Assert.assertTrue(
+          HeartbeatScheduler.await(HeartbeatContext.WORKER_LINEAGE_SYNC, 5000, TimeUnit.SECONDS));
+
+      infos = lineageMasterClient.getLineageInfoList();
+      Assert.assertEquals(LineageFileState.PERSISTED.toString(),
+          infos.get(0).outputFiles.get(0).state);
+
+    } finally {
+      lineageMasterClient.close();
+    }
+  }
+
+  private LineageMasterClient getLineageMasterClient() {
+    return new LineageMasterClient(mLocalTachyonClusterResource.get().getMaster().getAddress(),
+        mTestConf);
+  }
 }
