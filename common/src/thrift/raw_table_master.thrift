2679cb87349a90025cd7885d5fb25adb8fd91bf3
Shaoshan Liu <liushaoshan@baidu.com>
2015-11-17 23:45:35 -0800

    use javadoc in raw_table_master.thrift

--- common/src/thrift/raw_table_master.thrift
+++ common/src/thrift/raw_table_master.thrift
@@ -17,7 +17,14 @@ struct RawTableInfo {
 service RawTableMasterService extends common.TachyonService {
 
   /**
-   * Creates a raw table and returns the raw table id
+   * Creates a raw table.
+   * @param rpcOptions
+   * @param path
+   * @param columns
+   * @param metadata
+   * @return the raw table id
+   * @throws TachyonTException
+   * @throws ThriftIOException
    */
   i64 createRawTable( /** the RPC options */ 1: common.RpcOptions rpcOptions,
       /** the path of the raw table */ 2: string path,
@@ -27,24 +34,36 @@ service RawTableMasterService extends common.TachyonService {
 
   /**
    * Returns raw table information for the given id.
+   * @param id
+   * @return raw table information for the given id
+   * @throws TachyonTException
    */
   RawTableInfo getClientRawTableInfoById( /** the id of the table */ 1: i64 id)
       throws (1: exception.TachyonTException e)
 
   /**
    * Returns raw table information for the given path.
+   * @param path
+   * @return raw table information for the given path
+   * @throws TachyonTException
    */
   RawTableInfo getClientRawTableInfoByPath( /** the path of the table */ 1: string path)
       throws (1: exception.TachyonTException e)
 
   /**
    * Returns raw table id for the given path.
+   * @param path
+   * @return raw table id for the given path
+   * @throws TachyonTException
    */
   i64 getRawTableId( /** the path of the table */ 1: string path)
       throws (1: exception.TachyonTException e)
 
   /**
    * Updates raw table metadata.
+   * @param tableId
+   * @param metadata
+   * @throws TachyonTException
    */
   void updateRawTableMetadata( /** the id of the table */ 1: i64 tableId,
       /** the metadata for the table */ 2: binary metadata)
