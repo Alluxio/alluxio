ca12303d48fe034123f0ff990f359b6d5c284bd9
Shaoshan Liu <liushaoshan@baidu.com>
2015-11-20 21:24:20 -0800

    new way to add javadoc for thrift file raw_table_master.thrift

--- common/src/thrift/raw_table_master.thrift
+++ common/src/thrift/raw_table_master.thrift
@@ -17,52 +17,36 @@ struct RawTableInfo {
 service RawTableMasterService extends common.TachyonService {
 
   /**
-   * Creates a raw table.
-   * @param rpcOptions
-   * @param path
-   * @param columns
-   * @param metadata
-   * @return the raw table id
-   * @throws TachyonTException
-   * @throws ThriftIOException
+   * Creates a raw table and returns the raw table id
    */
-  i64 createRawTable(1: common.RpcOptions rpcOptions, 2: string path, 3: i32 columns, 
-      4: binary metadata)
-    throws (1: exception.TachyonTException e, 2: exception.ThriftIOException ioe)
+  i64 createRawTable( /** the RPC options */ 1: common.RpcOptions rpcOptions,
+      /** the path of the raw table */ 2: string path,
+      /** the number of columns */ 3: i32 columns,
+      /** the metadata for the table */ 4: binary metadata)
+      throws (1: exception.TachyonTException e, 2: exception.ThriftIOException ioe)
 
   /**
    * Returns raw table information for the given id.
-   * @param id
-   * @return raw table information for the given id
-   * @throws TachyonTException
    */
-  RawTableInfo getClientRawTableInfoById(1: i64 id)
-    throws (1: exception.TachyonTException e)
+  RawTableInfo getClientRawTableInfoById( /** the id of the table */ 1: i64 id)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Returns raw table information for the given path.
-   * @param path
-   * @return raw table information for the given path
-   * @throws TachyonTException
    */
-  RawTableInfo getClientRawTableInfoByPath(1: string path)
-    throws (1: exception.TachyonTException e)
+  RawTableInfo getClientRawTableInfoByPath( /** the path of the table */ 1: string path)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Returns raw table id for the given path.
-   * @param path
-   * @return raw table id for the given path
-   * @throws TachyonTException
    */
-  i64 getRawTableId(1: string path)
-    throws (1: exception.TachyonTException e)
+  i64 getRawTableId( /** the path of the table */ 1: string path)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Updates raw table metadata.
-   * @param tableId
-   * @param metadata
-   * @throws TachyonTException
    */
-  void updateRawTableMetadata(1: i64 tableId, 2: binary metadata)
-    throws (1: exception.TachyonTException e)
+  void updateRawTableMetadata( /** the id of the table */ 1: i64 tableId,
+      /** the metadata for the table */ 2: binary metadata)
+      throws (1: exception.TachyonTException e)
 }
