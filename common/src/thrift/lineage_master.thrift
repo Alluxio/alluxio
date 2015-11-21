8eb068c5978dd811e6392c33d206225d3edd0866
Shaoshan Liu <liushaoshan@baidu.com>
2015-11-20 21:20:42 -0800

    new way to add javadoc for thrift file lineage_master.thrift

--- common/src/thrift/lineage_master.thrift
+++ common/src/thrift/lineage_master.thrift
@@ -44,66 +44,50 @@ service LineageMasterService extends common.TachyonService {
 
   /**
    * Marks a file as completed and initiates asynchronous persistence (if applicable).
-   * @param fileId
-   * @throws TachyonTException
    */
-  void asyncCompleteFile(1: i64 fileId)
+  void asyncCompleteFile( /** the id of the file */ 1: i64 fileId)
     throws (1: exception.TachyonTException e)
 
   /**
-   * Creates a lineage.
-   * @param inputFiles
-   * @param outputFiles
-   * @param job
-   * @return lineageId
-   * @throws TachyonTException
-   * @throws ThriftIOException
+   * Creates a lineage and returns the lineage id
    */
-  i64 createLineage(1: list<string> inputFiles, 2: list<string> outputFiles, 3: CommandLineJobInfo job)
-    throws (1: exception.TachyonTException e, 2: exception.ThriftIOException ioe)
+  i64 createLineage( /** the list of input files */ 1: list<string> inputFiles,
+      /** the list of output files */ 2: list<string> outputFiles,
+      /** the command line job info */ 3: CommandLineJobInfo job)
+      throws (1: exception.TachyonTException e, 2: exception.ThriftIOException ioe)
 
   /**
-   * Deletes a lineage.
-   * @param linearId
-   * @param cascade
-   * @return whether deletion succeeded
-   * @throws TachyonTException
+   * Deletes a lineage and returns whether the deletion succeeded.
    */
-  bool deleteLineage(1: i64 lineageId, 2: bool cascade)
-    throws (1: exception.TachyonTException e)
+  bool deleteLineage( /** the lineage id */ 1: i64 lineageId,
+      /** whether to delete the lineage in cascade */ 2: bool cascade)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Returns a list of existing lineages.
-   * @return a list of existing lineages
    */
   list<LineageInfo> getLineageInfoList()
 
   /**
-   * Reinitializes a file.
-   * @param path
-   * @param blockSizeBytes
-   * @param ttl
-   * @return the id of the reinitialized file when the file is lost or not completed, -1 otherwise
-   * @throws TachyonTException
+   * Reinitializes a file. Returns the id of the reinitialized file when the
+   * file is lost or not completed, -1 otherwise
    */
-  i64 reinitializeFile(1: string path, 2: i64 blockSizeBytes, 3: i64 ttl)
-    throws (1: exception.TachyonTException e)
+  i64 reinitializeFile( /** the path of the file */ 1: string path,
+      /** block size in bytes */ 2: i64 blockSizeBytes,
+      /** time to live */ 3: i64 ttl)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Reports file as lost.
-   * @param path
-   * @throws TachyonTException
    */
-  void reportLostFile(1: string path)
+  void reportLostFile( /** the path of the file */ 1: string path)
     throws (1: exception.TachyonTException e)
 
   /**
-   * Periodic lineage worker heartbeat.
-   * @param workerId
-   * @param persistedFiles
-   * @return the command for checkpointing the blocks of a file
-   * @throws TachyonTException
+   * Periodic lineage worker heartbeat. Returns the command for checkpointing
+   * the blocks of a file
    */
-  LineageCommand workerLineageHeartbeat(1: i64 workerId, 2: list<i64> persistedFiles)
-    throws (1: exception.TachyonTException e)
+  LineageCommand workerLineageHeartbeat( /** the id of the worker */ 1: i64 workerId,
+      /** the list of persisted files */ 2: list<i64> persistedFiles)
+      throws (1: exception.TachyonTException e)
 }
