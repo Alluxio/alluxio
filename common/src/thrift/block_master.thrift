de36de5abb972e2342f9f8a9141a11a27002883b
Shaoshan Liu <liushaoshan@baidu.com>
2015-11-20 17:44:40 -0800

    new way to add javadoc for thrift file block_master.thrift

--- common/src/thrift/block_master.thrift
+++ common/src/thrift/block_master.thrift
@@ -17,72 +17,54 @@ service BlockMasterService extends common.TachyonService {
 
   /**
    * Returns the block information for the given block id.
-   * @param blockId
-   * @return the block information for the given block id
-   * @throws TachyonTException
    */
-  common.BlockInfo getBlockInfo(1: i64 blockId) throws (1: exception.TachyonTException e)
+  common.BlockInfo getBlockInfo( /** the id of the block */  1: i64 blockId)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Returns the capacity (in bytes).
-   * @return the block capacity in number of bytes
    */
   i64 getCapacityBytes()
 
   /**
    * Returns the used storage (in bytes).
-   * @return the block usage in number of bytes
    */
   i64 getUsedBytes()
 
   /**
    * Returns a list of workers information.
-   * @return a list of workers information
    */
   list<WorkerInfo> getWorkerInfoList()
 
   /**
    * Marks the given block as committed.
-   * @param workerId
-   * @param usedBytesOnTier
-   * @param tierAlias
-   * @param blockId
-   * @param length
    */
-  void workerCommitBlock(1: i64 workerId, 2: i64 usedBytesOnTier, 3: string tierAlias, 4: i64 blockId,
-      5: i64 length)
+  void workerCommitBlock( /** the id of the worker */  1: i64 workerId,
+      /** the space used in bytes on the target tier */ 2: i64 usedBytesOnTier,
+      /** the alias of the target tier */ 3: string tierAlias,
+      /** the id of the block being committed */ 4: i64 blockId,
+      /** the id of the block being committed */ 5: i64 length)
 
   /**
    * Returns a worker id for the given network address.
-   * @param workerNetAddress
-   * @return a worker id for the given network address
    */
-  i64 workerGetWorkerId(1: common.NetAddress workerNetAddress)
+  i64 workerGetWorkerId( /** the worker network address */ 1: common.NetAddress workerNetAddress)
 
   /**
-   * Periodic worker heartbeat.
-   * @param workerId
-   * @param usedBytesOnTiers
-   * @param removedBlockIds
-   * @param addedBlocksOnTiers
-   * @return  an optional command for the worker to execute
+   * Periodic worker heartbeat returns an optional command for the worker to execute
    */
-  common.Command workerHeartbeat(1: i64 workerId, 2: map<string, i64> usedBytesOnTiers,
-      3: list<i
-   * @param totalBytesOnTiers
-   * @param usedBytesOnTiers
-   * @param currentBlocksOnTiers64> removedBlockIds, 4: map<string, list<i64>> addedBlocksOnTiers)
-
+  common.Command workerHeartbeat( /** the id of the worker */ 1: i64 workerId,
+      /** the map of space used in bytes on all tiers */ 2: map<string, i64> usedBytesOnTiers,
+      /** the list of removed block Ids */ 3: list<i64> removedBlockIds,
+      /** the map of added blocks on all tiers */ 4: map<string, list<i64>> addedBlocksOnTiers)
   /**
    * Registers a worker.
-   * @param workerId
-   * @param storageTiers
-   * @param totalBytesOnTiers
-   * @param usedBytesOnTiers
-   * @param currentBlocksOnTiers
-   * @throws TachyonTException
    */
-  void workerRegister(1: i64 workerId, 2: list<string> storageid of the workertesOnTiers,
-      4: map<string, i64> usedBytesOnTiers, 5: map<string, list<i64>> currentBlocksOnTiers)
+
+  void workerRegister( /** the id of the worker */  1: i64 workerId,
+      /** the list of storage tiers */  2: list<string> storageTiers,
+      /** the map of total bytes on each tier */  3: map<string, i64> totalBytesOnTiers,
+      /** the map of used bytes on each tier */  4: map<string, i64> usedBytesOnTiers,
+      /** the map of list of blocks on each tier */  5: map<string, list<i64>> currentBlocksOnTiers)
     throws (1: exception.TachyonTException e)
 }
