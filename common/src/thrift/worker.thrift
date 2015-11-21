9a39b16696543e59b7af5eb01a51477aa6c21f04
Shaoshan Liu <liushaoshan@baidu.com>
2015-11-20 17:55:36 -0800

    new way to add javadoc for thrift file worker.thrift

--- common/src/thrift/worker.thrift
+++ common/src/thrift/worker.thrift
@@ -6,26 +6,20 @@ include "exception.thrift"
 service WorkerService extends common.TachyonService {
 
   /**
-   * @param blockId
+   * access a block given the block id
    */
-  void accessBlock(1: i64 blockId)
+  void accessBlock( /** the id of the block being accessed */ 1: i64 blockId)
 
   /**
-   * @param fileId
-   * @return whether the checkpoint operation succeeded
-   * @throws TachyonTException
+   * asynchronously checkpoint a file: return whether the checkpoint operation succeeded
    */
-  bool asyncCheckpoint(1: i64 fileId)
-    throws (1: exception.TachyonTException e)
+  bool asyncCheckpoint( /** the id of the file being accessed */ 1: i64 fileId)
+      throws (1: exception.TachyonTException e)
 
   /**
    * Used to cache a block into Tachyon space, worker will move the temporary block file from session
    * folder to data folder, and update the space usage information related. then update the block
    * information to master.
-   * @param sessionId
-   * @param blockId
-   * @throws TachyonTException
-   * @throws ThriftIOException
    */
   void cacheBlock( /** the id of the current session */ 1: i64 sessionId,
       /** the id of the block being accessed */ 2: i64 blockId)
@@ -34,10 +28,6 @@ service WorkerService extends common.TachyonService {
   /**
    * Used to cancel a block which is being written. worker will delete the temporary block file and
    * the location and space information related, then reclaim space allocated to the block.
-   * @param sessionId
-   * @param blockId
-   * @throws TachyonTException
-   * @throws ThriftIOException
    */
   void cancelBlock( /** the id of the current session */ 1: i64 sessionId,
       /** the id of the block being accessed */ 2: i64 blockId)
@@ -47,10 +37,6 @@ service WorkerService extends common.TachyonService {
    * Lock the file in Tachyon's space while the session is reading it, and the path of the block file
    * locked will be returned, if the block file is not found, FileDoesNotExistException will be
    * thrown.
-   * @param blockId
-   * @param sessionId
-   * @return the path of the block file locked
-   * @throws TachyonTException
    */
   string lockBlock( /** the id of the block being accessed */ 1: i64 blockId,
       /** the id of the current session */ 2: i64 sessionId)
@@ -60,10 +46,6 @@ service WorkerService extends common.TachyonService {
    * Used to promote block on under storage layer to top storage layer when there are more than one
    * storage layers in Tachyon's space. return true if the block is successfully promoted, false
    * otherwise.
-   * @param blockId
-   * @return whether the promotBlock operation succeeded
-   * @throws TachyonTException
-   * @throws ThriftIOException
    */
   bool promoteBlock( /** the id of the block being accessed */ 1: i64 blockId)
     throws (1: exception.TachyonTException e, 2: exception.ThriftIOException ioe)
@@ -74,12 +56,6 @@ service WorkerService extends common.TachyonService {
    * temporary file path of the block file will be returned. if there is no enough space on Tachyon
    * storage OutOfSpaceException will be thrown, if the file is already being written by the session,
    * FileAlreadyExistsException will be thrown.
-   * @param sessionId
-   * @param blockId
-   * @param initialBytes
-   * @return the temporary file path of the block file
-   * @throws TachyonTException
-   * @throws ThriftIOException
    */
   string requestBlockLocation( /** the id of the current session */ 1: i64 sessionId,
       /** the id of the block being accessed */ 2: i64 blockId,
@@ -90,11 +66,6 @@ service WorkerService extends common.TachyonService {
    * Used to request space for some block file. return true if the worker successfully allocates
    * space for the block on block’s location, false if there is no enough space, if there is no
    * information of the block on worker, FileDoesNotExistException will be thrown.
-   * @param sessionId
-   * @param blockId
-   * @param requestBytes
-   * @return whether the requestSpace operation succeeded
-   * @throws TachyonTException
    */
   bool requestSpace( /** the id of the current session */ 1: i64 sessionId,
       /** the id of the block being accessed */ 2: i64 blockId,
@@ -104,8 +75,6 @@ service WorkerService extends common.TachyonService {
   /**
    * Local session send heartbeat to local worker to keep its temporary folder. It also sends client
    * metrics to the worker.
-   * @param sessionId
-   * @param metrics
    */
   void sessionHeartbeat( /** the id of the current session */ 1: i64 sessionId,
       /** the client metrics */ 2: list<i64> metrics)
@@ -114,9 +83,6 @@ service WorkerService extends common.TachyonService {
    * Used to unlock a block after the block is accessed, if the block is to be removed, delete the
    * block file. return true if successfully unlock the block, return false if the block is not
    * found or failed to delete the block.
-   * @param blockId
-   * @param sessionId
-   * @return whether the unlockBlock operation succeeded
    */
   bool unlockBlock( /** the id of the block being accessed */ 1: i64 blockId,
       /** the id of the current session */ 2: i64 sessionId)
