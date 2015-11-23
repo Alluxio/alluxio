91d0e2eb66b9d17de66302bc917d8e08f4d3f288
Andrew Audibert <aaudibert10@gmail.com>
2015-11-23 11:35:01 -0800

    Simplify code

--- servers/src/main/java/tachyon/worker/block/evictor/LRFUEvictor.java
+++ servers/src/main/java/tachyon/worker/block/evictor/LRFUEvictor.java
@@ -147,14 +147,7 @@ public final class LRFUEvictor extends EvictorBase {
     Collections.sort(sortedCRF, new Comparator<Map.Entry<Long, Double>>() {
       @Override
       public int compare(Entry<Long, Double> o1, Entry<Long, Double> o2) {
-        double res = o1.getValue() - o2.getValue();
-        if (Double.compare(res, 0) < 0) {
-          return -1;
-        } else if (Double.compare(res, 0) > 0) {
-          return 1;
-        } else {
-          return 0;
-        }
+        return Double.compare(o1.getValue(), o2.getValue());
       }
     });
     return sortedCRF;
