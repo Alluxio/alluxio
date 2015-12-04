f5725b4443f9ebf783265b3253f7320d444cee95
Calvin Jia <jia.calvin@gmail.com>
2015-12-04 15:01:00 -0800

    Add javadoc for tests.

--- clients/unshaded/src/test/java/tachyon/client/block/BlockStoreContextTest.java
+++ clients/unshaded/src/test/java/tachyon/client/block/BlockStoreContextTest.java
@@ -29,8 +29,14 @@ import tachyon.client.ClientContext;
  * Tests {@link BlockStoreContext}.
  */
 public final class BlockStoreContextTest {
-  // If the test takes longer than 10 seconds, most likely a deadlock occurred preventing the
-  // release of the master clients.
+  /**
+   * This test ensures acquiring all the available BlockStore master clients blocks further
+   * requests for clients. It also ensures clients are available for reuse after they are released
+   * by the previous owners. If the test takes longer than 10 seconds, a deadlock most likely
+   * occurred preventing the release of the master clients.
+   *
+   * @throws Exception if an unexpected error occurs during the test
+   */
   @Test(timeout = 10000)
   public void acquireAtMaxLimitTest() throws Exception {
     final List<BlockMasterClient> clients = Lists.newArrayList();
