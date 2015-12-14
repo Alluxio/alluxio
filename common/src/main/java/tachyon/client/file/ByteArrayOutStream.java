a63477dcae02fad7cb7f5cbc902c0d58dfb576a6
Bin Fan <fanbin103@gmail.com>
2015-12-14 11:05:53 -0800

    Move MockOutputStream up to common

---
+++ common/src/main/java/tachyon/client/file/ByteArrayOutStream.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the University of California, Berkeley under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package tachyon.client.file;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * A mock implementation of {@link AbstractOutStream} backed by a byte stream. Supposed to be only
+ * used for tests.
+ *
+ * TODO(binfan): move this to test jar if it turns out only used in test.
+ */
+public final class ByteArrayOutStream extends AbstractOutStream {
+  private ByteArrayOutputStream mOut;
+
+  public ByteArrayOutStream() {
+    /** set an initial size of underlying ByteArray, which will grow automatically */
+    mOut = new ByteArrayOutputStream(1000);
+  }
+
+  public void write(int b) {
+    mOut.write(b);
+    mCount ++;
+  }
+
+  /**
+   * @return the number of bytes written to this output stream
+   */
+  public int getCount() {
+    return mCount;
+  }
+
+  public byte[] toByteArray() {
+    return mOut.toByteArray();
+  }
+}
--- common/src/test/java/tachyon/worker/keyvalue/PayloadReaderWriterTest.java
+++ common/src/test/java/tachyon/worker/keyvalue/PayloadReaderWriterTest.java
@@ -2,6 +2,7 @@ package tachyon.worker.keyvalue;
 
 import org.junit.Assert;
 import org.junit.Test;
+import tachyon.client.file.ByteArrayOutStream;
 
 /**
  * Unit test of {@link PayloadWriter}.
@@ -13,7 +14,7 @@ public class PayloadReaderWriterTest {
   private static final byte[] VALUE1 = "value1".getBytes();
   private static final byte[] VALUE2 = "value2_bar".getBytes();
 
-  private Utils.MockOutStream mTestOutStream = new Utils.MockOutStream();
+  private ByteArrayOutStream mTestOutStream = new ByteArrayOutStream();
   private PayloadWriter mTestWriter = new PayloadWriter(mTestOutStream);
   private PayloadReader mTestReader;
 
