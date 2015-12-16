5813357272202a296141ca7be6dca04ff32d7db8
Bin Fan <fanbin103@gmail.com>
2015-12-15 18:51:59 -0800

    Add KeyValueWorkerService and initial implementation of KeyValueWorker

---
+++ common/src/thrift/keyvalue_worker.thrift
@@ -0,0 +1,13 @@
+namespace java tachyon.thrift
+
+include "common.thrift"
+include "exception.thrift"
+
+service KeyValueWorkerService extends common.TachyonService {
+  /**
+   * Accesses a block given the block id.
+   */
+  binary get( /** the id of the block being accessed */ 1: i64 blockId,
+      /** binary of the key */ 2: binary key)
+    throws (1: exception.TachyonTException e, 2: exception.ThriftIOException ioe)
+}
