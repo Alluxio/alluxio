04242c2caff406d62d3627490d95bc3c0214d047
Gene Pang <gene.pang@gmail.com>
2015-12-16 07:11:16 -0800

    Remove unnecessary period

--- minicluster/src/main/java/tachyon/master/LocalTachyonMaster.java
+++ minicluster/src/main/java/tachyon/master/LocalTachyonMaster.java
@@ -142,7 +142,7 @@ public final class LocalTachyonMaster {
   /**
    * Kill the master thread, by calling {@link Thread#interrupt()}.
    *
-   * @throws Exception if master thread cannot be interrupted.
+   * @throws Exception if master thread cannot be interrupted
    */
   public void kill() throws Exception {
     mMasterThread.interrupt();
