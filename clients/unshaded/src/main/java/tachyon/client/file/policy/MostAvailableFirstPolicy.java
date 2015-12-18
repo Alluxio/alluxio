a80b6cb7eb33bd33255bc4770930c364dc861c69
Yupeng Fu <yupeng.fu@gmail.com>
2015-12-17 17:09:38 -0800

    Added a most available first policy

---
+++ clients/unshaded/src/main/java/tachyon/client/file/policy/MostAvailableFirstPolicy.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the University of California, Berkeley under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package tachyon.client.file.policy;
+
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
+import tachyon.client.block.BlockWorkerInfo;
+import tachyon.client.file.options.OutStreamOptions;
+
+/**
+ * A policy that returns the worker with the most available bytes. The policy returns null if no
+ * worker is qualified.
+ */
+public final class MostAvailableFirstPolicy implements FileWriteLocationPolicy {
+  private final OutStreamOptions mOptions;
+
+  /**
+   * Creates the policy.
+   */
+  public MostAvailableFirstPolicy(List<BlockWorkerInfo> workerInfoList, OutStreamOptions options) {
+    mOptions = Preconditions.checkNotNull(options);
+  }
+
+  @Override
+  public String getWorkerForNextBlock(List<BlockWorkerInfo> workerInfoList) {
+    List<BlockWorkerInfo> inputList = Lists.newArrayList(workerInfoList);
+    long mostAvailableBytes = -1;
+    String result = null;
+    for (BlockWorkerInfo workerInfo : inputList) {
+      if (workerInfo.getCapacityBytes() - workerInfo.getUsedBytes() > mostAvailableBytes) {
+        mostAvailableBytes = workerInfo.getCapacityBytes() - workerInfo.getUsedBytes();
+        result = workerInfo.getHost();
+      }
+    }
+    if (mostAvailableBytes < mOptions.getBlockSizeBytes()) {
+      // no worker has enough space
+      return null;
+    }
+    return result;
+  }
+}
