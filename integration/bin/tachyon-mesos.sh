8bbf1a7f4a19c22445dfe4e1a0f6a0018ee0f43c
Adrian Bridgett <adrian@smop.co.uk>
2015-12-18 14:59:21 +0000

    optionally wait for process to finish/die this is useful when running under cluster managers etc (TACHYON-1346)

--- integration/bin/tachyon-mesos.sh
+++ integration/bin/tachyon-mesos.sh
@@ -7,7 +7,37 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 source "${SCRIPT_DIR}/common.sh"
 TACHYON_FRAMEWORK_JAVA_OPTS="${TACHYON_FRAMEWORK_JAVA_OPTS:-${TACHYON_JAVA_OPTS}}"
 MESOS_LIBRARY_PATH="${MESOS_LIBRARY_PATH:-/usr/local/lib}"
+
+Usage="Usage: tachyon-mesos.sh [-hw] MESOS_MASTER_ADDRESS
++MESOS_MASTER_ADDRESS is of the form 'mesos.example.com:5050'
++
++-w  Wait for process to finish before returning
++
++-h  display this help."
+
+while getopts "hw" o; do
+  case "${o}" in
+    h)
+      echo -e "$Usage"
+      exit 0
+      ;;
+    w)
+      wait="true"
+      ;;
+    *)
+      echo -e "$Usage"
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
 MESOS_MASTER_ADDRESS="$1"
+if [ ! "$MESOS_MASTER_ADDRESS" ]; then
+  echo -e "$Usage"
+  exit 1
+fi
 
 mkdir -p "${TACHYON_LOGS_DIR}"
 
@@ -17,3 +47,7 @@ mkdir -p "${TACHYON_LOGS_DIR}"
   -Dtachyon.home="${TACHYON_HOME}" \
   -Dtachyon.logs.dir="${TACHYON_LOGS_DIR}" \
   tachyon.mesos.TachyonFramework "${MESOS_MASTER_ADDRESS}" > "${TACHYON_LOGS_DIR}"/framework.out 2>&1 &
+
+if [[ "${wait}" ]]; then
+  wait
+fi
