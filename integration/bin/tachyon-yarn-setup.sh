ec4152ea5698eb9ce01dea9c89888200db00da51
Andrew Audibert <aaudibert10@gmail.com>
2015-12-20 05:10:36 -0800

    Improve tachyon-yarn-setup.sh

--- integration/bin/tachyon-yarn-setup.sh
+++ integration/bin/tachyon-yarn-setup.sh
@@ -3,14 +3,17 @@
 # prepare Tachyon dir structure for a YARN container to launch master or worker
 tar zxf tachyon.tar.gz
 
+CONTAINER_TYPE=$1
+shift
+
 # launch master or worker on this container
-if [ $1 = 'tachyon-master' ]; then
-  ./integration/bin/tachyon-master-yarn.sh
+if [ $CONTAINER_TYPE = 'tachyon-master' ]; then
+  ./integration/bin/tachyon-master-yarn.sh $@
+elif [ $CONTAINER_TYPE = 'tachyon-worker' ]; then
+  ./integration/bin/tachyon-worker-yarn.sh $@
+elif [ $CONTAINER_TYPE = 'application-master' ]; then
+  ./integration/bin/tachyon-application-master.sh $@
 else
-  if  [ $1 = 'tachyon-worker' ]; then
-    ./integration/bin/tachyon-worker-yarn.sh
-  else
-    shift
-    ./integration/bin/tachyon-application-master.sh $@
-  fi
+  echo "Unrecognized container type: $CONTAINER_TYPE"
+  exit 1
 fi
