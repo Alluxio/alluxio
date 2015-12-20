f44598b1cca0d4f109d84c87168efdca4d947a33
Bin Fan <fanbin103@gmail.com>
2015-12-20 13:04:50 -0800

    Address code style issues in clients

--- clients/unshaded/src/main/java/tachyon/client/keyvalue/KeyValueFileReader.java
+++ clients/unshaded/src/main/java/tachyon/client/keyvalue/KeyValueFileReader.java
@@ -11,7 +11,6 @@
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
- *
  */
 
 package tachyon.client.keyvalue;
@@ -20,10 +19,10 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import com.google.common.base.Preconditions;
+
 import tachyon.TachyonURI;
 import tachyon.exception.TachyonException;
-
-import com.google.common.base.Preconditions;
 
 /**
  * Interface of the reader class to access a Tachyon key-value file.
@@ -56,8 +55,8 @@ public interface KeyValueFileReader extends Closeable {
   byte[] get(byte[] key) throws IOException, TachyonException;
 
   /**
-   * Gets the value associated with the given key in the key-value file, returning null if the
-   * key is not found. Both key and value are in ByteBuffer to make zero-copy possible for better
+   * Gets the value associated with the given key in the key-value file, returning null if the key
+   * is not found. Both key and value are in ByteBuffer to make zero-copy possible for better
    * performance.
    *
    * @param key key to get, cannot be null
