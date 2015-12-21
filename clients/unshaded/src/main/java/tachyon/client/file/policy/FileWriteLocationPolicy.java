2ba4c2deddb5085e613c822d489dec5c1656a20f
Yupeng Fu <yupeng.fu@gmail.com>
2015-12-20 16:34:48 -0800

    Finish localFirstPolicy

--- clients/unshaded/src/main/java/tachyon/client/file/policy/FileWriteLocationPolicy.java
+++ clients/unshaded/src/main/java/tachyon/client/file/policy/FileWriteLocationPolicy.java
@@ -19,20 +19,40 @@ import java.util.List;
 
 import tachyon.annotation.PublicApi;
 import tachyon.client.block.BlockWorkerInfo;
+import tachyon.client.file.FileOutStream;
+import tachyon.client.file.options.OutStreamOptions;
 
 /**
- * Interface for the location policy of which workers a file's block are written into. A file policy
- * instance is used only once per file write, and only when the hostname in the options is not
- * specified.
+ * <p>
+ * Interface for the location policy of which workers a file's block are written into. A location
+ * policy instance is used only once per file write.
+ * </p>
+ *
+ * <p>
+ * The {@link FileOutStream} creates a new policy based on the
+ * {@link FileWriteLocationPolicyOptions} passed in {@link OutStreamOptions}. And then the stream
+ * calls the {@link #initialize(List, FileWriteLocationPolicyOptions)} to initialize the policy.
+ * Then the stream calls {@link #getWorkerForNextBlock(List)} to decide which worker to write the
+ * next block per block write.
+ * </p>
  */
 @PublicApi
 public interface FileWriteLocationPolicy<T extends FileWriteLocationPolicyOptions> {
 
   /**
+   * Initializes the location policy with the information of the active workers and the options.
+   *
+   * @param workerInfoList the list of active workers information
+   * @param policyOptions the policy options for configuring the policy
+   */
+  public void initialize(List<BlockWorkerInfo> workerInfoList, T policyOptions);
+
+  /**
    * Gets the worker's host name for the next block to write to.
    *
    * @param workerInfoList the info of the active workers
+   * @param the size of the block in bytes
    * @return the host name of worker to write to
    */
-  public String getWorkerForNextBlock(List<BlockWorkerInfo> workerInfoList);
+  public String getWorkerForNextBlock(List<BlockWorkerInfo> workerInfoList, long blockSizeBytes);
 }
