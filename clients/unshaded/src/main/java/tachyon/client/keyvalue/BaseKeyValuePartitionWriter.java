2204dbeff9c942354954dcd536b219c4e5014357
Bin Fan <fanbin103@gmail.com>
2016-01-03 17:27:17 -0800

    Add ByteBufferKeyValuePartitionReaderTest, add two properties about key-value store

--- clients/unshaded/src/main/java/tachyon/client/keyvalue/BaseKeyValuePartitionWriter.java
+++ clients/unshaded/src/main/java/tachyon/client/keyvalue/BaseKeyValuePartitionWriter.java
@@ -24,6 +24,7 @@ import com.google.common.base.Preconditions;
 
 import tachyon.Constants;
 import tachyon.client.AbstractOutStream;
+import tachyon.client.ClientContext;
 import tachyon.util.io.ByteIOUtils;
 
 /**
@@ -49,6 +50,8 @@ public final class BaseKeyValuePartitionWriter implements KeyValuePartitionWrite
   private boolean mClosed;
   /** whether this writer is canceled */
   private boolean mCanceled;
+  /** maximum size of this partition in bytes */
+  private long mMaxSizeBytes;
 
   /**
    * @param fileOutStream output stream to store the key-value file
@@ -62,6 +65,7 @@ public final class BaseKeyValuePartitionWriter implements KeyValuePartitionWrite
     mIndex = LinearProbingIndex.createEmptyIndex();
     mClosed = false;
     mCanceled = false;
+    mMaxSizeBytes = ClientContext.getConf().getBytes(Constants.KEYVALUE_PARTITION_SIZE_BYTES_MAX);
   }
 
   @Override
@@ -89,14 +93,15 @@ public final class BaseKeyValuePartitionWriter implements KeyValuePartitionWrite
     Preconditions.checkNotNull(key);
     Preconditions.checkNotNull(value);
     Preconditions.checkState(!mClosed);
+    Preconditions.checkState(!isFull());
     mIndex.put(key, value, mPayloadWriter);
     mKeyCount ++;
   }
 
   @Override
   public boolean isFull() {
-    // TODO(binfan): make this configurable.
-    return byteCount() >= Constants.GB;
+    long size = byteCount();
+    return size >= mMaxSizeBytes;
   }
 
   /**
