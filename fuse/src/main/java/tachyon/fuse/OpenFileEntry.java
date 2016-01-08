50a4c32534868ad042ae44b083dfe008f28a2999
Jan Hentschel <jan.hentschel@ultratendency.com>
2016-01-08 23:37:39 +0100

    Incorporated PR comments

--- fuse/src/main/java/tachyon/fuse/OpenFileEntry.java
+++ fuse/src/main/java/tachyon/fuse/OpenFileEntry.java
@@ -42,7 +42,7 @@ final class OpenFileEntry implements Closeable {
   }
 
   /**
-   * Gets the opened input stream for this open file entry.  The value returned can be {@code null}
+   * Gets the opened input stream for this open file entry. The value returned can be {@code null}
    * if the file is not open for reading.
    *
    * @return an opened input stream for the open tachyon file, or null
@@ -52,7 +52,7 @@ final class OpenFileEntry implements Closeable {
   }
 
   /**
-   * Gets the opened output stream for this open file entry.  The value returned can be {@code null}
+   * Gets the opened output stream for this open file entry. The value returned can be {@code null}
    * if the file is not open for writing.
    *
    * @return an opened input stream for the open tachyon file, or null
--- integration-tests/src/test/java/tachyon/client/RemoteBlockInStreamIntegrationTest.java
+++ integration-tests/src/test/java/tachyon/client/RemoteBlockInStreamIntegrationTest.java
@@ -543,7 +543,7 @@ public class RemoteBlockInStreamIntegrationTest {
   }
 
   /**
-   * Test remote read stream lock in {@code RemoteBlockInStream}.
+   * Tests remote read stream lock in {@link RemoteBlockInStream}.
    */
   @Test
   public void remoteReadLockTest() throws Exception {
