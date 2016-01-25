5129ca09b327c1b5f675aca3b550f5c601e7f200
Jiri Simsa <jiri.simsa@gmail.com>
2016-01-25 11:20:43 -0800

    Adding thread safety annotations for tachyon.master.lineage.meta package in servers module.

--- servers/src/main/java/tachyon/master/lineage/meta/Lineage.java
+++ servers/src/main/java/tachyon/master/lineage/meta/Lineage.java
@@ -18,6 +18,8 @@ package tachyon.master.lineage.meta;
 import java.util.Collections;
 import java.util.List;
 
+import javax.annotation.concurrent.ThreadSafe;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 import com.google.common.base.Preconditions;
@@ -34,6 +36,7 @@ import tachyon.proto.journal.Lineage.LineageEntry;
  * A lineage tracks the dependencies imposed by a job, including the input files the job depends on,
  * and the output files the job generates.
  */
+@ThreadSafe
 public final class Lineage implements JournalEntryRepresentable {
   private final long mId;
   private final List<Long> mInputFiles;
@@ -90,21 +93,21 @@ public final class Lineage implements JournalEntryRepresentable {
   /**
    * @return the job
    */
-  public Job getJob() {
+  public synchronized Job getJob() {
     return mJob;
   }
 
   /**
    * @return the lineage id
    */
-  public long getId() {
+  public synchronized long getId() {
     return mId;
   }
 
   /**
    * @return the creation time
    */
-  public long getCreationTime() {
+  public synchronized long getCreationTime() {
     return mCreationTimeMs;
   }
 
