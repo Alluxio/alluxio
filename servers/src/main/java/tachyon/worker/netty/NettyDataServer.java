2bdd5aa9c4af1c81b707caba28abb29bb637be39
cc <myairia@gmail.com>
2016-01-26 06:42:57 +0800

    Fixes in worker package to make CheckStyle happy

--- servers/src/main/java/tachyon/worker/netty/NettyDataServer.java
+++ servers/src/main/java/tachyon/worker/netty/NettyDataServer.java
@@ -50,7 +50,7 @@ public final class NettyDataServer implements DataServer {
    * Creates a new instance of {@link NettyDataServer}.
    *
    * @param address the server address
-   * @param dataManager a block data manager handle
+   * @param blockWorker a block worker handle
    * @param tachyonConf Tachyon configuration
    */
   public NettyDataServer(final InetSocketAddress address, final BlockWorker blockWorker,
