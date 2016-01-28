b144b0616cb525e543444c1f61a40114d0e774cd
luoli <luoli523@gmail.com>
2016-01-29 00:25:22 +0800

    [TACHYON-1604] complete  unit tests for untested public methods.

--- core/server/src/test/java/tachyon/worker/block/BlockWorkerTest.java
+++ core/server/src/test/java/tachyon/worker/block/BlockWorkerTest.java
@@ -16,9 +16,13 @@
 package tachyon.worker.block;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.After;
@@ -45,13 +49,12 @@ import tachyon.worker.block.meta.StorageDir;
 import tachyon.worker.block.meta.TempBlockMeta;
 import tachyon.worker.file.FileSystemMasterClient;
 
-import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Unit tests for {@link BlockDataManager}.
+ * Unit tests for {@link BlockWorker}.
  */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({BlockMasterClient.class, FileSystemMasterClient.class,
@@ -121,8 +124,10 @@ public class BlockWorkerTest {
    */
   @Test
   public void abortBlockTest() throws Exception {
-    mBlockWorker.abortBlock(anyLong(), anyLong());
-    verify(mBlockStore).abortBlock(anyLong(), anyLong());
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    mBlockWorker.abortBlock(sessionId, blockId);
+    verify(mBlockStore).abortBlock(sessionId, blockId);
   }
 
   /**
@@ -132,8 +137,10 @@ public class BlockWorkerTest {
    */
   @Test
   public void accessBlockTest() throws Exception {
-    mBlockWorker.accessBlock(anyLong(), anyLong());
-    verify(mBlockStore).accessBlock(anyLong(), anyLong());
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    mBlockWorker.accessBlock(sessionId, blockId);
+    verify(mBlockStore).accessBlock(sessionId, blockId);
   }
 
   /**
@@ -207,5 +214,248 @@ public class BlockWorkerTest {
         mBlockWorker.createBlock(sessionId, blockId, tierAlias, initialBytes));
   }
 
-  //TODO(luoli523): Write unit tests for untested public methods.
+  /**
+   * Tests the {@link BlockWorker#createBlockRemote(long, long, String, long)} method.
+   *
+   * @throws Exception if the creation of the block or its metadata fails
+   */
+  @Test
+  public void createBlockRemoteTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long initialBytes = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    String tierAlias = "MEM";
+    BlockStoreLocation location = BlockStoreLocation.anyDirInTier(tierAlias);
+    StorageDir storageDir = Mockito.mock(StorageDir.class);
+    TempBlockMeta meta = new TempBlockMeta(sessionId, blockId, initialBytes, storageDir);
+
+    when(mBlockStore.createBlockMeta(sessionId, blockId, location, initialBytes))
+        .thenReturn(meta);
+    when(storageDir.getDirPath()).thenReturn("/tmp");
+    assertEquals(PathUtils.concatPath("/tmp", sessionId, blockId),
+        mBlockWorker.createBlock(sessionId, blockId, tierAlias, initialBytes));
+  }
+
+  /**
+   * Tests the {@link BlockWorker#freeSpace(long, long, String)} method.
+   *
+   * @throws Exception if the free space check fails
+   */
+  @Test
+  public void freeSpaceTest() throws Exception {
+    long sessionId = mRandom.nextLong();
+    long availableBytes = mRandom.nextLong();
+    String tierAlias = "MEM";
+    BlockStoreLocation location = BlockStoreLocation.anyDirInTier(tierAlias);
+    mBlockWorker.freeSpace(sessionId, availableBytes, tierAlias);
+    verify(mBlockStore).freeSpace(sessionId, availableBytes, location);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#getTempBlockWriterRemote(long, long)} method.
+   *
+   * @throws Exception if the method check fails
+   */
+  @Test
+  public void getTempBlockWriterRemoteTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    mBlockWorker.getTempBlockWriterRemote(sessionId, blockId);
+    verify(mBlockStore).getBlockWriter(sessionId, blockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#getReport()} method.
+   *
+   */
+  @Test
+  public void getReportTest() {
+    mBlockWorker.getReport();
+    verify(mHeartbeatReporter).generateReport();
+  }
+
+  /**
+   * Tests the {@link BlockWorker#getStoreMeta()} method.
+   *
+   */
+  @Test
+  public void getStoreMetaTest() {
+    mBlockWorker.getStoreMeta();
+    verify(mBlockStore).getBlockStoreMeta();
+  }
+
+  /**
+   * Tests the {@link BlockWorker#getVolatileBlockMeta(long)} method.
+   *
+   * @throws Exception if the getVolatileBlockMeta check fails
+   */
+  @Test
+  public void getVolatileBlockMetaTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    mBlockWorker.getVolatileBlockMeta(blockId);
+    verify(mBlockStore).getVolatileBlockMeta(blockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#hasBlockMeta(long)} method.
+   *
+   */
+  @Test
+  public void hasBlockMetaTest() {
+    long blockId = mRandom.nextLong();
+    mBlockWorker.hasBlockMeta(blockId);
+    verify(mBlockStore).hasBlockMeta(blockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#lockBlock(long, long)} method.
+   *
+   * @throws Exception if the lockBlock check fails
+   */
+  @Test
+  public void lockBlockTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    mBlockWorker.lockBlock(sessionId, blockId);
+    verify(mBlockStore).lockBlock(sessionId, blockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#moveBlock(long, long, String)} method.
+   *
+   * @throws Exception if the moveBlock check fails
+   */
+  @Test
+  public void moveBlockTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    String tierAlias = "MEM";
+    BlockStoreLocation location = BlockStoreLocation.anyDirInTier(tierAlias);
+    mBlockWorker.moveBlock(sessionId, blockId, tierAlias);
+    verify(mBlockStore).moveBlock(sessionId, blockId, location);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#readBlock(long, long, long)} method.
+   *
+   * @throws Exception if the readBlock check fails
+   */
+  @Test
+  public void readBlockTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    long lockId = mRandom.nextLong();
+    long blockSize = mRandom.nextLong();
+    StorageDir storageDir = Mockito.mock(StorageDir.class);
+    when(storageDir.getDirPath()).thenReturn("/tmp");
+    BlockMeta meta = new BlockMeta(blockId, blockSize, storageDir);
+    when(mBlockStore.getBlockMeta(sessionId, blockId, lockId)).thenReturn(meta);
+
+    mBlockWorker.readBlock(sessionId, blockId, lockId);
+    verify(mBlockStore).getBlockMeta(sessionId, blockId, lockId);
+    assertEquals(PathUtils.concatPath("/tmp", blockId),
+        mBlockWorker.readBlock(sessionId, blockId, lockId));
+  }
+
+  /**
+   * Tests the {@link BlockWorker#readBlockRemote(long, long, long)} method.
+   *
+   * @throws Exception if the readBlockRemote check fails
+   */
+  @Test
+  public void readBlockRemote() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    long lockId = mRandom.nextLong();
+
+    mBlockWorker.readBlockRemote(sessionId, blockId, lockId);
+    verify(mBlockStore).getBlockReader(sessionId, blockId, lockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#removeBlock(long, long)} method.
+   *
+   * @throws Exception if the removeBlock check fails
+   */
+  @Test
+  public void removeBlockTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    mBlockWorker.removeBlock(sessionId, blockId);
+    verify(mBlockStore).removeBlock(sessionId, blockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#requestSpace(long, long, long)} method.
+   *
+   * @throws Exception if the requestSpace check fails
+   */
+  @Test
+  public void requestSpaceTest() throws Exception {
+    long blockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    long additionalBytes = mRandom.nextLong();
+    mBlockWorker.requestSpace(sessionId, blockId, additionalBytes);
+    verify(mBlockStore).requestSpace(sessionId, blockId, additionalBytes);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#unlockBlock(long)} and
+   * {@link BlockWorker#unlockBlock(long, long)} method.
+   *
+   * @throws Exception if the unlockBlock check fails
+   */
+  @Test
+  public void unlockBlockTest() throws Exception {
+    long lockId = mRandom.nextLong();
+    long sessionId = mRandom.nextLong();
+    long blockId = mRandom.nextLong();
+
+    mBlockWorker.unlockBlock(lockId);
+    verify(mBlockStore).unlockBlock(lockId);
+
+    mBlockWorker.unlockBlock(sessionId, blockId);
+    verify(mBlockStore).unlockBlock(sessionId, blockId);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#sessionHeartbeat(long, List<long>)} method.
+   *
+   */
+  @Test
+  public void sessionHeartbeatTest() {
+    long sessionId = mRandom.nextLong();
+    List<Long> metrics = new ArrayList<Long>();
+    metrics.add(mRandom.nextLong());
+
+    mBlockWorker.sessionHeartbeat(sessionId, metrics);
+    verify(mSessions).sessionHeartbeat(sessionId);
+    verify(mMetricsReporter).updateClientMetrics(metrics);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#updatePinList(Set<long>)} method.
+   *
+   */
+  @Test
+  public void updatePinListTest() {
+    Set<Long> pinnedInodes = new HashSet<Long>();
+    pinnedInodes.add(mRandom.nextLong());
+
+    mBlockWorker.updatePinList(pinnedInodes);
+    verify(mBlockStore).updatePinnedInodes(pinnedInodes);
+  }
+
+  /**
+   * Tests the {@link BlockWorker#getFileInfo(long)} method.
+   *
+   * @throws Exception if the getFileInfo check fails
+   */
+  @Test
+  public void getFileInfoTest() throws Exception {
+    long fileId = mRandom.nextLong();
+
+    mBlockWorker.getFileInfo(fileId);
+    verify(mFileSystemMasterClient).getFileInfo(fileId);
+  }
 }
