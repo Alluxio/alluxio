f98ac2fa6bc17e50ec2827fb6b4ce2d38cee1d97
Andrew Audibert <aaudibert10@gmail.com>
2016-01-29 10:34:15 -0800

    Fix references to tier count configuration

--- deploy/vagrant/provision/roles/lib/files/tachyon_tiered.sh
+++ deploy/vagrant/provision/roles/lib/files/tachyon_tiered.sh
@@ -7,11 +7,11 @@ for disk in $DISK; do
  TIERED_PATH=/$disk,$TIERED_PATH
  quota=`df -h | grep "/$disk" | awk '{print $2}'`
  TIERED_QUOTA=${quota}B,$TIERED_QUOTA
-done 
+done
 
 [[ "$TIERED_PATH" == "" ]] && exit 0
 
-sed -i "s/tachyon.worker.tieredstore.level.max=1/tachyon.worker.tieredstore.level.max=2/g" /tachyon/conf/tachyon-env.sh
+sed -i "s/tachyon.worker.tieredstore.levels=1/tachyon.worker.tieredstore.levels=2/g" /tachyon/conf/tachyon-env.sh
 
 sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
   -Dtachyon.worker.tieredstore.level1.dirs.quota=$TIERED_QUOTA
