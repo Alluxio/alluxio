ca6bedb0c3a3ba10041ccd7f1b353243d16263f1
Carlos de Carvalho <caadecarvalho@gmail.com>
2016-02-03 10:44:19 -0200

    Update ClientRWLockTest.java
    Corrected javadoc for `notSameLockTest()` method

--- core/server/src/test/java/tachyon/worker/block/ClientRWLockTest.java
+++ core/server/src/test/java/tachyon/worker/block/ClientRWLockTest.java
@@ -39,7 +39,7 @@ public final class ClientRWLockTest {
   }
 
   /**
-   * Tests the equalty for two {@link ClientRWLock} constructors.
+   * Tests the equality for both {@link ClientRWLock} constructors.
    */
   @Test
   public void notSameLockTest() {
