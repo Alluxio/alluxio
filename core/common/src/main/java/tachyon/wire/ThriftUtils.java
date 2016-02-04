1aaffaf8df78d17e71243df6d94d687893802bb5
Jiri Simsa <jiri.simsa@gmail.com>
2016-02-03 17:27:01 -0800

    Fixing typos.

--- core/common/src/main/java/tachyon/wire/ThriftUtils.java
+++ core/common/src/main/java/tachyon/wire/ThriftUtils.java
@@ -98,7 +98,7 @@ public class ThriftUtils {
     result.setFileId(fileInfo.getFileId());
     result.setName(fileInfo.getName());
     result.setPath(fileInfo.getPath());
-    result.setUfsPath(fileInfo.getPath());
+    result.setUfsPath(fileInfo.getUfsPath());
     result.setLength(fileInfo.getLength());
     result.setBlockSizeBytes(fileInfo.getBlockSizeBytes());
     result.setCreationTimeMs(fileInfo.getCreationTimeMs());
@@ -144,7 +144,7 @@ public class ThriftUtils {
     result.setJob(fromThrift(lineageInfo.getJob()));
     result.setCreationTimeMs(lineageInfo.getCreationTimeMs());
     result.setParents(lineageInfo.getParents());
-    result.setChildren(lineageInfo.getParents());
+    result.setChildren(lineageInfo.getChildren());
     return result;
   }
 
