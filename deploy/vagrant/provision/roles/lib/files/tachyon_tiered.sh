a6e36e9f82c62230beaedef92c67ffa4e1a872ea
Calvin Jia <jia.calvin@gmail.com>
2016-02-05 14:33:21 -0800

    Update deploy provision.

--- deploy/vagrant/provision/roles/lib/files/tachyon_tiered.sh
+++ deploy/vagrant/provision/roles/lib/files/tachyon_tiered.sh
@@ -11,16 +11,16 @@ done
 
 [[ "$TIERED_PATH" == "" ]] && exit 0
 
-sed -i "s/alluxio.worker.tieredstore.levels=1/alluxio.worker.tieredstore.levels=2/g" /tachyon/conf/tachyon-env.sh
+sed -i "s/alluxio.worker.tieredstore.levels=1/alluxio.worker.tieredstore.levels=2/g" /alluxio/conf/alluxio-env.sh
 
-sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
-  -Dtachyon.worker.tieredstore.level1.dirs.quota=$TIERED_QUOTA
-" /tachyon/conf/tachyon-env.sh
+sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
+  -Dalluxio.worker.tieredstore.level1.dirs.quota=$TIERED_QUOTA
+" /alluxio/conf/alluxio-env.sh
 
-sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
-  -Dtachyon.worker.tieredstore.level1.dirs.path=$TIERED_PATH
-" /tachyon/conf/tachyon-env.sh
+sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
+  -Dalluxio.worker.tieredstore.level1.dirs.path=$TIERED_PATH
+" /alluxio/conf/alluxio-env.sh
 
-sed -i "/export TACHYON_JAVA_OPTS+=\"/ a\
-  -Dtachyon.worker.tieredstore.level1.alias=SSD
-" /tachyon/conf/tachyon-env.sh
+sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
+  -Dalluxio.worker.tieredstore.level1.alias=SSD
+" /alluxio/conf/alluxio-env.sh
