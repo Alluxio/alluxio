7d903d33388ae373cf5f8dc5c027941b01432b1e
Calvin Jia <jia.calvin@gmail.com>
2016-02-05 18:01:13 -0800

    Finish renaming in deploy module.

--- deploy/vagrant/vbox/buildbox.sh
+++ deploy/vagrant/vbox/buildbox.sh
@@ -2,7 +2,7 @@
 
 OLD_BOX="$(vagrant box list | grep alluxio-dev | cut -d ' ' -f1)"
 if [[ "${OLD_BOX}" != "" ]]; then
-  echo "Tachyon base image ${OLD_BOX} exists."
+  echo "Alluxio base image ${OLD_BOX} exists."
   echo "If you want to remove image ${OLD_BOX}, please run: vagrant box remove ${OLD_BOX}"
   exit 0
 fi
@@ -10,14 +10,14 @@ fi
 HERE="$(dirname $0)"
 pushd "${HERE}" >/dev/null
 
-if [[ -f tachyon-dev.box ]]; then
-  rm -f tachyon-dev.box
+if [[ -f alluxio-dev.box ]]; then
+  rm -f alluxio-dev.box
 fi
 
 echo "Generating alluxio base image 'alluxio-dev.box' ..."
 vagrant up
-vagrant package --output tachyon-dev.box default
+vagrant package --output alluxio-dev.box default
 vagrant destroy -f
-vagrant box add tachyon-dev tachyon-dev.box
+vagrant box add alluxio-dev alluxio-dev.box
 
 popd >/dev/null
