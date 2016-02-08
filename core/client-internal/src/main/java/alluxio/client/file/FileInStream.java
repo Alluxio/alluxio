6074514548a570cb0865b686ea5754c4e10366ce
Calvin Jia <jia.calvin@gmail.com>
2016-02-08 15:01:23 -0800

    Fix edge cases.

--- core/client-internal/src/main/java/alluxio/client/file/FileInStream.java
+++ core/client-internal/src/main/java/alluxio/client/file/FileInStream.java
@@ -286,11 +286,11 @@ public class FileInStream extends InputStream implements BoundedStream, Seekable
    */
   private long getCurrentBlockSize() {
     // If there is only one block in this file.
-    if (mFileLength < mBlockSize) {
+    if (mFileLength <= mBlockSize) {
       return mFileLength;
     }
     // If we are not in the last block, return the block size, else return size of the last block.
-    if (mFileLength - mPos < mBlockSize) {
+    if (mFileLength - mPos >= mBlockSize) {
       return mBlockSize;
     } else {
       return mFileLength % mBlockSize;
