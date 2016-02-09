09a5c3aad24b97969a7952132db5e8f76dea46de
Calvin Jia <jia.calvin@gmail.com>
2016-02-09 13:06:22 -0800

    Check move location before moving.

--- core/server/src/main/java/alluxio/worker/block/BlockWorker.java
+++ core/server/src/main/java/alluxio/worker/block/BlockWorker.java
@@ -493,7 +493,18 @@ public final class BlockWorker extends WorkerBase {
   public void moveBlock(long sessionId, long blockId, String tierAlias)
       throws BlockDoesNotExistException, BlockAlreadyExistsException, InvalidWorkerStateException,
       WorkerOutOfSpaceException, IOException {
+    // Because the move operation is expensive, we first check if the operation is necessary
     BlockStoreLocation dst = BlockStoreLocation.anyDirInTier(tierAlias);
+    long lockId = mBlockStore.lockBlock(sessionId, blockId);
+    try {
+      BlockMeta meta = mBlockStore.getBlockMeta(sessionId, blockId, lockId);
+      if (meta.getBlockLocation().belongsTo(dst)) {
+        return;
+      }
+    } finally {
+      mBlockStore.unlockBlock(lockId);
+    }
+    // Execute the block move if necessary
     mBlockStore.moveBlock(sessionId, blockId, dst);
   }
 
