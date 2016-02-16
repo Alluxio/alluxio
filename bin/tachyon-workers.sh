a9c2fb5f018161ceb92e638cb0a4dc5717abe3b0
caadecarvalho <caadecarvalho@gmail.com>
2016-02-16 00:57:48 -0200

    Enhanced information under log file to understand the step being executed.

--- bin/tachyon-workers.sh
+++ bin/tachyon-workers.sh
@@ -22,9 +22,17 @@ TACHYON_LIBEXEC_DIR=${TACHYON_LIBEXEC_DIR:-$DEFAULT_LIBEXEC_DIR}
 HOSTLIST=$(cat ${TACHYON_CONF_DIR}/workers | sed  "s/#.*$//;/^$/d")
 TACHYON_TASK_LOG="$(echo ${BIN} | sed 's/bin$//g')"logs/task.log
 
+if [ "${3}" == "tachyon.worker.TachyonWorker" ]; then
+  WORKER_ACTION_TYPE="WORKERS"
+else
+  WORKER_ACTION_TYPE="MASTER"
+fi
+
 for worker in $(echo ${HOSTLIST}); do
   echo "$(date +"%F %H:%M:%S,$(date +"%s%N" | cut -c 11- | cut -c 1-3)") INFO ${WORKER_ACTION_TYPE}  Connecting to $worker as $USER..." >> ${TACHYON_TASK_LOG}
   nohup ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -t ${worker} $LAUNCHER $"${@// /\\ }" >> ${TACHYON_TASK_LOG} 2>&1&
 done
 
+echo "Waiting for ${WORKER_ACTION_TYPE} tasks to finish..."
 wait
+echo "All ${WORKER_ACTION_TYPE} tasks finished, please analyze the log at ${TACHYON_TASK_LOG}."
