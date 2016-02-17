f70f5fc47bc9cb014bb8ee423087b26b534abfcb
Bin Fan <fanbin103@gmail.com>
2016-02-17 15:10:21 -0800

    Combine ls and lsr tests

--- tests/src/test/java/alluxio/shell/command/LsCommandTest.java
+++ tests/src/test/java/alluxio/shell/command/LsCommandTest.java
@@ -142,4 +142,49 @@ public class LsCommandTest extends AbstractAlluxioShellTest {
     mFsShell.run("ls", "/testWildCards/*");
     Assert.assertEquals(expect, mOutput.toString());
   }
+
+  @Test
+  public void lsrNoAclTest() throws IOException, AlluxioException {
+    URIStatus[] files = createFiles();
+    mFsShell.run("lsr", "/testRoot");
+    String expected = "";
+    expected += "WARNING: lsr is deprecated. Please use ls -R instead.\n";
+    expected += getLsNoAclResultStr("/testRoot/testFileA", files[0].getCreationTimeMs(), 10,
+        LsCommand.STATE_FILE_IN_MEMORY);
+    expected += getLsNoAclResultStr("/testRoot/testDir", files[1].getCreationTimeMs(), 0,
+        LsCommand.STATE_FOLDER);
+    expected += getLsNoAclResultStr("/testRoot/testDir/testFileB", files[2].getCreationTimeMs(), 20,
+        LsCommand.STATE_FILE_IN_MEMORY);
+    expected += getLsNoAclResultStr("/testRoot/testFileC", files[3].getCreationTimeMs(), 30,
+        LsCommand.STATE_FILE_NOT_IN_MEMORY);
+    Assert.assertEquals(expected, mOutput.toString());
+  }
+
+  @Test
+  @LocalAlluxioClusterResource.Config(
+      confParams = {Constants.SECURITY_AUTHORIZATION_PERMISSION_ENABLED, "true",
+          Constants.SECURITY_AUTHENTICATION_TYPE, "SIMPLE", Constants.SECURITY_GROUP_MAPPING,
+          "alluxio.security.group.provider.IdentityUserGroupsMapping"})
+  public void lsrTest() throws IOException, AlluxioException {
+    String testUser = "test_user_lsr";
+    clearAndLogin(testUser);
+
+    URIStatus[] files = createFiles();
+    mFsShell.run("lsr", "/testRoot");
+    String expected = "";
+    expected += "WARNING: lsr is deprecated. Please use ls -R instead.\n";
+    expected += getLsResultStr("/testRoot/testFileA", files[0].getCreationTimeMs(), 10,
+        LsCommand.STATE_FILE_IN_MEMORY, testUser, testUser, files[0].getPermission(),
+        files[0].isFolder());
+    expected +=
+        getLsResultStr("/testRoot/testDir", files[1].getCreationTimeMs(), 0, LsCommand.STATE_FOLDER,
+            testUser, testUser, files[1].getPermission(), files[1].isFolder());
+    expected += getLsResultStr("/testRoot/testDir/testFileB", files[2].getCreationTimeMs(), 20,
+        LsCommand.STATE_FILE_IN_MEMORY, testUser, testUser, files[2].getPermission(),
+        files[2].isFolder());
+    expected += getLsResultStr("/testRoot/testFileC", files[3].getCreationTimeMs(), 30,
+        LsCommand.STATE_FILE_NOT_IN_MEMORY, testUser, testUser, files[3].getPermission(),
+        files[3].isFolder());
+    Assert.assertEquals(expected, mOutput.toString());
+  }
 }
