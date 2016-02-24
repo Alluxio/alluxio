9e4a64b596f8cab2ac157d1c10057deb3b8ef3f5
Chaomin Yu <ifcharming@gmail.com>
2016-02-23 17:25:15 -0800

    [SMALLFIX] Simplify a small issue with Collections.addAll

--- core/common/src/test/java/alluxio/util/CommonUtilsTest.java
+++ core/common/src/test/java/alluxio/util/CommonUtilsTest.java
@@ -110,9 +110,7 @@ public class CommonUtilsTest {
 
     for (TestCase testCase : testCases) {
       ArrayList<String> input = new ArrayList<String>();
-      for (String s : testCase.mExpected) {
-        input.add(s);
-      }
+      Collections.addAll(input, testCase.mExpected);
       String[] got = CommonUtils.toStringArray(input);
       Assert.assertEquals(testCase.mExpected.length, got.length);
       for (int k = 0; k < got.length; k++) {
