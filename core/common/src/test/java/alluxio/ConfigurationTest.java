175f9f119e75281a4548a35a9b915cbefe270c30
Pengfei Xuan <pfxuan@gmail.com>
2016-02-27 12:42:38 -0500

    Fix configuration testing.

--- core/common/src/test/java/alluxio/ConfigurationTest.java
+++ core/common/src/test/java/alluxio/ConfigurationTest.java
@@ -178,7 +178,7 @@ public class ConfigurationTest {
     Assert.assertEquals(Constants.SECOND_MS, intValue);
 
     intValue = sDefaultConfiguration.getInt(Constants.MASTER_WORKER_THREADS_MIN);
-    Assert.assertEquals(Runtime.getRuntime().availableProcessors(), intValue);
+    Assert.assertEquals(512, intValue);
 
     intValue = sDefaultConfiguration.getInt(Constants.MASTER_WORKER_TIMEOUT_MS);
     Assert.assertEquals(10 * Constants.SECOND_MS, intValue);
@@ -221,7 +221,7 @@ public class ConfigurationTest {
     Assert.assertEquals(Constants.SECOND_MS, intValue);
 
     intValue = sDefaultConfiguration.getInt(Constants.WORKER_WORKER_BLOCK_THREADS_MIN);
-    Assert.assertEquals(1, intValue);
+    Assert.assertEquals(256, intValue);
 
     intValue = sDefaultConfiguration.getInt(Constants.WORKER_SESSION_TIMEOUT_MS);
     Assert.assertEquals(10 * Constants.SECOND_MS, intValue);
