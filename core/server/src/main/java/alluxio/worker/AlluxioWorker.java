fd94a0e751835495d13055673356c3b2cc19b50c
Yupeng Fu <yupeng.fu@gmail.com>
2016-02-29 13:37:44 -0800

    validate the worker

--- core/server/src/main/java/alluxio/worker/AlluxioWorker.java
+++ core/server/src/main/java/alluxio/worker/AlluxioWorker.java
@@ -13,6 +13,7 @@ package alluxio.worker;
 
 import alluxio.Configuration;
 import alluxio.Constants;
+import alluxio.ValidateConf;
 import alluxio.Version;
 import alluxio.metrics.MetricsSystem;
 import alluxio.security.authentication.AuthenticationUtils;
@@ -64,6 +65,10 @@ public final class AlluxioWorker {
    */
   public static void main(String[] args) {
     checkArgs(args);
+    // validate the conf
+    if (!ValidateConf.validate()) {
+      System.exit(-1);
+    }
     AlluxioWorker worker = get();
     try {
       worker.start();
