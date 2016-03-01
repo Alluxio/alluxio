acb3190380a8ac3190d61b056e4bbbcb2f0b310a
Yupeng Fu <yupeng.fu@gmail.com>
2016-03-01 13:22:04 -0800

    Restore the accidental removalof logger type

--- integration/bin/alluxio-master-yarn.sh
+++ integration/bin/alluxio-master-yarn.sh
@@ -13,13 +13,13 @@ echo "Formatting Alluxio Master"
   ${ALLUXIO_MASTER_JAVA_OPTS} \
   -Dalluxio.home="${ALLUXIO_HOME}" \
   -Dalluxio.logs.dir="${YARN_LOG_DIR}" \
+  -Dalluxio.logger.type="MASTER_LOGGER"  
   alluxio.Format master > "${YARN_LOG_DIR}"/master.out 2>&1
 
 echo "Starting Alluxio Master"
 
 "${JAVA}" -cp "${CLASSPATH}" \
   ${ALLUXIO_MASTER_JAVA_OPTS} \
-  -Dalluxio.accesslogger.type="MASTER_ACCESS_LOGGER" \
   -Dalluxio.home="${ALLUXIO_HOME}" \
   -Dalluxio.logger.type="MASTER_LOGGER" \
   -Dalluxio.logs.dir="${YARN_LOG_DIR}" \
