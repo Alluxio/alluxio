8dde1c4cab1c4a2aa41d6f0dd15347bebe06e816
Yupeng Fu <yupeng.fu@gmail.com>
2016-03-03 10:32:39 -0800

    addressed comments

--- core/common/src/test/java/alluxio/replay/ReplayCacheTest.java
+++ core/common/src/test/java/alluxio/replay/ReplayCacheTest.java
@@ -236,7 +236,7 @@ public final class ReplayCacheTest {
 
     @Override
     public Long call() throws AlluxioException {
-      // If it's a AlluxioException, don't wrap it in RuntimeException
+      // If it's an AlluxioException, don't wrap it in RuntimeException
       Throwables.propagateIfInstanceOf(mException, AlluxioException.class);
       throw Throwables.propagate(mException);
     }
