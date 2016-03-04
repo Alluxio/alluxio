8b000c434e7f89e5dee7780b00e4a0d1e2a85c9d
Andrew Audibert <aaudibert10@gmail.com>
2016-03-04 15:18:37 -0800

    Add javadocs

--- core/common/src/main/java/alluxio/resource/CloseableResource.java
+++ core/common/src/main/java/alluxio/resource/CloseableResource.java
@@ -16,17 +16,31 @@ import java.io.Closeable;
 /**
  * A {@code CloseableResource<T>} is a wrapper around a resource of type {@code T} which must do
  * some sort of cleanup when it is no longer in use.
+ *
+ * @param <T> the type of the wrapped resource
  */
 public abstract class CloseableResource<T> implements Closeable {
   private T mResource;
 
+  /**
+   * Creates a {@link CloseableResource<T>} wrapper around the given resource. This resource will
+   * be returned by the {@link CloseableResource#get()} method.
+   *
+   * @param resource the resource to wrap
+   */
   public CloseableResource(T resource) {
     mResource = resource;
   }
 
+  /**
+   * @return the resource
+   */
   public T get() {
     return mResource;
   }
 
+  /**
+   * Performs any cleanup operations necessary when the resource is no longer in use.
+   */
   public abstract void close();
 }
