2cc05db08c11a3da5a1e6bb617e6f7394a9632e4
aunglinn <aunglinn@felixmyanmar.net>
2016-03-14 14:24:42 -0700

    Layout changes
    Adding .img-responsive class with js to best fit for images
    Commented out modernize

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,14 @@
+jQuery(document).ready(function($) {
 
+  $('img').addClass('img-responsive');
+
+  //Bootstrap Navbar Click toggle Fix for hover effect
+  $('#navbar-toggle').click(function(){
+    $(this).toggleClass('open');
+  });
+
+  $('.dropdown').click(function(event){
+     event.stopPropagation();
+   });
+
+});
