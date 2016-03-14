08a18da9db7da685048647de381438bf6fe22782
cc <myairia@gmail.com>
2016-03-14 21:10:24 +0800

    Add REST endpoint for getting master metrics

--- core/server/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
+++ core/server/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -11,11 +11,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
 import com.qmino.miredot.annotations.ReturnType;
 
 import alluxio.Configuration;
 import alluxio.Constants;
 import alluxio.Version;
+import alluxio.util.CommonUtils;
 
 /**
  * This class is a REST handler for requesting general master information.
@@ -29,6 +33,7 @@ public final class AlluxioMasterRestServiceHandler {
   public static final String GET_CONFIGURATION = "configuration";
   public static final String GET_DEBUG = "debug";
   public static final String GET_ADDRESS = "address";
+  public static final String GET_METRICS = "metrics";
   public static final String GET_START_TIME_MS = "start_time_ms";
   public static final String GET_UPTIME_MS = "uptime_ms";
   public static final String GET_VERSION = "version";
@@ -73,6 +78,35 @@ public final class AlluxioMasterRestServiceHandler {
   @ReturnType("java.lang.String")
   public Response getAddress() {
     return Response.ok(mMasterConf.get(Constants.MASTER_ADDRESS)).build();
+  }
+
+  /**
+   * @summary get the master metrics
+   * @return the response object
+   */
+  @GET
+  @Path(GET_METRICS)
+  @ReturnType("java.util.Map<String, Long>")
+  public Response getMetrics() {
+    MetricRegistry metricRegistry = mMaster.getMasterMetricsSystem().getMetricRegistry();
+
+    // Get all counters.
+    Map<String, Counter> counters = metricRegistry.getCounters();
+
+    // Only the gauge for pinned files is retrieved here, other gauges are statistics of free/used
+    // spaces, those statistics can be gotten via other REST apis.
+    String filesPinnedProperty = CommonUtils.argsToString(".",
+        MasterContext.getMasterSource().getName(), MasterSource.FILES_PINNED);
+    Gauge filesPinned = metricRegistry.getGauges().get(filesPinnedProperty);
+
+    // Get values of the counters and gauges and put them into a metrics map.
+    Map<String, Long> metrics = new HashMap<>(counters.size() + 1);
+    for (Map.Entry<String, Counter> counter : counters.entrySet()) {
+      metrics.put(counter.getKey(), counter.getValue().getCount());
+    }
+    metrics.put(filesPinnedProperty, (Long) filesPinned.getValue());
+
+    return Response.ok(metrics).build();
   }
 
   /**
