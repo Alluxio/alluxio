32522073af6baf8b76cd018546d51bad308525a0
aunglinn <aunglinn@felixmyanmar.net>
2016-03-14 16:02:34 -0700

    Adding Table Responsive Class
    Adding table-responsive class for table tags

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
   $('img').addClass('img-responsive');
-
+  $('table').addClass('table-responsive');
   //Bootstrap Navbar Click toggle Fix for hover effect
   $('#navbar-toggle').click(function(){
     $(this).toggleClass('open');
