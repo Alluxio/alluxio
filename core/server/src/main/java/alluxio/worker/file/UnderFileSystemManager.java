cc9fbaf94366fe05366f5f6ac10d2b062973da5b
Calvin Jia <jia.calvin@gmail.com>
2016-03-14 18:22:10 -0700

    Add a simple implementation of a ufs stream for now.

--- core/server/src/main/java/alluxio/worker/file/UnderFileSystemManager.java
+++ core/server/src/main/java/alluxio/worker/file/UnderFileSystemManager.java
@@ -1,5 +1,6 @@
 package alluxio.worker.file;
 
+import alluxio.Configuration;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.FileAlreadyExistsException;
 import alluxio.exception.FileDoesNotExistException;
@@ -26,68 +27,72 @@ public class UnderFileSystemManager {
    * renamed to the final file path. This stream guarantees the temporary file will be cleaned up
    * when close or cancel is called.
    */
-  private class TemporaryOutputStream {
+  private class UnderFileSystemOutputStream {
+    private final Configuration mConf;
     private final OutputStream mStream;
     private final String mPath;
     private final String mTemporaryPath;
 
-    private TemporaryOutputStream(OutputStream stream, String path, String temporaryPath) {
-      long seed = Math.abs(new Random().nextLong());
-      mStream = stream;
-      mPath = path;
-      mTemporaryPath = temporaryPath;
+    private UnderFileSystemOutputStream(String ufsPath, Configuration conf)
+        throws FileAlreadyExistsException, IOException {
+      mConf = conf;
+      mPath = ufsPath;
+      mTemporaryPath = PathUtils.temporaryFileName(Math.abs(new Random().nextLong()), ufsPath);
+      UnderFileSystem ufs = UnderFileSystem.get(mPath, mConf);
+      if (ufs.exists(ufsPath)) {
+        throw new FileAlreadyExistsException(ExceptionMessage.FAILED_UFS_CREATE.getMessage(ufsPath));
+      }
+      mStream = ufs.create(mTemporaryPath);
     }
 
     public OutputStream getStream() {
       return mStream;
     }
 
-    public String getPath() {
-      return mPath;
+    public void cancel() throws IOException {
+      mStream.close();
+      UnderFileSystem ufs = UnderFileSystem.get(mPath, mConf);
+      ufs.delete(mTemporaryPath, false);
     }
 
-    public String getTemporaryPath() {
-      return mTemporaryPath;
+    public void close() throws IOException {
+      mStream.close();
+      UnderFileSystem ufs = UnderFileSystem.get(mPath, mConf);
+      ufs.rename(mTemporaryPath, mPath);
     }
   }
 
   private AtomicLong mIdGenerator;
-  private ConcurrentMap<Long, TemporaryOutputStream> mStreams;
+  private ConcurrentMap<Long, UnderFileSystemOutputStream> mStreams;
 
   public UnderFileSystemManager() {
+    mIdGenerator = new AtomicLong(new Random().nextLong());
     mStreams = new ConcurrentHashMap<>();
   }
 
   public long createFile(String ufsPath) throws FileAlreadyExistsException, IOException {
-    UnderFileSystem ufs = UnderFileSystem.get(ufsPath, WorkerContext.getConf());
-    if (ufs.exists(ufsPath)) {
-      throw new FileAlreadyExistsException(ExceptionMessage.FAILED_UFS_CREATE.getMessage(ufsPath));
-    }
     long workerFileId = mIdGenerator.getAndIncrement();
-    String tempPath = PathUtils.temporaryFileName(workerFileId, ufsPath);
-    OutputStream os = ufs.create(tempPath);
-    TemporaryOutputStream stream = new TemporaryOutputStream(os, ufsPath, tempPath);
+    UnderFileSystemOutputStream stream =
+        new UnderFileSystemOutputStream(ufsPath, WorkerContext.getConf());
     mStreams.put(workerFileId, stream);
     return workerFileId;
   }
 
   // TODO(calvin): Add the exception to Exception Messages
   public void cancelFile(long workerFileId) throws FileDoesNotExistException, IOException {
-    TemporaryOutputStream stream = mStreams.remove(workerFileId);
+    UnderFileSystemOutputStream stream = mStreams.remove(workerFileId);
     if (stream != null) {
-      stream.getStream().close();
+      stream.cancel();
     } else {
       throw new FileDoesNotExistException("Bad worker file id: " + workerFileId);
     }
-    UnderFileSystem ufs = UnderFileSystem.get(stream.getPath(), WorkerContext.getConf());
-    ufs.delete(stream.getPath(), false);
   }
 
   // TODO(calvin): Add the exception to Exception Messages
   public void completeFile(long workerFileId) throws FileDoesNotExistException, IOException {
-    TemporaryOutputStream stream = mStreams.remove(workerFileId);
+    UnderFileSystemOutputStream stream = mStreams.remove(workerFileId);
     if (stream != null) {
-      stream.getStream().close();
+      stream.close();
     } else {
       throw new FileDoesNotExistException("Bad worker file id: " + workerFileId);
     }
