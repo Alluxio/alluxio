29fe34f5c58bb6968df0d51915ccc68873426958
saltylin <saltylin711@gmail.com>
2016-03-20 22:11:30 +0800

    ALLUXIO-1556
    Modify the BaseKeyValueStoreWriter to prevent adding same key multiple times. A test method is also added.

--- core/common/src/main/java/alluxio/exception/ExceptionMessage.java
+++ core/common/src/main/java/alluxio/exception/ExceptionMessage.java
@@ -160,6 +160,7 @@ public enum ExceptionMessage {
   // key-value
   KEY_VALUE_TOO_LARGE("Unable to put key-value pair: key {0} bytes, value {1} bytes"),
   INVALID_KEY_VALUE_STORE_URI("The URI {0} exists but is not a key-value store"),
+  KEY_ALREADY_EXISTS("The input key already exists in the key-value store"),
 
   // SEMICOLON! minimize merge conflicts by putting it on its own line
   ;
