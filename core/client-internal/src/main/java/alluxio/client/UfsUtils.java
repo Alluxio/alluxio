b4309098dccfa611047565fbd34acce1fe1084b5
Calvin Jia <jia.calvin@gmail.com>
2016-03-23 10:15:50 -0700

    Address comments.

--- core/client-internal/src/main/java/alluxio/client/UfsUtils.java
+++ core/client-internal/src/main/java/alluxio/client/UfsUtils.java
@@ -40,53 +40,60 @@ public final class UfsUtils {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
 
   /**
-   * Mounts the given folder in the under storage system to an Alluxio path which does not exist.
-   * Then loads metadata for each file in the under file system unless excluded by the exclusions
-   * list. If the folder has already been mounted, this method assumes the mount is correct and
-   * will continue to load metadata for each file. This method can be called repeatedly to sync
-   * the latest metadata from the under file system.
+   * Mounts the given folder in the under storage system to an Alluxio path which must not already
+   * exist. Then loads metadata for each file in the under file system unless excluding any file
+   * in the under file system matching a prefix in the exclusions list. If the folder has already
+   * been mounted, this method assumes the mount is correct and will continue to load metadata
+   * for each file. This method can be called repeatedly to load new files written directly to
+   * the under file system. File deletions will not be reflected, ie. deleting a file, foo, in
+   * the under file system and then running loadUfs will not delete the file foo in Alluxio.
    *
-   * @param mountPointUri the destination point in Alluxio file system to load the under file system
+   * @param alluxioUri the destination point in Alluxio file system to load the under file system
    *        path onto, this must not already exist
-   * @param mountDirectoryUri the directory in the under file system to mount
-   * @param exclusions prefixes to exclude which will not be registered in the Alluxio file system
+   * @param ufsUri the directory in the under file system to mount
+   * @param exclusions prefixes to exclude which will not be registered in the Alluxio file
+   *                   system, these should be relative to the under file system path and not the
+   *                   Alluxio path
    * @param conf instance of {@link Configuration}
    * @throws IOException if an error occurs when operating on the under file system
    * @throws AlluxioException if an unexpected Alluxio error occurs
+   * @deprecated This utility will be replaced with the mount and recursive load metadata
+   *             operations.
    */
-  public static void loadUfs(AlluxioURI mountPointUri, AlluxioURI mountDirectoryUri,
+  @Deprecated
+  public static void loadUfs(AlluxioURI alluxioUri, AlluxioURI ufsUri,
       PrefixList exclusions, Configuration conf) throws AlluxioException, IOException {
     FileSystem fs = FileSystem.Factory.get();
-    LOG.info("Mounting: {} to Alluxio directory: {}, excluding: {}", mountDirectoryUri,
-        mountPointUri, exclusions);
+    LOG.info("Mounting: {} to Alluxio directory: {}, excluding: {}", ufsUri,
+        alluxioUri, exclusions);
 
-    UnderFileSystem ufs = UnderFileSystem.get(mountDirectoryUri.toString(), conf);
+    UnderFileSystem ufs = UnderFileSystem.get(ufsUri.toString(), conf);
     // The mount directory must exist in the UFS
-    String mountDirectoryPath = mountDirectoryUri.getPath();
+    String mountDirectoryPath = ufsUri.getPath();
     if (!ufs.exists(mountDirectoryPath)) {
       throw new FileDoesNotExistException(
-          ExceptionMessage.UFS_PATH_DOES_NOT_EXIST.getMessage(mountDirectoryUri));
+          ExceptionMessage.UFS_PATH_DOES_NOT_EXIST.getMessage(ufsUri));
     }
     // Mount the ufs directory to the Alluxio mount point
     try {
-      fs.mount(mountPointUri, mountDirectoryUri);
+      fs.mount(alluxioUri, ufsUri);
     } catch (InvalidPathException e) {
       // If the mount point already exists, ignore it and assume the mount is consistent
-      if (!ExceptionMessage.MOUNT_POINT_ALREADY_EXISTS.getMessage(mountPointUri)
+      if (!ExceptionMessage.MOUNT_POINT_ALREADY_EXISTS.getMessage(alluxioUri)
           .equals(e.getMessage())) {
         throw e;
       }
-      LOG.warn("Mount point: " + mountPointUri + " has already been mounted! Assuming the mount "
+      LOG.warn("Mount point: " + alluxioUri + " has already been mounted! Assuming the mount "
           + "is consistent and proceeding to load metadata.");
     }
     // Get the list of files to load
     List<String> files = Arrays.asList(ufs.listRecursive(mountDirectoryPath));
     for (String file : files) {
-      LOG.info("Considering file: " + file);
+      LOG.debug("Considering ufs path: " + file);
       // Not excluded
       if (exclusions.outList(file)) {
-        AlluxioURI alluxioUri = mountPointUri.join(file);
-        LOG.info("Loading metadata for file: " + alluxioUri);
+        AlluxioURI alluxioUriToLoad = alluxioUri.join(file);
+        LOG.debug("Loading metadata for Alluxio uri: " + alluxioUriToLoad);
         // TODO(calvin): Remove the need for this hack
         AlluxioURI alluxioPath = new AlluxioURI(alluxioUri.getPath());
         if (!fs.exists(alluxioPath)) {
@@ -98,14 +105,16 @@ public final class UfsUtils {
 
   /**
    * Starts the command line utility to mount the UfsPath to the AlluxioPath. In addition to the
-   * regular mount operation, the metadata for all files will be loaded, unless they match the
-   * exclusions prefix list. This utility can be called again on an already mounted directory to
-   * sync with the under file system.
+   * regular mount operation, the metadata for all files will be loaded, unless the ufs path matches
+   * an exclusion in the exclusions prefix list. This utility can be called repeatedly to load new
+   * files written directly to the under file system. File deletions will not be reflected, ie.
+   * deleting a file, foo, in the under file system and then running loadUfs will not delete the
+   * file foo in Alluxio.
    *
    * NOTE: The user must guarantee additional calls to the utility do not change the Alluxio/Ufs
    * Path pairing.
    *
-   * @param args the parameters as <AlluxioPath> <UfsPath> [<Optional ExcludePathPrefix, seperated
+   * @param args the parameters as <AlluxioUri> <UfsUri> [<Optional ExcludePathPrefix, seperated
    *             by ;>]
    */
   public static void main(String[] args) {
@@ -134,7 +143,7 @@ public final class UfsUtils {
   public static void printUsage() {
     String cmd = "java -cp " + Version.ALLUXIO_JAR + " alluxio.client.UfsUtils ";
 
-    System.out.println("Usage: " + cmd + "<AlluxioPath> <UfsPath> "
+    System.out.println("Usage: " + cmd + "<AlluxioUri> <UfsUri> "
         + "[<Optional ExcludePathPrefix, separated by ;>]");
     System.out.println("Example: " + cmd + "alluxio://127.0.0.1:19998/a hdfs://localhost:9000/b c");
     System.out.println("Example: " + cmd + "alluxio://127.0.0.1:19998/a file:///b c");
