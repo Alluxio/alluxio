ee77e64ce950b281b1396640d774657979caa3f3
Calvin Jia <jia.calvin@gmail.com>
2016-03-24 15:34:43 -0700

    Clarify javadoc.

--- core/server/src/main/java/alluxio/worker/block/BlockWorker.java
+++ core/server/src/main/java/alluxio/worker/block/BlockWorker.java
@@ -634,8 +634,9 @@ public final class BlockWorker extends AbstractWorker {
 
   /**
    * Creates a file to represent a block denoted by the given block path. This file will be owned
-   * by the Alluxio worker but have 777 permissions so clients can read and write to the file
-   * without going through the worker. Only the worker can delete or rename this file.
+   * by the Alluxio worker but have 777 permissions so processes under users different from the
+   * user that launched the Alluxio worker can read and write to the file. The tiered storage
+   * directory has the sticky bit so only the worker user can delete or rename files it creates.
    *
    * @param blockPath the block path to create
    * @throws IOException if the file cannot be created in the tiered storage folder
