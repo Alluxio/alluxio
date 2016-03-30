2a70392661e28741c694756dc3ae7a56fc67b064
Dong Chen <dong1.chen@intel.com>
2016-03-30 05:08:55 -0400

    Change thrown exception

--- core/server/src/main/java/alluxio/master/file/meta/InodeTree.java
+++ core/server/src/main/java/alluxio/master/file/meta/InodeTree.java
@@ -14,6 +14,7 @@ package alluxio.master.file.meta;
 import alluxio.AlluxioURI;
 import alluxio.Constants;
 import alluxio.collections.IndexedSet;
+import alluxio.exception.AccessControlException;
 import alluxio.exception.BlockInfoException;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.FileAlreadyExistsException;
@@ -500,9 +501,9 @@ public final class InodeTree implements JournalCheckpointStreamable {
    * represents the root inode, the tree is "reset", and all state is cleared.
    *
    * @param entry the journal entry representing an inode
-   * @throws IOException when owner of mRoot is not the owner of root journal entry
+   * @throws AccessControlException when owner of mRoot is not the owner of root journal entry
    */
-  public void addInodeFromJournal(JournalEntry entry) throws IOException {
+  public void addInodeFromJournal(JournalEntry entry) throws AccessControlException {
     Message innerEntry = JournalProtoUtils.unwrap(entry);
     if (innerEntry instanceof InodeFileEntry) {
       InodeFile file = InodeFile.fromJournalEntry((InodeFileEntry) innerEntry);
@@ -515,7 +516,7 @@ public final class InodeTree implements JournalCheckpointStreamable {
         if (SecurityUtils.isSecurityEnabled(MasterContext.getConf())
             && mRoot != null && !mRoot.getUserName().equals(directory.getUserName())) {
           // user is not the owner of journal root entry
-          throw new IOException(
+          throw new AccessControlException(
               ExceptionMessage.PERMISSION_DENIED.getMessage("Unauthorized user on root"));
         }
         mInodes.clear();
