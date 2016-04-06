171b51033cad67eb4eb74de13fd1a98877d94f40
Bin Fan <fanbin103@gmail.com>
2016-04-06 09:57:29 -0700

    Remove unnecessary type cast

--- core/server/src/main/java/alluxio/master/file/FileSystemMaster.java
+++ core/server/src/main/java/alluxio/master/file/FileSystemMaster.java
@@ -698,13 +698,13 @@ public final class FileSystemMaster extends AbstractMaster {
   public long getNewBlockIdForFile(AlluxioURI path)
       throws FileDoesNotExistException, InvalidPathException, AccessControlException {
     MasterContext.getMasterSource().incGetNewBlockOps(1);
-    Inode<?> inode;
+    InodeFile inode;
     synchronized (mInodeTree) {
       mPermissionChecker.checkPermission(FileSystemAction.WRITE, path);
       inode = mInodeTree.getInodeFileByPath(path);
     }
     MasterContext.getMasterSource().incNewBlocksGot(1);
-    return ((InodeFile) inode).getNewBlockId();
+    return inode.getNewBlockId();
   }
 
   /**
