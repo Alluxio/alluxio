54f758df36847761fdb2310188368bc796db16e5
Pei Sun <peis@alluxio.com>
2016-04-07 15:48:39 -0700

    allow NoMount in local mode

--- bin/alluxio-start.sh
+++ bin/alluxio-start.sh
@@ -212,6 +212,16 @@ case "${WHAT}" in
       stop ${BIN}
       sleep 1
     fi
+    if [[ $2 != "NoMount" ]]; then
+      ${LAUNCHER} ${BIN}/alluxio-mount.sh SudoMount
+      stat=$?
+      if [[ ${stat} -ne 0 ]]; the
+        echo "Mount failed, not starting"
+        exit 1
+      fi
+    else
+      shift
+    fi
     if [ ! -z $2 ] && [ $2 != "-f" ]; then
       echo -e "${USAGE}"
       exit 1
