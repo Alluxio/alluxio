b448adcd7ee9a5d3df19088c3bcdcac3746f79c9
Chaomin Yu <ifcharming@gmail.com>
2016-04-08 11:21:10 -0700

    [ALLUXIO-1824] Improve config_alluxio env setting.

--- deploy/vagrant/provision/roles/ufs_gcs/files/config_alluxio.sh
+++ deploy/vagrant/provision/roles/ufs_gcs/files/config_alluxio.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sed -i "s|^export ALLUXIO_UNDERFS_ADDRESS=.*|export ALLUXIO_UNDERFS_ADDRESS=gs://${GCS_BUCKET}|g" /alluxio/conf/alluxio-env.sh
+export ALLUXIO_UNDERFS_ADDRESS="gs://${GCS_BUCKET}"
 
-sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
-  -Dfs.gcs.secretAccessKey=${GCS_KEY}" /alluxio/conf/alluxio-env.sh
-
-sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
-  -Dfs.gcs.accessKeyId=${GCS_ID}" /alluxio/conf/alluxio-env.sh
+export ALLUXIO_JAVA_OPTS+="
+  -Dfs.gcs.accessKeyId=${GCS_ID}
+  -Dfs.gcs.secretAccessKey=${GCS_KEY}
+"
--- deploy/vagrant/provision/roles/ufs_s3/files/config_alluxio.sh
+++ deploy/vagrant/provision/roles/ufs_s3/files/config_alluxio.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-sed -i "s|^export ALLUXIO_UNDERFS_ADDRESS=.*|export ALLUXIO_UNDERFS_ADDRESS=s3n://${S3_BUCKET}|g" /alluxio/conf/alluxio-env.sh
+export ALLUXIO_UNDERFS_ADDRESS="s3n://${S3_BUCKET}"
 
-sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
-  -Dfs.s3n.awsSecretAccessKey=${S3_KEY}" /alluxio/conf/alluxio-env.sh
-
-sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
-  -Dfs.s3n.awsAccessKeyId=${S3_ID}" /alluxio/conf/alluxio-env.sh
+export ALLUXIO_JAVA_OPTS+="
+  -Dfs.s3n.awsSecretAccessKey=${S3_KEY}
+  -Dfs.s3n.awsAccessKeyId=${S3_ID}
+"
 
 # For Alluxio version earlier than 0.8, remove schema "s3n" from default prefixes to be handled by HdfsUnderFileSystem.
 # This property is changed to "alluxio.underfs.hdfs.prefixes" after version 0.8 and s3n is not included by default.
@@ -14,6 +13,5 @@ PREFIXES=$(grep alluxio.underfs.hadoop.prefixes /alluxio/common/src/main/resourc
 if [[ "$PREFIXES" != "" ]]; then
   PREFIXES=$(echo ${PREFIXES} | sed -i "s|s3n://,||g")
   # After this change, only S3UnderFileSystem will support s3n://
-  sed -i "/export ALLUXIO_JAVA_OPTS+=\"/ a\
-  -D${PREFIXES}" /alluxio/conf/alluxio-env.sh
+  export ALLUXIO_JAVA_OPTS+="-D${PREFIXES}"
 fi
