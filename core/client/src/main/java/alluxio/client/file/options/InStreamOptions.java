6586b6ae0a8cb0660e219a8b2b32ca75a218ddb7
Pei Sun <peis@alluxio.com>
2016-04-26 16:33:43 -0700

    Fix InStreamOptionsTest failure

--- core/client/src/main/java/alluxio/client/file/options/InStreamOptions.java
+++ core/client/src/main/java/alluxio/client/file/options/InStreamOptions.java
@@ -117,18 +117,19 @@ public final class InStreamOptions {
       return false;
     }
     InStreamOptions that = (InStreamOptions) o;
-    return Objects.equal(mLocationPolicy, that.mLocationPolicy) && Objects
-        .equal(mReadType, that.mReadType);
+    return Objects.equal(mLocationPolicy, that.mLocationPolicy) &&
+        Objects.equal(mReadType, that.mReadType) &&
+        Objects.equal(mCacheIncompleteBlock, that.mCacheIncompleteBlock);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(mLocationPolicy, mReadType);
+    return Objects.hashCode(mLocationPolicy, mReadType, mCacheIncompleteBlock);
   }
 
   @Override
   public String toString() {
     return Objects.toStringHelper(this).add("locationPolicy", mLocationPolicy)
-        .add("readType", mReadType).toString();
+        .add("readType", mReadType).add("cacheIncompleteBlock", mCacheIncompleteBlock).toString();
   }
 }
