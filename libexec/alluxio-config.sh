db09db0e2a917de7bd39910efaf03ffe837254ba
Bin Fan <fanbin103@gmail.com>
2016-04-27 17:38:12 -0700

    [ALLUXIO-1909] Futher update on the scripts

--- libexec/alluxio-config.sh
+++ libexec/alluxio-config.sh
@@ -16,25 +16,23 @@ config_bin=$(cd "$config_bin"; pwd)
 this="$config_bin/$script"
 
 # Allow for a script which overrides the default settings for system integration folks.
-# [ -f "$common_bin/alluxio-layout.sh" ] && . "$common_bin/alluxio-layout.sh"
+[ -f "$common_bin/alluxio-layout.sh" ] && . "$common_bin/alluxio-layout.sh"
 
 # This will set the default installation for a tarball installation while os distributors can create
 # their own alluxio-layout.sh file to set system installation locations.
 if [[ -z "$ALLUXIO_SYSTEM_INSTALLATION" ]]; then
   VERSION=1.1.0-SNAPSHOT
-  ALLUXIO_PREFIX=$(dirname $(dirname "$this"))
-  ALLUXIO_HOME=${ALLUXIO_PREFIX}
-  ALLUXIO_CONF_DIR="$ALLUXIO_HOME/conf"
-  ALLUXIO_JARS="$ALLUXIO_HOME/assembly/target/alluxio-assemblies-${VERSION}-jar-with-dependencies.jar"
+  ALLUXIO_HOME=$(dirname $(dirname "$this"))
+  ALLUXIO_CONF_DIR="${ALLUXIO_HOME}/conf"
+  ALLUXIO_JARS="${ALLUXIO_HOME}/assembly/target/alluxio-assemblies-${VERSION}-jar-with-dependencies.jar"
 fi
 
-export JAVA_HOME=${JAVA_HOME:-"$(dirname $(which java))/../.."}
-export JAVA=${JAVA:-"${JAVA_HOME}/bin/java"}
+JAVA_HOME=${JAVA_HOME:-"$(dirname $(which java))/../.."}
+JAVA=${JAVA:-"${JAVA_HOME}/bin/java"}
 
 if [[ $(uname -s) == Darwin ]]; then
   # Assuming Mac OS X
   export ALLUXIO_RAM_FOLDER=${ALLUXIO_RAM_FOLDER:-"/Volumes/ramdisk"}
-  export ALLUXIO_JAVA_OPTS+="-Djava.security.krb5.realm= -Djava.security.krb5.kdc="
 else
   # Assuming Linux
   export ALLUXIO_RAM_FOLDER=${ALLUXIO_RAM_FOLDER:-"/mnt/ramdisk"}
@@ -50,37 +48,39 @@ fi
 
 . $ALLUXIO_CONF_DIR/alluxio-env.sh
 
-if [[ -z "${ALLUXIO_HOME}" ]]; then
+if [[ -n "${ALLUXIO_HOME}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.home=${ALLUXIO_HOME}"
 fi
 
-if [[ -z "${ALLUXIO_LOGS_DIR}" ]]; then
+if [[ -n "${ALLUXIO_LOGS_DIR}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.logs.dir=${ALLUXIO_LOGS_DIR}"
 fi
 
-if [[ -z "${ALLUXIO_RAM_FOLDER}" ]]; then
+if [[ -n "${ALLUXIO_RAM_FOLDER}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.worker.tieredstore.level0.dirs.path=${ALLUXIO_RAM_FOLDER}"
 fi
 
-if [[ -z "${ALLUXIO_WORKER_MEMORY_SIZE}" ]]; then
+if [[ -n "${ALLUXIO_WORKER_MEMORY_SIZE}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.worker.tieredstore.level0.dirs.quota=${ALLUXIO_WORKER_MEMORY_SIZE}"
 fi
 
-if [[ -z "${ALLUXIO_MASTER_ADDRESS}" ]]; then
+if [[ -n "${ALLUXIO_MASTER_ADDRESS}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.master.hostname=${ALLUXIO_MASTER_ADDRESS}"
 fi
 
-if [[ -z "${ALLUXIO_UNDERFS_ADDRESS}" ]]; then
+if [[ -n "${ALLUXIO_UNDERFS_ADDRESS}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.underfs.address=${ALLUXIO_UNDERFS_ADDRESS}"
 fi
 
-if [[ -z "${ALLUXIO_WORKER_MEMORY_SIZES}" ]]; then
+if [[ -n "${ALLUXIO_WORKER_MEMORY_SIZES}" ]]; then
   ALLUXIO_JAVA_OPTS+=" -Dalluxio.worker.memory.size=${ALLUXIO_WORKER_MEMORY_SIZE}"
 fi
 
 ALLUXIO_JAVA_OPTS+=" -Dlog4j.configuration=file:${ALLUXIO_CONF_DIR}/log4j.properties"
 ALLUXIO_JAVA_OPTS+=" -Dorg.apache.jasper.compiler.disablejsr199=true"
 ALLUXIO_JAVA_OPTS+=" -Djava.net.preferIPv4Stack=true"
+ALLUXIO_JAVA_OPTS+=" -Djava.security.krb5.realm="
+ALLUXIO_JAVA_OPTS+=" -Djava.security.krb5.kdc="
 
 # Master specific parameters based on ALLUXIO_JAVA_OPTS.
 ALLUXIO_MASTER_JAVA_OPTS=${ALLUXIO_JAVA_OPTS}
