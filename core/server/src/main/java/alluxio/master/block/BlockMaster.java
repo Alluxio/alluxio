4b0a3e56b9047187838a444843a4181186a93039
Gene Pang <gene.pang@gmail.com>
2016-04-29 07:59:59 -0700

    Update blockmaster deleteBlocks for fine-grain locking

--- core/server/src/main/java/alluxio/master/block/BlockMaster.java
+++ core/server/src/main/java/alluxio/master/block/BlockMaster.java
@@ -308,28 +308,34 @@ public final class BlockMaster extends AbstractMaster implements ContainerIdGene
    * @param delete whether to delete blocks metadata in Master
    */
   public void removeBlocks(List<Long> blockIds, boolean delete) {
-    synchronized (mBlocks) {
-      synchronized (mWorkers) {
-        for (long blockId : blockIds) {
-          MasterBlockInfo masterBlockInfo = mBlocks.get(blockId);
-          if (masterBlockInfo == null) {
-            continue;
-          }
-          for (long workerId : new ArrayList<Long>(masterBlockInfo.getWorkers())) {
-            MasterWorkerInfo workerInfo = mWorkers.getFirstByField(mIdIndex, workerId);
-            if (workerInfo != null) {
-              workerInfo.updateToRemovedBlock(true, blockId);
-            }
-          }
-          // Two cases here:
-          // 1) For delete: delete the block metadata.
-          // 2) For free: keep the block metadata. mLostBlocks will be changed in
-          // processWorkerRemovedBlocks
-          if (delete) {
-            // Make sure blockId is removed from mLostBlocks when the block metadata is deleted.
-            // Otherwise blockId in mLostBlock can be dangling index if the metadata is gone.
-            mLostBlocks.remove(blockId);
-            mBlocks.remove(blockId);
+    HashSet<Long> workerIds = new HashSet<>();
+    for (long blockId : blockIds) {
+      MasterBlockInfo masterBlockInfo = mBlocks.get(blockId);
+      if (masterBlockInfo == null) {
+        continue;
+      }
+      workerIds.clear();
+      synchronized (masterBlockInfo) {
+        workerIds.addAll(masterBlockInfo.getWorkers());
+        // Two cases here:
+        // 1) For delete: delete the block metadata.
+        // 2) For free: keep the block metadata. mLostBlocks will be changed in
+        // processWorkerRemovedBlocks
+        if (delete) {
+          // Make sure blockId is removed from mLostBlocks when the block metadata is deleted.
+          // Otherwise blockId in mLostBlock can be dangling index if the metadata is gone.
+          mLostBlocks.remove(blockId);
+          mBlocks.remove(blockId);
+        }
+      }
+
+      // Outside of locking the block. This does not have to be synchronized with the block
+      // metadata, since it is essentially an asynchronous signal to the worker to remove the block.
+      for (long workerId : workerIds) {
+        MasterWorkerInfo workerInfo = mWorkers.getFirstByField(mIdIndex, workerId);
+        if (workerInfo != null) {
+          synchronized (workerInfo) {
+            workerInfo.updateToRemovedBlock(true, blockId);
           }
         }
       }
