dc80d88382a4124a0c329de08ca527e537f2af38
Pei Sun <peis@alluxio.com>
2016-05-03 16:15:00 -0700

    Remove the read to close

--- core/client/src/main/java/alluxio/client/file/FileInStream.java
+++ core/client/src/main/java/alluxio/client/file/FileInStream.java
@@ -133,6 +133,9 @@ public class FileInStream extends InputStream implements BoundedStream, Seekable
     if (mClosed) {
       return;
     }
+    if (mShouldCacheCurrentBlock && mShouldCacheIncompleteBlock) {
+      readCurrentBlockTill(mFileLength);
+    }
     if (mCurrentBlockInStream != null) {
       mCurrentBlockInStream.close();
     }
@@ -392,9 +395,6 @@ public class FileInStream extends InputStream implements BoundedStream, Seekable
       return;
     }
     if (shouldCloseCacheStream()) {
-      mCurrentCacheStream.close();
-    } else if (mShouldCacheCurrentBlock && mShouldCacheIncompleteBlock) {
-      readCurrentBlockTill(mFileLength);
       mCurrentCacheStream.close();
     } else {
       mCurrentCacheStream.cancel();
