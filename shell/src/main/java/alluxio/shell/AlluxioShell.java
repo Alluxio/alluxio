22b3887077a692ca280fbdc6ac761191bb1b8a47
Bin Fan <fanbin103@gmail.com>
2016-05-03 23:53:35 -0700

    [ALLUXIO-1910] Code cleanup

--- shell/src/main/java/alluxio/shell/AlluxioShell.java
+++ shell/src/main/java/alluxio/shell/AlluxioShell.java
@@ -41,12 +41,9 @@ import javax.annotation.concurrent.NotThreadSafe;
  * Class for handling command line inputs.
  */
 @NotThreadSafe
-public class AlluxioShell implements Closeable {
+public final class AlluxioShell implements Closeable {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
   private static final Map<String, String[]> CMD_ALIAS = ImmutableMap.<String, String[]>builder()
-      .put("chgrpr", new String[] {"chgrp", "-R"})
-      .put("chmodr", new String[] {"chmod", "-R"})
-      .put("chownr", new String[] {"chown", "-R"})
       .put("lsr", new String[] {"ls", "-R"})
       .put("rmr", new String[] {"rm", "-R"})
       .build();
