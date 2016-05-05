ae9ce3b9b3b5d167c21861da10cb10c98bcd2454
Jan Hentschel <jan.hentschel@ultratendency.com>
2016-05-05 14:24:31 +0200

    [SMALLFIX] Removed explicit parameter types in ReplayCache

--- core/common/src/main/java/alluxio/replay/ReplayCache.java
+++ core/common/src/main/java/alluxio/replay/ReplayCache.java
@@ -85,7 +85,7 @@ public final class ReplayCache<V> {
    * @return a cache with the specified maximum size and expiration time
    */
   public static <V> ReplayCache<V> newInstance(long maxSize, long expireMs) {
-    return new ReplayCache<V>(maxSize, expireMs);
+    return new ReplayCache<>(maxSize, expireMs);
   }
 
   /**
@@ -98,7 +98,7 @@ public final class ReplayCache<V> {
     // TODO(andrew) Make it possible for users to configure this cache via
     // CacheBuilder<Object, Object> from(String spec)
     mCache = CacheBuilder.newBuilder().maximumSize(maxSize)
-        .expireAfterWrite(expireMs, TimeUnit.MILLISECONDS).<String, V>build();
+        .expireAfterWrite(expireMs, TimeUnit.MILLISECONDS).build();
   }
 
   /**
