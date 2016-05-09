d918b24c6ffd7113eeeec8c18781fd6e9eaf8073
Bin Fan <fanbin103@gmail.com>
2016-05-09 13:47:11 -0700

    [ALLUXIO-1910] Move AlluxioShell entry to cli package

--- shell/src/main/java/alluxio/cli/AlluxioShell.java
+++ shell/src/main/java/alluxio/cli/AlluxioShell.java
@@ -9,7 +9,7 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
-package alluxio.shell;
+package alluxio.cli;
 
 import alluxio.Configuration;
 import alluxio.Constants;
