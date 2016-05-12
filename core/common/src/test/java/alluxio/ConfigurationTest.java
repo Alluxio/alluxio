58fb7406bdac73d16efd7aa263cb79907b56803e
Bin Fan <fanbin103@gmail.com>
2016-05-12 14:54:14 -0700

    Update ConfigurationTest.java

--- core/common/src/test/java/alluxio/ConfigurationTest.java
+++ core/common/src/test/java/alluxio/ConfigurationTest.java
@@ -130,7 +130,7 @@ public class ConfigurationTest {
     Assert.assertEquals(Constants.MB * 8, longBytesValue);
 
     longBytesValue = sDefaultConfiguration.getBytes(Constants.NETWORK_THRIFT_FRAME_SIZE_BYTES_MAX);
-    Assert.assertEquals(Constants.MB * 64, longBytesValue);
+    Assert.assertEquals(Constants.MB * 16, longBytesValue);
 
     int maxTry = sDefaultConfiguration.getInt(Constants.ZOOKEEPER_LEADER_INQUIRY_RETRY_COUNT);
     Assert.assertEquals(10, maxTry);
