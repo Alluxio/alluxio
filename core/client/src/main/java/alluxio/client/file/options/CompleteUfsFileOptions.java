e391e188dd2a86af4c1ea6efc7b583f6b8c886ec
Calvin Jia <jia.calvin@gmail.com>
2016-05-15 12:16:07 -0700

    Options boilerplate.

---
+++ core/client/src/main/java/alluxio/client/file/options/CompleteUfsFileOptions.java
@@ -0,0 +1,48 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the “License”). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.client.file.options;
+
+import alluxio.thrift.CompleteUfsFileTOptions;
+
+import com.google.common.base.Objects;
+
+public class CompleteUfsFileOptions {
+  /**
+   * @return the default {@link CompleteUfsFileOptions}
+   */
+  public static CompleteUfsFileOptions defaults() {
+    return new CompleteUfsFileOptions();
+  }
+
+  private CompleteUfsFileOptions() {
+    // No options currently
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    return this == o || o instanceof CompleteUfsFileOptions;
+  }
+
+  @Override
+  public int hashCode() {
+    return 0;
+  }
+
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this).toString();
+  }
+
+  public CompleteUfsFileTOptions toThrift() {
+    return new CompleteUfsFileTOptions();
+  }
+}
