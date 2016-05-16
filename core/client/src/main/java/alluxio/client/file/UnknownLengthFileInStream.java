3b1d0f39d98aabfdf53e2f6862e135ae878d982e
Calvin Jia <jia.calvin@gmail.com>
2016-05-16 04:23:53 -0700

    Switch on using the direct or delegated input streams.

--- core/client/src/main/java/alluxio/client/file/UnknownLengthFileInStream.java
+++ core/client/src/main/java/alluxio/client/file/UnknownLengthFileInStream.java
@@ -14,8 +14,10 @@ package alluxio.client.file;
 import alluxio.AlluxioURI;
 import alluxio.Constants;
 import alluxio.annotation.PublicApi;
+import alluxio.client.ClientContext;
 import alluxio.client.block.BlockInStream;
-import alluxio.client.block.UnderStoreBlockInStream;
+import alluxio.client.block.DelegatedUnderStoreBlockInStream;
+import alluxio.client.block.DirectUnderStoreBlockInStream;
 import alluxio.client.file.options.CompleteFileOptions;
 import alluxio.client.file.options.InStreamOptions;
 import alluxio.exception.AlluxioException;
@@ -103,7 +105,12 @@ public final class UnknownLengthFileInStream extends FileInStream {
   @Override
   protected BlockInStream createUnderStoreBlockInStream(long blockStart, long length, String path)
       throws IOException {
-    return new UnderStoreBlockInStream(blockStart, Constants.UNKNOWN_SIZE, length, path);
+    boolean delegate = ClientContext.getConf().getBoolean(Constants.USER_UFS_OPERATION_DELEGATION);
+    if (delegate) {
+      return new DelegatedUnderStoreBlockInStream(blockStart, Constants.UNKNOWN_SIZE, length, path);
+    } else {
+      return new DirectUnderStoreBlockInStream(blockStart, Constants.UNKNOWN_SIZE, length, path);
+    }
   }
 
   @Override
