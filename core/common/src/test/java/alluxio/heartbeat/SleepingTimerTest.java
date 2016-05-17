0da7a744c16d8a29e62b6e50a93a6fb0d80c6700
shayne <shayneqi@gmail.com>
2016-05-17 16:30:21 -0700

    formatting

--- core/common/src/test/java/alluxio/heartbeat/SleepingTimerTest.java
+++ core/common/src/test/java/alluxio/heartbeat/SleepingTimerTest.java
@@ -1,6 +1,5 @@
 package alluxio.heartbeat;
 
-import alluxio.util.CommonUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,55 +9,57 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 import org.slf4j.Logger;
 
+import alluxio.util.CommonUtils;
 
 /**
  * Created by shayne on 5/17/16.
  *
  * Unit tests for {@link SleepingTimer}.
  *
- * 1. Sleep more than the interval of SleepingTimer and see if the SleepingTimer warns correctly
- * 2. Tick continuously for several time and see if the time interval is correct
- * 3. Sleep less than the interval of SleepingTimer and see if the time interval is correct
+ * 1. Sleep more than the interval of SleepingTimer and see if the SleepingTimer warns correctly 2.
+ * Tick continuously for several time and see if the time interval is correct 3. Sleep less than the
+ * interval of SleepingTimer and see if the time interval is correct
  */
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({SleepingTimer.class})
 public class SleepingTimerTest {
-    private static final String THREAD_NAME = "sleepingtimer-test-thread-name";
-    private static final long INTERVAL_MS = 500;
+  private static final String THREAD_NAME = "sleepingtimer-test-thread-name";
+  private static final long INTERVAL_MS = 500;
 
-    @Test
-    public void test() throws Exception {
-        SleepingTimer stimer =new SleepingTimer(THREAD_NAME, INTERVAL_MS);
+  @Test
+  public void test() throws Exception {
+    SleepingTimer stimer = new SleepingTimer(THREAD_NAME, INTERVAL_MS);
 
-        Logger logger= Mockito.mock(Logger.class);
-        long PreTickMs = 0;
+    Logger logger = Mockito.mock(Logger.class);
+    long PreTickMs = 0;
 
-        Whitebox.setInternalState(SleepingTimer.class,"LOG",logger);
+    Whitebox.setInternalState(SleepingTimer.class, "LOG", logger);
 
-        Whitebox.setInternalState(stimer,"mPreviousTickMs",PreTickMs);
-
-
-        stimer.tick();
-        CommonUtils.sleepMs(5 * INTERVAL_MS);
-        stimer.tick();
-
-        Mockito.verify(logger).warn(Mockito.anyString(), Mockito.anyString(), Mockito.anyLong(), Mockito.anyLong());
-
-        long timeBeforeMs = PreTickMs;
-        stimer.tick();
-        stimer.tick();
-        stimer.tick();
-        long timeIntervalMs = System.currentTimeMillis() - timeBeforeMs;
-        Assert.assertTrue(timeIntervalMs >= 3 * INTERVAL_MS);
+    Whitebox.setInternalState(stimer, "mPreviousTickMs", PreTickMs);
 
 
+    stimer.tick();
+    CommonUtils.sleepMs(5 * INTERVAL_MS);
+    stimer.tick();
 
-        timeBeforeMs = PreTickMs;
-        CommonUtils.sleepMs(INTERVAL_MS / 2);
-        stimer.tick();
-        timeIntervalMs = System.currentTimeMillis() - timeBeforeMs;
-        Assert.assertTrue(timeIntervalMs >= INTERVAL_MS);
+    Mockito.verify(logger).warn(Mockito.anyString(), Mockito.anyString(), Mockito.anyLong(),
+        Mockito.anyLong());
 
-    }
+    long timeBeforeMs = PreTickMs;
+    stimer.tick();
+    stimer.tick();
+    stimer.tick();
+    long timeIntervalMs = System.currentTimeMillis() - timeBeforeMs;
+    Assert.assertTrue(timeIntervalMs >= 3 * INTERVAL_MS);
+
+
+
+    timeBeforeMs = PreTickMs;
+    CommonUtils.sleepMs(INTERVAL_MS / 2);
+    stimer.tick();
+    timeIntervalMs = System.currentTimeMillis() - timeBeforeMs;
+    Assert.assertTrue(timeIntervalMs >= INTERVAL_MS);
+
+  }
 }
