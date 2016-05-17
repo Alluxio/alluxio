9d7de0112b6aa8bbe9d8a5b0f83e4c347cbf0424
TeddyBear1314 <huangzhi19921023@sina.com>
2016-05-17 13:01:08 -0400

    SMALLFIX

--- core/common/src/main/java/alluxio/util/io/PathUtils.java
+++ core/common/src/main/java/alluxio/util/io/PathUtils.java
@@ -228,7 +228,7 @@ public final class PathUtils {
   }
 
   /**
-   * Adds a trailing separator if it does not exists in path.
+   * Adds a trailing separator if it does not exist in path.
    *
    * @param path the file name
    * @param separator trailing separator to add
