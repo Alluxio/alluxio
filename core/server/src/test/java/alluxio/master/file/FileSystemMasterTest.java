8707bd3052e4a21c86a96c03929dd8c834d78cbc
Pei Sun <peis@alluxio.com>
2016-05-17 12:36:10 -0700

    Add error msg

--- core/server/src/test/java/alluxio/master/file/FileSystemMasterTest.java
+++ core/server/src/test/java/alluxio/master/file/FileSystemMasterTest.java
@@ -742,7 +742,7 @@ public final class FileSystemMasterTest {
     try {
       mFileSystemMaster
           .createDirectory(new AlluxioURI("alluxio:/a"), CreateDirectoryOptions.defaults());
-      Assert.fail();
+      Assert.fail("createDirectory was expected to fail with FileAlreadyExistsException");
     } catch (FileAlreadyExistsException e) {
     }
 
@@ -753,7 +753,7 @@ public final class FileSystemMasterTest {
     // TODO(peis): Avoid this hack by adding an option in getFileInfo to skip loading metadata.
     try {
       mFileSystemMaster.createFile(new AlluxioURI("alluxio:/a/f"), CreateFileOptions.defaults());
-      Assert.fail();
+      Assert.fail("createDirectory was expected to fail with FileAlreadyExistsException");
     } catch (FileAlreadyExistsException e) {
     }
   }
