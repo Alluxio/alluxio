2ffd8f9f20716a1094726eac4c1f7401eefa2369
Jiri Simsa <jiri.simsa@gmail.com>
2016-05-21 11:59:15 -0700

    Increasing license check coverage.

--- integration/bin/alluxio-yarn-setup.sh
+++ integration/bin/alluxio-yarn-setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+# (the “License”). You may not use this work except in compliance with the License, which is
+# available at www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied, as more fully set forth in the License.
+#
+# See the NOTICE file distributed with this work for information regarding copyright ownership.
+#
+
 
 # prepare Alluxio dir structure for a YARN container to launch master or worker
 # the name of this tar file is referenced in code
