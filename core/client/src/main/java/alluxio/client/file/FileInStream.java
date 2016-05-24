9089b35ac035b09a824d151ede4f180b0b77928f
Pei Sun <peis@alluxio.com>
2016-05-24 16:42:51 -0700

    Change a logging level from warn to info

--- core/client/src/main/java/alluxio/client/file/FileInStream.java
+++ core/client/src/main/java/alluxio/client/file/FileInStream.java
@@ -358,7 +358,7 @@ public class FileInStream extends InputStream implements BoundedStream, Seekable
         // server unreachable due to network partition, server busy due to alluxio worker is
         // busy, timeout due to congested network etc). But we want to proceed since we want
         // the user to continue reading when one Alluxio worker is having trouble.
-        LOG.warn("Cache stream close or cancel throws IOExecption {}, read continues.",
+        LOG.info("Cache stream close or cancel throws IOExecption {}, read continues.",
             e.getMessage());
       }
     }
