aa5e8a9192bc72a3d9da3d63d6598c7bac84a1fc
Calvin Jia <jia.calvin@gmail.com>
2016-05-26 16:54:52 -0700

    Configurable mutli-part upload threading.

--- core/common/src/main/java/alluxio/Constants.java
+++ core/common/src/main/java/alluxio/Constants.java
@@ -158,6 +158,8 @@ public final class Constants {
       "alluxio.underfs.s3.disable.dns.buckets";
   public static final String UNDERFS_S3_SERVER_SIDE_ENCRYPTION =
       "alluxio.underfs.s3.server.side.encryption";
+  public static final String UNDERFS_S3_MAX_UPLOAD_THREADS =
+      "alluxio.underfs.s3.max.upload.threads";
   public static final String ZOOKEEPER_ENABLED = "alluxio.zookeeper.enabled";
   public static final String ZOOKEEPER_ADDRESS = "alluxio.zookeeper.address";
   public static final String ZOOKEEPER_ELECTION_PATH = "alluxio.zookeeper.election.path";
