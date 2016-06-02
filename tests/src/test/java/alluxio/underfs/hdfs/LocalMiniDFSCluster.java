5af48f76d16784011aef8bc9f25687b03b1d12b5
Jan Hentschel <jan.hentschel@ultratendency.com>
2016-06-02 12:45:54 +0200

    [SMALLFIX] Clean up in underfs and test module

--- tests/src/test/java/alluxio/underfs/hdfs/LocalMiniDFSCluster.java
+++ tests/src/test/java/alluxio/underfs/hdfs/LocalMiniDFSCluster.java
@@ -232,7 +232,7 @@ public class LocalMiniDFSCluster extends UnderFileSystemCluster {
 
       // For HDFS of earlier versions, getFileSystem() returns an instance of type
       // {@link org.apache.hadoop.fs.FileSystem} rather than {@link DistributedFileSystem}
-      mDfsClient = (DistributedFileSystem) mDfsCluster.getFileSystem();
+      mDfsClient = mDfsCluster.getFileSystem();
       mIsStarted = true;
     }
   }
