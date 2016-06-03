9551219f39dfb333e00846b1306b8e523f507bc2
Bin Fan <fanbin103@gmail.com>
2016-06-03 12:44:56 -0700

    [SMALLFIX] Fix style

--- core/server/src/main/java/alluxio/worker/block/TieredBlockStore.java
+++ core/server/src/main/java/alluxio/worker/block/TieredBlockStore.java
@@ -492,8 +492,8 @@ public final class TieredBlockStore implements BlockStore {
 
       try (LockResource r = new LockResource(mMetadataWriteLock)) {
         mMetaManager.commitTempBlockMeta(tempBlockMeta);
-      } catch (BlockAlreadyExistsException | BlockDoesNotExistException |
-          WorkerOutOfSpaceException e) {
+      } catch (BlockAlreadyExistsException | BlockDoesNotExistException
+          | WorkerOutOfSpaceException e) {
         throw Throwables.propagate(e); // we shall never reach here
       }
       return loc;
