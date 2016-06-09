9593ac9397b616b3ff74a246d79410f96a014043
Andrew Audibert <aaudibert10@gmail.com>
2016-06-09 14:08:03 -0700

    Remove environment overriding from integration/bin/common.sh

--- integration/bin/common.sh
+++ integration/bin/common.sh
@@ -16,6 +16,3 @@ DEFAULT_LIBEXEC_DIR="${SCRIPT_DIR}/../../libexec"
 ALLUXIO_LIBEXEC_DIR="${ALLUXIO_LIBEXEC_DIR:-${DEFAULT_LIBEXEC_DIR}}"
 source "${ALLUXIO_LIBEXEC_DIR}/alluxio-config.sh"
 # TODO: Remove for v2.0 release, below line in support of backwards compatibility
-MASTER_HOSTNAME="${ALLUXIO_MASTER_ADDRESS:-localhost}"
-ALLUXIO_MASTER_HOSTNAME="${MASTER_HOSTNAME:-localhost}"
-ALLUXIO_HOME="${ALLUXIO_HOME:-${SCRIPT_DIR}/../..}"
