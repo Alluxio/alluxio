21b7493374350c63b7853754792df21c09df0adb
Jan Hentschel <jan.hentschel@ultratendency.com>
2016-06-10 16:22:19 +0200

    [SMALLFIX] Fixed imports

--- examples/src/main/java/alluxio/cli/JournalCrashTest.java
+++ examples/src/main/java/alluxio/cli/JournalCrashTest.java
@@ -22,7 +22,12 @@ import alluxio.exception.AlluxioException;
 import alluxio.exception.FileAlreadyExistsException;
 import alluxio.util.CommonUtils;
 
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
