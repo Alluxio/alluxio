4478fe43debf964f5141d699ac159b247ca8bb33
xhad1234 <shayneqi@gmail.com>
2016-06-16 16:40:44 -0700

    update according to pr comments

--- examples/src/main/java/alluxio/examples/LoadGenerator.java
+++ examples/src/main/java/alluxio/examples/LoadGenerator.java
@@ -25,9 +25,12 @@ import java.io.IOException;
 import java.util.Random;
 
 /**
- * Create a new Alluxio file system, generating empty files in the root directory with random name.
+ * Creates a new Alluxio file system, generating empty files in the root directory with random name.
  */
 public class LoadGenerator {
+  private static final int FILE_NUMBER = 8000000;
+  private static final int INTERVAL = 1000000;
+
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
 
   private static class RandomString {
@@ -66,36 +69,35 @@ public class LoadGenerator {
   }
 
   /**
-   * Constructor.
+   * Creates a new instance of {@link LoadGenerator}.
    */
   public LoadGenerator() {}
 
   /**
-   * Create files in flat hierarchy and pause according to file number interval.
+   * Creates files in flat hierarchy and pauses according to file number interval.
    *
-   * @param args could provide one parameter of file number
+   * @param args could provide one parameter, specifying the number of files to create
    */
   public static void main(String[] args) {
     AlluxioURI file;
     FileOutStream out;
     CreateFileOptions options = CreateFileOptions.defaults();
-    int filenumber = 8000000;
+    int fileNumber = FILE_NUMBER;
     if (args.length == 1) {
-
-      filenumber = Integer.parseInt(args[0]);
+      fileNumber = Integer.parseInt(args[0]);
     }
 
-    System.out.printf("start, default file number %d%n", filenumber);
+    System.out.printf("start, default file number %d%n", fileNumber);
     FileSystem fs = FileSystem.Factory.get();
     RandomString fileNameGenerator = new RandomString(6);
     try {
-      for (int i = 0; i < filenumber; i++) {
+      for (int i = 0; i < fileNumber; i++) {
         file = new AlluxioURI(fileNameGenerator.nextString() + "A" + Integer.toString(i));
 
         out = fs.createFile(file, options);
         out.close();
 
-        if (i % 500000 == 0) {
+        if (i % INTERVAL == 0) {
           System.out.printf("File Number: %d, Press enter to continue%n", i);
         }
       }
