5e5f42b6960b17cdef494bf001c174f8afe83db2
Chaomin Yu <ifcharming@gmail.com>
2016-06-20 15:59:28 -0700

    Do not checkfail when AUTHORIZED_CLIENT_USER_IS_NULL

--- core/common/src/main/java/alluxio/security/authorization/Permission.java
+++ core/common/src/main/java/alluxio/security/authorization/Permission.java
@@ -143,7 +143,9 @@ public final class Permission {
     }
     // get the username through the authentication mechanism
     User user = AuthenticatedClientUser.get(conf);
-    Preconditions.checkNotNull(user, ExceptionMessage.AUTHORIZED_CLIENT_USER_IS_NULL.getMessage());
+    if (user == null) {
+      throw new IOException(ExceptionMessage.AUTHORIZED_CLIENT_USER_IS_NULL.getMessage());
+    }
     mUserName = user.getName();
     mGroupName = CommonUtils.getPrimaryGroupName(conf, user.getName());
     return this;
