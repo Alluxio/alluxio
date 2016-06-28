da49d6c11e226e5d45bc1c9b63ded535683a86b7
Bin Fan <fanbin103@gmail.com>
2016-06-28 12:59:32 -0700

    [SMALLFIX] Fix a race condition when concurrently creating blocks on worker

--- core/common/src/main/java/alluxio/util/io/FileUtils.java
+++ core/common/src/main/java/alluxio/util/io/FileUtils.java
@@ -218,15 +218,15 @@ public final class FileUtils {
   public static void createStorageDirPath(String path) throws IOException {
     File dir = new File(path);
     String absolutePath = dir.getAbsolutePath();
-    if (!dir.exists()) {
-      if (dir.mkdirs()) {
-        changeLocalFileToFullPermission(absolutePath);
-        setLocalDirStickyBit(absolutePath);
-        LOG.info("Folder {} was created!", path);
-      } else {
-        throw new IOException("Failed to create folder " + path);
-      }
+    if (dir.exists()) {
+      return;
     }
+    if (!dir.mkdirs() || !dir.exists()) {
+      throw new IOException("Failed to create folder " + path);
+    }
+    changeLocalFileToFullPermission(absolutePath);
+    setLocalDirStickyBit(absolutePath);
+    LOG.info("Folder {} was created!", path);
   }
 
   /**
