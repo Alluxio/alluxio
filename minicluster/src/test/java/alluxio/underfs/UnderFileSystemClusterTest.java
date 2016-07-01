e0b1d3d97b8f3675c42f6b79354e40e8a012b321
madanadit <adit@alluxio.com>
2016-06-30 17:31:27 -0700

    UFSCluster.getUnderFSClass returns a non-null value for default LocalFileSystemCluster

--- minicluster/src/test/java/alluxio/underfs/UnderFileSystemClusterTest.java
+++ minicluster/src/test/java/alluxio/underfs/UnderFileSystemClusterTest.java
@@ -64,20 +64,13 @@ public class UnderFileSystemClusterTest {
   }
 
   /**
-   * Tests that the {UnderFileSystemCluster{@link #readEOFReturnsNegativeTest()} method will return
-   * true only when the cluster type is "alluxio.underfs.hdfs.LocalMiniDFSCluster".
+   * Tests that the {@link UnderFileSystemCluster#getUnderFSClass()} method will return
+   * LocalFileSystemCluster by default.
    */
   @Test
-  public void readEOFReturnsNegativeTest() {
-    Whitebox.setInternalState(UnderFileSystemCluster.class, "sUnderFSClass",
-        (String) null);
+  public void getUnderFSClassTest() {
     String underFSClass = UnderFileSystemCluster.getUnderFSClass();
-    Assert.assertNull(underFSClass);
-
-    Whitebox.setInternalState(UnderFileSystemCluster.class, "sUnderFSClass",
-        "XXXX");
-    underFSClass = UnderFileSystemCluster.getUnderFSClass();
-    Assert.assertFalse("alluxio.underfs.hdfs.LocalMiniDFSCluster".equals(underFSClass));
+    Assert.assertEquals("alluxio.underfs.LocalFileSystemCluster", underFSClass);
 
     Whitebox.setInternalState(UnderFileSystemCluster.class, "sUnderFSClass",
         "alluxio.underfs.hdfs.LocalMiniDFSCluster");
