91a2799fb44882bf7af99e7f7e162ef26c3a038a
Nate Putnanm <nate@cloudability.com>
2016-07-08 11:31:01 -0700

    Add in comments from example.

--- core/server/src/main/java/alluxio/web/UIWebServer.java
+++ core/server/src/main/java/alluxio/web/UIWebServer.java
@@ -93,12 +93,19 @@ public abstract class UIWebServer {
 
     mWebAppContext.setAttribute("org.eclipse.jetty.containerInitializers", jspInitializers());
 
+    // This webapp will use jsps and jstl. We need to enable the
+    // AnnotationConfiguration in order to correctly
+    // set up the jsp container
     org.eclipse.jetty.webapp.Configuration.ClassList classList =
         org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(mServer);
     classList.addBefore(
         "org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
         "org.eclipse.jetty.annotations.AnnotationConfiguration");
 
+    // Set the ContainerIncludeJarPattern so that jetty examines these
+    // container-path jars for tlds, web-fragments etc.
+    // If you omit the jar that contains the jstl .tlds, the jsp engine will
+    // scan for them instead.
     mWebAppContext.setAttribute(
         "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
         ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$"
