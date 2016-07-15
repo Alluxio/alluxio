433929f38cb6f254069dd7f0e89c64ef763348d0
xhad1234 <shayneqi@gmail.com>
2016-07-15 10:23:08 -0700

    change ID_INDEX in masterBlock to private

--- core/server/src/test/java/alluxio/master/block/BlockMasterTest.java
+++ core/server/src/test/java/alluxio/master/block/BlockMasterTest.java
@@ -388,7 +388,7 @@ public class BlockMasterTest {
 
     PrivateAccess(BlockMaster blockMaster) {
       mBlocks = Whitebox.getInternalState(mMaster, "mBlocks");
-      mIdIndex = blockMaster.ID_INDEX;
+      mIdIndex = Whitebox.getInternalState(BlockMaster.class, "ID_INDEX");
       mLostWorkers = Whitebox.getInternalState(mMaster, "mLostWorkers");
       mWorkers = Whitebox.getInternalState(mMaster, "mWorkers");
     }
