03a407316ac0030669085d37a5ff6456e06083ec
xhad1234 <shayneqi@gmail.com>
2016-07-18 15:12:53 -0700

    nit

--- core/server/src/main/java/alluxio/master/file/meta/InodeDirectory.java
+++ core/server/src/main/java/alluxio/master/file/meta/InodeDirectory.java
@@ -42,6 +42,7 @@ public final class InodeDirectory extends Inode<InodeDirectory> {
   };
 
   @SuppressWarnings("unchecked")
+  /** use UniqueFieldIndex directly for name index rather than using IndexedSet */
   private FieldIndex<Inode<?>> mChildren = new UniqueFieldIndex<Inode<?>>(NAME_INDEX);
 
   private boolean mMountPoint;
