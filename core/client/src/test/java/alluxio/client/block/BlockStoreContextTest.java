7c7309b5fb6276e4098921915a8e0c39a5cd3450
Andrew Audibert <aaudibert10@gmail.com>
2016-07-26 15:50:45 -0700

    Fix powermock calls

--- core/client/src/test/java/alluxio/client/block/BlockStoreContextTest.java
+++ core/client/src/test/java/alluxio/client/block/BlockStoreContextTest.java
@@ -35,8 +35,8 @@ import java.util.List;
  * Tests {@link BlockStoreContext}.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({BlockMasterClient.class, BlockMasterClientPool.class, BlockStoreContext.class,
-    DefaultBlockWorkerClient.class, BlockWorkerClientPool.class})
+@PrepareForTest({RetryHandlingBlockMasterClient.class, BlockMasterClientPool.class,
+    BlockStoreContext.class, DefaultBlockWorkerClient.class, BlockWorkerClientPool.class})
 public final class BlockStoreContextTest {
 
   /**
@@ -110,11 +110,12 @@ public final class BlockStoreContextTest {
     // Use mocks for the master client to make sure the pool of local block worker clients is
     // initialized properly.
     Whitebox.setInternalState(NetworkAddressUtils.class, "sLocalHost", "localhost");
-    BlockMasterClient masterClientMock = PowerMockito.mock(BlockMasterClient.class);
+    RetryHandlingBlockMasterClient masterClientMock =
+        PowerMockito.mock(RetryHandlingBlockMasterClient.class);
     List<WorkerInfo> list = new ArrayList<>();
     list.add(new WorkerInfo().setAddress(new WorkerNetAddress().setHost("localhost")));
     PowerMockito.doReturn(list).when(masterClientMock).getWorkerInfoList();
-    PowerMockito.whenNew(BlockMasterClient.class).withArguments(Mockito.any())
+    PowerMockito.whenNew(RetryHandlingBlockMasterClient.class).withArguments(Mockito.any())
         .thenReturn(masterClientMock);
 
     // Use mocks for the block worker client to prevent it from trying to invoke the session
@@ -175,11 +176,12 @@ public final class BlockStoreContextTest {
     // Use mocks for the master client to make sure the pool of local block worker clients is
     // initialized properly.
     Whitebox.setInternalState(NetworkAddressUtils.class, "sLocalHost", "localhost");
-    BlockMasterClient masterClientMock = PowerMockito.mock(BlockMasterClient.class);
+    RetryHandlingBlockMasterClient masterClientMock =
+        PowerMockito.mock(RetryHandlingBlockMasterClient.class);
     List<WorkerInfo> list = new ArrayList<>();
     list.add(new WorkerInfo().setAddress(new WorkerNetAddress().setHost("localhost")));
     PowerMockito.doReturn(list).when(masterClientMock).getWorkerInfoList();
-    PowerMockito.whenNew(BlockMasterClient.class).withArguments(Mockito.any())
+    PowerMockito.whenNew(RetryHandlingBlockMasterClient.class).withArguments(Mockito.any())
         .thenReturn(masterClientMock);
 
     Assert.assertTrue(BlockStoreContext.INSTANCE.hasLocalWorker());
@@ -193,12 +195,13 @@ public final class BlockStoreContextTest {
     // Use mocks for the master client to make sure the pool of local block worker clients is
     // initialized properly.
     Whitebox.setInternalState(NetworkAddressUtils.class, "sLocalHost", "localhost");
-    BlockMasterClient masterClientMock = PowerMockito.mock(BlockMasterClient.class);
+    RetryHandlingBlockMasterClient masterClientMock =
+        PowerMockito.mock(RetryHandlingBlockMasterClient.class);
     List<WorkerInfo> list = new ArrayList<>();
     list.add(new WorkerInfo().setAddress(new WorkerNetAddress().setHost("foo")));
     list.add(new WorkerInfo().setAddress(new WorkerNetAddress().setHost("bar")));
     PowerMockito.doReturn(list).when(masterClientMock).getWorkerInfoList();
-    PowerMockito.whenNew(BlockMasterClient.class).withArguments(Mockito.any())
+    PowerMockito.whenNew(RetryHandlingBlockMasterClient.class).withArguments(Mockito.any())
         .thenReturn(masterClientMock);
 
     Assert.assertFalse(BlockStoreContext.INSTANCE.hasLocalWorker());
