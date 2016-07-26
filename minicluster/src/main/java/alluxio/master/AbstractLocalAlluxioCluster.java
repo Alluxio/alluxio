1ada0ec22c99f6ddb00bda3e8fe61798a53cfc7e
Andrew Audibert <aaudibert10@gmail.com>
2016-07-26 13:12:42 -0700

    Fix whitebox calls

--- minicluster/src/main/java/alluxio/master/AbstractLocalAlluxioCluster.java
+++ minicluster/src/main/java/alluxio/master/AbstractLocalAlluxioCluster.java
@@ -389,7 +389,7 @@ public abstract class AbstractLocalAlluxioCluster {
    */
   protected void runWorker() throws IOException, ConnectionFailedException {
     mWorker = new DefaultAlluxioWorker();
-    Whitebox.setInternalState(DefaultAlluxioWorker.class, "sAlluxioWorker", mWorker);
+    Whitebox.setInternalState(AlluxioWorkerService.Factory.class, "sAlluxioWorker", mWorker);
 
     Runnable runWorker = new Runnable() {
       @Override
