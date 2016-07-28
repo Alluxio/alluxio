5cbd6d1b79db5e117a2e1000a74eb520a38d02d3
Andrew Audibert <aaudibert10@gmail.com>
2016-07-28 02:27:48 -0700

    Move AbstractClientTest to the right package

--- core/server/src/test/java/alluxio/AbstractClientTest.java
+++ core/server/src/test/java/alluxio/AbstractClientTest.java
@@ -9,7 +9,7 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
-package alluxio.worker.block;
+package alluxio;
 
 import alluxio.AbstractClient;
 import alluxio.exception.ExceptionMessage;
