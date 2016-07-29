ee6e347afb0558bb54c7d692d610ed53492eb8d9
Andrew Audibert <aaudibert10@gmail.com>
2016-07-28 23:41:35 -0700

    Fix compilation

--- core/server/src/test/java/alluxio/master/file/meta/TtlBucketTest.java
+++ core/server/src/test/java/alluxio/master/file/meta/TtlBucketTest.java
@@ -74,8 +74,8 @@ public class TtlBucketTest {
    */
   @Test
   public void addAndRemoveFileTest() {
-    InodeFile mFileTtl1 = InodeFile.create(0, 0, "test1", CreateFileOptions.defaults().setTtl(1));
-    InodeFile mFileTtl2 = InodeFile.create(1, 0, "test1", CreateFileOptions.defaults().setTtl(2));
+    InodeFile mFileTtl1 = InodeFile.create(0, 0, "test1", 0, CreateFileOptions.defaults().setTtl(1));
+    InodeFile mFileTtl2 = InodeFile.create(1, 0, "test1", 0, CreateFileOptions.defaults().setTtl(2));
     Assert.assertTrue(mBucket.getFiles().isEmpty());
 
     mBucket.addFile(mFileTtl1);
