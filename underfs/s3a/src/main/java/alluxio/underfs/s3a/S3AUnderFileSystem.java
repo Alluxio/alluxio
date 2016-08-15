d6bf180471bc6e5a994a213ce25d094b4c06dbd9
Chaomin Yu <ifcharming@gmail.com>
2016-08-15 14:07:50 -0700

    Disallow setOwner/setMode to object storage in FileSystemMaster, not directly on UnderFileSystem, to make delegation happy.

--- underfs/s3a/src/main/java/alluxio/underfs/s3a/S3AUnderFileSystem.java
+++ underfs/s3a/src/main/java/alluxio/underfs/s3a/S3AUnderFileSystem.java
@@ -409,17 +409,13 @@ public class S3AUnderFileSystem extends UnderFileSystem {
   @Override
   public void setConf(Object conf) {}
 
+  // Setting S3 owner via Alluxio is not supported yet. This is a no-op.
   @Override
-  public void setOwner(String path, String user, String group) throws IOException {
-    // Do not allow setting S3A owner via Alluxio yet.
-    throw new IOException("setOwner is not supported to S3A via Alluxio.");
-  }
+  public void setOwner(String path, String user, String group) {}
 
+  // Setting S3 mode via Alluxio is not supported yet. This is a no-op.
   @Override
-  public void setMode(String path, short mode) throws IOException {
-    // Do not allow setting S3A owner permission via Alluxio yet.
-    throw new IOException("setMode is not supported to S3A via Alluxio.");
-  }
+  public void setMode(String path, short mode) throws IOException {}
 
   // Returns the bucket owner.
   @Override
