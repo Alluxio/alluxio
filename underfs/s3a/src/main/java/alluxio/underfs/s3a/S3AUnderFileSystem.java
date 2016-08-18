1c8b20e4d36894e800989aa95b872a0ba5eb1df6
Chaomin Yu <ifcharming@gmail.com>
2016-08-18 16:03:10 -0700

    Rename S3_CANONICAL_USER_ID to S3_OWNER_ID

--- underfs/s3a/src/main/java/alluxio/underfs/s3a/S3AUnderFileSystem.java
+++ underfs/s3a/src/main/java/alluxio/underfs/s3a/S3AUnderFileSystem.java
@@ -165,7 +165,7 @@ public class S3AUnderFileSystem extends UnderFileSystem {
     // Gets the owner from user-defined static mapping from S3 canonical user id  to Alluxio
     // user name.
     String owner = CommonUtils.getValueFromStaticMapping(
-        Configuration.get(PropertyKey.UNDERFS_S3_CANONICAL_USER_ID_TO_USERNAME_MAPPING),
+        Configuration.get(PropertyKey.UNDERFS_S3_OWNER_ID_TO_USERNAME_MAPPING),
         mAccountOwnerId);
     // If there is no user-defined mapping, use the display name.
     if (owner == null) {
