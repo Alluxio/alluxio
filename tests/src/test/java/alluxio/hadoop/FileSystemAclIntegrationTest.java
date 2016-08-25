3024d5917df5bc1e3c08ba62f067da1076a6c4c5
Calvin Jia <jia.calvin@gmail.com>
2016-08-24 18:31:09 -0700

    Fix merge conflict resolution.

--- tests/src/test/java/alluxio/hadoop/FileSystemAclIntegrationTest.java
+++ tests/src/test/java/alluxio/hadoop/FileSystemAclIntegrationTest.java
@@ -139,7 +139,7 @@ public final class FileSystemAclIntegrationTest {
 
     // Expect a IOException for not able to setOwner for UFS with invalid owner name.
     mThrown.expect(IOException.class);
-    mThrown.expectMessage("Could not set owner for UFS file");
+    mThrown.expectMessage("Could not setOwner for UFS file");
     sTFS.setOwner(fileA, nonexistentOwner, null);
   }
 
@@ -172,7 +172,7 @@ public final class FileSystemAclIntegrationTest {
 
     // Expect a IOException for not able to setOwner for UFS with invalid group name.
     mThrown.expect(IOException.class);
-    mThrown.expectMessage("Could not set owner for UFS file");
+    mThrown.expectMessage("Could not setOwner for UFS file");
     sTFS.setOwner(fileB, null, nonexistentGroup);
   }
 
@@ -204,7 +204,7 @@ public final class FileSystemAclIntegrationTest {
     Assert.assertNotEquals(defaultGroup, nonexistentGroup);
 
     mThrown.expect(IOException.class);
-    mThrown.expectMessage("Could not set owner for UFS file");
+    mThrown.expectMessage("Could not setOwner for UFS file");
     sTFS.setOwner(fileC, nonexistentOwner, nonexistentGroup);
   }
 
