ccf9f8d5caa04604569f75c83fb004255953af0d
Andrew Audibert <aaudibert10@gmail.com>
2016-09-09 15:36:03 -0700

    Fix remaining integration test failures

--- tests/src/test/java/alluxio/worker/DataServerIntegrationTest.java
+++ tests/src/test/java/alluxio/worker/DataServerIntegrationTest.java
@@ -80,10 +80,12 @@ public class DataServerIntegrationTest {
   public DataServerIntegrationTest(String className, String nettyTransferType, String blockReader) {
     mLocalAlluxioClusterResource =
         new LocalAlluxioClusterResource.Builder()
-            .setProperty(PropertyKey.WORKER_DATA_SERVER_CLASS, className)
-            .setProperty(PropertyKey.WORKER_NETWORK_NETTY_FILE_TRANSFER_TYPE, nettyTransferType)
-            .setProperty(PropertyKey.USER_FILE_BUFFER_BYTES, String.valueOf(100))
             .setProperty(PropertyKey.USER_BLOCK_REMOTE_READER_CLASS, blockReader)
+            .setProperty(PropertyKey.USER_BLOCK_SIZE_BYTES_DEFAULT, Constants.MB)
+            .setProperty(PropertyKey.USER_FILE_BUFFER_BYTES, String.valueOf(100))
+            .setProperty(PropertyKey.WORKER_DATA_SERVER_CLASS, className)
+            .setProperty(PropertyKey.WORKER_MEMORY_SIZE, WORKER_CAPACITY_BYTES)
+            .setProperty(PropertyKey.WORKER_NETWORK_NETTY_FILE_TRANSFER_TYPE, nettyTransferType)
             .build();
   }
 
