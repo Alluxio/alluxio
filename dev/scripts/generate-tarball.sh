b4081a9b336e8d2e4ba2a32fd59bfea726ec5950
Andrew Audibert <aaudibert10@gmail.com>
2016-09-16 16:31:54 -0700

    Fix licenses and improve pathing

--- dev/scripts/generate-tarball.sh
+++ dev/scripts/generate-tarball.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
+#
+# The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+# (the "License"). You may not use this work except in compliance with the License, which is
+# available at www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied, as more fully set forth in the License.
+#
+# See the NOTICE file distributed with this work for information regarding copyright ownership.
+#
 
 set -e
 
 THIS=$(cd "$( dirname "$0" )"; pwd)
-TARBALL_DIR="${THIS}/tarballs"
+cd ${THIS}
+TARBALL_DIR="tarballs"
 WORK_DIR="workdir"
 
 mkdir -p ${TARBALL_DIR}
@@ -26,4 +37,4 @@ rsync -aq --exclude='.git*' --exclude='log' --exclude='dev' ${HOME} ${DIRNAME}
 
 gtar -czf ${TARGET} ${DIRNAME}
 
-echo "Generated tarball at ${TARGET}"
+echo "Generated tarball at ${THIS}/${TARGET}"
