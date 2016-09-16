7f24bba1790a531dd9e20c0b565b5818ae71bdac
Andrew Audibert <aaudibert10@gmail.com>
2016-09-16 16:21:57 -0700

    tarball generation script improvements

--- dev/scripts/generate-tarball.sh
+++ dev/scripts/generate-tarball.sh
@@ -11,15 +11,19 @@ mkdir -p ${WORK_DIR}
 HOME="${THIS}/../.."
 
 pushd ${HOME} > /dev/null
-#mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos ${BUILD_OPT}
+echo "Running build and logging to build.log"
+mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos ${BUILD_OPT} > build.log 2>&1
 popd > /dev/null
 
 VERSION=$(${HOME}/bin/alluxio version)
 PREFIX=alluxio-${VERSION}
 DIRNAME=${WORK_DIR}/${PREFIX}
+TARGET=${TARBALL_DIR}/${PREFIX}.tar.gz
 
 rm -rf ${DIRNAME}
 
 rsync -aq --exclude='.git*' --exclude='log' --exclude='dev' ${HOME} ${DIRNAME}
 
-gtar -czf ${TARBALL_DIR}/${PREFIX}.tar.gz ${DIRNAME}
+gtar -czf ${TARGET} ${DIRNAME}
+
+echo "Generated tarball at ${TARGET}"
