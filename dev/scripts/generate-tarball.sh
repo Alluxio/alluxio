3bcf79ade16dee678925a86c1c3104b193d589cb
Andrew Audibert <aaudibert10@gmail.com>
2016-09-19 15:53:30 -0700

    Make tarball generation script less invasive

--- dev/scripts/generate-tarball.sh
+++ dev/scripts/generate-tarball.sh
@@ -9,6 +9,13 @@
 #
 # See the NOTICE file distributed with this work for information regarding copyright ownership.
 #
+# This script generates a tarball of the current Alluxio commit. It does the following:
+# 1. Copy everything except logs/ and dev/ to a work directory
+# 2. Clean out unrevisioned files
+# 3. Compile
+# 4. Use bin/alluxio version to determine the right directory name, e.g. alluxio-1.2.0
+# 5. Tar everything up and put it in dev/scripts/tarballs
+#
 
 set -e
 
@@ -21,20 +28,22 @@ mkdir -p ${TARBALL_DIR}
 mkdir -p ${WORK_DIR}
 HOME="${THIS}/../.."
 
-pushd ${HOME} > /dev/null
-echo "Running build and logging to build.log"
-mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos ${BUILD_OPT} > build.log 2>&1
-popd > /dev/null
+REPO_COPY=${WORK_DIR}/alluxio
+rsync -aq --exclude='logs' --exclude='dev' ${HOME} ${REPO_COPY}
 
+pushd ${REPO_COPY} > /dev/null
+git clean -fdx
+BUILD_LOG="${HOME}/logs/build.log"
+echo "Running build and logging to ${BUILD_LOG}"
+mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos ${BUILD_OPT} | tee ${BUILD_LOG} 2>&1
 VERSION=$(${HOME}/bin/alluxio version)
 PREFIX=alluxio-${VERSION}
-DIRNAME=${WORK_DIR}/${PREFIX}
+cd ..
+rm -rf ${PREFIX}
+mv alluxio ${PREFIX}
+popd > /dev/null
+
 TARGET=${TARBALL_DIR}/${PREFIX}.tar.gz
-
-rm -rf ${DIRNAME}
-
-rsync -aq --exclude='.git*' --exclude='log' --exclude='dev' ${HOME} ${DIRNAME}
-
-gtar -czf ${TARGET} ${DIRNAME}
+gtar -czf ${TARGET} ${WORK_DIR}/${PREFIX} --exclude-vcs
 
 echo "Generated tarball at ${THIS}/${TARGET}"
