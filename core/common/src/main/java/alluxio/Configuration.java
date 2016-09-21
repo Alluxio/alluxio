e61781628268cfba6ac5356eeb75e87b49c46ee8
Chaomin Yu <ifcharming@gmail.com>
2016-09-20 17:06:49 -0700

    Minor nits

--- core/common/src/main/java/alluxio/Configuration.java
+++ core/common/src/main/java/alluxio/Configuration.java
@@ -200,8 +200,7 @@ public final class Configuration {
    * @param key the key to unset
    */
   public static void unset(PropertyKey key) {
-    Preconditions.checkArgument(key != null,
-        String.format("the key to unset (%s) cannot have null", key));
+    Preconditions.checkArgument(key != null, String.format("the key to unset cannot be null"));
     PROPERTIES.remove(key.toString());
   }
 
