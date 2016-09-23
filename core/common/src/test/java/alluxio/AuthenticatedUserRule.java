08d1662df0a9d141984b6cabab954a989102d46d
Chaomin Yu <ifcharming@gmail.com>
2016-09-23 16:19:56 -0700

    Fix javadocs

--- core/common/src/test/java/alluxio/AuthenticatedUserRule.java
+++ core/common/src/test/java/alluxio/AuthenticatedUserRule.java
@@ -19,11 +19,16 @@ import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
 /**
- * A rule for login a Alluxio user during a test suite.
+ * A rule for login an Alluxio user during a test suite. It sets {@link AuthenticatedClientUser}
+ * and {@link PropertyKey#SECURITY_LOGIN_USERNAME} to the specified user name during the lifetime
+ * of this rule. Note: {@link AuthenticatedClientUser} only takes effect within the caller thread.
  */
-public class AuthenticatedUserRule implements TestRule {
+public final class AuthenticatedUserRule implements TestRule {
   private final String mUser;
 
+  /**
+   * @param user the user name to set as authenticated user
+   */
   public AuthenticatedUserRule(String user) {
     mUser = user;
   }
