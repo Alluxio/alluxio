3accf765e68d502ebcd7771252cf92d806a8f302
Pei Sun <peis@alluxio.com>
2016-09-23 19:06:45 -0700

    fix test

--- core/common/src/test/java/alluxio/resource/DynamicResourcePoolTest.java
+++ core/common/src/test/java/alluxio/resource/DynamicResourcePoolTest.java
@@ -55,7 +55,7 @@ public final class DynamicResourcePoolTest {
 
     @Override
     protected boolean shouldGc(ResourceInternal<Resource> resourceInternal) {
-      return System.currentTimeMillis() - resourceInternal.getLastAccessTimeMs()
+      return mClock.millis() - resourceInternal.getLastAccessTimeMs()
           >= (long) mGcThresholdInSecs * (long) Constants.SECOND_MS;
     }
 
@@ -201,7 +201,7 @@ public final class DynamicResourcePoolTest {
     resourceList.add(pool.acquire());
 
     pool.release(resourceList.get(0));
-    manualClock.addTimeMs(1020);
+    manualClock.addTimeMs(1001);
 
     // Sleep 1 second to make sure the GC has run.
     Thread.sleep(1000);
