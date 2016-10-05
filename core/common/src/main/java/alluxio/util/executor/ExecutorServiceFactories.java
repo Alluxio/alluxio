c39341767af12820a4d46cffc41f38ceeb975bad
Andrew Audibert <aaudibert10@gmail.com>
2016-10-05 16:38:02 -0700

    Update javadoc

--- core/common/src/main/java/alluxio/util/executor/ExecutorServiceFactories.java
+++ core/common/src/main/java/alluxio/util/executor/ExecutorServiceFactories.java
@@ -21,10 +21,12 @@ import java.util.concurrent.Executors;
  */
 public final class ExecutorServiceFactories {
   /**
+   * Returns a {@link ExecutorServiceFactory} which creates threadpool executors with the given base
+   * name and number of threads. Created threads will be daemonic.
+   *
    * @param name the base name for executor thread names
    * @param nThreads the number of threads to create executors with
-   * @return an {@link ExecutorServiceFactory} which creates threadpool executors with the given
-   *         name and number of threads
+   * @return the {@link ExecutorServiceFactory}
    */
   public static ExecutorServiceFactory fixedThreadPoolExecutorServiceFactory(final String name,
       final int nThreads) {
