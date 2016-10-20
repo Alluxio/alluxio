3595fd9d10366725d6e08edd8699e0247caac425
nicegdhj <438499123@qq.com>
2016-10-20 09:19:52 +0800

    coodestyle check

--- core/common/src/test/java/alluxio/metrics/MetricsConfigTest.java
+++ core/common/src/test/java/alluxio/metrics/MetricsConfigTest.java
@@ -63,7 +63,7 @@ public class MetricsConfigTest {
     Properties properties = config.getProperties();
 
     Map<String, Properties> sinkProps = 
-    MetricsConfig.subProperties(properties, MetricsSystem.SINK_REGEX);
+        MetricsConfig.subProperties(properties, MetricsSystem.SINK_REGEX);
     Assert.assertEquals(2, sinkProps.size());
     Assert.assertTrue(sinkProps.containsKey("console"));
     Assert.assertTrue(sinkProps.containsKey("jmx"));
