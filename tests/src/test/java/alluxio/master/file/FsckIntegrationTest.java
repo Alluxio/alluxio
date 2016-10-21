6e44c06eb7b1ecf4eb60172b4fe009c509e714e0
Calvin Jia <jia.calvin@gmail.com>
2016-10-20 18:04:41 -0700

    License/docs.

--- tests/src/test/java/alluxio/master/file/FsckIntegrationTest.java
+++ tests/src/test/java/alluxio/master/file/FsckIntegrationTest.java
@@ -1,3 +1,14 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package alluxio.master.file;
 
 import alluxio.AlluxioURI;
@@ -17,6 +28,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Integration test for the fsck command.
+ */
 public class FsckIntegrationTest {
   private static final AlluxioURI DIRECTORY = new AlluxioURI("/dir");
   private static final AlluxioURI FILE = new AlluxioURI("/dir/file");
