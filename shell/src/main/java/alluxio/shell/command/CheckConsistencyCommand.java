360bfae05125b15a6378f7105178b3a819f6c8ce
Calvin Jia <jia.calvin@gmail.com>
2016-10-26 16:51:27 -0700

    Add shell command for check consistency.

---
+++ shell/src/main/java/alluxio/shell/command/CheckConsistencyCommand.java
@@ -0,0 +1,57 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.shell.command;
+
+import alluxio.AlluxioURI;
+import alluxio.client.file.FileSystem;
+import alluxio.exception.AlluxioException;
+
+import org.apache.commons.cli.CommandLine;
+
+import java.io.IOException;
+
+/**
+ * Command for checking the consistency of a file or folder between Alluxio and the under storage.
+ */
+public class CheckConsistencyCommand extends AbstractShellCommand {
+  public CheckConsistencyCommand(FileSystem fs) {
+    super(fs);
+  }
+
+  @Override
+  int getNumOfArgs() {
+    return 1;
+  }
+
+  @Override
+  public String getCommandName() {
+    return "checkConsistency";
+  }
+
+  @Override
+  public void run(CommandLine cl) throws AlluxioException, IOException {
+    String[] args = cl.getArgs();
+    AlluxioURI root = new AlluxioURI(args[0]);
+  }
+
+  @Override
+  public String getUsage() {
+    return "checkConsistency <Alluxio path>";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Checks the consistency of a persisted file or directory in Alluxio. Any files or " +
+        "directories which only exist in Alluxio or do not match the metadata of files in the " +
+        "under storage will be returned. An administrator should then reconcile the differences.";
+  }
+}
