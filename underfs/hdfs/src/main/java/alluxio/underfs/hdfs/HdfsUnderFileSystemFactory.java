735fffb76c53369da682dea90d4027234dff0e49
madanadit <adit@alluxio.com>
2016-10-26 15:23:40 -0700

    Fix style

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystemFactory.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystemFactory.java
@@ -43,9 +43,9 @@ public final class HdfsUnderFileSystemFactory implements UnderFileSystemFactory 
   public boolean supportsPath(String path) {
     if (path != null) {
       // TODO(hy): In Hadoop 2.x this can be replaced with the simpler call to
-      // FileSystem.getFileSystemClass() without any need for having users explicitly declare the file
-      // system schemes to treat as being HDFS. However as long as pre 2.x versions of Hadoop are
-      // supported this is not an option and we have to continue to use this method.
+      // FileSystem.getFileSystemClass() without any need for having users explicitly declare the
+      // file system schemes to treat as being HDFS. However as long as pre 2.x versions of Hadoop
+      // are supported this is not an option and we have to continue to use this method.
       for (final String prefix : Configuration.getList(PropertyKey.UNDERFS_HDFS_PREFIXES, ",")) {
         if (path.startsWith(prefix)) {
           return true;
