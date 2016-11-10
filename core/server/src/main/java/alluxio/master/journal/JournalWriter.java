ede44c3776589eb86d3049ecd349909ab934a2ea
madanadit <adit@alluxio.com>
2016-11-09 16:31:14 -0800

    Fix build

--- core/server/src/main/java/alluxio/master/journal/JournalWriter.java
+++ core/server/src/main/java/alluxio/master/journal/JournalWriter.java
@@ -129,7 +129,7 @@ public final class JournalWriter {
       mNextEntrySequenceNumber = latestSequenceNumber + 1;
       LOG.info("Latest journal sequence number: {} Next journal sequence number: {}",
           latestSequenceNumber, mNextEntrySequenceNumber);
-      UnderFileSystemUtils.deleteIfExists(mUfs, mTempCheckpointPath);
+      UnderFileSystemUtils.deleteFileIfExists(mUfs, mTempCheckpointPath);
       mCheckpointOutputStream =
           new CheckpointOutputStream(new DataOutputStream(mUfs.create(mTempCheckpointPath)));
     }
