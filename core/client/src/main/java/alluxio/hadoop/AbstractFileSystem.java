eba21442eb7ab27514cf82240bb443955c5e52c4
TeddyBear1314 <huangzhi19921023@sina.com>
2016-11-16 09:24:22 +0800

    fix FileStatus 5th argument modificationTime

--- core/client/src/main/java/alluxio/hadoop/AbstractFileSystem.java
+++ core/client/src/main/java/alluxio/hadoop/AbstractFileSystem.java
@@ -465,7 +465,7 @@ abstract class AbstractFileSystem extends org.apache.hadoop.fs.FileSystem {
       URIStatus status = statuses.get(k);
       // TODO(hy): Replicate 3 with the number of disk replications.
       ret[k] = new FileStatus(status.getLength(), status.isFolder(), 3, status.getBlockSizeBytes(),
-          status.getCreationTimeMs(), status.getCreationTimeMs(), null, null, null,
+          status.getLastModificationTimeMs(), status.getCreationTimeMs(), null, null, null,
           new Path(mAlluxioHeader + status.getPath()));
     }
     return ret;
