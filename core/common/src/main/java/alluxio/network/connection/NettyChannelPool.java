1ce2aa1f27cee405dd7a60d052f62388302fa642
Gene Pang <gene.pang@gmail.com>
2016-11-23 13:22:23 -0800

    Fix typo in log message

--- core/common/src/main/java/alluxio/network/connection/NettyChannelPool.java
+++ core/common/src/main/java/alluxio/network/connection/NettyChannelPool.java
@@ -93,7 +93,7 @@ public final class NettyChannelPool extends DynamicResourcePool<Channel> {
         LOG.info("Created netty channel with netty bootstrap {}.", mBootstrap);
         return channelFuture.channel();
       } else {
-        LOG.error("Failed to create netty channel with netty boostrap {} and error {}.",
+        LOG.error("Failed to create netty channel with netty bootstrap {} and error {}.",
             mBootstrap, channelFuture.cause().getMessage());
         throw new IOException(channelFuture.cause());
       }
