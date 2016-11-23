3704730717105e07d709cdade661afa891bce9d3
Andrew Audibert <aaudibert10@gmail.com>
2016-11-23 08:19:59 -0800

    Add class for representing master zk nodes

--- core/server/src/test/java/alluxio/master/MasterZookeeperNodeTest.java
+++ core/server/src/test/java/alluxio/master/MasterZookeeperNodeTest.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2016 Alluxio, Inc. All rights reserved.
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
  *
- * This software and all information contained herein is confidential and proprietary to Alluxio,
- * and is protected by copyright and other applicable laws in the United States and other
- * jurisdictions. You may not use, modify, reproduce, distribute, or disclose this software without
- * the express written permission of Alluxio.
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
 package alluxio.master;
