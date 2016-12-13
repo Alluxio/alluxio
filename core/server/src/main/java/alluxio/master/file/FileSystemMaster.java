3e9f3ec9eee6ead35c7b0fb0b3daa1ce64dad8b3
Gene Pang <gene.pang@gmail.com>
2016-12-13 09:18:48 -0800

    Optimistically read lock for getFileInfo

--- core/server/src/main/java/alluxio/master/file/FileSystemMaster.java
+++ core/server/src/main/java/alluxio/master/file/FileSystemMaster.java
@@ -704,6 +704,15 @@ public final class FileSystemMaster extends AbstractMaster {
       throws FileDoesNotExistException, InvalidPathException, AccessControlException {
     Metrics.GET_FILE_INFO_OPS.inc();
     long flushCounter = AsyncJournalWriter.INVALID_FLUSH_COUNTER;
+
+    try (LockedInodePath inodePath = mInodeTree.lockInodePath(path, InodeTree.LockMode.READ)) {
+      mPermissionChecker.checkPermission(Mode.Bits.READ, inodePath);
+      if (inodePath.fullPathExists()) {
+        // The file already exists, so metadata does not need to be loaded.
+        return getFileInfoInternal(inodePath);
+      }
+    }
+
     try (LockedInodePath inodePath = mInodeTree.lockInodePath(path, InodeTree.LockMode.WRITE)) {
       // This is WRITE locked, since loading metadata is possible.
       mPermissionChecker.checkPermission(Mode.Bits.READ, inodePath);
