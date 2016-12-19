57f8d26bc2aad0a91ab8a85be7e192ef469489f9
Andrew Audibert <aaudibert10@gmail.com>
2016-12-18 23:30:56 -0800

    Fix compilation

--- core/common/src/test/java/alluxio/wire/AlluxioMasterInfoTest.java
+++ core/common/src/test/java/alluxio/wire/AlluxioMasterInfoTest.java
@@ -74,11 +74,11 @@ public class AlluxioMasterInfoTest {
     String rpcAddress = CommonUtils.randomAlphaNumString(random.nextInt(10));
     long startTimeMs = random.nextLong();
     StartupConsistencyCheck check = new StartupConsistencyCheck();
-    check.setStatus(CommonUtils.randomString(random.nextInt(10)));
+    check.setStatus(CommonUtils.randomAlphaNumString(random.nextInt(10)));
     int numUris = random.nextInt(10);
     List<String> uris = new ArrayList<>(numUris);
     for (int i = 0; i < numUris; i++) {
-      uris.add(CommonUtils.randomString(random.nextInt(10)));
+      uris.add(CommonUtils.randomAlphaNumString(random.nextInt(10)));
     }
     check.setInconsistentUris(uris);
     Map<String, Capacity> tierCapacity = new HashMap<>();
