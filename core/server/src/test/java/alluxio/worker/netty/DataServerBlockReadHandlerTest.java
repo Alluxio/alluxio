4c3300ac9be8bece12cbb4ea3e21ff0d11af4d16
Pei Sun <peis@alluxio.com>
2017-01-05 17:02:44 -0800

    More read tests.

--- core/server/src/test/java/alluxio/worker/netty/DataServerBlockReadHandlerTest.java
+++ core/server/src/test/java/alluxio/worker/netty/DataServerBlockReadHandlerTest.java
@@ -11,9 +11,13 @@
 
 package alluxio.worker.netty;
 
+import alluxio.Configuration;
 import alluxio.Constants;
+import alluxio.PropertyKey;
 import alluxio.network.protocol.RPCProtoMessage;
 import alluxio.network.protocol.databuffer.DataBuffer;
+import alluxio.network.protocol.databuffer.DataFileChannel;
+import alluxio.network.protocol.databuffer.DataNettyBufferV2;
 import alluxio.proto.dataserver.Protocol;
 import alluxio.util.CommonUtils;
 import alluxio.util.io.BufferUtils;
@@ -36,11 +40,14 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.util.Random;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({BlockWorker.class})
 public final class DataServerBlockReadHandlerTest {
+  private static final long PACKET_SIZE =
+      Configuration.getBytes(PropertyKey.WORKER_NETWORK_NETTY_READER_PACKET_SIZE_BYTES);
   private final Random mRandom = new Random();
   private final long mBlockId = 1L;
 
@@ -63,31 +70,65 @@ public final class DataServerBlockReadHandlerTest {
   }
 
   @Test
-  public void readEmptyFile() throws Exception {
-    populateInputFile(1);
-    mChannel.writeInbound(buildReadRequest(0, 1));
-    mChannel.runPendingTasks();
+  public void readFullFile() throws Exception {
+    populateInputFile(PACKET_SIZE * 10, 0, PACKET_SIZE * 10 - 1);
+    mChannel.writeInbound(buildReadRequest(0, PACKET_SIZE * 10));
     checkAllReadResponses();
   }
 
-  /*
   @Test
-  public void writeNonEmptyFile() throws Exception {
-    final EmbeddedChannel channel = new EmbeddedChannel(
-        new DataServerBlockWriteHandler(NettyExecutors.BLOCK_WRITER_EXECUTOR, mBlockWorker));
-    long len = 0;
-    for (int i = 0; i < 128; i++) {
-      channel.writeInbound(buildWriteRequest(len, 1024));
-      len += 1024;
-    }
-    // EOF.
-    channel.writeInbound(buildWriteRequest(len, 0));
-
-    Object writeResponse = waitForResponse(channel);
-    checkWriteResponse(writeResponse, Protocol.Status.Code.OK);
-    checkFileContent(len);
+  public void readPartialFile() throws Exception {
+    long start = 3;
+    long end = PACKET_SIZE * 10 - 99;
+    populateInputFile(PACKET_SIZE * 10, start, end);
+    mChannel.writeInbound(buildReadRequest(start, end + 1 - start));
+    checkAllReadResponses();
   }
 
+  @Test
+  public void reuseChannel() throws Exception {
+    long fileSize = PACKET_SIZE * 5;
+    populateInputFile(fileSize, 0, fileSize - 1);
+    mChannel.writeInbound(buildReadRequest(0, fileSize));
+    checkAllReadResponses();
+
+    fileSize = fileSize / 2 + 1;
+    long start = 3;
+    long end = fileSize - 1;
+    populateInputFile(fileSize, start, end);
+    mChannel.writeInbound(buildReadRequest(start, end - start + 1));
+    checkAllReadResponses();
+  }
+
+  @Test
+  public void transferType() throws Exception {
+    mChannel = new EmbeddedChannel(
+        new DataServerBlockReadHandler(NettyExecutors.BLOCK_READER_EXECUTOR, mBlockWorker,
+            FileTransferType.TRANSFER));
+
+    long fileSize = PACKET_SIZE * 2;
+    populateInputFile(fileSize, 0, fileSize - 1);
+
+    BlockReader blockReader = PowerMockito.spy(mBlockReader);
+    // Do not call close so that we can check result. It will be closed explicitly.
+    PowerMockito.doNothing().when(blockReader).close();
+    PowerMockito
+        .when(mBlockWorker.readBlockRemote(Mockito.anyLong(), Mockito.anyLong(), Mockito.anyLong()))
+        .thenReturn(blockReader);
+    mChannel.writeInbound(buildReadRequest(0, fileSize));
+    checkAllReadResponses();
+    mBlockReader.close();
+  }
+
+  @Test
+  public void readEmptyFile() throws Exception {
+    populateInputFile(0, 0, 0);
+    mChannel.writeInbound(buildReadRequest(0, 0));
+    checkAllReadResponses();
+  }
+
+
+  /*
   @Test
   public void writeInvalidOffset() throws Exception {
     final EmbeddedChannel channel = new EmbeddedChannel(
@@ -116,15 +157,20 @@ public final class DataServerBlockReadHandlerTest {
   }
   */
 
-  private void populateInputFile(long length) throws Exception {
+  private void populateInputFile(long length, long start, long end) throws Exception {
+    mChecksum = 0;
     File file = mTestFolder.newFile();
+    long pos = 0;
     if (length > 0) {
       FileOutputStream fileOutputStream = new FileOutputStream(file);
       while (length > 0) {
         byte[] buffer = new byte[(int) Math.min(length, Constants.MB)];
         mRandom.nextBytes(buffer);
         for (int i = 0; i < buffer.length; i++) {
-          mChecksum += BufferUtils.byteToInt(buffer[i]);
+          if (pos >= start && pos <= end) {
+            mChecksum += BufferUtils.byteToInt(buffer[i]);
+          }
+          pos++;
         }
         fileOutputStream.write(buffer);
         length -= buffer.length;
@@ -160,11 +206,21 @@ public final class DataServerBlockReadHandlerTest {
       DataBuffer buffer = checkReadResponse(readResponse, Protocol.Status.Code.OK);
       EOF = buffer == null;
       if (buffer != null) {
-        ByteBuf buf = (ByteBuf) buffer.getNettyOutput();
-        while (buf.readableBytes() > 0) {
-          checksumActual += BufferUtils.byteToInt(buf.readByte());
+        if (buffer instanceof DataNettyBufferV2) {
+          ByteBuf buf = (ByteBuf) buffer.getNettyOutput();
+          while (buf.readableBytes() > 0) {
+            checksumActual += BufferUtils.byteToInt(buf.readByte());
+          }
+          buf.release();
+        } else {
+          Assert.assertTrue(buffer instanceof DataFileChannel);
+          ByteBuffer buf = buffer.getReadOnlyByteBuffer();
+          byte[] array = new byte[buf.remaining()];
+          buf.get(array);
+          for (int i = 0; i < array.length; i++) {
+            checksumActual += BufferUtils.byteToInt(array[i]);
+          }
         }
-        buf.release();
       }
     }
     Assert.assertEquals(mChecksum, checksumActual);
