2226dd58a87cf2af72c8c7beab805e5760bf06d7
yindaren <LLiang_Yin@163.com>
2017-01-18 11:12:48 +0800

    smallfix

--- tests/src/test/java/alluxio/hadoop/fs/DFSIOIntegrationTest.java
+++ tests/src/test/java/alluxio/hadoop/fs/DFSIOIntegrationTest.java
@@ -232,12 +232,7 @@ public class DFSIOIntegrationTest implements Tool {
         org.apache.hadoop.fs.FileSystem.get(sLocalAlluxioClusterUri, sBench.getConf());
     sBench.cleanup(fs);
   }
-
-  /**
-   * writes into files, then calculates and collects the write test statistics.
-   *
-   * @throws Exception if has error
-   */
+  
   public static void writeTest() throws Exception {
     org.apache.hadoop.fs.FileSystem fs =
         org.apache.hadoop.fs.FileSystem.get(sLocalAlluxioClusterUri, sBench.getConf());
