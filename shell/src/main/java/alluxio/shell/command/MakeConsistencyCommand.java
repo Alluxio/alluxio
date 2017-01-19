6fecfeb19ab56b6b98da9b7c3e88e90cc77914e0
gjhkael <guojhltt@gmail.com>
2017-01-19 14:19:52 +0800

    Add makeConsistency command

---
+++ shell/src/main/java/alluxio/shell/command/MakeConsistencyCommand.java
@@ -0,0 +1,149 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.shell.command;
+
+import alluxio.AlluxioURI;
+import alluxio.Constants;
+import alluxio.client.ReadType;
+import alluxio.client.file.FileInStream;
+import alluxio.client.file.FileSystem;
+import alluxio.client.file.FileSystemUtils;
+import alluxio.client.file.URIStatus;
+import alluxio.client.file.options.CheckConsistencyOptions;
+import alluxio.client.file.options.DeleteOptions;
+import alluxio.client.file.options.ListStatusOptions;
+import alluxio.client.file.options.OpenFileOptions;
+import alluxio.exception.AlluxioException;
+import alluxio.wire.LoadMetadataType;
+
+import com.google.common.io.Closer;
+
+import org.apache.commons.cli.CommandLine;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Command for make the consistency of a file between Alluxio and the under storage.
+ */
+public class MakeConsistencyCommand extends AbstractShellCommand {
+  /**
+   * @param fs the filesystem of Alluxio
+   */
+  public MakeConsistencyCommand(FileSystem fs) {
+    super(fs);
+  }
+
+  @Override
+  protected int getNumOfArgs() {
+    return 1;
+  }
+
+  @Override
+  public String getCommandName() {
+    return "makeConsistency";
+  }
+
+  @Override
+  public void run(CommandLine cl) throws AlluxioException, IOException {
+    String[] args = cl.getArgs();
+    AlluxioURI root = new AlluxioURI(args[0]);
+    CheckConsistencyOptions options = CheckConsistencyOptions.defaults();
+    List<AlluxioURI> inconsistentUris = FileSystemUtils.checkConsistency(root, options);
+    if (inconsistentUris.isEmpty()) {
+      System.out.println(root + " is consistent with the under storage system.");
+      ListStatusOptions options1 = ListStatusOptions.defaults();
+      options1.setLoadMetadataType(LoadMetadataType.Always);
+      mFileSystem.listStatus(root, options1);
+      loadFile(root);
+    } else {
+      Collections.sort(inconsistentUris);
+      System.out.println(root + " have: " + inconsistentUris.size() + "files inconsistent.");
+      for (int i = 0; i < inconsistentUris.size(); i++) {
+        System.out.println("Fixing path: " + inconsistentUris.get(i).getPath());
+        DeleteOptions deleteOptions = DeleteOptions.defaults().setRemoveUFSFile(false);
+        mFileSystem.delete(inconsistentUris.get(i), deleteOptions);
+        Closer closer = Closer.create();
+        try {
+          OpenFileOptions openFileOptions = OpenFileOptions.defaults()
+              .setReadType(ReadType.CACHE_PROMOTE);
+          if (mFileSystem.exists(inconsistentUris.get(i))) {
+            FileInStream in = closer.register(mFileSystem.openFile(inconsistentUris.get(i),
+                openFileOptions));
+            byte[] buf = new byte[8 * Constants.MB];
+            while (in.read(buf) != -1) {
+            }
+          }
+        } catch (Exception e) {
+          throw closer.rethrow(e);
+        } finally {
+          closer.close();
+        }
+        System.out.println(inconsistentUris.get(i).getPath() + "fixed");
+        System.out.println();
+      }
+      ListStatusOptions options1 = ListStatusOptions.defaults();
+      options1.setLoadMetadataType(LoadMetadataType.Always);
+      mFileSystem.listStatus(root, options1);
+      loadFile(root);
+    }
+  }
+
+  /**
+   * @param filePath The {@link AlluxioURI} path to load into Alluxio memory
+   * @throws AlluxioException when Alluxio exception occurs
+   * @throws IOException when non-Alluxio exception occurs
+   */
+  private void loadFile(AlluxioURI filePath) throws AlluxioException, IOException {
+    URIStatus status = mFileSystem.getStatus(filePath);
+    if (status.isFolder()) {
+      List<URIStatus> statuses = mFileSystem.listStatus(filePath);
+      for (URIStatus uriStatus : statuses) {
+        AlluxioURI newPath = new AlluxioURI(uriStatus.getPath());
+        loadFile(newPath);
+      }
+    } else {
+      if (status.getInMemoryPercentage() == 100) {
+        // The file has already been fully loaded into Alluxio memory.
+        return;
+      }
+      Closer closer = Closer.create();
+      try {
+        OpenFileOptions options = OpenFileOptions.defaults().setReadType(ReadType.CACHE_PROMOTE);
+        FileInStream in = closer.register(mFileSystem.openFile(filePath, options));
+        byte[] buf = new byte[8 * Constants.MB];
+        while (in.read(buf) != -1) {
+        }
+      } catch (Exception e) {
+        throw closer.rethrow(e);
+      } finally {
+        closer.close();
+      }
+    }
+    if (!status.isFolder()) {
+      System.out.println("The " + filePath + " is new added in UFS! success loaded");
+    }
+  }
+
+  @Override
+  public String getUsage() {
+    return "makeConsistency <Alluxio path>";
+  }
+
+  @Override
+  public String getDescription() {
+    return "make consistency of a persisted file in Alluxio. Any files or "
+      + "directories which only exist in Alluxio or do not match the metadata of files in the "
+      + "under storage will be fixed.";
+  }
+}
