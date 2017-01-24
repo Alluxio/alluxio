136367dbe22e27b8ef9e2e94c32faec31cddf5d6
Jiri Simsa <jiri.simsa@gmail.com>
2017-01-24 13:28:55 -0800

    Added documentation on journaling.

--- core/server/src/main/java/alluxio/master/file/FileSystemMaster.java
+++ core/server/src/main/java/alluxio/master/file/FileSystemMaster.java
@@ -168,6 +168,27 @@ public final class FileSystemMaster extends AbstractMaster {
    * methods which iterate over child nodes can safely ignore the exception and treat the inode
    * as no longer a child.
    *
+   * Journaling in the FileSystemMaster
+   *
+   * Any changes to file system metadata that need to survive system restarts and / or failures
+   * should be journaled. The intent to journal and the actual writing of the journal is decoupled
+   * so that operations are not holding metadata locks waiting on the journal IO. In particular,
+   * file system operations are expected to create a {@link JournalContext} resource and is use it
+   * throughout the lifetime of an operation to collect journal events through
+   * {@link #appendJournalEntry(JournalEntry, JournalContext)} and then close the resource, which
+   * will persist the journal events. In order to ensure the journal events are always persisted,
+   * the following paradigm is recommended:
+   *
+   * <p><blockquote><pre>
+   *    try (JournalContext journalContext = createJournalContext()) {
+   *      ...
+   *    }
+   * </pre></blockquote>
+   *
+   * NOTE: Because resources are released in the opposite order they are acquired, the
+   * {@link JournalContext} resources should be always created before the {@link LockedInodePath}
+   * resource.
+   *
    * Method Conventions in the FileSystemMaster
    *
    * All of the flow of the FileSystemMaster follow a convention. There are essentially 4 main
