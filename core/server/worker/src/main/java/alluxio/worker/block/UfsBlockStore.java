a664c78dc04ed4d1128be3f94f39b7f52a36c5e7
Pei Sun <peis@alluxio.com>
2017-02-20 23:06:41 -0800

    Style fix

--- core/server/worker/src/main/java/alluxio/worker/block/UfsBlockStore.java
+++ core/server/worker/src/main/java/alluxio/worker/block/UfsBlockStore.java
@@ -17,18 +17,15 @@ import alluxio.exception.BlockDoesNotExistException;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.UfsBlockAccessTokenUnavailableException;
 import alluxio.worker.block.io.BlockReader;
-import alluxio.worker.block.io.BlockWriter;
 import alluxio.worker.block.meta.UfsBlockMeta;
 
 import com.google.common.base.Objects;
-import org.apache.commons.lang.NotImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
@@ -37,26 +34,48 @@ import javax.annotation.concurrent.GuardedBy;
 
 /**
  * This class manages the virtual blocks in the UFS for delegated UFS read/write.
+ *
+ * The usage pattern:
+ * acquireAccess(blockMeta, maxConcurrency)
+ * cleanup(sessionId, blockId)
+ * releaseAccess(sessionId, blockId)
  */
 public final class UfsBlockStore {
   private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
 
   private final ReentrantLock mLock = new ReentrantLock();
   @GuardedBy("mLock")
+  /** Maps from the {@link Key} to the {@link UfsBlockMeta}. */
   private final Map<Key, UfsBlockMeta> mBlocks = new HashMap<>();
   @GuardedBy("mLock")
+  /** Maps from the session ID to the block IDs. */
   private final Map<Long, Set<Long>> mSessionIdToBlockIds = new HashMap<>();
   @GuardedBy("mLock")
+  /** Maps from the block ID to the session IDs. */
   private final Map<Long, Set<Long>> mBlockIdToSessionIds = new HashMap<>();
 
+  /** The Alluxio block store. */
   private final BlockStore mAlluxioBlockStore;
 
+  /**
+   * Creates an instance of {@link UfsBlockStore}.
+   *
+   * @param alluxioBlockStore the Alluxio block store
+   */
   public UfsBlockStore(BlockStore alluxioBlockStore) {
     mAlluxioBlockStore = alluxioBlockStore;
   }
 
-  // acquire --> cleanup --> release
-
+  /**
+   * Acquires access for a UFS block given a {@link UfsBlockMeta} and the limit on the maximum
+   * concurrent accessors on the block.
+   *
+   * @param blockMeta the block meta
+   * @param maxConcurrency the maximum concurrency
+   * @throws BlockAlreadyExistsException if the block already exists for a session ID
+   * @throws UfsBlockAccessTokenUnavailableException if there are too many concurrent sessions
+   *         accessing the block
+   */
   public void acquireAccess(UfsBlockMeta blockMeta, int maxConcurrency) throws
       BlockAlreadyExistsException, UfsBlockAccessTokenUnavailableException {
     long sessionId = blockMeta.getSessionId();
@@ -93,18 +112,17 @@ public final class UfsBlockStore {
   }
 
   /**
-   * Finishes accessing to this block by cleaning up the block reader or writer.
+   * Cleans up the block reader or writer.
    *
    * During UFS block read, this is triggered when the block is unlocked.
    * During UFS block write, this is triggered when the UFS block is committed.
    *
-   * @param sessionId
-   * @param blockId
-   * @return
-   * @throws BlockDoesNotExistException
+   * @param sessionId the session ID
+   * @param blockId the block ID
+   * @return true if block is to be committed into Alluxio block store
+   * @throws IOException if it fails to clean up
    */
-  public boolean cleanup(long sessionId, long blockId)
-      throws BlockDoesNotExistException, IOException {
+  public boolean cleanup(long sessionId, long blockId) throws IOException {
     UfsBlockMeta blockMeta;
     mLock.lock();
     try {
@@ -117,22 +135,6 @@ public final class UfsBlockStore {
     }
     blockMeta.closeReaderOrWriter();
     return blockMeta.getCommitPending();
-  }
-
-  private UfsBlockMeta getBlockMeta(long sessionId, long blockId)
-      throws BlockDoesNotExistException {
-    Key key = new Key(sessionId, blockId);
-    UfsBlockMeta blockMeta = mBlocks.get(key);
-    if (blockMeta == null) {
-      try {
-        throw new BlockDoesNotExistException(ExceptionMessage.UFS_BLOCK_DOES_NOT_EXIST_FOR_SESSION,
-            blockId, sessionId);
-      } catch (Throwable e) {
-        LOG.error("UFS Block does not exist.", e);
-        throw e;
-      }
-    }
-    return blockMeta;
   }
 
   /**
@@ -191,13 +193,13 @@ public final class UfsBlockStore {
   }
 
   /**
-   * Creates a block reader
+   * Creates a UFS block reader for a UFS block.
    *
-   * @param sessionId
-   * @param blockId
+   * @param sessionId the session ID
+   * @param blockId the block ID
    * @param offset the offset within the block (NOT the file)
-   * @return
-   * @throws BlockDoesNotExistException
+   * @return the block reader instance
+   * @throws BlockDoesNotExistException if the UFS block does not exist in the {@link UfsBlockStore}
    */
   public BlockReader getBlockReader(final long sessionId, long blockId, long offset,
       boolean noCache) throws BlockDoesNotExistException, IOException {
@@ -212,6 +214,30 @@ public final class UfsBlockStore {
       mLock.unlock();
     }
     return new UfsBlockReader(blockMeta, offset, noCache, mAlluxioBlockStore);
+  }
+
+  /**
+   * Gets the {@link UfsBlockMeta} for a session ID and block ID pair.
+   *
+   * @param sessionId the session ID
+   * @param blockId the block ID
+   * @return the {@link UfsBlockMeta} instance
+   * @throws BlockDoesNotExistException if the UFS block does not exist in the {@link UfsBlockStore}
+   */
+  private UfsBlockMeta getBlockMeta(long sessionId, long blockId)
+      throws BlockDoesNotExistException {
+    Key key = new Key(sessionId, blockId);
+    UfsBlockMeta blockMeta = mBlocks.get(key);
+    if (blockMeta == null) {
+      try {
+        throw new BlockDoesNotExistException(ExceptionMessage.UFS_BLOCK_DOES_NOT_EXIST_FOR_SESSION,
+            blockId, sessionId);
+      } catch (Throwable e) {
+        LOG.error("UFS Block does not exist.", e);
+        throw e;
+      }
+    }
+    return blockMeta;
   }
 
   private static class Key {
--- core/server/worker/src/main/java/alluxio/worker/block/meta/UfsBlockMeta.java
+++ core/server/worker/src/main/java/alluxio/worker/block/meta/UfsBlockMeta.java
@@ -34,11 +34,26 @@ public final class UfsBlockMeta {
   private BlockReader mBlockReader;
   private BlockWriter mBlockWriter;
 
+  /**
+   * Creates {@link UfsBlockMeta} from a {@link LockBlockTOptions}.
+   *
+   * @param sessionId the session ID
+   * @param blockId the block ID
+   * @param options the thrift lock options
+   * @return the {@link UfsBlockMeta}
+   */
   public static UfsBlockMeta fromLockBlockOptions(long sessionId, long blockId,
       LockBlockTOptions options) {
     return new UfsBlockMeta(sessionId, blockId, options);
   }
 
+  /**
+   * Creates a {@link UfsBlockMeta}.
+   *
+   * @param sessionId the session ID
+   * @param blockId the block ID
+   * @param options the thrift lock block options
+   */
   private UfsBlockMeta(long sessionId, long blockId, LockBlockTOptions options) {
     mSessionId = sessionId;
     mBlockId = blockId;
@@ -47,26 +62,91 @@ public final class UfsBlockMeta {
     mBlockSize = options.getBlockSize();
   }
 
+  /**
+   * @return the session ID
+   */
   public long getSessionId() {
     return mSessionId;
   }
 
+  /**
+   * @return the block ID
+   */
   public long getBlockId() {
     return mBlockId;
   }
 
+  /**
+   * @return the UFS path
+   */
   public String getUfsPath() {
     return mUfsPath;
   }
 
+  /**
+   * @return the offset of the block in the UFS file
+   */
   public long getOffset() {
     return mOffset;
   }
 
+  /**
+   * @return the block size in bytes
+   */
   public long getBlockSize() {
     return mBlockSize;
   }
 
+  /**
+   * @return the cached the block reader if it is not closed
+   */
+  public BlockReader getBlockReader() {
+    if (mBlockReader != null && mBlockReader.isClosed()) {
+      mBlockReader = null;
+    }
+    return mBlockReader;
+  }
+
+  /**
+   * @return the block writer
+   */
+  public BlockWriter getBlockWriter() {
+    return mBlockWriter;
+  }
+
+  /**
+   * @return true if the block is pending to be committed in the Alluxio block store
+   */
+  public boolean getCommitPending() {
+    return mCommitPending;
+  }
+
+  /**
+   * @param commitPending set to true if the block is pending to be committed
+   */
+  public void setCommitPending(boolean commitPending) {
+    mCommitPending = commitPending;
+  }
+
+  /**
+   * @param blockReader the block reader to be set
+   */
+  public void setBlockReader(BlockReader blockReader) {
+    mBlockReader = blockReader;
+  }
+
+  /**
+   * @param blockWriter the block writer to be set
+   */
+  public void setBlockWriter(BlockWriter blockWriter) {
+    mBlockWriter = blockWriter;
+  }
+
+  /**
+   * Closes the block reader or writer.
+   *
+   * @throws IOException if it fails to close block reader or writer
+   */
   public void closeReaderOrWriter() throws IOException {
     if (mBlockReader != null) {
       mBlockReader.close();
@@ -76,33 +156,6 @@ public final class UfsBlockMeta {
       mBlockWriter.close();
       mBlockWriter = null;
     }
-  }
-
-  public BlockReader getBlockReader() {
-    if (mBlockReader != null && mBlockReader.isClosed()) {
-      mBlockReader = null;
-    }
-    return mBlockReader;
-  }
-
-  public BlockWriter getBlockWriter() {
-    return mBlockWriter;
-  }
-
-  public boolean getCommitPending() {
-    return mCommitPending;
-  }
-
-  public void setCommitPending(boolean commitPending) {
-    mCommitPending = commitPending;
-  }
-
-  public void setBlockReader(BlockReader blockReader) {
-    mBlockReader = blockReader;
-  }
-
-  public void setBlockWriter(BlockWriter blockWriter) {
-    mBlockWriter = blockWriter;
   }
 }
 
