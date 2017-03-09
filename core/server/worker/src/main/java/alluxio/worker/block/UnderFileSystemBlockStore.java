e53bb6aa8508a2f67448ff87c2bf916d2ef71d1a
Pei Sun <peis@alluxio.com>
2017-03-09 14:36:40 -0800

    Fix a conflict caused by revert

--- core/server/worker/src/main/java/alluxio/worker/block/UnderFileSystemBlockStore.java
+++ core/server/worker/src/main/java/alluxio/worker/block/UnderFileSystemBlockStore.java
@@ -248,18 +248,12 @@ public final class UnderFileSystemBlockStore {
    * @throws BlockDoesNotExistException if the UFS block does not exist in the
    * {@link UnderFileSystemBlockStore}
    */
-  private BlockInfo getBlockInfo(long sessionId, long blockId)
-      throws BlockDoesNotExistException {
+  private BlockInfo getBlockInfo(long sessionId, long blockId) throws BlockDoesNotExistException {
     Key key = new Key(sessionId, blockId);
     BlockInfo blockInfo = mBlocks.get(key);
     if (blockInfo == null) {
-      try {
-        throw new BlockDoesNotExistException(ExceptionMessage.UFS_BLOCK_DOES_NOT_EXIST_FOR_SESSION,
-            blockId, sessionId);
-      } catch (Throwable e) {
-        LOG.error("UFS Block does not exist.", e);
-        throw e;
-      }
+      throw new BlockDoesNotExistException(ExceptionMessage.UFS_BLOCK_DOES_NOT_EXIST_FOR_SESSION,
+          blockId, sessionId);
     }
     return blockInfo;
   }
