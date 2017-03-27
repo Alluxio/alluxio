e9d717d340107db938ed43aec10cb65d086af0e9
Wu Hang <wuhang0613@gmail.com>
2017-03-27 00:28:15 -0400

    aboslutepath

--- shell/src/main/java/alluxio/shell/command/CopyToLocalCommand.java
+++ shell/src/main/java/alluxio/shell/command/CopyToLocalCommand.java
@@ -51,7 +51,7 @@ public final class CopyToLocalCommand extends AbstractShellCommand {
   public void run(CommandLine cl) throws AlluxioException, IOException {
     String[] args = cl.getArgs();
     String dst = args[1];
-    cl.getArgList().set(1, "file://" + new File(dst).getCanonicalPath());
+    cl.getArgList().set(1, "file://" + new File(dst).getAbsolutePath());
     mCpCommand.run(cl);
   }
 
