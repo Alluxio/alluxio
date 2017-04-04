447a9fae7d7c79d054eaf9f45e10291f8bbd5521
Pei Sun <peis@alluxio.com>
2017-04-04 10:44:45 -0700

    unittests

---
+++ tests/src/test/java/alluxio/master/journal/ufs/JournalCheckpointThreadTest.java
@@ -0,0 +1,192 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master.journal.ufs;
+
+import alluxio.Configuration;
+import alluxio.PropertyKey;
+import alluxio.master.Master;
+import alluxio.master.journal.JournalCheckpointThread;
+import alluxio.master.journal.JournalWriter;
+import alluxio.master.journal.options.JournalWriterOptions;
+import alluxio.proto.journal.Journal;
+import alluxio.underfs.UnderFileSystem;
+import alluxio.util.CommonUtils;
+import alluxio.util.URIUtils;
+import alluxio.util.WaitForOptions;
+
+import com.google.common.base.Function;
+import org.apache.thrift.TProcessor;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * Unit tests for {@link alluxio.master.journal.JournalCheckpointThread}.
+ */
+public final class JournalCheckpointThreadTest {
+  private static class FakeMaster implements Master {
+    private Queue<Journal.JournalEntry> mEntries;
+
+    FakeMaster() {
+      mEntries = new ArrayDeque<>();
+    }
+
+    @Override
+    public Map<String, TProcessor> getServices() {
+      return null;
+    }
+
+    @Override
+    public String getName() {
+      return "FakeMaster";
+    }
+
+    @Override
+    public Set<Class<?>> getDependencies() {
+      return null;
+    }
+
+    @Override
+    public void processJournalEntry(Journal.JournalEntry entry) throws IOException {
+      mEntries.add(entry);
+    }
+
+    @Override
+    public void start(boolean isPrimary) throws IOException {
+    }
+
+    @Override
+    public void stop() throws IOException {
+    }
+
+    @Override
+    public Iterator<Journal.JournalEntry> iterator() {
+      return mEntries.iterator();
+    }
+  }
+
+  @Rule
+  public TemporaryFolder mFolder = new TemporaryFolder();
+
+  private UfsJournal mJournal;
+  private UnderFileSystem mUfs;
+
+  @Before
+  public void before() throws Exception {
+    URI location = URIUtils
+        .appendPathOrDie(new URI(mFolder.newFolder().getAbsolutePath()), "FileSystemMaster");
+    mUfs = Mockito.spy(UnderFileSystem.Factory.get(location.toString()));
+    mJournal = new UfsJournal(location, mUfs);
+  }
+
+  @After
+  public void after() throws Exception {
+    Configuration.defaultInit();
+  }
+
+  /**
+   * The checkpoint thread replays all the logs and checkpoint periodically if not shutdown.
+   */
+  @Test
+  public void checkpointBeforeShutdown() throws Exception {
+    Configuration.set(PropertyKey.MASTER_JOURNAL_CHECKPOINT_PERIOD_ENTRIES, "2");
+    buildCompletedLog(0, 10);
+    buildIncompleteLog(10, 15);
+    FakeMaster fakeMaster = new FakeMaster();
+    JournalCheckpointThread checkpointThread = new JournalCheckpointThread(fakeMaster, mJournal);
+    checkpointThread.start();
+    CommonUtils.waitFor("checkpoint", new Function<Void, Boolean>() {
+      @Override
+      public Boolean apply(Void input) {
+        try {
+          UfsJournal.Snapshot snapshot = mJournal.getSnapshot();
+          if (!snapshot.mCheckpoints.isEmpty()
+              && snapshot.mCheckpoints.get(snapshot.mCheckpoints.size() - 1).getEnd() == 10) {
+            return true;
+          }
+        } catch (IOException e) {
+          return false;
+        }
+        return false;
+      }
+    }, WaitForOptions.defaults().setTimeout(20000));
+    UfsJournal.Snapshot snapshot = mJournal.getSnapshot();
+    Assert.assertEquals(5, snapshot.mCheckpoints.size());
+    for (int i = 0; i < 5; ++i) {
+      Assert.assertEquals(i * 2 + 2, snapshot.mCheckpoints.get(i).getEnd());
+    }
+    checkpointThread.awaitTermination();
+  }
+
+  /**
+   * The checkpoint thread replays all the logs before shutting down.
+   */
+  @Test
+  public void checkpointAfterShutdown() throws Exception {
+    Configuration.set(PropertyKey.MASTER_JOURNAL_CHECKPOINT_PERIOD_ENTRIES, "2");
+    buildCompletedLog(0, 10);
+    buildIncompleteLog(10, 15);
+    FakeMaster fakeMaster = new FakeMaster();
+    JournalCheckpointThread checkpointThread = new JournalCheckpointThread(fakeMaster, mJournal);
+    checkpointThread.start();
+    checkpointThread.awaitTermination();
+
+    // Make sure all the journal entries have been processed. Note that it is not necessary that
+    // the they are checkpointed.
+    Iterator<Journal.JournalEntry> it = fakeMaster.iterator();
+    int sz = 0;
+    while (it.hasNext()) {
+      it.next();
+      sz++;
+    }
+    Assert.assertEquals(10, sz);
+  }
+
+  private void buildCompletedLog(long start, long end) throws Exception {
+    Mockito.when(mUfs.supportsFlush()).thenReturn(true);
+    JournalWriter writer = mJournal.getWriter(
+        JournalWriterOptions.defaults().setPrimary(true).setNextSequenceNumber(start));
+    for (long i = start; i < end; ++i) {
+      writer.write(newEntry(i));
+    }
+    writer.close();
+  }
+
+  private void buildIncompleteLog(long start, long end) throws Exception {
+    Mockito.when(mUfs.supportsFlush()).thenReturn(true);
+    buildCompletedLog(start, end);
+    Assert.assertTrue(mUfs.renameFile(mJournal.encodeLogFileLocation(start, end).toString(),
+        mJournal.encodeLogFileLocation(start, UfsJournal.UNKNOWN_SEQUENCE_NUMBER).toString()));
+  }
+
+  /**
+   * Creates a dummy journal entry with the given sequence number.
+   *
+   * @param sequenceNumber the sequence number
+   * @return the journal entry
+   */
+  private Journal.JournalEntry newEntry(long sequenceNumber) {
+    return Journal.JournalEntry.newBuilder().setSequenceNumber(sequenceNumber).build();
+  }
+}
---
+++ tests/src/test/java/alluxio/master/journal/ufs/UfsJournalTest.java
@@ -0,0 +1,179 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master.journal.ufs;
+
+import alluxio.util.URIUtils;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.net.URI;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * Unit tests for {@link UfsJournal}.
+ */
+public final class UfsJournalTest {
+  @Rule
+  public TemporaryFolder mFolder = new TemporaryFolder();
+
+  private UfsJournal mJournal;
+
+  @Before
+  public void before() throws Exception {
+    mJournal = new UfsJournal(URIUtils
+        .appendPathOrDie(new URI(mFolder.newFolder().getAbsolutePath()), "FileSystemMaster"));
+  }
+
+  /**
+   * Encodes/decodes completed log file names.
+   */
+  @Test
+  public void completedLogFilename() throws Exception {
+    String location = mJournal.encodeLogFileLocation(0x10, 0x100).toString();
+    Assert.assertEquals(URIUtils.appendPathOrDie(mJournal.getLogDir(), "0x10-0x100").toString(),
+        location);
+    UfsJournalFile file = mJournal.decodeCheckpointOrLogFile("0x10-0x100", false);
+    Assert.assertTrue(file.isCompletedLog());
+    Assert.assertEquals(0x10, file.getStart());
+    Assert.assertEquals(0x100, file.getEnd());
+    Assert.assertEquals(location, file.getLocation().toString());
+  }
+
+  /**
+   * Encodes/decodes incomplete log file names.
+   */
+  @Test
+  public void incompleteLogFilename() throws Exception {
+    String location =
+        mJournal.encodeLogFileLocation(0x10, UfsJournal.UNKNOWN_SEQUENCE_NUMBER).toString();
+    String expectedFilename = "0x10-0x" + Long.toHexString(UfsJournal.UNKNOWN_SEQUENCE_NUMBER);
+    Assert.assertEquals(URIUtils.appendPathOrDie(mJournal.getLogDir(), expectedFilename).toString(),
+        location);
+    UfsJournalFile file = mJournal.decodeCheckpointOrLogFile(expectedFilename, false);
+    Assert.assertTrue(file.isIncompleteLog());
+    Assert.assertEquals(0x10, file.getStart());
+    Assert.assertEquals(UfsJournal.UNKNOWN_SEQUENCE_NUMBER, file.getEnd());
+    Assert.assertEquals(location, file.getLocation().toString());
+  }
+
+  /**
+   * Encodes/decodes checkpoint filename.
+   */
+  @Test
+  public void checkpointFilename() throws Exception {
+    String location = mJournal.encodeCheckpointFileLocation(0x10).toString();
+    String expectedFilename = "0x0-0x10";
+    Assert.assertEquals(
+        URIUtils.appendPathOrDie(mJournal.getCheckpointDir(), expectedFilename).toString(),
+        location);
+    UfsJournalFile file = mJournal.decodeCheckpointOrLogFile(expectedFilename, true);
+    Assert.assertTrue(file.isCheckpoint());
+    Assert.assertEquals(0x0, file.getStart());
+    Assert.assertEquals(0x10, file.getEnd());
+    Assert.assertEquals(location, file.getLocation().toString());
+  }
+
+  /**
+   * Encodes/decodes temporary checkpoint filename.
+   */
+  @Test
+  public void temporaryCheckpointFilename() throws Exception {
+    String location = mJournal.encodeTemporaryCheckpointFileLocation().toString();
+    Assert.assertTrue(location.startsWith(mJournal.getTmpDir().toString()));
+    UfsJournalFile file =
+        mJournal.decodeTemporaryCheckpointFile(location.substring(location.lastIndexOf('/') + 1));
+    Assert.assertTrue(file.isTmpCheckpoint());
+    Assert.assertEquals(UfsJournal.UNKNOWN_SEQUENCE_NUMBER, file.getStart());
+    Assert.assertEquals(UfsJournal.UNKNOWN_SEQUENCE_NUMBER, file.getEnd());
+    Assert.assertEquals(location, file.getLocation().toString());
+  }
+
+  /**
+   * Tests snapshotting the journal.
+   */
+  @Test
+  public void snapshot() throws Exception {
+    String c1 = mJournal.encodeCheckpointFileLocation(0x10).toString();
+    String c2 = mJournal.encodeCheckpointFileLocation(0x12).toString();
+    mJournal.getUfs().create(c1).close();
+    mJournal.getUfs().create(c2).close();
+
+    String t = mJournal.encodeTemporaryCheckpointFileLocation().toString();
+    mJournal.getUfs().create(t).close();
+
+    long start = 0x11;
+    Queue<String> expectedLogs = new ArrayDeque<>();
+    for (int i = 0; i < 10; ++i) {
+      String l = mJournal.encodeLogFileLocation(start + i, start + i + 2).toString();
+      expectedLogs.add(l);
+      mJournal.getUfs().create(l).close();
+      start = start + i + 2;
+    }
+    String currentLog =
+        mJournal.encodeLogFileLocation(start, UfsJournal.UNKNOWN_SEQUENCE_NUMBER).toString();
+    expectedLogs.add(currentLog);
+    mJournal.getUfs().create(currentLog).close();
+
+    // Write a malformed log file which should be skipped.
+    mJournal.getUfs().create(mJournal.encodeLogFileLocation(0x10, 0x100).toString() + ".tmp")
+        .close();
+
+    UfsJournal.Snapshot snapshot = mJournal.getSnapshot();
+    Assert.assertEquals(c1, snapshot.mCheckpoints.get(0).getLocation().toString());
+    Assert.assertEquals(c2, snapshot.mCheckpoints.get(1).getLocation().toString());
+
+    Assert.assertEquals(t, snapshot.mTemporaryCheckpoints.get(0).getLocation().toString());
+
+    Assert.assertEquals(expectedLogs.size(), snapshot.mLogs.size());
+    for (UfsJournalFile log : snapshot.mLogs) {
+      Assert.assertEquals(expectedLogs.poll(), log.getLocation().toString());
+    }
+
+    Assert.assertEquals(currentLog, mJournal.getCurrentLog().getLocation().toString());
+
+    Assert.assertEquals(0x12, mJournal.getNextLogSequenceToCheckpoint());
+  }
+
+  /**
+   * Tests formatting journal.
+   */
+  @Test
+  public void format() throws Exception {
+    mJournal.getUfs().create(mJournal.encodeCheckpointFileLocation(0x12).toString()).close();
+    mJournal.getUfs().create(mJournal.encodeTemporaryCheckpointFileLocation().toString()).close();
+
+    long start = 0x11;
+    for (int i = 0; i < 10; ++i) {
+      String l = mJournal.encodeLogFileLocation(start + i, start + i + 2).toString();
+      mJournal.getUfs().create(l).close();
+      start = start + i + 2;
+    }
+    String currentLog =
+        mJournal.encodeLogFileLocation(start, UfsJournal.UNKNOWN_SEQUENCE_NUMBER).toString();
+    mJournal.getUfs().create(currentLog).close();
+    // Write a malformed log file which should be skipped.
+    mJournal.getUfs().create(mJournal.encodeLogFileLocation(0x10, 0x100).toString() + ".tmp")
+        .close();
+
+    mJournal.format();
+    Assert.assertTrue(mJournal.isFormatted());
+    UfsJournal.Snapshot snapshot = mJournal.getSnapshot();
+    Assert.assertTrue(snapshot.mCheckpoints.isEmpty());
+    Assert.assertTrue(snapshot.mLogs.isEmpty());
+    Assert.assertTrue(snapshot.mTemporaryCheckpoints.isEmpty());
+  }
+}
