ce53a6874b919d79f06e563b6edd0c7e8302e33f
Bin Fan <fanbin103@gmail.com>
2017-04-05 17:18:55 -0700

    [ALLUXIO-2743] Fix style check

--- core/common/src/main/java/alluxio/PropertyKey.java
+++ core/common/src/main/java/alluxio/PropertyKey.java
@@ -31,9 +31,9 @@ import javax.annotation.concurrent.ThreadSafe;
 public class PropertyKey {
   // The following two maps must be the first to initialize within this file.
   /** A map from default property key's string name to the key. */
-  private static Map<String, PropertyKey> DEFAULT_KEYS_MAP = new HashMap<>();
+  private static final Map<String, PropertyKey> DEFAULT_KEYS_MAP = new HashMap<>();
   /** A map from default property key's string name to the key. */
-  private static Map<PropertyKey, Object> DEFAULT_VALUES = new HashMap<>();
+  private static final Map<PropertyKey, Object> DEFAULT_VALUES = new HashMap<>();
 
   public static final PropertyKey CONF_DIR =
       create(Name.CONF_DIR, String.format("${%s}/conf", Name.HOME));
