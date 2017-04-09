3b9103515ecc03c202d09be1ab17a385c139cb73
qiusg <3182703560@qq.com>
2017-04-09 05:39:17 -0400

    Specify null variable in FileOutStream.java precondition check

--- core/client/src/main/java/alluxio/client/file/FileOutStream.java
+++ core/client/src/main/java/alluxio/client/file/FileOutStream.java
@@ -106,7 +106,8 @@ public class FileOutStream extends AbstractOutStream {
   public FileOutStream(AlluxioURI path, OutStreamOptions options, FileSystemContext context,
       UnderFileSystemFileOutStream.Factory underOutStreamFactory) throws IOException {
     mCloser = Closer.create();
-    mUri = Preconditions.checkNotNull(path);
+    //mUri = Preconditions.checkNotNull(path);
+    mUri = Preconditions.checkNotNull(path,"path");
     mBlockSize = options.getBlockSizeBytes();
     mAlluxioStorageType = options.getAlluxioStorageType();
     mUnderStorageType = options.getUnderStorageType();
