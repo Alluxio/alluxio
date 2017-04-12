d8bd1849bc5f32edbf153e5a650404ce1323d95a
madanadit <adit@alluxio.com>
2017-04-11 18:43:42 -0700

    update delete thrift options

--- core/common/src/thrift/file_system_master.thrift
+++ core/common/src/thrift/file_system_master.thrift
@@ -31,6 +31,7 @@ struct CreateFileTOptions {
 struct DeleteTOptions {
   1: optional bool recursive
   2: optional bool alluxioOnly
+  3: optional bool skipConsistencyCheck
 }
 
 struct FreeTOptions {
