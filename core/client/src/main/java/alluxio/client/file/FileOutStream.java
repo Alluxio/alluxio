69c22e8571ebba4de1500dc92c33c9a6e9f33932
Calvin Jia <jia.calvin@gmail.com>
2017-04-13 16:26:01 -0700

    Checkstyle.

--- core/client/src/main/java/alluxio/client/file/FileOutStream.java
+++ core/client/src/main/java/alluxio/client/file/FileOutStream.java
@@ -31,7 +31,6 @@ import alluxio.wire.WorkerNetAddress;
 
 import com.codahale.metrics.Counter;
 import com.google.common.base.Preconditions;
-import com.google.common.base.Throwables;
 import com.google.common.io.Closer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
