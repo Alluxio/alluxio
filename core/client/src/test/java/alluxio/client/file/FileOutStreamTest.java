048fbc7d89844421f65e4e09223d36d4ed2ba28e
Calvin Jia <jia.calvin@gmail.com>
2017-04-13 13:15:49 -0700

    Clean up file out stream test.

--- core/client/src/test/java/alluxio/client/file/FileOutStreamTest.java
+++ core/client/src/test/java/alluxio/client/file/FileOutStreamTest.java
@@ -15,7 +15,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyByte;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -35,15 +34,12 @@ import alluxio.client.block.stream.BlockOutStream;
 import alluxio.client.block.stream.TestBlockOutStream;
 import alluxio.client.block.stream.UnderFileSystemFileOutStream;
 import alluxio.client.file.options.CompleteFileOptions;
-import alluxio.client.file.options.CompleteUfsFileOptions;
-import alluxio.client.file.options.CreateUfsFileOptions;
 import alluxio.client.file.options.OutStreamOptions;
 import alluxio.client.util.ClientTestUtils;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.PreconditionMessage;
 import alluxio.resource.DummyCloseableResource;
 import alluxio.security.GroupMappingServiceTestUtils;
-import alluxio.security.authorization.Mode;
 import alluxio.util.io.BufferUtils;
 import alluxio.wire.FileInfo;
 import alluxio.wire.WorkerNetAddress;
@@ -81,13 +77,10 @@ public class FileOutStreamTest {
 
   private static final long BLOCK_LENGTH = 100L;
   private static final AlluxioURI FILE_NAME = new AlluxioURI("/file");
-  /** Used if ufs operation delegation is enabled. */
-  private static final long UFS_FILE_ID = 1L;
 
   private FileSystemContext mFileSystemContext;
   private AlluxioBlockStore mBlockStore;
   private FileSystemMasterClient mFileSystemMasterClient;
-  private FileSystemWorkerClient mWorkerClient;
 
   private Map<Long, TestBlockOutStream> mAlluxioOutStreamMap;
   private ByteArrayOutputStream mUnderStorageOutputStream;
@@ -115,12 +108,6 @@ public class FileOutStreamTest {
         .thenReturn(new DummyCloseableResource<>(mFileSystemMasterClient));
     when(mFileSystemMasterClient.getStatus(any(AlluxioURI.class))).thenReturn(
         new URIStatus(new FileInfo()));
-
-    // Worker file client mocking
-    mWorkerClient = PowerMockito.mock(FileSystemWorkerClient.class);
-    when(mFileSystemContext.createFileSystemWorkerClient()).thenReturn(mWorkerClient);
-    when(mWorkerClient.createUfsFile(any(AlluxioURI.class), any(CreateUfsFileOptions.class)))
-        .thenReturn(UFS_FILE_ID);
 
     // Return sequentially increasing numbers for new block ids
     when(mFileSystemMasterClient.getNewBlockIdForFile(FILE_NAME))
@@ -256,8 +243,9 @@ public class FileOutStreamTest {
       Assert.assertTrue(mAlluxioOutStreamMap.get(streamIndex).isClosed());
       Assert.assertTrue(mAlluxioOutStreamMap.get(streamIndex).isCanceled());
     }
-    // Don't persist or complete the file if the stream was canceled
-    verify(mWorkerClient, times(0)).completeUfsFile(UFS_FILE_ID, CompleteUfsFileOptions.defaults());
+    // Don't complete the file if the stream was canceled
+    verify(mFileSystemMasterClient, times(0)).completeFile(any(AlluxioURI.class),
+        any(CompleteFileOptions.class));
   }
 
   /**
