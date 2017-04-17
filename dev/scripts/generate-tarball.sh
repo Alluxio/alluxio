8a10402b42f063ba19b47c0c224fb6fdadd1cc05
Yupeng Fu <yupeng.fu@gmail.com>
2017-04-17 16:05:58 -0700

    more comments

--- dev/scripts/generate-tarball.sh
+++ dev/scripts/generate-tarball.sh
@@ -42,15 +42,15 @@ rsync -aq --exclude='logs' --exclude='dev' ${HOME} ${REPO_COPY}
 pushd ${REPO_COPY} > /dev/null
 git clean -fdx
 git reset --hard HEAD
-mkdir ${CLIENT_DIR}
+mkdir -p ${CLIENT_DIR}
 for PROFILE in "${FRAMEWORKS[@]}"; do
-echo "Running build ${PROFILE} and logging to ${BUILD_LOG}"
+  echo "Running build ${PROFILE} and logging to ${BUILD_LOG}"
   mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos -P${PROFILE} ${BUILD_OPTS} | tee ${BUILD_LOG} 2>&1
   # Temporarily create alluxio-env.sh so that we can call bin/alluxio version
   touch conf/alluxio-env.sh
   VERSION=$(bin/alluxio version)
   rm conf/alluxio-env.sh
-  mkdir ${CLIENT_DIR}/${PROFILE}
+  mkdir -p ${CLIENT_DIR}/${PROFILE}
   cp core/client/target/alluxio-core-client-${VERSION}-jar-with-dependencies.jar ${CLIENT_DIR}/${PROFILE}/alluxio-${VERSION}-${PROFILE}-client.jar
 done
 PREFIX=alluxio-${VERSION}
