28b9a55928a79d6f8c23b3f7f0ac88da7acc4816
Bin Fan <fanbin103@gmail.com>
2017-04-23 09:38:57 -0700

    [ALLUXIO-2743] Add unit test for MountTable.getMountInfo

--- core/server/master/src/test/java/alluxio/master/file/meta/MountTableTest.java
+++ core/server/master/src/test/java/alluxio/master/file/meta/MountTableTest.java
@@ -249,4 +249,22 @@ public final class MountTableTest {
     }
     Assert.assertEquals(mountTable, mMountTable.getMountTable());
   }
+
+  /**
+   * Tests the method for getting mount info given ufs id.
+   */
+  @Test
+  public void getMountInfo() throws Exception {
+    MountInfo info1 =
+        new MountInfo(new AlluxioURI("hdfs://localhost:5678/foo"), 1L, MountOptions.defaults());
+    MountInfo info2 =
+        new MountInfo(new AlluxioURI("hdfs://localhost:5678/bar"), 2L, MountOptions.defaults());
+    mMountTable
+        .add(new AlluxioURI("/mnt/foo"), info1.getUfsUri(), info1.getUfsId(), info1.getOptions());
+    mMountTable
+        .add(new AlluxioURI("/mnt/bar"), info2.getUfsUri(), info2.getUfsId(), info2.getOptions());
+    Assert.assertEquals(info1, mMountTable.getMountInfo(info1.getUfsId()));
+    Assert.assertEquals(info2, mMountTable.getMountInfo(info2.getUfsId()));
+    Assert.assertEquals(null, mMountTable.getMountInfo(3L));
+  }
 }
