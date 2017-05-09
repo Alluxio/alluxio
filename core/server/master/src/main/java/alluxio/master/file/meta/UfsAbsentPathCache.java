b4af265b70e3125e4c059677bae79f2d22d43a54
Gene Pang <gene.pang@gmail.com>
2017-05-09 15:31:40 -0700

    Add javadoc

--- core/server/master/src/main/java/alluxio/master/file/meta/UfsAbsentPathCache.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/UfsAbsentPathCache.java
@@ -27,6 +27,9 @@ import java.util.concurrent.ConcurrentSkipListSet;
 
 import javax.annotation.concurrent.ThreadSafe;
 
+/**
+ * This class caches paths in the UFS which do not exist.
+ */
 @ThreadSafe
 public class UfsAbsentPathCache {
   private static final Logger LOG = LoggerFactory.getLogger(UfsAbsentPathCache.class);
@@ -46,10 +49,22 @@ public class UfsAbsentPathCache {
     mCaches = new ConcurrentHashMapV8<>(8, 0.95f, 8);
   }
 
+  /**
+   * Removes the cache for the given mount id.
+   *
+   * @param mountId the mount id
+   */
   public void removeMountPoint(long mountId) {
     mCaches.remove(mountId);
   }
 
+  /**
+   * Adds the given absent path into the cache. This will sequentially walk down the path to find
+   * the first component which does not exist in the ufs.
+   *
+   * @param path the absent path to add to the cache
+   * @throws InvalidPathException if the path is invalid
+   */
   public void addAbsentPath(AlluxioURI path) throws InvalidPathException {
     MountTable.Resolution resolution = mMountTable.resolve(path);
     AlluxioURI ufsUri = resolution.getUri();
@@ -105,6 +120,12 @@ public class UfsAbsentPathCache {
     }
   }
 
+  /**
+   * Removes an absent path from the cache.
+   *
+   * @param path the path to remove from the cache
+   * @throws InvalidPathException if the path is invalid
+   */
   public void removeAbsentPath(AlluxioURI path) throws InvalidPathException {
     MountTable.Resolution resolution = mMountTable.resolve(path);
     AlluxioURI ufsUri = resolution.getUri();
@@ -125,6 +146,14 @@ public class UfsAbsentPathCache {
     }
   }
 
+  /**
+   * Returns true if the given path is absent, according to this cache. A path is absent if one of
+   * its ancestors is absent.
+   *
+   * @param path the path to check
+   * @return true if the path is absent according to the cache
+   * @throws InvalidPathException if the path is invalid
+   */
   public boolean isAbsent(AlluxioURI path) throws InvalidPathException {
     MountTable.Resolution resolution = mMountTable.resolve(path);
     AlluxioURI ufsUri = resolution.getUri();
@@ -140,6 +169,12 @@ public class UfsAbsentPathCache {
     return false;
   }
 
+  /**
+   * Returns the set representing the cache for the given mount id.
+   *
+   * @param mountId the mount id to get the cache for
+   * @return the cache for the mount id
+   */
   protected ConcurrentSkipListSet<String> getCache(long mountId) {
     ConcurrentSkipListSet<String> set = mCaches.get(mountId);
     if (set != null) {
