8c0e143e17ee4824358cddfc4b2e71bf4c6ad885
mbl <maobaolong@139.com>
2017-05-10 14:12:26 +0800

    Fix findbugs error

--- core/server/common/src/main/java/alluxio/web/LogLevel.java
+++ core/server/common/src/main/java/alluxio/web/LogLevel.java
@@ -73,22 +73,30 @@ public class LogLevel {
   }
 
   private static void process(String urlstring) {
+    BufferedReader in = null;
     try {
       URL url = new URL(urlstring);
       System.out.println("Connecting to " + url);
       URLConnection connection = url.openConnection();
       connection.connect();
 
-      BufferedReader in = new BufferedReader(new InputStreamReader(
+      in = new BufferedReader(new InputStreamReader(
           connection.getInputStream(), Charsets.UTF_8));
       for (String line; (line = in.readLine()) != null; ) {
         if (line.startsWith(MARKER)) {
           System.out.println(TAG.matcher(line).replaceAll(""));
         }
       }
-      in.close();
     } catch (IOException ioe) {
       System.err.println("" + ioe);
+    } finally {
+      if (in != null) {
+        try {
+          in.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
   }
 
