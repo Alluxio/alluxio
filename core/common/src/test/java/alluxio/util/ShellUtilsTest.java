47dedce94122e408b81f697a7c1d6ce0df4b7c26
Andrew Audibert <andrew@alluxio.com>
2017-05-17 20:53:13 -0700

    Add test for space in mount path

--- core/common/src/test/java/alluxio/util/ShellUtilsTest.java
+++ core/common/src/test/java/alluxio/util/ShellUtilsTest.java
@@ -85,6 +85,16 @@ public final class ShellUtilsTest {
   }
 
   @Test
+  public void parseMountInfoSpaceInPath() throws Exception {
+    UnixMountInfo info = ShellUtils.parseMountInfo("/dev/disk4s1 on /Volumes/Space Path"
+        + "(hfs, local, nodev, nosuid, read-only, noowners, quarantine)");
+    assertEquals("/dev/disk4s1", info.getDeviceSpec());
+    assertEquals("/Volumes/Amazon Music", info.getMountPoint());
+    assertEquals(null, info.getFsType());
+    assertEquals(null, info.getOptions().getSize());
+  }
+
+  @Test
   public void getMountInfo() throws Exception {
     assumeTrue(OSUtils.isMacOS() || OSUtils.isLinux());
     List<UnixMountInfo> info = ShellUtils.getUnixMountInfo();
