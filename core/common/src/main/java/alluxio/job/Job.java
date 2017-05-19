7f04c91df34307b0dfb4fc19f17331e7157f63ff
ymniam-MKorch <1013287904@qq.com>
2017-05-19 15:34:33 +0800

    Specify null variable in Job.java precondition check

--- core/common/src/main/java/alluxio/job/Job.java
+++ core/common/src/main/java/alluxio/job/Job.java
@@ -1,4 +1,4 @@
-/*
+/*.
  * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
  * (the "License"). You may not use this work except in compliance with the License, which is
  * available at www.apache.org/licenses/LICENSE-2.0
@@ -34,7 +34,7 @@ public abstract class Job implements Serializable {
    * @param jobConf the job configuration
    */
   public Job(JobConf jobConf) {
-    mJobConf = Preconditions.checkNotNull(jobConf);
+    mJobConf = Preconditions.checkNotNull(jobConf, "jobConf");
   }
 
   /**
