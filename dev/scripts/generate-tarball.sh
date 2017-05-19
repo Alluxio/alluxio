fb189da342212483d4ba9815ab409693ef038ab0
Bin Fan <fanbin103@gmail.com>
2017-05-18 22:35:42 -0700

    [SMALLFIX] Improvement on generate-tarball.sh

--- dev/scripts/generate-tarball.sh
+++ dev/scripts/generate-tarball.sh
@@ -45,10 +45,7 @@ mkdir -p ${CLIENT_DIR}
 for PROFILE in "${FRAMEWORKS[@]}"; do
   echo "Running build ${PROFILE} and logging to ${BUILD_LOG}"
   mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos -P${PROFILE} ${BUILD_OPTS} | tee ${BUILD_LOG} 2>&1
-  # Temporarily create alluxio-env.sh so that we can call bin/alluxio version
-  touch conf/alluxio-env.sh
   VERSION=$(bin/alluxio version)
-  rm conf/alluxio-env.sh
   mkdir -p ${CLIENT_DIR}/${PROFILE}
   cp core/client/runtime/target/alluxio-core-client-runtime-${VERSION}-jar-with-dependencies.jar ${CLIENT_DIR}/${PROFILE}/alluxio-${VERSION}-${PROFILE}-client.jar
 done
@@ -59,7 +56,7 @@ rm -rf ${PREFIX}
 mv alluxio ${PREFIX}
 TARGET=${TARBALL_DIR}/${PREFIX}.tar.gz
 
-gtar -czf ${TARGET} ${PREFIX} --exclude-vcs
+gtar -czf ${TARGET} ${PREFIX} --exclude-vcs --exclude='*.class'
 popd > /dev/null
 
 echo "Generated tarball at ${TARGET}"
