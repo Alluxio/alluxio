5d5e4b78d0d101f0f5d4db09f080039ac3f44a8c
Andrew Audibert <andrew@alluxio.com>
2017-05-22 14:46:23 -0700

    add option for skipping framework client jars when generating tarballs

--- dev/scripts/generate-tarball.sh
+++ dev/scripts/generate-tarball.sh
@@ -21,13 +21,23 @@
 #
 # Example: BUILD_OPTS="-Phadoop-2.7" ./generate-tarball.sh
 #
+# The --skipFrameworks flag may be used to avoid building per-framework clients.
+#
 
 set -e
+
+buildFrameworks=true
+while [[ "$#" > 0 ]]; do
+  case $1 in
+    --skipFrameworks) buildFrameworks=false; shift;;
+    *) echo "Unrecognized option: $1"; exit 1;;
+  esac
+done
 
 THIS=$(cd "$( dirname "$0" )"; pwd)
 cd ${THIS}
 TARBALL_DIR="${THIS}/tarballs"
-WORK_DIR="workdir"
+WORK_DIR="${THIS}/workdir"
 CLIENT_DIR="client"
 FRAMEWORKS=( "flink" "presto" "spark" "hadoop" )
 
@@ -40,15 +50,19 @@ rm -rf ${REPO_COPY}
 rsync -aq --exclude='logs' --exclude='dev' ${HOME} ${REPO_COPY}
 
 pushd ${REPO_COPY} > /dev/null
-git clean -fdX
+git clean -qfdX
 mkdir -p ${CLIENT_DIR}
-for PROFILE in "${FRAMEWORKS[@]}"; do
-  echo "Running build ${PROFILE} and logging to ${BUILD_LOG}"
-  mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos -P${PROFILE} ${BUILD_OPTS} | tee ${BUILD_LOG} 2>&1
-  VERSION=$(bin/alluxio version)
-  mkdir -p ${CLIENT_DIR}/${PROFILE}
-  cp core/client/runtime/target/alluxio-core-client-runtime-${VERSION}-jar-with-dependencies.jar ${CLIENT_DIR}/${PROFILE}/alluxio-${VERSION}-${PROFILE}-client.jar
-done
+if [[ "${buildFrameworks}" == true ]]; then
+  for PROFILE in "${FRAMEWORKS[@]}"; do
+    echo "Running build ${PROFILE} and logging to ${BUILD_LOG}"
+    mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos -P${PROFILE} ${BUILD_OPTS} > ${BUILD_LOG} 2>&1
+    mkdir -p ${CLIENT_DIR}/${PROFILE}
+    cp core/client/runtime/target/alluxio-core-client-runtime-${VERSION}-jar-with-dependencies.jar ${CLIENT_DIR}/${PROFILE}/alluxio-${VERSION}-${PROFILE}-client.jar
+  done
+fi
+echo "Running default build and logging to ${BUILD_LOG}"
+mvn -T 4C clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos ${BUILD_OPTS} > ${BUILD_LOG} 2>&1
+VERSION=$(bin/alluxio version)
 PREFIX=alluxio-${VERSION}
 
 cd ..
