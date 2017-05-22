534f9f624c4fd5f9eac9d6f1585bee591da5d4b9
Pei Sun <peis@alluxio.com>
2017-05-22 15:26:37 -0700

    checkstyle

--- core/server/worker/src/main/java/alluxio/worker/netty/NettyExecutors.java
+++ core/server/worker/src/main/java/alluxio/worker/netty/NettyExecutors.java
@@ -18,7 +18,6 @@ import alluxio.util.ThreadFactoryUtils;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
