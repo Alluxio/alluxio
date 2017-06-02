b7360ea8bebbcd2a9f01f2ee7c5008849becdd4f
maobaolong <maobaolong@139.com>
2017-06-02 17:28:11 +0800

    Fix checkstyle errors.

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -47,7 +47,11 @@ import java.util.TreeMap;
 
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.servlet.ServletContext;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
