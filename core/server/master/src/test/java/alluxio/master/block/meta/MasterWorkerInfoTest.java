3e4fe4af492300a7bb5458196bed59631ebf2b91
Xiaoqi Wu <pandajiuxian@gmail.com>
2017-06-10 11:41:28 +0800

    static import for MasterWorkerInfoTest

--- core/server/master/src/test/java/alluxio/master/block/meta/MasterWorkerInfoTest.java
+++ core/server/master/src/test/java/alluxio/master/block/meta/MasterWorkerInfoTest.java
@@ -11,6 +11,10 @@
 
 package alluxio.master.block.meta;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 import alluxio.Constants;
 import alluxio.MasterStorageTierAssoc;
 import alluxio.StorageTierAssoc;
@@ -20,9 +24,6 @@ import alluxio.wire.WorkerNetAddress;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import org.junit.Assert.assertEquals;
-import org.junit.Assert.assertTrue;
-import org.junit.Assert.assertFalse;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
