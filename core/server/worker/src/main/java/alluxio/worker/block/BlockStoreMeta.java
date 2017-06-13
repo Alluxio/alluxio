46941b16a30e493238924701f938cbf3318eb355
Min Chen <chenminzoe@163.com>
2017-06-13 17:23:00 +0800

    [SMALLFIX] Fix Preconditions.checkNotNull in BlockStoreMeta.java

--- core/server/worker/src/main/java/alluxio/worker/block/BlockStoreMeta.java
+++ core/server/worker/src/main/java/alluxio/worker/block/BlockStoreMeta.java
@@ -77,7 +77,7 @@ public final class BlockStoreMeta {
    * @return A mapping from storage tier alias to blocks
    */
   public Map<String, List<Long>> getBlockList() {
-    Preconditions.checkNotNull(mBlockIdsOnTiers);
+    Preconditions.checkNotNull(mBlockIdsOnTiers, "mBlockIdsOnTiers");
 
     return mBlockIdsOnTiers;
   }
@@ -128,7 +128,7 @@ public final class BlockStoreMeta {
    * @return the number of blocks
    */
   public int getNumberOfBlocks() {
-    Preconditions.checkNotNull(mBlockIdsOnTiers);
+    Preconditions.checkNotNull(mBlockIdsOnTiers, "mBlockIdsOnTiers");
 
     int numberOfBlocks = 0;
     for (List<Long> blockIds : mBlockIdsOnTiers.values()) {
@@ -168,7 +168,7 @@ public final class BlockStoreMeta {
    * @param manager a block metadata manager handle
    */
   private BlockStoreMeta(BlockMetadataManager manager, boolean shouldIncludeBlockIds) {
-    Preconditions.checkNotNull(manager);
+    Preconditions.checkNotNull(manager, "manager");
     for (StorageTier tier : manager.getTiers()) {
       Long capacityBytes = mCapacityBytesOnTiers.get(tier.getTierAlias());
       Long usedBytes = mUsedBytesOnTiers.get(tier.getTierAlias());
