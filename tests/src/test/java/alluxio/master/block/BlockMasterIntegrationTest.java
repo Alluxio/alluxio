8f9eec01ba643586ca1d33e36192903c275710ff
Andrew Audibert <andrew@alluxio.com>
2017-06-14 17:53:26 -0700

    Add test for block deletion journaling

---
+++ tests/src/test/java/alluxio/master/block/BlockMasterIntegrationTest.java
@@ -0,0 +1,63 @@
+package alluxio.master.block;
+
+import alluxio.AlluxioURI;
+import alluxio.client.WriteType;
+import alluxio.client.file.FileSystem;
+import alluxio.client.file.FileSystemTestUtils;
+import alluxio.client.file.URIStatus;
+import alluxio.exception.BlockInfoException;
+import alluxio.master.LocalAlluxioCluster;
+import alluxio.master.MasterRegistry;
+import alluxio.master.MasterTestUtils;
+import alluxio.wire.WorkerNetAddress;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Integration test for block master functionality.
+ */
+public class BlockMasterIntegrationTest {
+
+  private ExecutorService mExecutorsForClient;
+  private LocalAlluxioCluster mCluster;
+
+  @Before
+  public void before() throws Exception {
+    mCluster = new LocalAlluxioCluster();
+    mCluster.initConfiguration();
+    mCluster.start();
+  }
+
+  @Test
+  public void journalBlockDeletion() throws Exception {
+    FileSystem fs = mCluster.getClient();
+    BlockMaster blockMaster = mCluster.getLocalAlluxioMaster().getMasterProcess().getMaster(BlockMaster.class);
+    AlluxioURI file = new AlluxioURI("/test");
+    FileSystemTestUtils.createByteFile(fs, file, WriteType.MUST_CACHE, 10);
+    URIStatus status = fs.getStatus(file);
+    Long blockId = status.getBlockIds().get(0);
+    Assert.assertNotNull(blockMaster.getBlockInfo(blockId));
+    fs.delete(file);
+    WorkerNetAddress workerAddress = mCluster.getWorkerAddress();
+    try {
+      blockMaster.getBlockInfo(blockId);
+      fail("Expected the block to be deleted");
+    } catch (BlockInfoException e) {
+      // expected
+    }
+    mCluster.stop();
+    MasterRegistry registry = MasterTestUtils.createLeaderFileSystemMasterFromJournal();
+    try {
+      registry.get(BlockMaster.class).getBlockInfo(blockId);
+      fail("Expected the block to be deleted after restart");
+    } catch (BlockInfoException e) {
+      // expected
+    }
+    registry.stop();
+  }
+}
