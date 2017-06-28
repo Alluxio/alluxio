9c12ee5027c0030d7a2596b80a7827b7970d8d9f
dayhost <dayhost@users.noreply.github.com>
2017-06-28 00:56:05 -0400

    remove import java.nio.*

--- core/common/src/main/java/alluxio/util/io/FileUtils.java
+++ core/common/src/main/java/alluxio/util/io/FileUtils.java
@@ -19,7 +19,16 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.CopyOption;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.GroupPrincipal;
 import java.nio.file.attribute.PosixFileAttributeView;
