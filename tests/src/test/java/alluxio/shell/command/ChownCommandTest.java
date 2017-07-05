351ce2db3755676840ca7b933cf017310c4c0cd2
Yanqin Jin <yanqinjin.cs@gmail.com>
2017-07-04 22:14:28 -0700

    Modify test case to cover different cases of using chown; more to be added later

--- tests/src/test/java/alluxio/shell/command/ChownCommandTest.java
+++ tests/src/test/java/alluxio/shell/command/ChownCommandTest.java
@@ -12,62 +12,121 @@
 package alluxio.shell.command;
 
 import alluxio.AlluxioURI;
-import alluxio.Configuration;
+import alluxio.ConfigurationRule;
 import alluxio.PropertyKey;
 import alluxio.client.WriteType;
 import alluxio.client.file.FileSystemTestUtils;
 import alluxio.exception.AlluxioException;
-import alluxio.security.GroupMappingServiceTestUtils;
-import alluxio.security.authentication.AuthType;
-import alluxio.security.group.provider.IdentityUserGroupsMapping;
+import alluxio.security.group.GroupMappingService;
 import alluxio.shell.AbstractAlluxioShellTest;
-import alluxio.util.CommonUtils;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Tests for chown command.
  */
 public final class ChownCommandTest extends AbstractAlluxioShellTest {
+  /*
+   * The user and group mappings for testing are:
+   *    alice -> alice,staff
+   *    bob   -> bob,staff
+   */
+  private static final TestUser TEST_USER_1 =
+      new TestUser("alice", "alice,staff");
+  private static final TestUser TEST_USER_2 =
+      new TestUser("bob", "bob,staff");
+
+  @Rule
+  public ConfigurationRule mConfiguration = new ConfigurationRule(ImmutableMap
+      .of(PropertyKey.SECURITY_GROUP_MAPPING_CLASS, FakeUserGroupsMapping.class.getName()));
+
+  /**
+   * A simple structure to represent a user and its groups.
+   */
+  private static final class TestUser {
+    private String mUser;
+    private String mGroup;
+
+    TestUser(String user, String group) {
+      mUser = user;
+      mGroup = group;
+    }
+
+    String getUser() {
+      return mUser;
+    }
+
+    String getGroup() {
+      return mGroup;
+    }
+  }
+
+  /**
+   * Test class implements {@link GroupMappingService} providing user-to-groups mapping.
+   */
+  public static class FakeUserGroupsMapping implements GroupMappingService {
+    private HashMap<String, String> mUserGroups = new HashMap<>();
+
+    /**
+     * Constructor of {@link FakeUserGroupsMapping} to put the user and groups in user-to-groups
+     * HashMap.
+     */
+    public FakeUserGroupsMapping() {
+      mUserGroups.put(TEST_USER_1.getUser(), TEST_USER_1.getGroup());
+      mUserGroups.put(TEST_USER_2.getUser(), TEST_USER_2.getGroup());
+    }
+
+    @Override
+    public List<String> getGroups(String user) throws IOException {
+      if (mUserGroups.containsKey(user)) {
+        return Lists.newArrayList(mUserGroups.get(user).split(","));
+      }
+      return new ArrayList<>();
+    }
+  }
+
   @Test
   public void chown() throws IOException, AlluxioException {
     clearLoginUser();
     FileSystemTestUtils.createByteFile(mFileSystem, "/testFile", WriteType.MUST_CACHE, 10);
-    mFsShell.run("chown", "user1", "/testFile");
+    mFsShell.run("chown", TEST_USER_1.getUser(), "/testFile");
     String owner = mFileSystem.getStatus(new AlluxioURI("/testFile")).getOwner();
-    Assert.assertEquals("user1", owner);
-    mFsShell.run("chown", "user2", "/testFile");
+    Assert.assertEquals(TEST_USER_1.getUser(), owner);
+    mFsShell.run("chown", TEST_USER_2.getUser(), "/testFile");
     owner = mFileSystem.getStatus(new AlluxioURI("/testFile")).getOwner();
-    Assert.assertEquals("user2", owner);
+    Assert.assertEquals(TEST_USER_2.getUser(), owner);
   }
 
   @Test
   public void chownValidOwnerValidGroup() throws Exception {
-    setupTestGroupMappingService();
+    clearLoginUser();
     FileSystemTestUtils.createByteFile(mFileSystem, "/testFile", WriteType.MUST_CACHE, 10);
-    String newOwner = "alice";
-    List<String> groups = CommonUtils.getGroups(newOwner);
-    String group = groups.get(0);
+    String newOwner = TEST_USER_1.getUser();
+    String group = "staff";
     String expectedCommandOutput =
-        "Changed owner:group of /testFile to alice:" +  group + ".";
+        "Changed owner:group of /testFile to " + newOwner + ":" +  group + ".";
     runChownOwnerAndGroup("/testFile", 0, expectedCommandOutput, newOwner, group,
         "chown", newOwner + ":" + group, "/testFile");
   }
 
   @Test
   public void chownInvalidOwnerValidGroup() throws Exception {
-    setupTestGroupMappingService();
+    clearLoginUser();
     FileSystemTestUtils.createByteFile(mFileSystem, "/testFile", WriteType.MUST_CACHE, 10);
-    String user = "alice";
     String nonexistUser = "nonexistuser";
-    List<String> groups = CommonUtils.getGroups(user);
     String originalOwner = mFileSystem.getStatus(new AlluxioURI("/testFile")).getOwner();
     String originalGroup = mFileSystem.getStatus(new AlluxioURI("/testFile")).getGroup();
-    String group = groups.get(0);
+    String group = "staff";
     String expectedCommandOutput = "Could not setOwner for /testFile.";
     runChownOwnerAndGroup("/testFile", -1, expectedCommandOutput, originalOwner, originalGroup,
         "chown", nonexistUser + ":" + group, "/testFile");
@@ -75,9 +134,9 @@ public final class ChownCommandTest extends AbstractAlluxioShellTest {
 
   @Test
   public void chownValidOwnerInvalidGroup() throws Exception {
-    setupTestGroupMappingService();
+    clearLoginUser();
     FileSystemTestUtils.createByteFile(mFileSystem, "/testFile", WriteType.MUST_CACHE, 10);
-    String newOwner = "alice";
+    String newOwner = TEST_USER_1.getUser();
     String nonexistGroup = "nonexistgroup";
     String originalOwner = mFileSystem.getStatus(new AlluxioURI("/testFile")).getOwner();
     String originalGroup = mFileSystem.getStatus(new AlluxioURI("/testFile")).getGroup();
@@ -88,7 +147,7 @@ public final class ChownCommandTest extends AbstractAlluxioShellTest {
 
   @Test
   public void chownInvalidOwnerInvalidGroup() throws Exception {
-    setupTestGroupMappingService();
+    clearLoginUser();
     FileSystemTestUtils.createByteFile(mFileSystem, "/testFile", WriteType.MUST_CACHE, 10);
     String nonexistUser = "nonexistuser";
     String nonexistGroup = "nonexistgroup";
@@ -106,14 +165,14 @@ public final class ChownCommandTest extends AbstractAlluxioShellTest {
   public void chownRecursive() throws IOException, AlluxioException {
     clearLoginUser();
     FileSystemTestUtils.createByteFile(mFileSystem, "/testDir/testFile", WriteType.MUST_CACHE, 10);
-    mFsShell.run("chown", "-R", "user1", "/testDir");
+    mFsShell.run("chown", "-R", TEST_USER_1.getUser(), "/testDir");
     String owner = mFileSystem.getStatus(new AlluxioURI("/testDir/testFile")).getOwner();
-    Assert.assertEquals("user1", owner);
+    Assert.assertEquals(TEST_USER_1.getUser(), owner);
     owner = mFileSystem.getStatus(new AlluxioURI("/testDir")).getOwner();
-    Assert.assertEquals("user1", owner);
-    mFsShell.run("chown", "-R", "user2", "/testDir");
+    Assert.assertEquals(TEST_USER_1.getUser(), owner);
+    mFsShell.run("chown", "-R", TEST_USER_2.getUser(), "/testDir");
     owner = mFileSystem.getStatus(new AlluxioURI("/testDir/testFile")).getOwner();
-    Assert.assertEquals("user2", owner);
+    Assert.assertEquals(TEST_USER_2.getUser(), owner);
   }
 
   /**
@@ -145,17 +204,5 @@ public final class ChownCommandTest extends AbstractAlluxioShellTest {
     String currentGroup = mFileSystem.getStatus(new AlluxioURI(path)).getGroup();
     Assert.assertEquals(expectedOwner, currentOwner);
     Assert.assertEquals(expectedGroup, currentGroup);
-  }
-
-  /**
-   * Set up a group mapping service for test with {@link IdentityUserGroupsMapping}.
-   */
-  private void setupTestGroupMappingService() {
-    clearLoginUser();
-    GroupMappingServiceTestUtils.resetCache();
-    Configuration.set(PropertyKey.SECURITY_GROUP_MAPPING_CLASS,
-        IdentityUserGroupsMapping.class.getName());
-    Configuration.set(PropertyKey.SECURITY_AUTHENTICATION_TYPE, AuthType.SIMPLE.getAuthName());
-    Configuration.set(PropertyKey.SECURITY_AUTHORIZATION_PERMISSION_ENABLED, "true");
   }
 }
