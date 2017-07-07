170bbbf06ddf9252f55593a24d3f8fd4cdb5ae72
Bin Fan <fanbin103@gmail.com>
2017-07-07 11:18:58 -0700

    [ALLUXIO-2907] Fix GCS UFS test

--- underfs/gcs/src/test/java/alluxio/underfs/gcs/GCSUnderFileSystemContractTest.java
+++ underfs/gcs/src/test/java/alluxio/underfs/gcs/GCSUnderFileSystemContractTest.java
@@ -16,19 +16,20 @@ import alluxio.underfs.UnderFileSystem;
 import alluxio.underfs.UnderFileSystemConfiguration;
 
 import com.google.common.base.Preconditions;
+import org.junit.BeforeClass;
 
 /**
  * This UFS contract test will use Google Cloud Storage as the backing store.
  */
 public final class GCSUnderFileSystemContractTest extends AbstractUnderFileSystemContractTest {
-  private static final String INTEGRATION_GCS_BUCKET = "gcsBucket";
-  private String mGCSBucket;
+  private static final String GCS_BUCKET_CONF = "testGCSBucket";
+  private static final String GCS_BUCKET = System.getProperty(GCS_BUCKET_CONF);
 
-  public GCSUnderFileSystemContractTest() {
-    String gcsBucket = System.getProperty(INTEGRATION_GCS_BUCKET);
-    Preconditions.checkState(gcsBucket != null);
-    Preconditions.checkState(new GCSUnderFileSystemFactory().supportsPath(gcsBucket));
-    mGCSBucket = gcsBucket;
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    Preconditions.checkNotNull(GCS_BUCKET);
+    Preconditions.checkState(new GCSUnderFileSystemFactory().supportsPath(GCS_BUCKET),
+        String.format("%s is not a valid GCS path", GCS_BUCKET));
   }
 
   @Override
@@ -39,6 +40,6 @@ public final class GCSUnderFileSystemContractTest extends AbstractUnderFileSyste
 
   @Override
   public String getUfsBaseDir() {
-    return mGCSBucket;
+    return GCS_BUCKET;
   }
 }
