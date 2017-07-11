8871348d3b8e8c001c3c7798eb32b6d9b09ef0ba
YaoLi <417743771@qq.com>
2017-07-11 13:49:38 -0400

    reverted  change from LineageMaster to DefaultLineageMaster in LineageMasterFactory

--- core/server/master/src/main/java/alluxio/master/lineage/LineageMasterFactory.java
+++ core/server/master/src/main/java/alluxio/master/lineage/LineageMasterFactory.java
@@ -26,7 +26,7 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * Factory to create a {@link DefaultLineageMaster} instance.
+ * Factory to create a {@link LineageMaster} instance.
  */
 @ThreadSafe
 public final class LineageMasterFactory implements MasterFactory {
