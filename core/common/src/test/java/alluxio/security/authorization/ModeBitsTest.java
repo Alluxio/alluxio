a393db24ceb1812a305b4fe83073d3c3522ed487
Qiu Hu <whatbeg@users.noreply.github.com>
2017-07-13 13:10:37 +0800

    Update ModeBitsTest.java

--- core/common/src/test/java/alluxio/security/authorization/ModeBitsTest.java
+++ core/common/src/test/java/alluxio/security/authorization/ModeBitsTest.java
@@ -10,6 +10,7 @@
  */
 
 package alluxio.security.authorization;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
