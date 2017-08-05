1d22bf5e3a1c610f2a717de4a02b7c3b229b687a
nika11 <nika.y11@gmail.com>
2017-08-05 10:50:25 -0700

    [ALLUXIO-2889] 2nd task: small fix

--- shell/src/main/java/alluxio/cli/ConfigurationDocGenerator.java
+++ shell/src/main/java/alluxio/cli/ConfigurationDocGenerator.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.concurrent.ThreadSafe;
-//import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
