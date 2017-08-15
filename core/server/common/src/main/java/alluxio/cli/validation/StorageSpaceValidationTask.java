9d811b03cabd5a726a22378e81019160d6265bf6
bf8086 <binfeng@alluxio.com>
2017-08-15 15:20:54 -0700

    improved error message to show additional required free space

--- core/server/common/src/main/java/alluxio/cli/validation/StorageSpaceValidationTask.java
+++ core/server/common/src/main/java/alluxio/cli/validation/StorageSpaceValidationTask.java
@@ -86,11 +86,12 @@ public final class StorageSpaceValidationTask implements ValidationTask {
                 "Tier %d: Not enough space on %s. %n"
                     + "Desired quota: %s%n"
                     + "Used in tiered storage: %s%n"
-                    + "Available: %s%n",
+                    + "Available: %s (Additional %s free space required)%n",
                 level, storageEntry.getKey(),
                 FormatUtils.getSizeFromBytes(quota),
                 FormatUtils.getSizeFromBytes(used),
-                FormatUtils.getSizeFromBytes(available));
+                FormatUtils.getSizeFromBytes(available),
+                FormatUtils.getSizeFromBytes(quota - used - available));
             success = false;
           }
         }
