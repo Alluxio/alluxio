12b043439c7737e3730edb9d63799b44b967a579
Yanqin Jin <yanqinjin.cs@gmail.com>
2017-08-18 23:34:45 -0700

    Add java doc to a class.
    In the javadoc of class FileSystemMasterClientServiceProcess, explain
    the purpose of creating this class.

--- core/server/master/src/main/java/alluxio/master/file/FileSystemMasterClientServiceProcessor.java
+++ core/server/master/src/main/java/alluxio/master/file/FileSystemMasterClientServiceProcessor.java
@@ -7,6 +7,23 @@ import org.apache.thrift.transport.TSaslServerTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
+/**
+ * Alluxio customized Thrift processor to handle RPC and track client IP.
+ *
+ * Original Thrift framework does not expose client IP because:
+ * a) there is no such interface to query IP.
+ * b) IP is not stored by Thrift.
+ * c) The last stop from Thrift framework into Alluxio land is the
+ *    {@link org.apache.thrift.ProcessFunction#process} method of
+ *    {@link org.apache.thrift.ProcessFunction} class. This method has the information of in
+ *    protocol from which we can derive IP. However, this method is made final deliberately.
+ *    Consequently, it is very hard to subclass {@link org.apache.thrift.ProcessFunction}
+ *    and override process method to pass IP informaton to Alluxio via arguments.
+ * Based on a), b) and c), we decide to subclass the auto-generated
+ * {@link FileSystemMasterClientService.Processor} class and
+ * override its {@link org.apache.thrift.ProcessFunction#process} method. In this method, we store
+ * the client IP as a thread-local variable for future use by {@link DefaultFileSystemMaster}.
+ */
 public class FileSystemMasterClientServiceProcessor extends FileSystemMasterClientService.Processor {
   private static ThreadLocal<String> mClientIpThreadLocal;
 
