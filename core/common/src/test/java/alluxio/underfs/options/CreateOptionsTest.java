516411d5ab0f77e8a88cb35de294500d0a33b01c
Gene Pang <gene.pang@gmail.com>
2017-08-21 09:16:44 -0700

    [ALLUXIO-2998] remove unused import

--- core/common/src/test/java/alluxio/underfs/options/CreateOptionsTest.java
+++ core/common/src/test/java/alluxio/underfs/options/CreateOptionsTest.java
@@ -12,7 +12,6 @@
 package alluxio.underfs.options;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import alluxio.CommonTestUtils;
