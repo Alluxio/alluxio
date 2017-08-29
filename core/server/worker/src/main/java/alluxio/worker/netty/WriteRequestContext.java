71b0353eebaa9b31b51b93b61b488c474e661748
Bin Fan <fanbin103@gmail.com>
2017-08-28 18:28:42 -0700

    Update WriteRequestContext.java

--- core/server/worker/src/main/java/alluxio/worker/netty/WriteRequestContext.java
+++ core/server/worker/src/main/java/alluxio/worker/netty/WriteRequestContext.java
@@ -186,7 +186,7 @@ public class WriteRequestContext<T extends WriteRequest> {
   }
 
   /**
-   * @param done whether the SUCCESS or CANCEL response is sent
+   * @param done whether the EOF or CANCEL is received.
    */
   public void setDoneUnsafe(boolean done) {
     mDone = done;
