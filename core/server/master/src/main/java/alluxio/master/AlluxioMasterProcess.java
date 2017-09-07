8e6ecf472c001f6026f35ace4bf594a7c01bafea
whatbeg <huqiu020@163.com>
2017-09-07 19:13:34 +0800

    add Nullable notation for AlluxioMasterProcess.java

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
@@ -47,6 +47,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Map;
 
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
 
 /**
@@ -169,6 +170,7 @@ public class AlluxioMasterProcess implements MasterProcess {
   }
 
   @Override
+  @Nullable
   public InetSocketAddress getWebAddress() {
     if (mWebServer != null) {
       return new InetSocketAddress(mWebServer.getBindHost(), mWebServer.getLocalPort());
