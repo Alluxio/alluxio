17a88346fbd2b16c32b27b677414f70024a2e66a
yindaren <LLiang_Yin@163.com>
2017-09-08 18:48:07 +0800

    small fix

--- core/server/worker/src/test/java/alluxio/worker/block/ClientRWLockTest.java
+++ core/server/worker/src/test/java/alluxio/worker/block/ClientRWLockTest.java
@@ -16,7 +16,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
