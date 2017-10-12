36fed308cdfad3fc7f2aba9c552138345a94fe5e
yangxiaoshuo <1334197843@qq.com>
2017-10-12 11:02:28 +0800

    change code style

--- tests/src/test/java/alluxio/master/file/CheckConsistencyIntegrationTest.java
+++ tests/src/test/java/alluxio/master/file/CheckConsistencyIntegrationTest.java
@@ -33,7 +33,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
 import java.io.OutputStream;
 import java.util.ArrayList;
