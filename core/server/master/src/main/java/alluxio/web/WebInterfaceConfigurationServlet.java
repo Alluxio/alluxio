75fc6cce051cb146cd85598e52ed4baa003f118b
Zhang-HM <1462473940@qq.com>
2017-10-17 16:23:13 +0800

    Update WebInterfaceConfigurationServlet.java

--- core/server/master/src/main/java/alluxio/web/WebInterfaceConfigurationServlet.java
+++ core/server/master/src/main/java/alluxio/web/WebInterfaceConfigurationServlet.java
@@ -31,6 +31,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.google.common.collect.Sets;
 
 /**
