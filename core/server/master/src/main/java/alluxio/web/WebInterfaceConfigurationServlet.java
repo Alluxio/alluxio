429978a8ab586c5ca6846b7720d43b8b9937b0ab
Zhang-HM <1462473940@qq.com>
2017-10-17 16:21:28 +0800

    Update WebInterfaceConfigurationServlet.java

--- core/server/master/src/main/java/alluxio/web/WebInterfaceConfigurationServlet.java
+++ core/server/master/src/main/java/alluxio/web/WebInterfaceConfigurationServlet.java
@@ -31,7 +31,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.common.collect.Sets
+import com.google.common.collect.Sets;
 
 /**
  * Servlet that provides data for displaying the system's configuration.
