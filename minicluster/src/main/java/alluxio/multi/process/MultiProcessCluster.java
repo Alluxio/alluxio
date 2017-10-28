c6bff5c45c44eb1876cc2888ead4253a69facc4c
Andrew Audibert <andrew@alluxio.com>
2017-10-27 17:33:29 -0700

    Fix findbugs

--- minicluster/src/main/java/alluxio/multi/process/MultiProcessCluster.java
+++ minicluster/src/main/java/alluxio/multi/process/MultiProcessCluster.java
@@ -299,21 +299,21 @@ public final class MultiProcessCluster implements TestRule {
   /**
    * @return return the list of master addresses
    */
-  public List<MasterNetAddress> getMasterAddresses() {
+  public synchronized List<MasterNetAddress> getMasterAddresses() {
     return mMasterAddresses;
   }
 
   /**
    * Stops the Zookeeper cluster.
    */
-  public void stopZk() throws IOException {
+  public synchronized void stopZk() throws IOException {
     mCuratorServer.stop();
   }
 
   /**
    * Restarts the Zookeeper cluster.
    */
-  public void restartZk() throws Exception {
+  public synchronized void restartZk() throws Exception {
     Preconditions.checkNotNull(mCuratorServer, "mCuratorServer");
     mCuratorServer.restart();
   }
