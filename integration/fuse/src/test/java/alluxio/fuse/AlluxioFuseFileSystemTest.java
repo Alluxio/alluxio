38d82701494c7ca1364976fcf7a7a10f50bcfa77
Yupeng Fu <yupeng.fu@gmail.com>
2017-10-31 10:56:10 -0700

    Fix test

--- integration/fuse/src/test/java/alluxio/fuse/AlluxioFuseFileSystemTest.java
+++ integration/fuse/src/test/java/alluxio/fuse/AlluxioFuseFileSystemTest.java
@@ -137,14 +137,14 @@ public class AlluxioFuseFileSystemTest {
 
     FileStat stat = new FileStat(Runtime.getSystemRuntime());
     assertEquals(0, mFuseFs.getattr("/foo", stat));
-    assertEquals(status.getLength(), stat.st_size.get());
+    assertEquals(status.getLength(), stat.st_size.longValue());
     assertEquals(status.getLastModificationTimeMs() / 1000, stat.st_ctim.tv_sec.get());
-    assertEquals((status.getLastModificationTimeMs() % 1000) * 1000, stat.st_ctim.tv_nsec.get());
+    assertEquals((status.getLastModificationTimeMs() % 1000) * 1000, stat.st_ctim.tv_nsec.longValue());
     assertEquals(status.getLastModificationTimeMs() / 1000, stat.st_mtim.tv_sec.get());
-    assertEquals((status.getLastModificationTimeMs() % 1000) * 1000, stat.st_mtim.tv_nsec.get());
+    assertEquals((status.getLastModificationTimeMs() % 1000) * 1000, stat.st_mtim.tv_nsec.longValue());
     assertEquals(AlluxioFuseUtils.getUid(System.getProperty("user.name")), stat.st_uid.get());
     assertEquals(AlluxioFuseUtils.getGid(System.getProperty("user.name")), stat.st_gid.get());
-    assertEquals(123 | FileStat.S_IFDIR, stat.st_mode.get());
+    assertEquals(123 | FileStat.S_IFDIR, stat.st_mode.intValue());
   }
 
   @Test
