4e141b613d5d3400d39cd2cff29623e8efc04696
biaos <1440477585@qq.com>
2017-11-13 14:13:03 +0800

    use static import in class CapacityTest

--- core/common/src/test/java/alluxio/wire/CapacityTest.java
+++ core/common/src/test/java/alluxio/wire/CapacityTest.java
@@ -14,7 +14,7 @@ package alluxio.wire;
 import alluxio.CommonTestUtils;
 
 import org.codehaus.jackson.map.ObjectMapper;
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.Random;
@@ -44,9 +44,9 @@ public class CapacityTest {
    * @param b the second Capacity object to be checked
    */
   private void checkEquality(Capacity a, Capacity b) {
-    Assert.assertEquals(a.getTotal(), b.getTotal());
-    Assert.assertEquals(a.getUsed(), b.getUsed());
-    Assert.assertEquals(a, b);
+    assertEquals(a.getTotal(), b.getTotal());
+    assertEquals(a.getUsed(), b.getUsed());
+    assertEquals(a, b);
   }
 
   /**
