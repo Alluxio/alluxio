e042f898a11547ff5b05da2f808e582113177ff4
Cheng Chang <myairia@gmail.com>
2017-11-20 15:16:58 -0800

    Save one RPC call to HDFS.

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystem.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystem.java
@@ -231,9 +231,8 @@ public class HdfsUnderFileSystem extends BaseUnderFileSystem
     }
     List<String> ret = new ArrayList<>();
     try {
-      FileStatus fStatus = mFileSystem.getFileStatus(new Path(path));
       BlockLocation[] bLocations =
-          mFileSystem.getFileBlockLocations(fStatus, options.getOffset(), 1);
+          mFileSystem.getFileBlockLocations(new Path(path), options.getOffset(), 1);
       if (bLocations.length > 0) {
         String[] names = bLocations[0].getHosts();
         Collections.addAll(ret, names);
