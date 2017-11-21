a89d00ed73d0561f1ad8a538ffa2b2a2ad65add6
Gene Pang <gene.pang@gmail.com>
2017-11-21 08:18:16 -0800

    Send worker heartbeat options

--- core/server/worker/src/main/java/alluxio/worker/file/FileDataManager.java
+++ core/server/worker/src/main/java/alluxio/worker/file/FileDataManager.java
@@ -21,6 +21,7 @@ import alluxio.exception.AlluxioException;
 import alluxio.exception.BlockDoesNotExistException;
 import alluxio.exception.InvalidWorkerStateException;
 import alluxio.security.authorization.Mode;
+import alluxio.thrift.UfsFileTStatus;
 import alluxio.underfs.UfsFileStatus;
 import alluxio.underfs.UfsManager;
 import alluxio.underfs.UnderFileSystem;
@@ -349,8 +350,15 @@ public final class FileDataManager {
       return mIdList;
     }
 
-    public List<UfsFileStatus> fileStatusList() {
-      return mFileStatusList;
+    /**
+     * @return list of {@link UfsFileTStatus} of persisted files
+     */
+    public List<UfsFileTStatus> fileStatusTList() {
+      List<UfsFileTStatus> list = new ArrayList<>(mFileStatusList.size());
+      for (UfsFileStatus status : mFileStatusList) {
+        list.add(status.toThrift());
+      }
+      return list;
     }
   }
 }
