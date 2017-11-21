bc1348c63e385918a0cdbd5d0dc2d6e7519f5145
Andrew Audibert <andrew@alluxio.com>
2017-11-20 16:35:33 -0800

    Update LocalFirstAvoidEvictionPolicy for tiered locality

--- core/client/fs/src/main/java/alluxio/client/file/policy/LocalFirstAvoidEvictionPolicy.java
+++ core/client/fs/src/main/java/alluxio/client/file/policy/LocalFirstAvoidEvictionPolicy.java
@@ -11,19 +11,12 @@
 
 package alluxio.client.file.policy;
 
-import alluxio.Configuration;
-import alluxio.PropertyKey;
 import alluxio.client.block.BlockWorkerInfo;
 import alluxio.client.block.policy.BlockLocationPolicy;
 import alluxio.client.block.policy.options.GetWorkerOptions;
-import alluxio.util.network.NetworkAddressUtils;
 import alluxio.wire.WorkerNetAddress;
 
 import com.google.common.base.Objects;
-import com.google.common.collect.Lists;
-
-import java.util.Collections;
-import java.util.List;
 
 import javax.annotation.concurrent.ThreadSafe;
 /**
@@ -37,62 +30,24 @@ import javax.annotation.concurrent.ThreadSafe;
 @ThreadSafe
 public final class LocalFirstAvoidEvictionPolicy
     implements FileWriteLocationPolicy, BlockLocationPolicy {
-  private String mLocalHostName;
+  private final LocalFirstPolicy mPolicy;
 
   /**
    * Constructs a {@link LocalFirstAvoidEvictionPolicy}.
    */
   public LocalFirstAvoidEvictionPolicy() {
-    mLocalHostName = NetworkAddressUtils.getClientHostName();
+    mPolicy = new LocalFirstPolicy(true);
   }
 
   @Override
   public WorkerNetAddress getWorkerForNextBlock(Iterable<BlockWorkerInfo> workerInfoList,
       long blockSizeBytes) {
-    // try the local host first
-    WorkerNetAddress localWorkerNetAddress = null;
-    for (BlockWorkerInfo workerInfo : workerInfoList) {
-      if (workerInfo.getNetAddress().getHost().equals(mLocalHostName)) {
-        localWorkerNetAddress = workerInfo.getNetAddress();
-        if (getAvailableBytes(workerInfo) >= blockSizeBytes) {
-          return localWorkerNetAddress;
-        }
-      }
-    }
-
-    // otherwise randomly pick a worker that has enough availability
-    List<BlockWorkerInfo> shuffledWorkers = Lists.newArrayList(workerInfoList);
-    Collections.shuffle(shuffledWorkers);
-    for (BlockWorkerInfo workerInfo : shuffledWorkers) {
-      if (getAvailableBytes(workerInfo) >= blockSizeBytes) {
-        return workerInfo.getNetAddress();
-      }
-    }
-    if (localWorkerNetAddress == null && shuffledWorkers.size() > 0) {
-      return shuffledWorkers.get(0).getNetAddress();
-    }
-    return localWorkerNetAddress;
+    return mPolicy.getWorkerForNextBlock(workerInfoList, blockSizeBytes);
   }
 
   @Override
   public WorkerNetAddress getWorker(GetWorkerOptions options) {
-    return getWorkerForNextBlock(options.getBlockWorkerInfos(), options.getBlockSize());
-  }
-
-  /**
-   * The information of BlockWorkerInfo is update after a file complete write. To avoid evict,
-   * user should configure "alluxio.user.file.write.avoid.eviction.policy.reserved.size.bytes"
-   * to reserve some space to store the block.
-   *
-   * @param workerInfo BlockWorkerInfo of the worker
-   * @return the available bytes of the worker
-   */
-  private long getAvailableBytes(BlockWorkerInfo workerInfo) {
-    long mUserFileWriteCapacityReserved = Configuration
-            .getBytes(PropertyKey.USER_FILE_WRITE_AVOID_EVICTION_POLICY_RESERVED_BYTES);
-    long mCapacityBytes = workerInfo.getCapacityBytes();
-    long mUsedBytes = workerInfo.getUsedBytes();
-    return mCapacityBytes - mUsedBytes - mUserFileWriteCapacityReserved;
+    return mPolicy.getWorker(options);
   }
 
   @Override
@@ -104,18 +59,18 @@ public final class LocalFirstAvoidEvictionPolicy
       return false;
     }
     LocalFirstAvoidEvictionPolicy that = (LocalFirstAvoidEvictionPolicy) o;
-    return Objects.equal(mLocalHostName, that.mLocalHostName);
+    return Objects.equal(mPolicy, that.mPolicy);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(mLocalHostName);
+    return Objects.hashCode(mPolicy);
   }
 
   @Override
   public String toString() {
     return Objects.toStringHelper(this)
-        .add("localHostName", mLocalHostName)
+        .add("policy", mPolicy)
         .toString();
   }
 }
