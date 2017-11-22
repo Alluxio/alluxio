a4f3d0f6c59db442c36001e649a967393c293816
Yanqin Jin <yanqinjin.cs@gmail.com>
2017-11-21 17:36:49 -0800

    Add javadoc and fix a bug in ValidateEnv#validateLocal.

--- core/server/common/src/main/java/alluxio/cli/ValidateEnv.java
+++ core/server/common/src/main/java/alluxio/cli/ValidateEnv.java
@@ -247,7 +247,7 @@ public final class ValidateEnv {
     }
 
     System.out.println("Validation succeeded.");
-    return false;
+    return true;
   }
 
   private static boolean validateWorkers(String name) throws InterruptedException {
@@ -330,6 +330,17 @@ public final class ValidateEnv {
     System.exit(validate(args));
   }
 
+  /**
+   * Parses the command line arguments and options in {@code args}.
+   *
+   * After successful execution of this method, command line arguments can be
+   * retrieved by invoking {@link CommandLine#getArgs()}, and options can be
+   * retrieved by calling {@link CommandLine#getOptions()}.
+   *
+   * @param args
+   * @return
+   * @throws InvalidArgumentException
+   */
   private static CommandLine parseArgsAndOptions(String... args) throws InvalidArgumentException {
     Options opts = new Options().addOption(HADOOP_CONF_DIR_OPTION);
     CommandLineParser parser = new DefaultParser();
