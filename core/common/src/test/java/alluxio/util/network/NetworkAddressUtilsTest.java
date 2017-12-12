5d17ab2def0ee6052dab8a2530cf97db24727f08
Pavel Simo <pavel.simo@gmail.com>
2017-12-13 01:11:08 +0800

    Improve the usability of the method resolveIpAddress

--- core/common/src/test/java/alluxio/util/network/NetworkAddressUtilsTest.java
+++ core/common/src/test/java/alluxio/util/network/NetworkAddressUtilsTest.java
@@ -286,10 +286,24 @@ public class NetworkAddressUtilsTest {
    */
   @Test
   public void resolveIpAddress() throws UnknownHostException {
-    assertEquals(NetworkAddressUtils.resolveIpAddress(""), null);
-    assertEquals(NetworkAddressUtils.resolveIpAddress(null), null);
     assertEquals(NetworkAddressUtils.resolveIpAddress("localhost"), "127.0.0.1");
     assertEquals(NetworkAddressUtils.resolveIpAddress("127.0.0.1"), "127.0.0.1");
+  }
+
+  /**
+   * Tests the {@link NetworkAddressUtils#resolveIpAddress(String)} method.
+   */
+  @Test(expected = NullPointerException.class)
+  public void resolveNullIpAddress() throws UnknownHostException {
+    NetworkAddressUtils.resolveIpAddress(null);
+  }
+
+  /**
+   * Tests the {@link NetworkAddressUtils#resolveIpAddress(String)} method.
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void resolveEmptyIpAddress() throws UnknownHostException {
+    NetworkAddressUtils.resolveIpAddress("");
   }
 
   /**
