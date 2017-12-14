affd17995d7fe7761afd3d6d465a125567d959d2
Calvin Jia <jia.calvin@gmail.com>
2017-12-14 14:25:25 -0800

    Add a dummy async cache handler.

---
+++ core/server/worker/src/main/java/alluxio/worker/netty/AsyncCacheHandler.java
@@ -0,0 +1,25 @@
+package alluxio.worker.netty;
+
+import alluxio.network.protocol.RPCProtoMessage;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handler for async cache requests.
+ */
+public class AsyncCacheHandler extends ChannelInboundHandlerAdapter {
+  /**
+   * Constructs a new async cache handler.
+   */
+  public AsyncCacheHandler() {}
+
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object object) throws Exception {
+    if (object instanceof RPCProtoMessage
+        && ((RPCProtoMessage) object).getMessage().isAsyncCacheRequest()) {
+      ctx.writeAndFlush(RPCProtoMessage.createOkResponse(null));
+    } else {
+      ctx.fireChannelRead(object);
+    }
+  }
+}
