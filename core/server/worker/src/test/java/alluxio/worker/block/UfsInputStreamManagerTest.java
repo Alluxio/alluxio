c56870274b5018f7a5b49ad56d058c6c91e1cce1
Yupeng Fu <yupeng.fu@gmail.com>
2017-12-14 22:35:04 -0800

    add more tests

--- core/server/worker/src/test/java/alluxio/worker/block/UfsInputStreamManagerTest.java
+++ core/server/worker/src/test/java/alluxio/worker/block/UfsInputStreamManagerTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.io.Closeable;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,13 +24,6 @@ public class UfsInputStreamManagerTest {
   private List<SeekableUnderFileInputStream> mSeekableInStreams;
   private UfsInputStreamManager mManager;
 
-  public ConfigurationRule mConfigurationRule =
-      new ConfigurationRule(new HashMap<PropertyKey, String>() {
-        {
-          put(PropertyKey.WORKER_UFS_INSTREAM_CACHE_EXPIRE_MS, "200000");
-        }
-      });
-
   @Before
   public void before() throws Exception {
     mSeekableInStreams = new ArrayList<>();
@@ -37,9 +31,10 @@ public class UfsInputStreamManagerTest {
     Mockito.when(mUfs.isSeekable()).thenReturn(true);
     for(int i=0;i<3;i++) {
       SeekableUnderFileInputStream instream=createMockedSeekableStream();
-      Mockito.when(mUfs.open(Mockito.eq(FILE_NAME), Mockito.any(OpenOptions.class))).thenReturn(instream);
       mSeekableInStreams.add(instream);
     }
+    Mockito.when(mUfs.open(Mockito.eq(FILE_NAME), Mockito.any(OpenOptions.class))).thenReturn(
+        mSeekableInStreams.get(0), mSeekableInStreams.get(1), mSeekableInStreams.get(2));
     mManager = new UfsInputStreamManager();
   }
 
@@ -63,12 +58,47 @@ public class UfsInputStreamManagerTest {
     InputStream instream1 = mManager.checkOut(mUfs, FILE_NAME, 2);
     // check in back
     mManager.checkIn(instream1);
- // check out a stream again
+    // check out a stream again
     InputStream instream2 = mManager.checkOut(mUfs, FILE_NAME, 4);
 
     Assert.assertEquals(mockedStrem, instream1);
     Assert.assertEquals(mockedStrem, instream2);
     // ensure the second time the checked out instream is the same one but repositioned
     Mockito.verify(mockedStrem).seek(4);
+  }
+
+  @Test
+  public void testMultipleCheckIn() throws Exception {
+    mManager.checkOut(mUfs, FILE_NAME, 2);
+    mManager.checkOut(mUfs, FILE_NAME, 4);
+    mManager.checkOut(mUfs, FILE_NAME, 6);
+    // 3 different input streams are checked out
+    Mockito.verify(mUfs, Mockito.times(3)).open(Mockito.eq(FILE_NAME),
+        Mockito.any(OpenOptions.class));
+  }
+
+  @Test
+  public void testExpire() throws Exception {
+    try (Closeable r = new ConfigurationRule(new HashMap<PropertyKey, String>() {
+      {
+        put(PropertyKey.WORKER_UFS_INSTREAM_CACHE_EXPIRE_MS, "2");
+      }
+    }).toResource()) {
+      mManager = new UfsInputStreamManager();
+      InputStream instream;
+      // check out a stream
+      instream = mManager.checkOut(mUfs, FILE_NAME, 2);
+      // check in back
+      mManager.checkIn(instream);
+      Thread.sleep(10);
+      // check out another stream should trigger the timeout
+      mManager.checkOut(mUfs, FILE_NAME, 4);
+      Mockito.verify(mSeekableInStreams.get(0), Mockito.timeout(1000000).times(1)).close();
+    };
+  }
+
+  @Test
+  public void testConcurrency() throws Exception {
+
   }
 }
