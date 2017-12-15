af2fff9f305e163b194fe6796770d993c1ba6d60
Yupeng Fu <yupeng.fu@gmail.com>
2017-12-14 17:52:42 -0800

    add check-in and then check-out test

---
+++ core/server/worker/src/test/java/alluxio/worker/block/UfsInputStreamManagerTest.java
@@ -0,0 +1,74 @@
+package alluxio.worker.block;
+
+import alluxio.ConfigurationRule;
+import alluxio.PropertyKey;
+import alluxio.underfs.SeekableUnderFileInputStream;
+import alluxio.underfs.UnderFileSystem;
+import alluxio.underfs.options.OpenOptions;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UfsInputStreamManagerTest {
+  private static final String FILE_NAME = "/test";
+
+  private UnderFileSystem mUfs;
+  private List<SeekableUnderFileInputStream> mSeekableInStreams;
+  private UfsInputStreamManager mManager;
+
+  public ConfigurationRule mConfigurationRule =
+      new ConfigurationRule(new HashMap<PropertyKey, String>() {
+        {
+          put(PropertyKey.WORKER_UFS_INSTREAM_CACHE_EXPIRE_MS, "200000");
+        }
+      });
+
+  @Before
+  public void before() throws Exception {
+    mSeekableInStreams = new ArrayList<>();
+    mUfs = Mockito.mock(UnderFileSystem.class);
+    Mockito.when(mUfs.isSeekable()).thenReturn(true);
+    for(int i=0;i<3;i++) {
+      SeekableUnderFileInputStream instream=createMockedSeekableStream();
+      Mockito.when(mUfs.open(Mockito.eq(FILE_NAME), Mockito.any(OpenOptions.class))).thenReturn(instream);
+      mSeekableInStreams.add(instream);
+    }
+    mManager = new UfsInputStreamManager();
+  }
+
+  private SeekableUnderFileInputStream createMockedSeekableStream() {
+    SeekableUnderFileInputStream instream = Mockito.mock(SeekableUnderFileInputStream.class);
+    Mockito.when(instream.getFilePath()).thenReturn(FILE_NAME);
+    Mockito.doCallRealMethod().when(instream).setResourceId(Mockito.any(Long.class));
+    Mockito.when(instream.getResourceId()).thenCallRealMethod();
+    return instream;
+  }
+
+  @Test
+  public void testCheckInAndOut() throws Exception {
+    mSeekableInStreams = new ArrayList<>();
+    SeekableUnderFileInputStream mockedStrem = createMockedSeekableStream();
+    Mockito.when(mUfs.open(Mockito.eq(FILE_NAME), Mockito.any(OpenOptions.class))).thenReturn(mockedStrem)
+        .thenThrow(new IllegalStateException("Should only be called once"));
+    mSeekableInStreams.add(mockedStrem);
+
+    // check out a stream
+    InputStream instream1 = mManager.checkOut(mUfs, FILE_NAME, 2);
+    // check in back
+    mManager.checkIn(instream1);
+ // check out a stream again
+    InputStream instream2 = mManager.checkOut(mUfs, FILE_NAME, 4);
+
+    Assert.assertEquals(mockedStrem, instream1);
+    Assert.assertEquals(mockedStrem, instream2);
+    // ensure the second time the checked out instream is the same one but repositioned
+    Mockito.verify(mockedStrem).seek(4);
+  }
+}
