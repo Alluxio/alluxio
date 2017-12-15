c4da817547cb5b39ddd3b2ca3fc31051064f7022
Yupeng Fu <yupeng.fu@gmail.com>
2017-12-15 10:56:05 -0800

    add concurrency test

--- core/common/src/test/java/alluxio/ConcurrencyTestUtils.java
+++ core/common/src/test/java/alluxio/ConcurrencyTestUtils.java
@@ -9,7 +9,7 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
-package alluxio.client.concurrent;
+package alluxio;
 
 import org.junit.Assert;
 
