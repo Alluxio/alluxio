c6820d9328e09ead3b2d9324d438d4822f61024b
bf8086 <binfeng@alluxio.com>
2017-12-18 15:41:26 -0800

    Fix race condition and improve logic clarity.

--- core/common/src/main/java/alluxio/Configuration.java
+++ core/common/src/main/java/alluxio/Configuration.java
@@ -518,7 +518,7 @@ public final class Configuration {
     long waitTime = getMs(PropertyKey.MASTER_WORKER_CONNECT_WAIT_TIME);
     long retryInterval = getMs(PropertyKey.USER_RPC_RETRY_MAX_SLEEP_MS);
     if (waitTime < retryInterval) {
-      LOG.warn("%s=%dms is smaller than %s=%d. Workers might not have enough time to register. "
+      LOG.warn("%s=%dms is smaller than %s=%dms. Workers might not have enough time to register. "
           + "Consider either increasing %s or decreasing %s",
           PropertyKey.Name.MASTER_WORKER_CONNECT_WAIT_TIME, waitTime,
           PropertyKey.Name.USER_RPC_RETRY_MAX_SLEEP_MS, retryInterval,
