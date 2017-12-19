886caddb14dd49ba48282e6f4d065b7bb7efb3be
Yupeng Fu <yupeng.fu@gmail.com>
2017-12-19 10:34:29 -0800

    import fix

--- core/client/fs/src/test/java/alluxio/client/file/options/LoadMetadataOptionsTest.java
+++ core/client/fs/src/test/java/alluxio/client/file/options/LoadMetadataOptionsTest.java
@@ -11,11 +11,10 @@
 
 package alluxio.client.file.options;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import alluxio.test.utils.CommonTestUtils;
-
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
