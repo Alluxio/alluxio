ef1e47df31aeacd93b0655e2ffb033a633bf8e12
bf8086 <binfeng@alluxio.com>
2017-12-21 15:06:53 -0800

    Move safe mode manager to abstract master

--- core/server/common/src/main/java/alluxio/master/AbstractMaster.java
+++ core/server/common/src/main/java/alluxio/master/AbstractMaster.java
@@ -16,7 +16,6 @@ import alluxio.Server;
 import alluxio.clock.Clock;
 import alluxio.master.journal.Journal;
 import alluxio.master.journal.JournalContext;
-import alluxio.master.journal.JournalSystem;
 import alluxio.util.executor.ExecutorServiceFactory;
 
 import com.google.common.base.Preconditions;
@@ -53,18 +52,22 @@ public abstract class AbstractMaster implements Master {
   /** The clock to use for determining the time. */
   protected final Clock mClock;
 
+  /** The manager for safe mode state */
+  protected final SafeModeManager mSafeModeManager;
+
   /**
-   * @param journalSystem the journal system to use for tracking master operations
+   * @param masterContext the context for Alluxio master
    * @param clock the Clock to use for determining the time
    * @param executorServiceFactory a factory for creating the executor service to use for
    *        running maintenance threads
    */
-  protected AbstractMaster(JournalSystem journalSystem, Clock clock,
+  protected AbstractMaster(MasterContext masterContext, Clock clock,
       ExecutorServiceFactory executorServiceFactory) {
-    mJournal = journalSystem.createJournal(this);
-    mClock = Preconditions.checkNotNull(clock, "clock");
-    mExecutorServiceFactory =
-        Preconditions.checkNotNull(executorServiceFactory, "executorServiceFactory");
+    Preconditions.checkNotNull(masterContext, "masterContext");
+    mJournal = masterContext.getJournalSystem().createJournal(this);
+    mSafeModeManager = masterContext.getmSafeModeManager();
+    mClock = clock;
+    mExecutorServiceFactory = executorServiceFactory;
   }
 
   @Override
