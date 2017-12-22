e267a01c4419d6d702ee71b22644a010e98f43fe
Bin Fan <fanbin103@gmail.com>
2017-12-21 16:28:58 -0800

    [ALLUXIO-3069] Fix styles and comments

--- core/server/worker/src/main/java/alluxio/worker/block/AsyncCacheRequestManager.java
+++ core/server/worker/src/main/java/alluxio/worker/block/AsyncCacheRequestManager.java
@@ -50,6 +50,10 @@ public class AsyncCacheRequestManager {
   private final ConcurrentHashMap<Long, Protocol.AsyncCacheRequest> mPendingRequests;
   private final WorkerNetAddress mLocalWorkerAddress;
 
+  /**
+   * @param service thread pool to run the background caching work
+   * @param blockWorker handler to the block worker
+   */
   public AsyncCacheRequestManager(ExecutorService service, BlockWorker blockWorker) {
     mAsyncCacheExecutor = service;
     mBlockWorker = blockWorker;
@@ -78,7 +82,7 @@ public class AsyncCacheRequestManager {
       long sessionId = IdUtils.createSessionId();
       long blockSize = openUfsBlockOptions.getBlockSize();
       boolean isSourceLocal = mLocalWorkerAddress.getHost().equals(request.getSourceHost())
-          && mLocalWorkerAddress.getRpcPort() == request.getSourcePort();
+          && mLocalWorkerAddress.getDataPort() == request.getSourcePort();
       // Depends on the request, cache the target block from different sources
       try {
         if (isSourceLocal) {
