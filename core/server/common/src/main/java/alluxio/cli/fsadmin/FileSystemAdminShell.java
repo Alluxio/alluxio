f76f8a483e4b22263c0a56ae0b5c0a058e1c9a27
madanadit <adit@alluxio.com>
2018-01-03 15:16:49 -0800

    Introduce fsadmin shell

---
+++ core/server/common/src/main/java/alluxio/cli/fsadmin/FileSystemAdminShell.java
@@ -0,0 +1,49 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.cli.fsadmin;
+
+import java.util.Map;
+
+import alluxio.cli.AbstractShell;
+import alluxio.cli.Command;
+
+/**
+ * Shell for admin to manage file system.
+ */
+public final class FileSystemAdminShell extends AbstractShell {
+  /**
+   * Construct a new instance of {@link FileSystemAdminShell}.
+   */
+  FileSystemAdminShell() {
+    super(null);
+  }
+
+  /**
+   * Manage Alluxio file system.
+   *
+   * @param args array of arguments given by the user's input from the terminal
+   */
+  public static void main(String[] args) {
+    FileSystemAdminShell fsAdminShell = new FileSystemAdminShell();
+    System.exit(fsAdminShell.run(args));
+  }
+
+  @Override
+  protected String getShellName() {
+    return "fsadmin";
+  }
+
+  @Override
+  protected Map<String, Command> loadCommands() {
+    return FileSystemAdminShellUtils.loadCommands();
+  }
+}
