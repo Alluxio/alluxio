94062588b7ab1c6998eb0ede98051d08e7628bd4
LuQQiu <qiulu@gwu.edu>
2018-01-04 16:30:50 -0500

    shell keyword require non-zero return code

--- deploy/vagrant/provision/roles/ufs_glusterfs/tasks/files/compile_alluxio.sh
+++ deploy/vagrant/provision/roles/ufs_glusterfs/tasks/files/compile_alluxio.sh
@@ -2,3 +2,4 @@
 
 cd /alluxio
 mvn -q clean install -Dtest.profile=glusterfs -Dhadoop.version=2.3.0 -DskipTests -Dmaven.javadoc.skip=true -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos > mvn.log 2>&1
+exit 0
--- deploy/vagrant/provision/roles/ufs_s3/tasks/files/compile_alluxio.sh
+++ deploy/vagrant/provision/roles/ufs_s3/tasks/files/compile_alluxio.sh
@@ -2,3 +2,4 @@
 
 cd /alluxio
 mvn clean install -Dmaven.javadoc.skip=true -DskipTests -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Pmesos > mvn.log 2>&1
+exit 0
