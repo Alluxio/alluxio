34e81dd20389a9b3063004b4b48d65a28c4f3159
Bin Fan <fanbin103@gmail.com>
2018-01-04 16:42:06 -0800

    [SMALLFIX] Ignore exceptions on submitting async cache request on worker and make client call nonblocking

--- core/client/fs/src/main/java/alluxio/client/file/FileInStream.java
+++ core/client/fs/src/main/java/alluxio/client/file/FileInStream.java
@@ -283,7 +283,7 @@ public class FileInStream extends InputStream implements BoundedStream, Position
           try {
             NettyRPCContext rpcContext =
                 NettyRPCContext.defaults().setChannel(channel).setTimeout(channelTimeout);
-            NettyRPC.call(rpcContext, new ProtoMessage(request));
+            NettyRPC.callWithoutBlocking(rpcContext, new ProtoMessage(request));
           } finally {
             mContext.releaseNettyChannel(worker, channel);
           }
