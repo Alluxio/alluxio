52ba0904684b28598ca273404fb751ff278a2752
Andrew Audibert <andrew@alluxio.com>
2018-01-05 17:52:20 -0800

    Fix test

--- tests/src/test/java/alluxio/master/journal/ufs/UfsJournalIntegrationTest.java
+++ tests/src/test/java/alluxio/master/journal/ufs/UfsJournalIntegrationTest.java
@@ -29,6 +29,7 @@ import alluxio.master.LocalAlluxioCluster;
 import alluxio.master.MasterRegistry;
 import alluxio.master.MasterTestUtils;
 import alluxio.master.NoopMaster;
+import alluxio.master.file.DefaultFileSystemMaster;
 import alluxio.master.file.FileSystemMaster;
 import alluxio.master.file.options.GetStatusOptions;
 import alluxio.master.file.options.ListStatusOptions;
@@ -489,17 +490,14 @@ public class UfsJournalIntegrationTest extends BaseIntegrationTest {
 
     // Start a standby master, which will replay the mount entry from the journal.
     MasterRegistry registry = MasterTestUtils.createStandbyFileSystemMasterFromJournal();
-    final FileSystemMaster fsMaster = registry.get(FileSystemMaster.class);
+    Assert.assertTrue(registry.get(FileSystemMaster.class) instanceof DefaultFileSystemMaster);
+    final DefaultFileSystemMaster fsMaster =
+        (DefaultFileSystemMaster) registry.get(FileSystemMaster.class);
     try {
-      CommonUtils.waitFor("standby journal checkpoint replay", new Function<Void, Boolean>() {
+      CommonUtils.waitFor("journal checkpoint replay", new Function<Void, Boolean>() {
         @Override
         public Boolean apply(Void input) {
-          try {
-            fsMaster.listStatus(mountUri, ListStatusOptions.defaults());
-            return true;
-          } catch (Exception e) {
-            return false;
-          }
+          return fsMaster.getMountTable().containsKey(mountUri.toString());
         }
       }, WaitForOptions.defaults().setTimeoutMs(60 * Constants.SECOND_MS));
     } finally {
