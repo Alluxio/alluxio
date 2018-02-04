43652259cb35cc739bc11c0da2e3477fd090f030
lichongjie <lwx371423@163.com>
2018-02-04 17:52:36 +0800

    fix ljvm miss

---
+++ cpp/build/test/CMakeFiles/FILESYSTEM.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FILESYSTEM.dir/__/src/FileSystem.cc.o"
+  "CMakeFiles/FILESYSTEM.dir/__/src/JNIHelper.cc.o"
+  "CMakeFiles/FILESYSTEM.dir/__/src/Status.cc.o"
+  "CMakeFiles/FILESYSTEM.dir/__/src/FileInStream.cc.o"
+  "CMakeFiles/FILESYSTEM.dir/__/src/FileOutStream.cc.o"
+  "libFILESYSTEM.pdb"
+  "libFILESYSTEM.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FILESYSTEM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
