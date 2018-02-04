75d531bfc323c8905be8c69d956d6d2f4c02be18
lichongjie <lwx371423@163.com>
2018-02-04 17:53:10 +0800

    delete dir

--- cpp/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/innkp/tachyon/cpp/test
-# Build directory: /home/innkp/tachyon/cpp/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_filesystem_API "/home/innkp/tachyon/cpp/bin/filesystem_test")
-add_test(test_read_write_API "/home/innkp/tachyon/cpp/bin/fileRW_test")
