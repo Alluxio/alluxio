c56a6932c3d096cfa733f0c2d6c07ff7c6cdc4c9
Pavel Simo <pavel.simo@gmail.com>
2018-02-10 23:14:55 +0800

    Update usage comments

--- bin/alluxio-start.sh
+++ bin/alluxio-start.sh
@@ -45,7 +45,7 @@ MOPT (Mount Option) is one of:
 -f  format Journal, UnderFS Data and Workers Folder on master
 -N  do not try to kill prior running masters and/or workers in all or local
 -w  wait for processes to end before returning
--m  launch monitor process to ensure both masters and workers come up, applies only for the all mode.
+-m  launch monitor process to ensure both masters and workers come up, only in all mode.
 -h  display this help."
 
 ensure_dirs() {
