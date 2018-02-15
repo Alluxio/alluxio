c415ad706d72708a1a86a15b2e441afdee980cb7
Sean <MitiskySean@gmail.com>
2018-02-16 06:40:53 +0800

    [ALLUXIO-3089]change to ConcurrentHashSet to void endless-loop (#6776)
    * change to ConcurrentHashSet to void endless-loop
    
    * use write lock instead of read lock
    
    * remove unused import
    
    * add test

--- core/server/worker/src/main/java/alluxio/worker/block/TieredBlockStore.java
+++ core/server/worker/src/main/java/alluxio/worker/block/TieredBlockStore.java
@@ -676,7 +676,8 @@ public class TieredBlockStore implements BlockStore {
   private void freeSpaceInternal(long sessionId, long availableBytes, BlockStoreLocation location)
       throws WorkerOutOfSpaceException, IOException {
     EvictionPlan plan;
-    try (LockResource r = new LockResource(mMetadataReadLock)) {
+    //NOTE:change the read lock to the write lock due to the endless-loop issue [ALLUXIO-3089]
+    try (LockResource r = new LockResource(mMetadataWriteLock)) {
       plan = mEvictor.freeSpaceWithView(availableBytes, location, getUpdatedView());
       // Absent plan means failed to evict enough space.
       if (plan == null) {
