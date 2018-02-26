f93045442cb70a4e43ec5f2046e514b5fc314918
bf8086 <binfeng@alluxio.com>
2018-02-26 13:38:52 -0800

    Fix style

--- core/client/hdfs/src/test/java/alluxio/hadoop/AbstractFileSystemTest.java
+++ core/client/hdfs/src/test/java/alluxio/hadoop/AbstractFileSystemTest.java
@@ -67,7 +67,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import javax.security.auth.Subject;
