41d4848de3eca613861fe1f2eceb7cce334758ab
Gene Pang <gene.pang@gmail.com>
2018-02-26 13:58:14 -0800

    [ALLUXIO-3115] Enable recoverLease for HDFS journal (#6904)
    * Enable recoverLease for HDFS journal
    
    * Improve comments and log

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystem.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystem.java
@@ -12,6 +12,7 @@
 package alluxio.underfs.hdfs;
 
 import alluxio.AlluxioURI;
+import alluxio.Constants;
 import alluxio.PropertyKey;
 import alluxio.retry.CountingRetry;
 import alluxio.retry.RetryPolicy;
@@ -28,6 +29,7 @@ import alluxio.underfs.options.DeleteOptions;
 import alluxio.underfs.options.FileLocationOptions;
 import alluxio.underfs.options.MkdirsOptions;
 import alluxio.underfs.options.OpenOptions;
+import alluxio.util.CommonUtils;
 import alluxio.util.UnderFileSystemUtils;
 
 import com.google.common.base.Preconditions;
@@ -428,6 +430,10 @@ public class HdfsUnderFileSystem extends BaseUnderFileSystem
   public InputStream open(String path, OpenOptions options) throws IOException {
     IOException te = null;
     RetryPolicy retryPolicy = new CountingRetry(MAX_TRY);
+    DistributedFileSystem dfs = null;
+    if (mFileSystem instanceof DistributedFileSystem) {
+      dfs = (DistributedFileSystem) mFileSystem;
+    }
     while (retryPolicy.attemptRetry()) {
       try {
         FSDataInputStream inputStream = mFileSystem.open(new Path(path));
@@ -441,6 +447,29 @@ public class HdfsUnderFileSystem extends BaseUnderFileSystem
       } catch (IOException e) {
         LOG.warn("{} try to open {} : {}", retryPolicy.getRetryCount(), path, e.getMessage());
         te = e;
+        if (options.getRecoverFailedOpen() && dfs != null && e.getMessage().toLowerCase()
+            .startsWith("cannot obtain block length for")) {
+          // This error can occur when an Alluxio journal file was not properly closed by Alluxio.
+          // In this scenario, the HDFS lease must be recovered in order for the file to be
+          // readable again. The 'recoverLease' API usually needs to be invoked multiple times
+          // to complete the lease recovery process.
+          try {
+            if (dfs.recoverLease(new Path(path))) {
+              LOG.warn("recoverLease-1 success for: {}", path);
+            } else {
+              // try one more time, after waiting
+              CommonUtils.sleepMs(5 * Constants.SECOND_MS);
+              if (dfs.recoverLease(new Path(path))) {
+                LOG.warn("recoverLease-2 success for: {}", path);
+              } else {
+                LOG.warn("recoverLease: path not closed: {}", path);
+              }
+            }
+          } catch (IOException e1) {
+            // ignore exception
+            LOG.warn("recoverLease failed for: {} error: {}", path, e1.getMessage());
+          }
+        }
       }
     }
     throw te;
