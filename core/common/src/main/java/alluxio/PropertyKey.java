519fa047cc42a9470fd0529cacefda8df5e0a90c
Calvin Jia <jia.calvin@gmail.com>
2018-03-13 14:36:07 -0700

    Change close timeout from 5m to 30m.

--- core/common/src/main/java/alluxio/PropertyKey.java
+++ core/common/src/main/java/alluxio/PropertyKey.java
@@ -1756,7 +1756,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
   public static final PropertyKey USER_NETWORK_NETTY_WRITER_CLOSE_TIMEOUT_MS =
       new Builder(Name.USER_NETWORK_NETTY_WRITER_CLOSE_TIMEOUT_MS)
           .setAlias(new String[]{"alluxio.user.network.netty.writer.close.timeout.ms"})
-          .setDefaultValue("5min")
+          .setDefaultValue("30min")
           .setDescription("The timeout to close a netty writer client.")
           .build();
   public static final PropertyKey USER_NETWORK_NETTY_READER_BUFFER_SIZE_PACKETS =
