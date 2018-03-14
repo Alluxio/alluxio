d1e5c077ab8d4bd697a2caae602d3232b148119c
Calvin Jia <jia.calvin@gmail.com>
2018-03-13 20:02:44 -0700

    [ALLUXIO-3128] Allow temporary directories to be configurable. (#6963)
    * Allow temporary directories to be configurable.
    
    * Update description.
    
    * Make test more informative on error.
    
    * Combine test cases.
    
    * Use whitebox.

--- core/common/src/main/java/alluxio/PropertyKey.java
+++ core/common/src/main/java/alluxio/PropertyKey.java
@@ -218,6 +218,14 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .setDefaultValue(false)
           .setDescription("Flag used only during tests to allow special behavior.")
           .build();
+  public static final PropertyKey TMP_DIRS =
+      new Builder(Name.TMP_DIRS)
+          .setDefaultValue("/tmp")
+          .setDescription("The path(s) to store Alluxio temporary files, use commas as delimiters. "
+              + "If multiple paths are specified, one will be selected at random per temporary "
+              + "file. Currently, only files to be uploaded to object stores are stored in these "
+              + "paths.")
+          .build();
   public static final PropertyKey VERSION =
       new Builder(Name.VERSION)
           .setDefaultValue(ProjectConstants.VERSION)
@@ -2121,6 +2129,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
         "alluxio.network.thrift.frame.size.bytes.max";
     public static final String SITE_CONF_DIR = "alluxio.site.conf.dir";
     public static final String TEST_MODE = "alluxio.test.mode";
+    public static final String TMP_DIRS = "alluxio.tmp.dirs";
     public static final String VERSION = "alluxio.version";
     public static final String WEB_RESOURCES = "alluxio.web.resources";
     public static final String WEB_THREADS = "alluxio.web.threads";
