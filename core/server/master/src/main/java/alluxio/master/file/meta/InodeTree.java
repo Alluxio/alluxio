7647d42d99db180745820d6b1d55cea5ff989086
bf8086 <binfeng@alluxio.com>
2018-03-14 15:30:38 -0700

    remove unused import

--- core/server/master/src/main/java/alluxio/master/file/meta/InodeTree.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/InodeTree.java
@@ -50,7 +50,6 @@ import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
