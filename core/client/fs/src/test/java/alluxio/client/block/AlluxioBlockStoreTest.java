1bb11bcb85edc2e9879425623c9338d759768b9e
Alluxio <qiulu@gwu.edu>
2018-03-22 11:37:43 -0700

    small fix

--- core/client/fs/src/test/java/alluxio/client/block/AlluxioBlockStoreTest.java
+++ core/client/fs/src/test/java/alluxio/client/block/AlluxioBlockStoreTest.java
@@ -18,7 +18,6 @@ import static org.mockito.Mockito.when;
 
 import alluxio.client.WriteType;
 import alluxio.client.block.options.WorkerInfoOptions;
-import alluxio.client.block.options.WorkerInfoOptions.WorkerRange;
 import alluxio.client.block.policy.BlockLocationPolicy;
 import alluxio.client.block.policy.options.GetWorkerOptions;
 import alluxio.client.block.stream.BlockInStream;
@@ -237,8 +236,7 @@ public final class AlluxioBlockStoreTest {
             new MockFileWriteLocationPolicy(Arrays.asList(worker1, worker2)));
     InStreamOptions options = new InStreamOptions(dummyStatus, readOptions);
     when(mMasterClient.getBlockInfo(BLOCK_ID)).thenReturn(new BlockInfo());
-    when(mMasterClient.getWorkerInfoList(WorkerInfoOptions.defaults()
-        .setWorkerRange(WorkerRange.LIVE))).thenReturn(
+    when(mMasterClient.getWorkerInfoList(WorkerInfoOptions.defaults())).thenReturn(
         Arrays.asList(new WorkerInfo().setAddress(worker1), new WorkerInfo().setAddress(worker2)));
 
     // Location policy chooses worker1 first.
@@ -360,8 +358,7 @@ public final class AlluxioBlockStoreTest {
         OpenFileOptions.defaults().setUfsReadLocationPolicy(mockPolicy);
     InStreamOptions options = new InStreamOptions(dummyStatus, readOptions);
     when(mMasterClient.getBlockInfo(BLOCK_ID)).thenReturn(info);
-    when(mMasterClient.getWorkerInfoList(
-        WorkerInfoOptions.defaults().setWorkerRange(WorkerRange.LIVE)))
+    when(mMasterClient.getWorkerInfoList(WorkerInfoOptions.defaults()))
         .thenReturn(Arrays.stream(workers)
         .map(x -> new WorkerInfo().setAddress(x)).collect((Collectors.toList())));
     Map<WorkerNetAddress, Long> failedWorkerAddresses = failedWorkers.entrySet().stream()
