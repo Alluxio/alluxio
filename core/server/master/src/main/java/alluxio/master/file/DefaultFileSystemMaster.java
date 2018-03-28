1bac3e1ae63cfd9977fbdc4de174d4d85376903e
Andrew Audibert <andrew@alluxio.com>
2018-03-28 11:31:03 -0700

    Avoid opening block context when no blocks need to be deleted (#7001)

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -3643,6 +3643,9 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
   }
 
   private void removeBlocks(List<Long> blocks) throws IOException {
+    if (blocks.isEmpty()) {
+      return;
+    }
     RetryPolicy retry = new CountingRetry(3);
     while (true) {
       try {
