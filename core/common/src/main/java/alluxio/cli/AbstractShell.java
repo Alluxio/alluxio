dd01fe7ea9437b08ade614f69b92d081231ea479
Pavel Simo <pavel.simo@gmail.com>
2018-04-02 00:12:19 +0800

    Remove unnecessary annotation

--- core/common/src/main/java/alluxio/cli/AbstractShell.java
+++ core/common/src/main/java/alluxio/cli/AbstractShell.java
@@ -16,7 +16,6 @@ import alluxio.exception.status.InvalidArgumentException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
-import org.checkerframework.checker.initialization.qual.Initialized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,7 +43,7 @@ public abstract class AbstractShell implements Closeable {
    *
    * @param commandAlias replacements for commands
    */
-//  @SuppressWarnings("nullness")
+  @SuppressWarnings("nullness")
   public AbstractShell(Map<String, String[]> commandAlias) {
     mCommands = loadCommands();
     mCommandAlias = commandAlias;
@@ -67,7 +66,6 @@ public abstract class AbstractShell implements Closeable {
     Command command = mCommands.get(cmd);
 
     if (command == null) {
-      @Initialized
       String[] replacementCmd = getReplacementCmd(cmd);
       if (replacementCmd == null) {
         // Unknown command (we didn't find the cmd in our dict)
