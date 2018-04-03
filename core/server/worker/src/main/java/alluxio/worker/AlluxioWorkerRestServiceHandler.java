b3061ce82460361a73cb2875bbfdd4cb0fde1246
wangyuxin <151220116@smail.nju.edu.cn>
2018-04-03 14:08:21 +0800

    Task Part_1 has been finished UPDATE

--- core/server/worker/src/main/java/alluxio/worker/AlluxioWorkerRestServiceHandler.java
+++ core/server/worker/src/main/java/alluxio/worker/AlluxioWorkerRestServiceHandler.java
@@ -311,12 +311,7 @@ public final class AlluxioWorkerRestServiceHandler {
   @ReturnType("java.lang.Long")
   @Deprecated
   public Response getUptimeMs() {
-    return RestUtils.call(new RestUtils.RestCallable<Long>() {
-      @Override
-      public Long call() throws Exception {
-        return mWorkerProcess.getUptimeMs();
-      }
-    });
+    return RestUtils.call(() -> mWorkerProcess.getStartTimeMs());
   }
 
   /**
