661f56d59312993f36ae1b8c5362a634ab321fee
Jackbrocp <ysr_nju@163.com>
2018-04-06 15:02:02 +0800

    Change

--- core/client/fs/src/test/java/alluxio/client/lineage/AlluxioLineageTest.java
+++ core/client/fs/src/test/java/alluxio/client/lineage/AlluxioLineageTest.java
@@ -25,7 +25,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import static org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
