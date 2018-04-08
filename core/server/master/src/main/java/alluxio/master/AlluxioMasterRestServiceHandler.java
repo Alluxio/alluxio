0f58f9b1290a7f89b9a2331b166e87067d31d362
troy <1158864287@qq.com>
2018-04-08 06:06:45 -0700

    a java 8 improvement

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -197,12 +197,7 @@ public final class AlluxioMasterRestServiceHandler {
   @ReturnType("java.lang.String")
   @Deprecated
   public Response getRpcAddress() {
-    return RestUtils.call(new RestUtils.RestCallable<String>() {
-      @Override
-      public String call() throws Exception {
-        return mMasterProcess.getRpcAddress().toString();
-      }
-    });
+    return RestUtils.call(() -> mMasterProcess.getRpcAddress().toString());
   }
 
   /**
