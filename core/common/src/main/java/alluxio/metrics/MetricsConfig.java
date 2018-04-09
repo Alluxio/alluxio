489dc068981ca96cdc5a32c10b71681d0a52205d
FREELANCER2017 <151220155@smail.nju.edu.cn>
2018-04-09 03:43:35 -0700

    Improve javadoc for MetricsConfig

--- core/common/src/main/java/alluxio/metrics/MetricsConfig.java
+++ core/common/src/main/java/alluxio/metrics/MetricsConfig.java
@@ -104,8 +104,8 @@ public final class MetricsConfig {
     }
   }
 
-  /**
-   * This method removes the instance prefix in the properties. This is to make the configuration
+  /*
+   * Removes the instance prefix in the properties. This is to make the configuration
    * parsing logic backward compatible with old configuration format.
    */
   private void removeInstancePrefix() {
