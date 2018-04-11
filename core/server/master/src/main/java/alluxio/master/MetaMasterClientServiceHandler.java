7406d40ed2c71373871f9d580b68e883471297d1
LuQQiu <luqiujob@gmail.com>
2018-04-10 17:11:26 -0700

    [ALLUXIO-3144] Report Metrics CLI (#7010)
    * create thrift server side codes
    
    * add thrift files
    
    * add report operation CLI
    
    * add tests and improve codes
    
    * add test and improve codes
    
    * thrift getMetrics() and format then in shell
    
    * add thrift files and make it alphabetize
    
    * turn metrics part to be simple and deal with all the logics in shell
    
    * small fix
    
    * change from FileSystemMaster to metaMaster
    
    * small fix
    
    * MetricValue only one of longValue or doubleValue should be set
    
    * delete unused metricvalue constructor
    
    * also show other metrics information
    
    * better print out format
    
    * remove null and change metrics keys to be original

--- core/server/master/src/main/java/alluxio/master/MetaMasterClientServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/MetaMasterClientServiceHandler.java
@@ -14,22 +14,31 @@ package alluxio.master;
 import alluxio.Constants;
 import alluxio.RpcUtils;
 import alluxio.RuntimeConstants;
+import alluxio.metrics.MetricsSystem;
 import alluxio.thrift.AlluxioTException;
 import alluxio.thrift.GetConfigurationTOptions;
 import alluxio.thrift.GetConfigurationTResponse;
 import alluxio.thrift.GetMasterInfoTOptions;
 import alluxio.thrift.GetMasterInfoTResponse;
+import alluxio.thrift.GetMetricsTOptions;
+import alluxio.thrift.GetMetricsTResponse;
 import alluxio.thrift.GetServiceVersionTOptions;
 import alluxio.thrift.GetServiceVersionTResponse;
 import alluxio.thrift.MasterInfo;
 import alluxio.thrift.MasterInfoField;
 import alluxio.thrift.MetaMasterClientService;
+import alluxio.wire.MetricValue;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -96,6 +105,35 @@ public final class MetaMasterClientServiceHandler implements MetaMasterClientSer
         }
       }
       return new GetMasterInfoTResponse(info);
+    });
+  }
+
+  @Override
+  public GetMetricsTResponse getMetrics(final GetMetricsTOptions options)
+      throws TException {
+    return RpcUtils.call(LOG, (RpcUtils.RpcCallable<GetMetricsTResponse>) () -> {
+      MetricRegistry mr = MetricsSystem.METRIC_REGISTRY;
+      Map<String, alluxio.thrift.MetricValue> metricsMap = new HashMap<>();
+
+      for (Map.Entry<String, Counter> entry : mr.getCounters().entrySet()) {
+        metricsMap.put(MetricsSystem.stripInstanceAndHost(entry.getKey()),
+            MetricValue.forLong(entry.getValue().getCount()).toThrift());
+      }
+
+      for (Map.Entry<String, Gauge> entry : mr.getGauges().entrySet()) {
+        Object value = entry.getValue().getValue();
+        if (value instanceof Integer) {
+          metricsMap.put(entry.getKey(),
+              MetricValue.forLong(Long.valueOf((Integer) value)).toThrift());
+        } else if (value instanceof Long) {
+          metricsMap.put(entry.getKey(),
+              MetricValue.forLong((Long) value).toThrift());
+        } else if (value instanceof Double) {
+          metricsMap.put(entry.getKey(),
+              MetricValue.forDouble((Double) value).toThrift());
+        }
+      }
+      return new GetMetricsTResponse(metricsMap);
     });
   }
 }
