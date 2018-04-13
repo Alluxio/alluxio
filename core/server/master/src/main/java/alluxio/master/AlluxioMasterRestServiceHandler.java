a27259e849a9fb1439bcab7b61a951befe8de041
GodLikeGXS <31882331+GodLikeGXS@users.noreply.github.com>
2018-04-14 02:22:05 +0800

    replace anoymous type with lamda expression (#7087)

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -309,12 +309,7 @@ public final class AlluxioMasterRestServiceHandler {
   @ReturnType("java.lang.Long")
   @Deprecated
   public Response getUfsCapacityBytes() {
-    return RestUtils.call(new RestUtils.RestCallable<Long>() {
-      @Override
-      public Long call() throws Exception {
-        return mUfs.getSpace(mUfsRoot, UnderFileSystem.SpaceType.SPACE_TOTAL);
-      }
-    });
+    return RestUtils.call(() -> mUfs.getSpace(mUfsRoot,UnderFileSystem.SpaceType.SPACE_USED));
   }
 
   /**
