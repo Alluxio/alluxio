0494eacb8f379391a63e7d66e0f1e45b86e21970
Tina-XX <31896240+Tina-XX@users.noreply.github.com>
2018-04-14 02:17:13 +0800

    151220138 (#7100)

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -178,12 +178,7 @@ public final class AlluxioMasterRestServiceHandler {
   @ReturnType("java.util.SortedMap<java.lang.String, java.lang.Long>")
   @Deprecated
   public Response getMetrics() {
-    return RestUtils.call(new RestUtils.RestCallable<Map<String, Long>>() {
-      @Override
-      public Map<String, Long> call() throws Exception {
-        return getMetricsInternal();
-      }
-    });
+    return RestUtils.call(() -> getMetricsInternal());
   }
 
   /**
