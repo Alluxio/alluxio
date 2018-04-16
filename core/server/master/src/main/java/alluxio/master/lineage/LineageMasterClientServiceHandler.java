3f60bcaa11dea689c528c1f29d4e62d466a0f405
d289760860 <2723141990@qq.com>
2018-04-16 09:40:30 -0700

    fix

--- core/server/master/src/main/java/alluxio/master/lineage/LineageMasterClientServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/lineage/LineageMasterClientServiceHandler.java
@@ -17,7 +17,6 @@ import alluxio.RpcUtils;
 import alluxio.RpcUtils.RpcCallable;
 import alluxio.RpcUtils.RpcCallableThrowsIOException;
 import alluxio.exception.AlluxioException;
-import alluxio.exception.status.AlluxioStatusException;
 import alluxio.job.CommandLineJob;
 import alluxio.job.JobConf;
 import alluxio.thrift.AlluxioTException;
@@ -128,8 +127,8 @@ public final class LineageMasterClientServiceHandler implements LineageMasterCli
   public ReinitializeFileTResponse reinitializeFile(final String path, final long blockSizeBytes,
       final long ttl, final TTtlAction ttlAction, ReinitializeFileTOptions options)
       throws AlluxioTException {
-    return RpcUtils.call(LOG, (RpcCallableThrowsIOException<ReinitializeFileTResponse>) () -> new ReinitializeFileTResponse(mLineageMaster.
-        reinitializeFile(path, blockSizeBytes, ttl, ThriftUtils.fromThrift(ttlAction))));
+    return RpcUtils.call(LOG, (RpcCallableThrowsIOException<ReinitializeFileTResponse>) () -> new ReinitializeFileTResponse(mLineageMaster
+        .reinitializeFile(path, blockSizeBytes, ttl, ThriftUtils.fromThrift(ttlAction))));
   }
 
   @Override
