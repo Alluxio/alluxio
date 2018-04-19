3d1b4ccb8f505395b2e5b6069136d90d623b097a
brucewayne0 <157915540@qq.com>
2018-04-18 22:19:03 -0700

    SmallFix

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -319,12 +319,7 @@ public final class AlluxioMasterRestServiceHandler {
   @ReturnType("java.lang.Long")
   @Deprecated
   public Response getUfsUsedBytes() {
-    return RestUtils.call(new RestUtils.RestCallable<Long>() {
-      @Override
-      public Long call() throws Exception {
-        return mUfs.getSpace(mUfsRoot, UnderFileSystem.SpaceType.SPACE_USED);
-      }
-    });
+    return RestUtils.call(() -> mUfs.getSpace(mUfsRoot, UnderFileSystem.SpaceType.SPACE_USED));
   }
 
   /**
