3edc5b4614c3cd922c7721a93919c226c03cf5a9
chy123chy <846878035@qq.com>
2018-04-22 01:48:57 +0800

    Use static imports for standard test utilities in alluxio.master.lineage.recompute.RecomputePlannerTest

--- core/server/master/src/test/java/alluxio/master/lineage/recompute/RecomputePlannerTest.java
+++ core/server/master/src/test/java/alluxio/master/lineage/recompute/RecomputePlannerTest.java
@@ -12,6 +12,7 @@
 package alluxio.master.lineage.recompute;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito;
 
 import alluxio.job.CommandLineJob;
 import alluxio.job.Job;
@@ -25,7 +26,6 @@ import alluxio.master.lineage.meta.LineageStore;
 import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
