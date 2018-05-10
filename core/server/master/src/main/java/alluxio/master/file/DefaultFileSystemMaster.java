7bcee0e9167cbf40fdf6fcf3be763c8d128e486c
David Zhu <david@alluxio.com>
2018-05-10 16:23:16 -0700

    style change

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -156,10 +156,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.Stack;
@@ -957,9 +955,10 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
       }
       if (recursive || currInodePath.equals(originalInodePath)) {
         for (Inode<?> child : ((InodeDirectory) inode).getChildren()) {
-          try (LockedInodePath childInodePath = mInodeTree.lockFullInodePath(child.getId(), InodeTree.LockMode.READ)) {
-            statusList.addAll(listStatusInternal
-                (childInodePath, auditContext, recursive, originalInodePath));
+          try (LockedInodePath childInodePath = mInodeTree.lockFullInodePath(child.getId(),
+              InodeTree.LockMode.READ)) {
+            statusList.addAll(listStatusInternal(childInodePath, auditContext,
+                recursive, originalInodePath));
           } catch (Exception e) {
             LOG.debug("ListStatus failed for path {}", mInodeTree.getPath(child).getPath(), e);
             continue;
