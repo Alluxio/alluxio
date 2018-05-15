8c48f4226f959083d074972396e07c3f37efa66f
Bin Fan <fanbin103@gmail.com>
2018-05-15 11:08:29 -0700

    [ALLUXIO-3194] Refactor Thrift related util methods (#7275)
    * Refactor Thrift related util methods
    
    * Fix style check
    
    * Address comments on Javadoc

--- core/common/src/main/java/alluxio/security/authentication/NoSaslTransportProvider.java
+++ core/common/src/main/java/alluxio/security/authentication/NoSaslTransportProvider.java
@@ -11,8 +11,7 @@
 
 package alluxio.security.authentication;
 
-import alluxio.Configuration;
-import alluxio.PropertyKey;
+import alluxio.network.thrift.ThriftUtils;
 
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportFactory;
@@ -28,9 +27,6 @@ import javax.security.auth.Subject;
  */
 @ThreadSafe
 public final class NoSaslTransportProvider implements TransportProvider {
-  /** Timeout for socket in ms. */
-  private static final int SOCKET_TIMEOUT_MS = (int) Configuration
-      .getMs(PropertyKey.SECURITY_AUTHENTICATION_SOCKET_TIMEOUT_MS);
 
   /**
    * Constructor for transport provider when authentication type is {@link AuthType#NOSASL}.
@@ -44,8 +40,7 @@ public final class NoSaslTransportProvider implements TransportProvider {
 
   @Override
   public TTransport getClientTransport(Subject subject, InetSocketAddress serverAddress) {
-    TTransport transport =
-        TransportProviderUtils.createThriftSocket(serverAddress, SOCKET_TIMEOUT_MS);
+    TTransport transport = ThriftUtils.createThriftSocket(serverAddress);
     return transport;
   }
 
