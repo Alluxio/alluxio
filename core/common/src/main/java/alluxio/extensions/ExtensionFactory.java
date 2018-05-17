332eb3d4b1b52f1edf2cce8f066a88ef09c13ab4
bf8086 <binfeng@alluxio.com>
2018-05-17 01:18:38 -0700

    generalize extension factory registry

---
+++ core/common/src/main/java/alluxio/extensions/ExtensionFactory.java
@@ -0,0 +1,37 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.extensions;
+
+import javax.annotation.Nullable;
+
+public interface ExtensionFactory<T, S> {
+  /**
+   * Creates a new extension for the given path. An {@link IllegalArgumentException} is
+   * thrown if this factory does not support extension for the given path or if the configuration
+   * provided is insufficient to create a client.
+   *
+   * @param path file path
+   * @param conf optional configuration object for the extension, may be null
+   * @return the client
+   */
+  T create(String path, @Nullable S conf);
+
+  /**
+   * Gets whether this factory supports the given path and thus whether calling the
+   * {@link #create(String, S)} can succeed for this path.
+   *
+   * @param path file path
+   * @param conf optional configuration object for the extension, may be null
+   * @return true if the path is supported, false otherwise
+   */
+  boolean supportsPath(String path, @Nullable S conf);
+}
