4fb7eb7f86efec112356877c727fceb1be9ed764
Andrew Audibert <andrew@alluxio.com>
2018-05-31 18:12:35 -0700

    Fix compilation

--- tests/src/test/java/alluxio/server/ft/journal/JournalShutdownIntegrationTest.java
+++ tests/src/test/java/alluxio/server/ft/journal/JournalShutdownIntegrationTest.java
@@ -47,6 +47,7 @@ import com.google.common.collect.ImmutableMap;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
