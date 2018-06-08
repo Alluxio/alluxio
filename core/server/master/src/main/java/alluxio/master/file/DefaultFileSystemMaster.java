88730cf3877416bd541eeee366df0b1d9de094a8
David Zhu <david@alluxio.com>
2018-06-08 14:44:45 -0700

    removing unnecessary check

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -987,16 +987,14 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
           throw e;
         }
       }
-      if (descendantType != DescendantType.NONE) {
-        DescendantType nextDescendantType = (descendantType == DescendantType.ALL)
-            ? DescendantType.ALL : DescendantType.NONE;
-        for (Inode<?> child : ((InodeDirectory) inode).getChildren()) {
-          // TODO(david): Make extending InodePath more efficient
-          try (LockedInodePath childInodePath = mInodeTree.lockFullInodePath(child.getId(),
-              InodeTree.LockMode.READ)) {
-            listStatusInternal(childInodePath, auditContext,
-                nextDescendantType, statusList);
-          }
+      DescendantType nextDescendantType = (descendantType == DescendantType.ALL)
+          ? DescendantType.ALL : DescendantType.NONE;
+      for (Inode<?> child : ((InodeDirectory) inode).getChildren()) {
+        // TODO(david): Make extending InodePath more efficient
+        try (LockedInodePath childInodePath = mInodeTree.lockFullInodePath(child.getId(),
+            InodeTree.LockMode.READ)) {
+          listStatusInternal(childInodePath, auditContext,
+              nextDescendantType, statusList);
         }
       }
     }
