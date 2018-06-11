225db4a00ca533a23e776999c8932bd3c1be0cbb
David Zhu <david@alluxio.com>
2018-06-11 14:54:52 -0700

    Small fix to enable createTempPathForExistingChild to take a different locking mode.

--- core/server/master/src/main/java/alluxio/master/file/meta/LockedInodePath.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/LockedInodePath.java
@@ -231,15 +231,17 @@ public abstract class LockedInodePath implements Closeable {
    * it does not close the existing path.
    *
    * @param child the inode of the direct child
+   * @param lockMode the desired locking mode for the child
    * @return a {@link LockedInodePath} for the direct child
    * @throws InvalidPathException if the path is invalid
    * @throws FileDoesNotExistException if the file does not exist
    */
-  public synchronized LockedInodePath createTempPathForExistingChild(Inode<?> child)
+  public synchronized LockedInodePath createTempPathForExistingChild(Inode<?> child,
+                                                                     InodeTree.LockMode lockMode)
       throws InvalidPathException, FileDoesNotExistException {
     InodeLockList lockList = new CompositeInodeLockList(mLockList);
     LockedInodePath lockedDescendantPath;
-    if (mLockMode == InodeTree.LockMode.READ) {
+    if (lockMode == InodeTree.LockMode.READ) {
       lockList.lockReadAndCheckParent(child, getInode());
       lockedDescendantPath = new MutableLockedInodePath(
           getUri().join(child.getName()), this, lockList);
