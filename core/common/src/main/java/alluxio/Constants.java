5085b78eade3e0d4aeb9d5ea63c19f9d3017f44d
Bin Fan <fanbin103@gmail.com>
2018-06-14 14:33:54 -0700

    [ALLUXIO-3194] Support cluster-wide default to simplify client-side configuration (#7308)
    * Add client/server transports to support conf bootstrap
    
    * Change the signature of getClientTransport to take the base transport
    
    * Add handshake() before connect() to connect to meta master and fetch the conf
    
    * Clean up
    
    * Add peakable transport to bootstrap transport
    
    * Fix typo
    
    * Fix style check
    
    * Fix check style
    
    * Fix tests and code cleanup
    
    * Address review comments
    
    * Cleanup a bunch properties with more accurate Scopes
    
    * Fix a failing test
    
    * Fix tests
    
    * Fix issues after merge
    
    * Address comments on class hierachy of Bootstrap transport
    
    * Minor variable naming in AbstractClient
    
    * Address comments on isOpen
    
    * Catch exception on cleanup for alluxio.multi.process.MultiProcessClusterTest#createAndOpenFile
    
    * Set UFS address for MultiProcess cluster
    
    * Cleanup logging message
    
    * Address comments
    
    * When loading cluster default, apply the conf to all scope

--- core/common/src/main/java/alluxio/Constants.java
+++ core/common/src/main/java/alluxio/Constants.java
@@ -105,9 +105,10 @@ public final class Constants {
   public static final String FILE_SYSTEM_MASTER_CLIENT_SERVICE_NAME = "FileSystemMasterClient";
   public static final String FILE_SYSTEM_MASTER_WORKER_SERVICE_NAME = "FileSystemMasterWorker";
   public static final String LINEAGE_MASTER_CLIENT_SERVICE_NAME = "LineageMasterClient";
-  public static final String META_MASTER_CLIENT_SERVICE_NAME = "MetaMasterClient";
+  // TODO(binfan): set META_MASTER_CLIENT_SERVICE_NAME to "MetaMasterClient" after 2.0.
+  // Its value is "MetaMaster" for backwards compatibility so 1.7 clients can talk to 1.8 MetaMaster
+  public static final String META_MASTER_CLIENT_SERVICE_NAME = "MetaMaster";
   public static final String META_MASTER_MASTER_SERVICE_NAME = "MetaMasterMaster";
-  public static final String META_MASTER_SERVICE_NAME = "MetaMaster";
   public static final String METRICS_MASTER_CLIENT_SERVICE_NAME = "MetricsMasterClient";
   public static final String BLOCK_WORKER_CLIENT_SERVICE_NAME = "BlockWorkerClient";
   public static final String FILE_SYSTEM_WORKER_CLIENT_SERVICE_NAME = "FileSystemWorkerClient";
