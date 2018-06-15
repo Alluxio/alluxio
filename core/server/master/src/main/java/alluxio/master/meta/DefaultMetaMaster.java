750dc1b6f165c0c5b23e2c2ddac1e2f2c844581f
Andrew Audibert <andrew@alluxio.com>
2018-06-15 14:28:48 -0700

    [SMALLFIX] Config checker improvements (#7407)
    * Config checker improvements
    
    * Fix bug in mFirst
    
    * Address review comments
    
    * Add comment

--- core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
+++ core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
@@ -445,19 +445,20 @@ public final class DefaultMetaMaster extends AbstractMaster implements MetaMaste
   }
 
   /**
-   * Periodic log config check report.
+   * Periodically log the config check report.
    */
   private final class LogConfigReportHeartbeatExecutor implements HeartbeatExecutor {
-
-    /**
-     * Constructs a new {@link LogConfigReportHeartbeatExecutor}.
-     */
-    public LogConfigReportHeartbeatExecutor() {
-    }
+    private volatile boolean mFirst = true;
 
     @Override
     public void heartbeat() {
-      mConfigChecker.logConfigReport();
+      // Skip the first heartbeat since it happens before servers have time to register their
+      // configurations.
+      if (mFirst) {
+        mFirst = false;
+      } else {
+        mConfigChecker.logConfigReport();
+      }
     }
 
     @Override
