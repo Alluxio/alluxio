a821ffcab6730fba439cd6d3c72a1b43502df105
Gene Pang <gene.pang@gmail.com>
2018-06-18 18:13:44 -0700

    Improve buffer factor

--- tests/src/test/java/alluxio/client/fs/ConcurrentFileSystemMasterCreateTest.java
+++ tests/src/test/java/alluxio/client/fs/ConcurrentFileSystemMasterCreateTest.java
@@ -270,20 +270,21 @@ public class ConcurrentFileSystemMasterCreateTest extends BaseIntegrationTest {
   private void runLoadMetadata(WriteType writeType, boolean useSinglePath, boolean createFiles,
       boolean listParentDir) throws Exception {
     int numThreads = CONCURRENCY_FACTOR;
+    int bufferFactor = CONCURRENCY_FACTOR / 2;
     // 2 nested components to create.
-    long limitMs = 2 * SLEEP_MS * 4;
+    long limitMs = 2 * SLEEP_MS * bufferFactor;
 
     int uniquePaths = useSinglePath ? 1 : numThreads;
 
     if (writeType != WriteType.CACHE_THROUGH) {
       // all 3 components must be synced to UFS.
-      limitMs = 3 * SLEEP_MS * 4;
+      limitMs = 3 * SLEEP_MS * bufferFactor;
     }
 
     if (listParentDir) {
       // Loading direct children needs to load each child, so reduce the branching factor.
       uniquePaths = 10;
-      limitMs = 2 * SLEEP_MS * 4 + uniquePaths * SLEEP_MS * 3;
+      limitMs = 2 + uniquePaths * SLEEP_MS * bufferFactor;
     }
 
     // Create UFS files outside of Alluxio.
