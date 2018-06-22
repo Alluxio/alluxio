2f19d5caf50da4d74890c0616e0ae582afc1f838
Calvin Jia <jia.calvin@gmail.com>
2018-06-22 16:00:11 -0700

    [ALLUXIO-3204] Refactor RpcUtils (#7411)
    * Refactor Rpc Utils.
    
    * Refactor Netty RPCs.
    
    * Refactor thrift RPCs.
    
    * Address comments.
    
    * Address comments.
    
    * Address comments.
    
    * Consistent exit/enter.

--- core/server/common/src/main/java/alluxio/RpcUtils.java
+++ core/server/common/src/main/java/alluxio/RpcUtils.java
@@ -28,60 +28,149 @@ import org.slf4j.Logger;
 import java.io.IOException;
 
 /**
- * Utilities for handling RPC calls.
+ * Utilities for handling server RPC calls.
+ *
+ * There are three types of RPC calls:
+ * 1. RPCs that only throw AlluxioException
+ * 2. RPCs that throw AlluxioException and IOException
+ * 3. Netty RPCs
+ *
+ * For each of these, there are two types of methods
+ * 1. call(callable) - for internal methods, executes the method without any logging or metrics
+ * 2. call(logger, callable, method name, failureOk, method description, arguments...) - for
+ * client initiated methods, executes the method with logging and metrics. If failureOk is set,
+ * non-fatal errors will only be logged at the DEBUG level and failure metrics will not be
+ * recorded.
  */
 public final class RpcUtils {
   private static final String METHOD_NAME_SEPARATOR = ":";
 
   /**
-   * Calls the given {@link RpcCallable} and handles any exceptions thrown. If the
-   * RPC fails, a warning or error will be logged.
+   * An interface representing a callable which can only throw Alluxio exceptions.
    *
-   * @param logger the logger to use for this call
+   * @param <T> the return type of the callable
+   */
+  public interface RpcCallable<T> {
+    /**
+     * The RPC implementation.
+     *
+     * @return the return value from the RPC
+     */
+    T call() throws AlluxioException;
+  }
+
+  /**
+   * Calls the given {@link RpcCallable} and handles any exceptions thrown. No call history or
+   * errors will be logged. This method should be used for internal RPCs.
+   *
    * @param callable the callable to call
    * @param <T> the return type of the callable
    * @return the return value from calling the callable
    * @throws AlluxioTException if the callable throws an exception
    */
-  public static <T> T call(Logger logger, RpcCallable<T> callable)
-      throws AlluxioTException {
-    return call(logger, callable, true);
+  public static <T> T call(RpcCallable<T> callable) throws AlluxioTException {
+    try {
+      return callable.call();
+    } catch (AlluxioException e) {
+      throw AlluxioStatusException.fromAlluxioException(e).toThrift();
+    } catch (RuntimeException e) {
+      throw new InternalException(e).toThrift();
+    }
+  }
+
+  /**
+   * Calls the given {@link RpcCallable} and handles any exceptions thrown. If the RPC fails, a
+   * warning or error will be logged.
+   *
+   * @param logger the logger to use for this call
+   * @param callable the callable to call
+   * @param methodName the name of the method, used for metrics
+   * @param description the format string of the description, used for logging
+   * @param args the arguments for the description
+   * @param <T> the return type of the callable
+   * @return the return value from calling the callable
+   * @throws AlluxioTException if the callable throws an exception
+   */
+  public static <T> T call(Logger logger, RpcCallable<T> callable, String methodName,
+      String description, Object... args) throws AlluxioTException {
+    return call(logger, callable, methodName, false, description, args);
   }
 
   /**
    * Calls the given {@link RpcCallable} and handles any exceptions thrown.
    *
-   * The logAnyFailure parameter indicates whether or not AlluxioExceptions are expected results
-   * (for example it would be false for the exists() call). In this case, we do not log the failure
-   * or increment failure metrics. When a RuntimeException is thrown, we always treat it as a
-   * failure and log an error and increment metrics.
+   * The failureOk parameter indicates whether or not AlluxioExceptions are expected results (for
+   * example it would be false for the exists() call). In this case, we do not log the failure or
+   * increment failure metrics. When a RuntimeException is thrown, we always treat it as a failure
+   * and log an error and increment metrics.
    *
    * @param logger the logger to use for this call
    * @param callable the callable to call
-   * @param logAnyFailure whether to log or increment metrics whenever the RPC fails
+   * @param methodName the name of the method, used for metrics
+   * @param failureOk whether failures are expected (affects logging and metrics)
+   * @param description the format string of the description, used for logging
+   * @param args the arguments for the description
    * @param <T> the return type of the callable
    * @return the return value from calling the callable
    * @throws AlluxioTException if the callable throws an exception
    */
-  public static <T> T call(Logger logger, RpcCallable<T> callable, boolean logAnyFailure)
-      throws AlluxioTException {
-    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(callable)).time()) {
-      logger.debug("Enter: {}", callable);
+  public static <T> T call(Logger logger, RpcCallable<T> callable, String methodName,
+      boolean failureOk, String description, Object... args) throws AlluxioTException {
+    // avoid string format for better performance if debug is off
+    String debugDesc = logger.isDebugEnabled() ? String.format(description, args) : null;
+    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(methodName)).time()) {
+      logger.debug("Enter: {}: {}", methodName, debugDesc);
       T ret = callable.call();
-      logger.debug("Exit (OK): {}", callable);
+      logger.debug("Exit (OK): {}: {}", methodName, debugDesc);
       return ret;
     } catch (AlluxioException e) {
-      logger.debug("Exit (Error): {}", callable, e);
-      if (logAnyFailure) {
-        MetricsSystem.counter(getQualifiedFailureMetricName(callable)).inc();
+      logger.debug("Exit (Error): {}: {}", methodName, debugDesc, e);
+      if (!failureOk) {
+        MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
         if (!logger.isDebugEnabled()) {
-          logger.warn("{}, Error={}", callable, e.getMessage());
+          logger.warn("Exit (Error): {}: {}, Error={}", methodName,
+              String.format(description, args), e);
         }
       }
       throw AlluxioStatusException.fromAlluxioException(e).toThrift();
     } catch (RuntimeException e) {
-      logger.error("Exit (Error): {}", callable, e);
-      MetricsSystem.counter(getQualifiedFailureMetricName(callable)).inc();
+      logger.error("Exit (Error): {}: {}", methodName, String.format(description, args), e);
+      MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
+      throw new InternalException(e).toThrift();
+    }
+  }
+
+  /**
+   * An interface representing a callable which can only throw Alluxio or IO exceptions.
+   *
+   * @param <T> the return type of the callable
+   */
+  public interface RpcCallableThrowsIOException<T> {
+    /**
+     * The RPC implementation.
+     *
+     * @return the return value from the RPC
+     */
+    T call() throws AlluxioException, IOException;
+  }
+
+  /**
+   * Calls the given {@link RpcCallableThrowsIOException} and handles any exceptions thrown. No call
+   * history or errors will be logged. This method should be used for internal RPCs.
+   *
+   * @param callable the callable to call
+   * @param <T> the return type of the callable
+   * @return the return value from calling the callable
+   * @throws AlluxioTException if the callable throws an exception
+   */
+  public static <T> T call(RpcCallableThrowsIOException<T> callable) throws AlluxioTException {
+    try {
+      return callable.call();
+    } catch (AlluxioException e) {
+      throw AlluxioStatusException.fromAlluxioException(e).toThrift();
+    } catch (IOException e) {
+      throw AlluxioStatusException.fromIOException(e).toThrift();
+    } catch (RuntimeException e) {
       throw new InternalException(e).toThrift();
     }
   }
@@ -92,94 +181,71 @@ public final class RpcUtils {
    *
    * @param logger the logger to use for this call
    * @param callable the callable to call
+   * @param methodName the name of the method, used for metrics
+   * @param description the format string of the description, used for logging
+   * @param args the arguments for the description
    * @param <T> the return type of the callable
    * @return the return value from calling the callable
    * @throws AlluxioTException if the callable throws an exception
    */
-  public static <T> T call(Logger logger, RpcCallableThrowsIOException<T> callable)
-      throws AlluxioTException {
-    return call(logger, callable, true);
+  public static <T> T call(Logger logger, RpcCallableThrowsIOException<T> callable,
+      String methodName, String description, Object... args) throws AlluxioTException {
+    return call(logger, callable, methodName, false, description, args);
   }
 
   /**
    * Calls the given {@link RpcCallableThrowsIOException} and handles any exceptions thrown.
    *
-   * The logAnyFailure parameter indicates whether or not AlluxioExceptions and IOExceptions are
+   * The failureOk parameter indicates whether or not AlluxioExceptions and IOExceptions are
    * expected results (for example it would be false for the exists() call). In this case, we do not
    * log the failure or increment failure metrics. When a RuntimeException is thrown, we always
    * treat it as a failure and log an error and increment metrics.
    *
    * @param logger the logger to use for this call
    * @param callable the callable to call
-   * @param logAnyFailure whether to log or increment metrics whenever the RPC fails
+   * @param methodName the name of the method, used for metrics
+   * @param failureOk whether failures are expected (affects logging and metrics)
+   * @param description the format string of the description, used for logging
+   * @param args the arguments for the description
    * @param <T> the return type of the callable
    * @return the return value from calling the callable
    * @throws AlluxioTException if the callable throws an exception
    */
   public static <T> T call(Logger logger, RpcCallableThrowsIOException<T> callable,
-      boolean logAnyFailure) throws AlluxioTException {
-    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(callable)).time()) {
-      logger.debug("Enter: {}", callable);
+      String methodName, boolean failureOk, String description, Object... args)
+      throws AlluxioTException {
+    // avoid string format for better performance if debug is off
+    String debugDesc = logger.isDebugEnabled() ? String.format(description, args) : null;
+    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(methodName)).time()) {
+      logger.debug("Enter: {}: {}", methodName, debugDesc);
       T ret = callable.call();
-      logger.debug("Exit (OK): {}", callable);
+      logger.debug("Exit (OK): {}: {}", methodName, debugDesc);
       return ret;
     } catch (AlluxioException e) {
-      logger.debug("Exit (Error): {}", callable, e);
-      if (logAnyFailure) {
-        MetricsSystem.counter(getQualifiedFailureMetricName(callable)).inc();
+      logger.debug("Exit (Error): {}: {}", methodName, debugDesc, e);
+      if (!failureOk) {
+        MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
         if (!logger.isDebugEnabled()) {
-          logger.warn("{}, Error={}", callable, e.getMessage());
+          logger.warn("Exit (Error): {}: {}, Error={}", methodName,
+              String.format(description, args), e);
         }
       }
       throw AlluxioStatusException.fromAlluxioException(e).toThrift();
     } catch (IOException e) {
-      logger.debug("Exit (Error): {}", callable, e);
-      if (logAnyFailure) {
-        MetricsSystem.counter(getQualifiedFailureMetricName(callable)).inc();
+      logger.debug("Exit (Error): {}: {}", methodName, debugDesc, e);
+      if (!failureOk) {
+        MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
         if (!logger.isDebugEnabled()) {
-          logger.warn("{}, Error={}", callable, e.getMessage());
+          logger.warn("Exit (Error): {}: {}, Error={}", methodName,
+              String.format(description, args), e);
         }
       }
       throw AlluxioStatusException.fromIOException(e).toThrift();
     } catch (RuntimeException e) {
-      logger.error("Exit (Error): {}", callable, e);
-      MetricsSystem.counter(getQualifiedFailureMetricName(callable)).inc();
+      logger.error("Exit (Error): {}: {}", methodName, String.format(description, args), e);
+      MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
       throw new InternalException(e).toThrift();
     }
-  }
-
-  /**
-   * An interface representing objects which denote their method in the toString method in the
-   * format of <Method Name>:<Optional description>.
-   */
-  interface NamedMethod {}
-
-  /**
-   * An interface representing a callable which can only throw Alluxio exceptions.
-   *
-   * @param <T> the return type of the callable
-   */
-  public interface RpcCallable<T> extends NamedMethod {
-    /**
-     * The RPC implementation.
-     *
-     * @return the return value from the RPC
-     */
-    T call() throws AlluxioException;
-  }
-
-  /**
-   * An interface representing a callable which can only throw Alluxio or IO exceptions.
-   *
-   * @param <T> the return type of the callable
-   */
-  public interface RpcCallableThrowsIOException<T> extends NamedMethod {
-    /**
-     * The RPC implementation.
-     *
-     * @return the return value from the RPC
-     */
-    T call() throws AlluxioException, IOException;
   }
 
   /**
@@ -187,7 +253,7 @@ public final class RpcUtils {
    *
    * @param <T> the return type of the callable
    */
-  public interface NettyRPCCallable<T> extends NamedMethod {
+  public interface NettyRpcCallable<T> {
     /**
      * The RPC implementation.
      *
@@ -206,32 +272,39 @@ public final class RpcUtils {
   /**
    * Handles a netty RPC callable with logging.
    *
-   * @param logger the logger
-   * @param callable the netty RPC callable
-   * @param <T> the return type
+   * @param logger the logger to use for this call
+   * @param callable the callable to call
+   * @param methodName the name of the method, used for metrics
+   * @param description the format string of the description, used for logging
+   * @param args the arguments for the description
+   * @param <T> the return type of the callable
    * @return the rpc result
    */
-  public static <T> T nettyRPCAndLog(Logger logger, NettyRPCCallable<T> callable) {
-    logger.debug("Enter: {}", callable);
-    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(callable)).time()) {
+  public static <T> T nettyRPCAndLog(Logger logger, NettyRpcCallable<T> callable,
+      String methodName, String description, Object... args) {
+    // avoid string format for better performance if debug is off
+    String debugDesc = logger.isDebugEnabled() ? String.format(description, args) : null;
+    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(methodName)).time()) {
+      logger.debug("Enter: {}: {}", methodName, debugDesc);
       T result = callable.call();
-      logger.debug("Exit (OK): {}", callable);
+      logger.debug("Exit (OK): {}: {}", methodName, debugDesc);
       return result;
     } catch (Exception e) {
-      logger.debug("Exit (Error): {}, Error={}", callable, e.getMessage());
-      MetricsSystem.counter(getQualifiedFailureMetricName(callable)).inc();
+      logger
+          .warn("Exit (Error): {}: {}, Error={}", methodName, String.format(description, args), e);
+      MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
       callable.exceptionCaught(e);
     }
     return null;
   }
 
-  private static String getQualifiedMetricName(NamedMethod method) {
-    String name = method.toString().split(METHOD_NAME_SEPARATOR)[0];
+  private static String getQualifiedMetricName(String methodName) {
+    String name = methodName.split(METHOD_NAME_SEPARATOR)[0];
     return getQualifiedMetricNameInternal(name);
   }
 
-  private static String getQualifiedFailureMetricName(NamedMethod method) {
-    String name = method.toString().split(METHOD_NAME_SEPARATOR)[0];
+  private static String getQualifiedFailureMetricName(String methodName) {
+    String name = methodName.split(METHOD_NAME_SEPARATOR)[0];
     return getQualifiedMetricNameInternal(name + "Failures");
   }
 
