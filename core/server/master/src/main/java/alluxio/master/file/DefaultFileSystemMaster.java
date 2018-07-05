6fa8297b68c14d1ec1376ee3e740d29e32b78dcd
madanadit <adit@alluxio.com>
2018-07-05 15:32:54 -0700

    Fix read only mount

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -1493,9 +1493,7 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
         auditContext.setAllowed(false);
         throw e;
       }
-      if (!options.isAlluxioOnly()) {
-        mMountTable.checkUnderWritableMountPoint(path);
-      }
+      mMountTable.checkUnderWritableMountPoint(path);
       // Possible ufs sync.
       syncMetadata(rpcContext, inodePath, lockingScheme,
           options.isRecursive() ? DescendantType.ALL : DescendantType.ONE);
@@ -3118,6 +3116,7 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
         auditContext.setAllowed(false);
         throw e;
       }
+      mMountTable.checkUnderWritableMountPoint(path);
       // Possible ufs sync.
       syncMetadata(rpcContext, inodePath, lockingScheme,
           options.isRecursive() ? DescendantType.ALL : DescendantType.ONE);
