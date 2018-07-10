782632bfcfc70db18d92144169e7404771f4c84d
David Zhu <david@alluxio.com>
2018-07-10 13:29:16 -0700

    remove unused imports

--- core/common/src/main/java/alluxio/security/authorization/AccessControlList.java
+++ core/common/src/main/java/alluxio/security/authorization/AccessControlList.java
@@ -21,7 +21,6 @@ import com.google.common.collect.ImmutableList;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
