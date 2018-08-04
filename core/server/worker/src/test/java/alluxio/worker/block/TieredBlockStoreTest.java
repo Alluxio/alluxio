6c3104f63157bbe0a873c009b5850cd4b9824e9b
wjj2 <17862700172@163.com>
2018-08-04 11:14:39 +0800

    wjj2-pasaclass

--- core/server/worker/src/test/java/alluxio/worker/block/TieredBlockStoreTest.java
+++ core/server/worker/src/test/java/alluxio/worker/block/TieredBlockStoreTest.java
@@ -6,7 +6,6 @@
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied, as more fully set forth in the License.
  *
- *
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
