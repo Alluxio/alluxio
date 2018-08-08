8d4351de98b20716395f8bc57292ef43bf0cf478
David Zhu <david@alluxio.com>
2018-08-08 16:46:13 -0700

    bug fix

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -2671,7 +2671,7 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
                 inodePath.createTempPathForChild(childStatus.getName())) {
               LoadMetadataOptions loadMetadataOptions =
                   LoadMetadataOptions.defaults().setLoadDescendantType(DescendantType.NONE)
-                      .setCreateAncestors(false).setUfsStatus(childStatus);
+                      .setCreateAncestors(true).setUfsStatus(childStatus);
               loadMetadataAndJournal(rpcContext, tempInodePath, loadMetadataOptions);
 
               if (options.getLoadDescendantType() == DescendantType.ALL
