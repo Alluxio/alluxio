b98902b6d6ddfca23e025c196d3a4b4e9c22e73b
Guoqiang Li <witgo@qq.com>
2018-08-09 18:56:00 +0800

    Modifying the ExceptionMessage#INVALID_ARGS_SORT_FIELD to use the ASCII quote `

--- core/common/src/main/java/alluxio/exception/ExceptionMessage.java
+++ core/common/src/main/java/alluxio/exception/ExceptionMessage.java
@@ -188,7 +188,7 @@ public enum ExceptionMessage {
   INVALID_ARGS_NUM("Command {0} takes {1} arguments, not {2}"),
   INVALID_ARGS_NUM_INSUFFICIENT("Command {0} requires at least {1} arguments ({2} provided)"),
   INVALID_ARGS_NUM_TOO_MANY("Command {0} requires at most {1} arguments ({2} provided)"),
-  INVALID_ARGS_SORT_FIELD("Invalid sort option ‘{0}’ for --sort"),
+  INVALID_ARGS_SORT_FIELD("Invalid sort option `{0}` for --sort"),
 
   // extension shell
   INVALID_EXTENSION_NOT_JAR("File {0} does not have the extension JAR"),
