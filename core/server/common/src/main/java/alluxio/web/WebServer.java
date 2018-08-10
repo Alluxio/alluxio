33dae17239d8140f29012b97581cdbc866b8f49a
bf8086 <binfeng@alluxio.com>
2018-08-10 13:27:24 -0700

    Fix code styles

--- core/server/common/src/main/java/alluxio/web/WebServer.java
+++ core/server/common/src/main/java/alluxio/web/WebServer.java
@@ -32,7 +32,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.annotation.concurrent.NotThreadSafe;
