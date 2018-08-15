69f9e3cc5646486d885608e70ac4b06a31eda74a
Andrew Audibert <andrew@alluxio.com>
2018-08-15 16:03:56 -0700

    Leave a directory mounted in Mount op

--- tests/src/main/java/alluxio/master/backcompat/ops/Mount.java
+++ tests/src/main/java/alluxio/master/backcompat/ops/Mount.java
@@ -24,25 +24,34 @@ import java.io.File;
  * Test for mounting and unmounting.
  */
 public final class Mount extends FsTestOp {
-  private static final AlluxioURI ALLUXIO_PATH = new AlluxioURI("/mount");
+  private static final AlluxioURI ALLUXIO_MOUNT_PATH = new AlluxioURI("/mount");
+  private static final AlluxioURI ALLUXIO_UNMOUNT_PATH = new AlluxioURI("/unmount");
 
-  private static final String LOCAL_FS_MOUNT_DIR = "/tmp/alluxioMount";
+  private static final String LOCAL_FS_MOUNT_DIR = "/tmp/alluxioTest/mount";
+  private static final String LOCAL_FS_UNMOUNT_DIR = "/tmp/alluxioTest/unmount";
   // This creates a requirement that /tmp/alluxioMount exists and is readable by the test
-  private static final AlluxioURI UFS_PATH = new AlluxioURI(LOCAL_FS_MOUNT_DIR);
+  private static final AlluxioURI UFS_MOUNT_PATH = new AlluxioURI(LOCAL_FS_MOUNT_DIR);
+  private static final AlluxioURI UFS_UNMOUNT_PATH = new AlluxioURI(LOCAL_FS_UNMOUNT_DIR);
 
   @Override
   @SuppressFBWarnings(
       value = "DMI_HARDCODED_ABSOLUTE_FILENAME",
       justification = "Findbugs doesn't like the use of an absolute path. However, we need one "
-      + "here so that generated journals are portable across machines")
+          + "here so that generated journals are portable across machines")
   public void apply(FileSystem fs) throws Exception {
     new File(LOCAL_FS_MOUNT_DIR).mkdirs();
-    fs.mount(ALLUXIO_PATH, UFS_PATH);
-    fs.unmount(ALLUXIO_PATH);
+    fs.mount(ALLUXIO_MOUNT_PATH, UFS_MOUNT_PATH);
+
+    new File(LOCAL_FS_UNMOUNT_DIR).mkdirs();
+    fs.mount(ALLUXIO_UNMOUNT_PATH, UFS_UNMOUNT_PATH);
+    fs.unmount(ALLUXIO_UNMOUNT_PATH);
   }
 
   @Override
   public void check(FileSystem fs) throws Exception {
-    Assert.assertFalse("Mounted and unmounted directory should not exist", fs.exists(ALLUXIO_PATH));
+    Assert.assertTrue("Mounted directory should be a mount point",
+        fs.getStatus(ALLUXIO_MOUNT_PATH).isMountPoint());
+    Assert.assertFalse("Mounted and unmounted directory should not exist",
+        fs.exists(ALLUXIO_UNMOUNT_PATH));
   }
 }
