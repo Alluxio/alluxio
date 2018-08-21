ec9a77359eaa34c914581dac00ae68447abc5550
Bin Fan <fanbin103@gmail.com>
2018-08-21 10:56:50 -0700

    [ALLUXIO-3259] Fix NPE when passing an option to UFS root from extension (#7784)
    * Fix NPE when passing an option to UFS root from extension
    
    * Fix failing test

--- core/common/src/test/java/alluxio/ConfigurationTest.java
+++ core/common/src/test/java/alluxio/ConfigurationTest.java
@@ -904,4 +904,14 @@ public class ConfigurationTest {
       assertEquals("0.0.0.0", Configuration.get(PropertyKey.PROXY_WEB_BIND_HOST));
     }
   }
+
+  @Test
+  public void initConfWithExtenstionProperty() throws Exception {
+    try (Closeable p = new SystemPropertyRule("alluxio.master.journal.ufs.option.fs.obs.endpoint",
+        "foo").toResource()) {
+      Configuration.reset();
+      assertEquals("foo",
+          Configuration.get(Template.MASTER_JOURNAL_UFS_OPTION_PROPERTY.format("fs.obs.endpoint")));
+    }
+  }
 }
