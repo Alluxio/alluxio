b575e61ac2037782b319d1949e9be45ffdf6f821
Andrew Audibert <andrew@alluxio.com>
2018-08-24 10:44:34 -0700

    Update UFS mode to journal as a part of modify (#7794)

--- core/server/master/src/main/java/alluxio/underfs/MasterUfsManager.java
+++ core/server/master/src/main/java/alluxio/underfs/MasterUfsManager.java
@@ -12,24 +12,25 @@
 package alluxio.underfs;
 
 import alluxio.AlluxioURI;
-import alluxio.collections.ConcurrentHashSet;
 import alluxio.exception.InvalidPathException;
-import alluxio.master.file.RpcContext;
+import alluxio.master.journal.JournalContext;
 import alluxio.master.journal.JournalEntryIterable;
-import alluxio.proto.journal.File.UfsMode;
+import alluxio.master.journal.JournalEntryReplayable;
+import alluxio.proto.journal.File;
 import alluxio.proto.journal.File.UpdateUfsModeEntry;
 import alluxio.proto.journal.Journal.JournalEntry;
-import alluxio.resource.CloseableResource;
+import alluxio.underfs.UnderFileSystem.UfsMode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Set;
+import java.util.function.Supplier;
 
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -37,112 +38,39 @@ import javax.annotation.concurrent.ThreadSafe;
  * A class that manages the UFS for master servers.
  */
 @ThreadSafe
-public final class MasterUfsManager extends AbstractUfsManager implements JournalEntryIterable {
+public final class MasterUfsManager extends AbstractUfsManager
+    implements JournalEntryIterable, JournalEntryReplayable {
   private static final Logger LOG = LoggerFactory.getLogger(MasterUfsManager.class);
 
-  /**
-   * {@link alluxio.underfs.UnderFileSystem.UfsMode} and mount ids corresponding to a physical ufs.
-   *
-   * Only the ufs modes part of this data structure is journaled.
-   */
-  public static class UfsState {
-    private UnderFileSystem.UfsMode mUfsMode;
-    private ConcurrentHashSet<Long> mMountIds;
+  private final State mState;
 
-    /**
-     * Construct a new instance of UfsState w/ defaults.
-     */
-    UfsState() {
-      mUfsMode = UnderFileSystem.UfsMode.READ_WRITE;
-      mMountIds = new ConcurrentHashSet<>();
-    }
+  /** A set of all managed ufs roots. */
+  private final Set<String> mUfsRoots;
 
-    /**
-     * Add a mount id to list using physical ufs.
-     *
-     * @param mountId the mount id
-     */
-    void addMount(long mountId) {
-      mMountIds.addIfAbsent(mountId);
-    }
-
-    /**
-     * Remove mount id from list using physical ufs.
-     *
-     * @param mountId the mount id
-     * @return true, if mount list is empty
-     */
-    boolean removeMount(long mountId) {
-      mMountIds.remove(mountId);
-      return mMountIds.size() == 0;
-    }
-
-    /**
-     * @return the physical ufs operation mode
-     */
-    UnderFileSystem.UfsMode getUfsMode() {
-      return mUfsMode;
-    }
-
-    /**
-     * Set the operation mode.
-     *
-     * @param ufsMode the ufs operation mode
-     */
-    void setUfsMode(UnderFileSystem.UfsMode ufsMode) {
-      mUfsMode = ufsMode;
-    }
-  }
-
-  // The physical ufs state for all managed mounts
-  private ConcurrentHashMap<String, UfsState> mPhysicalUfsToState =
-      new ConcurrentHashMap<>();
+  /** Mapping from mount ID to ufs root. */
+  private final Map<Long, String> mIdToRoot;
 
   /**
    * Constructs the instance of {@link MasterUfsManager}.
    */
-  public MasterUfsManager() {}
-
-  @Override
-  public void addMount(long mountId, final AlluxioURI ufsUri,
-                       final UnderFileSystemConfiguration ufsConf) {
-    super.addMount(mountId, ufsUri, ufsConf);
-
-    try (CloseableResource<UnderFileSystem> ufsResource = get(mountId).acquireUfsResource()) {
-      UnderFileSystem ufs = ufsResource.get();
-      for (String physicalUfs : ufs.getPhysicalStores()) {
-        mPhysicalUfsToState.compute(physicalUfs, (k, v) -> {
-          if (v == null) {
-            v = new UfsState();
-          }
-          v.addMount(mountId);
-          return v;
-        });
-      }
-    } catch (Exception e) {
-      // Unable to resolve mount point
-      LOG.error(e.getMessage());
-    }
+  public MasterUfsManager() {
+    mState = new State();
+    mUfsRoots = new HashSet<>();
+    mIdToRoot = new HashMap<>();
   }
 
   @Override
-  public void removeMount(long mountId) {
-    try (CloseableResource<UnderFileSystem> ufsResource = get(mountId).acquireUfsResource()) {
-      UnderFileSystem ufs = ufsResource.get();
-      for (String physicalUfs : ufs.getPhysicalStores()) {
-        mPhysicalUfsToState.computeIfPresent(physicalUfs, (k, v) -> {
-          if (v.removeMount(mountId)) {
-            // Remove key if list is empty
-            return null;
-          }
-          return v;
-        });
-      }
-    } catch (Exception e) {
-      // Unable to resolve mount point
-      LOG.error(e.getMessage());
-    }
+  public synchronized void addMount(long mountId, AlluxioURI ufsUri,
+      UnderFileSystemConfiguration ufsConf) {
+    super.addMount(mountId, ufsUri, ufsConf);
+    String root = ufsUri.getRootPath();
+    mUfsRoots.add(root);
+    mIdToRoot.put(mountId, root);
+  }
 
+  @Override
+  public synchronized void removeMount(long mountId) {
+    mIdToRoot.remove(mountId);
     super.removeMount(mountId);
   }
 
@@ -153,13 +81,11 @@ public final class MasterUfsManager extends AbstractUfsManager implements Journa
    * @param physicalStores the physical stores for the mount resolution
    * @return the state of physical UFS for given mount resolution
    */
-  public Map<String, UnderFileSystem.UfsMode> getPhysicalUfsState(List<String> physicalStores) {
+  public synchronized Map<String, UnderFileSystem.UfsMode> getPhysicalUfsState(
+      List<String> physicalStores) {
     Map<String, UnderFileSystem.UfsMode> ufsModeState = new HashMap<>();
     for (String physicalUfs : physicalStores) {
-      UfsState ufsState = mPhysicalUfsToState.get(new AlluxioURI(physicalUfs).getRootPath());
-      if (ufsState != null) {
-        ufsModeState.put(physicalUfs, ufsState.getUfsMode());
-      }
+      ufsModeState.put(physicalUfs, mState.getUfsMode(new AlluxioURI(physicalUfs).getRootPath()));
     }
     return ufsModeState;
   }
@@ -167,56 +93,81 @@ public final class MasterUfsManager extends AbstractUfsManager implements Journa
   /**
    * Set the operation mode the given physical ufs.
    *
+   * @param journalContext the journal context
    * @param ufsPath the physical ufs path (scheme and authority only)
    * @param ufsMode the ufs operation mode
-   * @param rpcContext rpc context
    * @throws InvalidPathException if no managed ufs covers the given path
    */
-  public void setUfsMode(AlluxioURI ufsPath, UnderFileSystem.UfsMode ufsMode,
-      RpcContext rpcContext)
-      throws InvalidPathException {
+  public synchronized void setUfsMode(Supplier<JournalContext> journalContext, AlluxioURI ufsPath,
+      UfsMode ufsMode) throws InvalidPathException {
     LOG.info("Set ufs mode for {} to {}", ufsPath, ufsMode);
-    String key = ufsPath.getRootPath();
-    UfsState state = mPhysicalUfsToState.compute(key, (k, v) -> {
-      if (v == null) {
-        // No managed ufs uses the given physical ufs path
-        return v;
-      }
-      // Found a managed ufs for the given physical path
-      v.setUfsMode(ufsMode);
-      return v;
-    });
 
-    if (state == null) {
-      LOG.warn("No managed ufs for physical ufs path {}", ufsPath);
-      throw new InvalidPathException(String.format("Ufs path %s is not managed", ufsPath));
+    String root = ufsPath.getRootPath();
+    if (!mUfsRoots.contains(root)) {
+      LOG.warn("No managed ufs for physical ufs path {}", root);
+      throw new InvalidPathException(String.format("Unknown Ufs path %s", root));
     }
 
-    rpcContext.journal(JournalEntry.newBuilder()
-        .setUpdateUfsMode(UpdateUfsModeEntry.newBuilder()
-            .setUfsPath(key)
-            .setUfsMode(UfsMode.valueOf(ufsMode.name())))
+    mState.applyAndJournal(journalContext, UpdateUfsModeEntry.newBuilder()
+        .setUfsPath(ufsPath.getRootPath())
+        .setUfsMode(File.UfsMode.valueOf(ufsMode.name()))
         .build());
   }
 
   @Override
   public Iterator<JournalEntry> getJournalEntryIterator() {
-    Iterator<Entry<String, UfsState>> it = mPhysicalUfsToState.entrySet().iterator();
-    return new Iterator<JournalEntry>() {
-      @Override
-      public boolean hasNext() {
-        return it.hasNext();
-      }
+    return mState.getJournalEntryIterator();
+  }
 
-      @Override
-      public JournalEntry next() {
-        Entry<String, UfsState> entry = it.next();
-        return JournalEntry.newBuilder()
-            .setUpdateUfsMode(UpdateUfsModeEntry.newBuilder()
-                .setUfsPath(entry.getKey())
-                .setUfsMode(UfsMode.valueOf(entry.getValue().getUfsMode().name())))
-            .build();
+  @Override
+  public boolean replayJournalEntryFromJournal(JournalEntry entry) {
+    return mState.replayJournalEntryFromJournal(entry);
+  }
+
+  private static class State implements JournalEntryReplayable, JournalEntryIterable {
+    // The physical ufs state for all managed mounts. The keys are URIs normalized to set the path
+    // to "/", e.g. "hdfs://namenode/" or just "/" for local filesystem.
+    private final Map<String, UfsMode> mUfsModes = new HashMap<>();
+
+    /**
+     * @param key a root ufs path
+     * @return the mode for that ufs
+     */
+    public UfsMode getUfsMode(String key) {
+      return mUfsModes.getOrDefault(key, UfsMode.READ_WRITE);
+    }
+
+    @Override
+    public boolean replayJournalEntryFromJournal(JournalEntry entry) {
+      if (entry.hasUpdateUfsMode()) {
+        apply(entry.getUpdateUfsMode());
+      } else {
+        return false;
       }
-    };
+      return true;
+    }
+
+    /**
+     * @param context journal context
+     * @param entry update ufs mode entry
+     */
+    public void applyAndJournal(Supplier<JournalContext> context, UpdateUfsModeEntry entry) {
+      apply(entry);
+      context.get().append(JournalEntry.newBuilder().setUpdateUfsMode(entry).build());
+    }
+
+    private void apply(UpdateUfsModeEntry entry) {
+      mUfsModes.put(entry.getUfsPath(), UfsMode.valueOf(entry.getUfsMode().name()));
+    }
+
+    @Override
+    public Iterator<JournalEntry> getJournalEntryIterator() {
+      return mUfsModes.entrySet().stream()
+          .map(e -> JournalEntry.newBuilder().setUpdateUfsMode(UpdateUfsModeEntry.newBuilder()
+              .setUfsPath(e.getKey())
+              .setUfsMode(File.UfsMode.valueOf(e.getValue().name())))
+              .build())
+          .iterator();
+    }
   }
 }
