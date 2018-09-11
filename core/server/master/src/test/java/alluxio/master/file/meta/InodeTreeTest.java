ec49242d5e1880963578ec180d3f39b8b8ad6459
Zac <zacdblanco@gmail.com>
2018-09-11 10:40:33 -0700

    [SMALLFIX] - Quote formatted paths in path exceptions (#7833) (#7834)
    * quote formatted paths in path exceptions
    
    * Fix unit and integration test

--- core/server/master/src/test/java/alluxio/master/file/meta/InodeTreeTest.java
+++ core/server/master/src/test/java/alluxio/master/file/meta/InodeTreeTest.java
@@ -397,7 +397,7 @@ public final class InodeTreeTest {
   @Test
   public void getInodeByNonexistingPath() throws Exception {
     mThrown.expect(FileDoesNotExistException.class);
-    mThrown.expectMessage("Path /test does not exist");
+    mThrown.expectMessage("Path \"/test\" does not exist");
 
     assertFalse(mTree.inodePathExists(TEST_URI));
     getInodeByPath(mTree, TEST_URI);
@@ -409,7 +409,7 @@ public final class InodeTreeTest {
   @Test
   public void getInodeByNonexistingNestedPath() throws Exception {
     mThrown.expect(FileDoesNotExistException.class);
-    mThrown.expectMessage("Path /nested/test/file does not exist");
+    mThrown.expectMessage("Path \"/nested/test/file\" does not exist");
 
     createPath(mTree, NESTED_URI, sNestedDirectoryOptions);
     assertFalse(mTree.inodePathExists(NESTED_FILE_URI));
