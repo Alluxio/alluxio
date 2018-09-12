2a432e8d2192e31baa63c5c6e33fb056ea462a4b
Andrew Audibert <andrew@alluxio.com>
2018-09-12 07:57:12 -1000

    [SMALLFIX] Refactor netty epoll check (#7838)
    * Refactor netty epoll check
    
    * Fix findbugs

--- core/common/src/main/java/alluxio/util/network/NettyUtils.java
+++ core/common/src/main/java/alluxio/util/network/NettyUtils.java
@@ -46,35 +46,12 @@ import javax.annotation.concurrent.ThreadSafe;
 public final class NettyUtils {
   private static final Logger LOG = LoggerFactory.getLogger(NettyUtils.class);
 
-  public static final ChannelType USER_CHANNEL_TYPE;
-  public static final ChannelType WORKER_CHANNEL_TYPE;
+  public static final ChannelType USER_CHANNEL_TYPE =
+      getChannelType(PropertyKey.USER_NETWORK_NETTY_CHANNEL);
+  public static final ChannelType WORKER_CHANNEL_TYPE =
+      getChannelType(PropertyKey.WORKER_NETWORK_NETTY_CHANNEL);
 
-  static {
-    boolean epollAvailable = Epoll.isAvailable();
-    if (!epollAvailable) {
-      LOG.info("EPOLL is not available, will use NIO");
-    } else {
-      // check that epoll is available in netty
-      try {
-        EpollChannelOption.class.getField("EPOLL_MODE");
-        LOG.info("EPOLL_MODE is available");
-        epollAvailable = true;
-      } catch (Throwable e) {
-        LOG.warn("EPOLL_MODE is not supported in netty with version < 4.0.26.Final, will use NIO");
-        epollAvailable = false;
-      }
-    }
-
-    if (epollAvailable) {
-      USER_CHANNEL_TYPE =
-          Configuration.getEnum(PropertyKey.USER_NETWORK_NETTY_CHANNEL, ChannelType.class);
-      WORKER_CHANNEL_TYPE =
-          Configuration.getEnum(PropertyKey.WORKER_NETWORK_NETTY_CHANNEL, ChannelType.class);
-    } else {
-      USER_CHANNEL_TYPE = ChannelType.NIO;
-      WORKER_CHANNEL_TYPE = ChannelType.NIO;
-    }
-  }
+  private static Boolean sNettyEpollAvailable = null;
 
   private NettyUtils() {}
 
@@ -183,5 +160,42 @@ public final class NettyUtils {
     } else {
       return workerNetAddress.getHost().equals(NetworkAddressUtils.getClientHostName());
     }
+  }
+
+  /**
+   * @return whether netty epoll is available to the system
+   */
+  public static synchronized boolean isNettyEpollAvailable() {
+    if (sNettyEpollAvailable == null) {
+      // Only call checkNettyEpollAvailable once ever so that we only log the result once.
+      sNettyEpollAvailable = checkNettyEpollAvailable();
+    }
+    return sNettyEpollAvailable;
+  }
+
+  private static boolean checkNettyEpollAvailable() {
+    if (!Epoll.isAvailable()) {
+      LOG.info("EPOLL is not available, will use NIO");
+      return false;
+    }
+    try {
+      EpollChannelOption.class.getField("EPOLL_MODE");
+      LOG.info("EPOLL_MODE is available");
+      return true;
+    } catch (Throwable e) {
+      LOG.warn("EPOLL_MODE is not supported in netty with version < 4.0.26.Final, will use NIO");
+      return false;
+    }
+  }
+
+  /**
+   * @param key the property key for looking up the configured channel type
+   * @return the channel type to use
+   */
+  private static ChannelType getChannelType(PropertyKey key) {
+    if (!isNettyEpollAvailable()) {
+      return ChannelType.NIO;
+    }
+    return Configuration.getEnum(key, ChannelType.class);
   }
 }
