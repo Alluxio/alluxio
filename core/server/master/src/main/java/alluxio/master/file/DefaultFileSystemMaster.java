f9e32c2daf5d9fabd8a89e06d42bff3b715346d4
David Zhu <davidyuzhu@gmail.com>
2018-09-14 17:02:34 -0700

    [SMALLFIX] Cherry-pick NPE fix for loadMetadata (#7855)
    * [SMALLFIX] Check for null in ListStatus (#7852)
    
    * Check for null in ListStatus
    
    * Add comments and change return statement to an exception
    
    * [SMALLFIX] Actually adding catch and log to loadMetadataInternal (#7854)
    
    * add an exception catch and log so that we continue to process loadMetadata if one of the children is
    not accessible.
    
    * change debug level to info

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -2640,6 +2640,12 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
             listOptions.setRecursive(false);
           }
           UfsStatus[] children = ufs.listStatus(ufsUri.toString(), listOptions);
+          // children can be null if the pathname does not denote a directory
+          // or if the we do not have permission to listStatus on the directory in the ufs.
+          if (children == null) {
+            throw new IOException("Failed to loadMetadata because ufs can not listStatus at path "
+                + ufsUri.toString());
+          }
           Arrays.sort(children, Comparator.comparing(UfsStatus::getName));
 
           for (UfsStatus childStatus : children) {
@@ -2660,8 +2666,13 @@ public final class DefaultFileSystemMaster extends AbstractMaster implements Fil
               LoadMetadataOptions loadMetadataOptions =
                   LoadMetadataOptions.defaults().setLoadDescendantType(DescendantType.NONE)
                       .setCreateAncestors(false).setUfsStatus(childStatus);
-              loadMetadataAndJournal(rpcContext, tempInodePath, loadMetadataOptions);
-
+              try {
+                loadMetadataAndJournal(rpcContext, tempInodePath, loadMetadataOptions);
+              } catch (Exception e) {
+                LOG.info("Failed to loadMetadata: inodePath={}, options={}.",
+                    tempInodePath.getUri(), loadMetadataOptions, e);
+                continue;
+              }
               if (options.getLoadDescendantType() == DescendantType.ALL
                   && tempInodePath.getInode().isDirectory()) {
                 InodeDirectory inodeDirectory = (InodeDirectory) tempInodePath.getInode();
