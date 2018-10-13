8bb07cf57b4d7a6a6ca653e05f2abea7d9138b1d
nju-grx <18362916392@163.com>
2018-10-13 14:30:58 +0800

    Delete unused import.

--- core/server/common/src/main/java/alluxio/underfs/AbstractUfsManager.java
+++ core/server/common/src/main/java/alluxio/underfs/AbstractUfsManager.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Supplier;
+
 
 /**
  * Basic implementation of {@link UfsManager}.
