214d80419d9ff8f6dfcc8b834ac27a5e966b4ebb
CarmenJi <747707593@qq.com>
2018-10-23 16:03:56 +0800

    [SMALLFIX] Java 8 Improvement: replace anonymous type with lambda in alluxio.worker.AlluxioJobWorkerRestServiceHandler

--- job/server/src/main/java/alluxio/worker/AlluxioJobWorkerRestServiceHandler.java
+++ job/server/src/main/java/alluxio/worker/AlluxioJobWorkerRestServiceHandler.java
@@ -42,64 +42,64 @@ import javax.ws.rs.core.Response;
 @Path(AlluxioJobWorkerRestServiceHandler.SERVICE_PREFIX)
 @Produces(MediaType.APPLICATION_JSON)
 public final class AlluxioJobWorkerRestServiceHandler {
-    public static final String SERVICE_PREFIX = "job_worker";
+  public static final String SERVICE_PREFIX = "job_worker";
 
-    // endpoints
-    public static final String GET_INFO = "info";
+  // endpoints
+  public static final String GET_INFO = "info";
 
-    // queries
-    public static final String QUERY_RAW_CONFIGURATION = "raw_configuration";
+  // queries
+  public static final String QUERY_RAW_CONFIGURATION = "raw_configuration";
 
-    private final JobWorkerProcess mJobWorker;
+  private final JobWorkerProcess mJobWorker;
 
-    /**
-     * @param context context for the servlet
-     */
-    public AlluxioJobWorkerRestServiceHandler(@Context ServletContext context) {
-        mJobWorker = (JobWorkerProcess) context
-                .getAttribute(JobWorkerWebServer.ALLUXIO_JOB_WORKER_SERVLET_RESOURCE_KEY);
-    }
+  /**
+   * @param context context for the servlet
+   */
+  public AlluxioJobWorkerRestServiceHandler(@Context ServletContext context) {
+    mJobWorker = (JobWorkerProcess) context
+        .getAttribute(JobWorkerWebServer.ALLUXIO_JOB_WORKER_SERVLET_RESOURCE_KEY);
+  }
 
-    /**
-     * @param rawConfiguration if it's true, raw configuration values are returned,
-     *                         otherwise, they are looked up; if it's not provided in URL queries, then
-     *                         it is null, which means false.
-     * @return the response object
-     * @summary get the Alluxio job worker information
-     */
-    @GET
-    @Path(GET_INFO)
-    @ReturnType("alluxio.wire.AlluxioJobWorkerInfo")
-    public Response getInfo(@QueryParam(QUERY_RAW_CONFIGURATION) final Boolean rawConfiguration) {
-        // TODO(jiri): Add a mechanism for retrieving only a subset of the fields.
-        return RestUtils.call(() -> {
-            boolean rawConfig = false;
-            if (rawConfiguration != null) {
-                rawConfig = rawConfiguration;
-            }
-            AlluxioJobWorkerInfo result =
-                    new AlluxioJobWorkerInfo()
-                            .setConfiguration(getConfigurationInternal(rawConfig))
-                            .setStartTimeMs(mJobWorker.getStartTimeMs())
-                            .setUptimeMs(mJobWorker.getUptimeMs())
-                            .setVersion(RuntimeConstants.VERSION);
-            return result;
-        });
-    }
+  /**
+   * @summary get the Alluxio job worker information
+   * @param rawConfiguration if it's true, raw configuration values are returned,
+   *    otherwise, they are looked up; if it's not provided in URL queries, then
+   *    it is null, which means false.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_INFO)
+  @ReturnType("alluxio.wire.AlluxioJobWorkerInfo")
+  public Response getInfo(@QueryParam(QUERY_RAW_CONFIGURATION) final Boolean rawConfiguration) {
+    // TODO(jiri): Add a mechanism for retrieving only a subset of the fields.
+    return RestUtils.call(() -> {
+      boolean rawConfig = false;
+      if (rawConfiguration != null) {
+        rawConfig = rawConfiguration;
+      }
+      AlluxioJobWorkerInfo result =
+          new AlluxioJobWorkerInfo()
+              .setConfiguration(getConfigurationInternal(rawConfig))
+              .setStartTimeMs(mJobWorker.getStartTimeMs())
+              .setUptimeMs(mJobWorker.getUptimeMs())
+              .setVersion(RuntimeConstants.VERSION);
+      return result;
+    });
+  }
 
-    private Map<String, String> getConfigurationInternal(boolean raw) {
-        Set<Map.Entry<String, String>> properties = Configuration.toMap().entrySet();
-        SortedMap<String, String> configuration = new TreeMap<>();
-        for (Map.Entry<String, String> entry : properties) {
-            String key = entry.getKey();
-            if (PropertyKey.isValid(key)) {
-                if (raw) {
-                    configuration.put(key, entry.getValue());
-                } else {
-                    configuration.put(key, Configuration.get(PropertyKey.fromString(key)));
-                }
-            }
+  private Map<String, String> getConfigurationInternal(boolean raw) {
+    Set<Map.Entry<String, String>> properties = Configuration.toMap().entrySet();
+    SortedMap<String, String> configuration = new TreeMap<>();
+    for (Map.Entry<String, String> entry : properties) {
+      String key = entry.getKey();
+      if (PropertyKey.isValid(key)) {
+        if (raw) {
+          configuration.put(key, entry.getValue());
+        } else {
+          configuration.put(key, Configuration.get(PropertyKey.fromString(key)));
         }
-        return configuration;
+      }
     }
+    return configuration;
+  }
 }
