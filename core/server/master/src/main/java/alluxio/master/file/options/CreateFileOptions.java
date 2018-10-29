f3c9561f4321cc8a9a869e6722880a4f29790a2f
Gokturk Gezer <gokturk@alluxio.com>
2018-10-29 16:48:00 -0700

    Fix default options for FSM CreateFile master option wrapper

--- core/server/master/src/main/java/alluxio/master/file/options/CreateFileOptions.java
+++ core/server/master/src/main/java/alluxio/master/file/options/CreateFileOptions.java
@@ -13,11 +13,13 @@ package alluxio.master.file.options;
 
 import alluxio.Configuration;
 import alluxio.PropertyKey;
+import alluxio.security.authorization.Mode;
 import alluxio.util.ModeUtils;
 
 import com.google.common.base.Objects;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import java.util.Collections;
 
 /**
  * Method options for creating a file.
@@ -25,8 +27,6 @@ import javax.annotation.concurrent.NotThreadSafe;
 @NotThreadSafe
 public final class CreateFileOptions
     extends alluxio.file.options.CreateFileOptions<CreateFileOptions> {
-  private long mBlockSizeBytes;
-  private boolean mCacheable;
 
   /**
    * @return the default {@link CreateFileOptions}
@@ -37,12 +37,21 @@ public final class CreateFileOptions
 
   private CreateFileOptions() {
     super();
+    mCommonOptions = CommonOptions.defaults();
+    mMountPoint = false;
+    mOwner = "";
+    mGroup = "";
+    mAcl = Collections.emptyList();
+    mOperationTimeMs = System.currentTimeMillis();
     mBlockSizeBytes = Configuration.getBytes(PropertyKey.USER_BLOCK_SIZE_BYTES_DEFAULT);
     mReplicationDurable = Configuration.getInt(PropertyKey.USER_FILE_REPLICATION_DURABLE);
     mReplicationMax = Configuration.getInt(PropertyKey.USER_FILE_REPLICATION_MAX);
     mReplicationMin = Configuration.getInt(PropertyKey.USER_FILE_REPLICATION_MIN);
-    mMode = ModeUtils.applyFileUMask(mMode);
+    mMode = ModeUtils.applyFileUMask(Mode.defaults());
     mCacheable = false;
+    mPersisted = false;
+    mRecursive = false;
+    mMetadataLoad = false;
   }
 
   /**
