803abe7e057805e35cfe9ddca0d57153f64b4ae9
usernamehcx <2364684794@qq.com>
2018-11-12 01:12:16 +0800

    replace anonymous type with lambda in alluxio.fuse.AlluxioFuseFileSystemTest

--- integration/fuse/src/test/java/alluxio/fuse/AlluxioFuseFileSystemTest.java
+++ integration/fuse/src/test/java/alluxio/fuse/AlluxioFuseFileSystemTest.java
@@ -218,16 +218,15 @@ public class AlluxioFuseFileSystemTest {
     setUpOpenMock(expectedPath);
 
     FileInStream fakeInStream = mock(FileInStream.class);
-    when(fakeInStream.read(any(byte[].class), anyInt(), anyInt())).then(new Answer<Integer>() {
-      @Override
-      public Integer answer(InvocationOnMock invocationOnMock) throws Throwable {
-        byte[] myDest = (byte[]) invocationOnMock.getArguments()[0];
-        for (byte i = 0; i < 4; i++) {
-          myDest[i] = i;
-        }
-        return 4;
+
+    when(fakeInStream.read(any(byte[].class), anyInt(), anyInt())).then((Answer<Integer>) invocationOnMock -> {
+      byte[] myDest = (byte[]) invocationOnMock.getArguments()[0];
+      for (byte i = 0; i < 4; i++) {
+        myDest[i] = i;
       }
+      return 4;
     });
+    
     when(mFileSystem.openFile(expectedPath)).thenReturn(fakeInStream);
     mFileInfo.flags.set(O_RDONLY.intValue());
 
