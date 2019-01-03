bcfeb2bb38c504c70cce6a5545bcbd0fbeaebf29
LuQQiu <luqiujob@gmail.com>
2019-01-02 17:05:49 -0800

    [ALLUXIO-3392] embedded journal uri (#8229)
    * add multi master uri
    
    * add multi master authority
    
    * improve import

--- core/common/src/test/java/alluxio/uri/AuthorityTest.java
+++ core/common/src/test/java/alluxio/uri/AuthorityTest.java
@@ -12,6 +12,7 @@
 package alluxio.uri;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -62,6 +63,30 @@ public class AuthorityTest {
     assertEquals("localhost:19998", authority.toString());
     assertEquals("localhost", authority.getHost());
     assertEquals(19998, authority.getPort());
+  }
+
+  @Test
+  public void multiMasterAuthorityTest() {
+    MultiMasterAuthority authority =
+        (MultiMasterAuthority) Authority.fromString("host1:19998,host2:19998,host3:19998");
+    assertEquals("host1:19998,host2:19998,host3:19998", authority.toString());
+    assertEquals("host1:19998,host2:19998,host3:19998", authority.getMasterAddresses());
+
+    authority = (MultiMasterAuthority) Authority
+         .fromString("127.0.0.1:213,127.0.0.2:532423,127.0.0.3:3213");
+    assertEquals("127.0.0.1:213,127.0.0.2:532423,127.0.0.3:3213", authority.toString());
+    assertEquals("127.0.0.1:213,127.0.0.2:532423,127.0.0.3:3213", authority.getMasterAddresses());
+
+    authority = (MultiMasterAuthority) Authority.fromString("host1:19998;host2:19998;host3:19998");
+    assertEquals("host1:19998;host2:19998;host3:19998", authority.toString());
+    assertEquals("host1:19998,host2:19998,host3:19998", authority.getMasterAddresses());
+
+    assertFalse(Authority.fromString("localhost:19998")
+        instanceof MultiMasterAuthority);
+    assertFalse(Authority.fromString("localhost:abc,127.0.0.1:dsa")
+        instanceof MultiMasterAuthority);
+    assertFalse(Authority.fromString(",,,") instanceof MultiMasterAuthority);
+    assertFalse(Authority.fromString(";;;") instanceof MultiMasterAuthority);
   }
 
   @Test
