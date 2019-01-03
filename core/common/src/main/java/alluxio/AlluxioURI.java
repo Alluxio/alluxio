b9953324d8cbb4f14baa70c135ca2735f34d6a3b
David Zhu <david@alluxio.com>
2019-01-03 11:43:12 -0800

    remove AlluxioURI#toString

--- core/common/src/main/java/alluxio/AlluxioURI.java
+++ core/common/src/main/java/alluxio/AlluxioURI.java
@@ -468,30 +468,6 @@ public final class AlluxioURI implements Comparable<AlluxioURI>, Serializable {
    */
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    if (mUri.getScheme() != null) {
-      sb.append(mUri.getScheme());
-      sb.append("://");
-    }
-    if (hasAuthority()) {
-      if (mUri.getScheme() == null) {
-        sb.append("//");
-      }
-      sb.append(mUri.getAuthority().toString());
-    }
-    if (mUri.getPath() != null) {
-      String path = mUri.getPath();
-      if (path.indexOf('/') == 0 && hasWindowsDrive(path, true) && // has windows drive
-          mUri.getScheme() == null && // but no scheme
-          mUri.getAuthority() == null) { // or authority
-        path = path.substring(1); // remove slash before drive
-      }
-      sb.append(path);
-    }
-    if (mUri.getQuery() != null) {
-      sb.append("?");
-      sb.append(mUri.getQuery());
-    }
-    return sb.toString();
+    return "";
   }
 }
