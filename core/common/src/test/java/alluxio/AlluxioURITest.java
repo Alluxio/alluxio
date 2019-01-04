d65451f549d33c21f4c9a70d6aeec35e75cca67b
Nakkul Sreenivas <nakkul@alluxio.com>
2019-01-04 10:18:45 -0800

    This feature is to allow multiple different HDFS versions to be mounted as a UFS to Alluxio. The core points are below.
    - alluxio.underfs.hdfs.version is now removed in favor of alluxio.underfs.version
    - Go build scripts now have the option --ufs-modules to allow building of an Alluxio release with a list of supported UFS versions

--- core/common/src/test/java/alluxio/AlluxioURITest.java
+++ core/common/src/test/java/alluxio/AlluxioURITest.java
@@ -152,7 +152,7 @@ public class AlluxioURITest {
     AlluxioURI uri =
         new AlluxioURI("alluxio://host1:1323;host2:54325;host3:64354/xy z/a b c");
     assertTrue(uri.hasAuthority());
-    assertEquals("host1:1323;host2:54325;host3:64354", uri.getAuthority().toString());
+    assertEquals("host1:1323,host2:54325,host3:64354", uri.getAuthority().toString());
     assertTrue(uri.getAuthority() instanceof MultiMasterAuthority);
     MultiMasterAuthority authority = (MultiMasterAuthority) uri.getAuthority();
     assertEquals("host1:1323,host2:54325,host3:64354", authority.getMasterAddresses());
@@ -163,7 +163,6 @@ public class AlluxioURITest {
     AlluxioURI uri =
         new AlluxioURI("alluxio://host1:526+host2:54325+host3:624/xy z/a b c");
     assertTrue(uri.hasAuthority());
-    assertEquals("host1:526+host2:54325+host3:624", uri.getAuthority().toString());
     assertTrue(uri.getAuthority() instanceof MultiMasterAuthority);
     MultiMasterAuthority authority = (MultiMasterAuthority) uri.getAuthority();
     assertEquals("host1:526,host2:54325,host3:624", authority.getMasterAddresses());
---
+++ dev/jenkins/run_docker.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+# (the "License"). You may not use this work except in compliance with the License, which is
+# available at www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied, as more fully set forth in the License.
+#
+# See the NOTICE file distributed with this work for information regarding copyright ownership.
+#
+
+set -e
+
+function main {
+  if [ -z "${ALLUXIO_DOCKER_ID}" ]
+  then
+    ALLUXIO_DOCKER_ID="$(id -u)"
+  fi
+  if [ -z "${ALLUXIO_DOCKER_M2}" ]
+  then
+    ALLUXIO_DOCKER_M2="${HOME}/.m2"
+  fi
+  if [ -z "${ALLUXIO_DOCKER_IMAGE}" ]
+  then
+    ALLUXIO_DOCKER_IMAGE="alluxio/alluxio-maven:0.0.3"
+  fi
+
+  local run_args="--rm"
+
+  if [ -z ${ALLUXIO_DOCKER_NO_TTY} ]
+  then
+    run_args+=" -it"
+  fi
+
+  local home="/home/jenkins"
+  # Needed to run fuse tests:
+  run_args+=" --cap-add SYS_ADMIN"
+  run_args+=" --device /dev/fuse"
+  run_args+=" --security-opt apparmor:unconfined"
+
+  # Run as the host jenkins user so that files written to .m2 are written as jenkins.
+  # Use group 0 to get certain elevated permissions.
+  run_args+=" --user ${ALLUXIO_DOCKER_ID}:0"
+
+  # Mount the local directory inside the docker container, and set it as the working directory
+  run_args+=" -v $(pwd):/usr/src/alluxio"
+  run_args+=" -w /usr/src/alluxio"
+
+  # Since we're running as a user unknown to the Docker container, we need to explicitly
+  # configure anything that's relative to ${HOME}.
+  run_args+=" -e HOME=${home}"
+  run_args+=" -v ${ALLUXIO_DOCKER_M2}:${home}/.m2"
+  run_args+=" -e npm_config_cache=${home}/.npm"
+  run_args+=" -e MAVEN_CONFIG=${home}/.m2"
+
+  run_args+=" -e ALLUXIO_USE_FIXED_TEST_PORTS=true"
+
+  # Use this as an entrypoint instead of image argument so that it can be interrupted by Ctrl-C
+  run_args+=" --entrypoint=dev/jenkins/build.sh"
+
+  docker run ${run_args} ${ALLUXIO_DOCKER_IMAGE} $@
+}
+
+main "$@"
