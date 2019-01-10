83b3b5669b6133053fe472d0f8efa0e49ef57bda
Göktürk Gezer <gokturk.gezer@gmail.com>
2019-01-09 21:24:23 -0800

    Create files recursively in examples (#8249)

--- examples/src/main/java/alluxio/examples/MultiMount.java
+++ examples/src/main/java/alluxio/examples/MultiMount.java
@@ -101,8 +101,8 @@ public final class MultiMount {
 
       // Open the output stream, setting the write type to make sure result is persisted.
       System.out.print("opening " + outputPath + " ... ");
-      CreateFilePOptions options =
-          CreateFilePOptions.newBuilder().setWriteType(WritePType.WRITE_CACHE_THROUGH).build();
+      CreateFilePOptions options = CreateFilePOptions.newBuilder()
+          .setWriteType(WritePType.WRITE_CACHE_THROUGH).setRecursive(true).build();
       FileOutStream os = fileSystem.createFile(outputPath, options);
       System.out.println("done");
 
