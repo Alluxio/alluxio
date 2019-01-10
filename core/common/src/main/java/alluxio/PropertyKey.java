40660886f98f9ef4f796827d7c1a2148d2a7284c
Nakkul Sreenivas <nakkul@alluxio.com>
2019-01-10 15:59:12 -0800

    Checkstyle fix for comments

--- core/common/src/main/java/alluxio/PropertyKey.java
+++ core/common/src/main/java/alluxio/PropertyKey.java
@@ -3302,8 +3302,8 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
           .setScope(Scope.NONE)
           .build();
-  //Assumes that HDFS is the UFS and version is 2.2
-  //TODO(ns) Fix default value to handle other UFS types
+  // Assumes that HDFS is the UFS and version is 2.2
+  // TODO(ns) Fix default value to handle other UFS types
   public static final PropertyKey UNDERFS_VERSION =
       new Builder(Name.UNDERFS_VERSION)
           .setDefaultValue("2.2")
