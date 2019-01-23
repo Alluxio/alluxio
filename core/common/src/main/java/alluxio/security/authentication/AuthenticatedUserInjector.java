bbdb5c7cd3e571b745f69e55676b39cdb5176307
Gokturk Gezer <gokturk@alluxio.com>
2019-01-22 18:26:36 -0800

    Authenticate server calls during the intercept time for bi-di calls.

--- core/common/src/main/java/alluxio/security/authentication/AuthenticatedUserInjector.java
+++ core/common/src/main/java/alluxio/security/authentication/AuthenticatedUserInjector.java
@@ -1,7 +1,7 @@
 /*
- * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
- * (the "License"). You may not use this work except in compliance with the License, which is
- * available at www.apache.org/licenses/LICENSE-2.0
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0 (the
+ * "License"). You may not use this work except in compliance with the License, which is available
+ * at www.apache.org/licenses/LICENSE-2.0
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied, as more fully set forth in the License.
@@ -19,22 +19,26 @@ import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import io.grpc.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.UUID;
 
 /**
- * Server side interceptor for setting authenticated user in {@link AuthenticatedClientUser}.
- * This interceptor requires {@link ChannelIdInjector} to have injected the channel id from which
- * the particular RPC is being made.
+ * Server side interceptor for setting authenticated user in {@link AuthenticatedClientUser}. This
+ * interceptor requires {@link ChannelIdInjector} to have injected the channel id from which the
+ * particular RPC is being made.
  */
 @ThreadSafe
 public final class AuthenticatedUserInjector implements ServerInterceptor {
 
+  private static final Logger LOG = LoggerFactory.getLogger(AuthenticatedUserInjector.class);
   private final AuthenticationServer mAuthenticationServer;
 
   /**
    * Creates {@link AuthenticationServer} with given authentication server.
+   * 
    * @param authenticationServer the authentication server
    */
   public AuthenticatedUserInjector(AuthenticationServer authenticationServer) {
@@ -44,39 +48,54 @@ public final class AuthenticatedUserInjector implements ServerInterceptor {
   @Override
   public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call,
       Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+    /**
+     * For streaming calls, below will make sure authenticated user is injected prior to creating
+     * the stream.
+     */
+    if (!authenticateCall(call, headers)) {
+      return next.startCall(call, headers);
+    }
+
+    /**
+     * For non-streaming calls to server, below listener will be invoked in the same thread that is
+     * serving the call.
+     */
     return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(
         next.startCall(call, headers)) {
-      /**
-       * onHalfClose is called on the same thread that calls the service handler.
-       */
       @Override
       public void onHalfClose() {
-        // Try to fetch channel Id from the metadata.
-        UUID channelId = headers.get(ChannelIdInjector.S_CLIENT_ID_KEY);
-        boolean callClosed = false;
-        if (channelId != null) {
-          try {
-            // Fetch authenticated username for this channel and set it.
-            String userName = mAuthenticationServer.getUserNameForChannel(channelId);
-            if (userName != null) {
-              AuthenticatedClientUser.set(userName);
-            } else {
-              AuthenticatedClientUser.remove();
-            }
-          } catch (UnauthenticatedException e) {
-            call.close(Status.UNAUTHENTICATED, headers);
-            callClosed = true;
-          }
-        } else {
-          call.close(Status.UNAUTHENTICATED, headers);
-          callClosed = true;
-        }
-        // Move the call up to the handler chain,
-        // if we have not closed the call already due to authentication error.
-        if (!callClosed) {
+        if (authenticateCall(call, headers)) {
           super.onHalfClose();
         }
       }
     };
+  }
+
+  private <ReqT, RespT> boolean authenticateCall(ServerCall<ReqT, RespT> call, Metadata headers) {
+    // Try to fetch channel Id from the metadata.
+    UUID channelId = headers.get(ChannelIdInjector.S_CLIENT_ID_KEY);
+    boolean callClosed = false;
+    if (channelId != null) {
+      try {
+        // Fetch authenticated username for this channel and set it.
+        String userName = mAuthenticationServer.getUserNameForChannel(channelId);
+        if (userName != null) {
+          AuthenticatedClientUser.set(userName);
+        } else {
+          AuthenticatedClientUser.remove();
+        }
+      } catch (UnauthenticatedException e) {
+        LOG.debug("Channel:{} is not authenticated for call:{}", channelId.toString(),
+            call.getMethodDescriptor().getFullMethodName());
+        call.close(Status.UNAUTHENTICATED, headers);
+        callClosed = true;
+      }
+    } else {
+      LOG.debug("Channel Id is missing for call:{}.",
+          call.getMethodDescriptor().getFullMethodName());
+      call.close(Status.UNAUTHENTICATED, headers);
+      callClosed = true;
+    }
+    return !callClosed;
   }
 }
