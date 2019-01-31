07283c7efdcdf224075ece6579c65a0ca2f94655
Zac Blanco <zac@alluxio.com>
2019-01-30 20:41:46 -0800

    [SMALLFIX] Create clients less often in REST handlers (#8329)
    * Create clients less often in REST handlers
    
    The web server rest handler instances are created every time a request
    is propagated. We shouldn't be creating new client objects within the
    service handlers.
    
    Instead we initialize a client in the servlet's `init` method which will
    only be called once. Then subsequent calls to the REST service handler
    wilk only utilize the single client.
    
    * Update resource key names
    
    It's good to be consistent.

--- core/server/master/src/main/java/alluxio/web/MasterWebServer.java
+++ core/server/master/src/main/java/alluxio/web/MasterWebServer.java
@@ -12,6 +12,7 @@
 package alluxio.web;
 
 import alluxio.Constants;
+import alluxio.client.file.FileSystem;
 import alluxio.conf.PropertyKey;
 import alluxio.conf.ServerConfiguration;
 import alluxio.master.AlluxioMasterProcess;
@@ -42,6 +43,8 @@ public final class MasterWebServer extends WebServer {
   private static final Logger LOG = LoggerFactory.getLogger(MasterWebServer.class);
 
   public static final String ALLUXIO_MASTER_SERVLET_RESOURCE_KEY = "Alluxio Master";
+  public static final String ALLUXIO_FILESYSTEM_CLIENT_RESOURCE_KEY =
+      "Alluxio Master FileSystem client";
 
   /**
    * Creates a new instance of {@link MasterWebServer}.
@@ -67,6 +70,8 @@ public final class MasterWebServer extends WebServer {
       public void init() throws ServletException {
         super.init();
         getServletContext().setAttribute(ALLUXIO_MASTER_SERVLET_RESOURCE_KEY, masterProcess);
+        getServletContext().setAttribute(ALLUXIO_FILESYSTEM_CLIENT_RESOURCE_KEY,
+            FileSystem.Factory.get(ServerConfiguration.global()));
       }
     };
 
