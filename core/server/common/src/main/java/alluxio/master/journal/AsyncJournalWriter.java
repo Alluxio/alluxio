b95a477243770e800638f3674a9a2d9ed2c1bfa0
Göktürk Gezer <gokturk.gezer@gmail.com>
2019-02-08 10:05:47 -0800

    Improve journal batching under heavy load (#8348)
    * Improve journal batching under heavy load
    
    * Improving flush guarantees.
    
    * In-line removal of tickets during iteration
    
    * PR feedback
    
    * small fixes
    
    * Use linked list for tickets
    
    * Fix checkstyle
    
    * Remove redundant warning supression
    
    * Revert ReentrantLock

--- core/server/common/src/main/java/alluxio/master/journal/AsyncJournalWriter.java
+++ core/server/common/src/main/java/alluxio/master/journal/AsyncJournalWriter.java
@@ -11,19 +11,30 @@
 
 package alluxio.master.journal;
 
-import alluxio.conf.ServerConfiguration;
 import alluxio.conf.PropertyKey;
+import alluxio.conf.ServerConfiguration;
 import alluxio.exception.JournalClosedException;
+import alluxio.exception.status.AlluxioStatusException;
+import alluxio.exception.status.Status;
 import alluxio.proto.journal.Journal.JournalEntry;
 import alluxio.resource.LockResource;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.SettableFuture;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
+import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
@@ -31,6 +42,35 @@ import javax.annotation.concurrent.ThreadSafe;
  */
 @ThreadSafe
 public final class AsyncJournalWriter {
+  /**
+   * Used to manage and keep track of pending callers of ::flush.
+   */
+  private class FlushTicket {
+    private final long mTargetCounter;
+    private SettableFuture<Void> mIsCompleted;
+
+    public FlushTicket(long targetCounter) {
+      mTargetCounter = targetCounter;
+      mIsCompleted = SettableFuture.create();
+    }
+
+    public long getTargetCounter() {
+      return mTargetCounter;
+    }
+
+    public void setCompleted() {
+      mIsCompleted.set(null);
+    }
+
+    public void setError(Throwable exc) {
+      mIsCompleted.setException(exc);
+    }
+
+    public void waitCompleted() throws InterruptedException, ExecutionException {
+      mIsCompleted.get();
+    }
+  }
+
   private final JournalWriter mJournalWriter;
   private final ConcurrentLinkedQueue<JournalEntry> mQueue;
   /** Represents the count of entries added to the journal queue. */
@@ -39,17 +79,39 @@ public final class AsyncJournalWriter {
   private final AtomicLong mFlushCounter;
   /**
    * Represents the count of entries written to the journal writer.
+   * This counter is only accessed by the dedicated journal thread.
    * Invariant: {@code mWriteCounter >= mFlushCounter}
    */
-  private final AtomicLong mWriteCounter;
+  private Long mWriteCounter;
   /** Maximum number of nanoseconds for a batch flush. */
   private final long mFlushBatchTimeNs;
 
   /**
-   * Use a {@link ReentrantLock} to guard the journal writing. Using the fairness policy seems to
-   * result in better throughput.
+   * List of flush tickets submitted by ::flush() method.
    */
-  private final ReentrantLock mFlushLock = new ReentrantLock(true);
+  @GuardedBy("mTicketLock")
+  private final List<FlushTicket> mTicketList = new LinkedList<>();
+
+  /**
+   * Used to guard access to ticket cache.
+   */
+  private final ReentrantLock mTicketLock = new ReentrantLock(true);
+
+  /**
+   * Dedicated thread for writing and flushing entries in journal queue.
+   * It goes over the {@code mTicketList} after every flush session and releases waiters.
+   */
+  private Thread mFlushThread = new Thread(this::doFlush, "AsyncJournalWriterThread");
+
+  /**
+   * Used to give permits to flush thread to start processing immediately.
+   */
+  private final Semaphore mFlushSemaphore = new Semaphore(0, true);
+
+  /**
+   * Control flag that is used to instruct flush thread to exit.
+   */
+  private volatile boolean mStopFlushing = false;
 
   /**
    * Creates a {@link AsyncJournalWriter}.
@@ -61,10 +123,11 @@ public final class AsyncJournalWriter {
     mQueue = new ConcurrentLinkedQueue<>();
     mCounter = new AtomicLong(0);
     mFlushCounter = new AtomicLong(0);
-    mWriteCounter = new AtomicLong(0);
-    // convert milliseconds to nanoseconds.
-    mFlushBatchTimeNs =
-        1000000L * ServerConfiguration.getMs(PropertyKey.MASTER_JOURNAL_FLUSH_BATCH_TIME_MS);
+    mWriteCounter = 0L;
+    mFlushBatchTimeNs = TimeUnit.NANOSECONDS.convert(
+        ServerConfiguration.getMs(PropertyKey.MASTER_JOURNAL_FLUSH_BATCH_TIME_MS),
+        TimeUnit.MILLISECONDS);
+    mFlushThread.start();
   }
 
   /**
@@ -99,49 +162,176 @@ public final class AsyncJournalWriter {
   }
 
   /**
-   * Flushes and waits until the specified counter is flushed to the journal. If the specified
-   * counter is already flushed, this is essentially a no-op.
-   *
-   * @param targetCounter the counter to flush
+   * Closes the async writer.
+   * PS: It's not guaranteed for pending entries to be flushed.
+   *     Use ::flush() for guaranteeing the entries have been flushed.
    */
-  @SuppressWarnings("Duplicates")
-  public void flush(final long targetCounter) throws IOException, JournalClosedException {
-    if (targetCounter <= mFlushCounter.get()) {
+  public void close() {
+    stop();
+  }
+
+  @VisibleForTesting
+  protected void stop() {
+    // Set termination flag.
+    mStopFlushing = true;
+    // Give a permit for flush thread to run, in case it was blocked on permit.
+    mFlushSemaphore.release();
+
+    try {
+      mFlushThread.join();
+    } catch (InterruptedException ie) {
+      Thread.currentThread().interrupt();
       return;
+    } finally {
+      mFlushThread = null;
+      // Try to reacquire the permit.
+      mFlushSemaphore.tryAcquire();
     }
-    // Using reentrant lock, since it seems to result in higher throughput than using 'synchronized'
-    try (LockResource lr = new LockResource(mFlushLock)) {
-      long startTime = System.nanoTime();
-      long flushCounter = mFlushCounter.get();
-      if (targetCounter <= flushCounter) {
-        // The specified counter is already flushed, so just return.
-        return;
+  }
+
+  @VisibleForTesting
+  protected void start() {
+    if (mFlushThread != null) {
+      close();
+    }
+    // Create a new thread.
+    mFlushThread = new Thread(this::doFlush);
+    // Reset termination flag before starting the new thread.
+    mStopFlushing = false;
+    mFlushThread.start();
+  }
+
+  /**
+   * A dedicated thread that goes over outstanding queue items and writes/flushes them. Other
+   * threads can track progress by submitting tickets via ::flush() call.
+   */
+  private void doFlush() {
+    // Runs the loop until ::stop() is called.
+    while (!mStopFlushing) {
+
+      /**
+       * Stand still unless;
+       * - queue has items
+       * - permit is given by:
+       *   - clients
+       *   -::stop()
+       */
+      while (mQueue.isEmpty() && !mStopFlushing) {
+        try {
+          // Wait for permit up to batch timeout.
+          // PS: We don't wait for permit indefinitely in order to process
+          // queued entries proactively.
+          if (mFlushSemaphore.tryAcquire(mFlushBatchTimeNs, TimeUnit.NANOSECONDS)) {
+            break;
+          }
+        } catch (InterruptedException ie) {
+          break;
+        }
       }
-      long writeCounter = mWriteCounter.get();
-      while (targetCounter > writeCounter) {
-        for (;;) {
+
+      try {
+        long startTime = System.nanoTime();
+
+        // Write pending entries to journal.
+        while (!mQueue.isEmpty()) {
           // Get, but do not remove, the head entry.
           JournalEntry entry = mQueue.peek();
           if (entry == null) {
-            // No more entries in the queue. Break out of the infinite for-loop.
+            // No more entries in the queue. Break write session.
             break;
           }
           mJournalWriter.write(entry);
           // Remove the head entry, after the entry was successfully written.
           mQueue.poll();
-          writeCounter = mWriteCounter.incrementAndGet();
+          mWriteCounter++;
 
-          if (writeCounter >= targetCounter) {
-            if ((System.nanoTime() - startTime) >= mFlushBatchTimeNs) {
-              // This thread has been writing to the journal for enough time. Break out of the
-              // infinite for-loop.
-              break;
+          if (((System.nanoTime() - startTime) >= mFlushBatchTimeNs) && !mStopFlushing) {
+            // This thread has been writing to the journal for enough time. Break out of the
+            // infinite while-loop.
+            break;
+          }
+        }
+
+        // Either written new entries or previous flush had been failed.
+        if (mFlushCounter.get() < mWriteCounter) {
+          mJournalWriter.flush();
+          mFlushCounter.set(mWriteCounter);
+        }
+
+        // Notify tickets that have been served to wake up.
+        try (LockResource lr = new LockResource(mTicketLock)) {
+          ListIterator<FlushTicket> ticketIterator = mTicketList.listIterator();
+          while (ticketIterator.hasNext()) {
+            FlushTicket ticket = ticketIterator.next();
+            if (ticket.getTargetCounter() <= mFlushCounter.get()) {
+              ticket.setCompleted();
+              ticketIterator.remove();
+            }
+          }
+        }
+      } catch (IOException | JournalClosedException exc) {
+        // Release only tickets that have been flushed. Fail the rest.
+        try (LockResource lr = new LockResource(mTicketLock)) {
+          ListIterator<FlushTicket> ticketIterator = mTicketList.listIterator();
+          while (ticketIterator.hasNext()) {
+            FlushTicket ticket = ticketIterator.next();
+            ticketIterator.remove();
+            if (ticket.getTargetCounter() <= mFlushCounter.get()) {
+              ticket.setCompleted();
+            } else {
+              ticket.setError(exc);
             }
           }
         }
       }
-      mJournalWriter.flush();
-      mFlushCounter.set(writeCounter);
+    }
+  }
+
+  /**
+   * Submits a ticket to flush thread and waits until ticket is served.
+   *
+   * If the specified counter is already flushed, this is essentially a no-op.
+   *
+   * @param targetCounter the counter to flush
+   */
+  public void flush(final long targetCounter) throws IOException, JournalClosedException {
+    // Return if flushed.
+    if (targetCounter <= mFlushCounter.get()) {
+      return;
+    }
+
+    // Submit the ticket for flush thread to process.
+    FlushTicket ticket = new FlushTicket(targetCounter);
+    try (LockResource lr = new LockResource(mTicketLock)) {
+      mTicketList.add(ticket);
+    }
+
+    try {
+      // Give a permit for flush thread to run.
+      mFlushSemaphore.release();
+
+      // Wait on the ticket until completed.
+      ticket.waitCompleted();
+    } catch (InterruptedException ie) {
+      // Interpret interruption as cancellation.
+      throw new AlluxioStatusException(Status.CANCELED, ie);
+    } catch (ExecutionException ee) {
+      // Filter, journal specific exception codes.
+      Throwable cause = ee.getCause();
+      if (cause != null && cause instanceof IOException) {
+        throw (IOException) cause;
+      }
+      if (cause != null && cause instanceof JournalClosedException) {
+        throw (JournalClosedException) cause;
+      }
+      // Not expected. throw internal error.
+      throw new AlluxioStatusException(Status.INTERNAL, ee);
+    } finally {
+      /**
+       * Client can only try to reacquire the permit it has given
+       * because the permit may or may not have been used by the flush thread.
+       */
+      mFlushSemaphore.tryAcquire();
     }
   }
 }
