eada18788abb2ca8080e548e8bd705b696541675
Andrew Audibert <andrew@alluxio.com>
2019-02-15 14:27:35 -0800

    Avoid scanning tree for TO_BE_PERSISTED files during startup (#8403)

--- core/server/master/src/main/java/alluxio/master/file/meta/InodeTree.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/InodeTree.java
@@ -13,7 +13,6 @@ package alluxio.master.file.meta;
 
 import alluxio.AlluxioURI;
 import alluxio.collections.Pair;
-import alluxio.conf.ServerConfiguration;
 import alluxio.concurrent.LockMode;
 import alluxio.conf.ServerConfiguration;
 import alluxio.exception.BlockInfoException;
@@ -958,7 +957,15 @@ public class InodeTree implements JournalEntryIterable, JournalEntryReplayable {
    * @return the set of file ids whose replication max is not infinity
    */
   public Set<Long> getReplicationLimitedFileIds() {
-    return java.util.Collections.unmodifiableSet(mState.getReplicationLimitedFileIds());
+    return mState.getReplicationLimitedFileIds();
+  }
+
+  /**
+   * @return an unmodifiable view of the files with persistence state
+   *         {@link PersistenceState#TO_BE_PERSISTED}
+   */
+  public Set<Long> getToBePersistedIds() {
+    return mState.getToBePersistedIds();
   }
 
   /**
