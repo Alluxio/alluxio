edc51ad80600bdd3a101ac3405b3dbbab53d10fa
Göktürk Gezer <gokturk.gezer@gmail.com>
2019-02-21 15:28:44 -0800

    Define a default value for gRPC client-side message size (100MB) (#8439)
    * Define a default value for gRPC client-side message size
    
    * Reuse existing propkey for max message size

--- core/common/src/main/java/alluxio/grpc/GrpcChannelBuilder.java
+++ core/common/src/main/java/alluxio/grpc/GrpcChannelBuilder.java
@@ -49,11 +49,14 @@ public final class GrpcChannelBuilder {
   protected AlluxioConfiguration mConfiguration;
 
   private GrpcChannelBuilder(SocketAddress address, AlluxioConfiguration conf) {
+    mConfiguration = conf;
     mChannelKey = GrpcManagedChannelPool.ChannelKey.create(conf);
+    // Set default overrides for the channel.
     mChannelKey.setAddress(address).usePlaintext();
+    mChannelKey.setMaxInboundMessageSize(
+        (int) mConfiguration.getBytes(PropertyKey.USER_NETWORK_MAX_INBOUND_MESSAGE_SIZE));
     mUseSubject = true;
     mAuthenticateChannel = true;
-    mConfiguration = conf;
   }
 
   /**
