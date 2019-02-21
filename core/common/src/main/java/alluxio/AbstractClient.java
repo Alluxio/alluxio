82e86d68b6d1f6df762132fdb39cf16f482a90dd
David Zhu <david@alluxio.com>
2019-02-21 12:44:35 -0800

    change client thread pool

--- core/common/src/main/java/alluxio/AbstractClient.java
+++ core/common/src/main/java/alluxio/AbstractClient.java
@@ -39,6 +39,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.function.Supplier;
 
@@ -216,7 +217,7 @@ public abstract class AbstractClient implements Client {
         mChannel = GrpcChannelBuilder
             .newBuilder(mAddress, mContext.getConf())
             .setSubject(mContext.getSubject())
-            .setExecutor(ForkJoinPool.commonPool())
+            .setExecutor(Executors.newFixedThreadPool(10))
             .build();
         // Create stub for version service on host
         mVersionService = ServiceVersionClientServiceGrpc.newBlockingStub(mChannel);
