a97b4a3d5cec84ffe80d8918a3785462716c1b5d
Nakkul Sreenivas <nakkul@alluxio.com>
2019-02-20 17:46:42 -0800

    Modified generate-tarball for new convention. Fixed logic for supportsPath

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystemFactory.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystemFactory.java
@@ -79,8 +79,8 @@ public final class HdfsUnderFileSystemFactory implements UnderFileSystemFactory 
       // are supported this is not an option and we have to continue to use this method.
       for (final String prefix : alluxioConf.getList(PropertyKey.UNDERFS_HDFS_PREFIXES, ",")) {
         if (path.startsWith(prefix)) {
-          if (conf != null && conf.get(PropertyKey.UNDERFS_VERSION) != getVersion()) {
-            continue;
+          if (conf != null && conf.get(PropertyKey.UNDERFS_VERSION).equals(getVersion())) {
+            return true;
           }
           if (conf != null && HdfsVersion
               .find(conf.get(PropertyKey.UNDERFS_VERSION)) != HdfsVersion.find(getVersion())) {
