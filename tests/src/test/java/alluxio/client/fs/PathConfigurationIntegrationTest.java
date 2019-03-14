95e44756d6831693ad83953b445de5e422eab44f
Cheng Chang <myairia@gmail.com>
2019-03-14 16:13:41 +0800

    Update integration test for path level configuration.

--- tests/src/test/java/alluxio/client/fs/PathConfigurationIntegrationTest.java
+++ tests/src/test/java/alluxio/client/fs/PathConfigurationIntegrationTest.java
@@ -35,19 +35,27 @@ public class PathConfigurationIntegrationTest {
   private static final byte[] TEST_BYTES = "TestBytes".getBytes();
   private static final int USER_QUOTA_UNIT_BYTES = 1000;
   private static final String REMOTE_DIR = "/remote/";
+  private static final String REMOTE_UNCACHED_FILE = "/remote/0";
   private static final String LOCAL_DIR = "/local/";
+  private static final String LOCAL_CACHED_FILE = "/local/0";
 
   @Rule
   public LocalAlluxioClusterResource mLocalAlluxioClusterResource =
       new LocalAlluxioClusterResource.Builder()
           .setProperty(PropertyKey.USER_FILE_BUFFER_BYTES, USER_QUOTA_UNIT_BYTES)
-          .setProperty(PropertyKey.PATHS, 2)
+          .setProperty(PropertyKey.PATHS, 4)
           .setProperty(PropertyKey.Template.PATH_INDEX.format(0), REMOTE_DIR)
           .setProperty(PropertyKey.Template.PATH_PROPERTY.format(0,
-              PropertyKey.USER_FILE_READ_TYPE_DEFAULT), ReadType.CACHE_PROMOTE.toString())
-          .setProperty(PropertyKey.Template.PATH_INDEX.format(1), LOCAL_DIR)
+              PropertyKey.USER_FILE_READ_TYPE_DEFAULT), ReadType.CACHE.toString())
+          .setProperty(PropertyKey.Template.PATH_INDEX.format(1), REMOTE_UNCACHED_FILE)
           .setProperty(PropertyKey.Template.PATH_PROPERTY.format(1,
               PropertyKey.USER_FILE_READ_TYPE_DEFAULT), ReadType.NO_CACHE.toString())
+          .setProperty(PropertyKey.Template.PATH_INDEX.format(2), LOCAL_DIR)
+          .setProperty(PropertyKey.Template.PATH_PROPERTY.format(2,
+              PropertyKey.USER_FILE_READ_TYPE_DEFAULT), ReadType.NO_CACHE.toString())
+          .setProperty(PropertyKey.Template.PATH_INDEX.format(3), LOCAL_CACHED_FILE)
+          .setProperty(PropertyKey.Template.PATH_PROPERTY.format(3,
+              PropertyKey.USER_FILE_READ_TYPE_DEFAULT), ReadType.CACHE.toString())
           .build();
   private FileSystem mFileSystem = null;
   private CreateFilePOptions mWriteThrough;
@@ -73,22 +81,23 @@ public class PathConfigurationIntegrationTest {
       }
     }
 
-    for (int i = 0; i < n; i++) {
-      AlluxioURI uri = new AlluxioURI(REMOTE_DIR + i);
-      Assert.assertEquals(0, mFileSystem.getStatus(uri).getInMemoryPercentage());
-      try (FileInStream is = mFileSystem.openFile(uri)) {
-        IOUtils.copy(is, ByteStreams.nullOutputStream());
-      }
-      Assert.assertEquals(100, mFileSystem.getStatus(uri).getInMemoryPercentage());
+    checkCacheStatus(REMOTE_UNCACHED_FILE, false);
+    for (int i = 1; i < n; i++) {
+      checkCacheStatus(REMOTE_DIR + i, true);
     }
 
-    for (int i = 0; i < n; i++) {
-      AlluxioURI uri = new AlluxioURI(LOCAL_DIR + i);
-      Assert.assertEquals(0, mFileSystem.getStatus(uri).getInMemoryPercentage());
-      try (FileInStream is = mFileSystem.openFile(uri)) {
-        IOUtils.copy(is, ByteStreams.nullOutputStream());
-      }
-      Assert.assertEquals(0, mFileSystem.getStatus(uri).getInMemoryPercentage());
+    checkCacheStatus(LOCAL_CACHED_FILE, true);
+    for (int i = 1; i < n; i++) {
+      checkCacheStatus(LOCAL_DIR + i, false);
     }
+  }
+
+  private void checkCacheStatus(String path, boolean shouldCache) throws Exception {
+    AlluxioURI uri = new AlluxioURI(path);
+    Assert.assertEquals(0, mFileSystem.getStatus(uri).getInMemoryPercentage());
+    try (FileInStream is = mFileSystem.openFile(uri)) {
+      IOUtils.copy(is, ByteStreams.nullOutputStream());
+    }
+    Assert.assertEquals(shouldCache ? 100 : 0, mFileSystem.getStatus(uri).getInMemoryPercentage());
   }
 }
