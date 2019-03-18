545404f17ced5c4350b30f60044becdceac04a55
Zac Blanco <zac@alluxio.com>
2019-03-18 13:52:18 -0700

    Updates to parsing for bash lines
    The logic was a bit more complex due to some corner cases with
    jekyll's rouge highlighter. We use some regex now to move a few
    span tags around newlines in the html. This improves the previous
    behavior and doesn't break when lines end with a higlighted span

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,29 @@
 var _pre = $("div.highlight > pre.highlight > code");
+var endSpan = "</span>"
+
+
+// Extra processing needs to be done in order to make sure all highlighting is
+// encapsulated on the same line
+// Sometimes lines may start with whitespace, but then begin with an end span
+// use regex to find matches
+var spanRegex = new RegExp('^\\s*' + endSpan)
 
 $.each(_pre, function(idx, object) {
-    object.innerHTML = "<span class='line'>" + object.innerHTML.split("\n").filter(Boolean).join("</span>\n<span class='line'>") + "</span>";
+    var lines = object.innerHTML.split("\n")
+
+    for(var i = 0; i < lines.length; i++) {
+        var matches = lines[i].match(spanRegex)
+        
+        if (matches != null && matches.length > 0 && i > 0) {
+            // Shouldn't match on i == 0
+            // line begins with </span>, so add to the previous line
+            lines[i-1] += endSpan
+            // Replace the first occurence of endspan with empty string
+            lines[i] = lines[i].replace(endSpan, "")
+        }
+    }
+    // Now we can wrap all lines with a span of class="line"
+    newlines = lines.map(x => "<span class='line'>" + x + "</span>" )
+    newlines.pop() // Remove the last element. It's _always_ an empty line
+    object.innerHTML = newlines.join("\n")
 })
