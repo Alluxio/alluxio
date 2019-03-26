817d7c7cf0b67289f4997f3943960604183c0262
AminoACID123 <1300019770@qq.com>
2019-03-26 13:38:27 +0800

    Replace explicit type argument with <>

--- core/base/src/main/java/alluxio/master/file/StartupConsistencyCheck.java
+++ core/base/src/main/java/alluxio/master/file/StartupConsistencyCheck.java
@@ -59,7 +59,7 @@ public final class StartupConsistencyCheck {
    * @return a result set to the failed status
    */
   public static StartupConsistencyCheck failed() {
-    return new StartupConsistencyCheck(Status.FAILED, new ArrayList<AlluxioURI>());
+    return new StartupConsistencyCheck(Status.FAILED, new ArrayList<>());
   }
 
   /**
