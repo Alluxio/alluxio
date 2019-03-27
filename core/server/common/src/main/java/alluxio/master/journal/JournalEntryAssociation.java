0aa6f83011ad3f6f7a8773a3f048c00bc1121cae
Cheng Chang <myairia@gmail.com>
2019-03-27 14:41:02 +0800

    Update journal entry association for meta master.

--- core/server/common/src/main/java/alluxio/master/journal/JournalEntryAssociation.java
+++ core/server/common/src/main/java/alluxio/master/journal/JournalEntryAssociation.java
@@ -52,6 +52,10 @@ public final class JournalEntryAssociation {
         || entry.hasBlockInfo()) {
       return Constants.BLOCK_MASTER_NAME;
     }
+    if (entry.hasPathProperties()
+        || entry.hasRemovePathProperties()) {
+      return Constants.META_MASTER_NAME;
+    }
     throw new IllegalStateException("Unrecognized journal entry: " + entry);
   }
 
