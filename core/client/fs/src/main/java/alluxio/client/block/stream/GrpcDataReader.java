67c3bc060ebbaef41dc9f4173a083e6a42d4fec5
bf8086 <binfeng@live.com>
2019-04-03 06:18:15 +0800

    Make read receipt more robust
    Two minor fixes:
    - Build read receipt from original request to work better with server
    validations.
    - Lower the log level to avoid excessive warnings on closing a read
    request.
    
    pr-link: Alluxio/alluxio#8670
    change-id: cid-1f59228d06409942d824cc4e771a496e39af4465

--- core/client/fs/src/main/java/alluxio/client/block/stream/GrpcDataReader.java
+++ core/client/fs/src/main/java/alluxio/client/block/stream/GrpcDataReader.java
@@ -142,10 +142,10 @@ public final class GrpcDataReader implements DataReader {
     }
     mPosToRead += buffer.readableBytes();
     try {
-      mStream.send(ReadRequest.newBuilder().setOffsetReceived(mPosToRead).build());
+      mStream.send(mReadRequest.toBuilder().setOffsetReceived(mPosToRead).build());
     } catch (Exception e) {
       // nothing is done as the receipt is sent at best effort
-      LOG.warn("Failed to send receipt of data to worker {} for request {}: {}.", mAddress,
+      LOG.debug("Failed to send receipt of data to worker {} for request {}: {}.", mAddress,
           mReadRequest, e.getMessage());
     }
     Preconditions.checkState(mPosToRead - mReadRequest.getOffset() <= mReadRequest.getLength());
