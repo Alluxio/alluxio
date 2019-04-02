343893edc130622e5dc9582529bf03418f586f24
Lu Qiu <luqiujob@gmail.com>
2019-04-02 16:01:31 -0700

    add test port and add description

--- tests/src/test/java/alluxio/server/configuration/ConfigCheckerIntegrationTest.java
+++ tests/src/test/java/alluxio/server/configuration/ConfigCheckerIntegrationTest.java
@@ -79,7 +79,8 @@ public class ConfigCheckerIntegrationTest extends BaseIntegrationTest {
     PropertyKey key = PropertyKey.MASTER_JOURNAL_FLUSH_TIMEOUT_MS;
     Map<Integer, Map<PropertyKey, String>> masterProperties
         = generatePropertyWithDifferentValues(TEST_NUM_MASTERS, key);
-    mCluster = MultiProcessCluster.newBuilder(PortCoordination.CONFIG_CHECKER_MULTI_MASTERS)
+    mCluster = MultiProcessCluster
+        .newBuilder(PortCoordination.CONFIG_CHECKER_MULTI_MASTERS_EMBEDDED_HA)
         .setClusterName("ConfigCheckerMultiMastersEmbeddedHATest")
         .setNumMasters(TEST_NUM_MASTERS)
         .setNumWorkers(0)
@@ -88,6 +89,8 @@ public class ConfigCheckerIntegrationTest extends BaseIntegrationTest {
         .build();
     mCluster.start();
     ConfigCheckReport report = getReport();
+    // The master values of {@link PropertyKey#ALLUXIO_MASTER_JOURNAL_FOLDER} are different
+    // when using embedded HA
     assertEquals(ConfigStatus.FAILED, report.getConfigStatus());
     assertThat(report.getConfigWarns().toString(),
         CoreMatchers.containsString(key.getName()));
