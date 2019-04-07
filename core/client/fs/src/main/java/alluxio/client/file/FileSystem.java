299ca375f8dc96cd4853be20b497ddc717dfa3a7
weiyuang <910941178@qq.com>
2019-04-07 04:49:25 -0700

    delete collections

--- core/client/fs/src/main/java/alluxio/client/file/FileSystem.java
+++ core/client/fs/src/main/java/alluxio/client/file/FileSystem.java
@@ -55,7 +55,6 @@ import org.slf4j.LoggerFactory;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
