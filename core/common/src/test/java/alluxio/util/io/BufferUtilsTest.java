fde3cba6d052a54bb272e76c6894a5485e9351c1
Acc-plus <1457895720@qq.com>
2019-04-10 01:40:41 +0800

    [SMALLFIX] Use static imports for standard test utilities

--- core/common/src/test/java/alluxio/util/io/BufferUtilsTest.java
+++ core/common/src/test/java/alluxio/util/io/BufferUtilsTest.java
@@ -14,6 +14,7 @@ package alluxio.util.io;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
