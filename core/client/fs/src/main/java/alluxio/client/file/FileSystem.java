9f76096297e1c03dfcab7b2fe4a9041c21b5649f
weiyuang <910941178@qq.com>
2019-04-12 05:13:39 -0700

    this line is no longer necessary

--- core/client/fs/src/main/java/alluxio/client/file/FileSystem.java
+++ core/client/fs/src/main/java/alluxio/client/file/FileSystem.java
@@ -135,7 +135,6 @@ public interface FileSystem extends Closeable {
         // Sort properties by name to keep output ordered.
         AlluxioConfiguration conf = context.getConf();
         List<PropertyKey> keys = new ArrayList<>(conf.keySet());
-        //Collections.sort(keys, Comparator.comparing(PropertyKey::getName));
         keys.sort(Comparator.comparing(PropertyKey::getName));
         for (PropertyKey key : keys) {
           String value = conf.getOrDefault(key, null);
