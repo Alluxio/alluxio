7771ddc00c2a684601f7883990e517b16d558882
161220012 <895977225@qq.com>
2019-04-16 23:47:18 +0800

    Update LRFUEvictor.java

--- core/server/worker/src/main/java/alluxio/worker/block/evictor/LRFUEvictor.java
+++ core/server/worker/src/main/java/alluxio/worker/block/evictor/LRFUEvictor.java
@@ -141,7 +141,7 @@ public final class LRFUEvictor extends AbstractEvictor {
    */
   private List<Map.Entry<Long, Double>> getSortedCRF() {
     List<Map.Entry<Long, Double>> sortedCRF = new ArrayList<>(mBlockIdToCRFValue.entrySet());
-    sortedCRF.sort(new Comparator<Map.Entry<Long, Double>>() {
+    Collections.sort(sortedCRF, new Comparator<Map.Entry<Long, Double>>() {
       @Override
       public int compare(Entry<Long, Double> o1, Entry<Long, Double> o2) {
         return Double.compare(o1.getValue(), o2.getValue());
