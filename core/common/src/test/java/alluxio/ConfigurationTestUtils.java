0b6017043b223d5d17b0f61834a4bfb22f731ec6
Lu Qiu <luqiujob@gmail.com>
2019-04-15 21:47:20 -0700

    make tests much faster

--- core/common/src/test/java/alluxio/ConfigurationTestUtils.java
+++ core/common/src/test/java/alluxio/ConfigurationTestUtils.java
@@ -127,6 +127,10 @@ public final class ConfigurationTestUtils {
       conf.put(PropertyKey.USER_RPC_RETRY_MAX_DURATION, "1s");
       // To keep tests fast, we should do more retries with a lower max wait time.
       conf.put(PropertyKey.USER_RPC_RETRY_MAX_NUM_RETRY, "60");
+    } else {
+      conf.put(PropertyKey.USER_RPC_RETRY_MAX_DURATION, "3s");
+      conf.put(PropertyKey.MASTER_EMBEDDED_JOURNAL_ELECTION_TIMEOUT, "1s");
+      conf.put(PropertyKey.MASTER_EMBEDDED_JOURNAL_HEARTBEAT_INTERVAL, "100ms");
     }
     conf.put(PropertyKey.USER_RPC_RETRY_MAX_SLEEP_MS, "500ms");
 
