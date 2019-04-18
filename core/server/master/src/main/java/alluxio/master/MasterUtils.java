b4dc19158d9e6f8e0bcdef90bd1a5c70343f4395
Yunchuan Zheng <yczheng1997@gmail.com>
2019-04-19 01:39:17 +0800

    [SMALLFIX] Use lambda in alluxio.master.MasterUtils#createMasters
    It's a new contributor task from:
    https://github.com/Alluxio/new-contributor-tasks/issues/394
    
    pr-link: Alluxio/alluxio#8429
    change-id: cid-4f28c5a1191948c09a4c211b5ee1993d179f118c

--- core/server/master/src/main/java/alluxio/master/MasterUtils.java
+++ core/server/master/src/main/java/alluxio/master/MasterUtils.java
@@ -44,14 +44,11 @@ final class MasterUtils {
   public static void createMasters(MasterRegistry registry, MasterContext context) {
     List<Callable<Void>> callables = new ArrayList<>();
     for (final MasterFactory factory : alluxio.master.ServiceUtils.getMasterServiceLoader()) {
-      callables.add(new Callable<Void>() {
-        @Override
-        public Void call() throws Exception {
-          if (factory.isEnabled()) {
-            factory.create(registry, context);
-          }
-          return null;
+      callables.add(() -> {
+        if (factory.isEnabled()) {
+          factory.create(registry, context);
         }
+        return null;
       });
     }
     try {
