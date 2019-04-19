513557625d8ac71b76b68938443bb08dce24c255
Asalea <asa893473663@gmail.com>
2019-04-19 09:11:40 -0700

    Modify Timeout... file

--- core/common/src/test/java/alluxio/refresh/TimeoutRefreshTest.java
+++ core/common/src/test/java/alluxio/refresh/TimeoutRefreshTest.java
@@ -13,7 +13,8 @@ package alluxio.refresh;
 
 import alluxio.util.CommonUtils;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 /**
@@ -30,14 +31,14 @@ public final class TimeoutRefreshTest {
     final long slackMs = 200;
     TimeoutRefresh timeoutRefresh = new TimeoutRefresh(timeoutMs);
     // First check, should attempt
-    Assert.assertTrue(timeoutRefresh.attempt());
+    assertTrue(timeoutRefresh.attempt());
     // Second check, should not attempt before refresh timeout
-    Assert.assertFalse(timeoutRefresh.attempt());
+    assertFalse(timeoutRefresh.attempt());
 
     CommonUtils.sleepMs(timeoutMs);
     CommonUtils.sleepMs(slackMs);
 
-    Assert.assertTrue(timeoutRefresh.attempt());
-    Assert.assertFalse(timeoutRefresh.attempt());
+    assertTrue(timeoutRefresh.attempt());
+    assertFalse(timeoutRefresh.attempt());
   }
 }
