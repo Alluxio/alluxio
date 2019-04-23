764c80dfdcb0123fce6a1e2c1feb16bd58d7e223
Lu Qiu <luqiujob@gmail.com>
2019-04-23 15:38:44 -0700

    fix checkstyle

--- underfs/kodo/src/main/java/alluxio/underfs/kodo/KodoUnderFileSystem.java
+++ underfs/kodo/src/main/java/alluxio/underfs/kodo/KodoUnderFileSystem.java
@@ -207,7 +207,7 @@ public class KodoUnderFileSystem extends ObjectUnderFileSystem {
   @Override
   protected InputStream openObject(String key, OpenOptions options, RetryPolicy retryPolicy) {
     try {
-      return new KodoInputStream(key, mKodoClinet, options.getOffset(),retryPolicy,
+      return new KodoInputStream(key, mKodoClinet, options.getOffset(), retryPolicy,
           mAlluxioConf.getBytes(PropertyKey.UNDERFS_OBJECT_STORE_MULTI_RANGE_CHUNK_SIZE));
     } catch (QiniuException e) {
       LOG.error("Failed to open Object {}, Msg: {}", key, e);
