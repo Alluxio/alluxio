97081fe171b498144c1986abfdf58c83d673caf4
ztw <852445892@qq.com>
2019-04-24 19:17:19 +0800

    [SMALLFIX] Use static imports for standard test utilities

--- core/client/fs/src/test/java/alluxio/client/block/policy/RoundRobinPolicyTest.java
+++ core/client/fs/src/test/java/alluxio/client/block/policy/RoundRobinPolicyTest.java
@@ -9,6 +9,7 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
+
 package alluxio.client.block.policy;
 
 import static org.junit.Assert.assertNotEquals;
