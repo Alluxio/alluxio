b41d669564b7e95467cc52d44f7d6c1f4f6b4fab
njuzmy <161220180@smail.nju.edu.cn>
2019-04-28 06:56:09 -0700

    STATE WHAT YOU CHANGED BRIEFLY HERE

--- core/server/worker/src/test/java/alluxio/worker/block/allocator/AllocatorTestBase.java
+++ core/server/worker/src/test/java/alluxio/worker/block/allocator/AllocatorTestBase.java
@@ -21,7 +21,8 @@ import alluxio.worker.block.meta.StorageDirView;
 import alluxio.worker.block.meta.StorageTier;
 import alluxio.worker.block.meta.TempBlockMeta;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
@@ -103,9 +104,9 @@ public class AllocatorTestBase {
         dirView == null ? null : dirView.createTempBlockMeta(SESSION_ID, mTestBlockId, blockSize);
 
     if (!avail) {
-      Assert.assertTrue(tempBlockMeta == null);
+      assertTrue(tempBlockMeta == null);
     } else {
-      Assert.assertTrue(tempBlockMeta != null);
+      assertTrue(tempBlockMeta != null);
     }
   }
 
@@ -131,15 +132,15 @@ public class AllocatorTestBase {
         dirView == null ? null : dirView.createTempBlockMeta(SESSION_ID, mTestBlockId, blockSize);
 
     if (!avail) {
-      Assert.assertTrue(tempBlockMeta == null);
+      assertTrue(tempBlockMeta == null);
     } else {
-      Assert.assertTrue(tempBlockMeta != null);
+      assertTrue(tempBlockMeta != null);
 
       StorageDir pDir = tempBlockMeta.getParentDir();
       StorageTier pTier = pDir.getParentTier();
 
-      Assert.assertTrue(pDir.getDirIndex() == dirIndex);
-      Assert.assertEquals(tierAlias, pTier.getTierAlias());
+      assertTrue(pDir.getDirIndex() == dirIndex);
+      assertEquals(tierAlias, pTier.getTierAlias());
 
       //update the dir meta info
       pDir.addBlockMeta(new BlockMeta(mTestBlockId, blockSize, pDir));
