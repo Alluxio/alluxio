7ac41b3ffea461c27feac4a70352d18dcd5b35b6
Gokturk Gezer <gokturk@alluxio.com>
2019-04-29 13:45:38 -0700

    Create an abstraction for journal dumping in the tool

--- core/server/common/src/main/java/alluxio/master/journal/JournalTool.java
+++ core/server/common/src/main/java/alluxio/master/journal/JournalTool.java
@@ -84,8 +84,6 @@ public final class JournalTool {
   private static long sStart;
   private static long sEnd;
   private static String sOutputDir;
-  private static String sCheckpointsDir;
-  private static String sJournalEntryFile;
 
   private JournalTool() {} // prevent instantiation
 
@@ -108,97 +106,26 @@ public final class JournalTool {
   }
 
   private static void dumpJournal() throws IOException {
-    System.out.printf("Dumping journal to %s%n", sOutputDir);
-    Files.createDirectories(Paths.get(sOutputDir));
-    UfsJournal journal =
-        new UfsJournalSystem(getJournalLocation(), 0).createJournal(new NoopMaster(sMaster));
-    try (PrintStream out =
-             new PrintStream(new BufferedOutputStream(new FileOutputStream(sJournalEntryFile)));
-         JournalReader reader = new UfsJournalReader(journal, sStart, true)) {
-      boolean done = false;
-      while (!done && reader.getNextSequenceNumber() < sEnd) {
-        State state = reader.advance();
-        switch (state) {
-          case CHECKPOINT:
-            try (CheckpointInputStream checkpoint = reader.getCheckpoint()) {
-              Path dir = Paths.get(sCheckpointsDir + "-" + reader.getNextSequenceNumber());
-              Files.createDirectories(dir);
-              readCheckpoint(checkpoint, dir);
-            }
-            break;
-          case LOG:
-            JournalEntry entry = reader.getEntry();
-            out.println(ENTRY_SEPARATOR);
-            out.print(entry);
-            break;
-          case DONE:
-            done = true;
-            break;
-          default:
-            throw new RuntimeException("Unknown state: " + state);
-        }
-      }
-    } catch (Exception e) {
-      LOG.error("Failed to read from journal.", e);
-    }
-  }
+    JournalType journalType =
+        ServerConfiguration.getEnum(PropertyKey.MASTER_JOURNAL_TYPE, JournalType.class);
 
-  private static void readCheckpoint(CheckpointInputStream checkpoint, Path path)
-      throws IOException {
-    LOG.debug("Reading checkpoint of type %s to %s%n", checkpoint.getType().name(), path);
-    switch (checkpoint.getType()) {
-      case COMPOUND:
-        readCompoundCheckpoint(checkpoint, path);
+    AbstractJournalDumper journalDumper;
+    switch (journalType) {
+      case UFS:
+        journalDumper = new UfsJournalDumper(sMaster, sStart, sEnd);
         break;
-      case ROCKS:
-        readRocksCheckpoint(checkpoint, path);
-        break;
+      case EMBEDDED:
+        journalDumper = null;
       default:
-        readRegularCheckpoint(checkpoint, path);
-        break;
+        System.err.printf("Unsupported journal type: %s\n", journalType.name());
+        return;
     }
-  }
 
-  private static void readCompoundCheckpoint(CheckpointInputStream checkpoint, Path path)
-      throws IOException {
-    Files.createDirectories(path);
-    CompoundCheckpointReader reader = new CompoundCheckpointReader(checkpoint);
-    Optional<Entry> entryOpt;
-    while ((entryOpt = reader.nextCheckpoint()).isPresent()) {
-      Entry entry = entryOpt.get();
-      Path checkpointPath = path.resolve(entry.getName().toString());
-      LOG.debug("Reading checkpoint for %s to %s%n", entry.getName(), checkpointPath);
-      readCheckpoint(entry.getStream(), checkpointPath);
-    }
-  }
-
-  private static void readRocksCheckpoint(CheckpointInputStream checkpoint, Path path)
-      throws IOException {
-    TarballCheckpointReader reader = new TarballCheckpointReader(checkpoint);
-    reader.unpackToDirectory(path);
-  }
-
-  private static void readRegularCheckpoint(CheckpointInputStream checkpoint, Path path)
-      throws IOException {
-    try (PrintStream out =
-        new PrintStream(new BufferedOutputStream(new FileOutputStream(path.toFile())))) {
-      checkpoint.getType().getCheckpointFormat().parseToHumanReadable(checkpoint, out);
-    }
-  }
-
-  /**
-   * @return the journal location
-   */
-  private static URI getJournalLocation() {
-    String journalDirectory = ServerConfiguration.get(PropertyKey.MASTER_JOURNAL_FOLDER);
-    if (!journalDirectory.endsWith(AlluxioURI.SEPARATOR)) {
-      journalDirectory += AlluxioURI.SEPARATOR;
-    }
-    try {
-      return new URI(journalDirectory);
-    } catch (URISyntaxException e) {
-      throw new RuntimeException(e);
-    }
+    System.out.printf("Dumping journal of type %s to %s\n", journalType.name(), sOutputDir);
+    // Ensure output directory structure.
+    Files.createDirectories(Paths.get(sOutputDir));
+    // Dump journal.
+    journalDumper.dumpJournal(sOutputDir);
   }
 
   /**
@@ -224,8 +151,6 @@ public final class JournalTool {
     sOutputDir =
         new File(cmd.getOptionValue("outputDir", "journal_dump-" + System.currentTimeMillis()))
             .getAbsolutePath();
-    sCheckpointsDir = PathUtils.concatPath(sOutputDir, "checkpoints");
-    sJournalEntryFile = PathUtils.concatPath(sOutputDir, "edits.txt");
     return true;
   }
 
@@ -238,5 +163,147 @@ public final class JournalTool {
             + "-jar-with-dependencies.jar alluxio.master.journal.JournalTool",
         "Read an Alluxio journal and write it to a directory in a human-readable format.", OPTIONS,
         "", true);
+  }
+
+  /**
+   * An abstract class for journal dumpers.
+   */
+  static abstract class AbstractJournalDumper {
+    protected final String mMaster;
+    protected final long mStart;
+    protected final long mEnd;
+
+    /**
+     * Creates abstract dumper with parameters.
+     *
+     * @param master journal master
+     * @param start journal start sequence
+     * @param end journal end sequence
+     */
+    public AbstractJournalDumper(String master, long start, long end) {
+      mMaster = master;
+      mStart = start;
+      mEnd = end;
+    }
+
+    /**
+     * Dumps journal.
+     *
+     * @param outputDir output directory for dump
+     */
+    abstract void dumpJournal(String outputDir);
+  }
+
+  /**
+   * Used to dump journal of type UFS.
+   */
+  static class UfsJournalDumper extends AbstractJournalDumper {
+
+    /**
+     * Creates UFS journal dumper.
+     *
+     * @param master journal master
+     * @param start journal start sequence number
+     * @param end journal end sequence number
+     */
+    public UfsJournalDumper(String master, long start, long end) {
+      super(master, start, end);
+    }
+
+    @Override
+    public void dumpJournal(String outputDir) {
+      String checkpointsDir = PathUtils.concatPath(outputDir, "checkpoints");
+      String journalEntryFile = PathUtils.concatPath(outputDir, "edits.txt");
+
+      UfsJournal journal =
+          new UfsJournalSystem(getJournalLocation(), 0).createJournal(new NoopMaster(sMaster));
+      try (
+          PrintStream out =
+              new PrintStream(new BufferedOutputStream(new FileOutputStream(journalEntryFile)));
+          JournalReader reader = new UfsJournalReader(journal, sStart, true)) {
+        boolean done = false;
+        while (!done && reader.getNextSequenceNumber() < sEnd) {
+          State state = reader.advance();
+          switch (state) {
+            case CHECKPOINT:
+              try (CheckpointInputStream checkpoint = reader.getCheckpoint()) {
+                Path dir = Paths.get(checkpointsDir + "-" + reader.getNextSequenceNumber());
+                Files.createDirectories(dir);
+                readCheckpoint(checkpoint, dir);
+              }
+              break;
+            case LOG:
+              JournalEntry entry = reader.getEntry();
+              out.println(ENTRY_SEPARATOR);
+              out.print(entry);
+              break;
+            case DONE:
+              done = true;
+              break;
+            default:
+              throw new RuntimeException("Unknown state: " + state);
+          }
+        }
+      } catch (Exception e) {
+        LOG.error("Failed to read from journal.", e);
+      }
+    }
+
+    /**
+     * @return the journal location
+     */
+    private URI getJournalLocation() {
+      String journalDirectory = ServerConfiguration.get(PropertyKey.MASTER_JOURNAL_FOLDER);
+      if (!journalDirectory.endsWith(AlluxioURI.SEPARATOR)) {
+        journalDirectory += AlluxioURI.SEPARATOR;
+      }
+      try {
+        return new URI(journalDirectory);
+      } catch (URISyntaxException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    private void readCheckpoint(CheckpointInputStream checkpoint, Path path) throws IOException {
+      LOG.debug("Reading checkpoint of type %s to %s%n", checkpoint.getType().name(), path);
+      switch (checkpoint.getType()) {
+        case COMPOUND:
+          readCompoundCheckpoint(checkpoint, path);
+          break;
+        case ROCKS:
+          readRocksCheckpoint(checkpoint, path);
+          break;
+        default:
+          readRegularCheckpoint(checkpoint, path);
+          break;
+      }
+    }
+
+    private void readCompoundCheckpoint(CheckpointInputStream checkpoint, Path path)
+        throws IOException {
+      Files.createDirectories(path);
+      CompoundCheckpointReader reader = new CompoundCheckpointReader(checkpoint);
+      Optional<Entry> entryOpt;
+      while ((entryOpt = reader.nextCheckpoint()).isPresent()) {
+        Entry entry = entryOpt.get();
+        Path checkpointPath = path.resolve(entry.getName().toString());
+        LOG.debug("Reading checkpoint for %s to %s%n", entry.getName(), checkpointPath);
+        readCheckpoint(entry.getStream(), checkpointPath);
+      }
+    }
+
+    private void readRocksCheckpoint(CheckpointInputStream checkpoint, Path path)
+        throws IOException {
+      TarballCheckpointReader reader = new TarballCheckpointReader(checkpoint);
+      reader.unpackToDirectory(path);
+    }
+
+    private void readRegularCheckpoint(CheckpointInputStream checkpoint, Path path)
+        throws IOException {
+      try (PrintStream out =
+          new PrintStream(new BufferedOutputStream(new FileOutputStream(path.toFile())))) {
+        checkpoint.getType().getCheckpointFormat().parseToHumanReadable(checkpoint, out);
+      }
+    }
   }
 }
