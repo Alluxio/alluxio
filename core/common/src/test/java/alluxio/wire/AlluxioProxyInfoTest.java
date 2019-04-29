0b799afee3efeabd450e96843e51c5d4bd481d33
单明慧 <belongtosmh@126.com>
2019-04-29 10:23:59 +0800

    Use static imports

--- core/common/src/test/java/alluxio/wire/AlluxioProxyInfoTest.java
+++ core/common/src/test/java/alluxio/wire/AlluxioProxyInfoTest.java
@@ -11,10 +11,11 @@
 
 package alluxio.wire;
 
+import static org.junit.Assert.assertEquals;
+
 import alluxio.util.CommonUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.util.HashMap;
