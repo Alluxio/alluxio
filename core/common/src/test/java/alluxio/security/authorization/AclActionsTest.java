3aae55ae49b65876297962d6df2133a2ec411209
Moya-Zhang <161271030@smail.nju.edu.cn>
2019-05-07 15:40:00 +0800

    add two new lines

--- core/common/src/test/java/alluxio/security/authorization/AclActionsTest.java
+++ core/common/src/test/java/alluxio/security/authorization/AclActionsTest.java
@@ -10,10 +10,12 @@
  */
 
 package alluxio.security.authorization;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 //import org.junit.Assert;
+
 import org.junit.Test;
 
 /**
