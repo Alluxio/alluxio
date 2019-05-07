b6848b29fe8cf1604d0f8f8402a723a253af1937
Cheng Chang <chengchang.greatwall@gmail.com>
2019-05-06 19:41:37 -0700

    Update configuration explicitly.

--- tests/src/test/java/alluxio/client/fs/PathConfigurationIntegrationTest.java
+++ tests/src/test/java/alluxio/client/fs/PathConfigurationIntegrationTest.java
@@ -71,6 +71,7 @@ public class PathConfigurationIntegrationTest {
         MasterClientContext.newBuilder(metaCtx.getClientContext()).build());
     setPathConfigurations(mMetaConfig);
     FileSystemContext fsCtx = FileSystemContext.create(ServerConfiguration.global());
+    fsCtx.getClientContext().updateClusterAndPathConf(fsCtx.getMasterAddress());
     mFileSystem = mLocalAlluxioClusterResource.get().getClient(fsCtx);
     mWriteThrough = CreateFilePOptions.newBuilder().setRecursive(true)
         .setWriteType(WritePType.THROUGH).build();
