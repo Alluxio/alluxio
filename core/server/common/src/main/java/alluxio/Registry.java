c011db766fe3a3761379e1c7fbadede55792abdd
turncoat54 <392207009@qq.com>
2019-05-11 19:51:54 +0800

    Update Registry.java

--- core/server/common/src/main/java/alluxio/Registry.java
+++ core/server/common/src/main/java/alluxio/Registry.java
@@ -113,7 +113,6 @@ public class Registry<T extends Server<U>, U> {
    */
   public List<T> getServers() {
     List<T> servers = new ArrayList<>(mRegistry.values());
-    //Collections.sort(servers, new DependencyComparator());
     servers.sort(new DependencyComparator());
     return servers;
   }
