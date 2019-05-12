4e91cf66faecdb6633390ad18e13fe33cc04c7fb
Bin Fan <fanbin103@gmail.com>
2019-05-11 21:18:10 -0700

    Update GrpcBlockingStreamTest.java

--- core/client/fs/src/test/java/alluxio/client/block/stream/GrpcBlockingStreamTest.java
+++ core/client/fs/src/test/java/alluxio/client/block/stream/GrpcBlockingStreamTest.java
@@ -12,16 +12,16 @@
 package alluxio.client.block.stream;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNull;
 
 import alluxio.Constants;
 import alluxio.exception.status.CancelledException;
