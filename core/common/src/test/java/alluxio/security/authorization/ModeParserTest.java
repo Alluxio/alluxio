54adb1b927dadfe7030879587b086f714f403379
Notteaquail <42956108+Notteaquail@users.noreply.github.com>
2019-05-14 18:58:36 +0800

    Update ModeParserTest.java

--- core/common/src/test/java/alluxio/security/authorization/ModeParserTest.java
+++ core/common/src/test/java/alluxio/security/authorization/ModeParserTest.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.assertEquals;
 
 import alluxio.exception.ExceptionMessage;
 
-//import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
