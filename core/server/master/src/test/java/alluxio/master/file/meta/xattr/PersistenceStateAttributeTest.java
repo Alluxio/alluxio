4c7312bf4ed7a32f3b99bf173ee80cf1a55de2ba
Zac Blanco <zac@alluxio.com>
2019-05-15 17:57:22 -0700

    Add licenses, rename test file

--- core/server/master/src/test/java/alluxio/master/file/meta/xattr/PersistenceStateAttributeTest.java
+++ core/server/master/src/test/java/alluxio/master/file/meta/xattr/PersistenceStateAttributeTest.java
@@ -1,3 +1,14 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package alluxio.master.file.meta.xattr;
 
 import static alluxio.master.file.meta.xattr.ExtendedAttribute.PERSISTENCE_STATE;
@@ -11,7 +22,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-public class PersistenceAttributeTest {
+public class PersistenceStateAttributeTest {
 
   @Test
   public void testSingleEncode() throws Exception {
