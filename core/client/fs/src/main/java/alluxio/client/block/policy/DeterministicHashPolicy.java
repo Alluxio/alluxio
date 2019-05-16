453d239dcbc6c31c10ebaacb0591a3d536a6d525
HenryYang <30862438+HenryYang99@users.noreply.github.com>
2019-05-16 11:01:13 +0800

    Update DeterministicHashPolicy.java

--- core/client/fs/src/main/java/alluxio/client/block/policy/DeterministicHashPolicy.java
+++ core/client/fs/src/main/java/alluxio/client/block/policy/DeterministicHashPolicy.java
@@ -70,8 +70,8 @@ public final class DeterministicHashPolicy implements BlockLocationPolicy {
   @Override
   public WorkerNetAddress getWorker(GetWorkerOptions options) {
     List<BlockWorkerInfo> workerInfos = Lists.newArrayList(options.getBlockWorkerInfos());
-Collections.sort(workerInfos, (o1, o2)
-            -> o1.getNetAddress().toString().compareToIgnoreCase(o2.getNetAddress().toString()));
+    Collections.sort(workerInfos, (o1, o2)
+    -> o1.getNetAddress().toString().compareToIgnoreCase(o2.getNetAddress().toString()));
     HashMap<WorkerNetAddress, BlockWorkerInfo> blockWorkerInfoMap = new HashMap<>();
     for (BlockWorkerInfo workerInfo : options.getBlockWorkerInfos()) {
       blockWorkerInfoMap.put(workerInfo.getNetAddress(), workerInfo);
