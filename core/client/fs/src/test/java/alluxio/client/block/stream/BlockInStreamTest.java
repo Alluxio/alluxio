b08c047125407581144689e67942997a0f3fee30
Adit Madan <adit@alluxio.com>
2019-05-28 17:51:48 -0700

    Fix locality in containerized environments
    In a containerized env such as k8s, the worker and client hostname may
    not match. To determine if a worker is local, file system inspection is
    used instead when property
    `alluxio.worker.data.server.domain.socket.as.uuid=true` is set.
    
    In this PR:
    - update the method to choose a local worker for BlockInStream/OutStream
    - if client container does not mount domain socket dir but has the same
    hostname as worker, do not attempt to open local file short circuit
    stream if worker supports domain socket (as /dev/shm is not accessible)
    - unmount as part of fuse startup in case k8s shutdown hook is flaky
    
    pr-link: Alluxio/alluxio#9143
    change-id: cid-eeb9aa299b842ab7086bde957649332a68416162

--- core/client/fs/src/test/java/alluxio/client/block/stream/BlockInStreamTest.java
+++ core/client/fs/src/test/java/alluxio/client/block/stream/BlockInStreamTest.java
@@ -133,8 +133,10 @@ public class BlockInStreamTest {
   @Test
   public void createDomainSocketEnabled() throws Exception {
     PowerMockito.mockStatic(NettyUtils.class);
-    PowerMockito.when(NettyUtils.isDomainSocketSupported(Matchers.any(WorkerNetAddress.class),
+    PowerMockito.when(NettyUtils.isDomainSocketAccessible(Matchers.any(WorkerNetAddress.class),
         Matchers.any(InstancedConfiguration.class)))
+        .thenReturn(true);
+    PowerMockito.when(NettyUtils.isDomainSocketSupported(Matchers.any(WorkerNetAddress.class)))
         .thenReturn(true);
     WorkerNetAddress dataSource = new WorkerNetAddress();
     BlockInStream.BlockInStreamSource dataSourceType = BlockInStream.BlockInStreamSource.LOCAL;
