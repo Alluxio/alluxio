cc6ebe6a2047768cce92b1f900eb8d39e1491454
madanadit <adit@alluxio.com>
2019-05-29 17:43:36 -0700

    Fix build

--- core/server/worker/src/main/java/alluxio/worker/grpc/ShortCircuitBlockWriteHandler.java
+++ core/server/worker/src/main/java/alluxio/worker/grpc/ShortCircuitBlockWriteHandler.java
@@ -167,7 +167,7 @@ class ShortCircuitBlockWriteHandler implements StreamObserver<CreateLocalBlockRe
           if (isCanceled) {
             mBlockWorker.abortBlock(mSessionId, mRequest.getBlockId());
           } else {
-            mBlockWorker.commitBlock(mSessionId, mRequest.getBlockId(), mRequest.getPinned());
+            mBlockWorker.commitBlock(mSessionId, mRequest.getBlockId(), mRequest.getPinOnCreate());
           }
         } finally {
           newContext.detach(previousContext);
