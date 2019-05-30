fcb60c58add177f251db4036b926d5a4e164d3db
Rico Chiu <ricochiu@gmail.com>
2019-05-29 17:49:07 -0700

    missed one

--- core/server/proxy/src/main/java/alluxio/proxy/PathsRestServiceHandler.java
+++ core/server/proxy/src/main/java/alluxio/proxy/PathsRestServiceHandler.java
@@ -294,7 +294,7 @@ public final class PathsRestServiceHandler {
   @Path(PATH_PARAM + OPEN_FILE)
   @Produces(MediaType.APPLICATION_JSON)
   @ApiOperation(value = "Opens the given path for reading, use the id with the stream api",
-                response = java.lang.Integer.class)
+      response = java.lang.Integer.class)
   public Response openFile(@PathParam("path") final String path, final OpenFileOptions options) {
     return RestUtils.call(new RestUtils.RestCallable<Integer>() {
       @Override
