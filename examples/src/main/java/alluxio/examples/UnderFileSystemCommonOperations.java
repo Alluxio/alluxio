ea25eef3a908e0b32ab5a8d83d39db0b4ae1dd8b
Lu Qiu <luqiujob@gmail.com>
2019-06-04 15:53:22 -0700

    fix checkstyle

--- examples/src/main/java/alluxio/examples/UnderFileSystemCommonOperations.java
+++ examples/src/main/java/alluxio/examples/UnderFileSystemCommonOperations.java
@@ -26,6 +26,7 @@ import alluxio.util.CommonUtils;
 import alluxio.util.UnderFileSystemUtils;
 import alluxio.util.WaitForOptions;
 import alluxio.util.io.PathUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
