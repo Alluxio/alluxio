21f6f674c433a4c5039b205a445f87421ed0d0c1
HelloHorizon <shouwei@alluxio.com>
2019-06-03 17:05:15 -0700

    fix style

--- core/common/src/test/java/alluxio/util/executor/ControllableSchedulerTest.java
+++ core/common/src/test/java/alluxio/util/executor/ControllableSchedulerTest.java
@@ -14,6 +14,7 @@ package alluxio.util.executor;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import java.util.Random;
