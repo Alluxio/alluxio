818ca0a7b291b0cb868ea553f7d119a126e6db62
ns1123 <45953825+ns1123@users.noreply.github.com>
2019-06-11 17:43:42 -0700

    [SMALLFIX] Create journal directory on startup with Embedded
    Check if Alluxio is in EMBEDDED mode and if so, create the journal
    directory if it doesn't exist.
    
    pr-link: Alluxio/alluxio#9247
    change-id: cid-9a6f7d44373731be2d3e5a7d9e9ab398b5cf6882

--- bin/alluxio-start.sh
+++ bin/alluxio-start.sh
@@ -232,6 +232,14 @@ start_logserver() {
 start_master() {
   if [[ "$1" == "-f" ]]; then
     ${LAUNCHER} ${BIN}/alluxio format
+  elif [[ `${LAUNCHER} ${BIN}/alluxio getConf ${ALLUXIO_MASTER_JAVA_OPTS} alluxio.master.journal.type` == "EMBEDDED" ]]; then
+      JOURNAL_DIR=`${LAUNCHER} ${BIN}/alluxio getConf ${ALLUXIO_MASTER_JAVA_OPTS} alluxio.master.journal.folder`
+      if [ -f "${JOURNAL_DIR}" ]; then
+        echo "Journal location ${JOURNAL_DIR} is a file not a directory. Please remove the file before retrying."
+      elif [ ! -e "${JOURNAL_DIR}" ]; then
+        ${LAUNCHER} ${BIN}/alluxio format
+      fi
+    fi
   fi
 
   if [[ ${ALLUXIO_MASTER_SECONDARY} == "true" ]]; then
