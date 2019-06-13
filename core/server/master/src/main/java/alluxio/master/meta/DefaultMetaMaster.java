f9dd6c16a79e7b33c540ecde6fb4be349c055974
ns1123 <45953825+ns1123@users.noreply.github.com>
2019-06-13 13:53:40 -0700

    Change journal backup default
    Change the default backup directory to be '/opt/alluxio/alluxio_backups'
    on the current Alluxio Primary Master
    If a path is provided via CLI, then fall back to using UFS (same
    behaviour)
    If a path is provided via CLI with the --local option, then backup to
    the specified directory on the primary master (same behaviour)
    
    pr-link: Alluxio/alluxio#9283
    change-id: cid-e32bd739a65b498dc42b6fe58ad4ef53819863ad

--- core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
+++ core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
@@ -268,7 +268,8 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
     String dir = options.hasTargetDirectory() ? options.getTargetDirectory()
         : ServerConfiguration.get(PropertyKey.MASTER_BACKUP_DIRECTORY);
     UnderFileSystem ufs = mUfs;
-    if (options.getLocalFileSystem() && !ufs.getUnderFSType().equals("local")) {
+    if ((options.getLocalFileSystem() || !options.hasTargetDirectory())
+            && !ufs.getUnderFSType().equals("local")) {
       ufs = UnderFileSystem.Factory.create("/",
           UnderFileSystemConfiguration.defaults(ServerConfiguration.global()));
       LOG.info("Backing up to local filesystem in directory {}", dir);
