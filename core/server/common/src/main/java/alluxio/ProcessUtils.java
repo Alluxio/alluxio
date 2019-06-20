11538cfe86c0f1dc25146fe224510b697d7a3924
David Zhu <david@alluxio.com>
2019-06-20 13:16:32 -0700

    fix checkstyle

--- core/server/common/src/main/java/alluxio/ProcessUtils.java
+++ core/server/common/src/main/java/alluxio/ProcessUtils.java
@@ -36,13 +36,14 @@ public final class ProcessUtils {
       LOG.info("Stopping {}.", process);
       System.exit(0);
     } catch (Throwable t) {
-      LOG.error("Uncaught exception while running {}, stopping it and exiting."
+      LOG.error("Uncaught exception while running {}, stopping it and exiting. "
               + "Exception {}, Root Cause {}", process, t, ExceptionUtils.getRootCause(t));
       try {
         process.stop();
       } catch (Throwable t2) {
         // continue to exit
-        LOG.error("Uncaught exception while stopping {}, simply exiting. Exception {}, Root Cause {}",
+        LOG.error("Uncaught exception while stopping {}, simply exiting. "
+                + "Exception {}, Root Cause {}",
             process, t2, ExceptionUtils.getRootCause(t2));
       }
       System.exit(-1);
