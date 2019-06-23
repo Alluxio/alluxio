7ce3bcc55984271175bde316a209c61ffa76a596
cheng-chang <cc@alluxio.com>
2019-06-23 14:31:38 -0700

    Update vagrant integration and documentation
    Auto cherry-pick of existing commit.
    orig-pr: Alluxio/alluxio#9351
    orig-commit: Alluxio/alluxio@22483706c21b7e816d3d8bd30fbc418f9816b9db
    orig-commit-author: cheng-chang <cc@alluxio.com>
    
    pr-link: Alluxio/alluxio#9351
    change-id: cid-afc8f7487560aa7e7c3f1b1f2ea35b75ff831164

--- deploy/vagrant/provision/roles/alluxio/files/set_master_address_old.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# create alluxio-env.sh
-/bin/cp /alluxio/conf/alluxio-env.sh.template /alluxio/conf/alluxio-env.sh
-
-sed -i "s/^export ALLUXIO_MASTER_HOSTNAME=.*/export ALLUXIO_MASTER_HOSTNAME=$(tail -n1 /alluxio/conf/workers)/g" /alluxio/conf/alluxio-env.sh
