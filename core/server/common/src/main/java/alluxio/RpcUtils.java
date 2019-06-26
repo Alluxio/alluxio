6dc59ce936c59b841060a6605c9869363ed2a7c7
Gokturk Gezer <gokturk@alluxio.com>
2019-06-26 14:40:14 -0700

    Refactor RpcUtils

--- core/server/common/src/main/java/alluxio/RpcUtils.java
+++ core/server/common/src/main/java/alluxio/RpcUtils.java
@@ -25,129 +25,16 @@ import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 
 import java.io.IOException;
-import javax.annotation.Nullable;
 
 /**
  * Utilities for handling server RPC calls.
  *
- * There are three types of RPC calls: 1. RPCs that only throw AlluxioException 2. RPCs that throw
- * AlluxioException and IOException 3. Streaming RPCs
- *
- * For each of these, there are two types of methods 1. call(callable) - for internal methods,
- * executes the method without any logging or metrics 2. call(logger, callable, method name,
- * failureOk, method description, arguments...) - for client initiated methods, executes the method
- * with logging and metrics. If failureOk is set, non-fatal errors will only be logged at the DEBUG
+ * There are three types of RPC calls: 1. RPCs that only throw IOException 2. Streaming RPCs
+ * For each of these, if failureOk is set, non-fatal errors will only be logged at the DEBUG
  * level and failure metrics will not be recorded.
  */
 public final class RpcUtils {
   private RpcUtils() {} // prevent instantiation
-
-  /**
-   * Calls the given {@link RpcCallable} and handles any exceptions thrown. No call history or
-   * errors will be logged. This method should be used for internal RPCs.
-   *
-   * @param callable the callable to call
-   * @param <T> the return type of the callable
-   * @param responseObserver gRPC response observer
-   */
-  public static <T> void call(RpcCallable<T> callable, StreamObserver<T> responseObserver) {
-    try {
-      T ret = callable.call();
-      responseObserver.onNext(ret);
-      responseObserver.onCompleted();
-    } catch (AlluxioException e) {
-      responseObserver
-          .onError(AlluxioStatusException.fromAlluxioException(e).toGrpcStatusException());
-    } catch (RuntimeException e) {
-      responseObserver.onError(new InternalException(e).toGrpcStatusException());
-    }
-  }
-
-  /**
-   * Calls the given {@link RpcCallable} and handles any exceptions thrown. If the RPC fails, a
-   * warning or error will be logged.
-   *
-   * @param logger the logger to use for this call
-   * @param callable the callable to call
-   * @param methodName the name of the method, used for metrics
-   * @param description the format string of the description, used for logging
-   * @param responseObserver gRPC response observer
-   * @param args the arguments for the description
-   * @param <T> the return type of the callable
-   */
-  public static <T> void call(Logger logger, RpcCallable<T> callable, String methodName,
-      String description, StreamObserver<T> responseObserver, Object... args) {
-    call(logger, callable, methodName, false, description, responseObserver, args);
-  }
-
-  /**
-   * Calls the given {@link RpcCallable} and handles any exceptions thrown.
-   *
-   * The failureOk parameter indicates whether or not AlluxioExceptions are expected results (for
-   * example it would be false for the exists() call). In this case, we do not log the failure or
-   * increment failure metrics. When a RuntimeException is thrown, we always treat it as a failure
-   * and log an error and increment metrics.
-   *
-   * @param logger the logger to use for this call
-   * @param callable the callable to call
-   * @param methodName the name of the method, used for metrics
-   * @param failureOk whether failures are expected (affects logging and metrics)
-   * @param description the format string of the description, used for logging
-   * @param responseObserver gRPC response observer
-   * @param args the arguments for the description
-   * @param <T> the return type of the callable
-   */
-  public static <T> void call(Logger logger, RpcCallable<T> callable, String methodName,
-      boolean failureOk, String description, StreamObserver<T> responseObserver, Object... args) {
-    // avoid string format for better performance if debug is off
-    String debugDesc = logger.isDebugEnabled() ? String.format(description, args) : null;
-    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(methodName)).time()) {
-      logger.debug("Enter: {}: {}", methodName, debugDesc);
-      T ret = callable.call();
-      logger.debug("Exit (OK): {}: {}", methodName, debugDesc);
-      responseObserver.onNext(ret);
-      responseObserver.onCompleted();
-    } catch (AlluxioException e) {
-      logger.debug("Exit (Error): {}: {}", methodName, debugDesc, e);
-      if (!failureOk) {
-        MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
-        if (!logger.isDebugEnabled()) {
-          logger.warn("Exit (Error): {}: {}, Error={}", methodName,
-              String.format(description, args), e);
-        }
-      }
-      responseObserver.onError(
-          AlluxioStatusException.fromAlluxioException(e).toGrpcStatusException());
-    } catch (RuntimeException e) {
-      logger.error("Exit (Error): {}: {}", methodName, String.format(description, args), e);
-      MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
-      responseObserver.onError(new InternalException(e).toGrpcStatusException());
-    }
-  }
-
-  /**
-   * Calls the given {@link RpcCallableThrowsIOException} and handles any exceptions thrown. No call
-   * history or errors will be logged. This method should be used for internal RPCs.
-   *
-   * @param callable the callable to call
-   * @param responseObserver gRPC response observer
-   * @param <T> the return type of the callable
-   */
-  public static <T> void call(RpcCallableThrowsIOException<T> callable,
-      StreamObserver<T> responseObserver) {
-    try {
-      T ret = callable.call();
-      responseObserver.onNext(ret);
-      responseObserver.onCompleted();
-    } catch (AlluxioException e) {
-      responseObserver
-          .onError(AlluxioStatusException.fromAlluxioException(e).toGrpcStatusException());
-    } catch (IOException e) {
-      responseObserver.onError(AlluxioStatusException.fromIOException(e).toGrpcStatusException());
-    } catch (RuntimeException e) {
-      responseObserver.onError((new InternalException(e).toGrpcStatusException()));
-    }
-  }
 
   /**
    * Calls the given {@link RpcCallableThrowsIOException} and handles any exceptions thrown. If the
@@ -186,44 +73,16 @@ public final class RpcUtils {
   public static <T> void call(Logger logger, RpcCallableThrowsIOException<T> callable,
       String methodName, boolean failureOk, String description, StreamObserver<T> responseObserver,
       Object... args) {
-    // avoid string format for better performance if debug is off
-    String debugDesc = logger.isDebugEnabled() ? String.format(description, args) : null;
-    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(methodName)).time()) {
-      logger.debug("Enter: {}: {}", methodName, debugDesc);
-      T ret = callable.call();
-      logger.debug("Exit (OK): {}: {}", methodName, debugDesc);
-      responseObserver.onNext(ret);
-      responseObserver.onCompleted();
-    } catch (AlluxioException e) {
-      logger.debug("Exit (Error): {}: {}", methodName, debugDesc, e);
-      if (!failureOk) {
-        MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
-        if (!logger.isDebugEnabled()) {
-          logger.warn("Exit (Error): {}: {}, Error={}", methodName,
-              String.format(description, args), e);
-        }
-      }
+    try {
       responseObserver
-          .onError(AlluxioStatusException.fromAlluxioException(e).toGrpcStatusException());
-    } catch (IOException e) {
-      logger.debug("Exit (Error): {}: {}", methodName, debugDesc, e);
-      if (!failureOk) {
-        MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
-        if (!logger.isDebugEnabled()) {
-          logger.warn("Exit (Error): {}: {}, Error={}", methodName,
-              String.format(description, args), e);
-        }
-      }
-      responseObserver.onError(AlluxioStatusException.fromIOException(e).toGrpcStatusException());
-    } catch (RuntimeException e) {
-      logger.error("Exit (Error): {}: {}", methodName, String.format(description, args), e);
-      MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
-      responseObserver.onError(new InternalException(e).toGrpcStatusException());
+          .onNext(callAndReturn(logger, callable, methodName, failureOk, description, args));
+      responseObserver.onCompleted();
+    } catch (StatusException e) {
+      responseObserver.onError(e);
     }
   }
 
   /**
-   * TODO(ggezer) Merge with call() family of methods.
    *
    * Calls the given {@link RpcCallableThrowsIOException} and returns its result. Exceptions are
    * logged, accounted for in metrics and then rethrown at the end.
@@ -285,36 +144,6 @@ public final class RpcUtils {
    * @param logger the logger to use for this call
    * @param callable the callable to call
    * @param methodName the name of the method, used for metrics
-   * @param description the format string of the description, used for logging
-   * @param args the arguments for the description
-   * @param <T> the return type of the callable
-   * @return the rpc result
-   */
-  @Nullable
-  public static <T> T streamingRPCAndLog(Logger logger, StreamingRpcCallable<T> callable,
-      String methodName, String description, Object... args) {
-    // avoid string format for better performance if debug is off
-    String debugDesc = logger.isDebugEnabled() ? String.format(description, args) : null;
-    try (Timer.Context ctx = MetricsSystem.timer(getQualifiedMetricName(methodName)).time()) {
-      logger.debug("Enter: {}: {}", methodName, debugDesc);
-      T result = callable.call();
-      logger.debug("Exit (OK): {}: {}", methodName, debugDesc);
-      return result;
-    } catch (Exception e) {
-      logger.warn("Exit (Error): {}: {}, Error={}", methodName, String.format(description, args),
-          e);
-      MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
-      callable.exceptionCaught(e);
-    }
-    return null;
-  }
-
-  /**
-   * Handles a streaming RPC callable with logging.
-   *
-   * @param logger the logger to use for this call
-   * @param callable the callable to call
-   * @param methodName the name of the method, used for metrics
    * @param sendResponse whether a response should send to the client
    * @param completeResponse whether onComplete should be called on the response observer
    * @param responseObserver gRPC response observer
@@ -362,20 +191,6 @@ public final class RpcUtils {
       return Metric.getMetricNameWithUserTag(name, user.getName());
     }
     return name;
-  }
-
-  /**
-   * An interface representing a callable which can only throw Alluxio exceptions.
-   *
-   * @param <T> the return type of the callable
-   */
-  public interface RpcCallable<T> {
-    /**
-     * The RPC implementation.
-     *
-     * @return the return value from the RPC
-     */
-    T call() throws AlluxioException;
   }
 
   /**
