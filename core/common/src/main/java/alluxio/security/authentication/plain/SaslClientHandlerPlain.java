3cc63b8844b2bff9466aff77ad7617ea8386627f
Gokturk Gezer <gokturk@alluxio.com>
2019-06-27 12:57:59 -0700

    Extract SaslClient/SaslServer logic from handlers as abstract classes

--- core/common/src/main/java/alluxio/security/authentication/plain/SaslClientHandlerPlain.java
+++ core/common/src/main/java/alluxio/security/authentication/plain/SaslClientHandlerPlain.java
@@ -15,6 +15,7 @@ import alluxio.conf.AlluxioConfiguration;
 import alluxio.exception.status.UnauthenticatedException;
 import alluxio.grpc.ChannelAuthenticationScheme;
 import alluxio.security.User;
+import alluxio.security.authentication.AbstractSaslClientHandler;
 import alluxio.security.authentication.SaslClientHandler;
 import alluxio.security.authentication.AuthenticationUserUtils;
 
@@ -31,11 +32,8 @@ import java.util.Set;
 /**
  * {@link SaslClientHandler} implementation for Plain/Custom schemes.
  */
-public class SaslClientHandlerPlain implements SaslClientHandler {
+public class SaslClientHandlerPlain extends AbstractSaslClientHandler {
   private static final Logger LOG = LoggerFactory.getLogger(SaslClientHandlerPlain.class);
-
-  /** Underlying SaslClient. */
-  private final SaslClient mSaslClient;
 
   /**
    * Creates {@link SaslClientHandler} instance for Plain/Custom.
@@ -90,21 +88,5 @@ public class SaslClientHandlerPlain implements SaslClientHandler {
   @Override
   public ChannelAuthenticationScheme getClientScheme() {
     return ChannelAuthenticationScheme.SIMPLE;
-  }
-
-  @Override
-  public SaslClient getSaslClient() {
-    return mSaslClient;
-  }
-
-  @Override
-  public void close() {
-    if (mSaslClient != null) {
-      try {
-        mSaslClient.dispose();
-      } catch (SaslException exc) {
-        LOG.debug("Failed to close SaslClient.", exc);
-      }
-    }
   }
 }
