8de7df99a1d8aeda9ff9605efbac1b835f48f29a
Nakkul Sreenivas <nakkul@alluxio.com>
2019-07-01 15:33:38 -0700

    Add import to fix build

--- core/common/src/test/java/alluxio/util/executor/ControllableSchedulerTest.java
+++ core/common/src/test/java/alluxio/util/executor/ControllableSchedulerTest.java
@@ -15,6 +15,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Random;
