6c5fbd74274b99b4e88f3c636ff5de3cb07d28d2
Oscilloputer0627 <151220121@smail.nju.edu.cn>
2019-07-03 07:27:08 +0800

    reset contents in webui

--- core/common/src/main/java/alluxio/AbstractClient.java
+++ core/common/src/main/java/alluxio/AbstractClient.java
@@ -112,7 +112,13 @@ public abstract class AbstractClient implements Client {
   protected abstract ServiceType getRemoteServiceType();
 
   protected long getRemoteServiceVersion() throws AlluxioStatusException {
-     return retryRPC(() -> mVersionService.getServiceVersion(GetServiceVersionPRequest.newBuilder().setServiceType(getRemoteServiceType()).build()).getVersion());
+    return retryRPC(new RpcCallable<Long>() {
+      public Long call() {
+        return mVersionService.getServiceVersion(
+            GetServiceVersionPRequest.newBuilder().setServiceType(getRemoteServiceType()).build())
+            .getVersion();
+      }
+    });
   }
 
   /**
