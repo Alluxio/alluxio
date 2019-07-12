d50eae2927d95767810641b0cfdc4c1bbce4ec7b
turncoat54 <392207009@qq.com>
2019-07-12 20:41:02 +0800

    [SMALLFIX] Replace Collections.sort() with List.sort()

--- core/server/common/src/main/java/alluxio/Registry.java
+++ core/server/common/src/main/java/alluxio/Registry.java
@@ -20,7 +20,6 @@ import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
