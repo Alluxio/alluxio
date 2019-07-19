289e62d06f66c0f2d05ad1bb054ffcbd1f7662f8
Zac Blanco <zac@alluxio.com>
2019-07-19 13:07:25 -0700

    Refactor alluxio EMR script

--- integration/emr/alluxio-emr.sh
+++ integration/emr/alluxio-emr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
 # (the "License"). You may not use this work except in compliance with the License, which is
@@ -10,99 +10,187 @@
 # See the NOTICE file distributed with this work for information regarding copyright ownership.
 #
 
-# This script is meant for bootstrapping the Alluxio service to an EMR cluster. Arguments for the script are listed below.
+# This script is meant for bootstrapping the Alluxio service to an EMR cluster.
+# Arguments for the script are listed below.
 # Arg 1. Download URI (ex. http://downloads.alluxio.io/downloads/files/2.0.0/alluxio-2.0.0-bin.tar.gz)
 # Arg 2. Root UFS URI (ex. s3://my-bucket/alluxio-emr/mount)
-# Arg 3. Extra Alluxio Options. These will be appended to alluxio-site.properties. Multiple options can be specified using ';' as a delimiter
-#        (ex. alluxio.user.file.writetype.default=CACHE_THROUGH;alluxio.user.file.readtype.default=CACHE)
+# Arg 3. (Optional) HTTP(S) or S3 URIs pointing to alluxio-site.properties file
+#        that will be used on cluster startup. The resource must be named "alluxio-site.properties"
+# Arg 4. (Optional) Delimeter for additional properties
+# Arg 5. (Optional) Extra Alluxio Options. These will be appended to
+#        alluxio-site.properties. Multiple options can be specified using Arg 4
+#        as a delimiter (ex. alluxio.user.file.writetype.default=CACHE_THROUGH;alluxio.user.file.readtype.default=CACHE)
 
-#Create user
-sudo groupadd alluxio -g 600
-sudo useradd alluxio -u 600 -g 600
+set -o errexit  # exit when a command fails - append "|| true" to allow a
+                # command to fail
+set -o nounset  # exit when attempting to use undeclared variables
 
-#Download the release
-#TODO Add metadata header tag to the wget for filtering out in download metrics.
-if [[ -z $1 ]]
-then
-  echo "No Download URL Provided. Please go to http://downloads.alluxio.io to see available release downloads."
-else
-  if [[ "${1}" == s3://* ]]
+
+set -x # debugging
+
+# script constants
+ALLUXIO_HOME=/opt/alluxio
+ALLUXIO_SITE_PROPERTIES=${ALLUXIO_HOME}/conf/alluxio-site.properties
+
+# Downloads a file to the local machine from a remote HTTP(S) or S3 URI into the cwd
+#
+# Args:
+#   $1: URI - the remote location to retrieve the file from
+download_file() {
+  local uri=$1
+
+  if [[ "${uri}" == s3://* ]]
   then
-    aws s3 cp ${1} ./
+    aws s3 cp ${uri} ./
   else
-    wget "${1}"
+    # TODO Add metadata header tag to the wget for filtering out in download metrics.
+    wget "${uri}"
   fi
-fi
-RELEASE=`basename ${1}`
-RELEASE_UNZIP=${RELEASE%"-bin.tar.gz"}
-RELEASE_UNZIP=${RELEASE_UNZIP%".tar.gz"}
+}
 
-#Unpack and inflate the release tar
-#TODO logic for different compression formats, s3 URIs, git URIs, etc.
-sudo cp $RELEASE /opt/
-sudo tar -xvf /opt/$RELEASE -C /opt/
-sudo rm -R /opt/$RELEASE
-sudo mv /opt/$RELEASE_UNZIP /opt/alluxio
-sudo chown -R alluxio:alluxio /opt/alluxio
-rm ${RELEASE}
-sudo runuser -l alluxio -c "cp /opt/alluxio/conf/alluxio-site.properties.template /opt/alluxio/conf/alluxio-site.properties"
+# Run a command as a specific user
+# Assumes the provided user already exists on the system and user running script has sudo access
+#
+# Args:
+#   $1: user - the username to run the command as
+#   $2: command - the command to run
+doas() {
+  local user=$1
+  local cmd=$2
+  sudo runuser -l ${user} -c "${cmd}"
+}
 
-#Get hostnames and load into masters/workers file
-EMR_CLUSTER=`jq '.jobFlowId' /mnt/var/lib/info/job-flow.json | sed -e 's/^"//' -e 's/"$//'`
-HOSTLIST=`aws emr list-instances --cluster-id $EMR_CLUSTER --region us-east-1 | jq '.Instances[].PrivateDnsName' | sed -e 's/^"//' -e 's/"$//'`
-MASTER=`jq '.masterHost' /mnt/var/lib/info/extraInstanceData.json | sed -e 's/^"//' -e 's/"$//' | nslookup | awk -v ip="$ip" '/name/{print substr($NF,1,length($NF)-1),ip}'`
+# Appends a property KV pair to the alluxio-site.properties file
+#
+# Args:
+#   $1: property name
+#   $2: property value
+append_alluxio_property() {
+  local property=$1
+  local value=$2
 
-if [[ -z "$MASTER" ]]
-then
-  MASTER=`hostname`
-  MASTER=${MASTER}".ec2.internal"
-fi
+  # Ok to fail in this section
+  set +o errexit
+  # /opt/alluxio/conf must exist
+  grep -q ${property} ${ALLUXIO_SITE_PROPERTIES} 2> /dev/null
+  local rv=$?
+  set -o errexit # errors not ok anymore
+  echo $rv
+  if [ $rv -ne 0 ]; then
+    echo "append prop"
+    doas alluxio "echo '${property}=${value}' >> ${ALLUXIO_SITE_PROPERTIES}"
+  fi
+}
 
-WORKERS=`printf '%s\n' "${HOSTLIST//$MASTER/}"`
+main() {
+  local alluxio_tarball
+  local root_ufs_uri
+  local site_properties_uri
+  local property_delimeter
+  local delimited_properties
+  alluxio_tarball=${1}
+  root_ufs_uri=${2}
+  site_properties_uri=${3}
+  property_delimeter=${4}
+  delimited_properties=${5}
 
-sudo runuser -l alluxio -c "echo '$MASTER' > /opt/alluxio/conf/masters"
-sudo runuser -l alluxio -c "echo '$WORKERS' > /opt/alluxio/conf/workers"
+  # Create user
+  sudo groupadd alluxio -g 600 || true
+  sudo useradd alluxio -u 600 -g 600 || true
 
-#Identify master
-IS_MASTER=`jq '.isMaster' /mnt/var/lib/info/instance.json`
+  # Download the release
+  if [[ -z ${alluxio_tarball} ]]
+  then
+    echo "No Download URL Provided. Please go to http://downloads.alluxio.io to see available release downloads."
+  else
+    download_file ${alluxio_tarball}
+  fi
 
-#Set up alluxio-site.properties
-sudo runuser -l alluxio -c "echo 'alluxio.master.hostname=$MASTER' > /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.master.journal.type=UFS' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.master.mount.table.root.ufs=$2' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.master.security.impersonation.hive.users=*' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.master.security.impersonation.presto.users=*' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.master.security.impersonation.yarn.users=*' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.worker.memory.size=20GB' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.worker.tieredstore.level0.alias=MEM' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.worker.tieredstore.level0.dirs.path=/mnt/ramdisk' >> /opt/alluxio/conf/alluxio-site.properties"
-sudo runuser -l alluxio -c "echo 'alluxio.worker.tieredstore.levels=1' >> /opt/alluxio/conf/alluxio-site.properties"
+  local release=`basename ${alluxio_tarball}`
+  local release_unzip=${release%"-bin.tar.gz"}
+  release_unzip=${release_unzip%".tar.gz"}
 
-#Inject user defined properties (semicolon separated)
-IFS=';'
-conf=($3)
-printf "%s\n" "${conf[@]}" | sudo tee -a /opt/alluxio/conf/alluxio-site.properties
+  # Unpack and inflate the release tar
+  # TODO logic for different compression formats, s3 URIs, git URIs, etc.
+  sudo cp ${release} /opt/
+  sudo tar -xvf /opt/${release} -C /opt/
+  sudo rm -R /opt/${release}
+  sudo mv /opt/${release_unzip} ${ALLUXIO_HOME}
+  sudo chown -R alluxio:alluxio ${ALLUXIO_HOME}
+  rm ${release}
+  doas alluxio "cp ${ALLUXIO_SITE_PROPERTIES}.template ${ALLUXIO_SITE_PROPERTIES}"
 
-#No ssh
-if [[ ${IS_MASTER} = "true" ]]
-then
-  sudo runuser -l alluxio -c "/opt/alluxio/bin/alluxio-start.sh -a master"
-  sudo runuser -l alluxio -c "/opt/alluxio/bin/alluxio-start.sh -a job_master"
-  sudo runuser -l alluxio -c "/opt/alluxio/bin/alluxio-start.sh -a proxy"
-else
-  /opt/alluxio/bin/alluxio-mount.sh SudoMount local
-  until /opt/alluxio/bin/alluxio fsadmin report
-  do
-    sleep 5
+  #Get hostnames and load into masters/workers file
+  local emr_cluster=`jq '.jobFlowId' /mnt/var/lib/info/job-flow.json | sed -e 's/^"//' -e 's/"$//'`
+  local hostlist=`aws emr list-instances --cluster-id ${emr_cluster} --region us-east-1 | jq '.Instances[].PrivateDnsName' | sed -e 's/^"//' -e 's/"$//'`
+  local master=`jq '.masterHost' /mnt/var/lib/info/extraInstanceData.json | sed -e 's/^"//' -e 's/"$//' | nslookup | awk -v ip="${ip}" '/name/{print substr($NF,1,length($NF)-1),ip}'`
+
+  if [[ -z "${master}" ]]
+  then
+    master=`hostname`
+    master=${master}".ec2.internal"
+  fi
+
+  local workers=`printf '%s\n' "${hostlist//$master/}"`
+
+  doas alluxio "echo '${master}' > ${ALLUXIO_HOME}/conf/masters"
+  doas alluxio "echo '${workers}' > ${ALLUXIO_HOME}/conf/workers"
+
+  # Identify master
+  local is_master=`jq '.isMaster' /mnt/var/lib/info/instance.json`
+
+  # Download user-specified site properties file to
+  # ${ALLUXIO_HOME/conf/alluxio-site.properties}. Must be named
+  # "alluxio-site.properties"
+  if [! -z "" ]; then
+    download_file ${site_properties_uri}
+    cp ./alluxio-site.properties ${ALLUXIO_SITE_PROPERTIES}
+  fi
+
+  # Inject user defined properties from args
+  IFS=${property_delimeter}
+  conf=(${delimited_properties})
+  for property in ${conf}; do
+    IFS=${property_delimeter} && read key value < ${property}
+    append_alluxio_property key value
   done
-  sudo runuser -l alluxio -c "/opt/alluxio/bin/alluxio-start.sh -a worker"
-  sudo runuser -l alluxio -c "/opt/alluxio/bin/alluxio-start.sh -a job_worker"
-  sudo runuser -l alluxio -c "/opt/alluxio/bin/alluxio-start.sh -a proxy"
-fi
 
-#Compute configs
-sudo runuser -l alluxio -c "ln -s /opt/alluxio/client/*client.jar /opt/alluxio/client/alluxio-client.jar"
-sudo mkdir -p /usr/lib/spark/jars/
-sudo ln -s /opt/alluxio/client/alluxio-client.jar /usr/lib/spark/jars/alluxio-client.jar
-sudo mkdir -p /usr/lib/presto/plugin/hive-hadoop2/
-sudo ln -s /opt/alluxio/client/alluxio-client.jar /usr/lib/presto/plugin/hive-hadoop2/alluxio-client.jar
+  # Append default configs to site properties if the user hasn't set them
+  # already
+  append_alluxio_property alluxio.master.hostname ${master}
+  append_alluxio_property alluxio.master.journal.type UFS
+  append_alluxio_property alluxio.master.mount.table.root.ufs ${root_ufs_uri}
+  append_alluxio_property alluxio.master.security.impersonation.hive.users "*"
+  append_alluxio_property alluxio.master.security.impersonation.presto.users "*"
+  append_alluxio_property alluxio.master.security.impersonation.yarn.users "*"
+  append_alluxio_property alluxio.worker.memory.size "20GB"
+  append_alluxio_property alluxio.worker.tieredstore.level0.alias MEM
+  append_alluxio_property alluxio.worker.tieredstore.level0.dirs.path /mnt/ramdisk
+  append_alluxio_property alluxio.worker.tieredstore.levels 1
+
+  # Alluxio can't rely on SSH to start services (i.e. no alluxio-start.sh all)
+  if [[ ${IS_MASTER} = "true" ]]
+  then
+    doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a master"
+    doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a job_master"
+    doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a proxy"
+  else
+    ${ALLUXIO_HOME}/bin/alluxio-mount.sh SudoMount local
+    until ${ALLUXIO_HOME}/bin/alluxio fsadmin report
+    do
+      sleep 5
+    done
+    doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a worker"
+    doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a job_worker"
+    doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a proxy"
+  fi
+
+  # Compute application configs
+  doas alluxio "ln -s ${ALLUXIO_HOME}/client/*client.jar ${ALLUXIO_HOME}/client/alluxio-client.jar"
+  sudo mkdir -p /usr/lib/spark/jars/
+  sudo ln -s ${ALLUXIO_HOME}/client/alluxio-client.jar /usr/lib/spark/jars/alluxio-client.jar
+  sudo mkdir -p /usr/lib/presto/plugin/hive-hadoop2/
+  sudo ln -s ${ALLUXIO_HOME}/client/alluxio-client.jar /usr/lib/presto/plugin/hive-hadoop2/alluxio-client.jar
+}
+
+"$@"
