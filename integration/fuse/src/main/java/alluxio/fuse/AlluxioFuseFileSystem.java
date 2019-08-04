434dad6f82718223b4cf2be287eece4932d8ac57
liuhongtong <hongtongliu@126.com>
2019-08-04 12:59:22 +0800

    delete unused import
    Signed-off-by: liuhongtong <hongtongliu@126.com>

--- integration/fuse/src/main/java/alluxio/fuse/AlluxioFuseFileSystem.java
+++ integration/fuse/src/main/java/alluxio/fuse/AlluxioFuseFileSystem.java
@@ -65,10 +65,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.concurrent.ThreadSafe;
