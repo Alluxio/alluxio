dc535fdfb1c8dbe4e4c4164e21be1468fcd806c3
madanadit <adit@alluxio.com>
2019-08-07 17:48:56 -0700

    Fix style

--- core/common/src/main/java/alluxio/util/network/NetworkAddressUtils.java
+++ core/common/src/main/java/alluxio/util/network/NetworkAddressUtils.java
@@ -236,7 +236,8 @@ public final class NetworkAddressUtils {
    */
   public static InetSocketAddress getConnectAddress(ServiceType service,
       AlluxioConfiguration conf) {
-    return InetSocketAddress.createUnresolved(getConnectHost(service, conf), getPort(service, conf));
+    return InetSocketAddress.createUnresolved(getConnectHost(service, conf),
+        getPort(service, conf));
   }
 
   /**
