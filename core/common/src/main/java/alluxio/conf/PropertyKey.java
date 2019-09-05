9d7338cb5806222fc3c31d252d29781068a18ff4
Bin Fan <fanbin103@gmail.com>
2019-09-05 01:44:00 -0700

    Reduce the default block size to 64 MB from 512 MB
    Fix https://github.com/Alluxio/alluxio/issues/9394 by reducing block
    size to 32 MB
    
    pr-link: Alluxio/alluxio#9768
    change-id: cid-5418b9c5447bc1d0b701cb5b50dda149afb9a400

--- core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -2805,7 +2805,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .build();
   public static final PropertyKey USER_BLOCK_SIZE_BYTES_DEFAULT =
       new Builder(Name.USER_BLOCK_SIZE_BYTES_DEFAULT)
-          .setDefaultValue("512MB")
+          .setDefaultValue("64MB")
           .setDescription("Default block size for Alluxio files.")
           .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
           .setScope(Scope.CLIENT)
