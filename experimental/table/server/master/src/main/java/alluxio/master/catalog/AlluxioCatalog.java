a58a327be783352d8b78efc27ca7c83bab38cef1
David Zhu <david@alluxio.com>
2019-09-11 17:25:12 -0700

    add filtering logic

--- experimental/table/server/master/src/main/java/alluxio/master/catalog/AlluxioCatalog.java
+++ experimental/table/server/master/src/main/java/alluxio/master/catalog/AlluxioCatalog.java
@@ -18,11 +18,14 @@ import alluxio.grpc.AllOrNoneSet;
 import alluxio.grpc.Constraint;
 import alluxio.grpc.Domain;
 import alluxio.grpc.FieldSchema;
+import alluxio.grpc.FieldTypeId;
 import alluxio.grpc.FileStatistics;
 import alluxio.grpc.PartitionInfo;
 import alluxio.table.common.udb.UdbContext;
 import alluxio.table.common.udb.UnderDatabaseRegistry;
 
+import com.google.common.base.Preconditions;
+import org.apache.iceberg.types.Types;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,6 +36,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+
+import static alluxio.grpc.FieldTypeId.BOOLEAN;
 
 // TODO(yuzhu): journal the state of the catalog
 /**
@@ -169,14 +174,13 @@ public class AlluxioCatalog {
       Constraint constraint) throws IOException {
     Table table = getTable(dbName, tableName);
     List<FieldSchema> cols = table.get().getPartitionKeys();
-    List<String> colNames = cols.stream().map(FieldSchema::getName).collect(Collectors.toList());
 
     List<PartitionInfo> parts = table.get().getPartitionInfo();
 
-    Map<String, Domain> partitionConstraints = new LinkedHashMap<>(); //maintain insertion order
+    Map<FieldSchema, Domain> partitionConstraints = new LinkedHashMap<>(); //maintain insertion order
 
-    for (String col : colNames) {
-      Domain domain = constraint.getColumnConstraintsMap().get(col);
+    for (FieldSchema col : cols) {
+      Domain domain = constraint.getColumnConstraintsMap().get(col.getName());
       if (domain != null) {
         partitionConstraints.put(col, domain);
       } else {
@@ -197,7 +201,38 @@ public class AlluxioCatalog {
     return returnList;
   }
 
-  private boolean checkDomain(PartitionInfo partitionInfo, Map<String, Domain> constraints) {
+  private boolean checkDomain(String value, FieldSchema schema, Domain constraint) {
+    Comparable object;
+    // TODO(yuzhu): handle more complex data types
+    switch (schema.getType().getType()) {
+      case BOOLEAN:
+        object = Boolean.valueOf(value);
+        break;
+      case INTEGER:
+        object = Integer.valueOf(value);
+        break;
+      case STRING:
+        object = value;
+        break;
+      case DOUBLE:
+        object = value;
+        break;
+      default:
+        return false;
+    }
+    return alluxio.master.catalog.Domain.parseFrom(constraint).isInDomain(object);
+  }
+
+  private boolean checkDomain(PartitionInfo partitionInfo, Map<FieldSchema, Domain> constraints) {
+    Preconditions.checkArgument(constraints.size() == partitionInfo.getValuesList().size(),
+        "partition key size is not the same as constraint size");
+    int index = 0;
+    for (Map.Entry<FieldSchema, Domain> entry : constraints.entrySet()) {
+      String val = partitionInfo.getValuesList().get(index++);
+      if (!checkDomain(val, entry.getKey(), entry.getValue())) {
+        return false;
+      }
+    }
     return true;
   }
 }
