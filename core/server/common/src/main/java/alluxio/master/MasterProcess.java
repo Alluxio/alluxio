6b428b3559d5cbe4ab8d98c4bb63520ceb795e8e
Gene Pang <gene.pang@gmail.com>
2019-09-14 21:57:12 -0700

    Fix checkstyle

--- core/server/common/src/main/java/alluxio/master/MasterProcess.java
+++ core/server/common/src/main/java/alluxio/master/MasterProcess.java
@@ -32,9 +32,7 @@ import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
