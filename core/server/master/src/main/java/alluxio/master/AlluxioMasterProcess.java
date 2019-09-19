1a9b7c9bfdaef679b1110ac6db35e4e1ab37de47
tianrenz2 <tianrenz@uci.edu>
2019-09-19 08:41:24 +0800

    Improve the error message for configuration of the Primary selection

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
@@ -380,7 +380,7 @@ public class AlluxioMasterProcess extends MasterProcess {
           .setLocation(journalLocation).build(ProcessType.MASTER);
       if (ServerConfiguration.getBoolean(PropertyKey.ZOOKEEPER_ENABLED)) {
         Preconditions.checkState(!(journalSystem instanceof RaftJournalSystem),
-            "Raft journal cannot be used with Zookeeper enabled");
+            "Raft and Zookeeper cannot be configured at the same time");
         PrimarySelector primarySelector = PrimarySelector.Factory.createZkPrimarySelector();
         return new FaultTolerantAlluxioMasterProcess(journalSystem, primarySelector);
       } else if (journalSystem instanceof RaftJournalSystem) {
