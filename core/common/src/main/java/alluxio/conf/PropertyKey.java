20265a484722153343d9843d0945174d62b7228a
Göktürk Gezer <gokturk.gezer@gmail.com>
2019-09-20 08:20:41 -0700

    doc fix
    Co-Authored-By: Gene Pang <gene.pang@gmail.com>

--- core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -1292,7 +1292,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
   public static final PropertyKey MASTER_EMBEDDED_JOURNAL_APPENDER_BATCH_SIZE_BYTES =
       new Builder(Name.MASTER_EMBEDDED_JOURNAL_APPENDER_BATCH_SIZE_BYTES)
           .setDescription("Amount of data that is appended from leader to followers "
-              + "in a single heartbeat. Setting higher values might require increasing"
+              + "in a single heartbeat. Setting higher values might require increasing "
               + "election timeout due to increased network delay. Setting lower values "
               + "might stall knowledge propagation between the leader and followers.")
           .setDefaultValue("32KB")
