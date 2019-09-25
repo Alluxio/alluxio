1b4511d220c28db603b78268e8bdaa3d4c573b27
Gene Pang <gene.pang@gmail.com>
2019-09-25 00:17:59 -0700

    Reduce intervals for faster persist tests

--- tests/src/test/java/alluxio/client/cli/fs/AbstractShellIntegrationTest.java
+++ tests/src/test/java/alluxio/client/cli/fs/AbstractShellIntegrationTest.java
@@ -32,6 +32,9 @@ public abstract class AbstractShellIntegrationTest extends BaseIntegrationTest {
   @Rule
   public LocalAlluxioClusterResource mLocalAlluxioClusterResource =
       new LocalAlluxioClusterResource.Builder()
+          .setProperty(PropertyKey.MASTER_PERSISTENCE_CHECKER_INTERVAL_MS, "10ms")
+          .setProperty(PropertyKey.MASTER_PERSISTENCE_SCHEDULER_INTERVAL_MS, "10ms")
+          .setProperty(PropertyKey.JOB_MASTER_WORKER_HEARTBEAT_INTERVAL, "200ms")
           .setProperty(PropertyKey.WORKER_MEMORY_SIZE, SIZE_BYTES)
           .setProperty(PropertyKey.USER_BLOCK_SIZE_BYTES_DEFAULT, SIZE_BYTES)
           .setProperty(PropertyKey.MASTER_TTL_CHECKER_INTERVAL_MS, Integer.MAX_VALUE)
