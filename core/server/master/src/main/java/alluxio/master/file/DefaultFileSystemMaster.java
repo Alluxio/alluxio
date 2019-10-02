f56f80dab3d64cdfb3bccd24dbcc13e7506b86e3
Calvin Jia <jia.calvin@gmail.com>
2019-10-01 17:56:55 -0700

    Remove from persist jobs synchronously
    There is a case when `handleSuccess` is expensive (for example for
    object stores). This leads to a large number of `handleSuccess` calls
    being queued up, possibly conflicting with each other and consuming the
    threadpool / queue.
    
    I validated it with S3, but unfortunately it is nontrivial to make a
    test for this case.
    
    Fixes #9925
    
    pr-link: Alluxio/alluxio#10015
    change-id: cid-18f9fd3c412190c3c67d806cd77f84b31294bbc3

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -4180,8 +4180,6 @@ public final class DefaultFileSystemMaster extends CoreMaster implements FileSys
           }
         }
         mPersistRequests.put(fileId, job.getTimer());
-      } finally {
-        mPersistJobs.remove(fileId);
       }
 
       // Cleanup possible staging UFS blocks files due to fast durable write fallback.
@@ -4264,6 +4262,7 @@ public final class DefaultFileSystemMaster extends CoreMaster implements FileSys
               mPersistJobs.remove(fileId);
               break;
             case COMPLETED:
+              mPersistJobs.remove(fileId);
               mPersistCheckerPool.execute(() -> handleSuccess(job));
               break;
             default:
