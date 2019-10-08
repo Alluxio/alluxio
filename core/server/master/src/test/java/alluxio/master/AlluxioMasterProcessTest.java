Alluxio/alluxio

commit 0455c219fcbf716c83025f59276c5de4bd002b7e

Move UfsManager to CoreMasterContext
Fix https://github.com/Alluxio/alluxio/issues/9240

This moves provides `UfsManager` to master context so it can be shared
across different masters. In this way, `DefaultMetaMaster` no longer
needs to call UFS Factory to make UFS instances but consolidating to
`UfsManager`

pr-link: Alluxio/alluxio#9808
change-id: cid-8cfd82ccab60e8f9082013583e52beb8d47dd04f

core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
@@ -30,12 +30,12 @@ import alluxio.master.journal.raft.RaftJournalSystem;
 import alluxio.metrics.MetricsSystem;
 import alluxio.metrics.sink.MetricsServlet;
 import alluxio.metrics.sink.PrometheusMetricsServlet;
+import alluxio.resource.CloseableResource;
 import alluxio.security.user.ServerUserState;
+import alluxio.underfs.MasterUfsManager;
 import alluxio.underfs.UnderFileSystem;
-import alluxio.underfs.UnderFileSystemConfiguration;
 import alluxio.util.CommonUtils.ProcessType;
 import alluxio.util.JvmPauseMonitor;
-import alluxio.util.URIUtils;
 import alluxio.util.network.NetworkAddressUtils;
 import alluxio.web.MasterWebServer;
 
@@ -87,6 +87,9 @@ public class AlluxioMasterProcess extends MasterProcess {
   /** The manager for creating and restoring backups. */
   private final BackupManager mBackupManager;
 
+  /** The manager of all ufs. */
+  private final MasterUfsManager mUfsManager;
+
   private ExecutorService mRPCExecutor = null;
 
   /**
@@ -106,6 +109,7 @@ public class AlluxioMasterProcess extends MasterProcess {
       mSafeModeManager = new DefaultSafeModeManager();
       mBackupManager = new BackupManager(mRegistry);
       String baseDir = ServerConfiguration.get(PropertyKey.MASTER_METASTORE_DIR);
+      mUfsManager = new MasterUfsManager();
       MasterContext context = CoreMasterContext.newBuilder()
           .setJournalSystem(mJournalSystem)
           .setSafeModeManager(mSafeModeManager)
@@ -115,6 +119,7 @@ public class AlluxioMasterProcess extends MasterProcess {
           .setStartTimeMs(mStartTimeMs)
           .setPort(NetworkAddressUtils
               .getPort(ServiceType.MASTER_RPC, ServerConfiguration.global()))
+          .setUfsManager(mUfsManager)
           .build();
       mPauseStateLock = context.pauseStateLock();
       MasterUtils.createMasters(mRegistry, context);
@@ -168,15 +173,8 @@ public class AlluxioMasterProcess extends MasterProcess {
   }
 
   private void initFromBackup(AlluxioURI backup) throws IOException {
-    UnderFileSystem ufs;
-    if (URIUtils.isLocalFilesystem(backup.toString())) {
-      ufs = UnderFileSystem.Factory.create("/",
-          UnderFileSystemConfiguration.defaults(ServerConfiguration.global()));
-    } else {
-      ufs = UnderFileSystem.Factory.createForRoot(ServerConfiguration.global());
-    }
-    try (UnderFileSystem closeUfs = ufs;
-        InputStream ufsIn = ufs.open(backup.getPath())) {
+    try (CloseableResource<UnderFileSystem> closeUfs = mUfsManager.getRoot().acquireUfsResource();
+         InputStream ufsIn = closeUfs.get().open(backup.getPath())) {
       LOG.info("Initializing metadata from backup {}", backup);
       mBackupManager.initFromBackup(ufsIn);
     }

core/server/master/src/main/java/alluxio/master/AlluxioSecondaryMaster.java
@@ -18,6 +18,7 @@ import alluxio.conf.PropertyKey;
 import alluxio.conf.ServerConfiguration;
 import alluxio.master.journal.JournalSystem;
 import alluxio.master.journal.JournalUtils;
+import alluxio.underfs.MasterUfsManager;
 import alluxio.util.CommonUtils;
 import alluxio.util.CommonUtils.ProcessType;
 import alluxio.util.WaitForOptions;
@@ -71,6 +72,7 @@ public final class AlluxioSecondaryMaster implements Process {
           .setInodeStoreFactory(MasterUtils.getInodeStoreFactory(baseDir))
           .setStartTimeMs(mStartTimeMs)
           .setPort(mPort)
+          .setUfsManager(new MasterUfsManager())
           .build());
       // Check that journals of each service have been formatted.
       if (!mJournalSystem.isFormatted()) {

core/server/master/src/main/java/alluxio/master/CoreMasterContext.java
@@ -15,6 +15,7 @@ import alluxio.master.journal.JournalSystem;
 import alluxio.master.metastore.BlockStore;
 import alluxio.master.metastore.InodeStore;
 import alluxio.security.user.UserState;
+import alluxio.underfs.MasterUfsManager;
 
 import com.google.common.base.Preconditions;
 
@@ -27,6 +28,7 @@ public class CoreMasterContext extends MasterContext {
   private final BlockStore.Factory mBlockStoreFactory;
   private final InodeStore.Factory mInodeStoreFactory;
   private final JournalSystem mJournalSystem;
+  private final MasterUfsManager mUfsManager;
   private final long mStartTimeMs;
   private final int mPort;
 
@@ -41,6 +43,7 @@ public class CoreMasterContext extends MasterContext {
         Preconditions.checkNotNull(builder.mInodeStoreFactory, "inodeStoreFactory");
     mJournalSystem = Preconditions.checkNotNull(builder.mJournalSystem, "journalSystem");
     mStartTimeMs = builder.mStartTimeMs;
+    mUfsManager = builder.mUfsManager;
     mPort = builder.mPort;
   }
 
@@ -93,6 +96,13 @@ public class CoreMasterContext extends MasterContext {
   }
 
   /**
+   * @return the UFS manager
+   */
+  public MasterUfsManager getUfsManager() {
+    return mUfsManager;
+  }
+
+  /**
    * @return a new builder
    */
   public static Builder newBuilder() {
@@ -109,6 +119,7 @@ public class CoreMasterContext extends MasterContext {
     private BackupManager mBackupManager;
     private BlockStore.Factory mBlockStoreFactory;
     private InodeStore.Factory mInodeStoreFactory;
+    private MasterUfsManager mUfsManager;
     private long mStartTimeMs;
     private int mPort;
 
@@ -181,6 +192,15 @@ public class CoreMasterContext extends MasterContext {
      */
     public Builder setPort(int port) {
       mPort = port;
+      return this;
+    }
+
+    /**
+     * @param ufsManager ufsManager
+     * @return the builder
+     */
+    public Builder setUfsManager(MasterUfsManager ufsManager) {
+      mUfsManager = ufsManager;
       return this;
     }
 

core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -413,7 +413,7 @@ public final class DefaultFileSystemMaster extends CoreMaster implements FileSys
 
     mBlockMaster = blockMaster;
     mDirectoryIdGenerator = new InodeDirectoryIdGenerator(mBlockMaster);
-    mUfsManager = new MasterUfsManager();
+    mUfsManager = masterContext.getUfsManager();
     mMountTable = new MountTable(mUfsManager, getRootMountInfo(mUfsManager));
     mInodeLockManager = new InodeLockManager();
     InodeStore inodeStore = masterContext.getInodeStoreFactory().apply(mInodeLockManager);

core/server/master/src/main/java/alluxio/master/file/activesync/ActiveSyncer.java
@@ -27,11 +27,12 @@ import com.google.common.base.Throwables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.concurrent.NotThreadSafe;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import javax.annotation.concurrent.NotThreadSafe;
 
 /**
  * Periodically sync the files for a particular mountpoint.

core/server/master/src/main/java/alluxio/master/meta/DailyMetadataBackup.java
@@ -16,6 +16,8 @@ import alluxio.conf.PropertyKey;
 import alluxio.conf.ServerConfiguration;
 import alluxio.grpc.BackupPOptions;
 import alluxio.master.BackupManager;
+import alluxio.resource.CloseableResource;
+import alluxio.underfs.UfsManager;
 import alluxio.underfs.UfsStatus;
 import alluxio.underfs.UnderFileSystem;
 import alluxio.util.CommonUtils;
@@ -51,7 +53,7 @@ public final class DailyMetadataBackup {
   private final MetaMaster mMetaMaster;
   private final int mRetainedFiles;
   private final ScheduledExecutorService mScheduledExecutor;
-  private final UnderFileSystem mUfs;
+  private final UfsManager mUfsManager;
 
   private ScheduledFuture<?> mBackup;
 
@@ -60,16 +62,19 @@ public final class DailyMetadataBackup {
    *
    * @param metaMaster the meta master
    * @param service a scheduled executor service
-   * @param ufs the under file system
+   * @param ufsManager the under file system Manager
    */
   DailyMetadataBackup(MetaMaster metaMaster,
-      ScheduledExecutorService service, UnderFileSystem ufs) {
+      ScheduledExecutorService service, UfsManager ufsManager) {
     mMetaMaster = metaMaster;
     mBackupDir = ServerConfiguration.get(PropertyKey.MASTER_BACKUP_DIRECTORY);
     mRetainedFiles = ServerConfiguration.getInt(PropertyKey.MASTER_DAILY_BACKUP_FILES_RETAINED);
     mScheduledExecutor = service;
-    mUfs = ufs;
-    mIsLocal = ufs.getUnderFSType().equals("local");
+    mUfsManager = ufsManager;
+    try (CloseableResource<UnderFileSystem> ufsResource =
+             mUfsManager.getRoot().acquireUfsResource()) {
+      mIsLocal = ufsResource.get().getUnderFSType().equals("local");
+    }
   }
 
   /**
@@ -133,34 +138,38 @@ public final class DailyMetadataBackup {
    * Deletes stale backup files to avoid consuming too many spaces.
    */
   private void deleteStaleBackups() throws Exception {
-    UfsStatus[] statuses = mUfs.listStatus(mBackupDir);
-    if (statuses.length <= mRetainedFiles) {
-      return;
-    }
+    try (CloseableResource<UnderFileSystem> ufsResource =
+             mUfsManager.getRoot().acquireUfsResource()) {
+      UnderFileSystem ufs = ufsResource.get();
+      UfsStatus[] statuses = ufs.listStatus(mBackupDir);
+      if (statuses.length <= mRetainedFiles) {
+        return;
+      }
 
-    // Sort the backup files according to create time from oldest to newest
-    TreeMap<Instant, String> timeToFile = new TreeMap<>((a, b) -> (
-        a.isBefore(b) ? -1 : a.isAfter(b) ? 1 : 0));
-    for (UfsStatus status : statuses) {
-      if (status.isFile()) {
-        Matcher matcher = BackupManager.BACKUP_FILE_PATTERN.matcher(status.getName());
-        if (matcher.matches()) {
-          timeToFile.put(Instant.ofEpochMilli(Long.parseLong(matcher.group(1))),
-              status.getName());
+      // Sort the backup files according to create time from oldest to newest
+      TreeMap<Instant, String> timeToFile = new TreeMap<>((a, b) -> (
+          a.isBefore(b) ? -1 : a.isAfter(b) ? 1 : 0));
+      for (UfsStatus status : statuses) {
+        if (status.isFile()) {
+          Matcher matcher = BackupManager.BACKUP_FILE_PATTERN.matcher(status.getName());
+          if (matcher.matches()) {
+            timeToFile.put(Instant.ofEpochMilli(Long.parseLong(matcher.group(1))),
+                status.getName());
+          }
         }
       }
-    }
 
-    int toDeleteFileNum = timeToFile.size() - mRetainedFiles;
-    if (toDeleteFileNum <= 0) {
-      return;
+      int toDeleteFileNum = timeToFile.size() - mRetainedFiles;
+      if (toDeleteFileNum <= 0) {
+        return;
+      }
+      for (int i = 0; i < toDeleteFileNum; i++) {
+        String toDeleteFile = PathUtils.concatPath(mBackupDir,
+            timeToFile.pollFirstEntry().getValue());
+        ufs.deleteExistingFile(toDeleteFile);
+      }
+      LOG.info("Deleted {} stale metadata backup files at {}", toDeleteFileNum, mBackupDir);
     }
-    for (int i = 0; i < toDeleteFileNum; i++) {
-      String toDeleteFile = PathUtils.concatPath(mBackupDir,
-          timeToFile.pollFirstEntry().getValue());
-      mUfs.deleteExistingFile(toDeleteFile);
-    }
-    LOG.info("Deleted {} stale metadata backup files at {}", toDeleteFileNum, mBackupDir);
   }
 
   /**

core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
@@ -47,14 +47,14 @@ import alluxio.master.meta.checkconf.ServerConfigurationChecker;
 import alluxio.master.meta.checkconf.ServerConfigurationStore;
 import alluxio.proto.journal.Journal;
 import alluxio.proto.journal.Meta;
+import alluxio.resource.CloseableResource;
 import alluxio.resource.LockResource;
+import alluxio.underfs.UfsManager;
 import alluxio.underfs.UnderFileSystem;
-import alluxio.underfs.UnderFileSystemConfiguration;
 import alluxio.underfs.options.MkdirsOptions;
 import alluxio.util.ConfigurationUtils;
 import alluxio.util.IdUtils;
 import alluxio.util.ThreadFactoryUtils;
-import alluxio.util.URIUtils;
 import alluxio.util.executor.ExecutorServiceFactories;
 import alluxio.util.executor.ExecutorServiceFactory;
 import alluxio.util.io.PathUtils;
@@ -145,8 +145,8 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
   /** The address of this master. */
   private Address mMasterAddress;
 
-  /** The root ufs. */
-  private final UnderFileSystem mUfs;
+  /** The manager of all ufs. */
+  private final UfsManager mUfsManager;
 
   /** The metadata daily backup. */
   private DailyMetadataBackup mDailyBackup;
@@ -247,13 +247,7 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
     mBlockMaster.registerWorkerLostListener(mWorkerConfigStore::handleNodeLost);
     mBlockMaster.registerNewWorkerConfListener(mWorkerConfigStore::registerNewConf);
 
-    if (URIUtils.isLocalFilesystem(ServerConfiguration
-        .get(PropertyKey.MASTER_MOUNT_TABLE_ROOT_UFS))) {
-      mUfs = UnderFileSystem.Factory
-          .create("/", UnderFileSystemConfiguration.defaults(ServerConfiguration.global()));
-    } else {
-      mUfs = UnderFileSystem.Factory.createForRoot(ServerConfiguration.global());
-    }
+    mUfsManager = masterContext.getUfsManager();
 
     mPathProperties = new PathProperties();
     mState = new State();
@@ -306,7 +300,7 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
 
       if (ServerConfiguration.getBoolean(PropertyKey.MASTER_DAILY_BACKUP_ENABLED)) {
         mDailyBackup = new DailyMetadataBackup(this, Executors.newSingleThreadScheduledExecutor(
-            ThreadFactoryUtils.build("DailyMetadataBackup-%d", true)), mUfs);
+            ThreadFactoryUtils.build("DailyMetadataBackup-%d", true)), mUfsManager);
         mDailyBackup.start();
       }
       if (mState.getClusterID() == INVALID_CLUSTER_ID) {
@@ -362,53 +356,49 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
   public BackupResponse backup(BackupPOptions options) throws IOException {
     String dir = options.hasTargetDirectory() ? options.getTargetDirectory()
         : ServerConfiguration.get(PropertyKey.MASTER_BACKUP_DIRECTORY);
-    UnderFileSystem ufs = mUfs;
-    if ((options.getLocalFileSystem() || !options.hasTargetDirectory())
-            && !ufs.getUnderFSType().equals("local")) {
-      ufs = UnderFileSystem.Factory.create("/",
-          UnderFileSystemConfiguration.defaults(ServerConfiguration.global()));
-      LOG.info("Backing up to local filesystem in directory {}", dir);
-    } else {
-      LOG.info("Backing up to root UFS in directory {}", dir);
-    }
-    if (!ufs.isDirectory(dir)) {
-      if (!ufs.mkdirs(dir, MkdirsOptions.defaults(ServerConfiguration.global())
-          .setCreateParent(true))) {
-        throw new IOException(String.format("Failed to create directory %s", dir));
-      }
-    }
-    String backupFilePath;
-    AtomicLong entryCount = new AtomicLong(0);
-    try (LockResource lr = new LockResource(mMasterContext.pauseStateLock())) {
-      Instant now = Instant.now();
-      String backupFileName = String.format(BackupManager.BACKUP_FILE_FORMAT,
-          DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.of("UTC")).format(now),
-          now.toEpochMilli());
-      backupFilePath = PathUtils.concatPath(dir, backupFileName);
-      try {
-        try (OutputStream ufsStream = ufs.create(backupFilePath)) {
-          mBackupManager.backup(ufsStream, entryCount);
+    try (CloseableResource<UnderFileSystem> ufsResource =
+             mUfsManager.getRoot().acquireUfsResource()) {
+      UnderFileSystem ufs = ufsResource.get();
+      if (!ufs.isDirectory(dir)) {
+        if (!ufs.mkdirs(dir, MkdirsOptions.defaults(ServerConfiguration.global())
+            .setCreateParent(true))) {
+          throw new IOException(String.format("Failed to create directory %s", dir));
         }
-      } catch (Throwable t) {
+      }
+      String backupFilePath;
+      AtomicLong entryCount = new AtomicLong(0);
+      try (LockResource lr = new LockResource(mMasterContext.pauseStateLock())) {
+        Instant now = Instant.now();
+        String backupFileName = String.format(BackupManager.BACKUP_FILE_FORMAT,
+            DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.of("UTC")).format(now),
+            now.toEpochMilli());
+        backupFilePath = PathUtils.concatPath(dir, backupFileName);
         try {
-          ufs.deleteExistingFile(backupFilePath);
-        } catch (Throwable t2) {
-          LOG.error("Failed to clean up failed backup at {}", backupFilePath, t2);
-          t.addSuppressed(t2);
+          try (OutputStream ufsStream = ufs.create(backupFilePath)) {
+            mBackupManager.backup(ufsStream, entryCount);
+          }
+        } catch (Throwable t) {
+          try {
+            ufs.deleteExistingFile(backupFilePath);
+          } catch (Throwable t2) {
+            LOG.error("Failed to clean up failed backup at {}", backupFilePath, t2);
+            t.addSuppressed(t2);
+          }
+          throw t;
         }
-        throw t;
       }
+      String rootUfs = ServerConfiguration.get(PropertyKey.MASTER_MOUNT_TABLE_ROOT_UFS);
+      if (options.getLocalFileSystem()) {
+        rootUfs = "file:///";
+      }
+      AlluxioURI backupUri =
+          new AlluxioURI(new AlluxioURI(rootUfs), new AlluxioURI(backupFilePath));
+      return new BackupResponse(
+          backupUri,
+          NetworkAddressUtils.getConnectHost(NetworkAddressUtils.ServiceType.MASTER_RPC,
+              ServerConfiguration.global()),
+          entryCount.get());
     }
-    String rootUfs = ServerConfiguration.get(PropertyKey.MASTER_MOUNT_TABLE_ROOT_UFS);
-    if (options.getLocalFileSystem()) {
-      rootUfs = "file:///";
-    }
-    AlluxioURI backupUri = new AlluxioURI(new AlluxioURI(rootUfs), new AlluxioURI(backupFilePath));
-    return new BackupResponse(
-        backupUri,
-        NetworkAddressUtils.getConnectHost(NetworkAddressUtils.ServiceType.MASTER_RPC,
-            ServerConfiguration.global()),
-        entryCount.get());
   }
 
   @Override

core/server/master/src/test/java/alluxio/master/MasterTestUtils.java
@@ -18,6 +18,7 @@ import alluxio.master.journal.noop.NoopJournalSystem;
 import alluxio.master.metastore.heap.HeapBlockStore;
 import alluxio.master.metastore.heap.HeapInodeStore;
 import alluxio.security.user.UserState;
+import alluxio.underfs.MasterUfsManager;
 
 /**
  * Util methods to help with master testing.
@@ -55,6 +56,7 @@ public final class MasterTestUtils {
         .setInodeStoreFactory(x -> new HeapInodeStore())
         .setStartTimeMs(-1)
         .setPort(-1)
+        .setUfsManager(new MasterUfsManager())
         .build();
   }
 

commit b0729e98ee560ea4b9816dbefae583ba52308efe

Fix restore from backup on local path
There was a regression introduced that caused restoring from a
local backup file to stop working. Without this, all backups would
need to be stored in the root UFS which is non-ideal.

This PR fixes the regression and adds a test to ensure that we can
restore from backup when the root UFS is not local and the backup
path is on the local system.

- Regression originally introduced in #9808
- Continuation of PR #9972
- Fixes #9969

pr-link: Alluxio/alluxio#10046
change-id: cid-d158076c1e3d16238d72cf1bbc647ec639bf8c4e

core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
@@ -34,8 +34,10 @@ import alluxio.resource.CloseableResource;
 import alluxio.security.user.ServerUserState;
 import alluxio.underfs.MasterUfsManager;
 import alluxio.underfs.UnderFileSystem;
+import alluxio.underfs.UnderFileSystemConfiguration;
 import alluxio.util.CommonUtils.ProcessType;
 import alluxio.util.JvmPauseMonitor;
+import alluxio.util.URIUtils;
 import alluxio.util.network.NetworkAddressUtils;
 import alluxio.web.MasterWebServer;
 
@@ -173,7 +175,18 @@ public class AlluxioMasterProcess extends MasterProcess {
   }
 
   private void initFromBackup(AlluxioURI backup) throws IOException {
-    try (CloseableResource<UnderFileSystem> closeUfs = mUfsManager.getRoot().acquireUfsResource();
+    CloseableResource<UnderFileSystem> ufsResource;
+    if (URIUtils.isLocalFilesystem(backup.toString())) {
+      UnderFileSystem ufs = UnderFileSystem.Factory.create("/",
+          UnderFileSystemConfiguration.defaults(ServerConfiguration.global()));
+      ufsResource = new CloseableResource<UnderFileSystem>(ufs) {
+        @Override
+        public void close() { }
+      };
+    } else {
+      ufsResource = mUfsManager.getRoot().acquireUfsResource();
+    }
+    try (CloseableResource<UnderFileSystem> closeUfs = ufsResource;
          InputStream ufsIn = closeUfs.get().open(backup.getPath())) {
       LOG.info("Initializing metadata from backup {}", backup);
       mBackupManager.initFromBackup(ufsIn);

core/server/master/src/test/java/alluxio/master/AlluxioMasterProcessTest.java
@@ -17,11 +17,17 @@ import static org.junit.Assert.assertTrue;
 import alluxio.conf.PropertyKey;
 import alluxio.conf.ServerConfiguration;
 import alluxio.master.journal.noop.NoopJournalSystem;
+import alluxio.master.journal.raft.RaftJournalConfiguration;
+import alluxio.master.journal.raft.RaftJournalSystem;
 import alluxio.util.CommonUtils;
+import alluxio.util.io.FileUtils;
+import alluxio.util.io.PathUtils;
 import alluxio.util.network.NetworkAddressUtils;
 import alluxio.util.network.NetworkAddressUtils.ServiceType;
 
+import com.google.common.base.Preconditions;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -32,6 +38,7 @@ import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -52,6 +59,7 @@ public final class AlluxioMasterProcessTest {
 
   @Before
   public void before() {
+    ServerConfiguration.reset();
     mRpcPort = mRpcPortRule.getPort();
     mWebPort = mWebPortRule.getPort();
     ServerConfiguration.set(PropertyKey.MASTER_RPC_PORT, mRpcPort);
@@ -77,6 +85,43 @@ public final class AlluxioMasterProcessTest {
   public void startStopSecondary() throws Exception {
     FaultTolerantAlluxioMasterProcess master = new FaultTolerantAlluxioMasterProcess(
         new NoopJournalSystem(), new AlwaysSecondaryPrimarySelector());
+    Thread t = new Thread(() -> {
+      try {
+        master.start();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    });
+    t.start();
+    startStopTest(master);
+  }
+
+  /**
+   * This test ensures that given a root UFS which is <i>not</i> local, that we can still provide a
+   * local path to an Alluxio backup and start the master.
+   *
+   * This test is ignored because it requires adding a new UFS dependency in the test scope, but
+   * adding the alluxio-underfs-web dependency causes the module to hang in the  builds even after
+   * the test completes. The hang seems to be due to a bug in maven 3.5.4.
+   */
+  @Test
+  @Ignore
+  public void restoreFromBackupLocal() throws Exception {
+    URL backupResource = this.getClass().getResource("/alluxio-local-backup.gz");
+    Preconditions.checkNotNull(backupResource);
+    String backupPath = backupResource.toURI().toString();
+    String journalPath = PathUtils.concatPath(mFolder.getRoot(), "journal");
+    FileUtils.createDir(journalPath);
+    String ufsPath = PathUtils.concatPath(mFolder.getRoot(), "ufs");
+    FileUtils.createDir(ufsPath);
+    ufsPath = "http://other_ufs/";
+    ServerConfiguration.set(PropertyKey.MASTER_EMBEDDED_JOURNAL_ELECTION_TIMEOUT, "550");
+    ServerConfiguration.set(PropertyKey.MASTER_EMBEDDED_JOURNAL_HEARTBEAT_INTERVAL, "250ms");
+    ServerConfiguration.set(PropertyKey.MASTER_JOURNAL_INIT_FROM_BACKUP, backupPath);
+    ServerConfiguration.set(PropertyKey.MASTER_JOURNAL_FOLDER, journalPath);
+    ServerConfiguration.set(PropertyKey.MASTER_MOUNT_TABLE_ROOT_UFS, ufsPath);
+    AlluxioMasterProcess master = new AlluxioMasterProcess(
+        RaftJournalSystem.create(RaftJournalConfiguration.defaults(ServiceType.MASTER_RAFT)));
     Thread t = new Thread(() -> {
       try {
         master.start();