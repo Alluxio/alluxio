2d3bb0bfc0ed361e0efe5891ef88745a780326ae
Bradley Yoo <bradleyyoo@gmail.com>
2019-10-14 11:14:21 -0700

    Create JobServiceMetrics Command
    Currently just contains just the number of jobs with a particular Status
    
    @calvinjia: I don't know how to launch these jobs for proper end to end
    testing, what's the best way to do this?
    
    pr-link: Alluxio/alluxio#10063
    change-id: cid-58509a96bbde82a267dca496a16ba3306226913d

--- shell/src/main/java/alluxio/cli/fsadmin/command/AbstractFsAdminCommand.java
+++ shell/src/main/java/alluxio/cli/fsadmin/command/AbstractFsAdminCommand.java
@@ -14,6 +14,7 @@ package alluxio.cli.fsadmin.command;
 import alluxio.cli.Command;
 import alluxio.client.block.BlockMasterClient;
 import alluxio.client.file.FileSystemMasterClient;
+import alluxio.client.job.JobMasterClient;
 import alluxio.client.journal.JournalMasterClient;
 import alluxio.client.meta.MetaMasterClient;
 import alluxio.client.meta.MetaMasterConfigClient;
@@ -32,6 +33,7 @@ public abstract class AbstractFsAdminCommand implements Command {
   protected final PrintStream mPrintStream;
   protected final JournalMasterClient mMasterJournalMasterClient;
   protected final JournalMasterClient mJobMasterJournalMasterClient;
+  protected final JobMasterClient mJobMasterClient;
 
   protected AbstractFsAdminCommand(Context context) {
     mFsClient = context.getFsClient();
@@ -40,6 +42,7 @@ public abstract class AbstractFsAdminCommand implements Command {
     mMetaConfigClient = context.getMetaConfigClient();
     mMasterJournalMasterClient = context.getJournalMasterClientForMaster();
     mJobMasterJournalMasterClient = context.getJournalMasterClientForJobMaster();
+    mJobMasterClient = context.getJobMasterClient();
     mPrintStream = context.getPrintStream();
   }
 }
