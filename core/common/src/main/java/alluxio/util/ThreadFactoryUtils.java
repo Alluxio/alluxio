5e366e132000f9358e75541c28026e187991ee60
Bradley Yoo <bradleyyoo@gmail.com>
2019-10-16 11:24:30 -0700

    Javadoc

--- core/common/src/main/java/alluxio/util/ThreadFactoryUtils.java
+++ core/common/src/main/java/alluxio/util/ThreadFactoryUtils.java
@@ -37,6 +37,16 @@ public final class ThreadFactoryUtils {
     return build(nameFormat, isDaemon, null);
   }
 
+  /**
+   * Creates a {@link java.util.concurrent.ThreadFactory} that spawns off threads.
+   *
+   * @param nameFormat name pattern for each thread. should contain '%d' to distinguish between
+   *                   threads.
+   * @param isDaemon if true, the {@link java.util.concurrent.ThreadFactory} will create
+   *                 daemon threads.
+   * @param priority priority of the threads created
+   * @return the created factory
+   */
   public static ThreadFactory build(final String nameFormat, boolean isDaemon, Integer priority) {
     ThreadFactoryBuilder threadFactoryBuilder = new ThreadFactoryBuilder().setDaemon(isDaemon)
             .setNameFormat(nameFormat);
