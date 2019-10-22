b8860349861157b0f736d2d293597340792f6687
David Zhu <david@alluxio.com>
2019-10-22 14:30:11 -0700

    Use a TestDatabase factory instead of powermocks

---
+++ table/server/master/src/test/java/alluxio/master/table/TestDatabase.java
@@ -0,0 +1,81 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master.table;
+
+import alluxio.table.common.udb.UdbConfiguration;
+import alluxio.table.common.udb.UdbContext;
+import alluxio.table.common.udb.UdbTable;
+import alluxio.table.common.udb.UnderDatabase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A udb implementation which does nothing, used for testing.
+ */
+public class TestDatabase implements UnderDatabase {
+  public static final String DB_NAME = "test_udb_name";
+  public static final String TABLE_NAME_PREFIX = "test_table_name";
+  private static final TestDatabase DATABASE = new TestDatabase();
+
+  private Map<String, UdbTable> mUdbTables;
+
+  private TestDatabase() {
+    mUdbTables = new HashMap<>();
+  }
+
+  /**
+   * Creates an instance.
+   *
+   * @param udbContext the db context
+   * @param configuration the configuration
+   * @return the new instance
+   */
+  public static TestDatabase create(UdbContext udbContext,
+      UdbConfiguration configuration) {
+    return DATABASE;
+  }
+
+  @Override
+  public String getType() {
+    return TestUdbFactory.TYPE;
+  }
+
+  @Override
+  public String getName() {
+    return DB_NAME;
+  }
+
+  @Override
+  public List<String> getTableNames() throws IOException {
+    return new ArrayList<>(mUdbTables.keySet());
+  }
+
+  @Override
+  public UdbTable getTable(String tableName) throws IOException {
+    return mUdbTables.get(tableName);
+  }
+
+  public static String getTableName(int i) {
+    return TABLE_NAME_PREFIX + Integer.toString(i);
+  }
+
+  public static void genTable(int numOfTable) {
+    DATABASE.mUdbTables.clear();
+    for (int i = 0; i < numOfTable; i++) {
+      DATABASE.mUdbTables.put(getTableName(i), new TestUdbTable(DB_NAME, getTableName(i)));
+    }
+  }
+}
--- tests/src/test/java/alluxio/server/ft/journal/TableMasterJournalIntegrationTest.java
+++ tests/src/test/java/alluxio/server/ft/journal/TableMasterJournalIntegrationTest.java
@@ -14,91 +14,35 @@ package alluxio.server.ft.journal;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.powermock.api.mockito.PowerMockito.doNothing;
-import static org.powermock.api.mockito.PowerMockito.when;
 
-import alluxio.grpc.table.Layout;
-import alluxio.grpc.table.Schema;
-import alluxio.grpc.table.layout.hive.PartitionInfo;
-import alluxio.grpc.table.layout.hive.Storage;
 import alluxio.master.LocalAlluxioCluster;
-import alluxio.master.table.AlluxioCatalog;
 import alluxio.master.table.Table;
 import alluxio.master.table.TableMaster;
-import alluxio.table.common.layout.HiveLayout;
-import alluxio.table.common.udb.UdbConfiguration;
-import alluxio.table.common.udb.UdbContext;
-import alluxio.table.common.udb.UdbTable;
-import alluxio.table.common.udb.UnderDatabase;
-import alluxio.table.common.udb.UnderDatabaseRegistry;
-import alluxio.table.under.hive.HiveDatabaseFactory;
-import alluxio.table.under.hive.HivePartition;
+import alluxio.master.table.TestDatabase;
+import alluxio.master.table.TestUdbFactory;
 import alluxio.testutils.LocalAlluxioClusterResource;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * Integration tests for table master functionality.
  */
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore({"javax.crypto.*", "javax.security.*", "sun.security.*", "javax.net.ssl.*"})
-@PrepareForTest(AlluxioCatalog.class)
 public class TableMasterJournalIntegrationTest {
   @Rule
   public LocalAlluxioClusterResource mClusterResource =
       new LocalAlluxioClusterResource.Builder().setStartCluster(false)
           .build();
 
-  private static final String DB_NAME = "testdb";
-  private static final String TABLE_NAME_PREFIX = "testtable";
-
-  private void createMockUdbAndTable(int numOfTables) throws Exception {
-    UnderDatabase udb = PowerMockito.mock(UnderDatabase.class);
-    when(udb.getName()).thenReturn(DB_NAME);
-    String[] tableNames = new String[numOfTables];
-    Arrays.setAll(tableNames, i -> TABLE_NAME_PREFIX + Integer.toString(i));
-    when(udb.getTableNames()).thenReturn(Arrays.asList(tableNames));
-
-    for (String tableName : tableNames) {
-      UdbTable udbTable = PowerMockito.mock(UdbTable.class);
-      when(udbTable.getName()).thenReturn(tableName);
-      when(udbTable.getOwner()).thenReturn("test");
-      when(udbTable.getPartitionCols()).thenReturn(Collections.emptyList());
-
-      when(udbTable.getLayout()).thenReturn(Layout.getDefaultInstance());
-      when(udbTable.getParameters()).thenReturn(Collections.emptyMap());
-      when(udbTable.getSchema()).thenReturn(Schema.getDefaultInstance());
-      when(udbTable.getPartitions()).thenReturn(Arrays.asList(new HivePartition(
-          new HiveLayout(PartitionInfo.newBuilder().setDbName(DB_NAME)
-              .setTableName(tableName).setStorage(Storage.getDefaultInstance()).build(),
-              Collections.emptyList()))));
-      when(udb.getTable(tableName)).thenReturn(udbTable);
-    }
-
-    UnderDatabaseRegistry registry = PowerMockito.mock(UnderDatabaseRegistry.class);
-
-    doNothing().when(registry).refresh();
-    when(registry.create(any(UdbContext.class), anyString(), any(UdbConfiguration.class)))
-        .thenReturn(udb);
-    PowerMockito.whenNew(UnderDatabaseRegistry.class).withNoArguments().thenReturn(registry);
-  }
+  private static final String DB_NAME = TestDatabase.DB_NAME;
 
   @Before
   public void before() throws Exception {
-    createMockUdbAndTable(1);
+    TestDatabase.genTable(1);
   }
 
   @Test
@@ -108,14 +52,14 @@ public class TableMasterJournalIntegrationTest {
     TableMaster tableMaster =
         mCluster.getLocalAlluxioMaster().getMasterProcess().getMaster(TableMaster.class);
 
-    tableMaster.attachDatabase(DB_NAME, HiveDatabaseFactory.TYPE, Collections.emptyMap());
+    tableMaster.attachDatabase(DB_NAME, TestUdbFactory.TYPE, Collections.emptyMap());
     List<String> oldTableNames = tableMaster.getAllTables(DB_NAME);
     Table tableOld = tableMaster.getTable(DB_NAME, oldTableNames.get(0));
 
     mCluster.stopMasters();
 
     // Update Udb, the table should stay the same, until we detach / reattach
-    createMockUdbAndTable(2);
+    TestDatabase.genTable(2);
 
     mCluster.startMasters();
     TableMaster tableMasterRestart =
@@ -126,9 +70,10 @@ public class TableMasterJournalIntegrationTest {
     assertEquals(tableOld.getName(), tableNew.getName());
     tableMasterRestart.detachDatabase(DB_NAME);
     assertTrue(tableMasterRestart.getAllDatabases().isEmpty());
-    tableMasterRestart.attachDatabase(DB_NAME, HiveDatabaseFactory.TYPE, Collections.emptyMap());
+    tableMasterRestart.attachDatabase(DB_NAME, TestUdbFactory.TYPE, Collections.emptyMap());
 
     List<String> reattachedTableNames = tableMasterRestart.getAllTables(DB_NAME);
+    assertEquals(2, reattachedTableNames.size());
     assertNotEquals(oldTableNames, reattachedTableNames);
   }
 }
