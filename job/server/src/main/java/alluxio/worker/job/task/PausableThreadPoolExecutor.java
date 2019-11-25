33217aa5aba3a5a5d211082556ecdfeb3d4aa887
Bradley Yoo <bradleyyoo@gmail.com>
2019-11-25 15:25:43 -0800

    fix header

--- job/server/src/main/java/alluxio/worker/job/task/PausableThreadPoolExecutor.java
+++ job/server/src/main/java/alluxio/worker/job/task/PausableThreadPoolExecutor.java
@@ -1,7 +1,7 @@
 /*
  * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
  * (the "License"). You may not use this work except in compliance with the License, which is
- * available at www.apache.org/licenses/LICENSE2.0
+ * available at www.apache.org/licenses/LICENSE-2.0
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied, as more fully set forth in the License.
@@ -81,7 +81,7 @@ public class PausableThreadPoolExecutor extends ThreadPoolExecutor {
       mPauseLock.unlock();
     }
   }
-  
+
   @Override
   protected void beforeExecute(Thread t, Runnable r) {
     super.beforeExecute(t, r);
