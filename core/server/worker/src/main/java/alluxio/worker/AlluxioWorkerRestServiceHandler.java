d1295bb4f163a9a3e58fce15df4fe8d5f3d18dfb
Zac Blanco <zac@alluxio.com>
2019-12-11 18:16:58 -0800

    Populate worker webUI info when blocks are present
    Fixes #10611

--- core/server/worker/src/main/java/alluxio/worker/AlluxioWorkerRestServiceHandler.java
+++ core/server/worker/src/main/java/alluxio/worker/AlluxioWorkerRestServiceHandler.java
@@ -323,7 +323,9 @@ public final class AlluxioWorkerRestServiceHandler {
       try {
         int offset = Integer.parseInt(requestOffset);
         int limit = Integer.parseInt(requestLimit);
+        // make the limit equal to the total number of files if request limit is > than what is available
         limit = offset == 0 && limit > fileIds.size() ? fileIds.size() : limit;
+        // if offset+limit is greater than the size of the list, then simply return the max amount of files available, otherwise the original value
         limit = offset + limit > fileIds.size() ? fileIds.size() - offset : limit;
         int sum = Math.addExact(offset, limit);
         List<Long> subFileIds = fileIds.subList(offset, sum);
@@ -344,7 +346,7 @@ public final class AlluxioWorkerRestServiceHandler {
                     .addBlock(blockMeta.getBlockLocation().tierAlias(), blockId, blockSize, -1);
               }
             }
-            if (uiFileInfo.getBlockIds().isEmpty()) {
+            if (!uiFileInfo.getBlockIds().isEmpty()) {
               uiFileInfos.add(uiFileInfo);
             }
           } catch (Exception e) {
