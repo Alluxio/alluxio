5c4c32d193496433195c75bf2a1c0f60cd54857b
Zac Blanco <zac@alluxio.com>
2020-01-10 12:36:39 -0800

    Release clients back into pools during even during context reinit
    This removes a resource leak which may occur when a thread on a client
    is interrupted while performing any kind of operation.
    
    Because it's possible for a fs context re-initialization to occur while
    the resource is
    acquired this solution removes the `blockReinit` which throws a runtime
    exception during
    an interrupt and is the root cause of the resource leak.
    
    There are three different cases to which may occur during the release of
    the resource
    
    1. Release while the context is re-initializing
    - The original mBlockMasterClientPool or mFileSystemMasterClientPool
    may be null, closed, or overwritten with a difference pool. The inner
    class here saves
    the original pool reference used to acquire the client initially.
    Releasing into the closed pool
    is harmless.
    2. Release after the context has been re-initialized
    - Similar to the above scenario the original mBlockMasterClientPool or
    mFileSystemMasterClientPool are going to be using an entirely new pool.
    Since
    this method will save the original pool reference, this method would
    result in releasing
    into a closed pool which is harmless
    3. Release before any re-initialization
    - This is the normal case. There are no special considerations
    
    The issues mainly affected the FSMaster and BlockMaster clients, however
    upon inspection, BlockWorker clients have a similar issue, but seems to
    happen less frequently than the master clients. This PR also applies a
    similar strategy for BlockWorker clients to hold onto the references of
    the pool used during the acquire method to make a best-effort to release
    the resource when finished being used.
    
    
    Closes #9922 #10468
    
    pr-link: Alluxio/alluxio#10719
    change-id: cid-75c41bc4a3a83a86dd35473ea64c20936c4fc77c

--- core/client/fs/src/main/java/alluxio/client/block/stream/LocalFileDataReader.java
+++ core/client/fs/src/main/java/alluxio/client/block/stream/LocalFileDataReader.java
@@ -22,6 +22,7 @@ import alluxio.metrics.ClientMetrics;
 import alluxio.metrics.MetricsSystem;
 import alluxio.network.protocol.databuffer.DataBuffer;
 import alluxio.network.protocol.databuffer.NioDataBuffer;
+import alluxio.resource.CloseableResource;
 import alluxio.wire.WorkerNetAddress;
 import alluxio.worker.block.io.LocalFileBlockReader;
 
@@ -93,9 +94,7 @@ public final class LocalFileDataReader implements DataReader {
    */
   @NotThreadSafe
   public static class Factory implements DataReader.Factory {
-    private final BlockWorkerClient mBlockWorker;
-    private final FileSystemContext mContext;
-    private final WorkerNetAddress mAddress;
+    private final CloseableResource<BlockWorkerClient> mBlockWorker;
     private final long mBlockId;
     private final String mPath;
     private final long mLocalReaderChunkSize;
@@ -118,8 +117,6 @@ public final class LocalFileDataReader implements DataReader {
     public Factory(FileSystemContext context, WorkerNetAddress address, long blockId,
         long localReaderChunkSize, InStreamOptions options) throws IOException {
       AlluxioConfiguration conf = context.getClusterConf();
-      mContext = context;
-      mAddress = address;
       mBlockId = blockId;
       mLocalReaderChunkSize = localReaderChunkSize;
       mReadBufferSize = conf.getInt(PropertyKey.USER_NETWORK_READER_BUFFER_SIZE_MESSAGES);
@@ -131,7 +128,7 @@ public final class LocalFileDataReader implements DataReader {
 
       mBlockWorker = context.acquireBlockWorkerClient(address);
       try {
-        mStream = new GrpcBlockingStream<>(mBlockWorker::openLocalBlock, mReadBufferSize,
+        mStream = new GrpcBlockingStream<>(mBlockWorker.get()::openLocalBlock, mReadBufferSize,
             MoreObjects.toStringHelper(LocalFileDataReader.class)
                 .add("request", request)
                 .add("address", address)
@@ -141,7 +138,7 @@ public final class LocalFileDataReader implements DataReader {
         Preconditions.checkState(response.hasPath());
         mPath = response.getPath();
       } catch (Exception e) {
-        context.releaseBlockWorkerClient(address, mBlockWorker);
+        mBlockWorker.close();
         throw e;
       }
     }
@@ -174,7 +171,7 @@ public final class LocalFileDataReader implements DataReader {
         mStream.waitForComplete(mDataTimeoutMs);
       } finally {
         mClosed = true;
-        mContext.releaseBlockWorkerClient(mAddress, mBlockWorker);
+        mBlockWorker.close();
       }
     }
   }
