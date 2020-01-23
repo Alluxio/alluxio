03d333ea11dd3631461265d5ac5c1d5fb05e6dee
Adit Madan <adit@alluxio.com>
2020-01-22 23:32:10 -0800

    Add positioned readable HDFS ufs input stream
    When HDFS is remote and slow, we use the positioned read API instead of
    seek with this PR.
    
    HDFS FSDataInputStream seek may be implemented as read and discard. Over
    remote HDFS, this may increase the amount of data read over the network
    many folds based on the workload. Using positioned read, the amount of
    data requested from the HDFS client matches the amount of data read from
    the HDFS datanode.
    
    pr-link: Alluxio/alluxio#10763
    change-id: cid-e343a33f11ae2dc0b8ff64d25c09926e9fc9113d

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystem.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileSystem.java
@@ -563,13 +563,10 @@ public class HdfsUnderFileSystem extends ConsistentUnderFileSystem
     while (retryPolicy.attempt()) {
       try {
         FSDataInputStream inputStream = hdfs.open(new Path(path));
-        try {
-          inputStream.seek(options.getOffset());
-        } catch (IOException e) {
-          inputStream.close();
-          throw e;
-        }
-        return new HdfsUnderFileInputStream(inputStream);
+        // pread API instead of seek is more efficient for FSDataInputStream.
+        // A seek on FSDataInputStream uses a skip op which is implemented as read + discard
+        // and hence ends up reading extra data from the datanode.
+        return new HdfsPositionedUnderFileInputStream(inputStream, options.getOffset());
       } catch (IOException e) {
         LOG.warn("{} try to open {} : {}", retryPolicy.getAttemptCount(), path, e.getMessage());
         te = e;
