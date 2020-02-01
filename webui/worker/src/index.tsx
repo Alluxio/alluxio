5b001c9b69e3075bba2460c7f4f9ca92d1a846cb
Zac Blanco <zac@alluxio.com>
2020-01-31 17:49:36 -0800

    Replace Source Code Pro with Anonymous Pro font
    The reason I'm proposing replacing SCP with AP is because there is a
    bug in firefox and/or MacOS which causes this font to render
    incorrectly.
    
    See:
    - https://bugzilla.mozilla.org/show_bug.cgi?id=1534503
    - https://bugzilla.mozilla.org/show_bug.cgi?id=1520157
    
    Anonymous Pro is a bit more lightweight and has at least been recently
    updated by its maintainers (1.44.0 was 3 months ago). SCP is definitely
    more ubiquitous and well-maintained but I would rather have a font that
    works everywhere. As an added bonus, this font is *much* more
    lightweight than SCP.
    
    Additionally, it seems the maintainers of the project have no desire to
    address or look into what triggered the regression:
    https://github.com/adobe-fonts/source-code-pro/issues/217
    
    pr-link: Alluxio/alluxio#10826
    change-id: cid-b0c413b42cf8cffd0913fbe581bde24d576efa89

--- webui/worker/src/index.tsx
+++ webui/worker/src/index.tsx
@@ -23,7 +23,7 @@ import { initialState } from './store';
 
 import 'source-sans-pro/source-sans-pro.css';
 import 'source-serif-pro/source-serif-pro.css';
-import 'source-code-pro/source-code-pro.css';
+import '@openfonts/anonymous-pro_all';
 
 import './index.css';
 import { IAppProps } from './containers/App/App';
