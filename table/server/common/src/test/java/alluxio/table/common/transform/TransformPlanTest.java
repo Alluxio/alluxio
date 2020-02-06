54ad9e52e26caa212e0df0dd4aa07477e4f7c8cf
David Zhu <david@alluxio.com>
2020-02-06 11:19:52 -0800

    Change the default coalescing behavior
    pr-link: Alluxio/alluxio#10718
    change-id: cid-a0d98fd6ed5a3a65fae94aa3d056c781202768e3

--- table/server/common/src/test/java/alluxio/table/common/transform/TransformPlanTest.java
+++ table/server/common/src/test/java/alluxio/table/common/transform/TransformPlanTest.java
@@ -28,7 +28,7 @@ public class TransformPlanTest {
     HiveLayout from = TableTestUtils.createLayout("/from");
     HiveLayout to = TableTestUtils.createLayout("/to");
     TransformDefinition definition =
-        TransformDefinition.parse("write(hive).option(hive.num.files, 12)");
+        TransformDefinition.parse("write(hive).option(hive.file.count.max, 12)");
 
     TransformPlan plan = new TransformPlan(from, to, definition);
     assertEquals(from, plan.getBaseLayout());
@@ -41,6 +41,6 @@ public class TransformPlanTest {
     assertEquals("hive", compact.getDatabaseType());
     assertEquals("/from", compact.getInput());
     assertEquals("/to", compact.getOutput());
-    assertEquals(12, compact.getNumFiles());
+    assertEquals(12, compact.getMaxNumFiles());
   }
 }
