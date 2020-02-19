Alluxio/alluxio

commit e6751bc1600b8a0be0884549071e1da8240eb984

Improve error handling for attach db and sync
This change adds an `attachdb` option `--ignore-sync-errors` to keep the
database attached even if a sync failure occurred.

pr-link: Alluxio/alluxio#10897
change-id: cid-e3cf732791d14375a81a21592e112ff7b548b67c

table/server/master/src/main/java/alluxio/master/table/AlluxioCatalog.java
@@ -19,6 +19,7 @@ import alluxio.exception.status.NotFoundException;
 import alluxio.grpc.table.ColumnStatisticsInfo;
 import alluxio.grpc.table.ColumnStatisticsList;
 import alluxio.grpc.table.Constraint;
+import alluxio.grpc.table.SyncStatus;
 import alluxio.master.journal.JournalContext;
 import alluxio.master.journal.JournalEntryIterable;
 import alluxio.master.journal.Journaled;
@@ -104,10 +105,12 @@ public class AlluxioCatalog implements Journaled {
    * @param udbDbName the database name in the udb
    * @param dbName the database name in Alluxio
    * @param map the configuration
-   * @return true if database successfully created
+   * @param ignoreSyncErrors if true, will ignore syncing errors during the attach
+   * @return the sync status for the attach
    */
-  public boolean attachDatabase(JournalContext journalContext, String udbType,
-      String udbConnectionUri, String udbDbName, String dbName, Map<String, String> map)
+  public SyncStatus attachDatabase(JournalContext journalContext, String udbType,
+      String udbConnectionUri, String udbDbName, String dbName, Map<String, String> map,
+      boolean ignoreSyncErrors)
       throws IOException {
     try (LockResource l = getDbLock(dbName)) {
       if (mDBs.containsKey(dbName)) {
@@ -124,19 +127,24 @@ public class AlluxioCatalog implements Journaled {
               .setDbName(dbName)
               .putAllConfig(map).build()).build());
 
+      boolean syncError = false;
       try {
-        mDBs.get(dbName).sync(journalContext, mExecutorServiceSupplier.get());
+        SyncStatus status =
+            mDBs.get(dbName).sync(journalContext, mExecutorServiceSupplier.get());
+        syncError = status.getTablesErrorsCount() > 0;
+        return status;
       } catch (Exception e) {
         // Failed to connect to and sync the udb.
-        applyAndJournal(journalContext, Journal.JournalEntry.newBuilder().setDetachDb(
-            alluxio.proto.journal.Table.DetachDbEntry.newBuilder().setDbName(dbName).build())
-            .build());
         throw new IOException(String
             .format("Failed to connect underDb for Alluxio db '%s': %s", dbName,
                 e.getMessage()), e);
+      } finally {
+        if (syncError && !ignoreSyncErrors) {
+          applyAndJournal(journalContext, Journal.JournalEntry.newBuilder().setDetachDb(
+              alluxio.proto.journal.Table.DetachDbEntry.newBuilder().setDbName(dbName).build())
+              .build());
+        }
       }
-
-      return true;
     }
   }
 
@@ -145,9 +153,9 @@ public class AlluxioCatalog implements Journaled {
    *
    * @param journalContext journal context
    * @param dbName database name
-   * @return true if the database changed as a result of fullSync
+   * @return the resulting sync status
    */
-  public boolean syncDatabase(JournalContext journalContext, String dbName) throws IOException {
+  public SyncStatus syncDatabase(JournalContext journalContext, String dbName) throws IOException {
     try (LockResource l = getDbLock(dbName)) {
       Database db = getDatabaseByName(dbName);
       return db.sync(journalContext, mExecutorServiceSupplier.get());

core/transport/src/grpc/table/table_master.proto
@@ -170,6 +170,13 @@ message DateColumnStatsData {
     optional string bit_vectors = 5;
 }
 
+message SyncStatus {
+    repeated string tables_unchanged = 1;
+    repeated string tables_updated = 2;
+    repeated string tables_removed = 3;
+    map<string, string> tables_errors = 4;
+}
+
 message GetAllDatabasesPRequest {
 }
 
@@ -208,10 +215,13 @@ message AttachDatabasePRequest {
     optional string udb_db_name = 3;
     optional string db_name = 4;
     map<string, string> options = 5;
+    optional bool ignore_sync_errors = 6;
 }
 
 message AttachDatabasePResponse {
+    // TODO(gpang): remove in favor of status
     optional bool success = 1;
+    optional SyncStatus sync_status = 2;
 }
 
 message DetachDatabasePRequest {
@@ -227,7 +237,9 @@ message SyncDatabasePRequest {
 }
 
 message SyncDatabasePResponse {
+    // TODO(gpang): remove in favor of status
     optional bool success = 1;
+    optional SyncStatus status = 2;
 }
 
 message FileStatistics {

core/transport/src/main/java/alluxio/grpc/table/AttachDatabasePRequest.java
@@ -90,6 +90,11 @@ private static final long serialVersionUID = 0L;
                 options__.getKey(), options__.getValue());
             break;
           }
+          case 48: {
+            bitField0_ |= 0x00000010;
+            ignoreSyncErrors_ = input.readBool();
+            break;
+          }
           default: {
             if (!parseUnknownField(
                 input, unknownFields, extensionRegistry, tag)) {
@@ -391,6 +396,23 @@ private static final long serialVersionUID = 0L;
     return map.get(key);
   }
 
+  public static final int IGNORE_SYNC_ERRORS_FIELD_NUMBER = 6;
+  private boolean ignoreSyncErrors_;
+  /**
+   * <code>optional bool ignore_sync_errors = 6;</code>
+   * @return Whether the ignoreSyncErrors field is set.
+   */
+  public boolean hasIgnoreSyncErrors() {
+    return ((bitField0_ & 0x00000010) != 0);
+  }
+  /**
+   * <code>optional bool ignore_sync_errors = 6;</code>
+   * @return The ignoreSyncErrors.
+   */
+  public boolean getIgnoreSyncErrors() {
+    return ignoreSyncErrors_;
+  }
+
   private byte memoizedIsInitialized = -1;
   @java.lang.Override
   public final boolean isInitialized() {
@@ -423,6 +445,9 @@ private static final long serialVersionUID = 0L;
         internalGetOptions(),
         OptionsDefaultEntryHolder.defaultEntry,
         5);
+    if (((bitField0_ & 0x00000010) != 0)) {
+      output.writeBool(6, ignoreSyncErrors_);
+    }
     unknownFields.writeTo(output);
   }
 
@@ -453,6 +478,10 @@ private static final long serialVersionUID = 0L;
           .build();
       size += com.google.protobuf.CodedOutputStream
           .computeMessageSize(5, options__);
+    }
+    if (((bitField0_ & 0x00000010) != 0)) {
+      size += com.google.protobuf.CodedOutputStream
+        .computeBoolSize(6, ignoreSyncErrors_);
     }
     size += unknownFields.getSerializedSize();
     memoizedSize = size;
@@ -491,6 +520,11 @@ private static final long serialVersionUID = 0L;
     }
     if (!internalGetOptions().equals(
         other.internalGetOptions())) return false;
+    if (hasIgnoreSyncErrors() != other.hasIgnoreSyncErrors()) return false;
+    if (hasIgnoreSyncErrors()) {
+      if (getIgnoreSyncErrors()
+          != other.getIgnoreSyncErrors()) return false;
+    }
     if (!unknownFields.equals(other.unknownFields)) return false;
     return true;
   }
@@ -521,6 +555,11 @@ private static final long serialVersionUID = 0L;
     if (!internalGetOptions().getMap().isEmpty()) {
       hash = (37 * hash) + OPTIONS_FIELD_NUMBER;
       hash = (53 * hash) + internalGetOptions().hashCode();
+    }
+    if (hasIgnoreSyncErrors()) {
+      hash = (37 * hash) + IGNORE_SYNC_ERRORS_FIELD_NUMBER;
+      hash = (53 * hash) + com.google.protobuf.Internal.hashBoolean(
+          getIgnoreSyncErrors());
     }
     hash = (29 * hash) + unknownFields.hashCode();
     memoizedHashCode = hash;
@@ -686,6 +725,8 @@ private static final long serialVersionUID = 0L;
       dbName_ = "";
       bitField0_ = (bitField0_ & ~0x00000008);
       internalGetMutableOptions().clear();
+      ignoreSyncErrors_ = false;
+      bitField0_ = (bitField0_ & ~0x00000020);
       return this;
     }
 
@@ -732,6 +773,10 @@ private static final long serialVersionUID = 0L;
       result.dbName_ = dbName_;
       result.options_ = internalGetOptions();
       result.options_.makeImmutable();
+      if (((from_bitField0_ & 0x00000020) != 0)) {
+        result.ignoreSyncErrors_ = ignoreSyncErrors_;
+        to_bitField0_ |= 0x00000010;
+      }
       result.bitField0_ = to_bitField0_;
       onBuilt();
       return result;
@@ -803,6 +848,9 @@ private static final long serialVersionUID = 0L;
       }
       internalGetMutableOptions().mergeFrom(
           other.internalGetOptions());
+      if (other.hasIgnoreSyncErrors()) {
+        setIgnoreSyncErrors(other.getIgnoreSyncErrors());
+      }
       this.mergeUnknownFields(other.unknownFields);
       onChanged();
       return this;
@@ -1289,6 +1337,43 @@ private static final long serialVersionUID = 0L;
         java.util.Map<java.lang.String, java.lang.String> values) {
       internalGetMutableOptions().getMutableMap()
           .putAll(values);
+      return this;
+    }
+
+    private boolean ignoreSyncErrors_ ;
+    /**
+     * <code>optional bool ignore_sync_errors = 6;</code>
+     * @return Whether the ignoreSyncErrors field is set.
+     */
+    public boolean hasIgnoreSyncErrors() {
+      return ((bitField0_ & 0x00000020) != 0);
+    }
+    /**
+     * <code>optional bool ignore_sync_errors = 6;</code>
+     * @return The ignoreSyncErrors.
+     */
+    public boolean getIgnoreSyncErrors() {
+      return ignoreSyncErrors_;
+    }
+    /**
+     * <code>optional bool ignore_sync_errors = 6;</code>
+     * @param value The ignoreSyncErrors to set.
+     * @return This builder for chaining.
+     */
+    public Builder setIgnoreSyncErrors(boolean value) {
+      bitField0_ |= 0x00000020;
+      ignoreSyncErrors_ = value;
+      onChanged();
+      return this;
+    }
+    /**
+     * <code>optional bool ignore_sync_errors = 6;</code>
+     * @return This builder for chaining.
+     */
+    public Builder clearIgnoreSyncErrors() {
+      bitField0_ = (bitField0_ & ~0x00000020);
+      ignoreSyncErrors_ = false;
+      onChanged();
       return this;
     }
     @java.lang.Override

core/transport/src/main/java/alluxio/grpc/table/AttachDatabasePRequestOrBuilder.java
@@ -108,4 +108,15 @@ public interface AttachDatabasePRequestOrBuilder extends
 
   java.lang.String getOptionsOrThrow(
       java.lang.String key);
+
+  /**
+   * <code>optional bool ignore_sync_errors = 6;</code>
+   * @return Whether the ignoreSyncErrors field is set.
+   */
+  boolean hasIgnoreSyncErrors();
+  /**
+   * <code>optional bool ignore_sync_errors = 6;</code>
+   * @return The ignoreSyncErrors.
+   */
+  boolean getIgnoreSyncErrors();
 }

core/transport/src/main/java/alluxio/grpc/table/AttachDatabasePResponse.java
@@ -54,6 +54,19 @@ private static final long serialVersionUID = 0L;
             success_ = input.readBool();
             break;
           }
+          case 18: {
+            alluxio.grpc.table.SyncStatus.Builder subBuilder = null;
+            if (((bitField0_ & 0x00000002) != 0)) {
+              subBuilder = syncStatus_.toBuilder();
+            }
+            syncStatus_ = input.readMessage(alluxio.grpc.table.SyncStatus.PARSER, extensionRegistry);
+            if (subBuilder != null) {
+              subBuilder.mergeFrom(syncStatus_);
+              syncStatus_ = subBuilder.buildPartial();
+            }
+            bitField0_ |= 0x00000002;
+            break;
+          }
           default: {
             if (!parseUnknownField(
                 input, unknownFields, extensionRegistry, tag)) {
@@ -90,6 +103,10 @@ private static final long serialVersionUID = 0L;
   public static final int SUCCESS_FIELD_NUMBER = 1;
   private boolean success_;
   /**
+   * <pre>
+   * TODO(gpang): remove in favor of status
+   * </pre>
+   *
    * <code>optional bool success = 1;</code>
    * @return Whether the success field is set.
    */
@@ -97,11 +114,38 @@ private static final long serialVersionUID = 0L;
     return ((bitField0_ & 0x00000001) != 0);
   }
   /**
+   * <pre>
+   * TODO(gpang): remove in favor of status
+   * </pre>
+   *
    * <code>optional bool success = 1;</code>
    * @return The success.
    */
   public boolean getSuccess() {
     return success_;
+  }
+
+  public static final int SYNC_STATUS_FIELD_NUMBER = 2;
+  private alluxio.grpc.table.SyncStatus syncStatus_;
+  /**
+   * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+   * @return Whether the syncStatus field is set.
+   */
+  public boolean hasSyncStatus() {
+    return ((bitField0_ & 0x00000002) != 0);
+  }
+  /**
+   * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+   * @return The syncStatus.
+   */
+  public alluxio.grpc.table.SyncStatus getSyncStatus() {
+    return syncStatus_ == null ? alluxio.grpc.table.SyncStatus.getDefaultInstance() : syncStatus_;
+  }
+  /**
+   * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+   */
+  public alluxio.grpc.table.SyncStatusOrBuilder getSyncStatusOrBuilder() {
+    return syncStatus_ == null ? alluxio.grpc.table.SyncStatus.getDefaultInstance() : syncStatus_;
   }
 
   private byte memoizedIsInitialized = -1;
@@ -121,6 +165,9 @@ private static final long serialVersionUID = 0L;
     if (((bitField0_ & 0x00000001) != 0)) {
       output.writeBool(1, success_);
     }
+    if (((bitField0_ & 0x00000002) != 0)) {
+      output.writeMessage(2, getSyncStatus());
+    }
     unknownFields.writeTo(output);
   }
 
@@ -133,6 +180,10 @@ private static final long serialVersionUID = 0L;
     if (((bitField0_ & 0x00000001) != 0)) {
       size += com.google.protobuf.CodedOutputStream
         .computeBoolSize(1, success_);
+    }
+    if (((bitField0_ & 0x00000002) != 0)) {
+      size += com.google.protobuf.CodedOutputStream
+        .computeMessageSize(2, getSyncStatus());
     }
     size += unknownFields.getSerializedSize();
     memoizedSize = size;
@@ -154,6 +205,11 @@ private static final long serialVersionUID = 0L;
       if (getSuccess()
           != other.getSuccess()) return false;
     }
+    if (hasSyncStatus() != other.hasSyncStatus()) return false;
+    if (hasSyncStatus()) {
+      if (!getSyncStatus()
+          .equals(other.getSyncStatus())) return false;
+    }
     if (!unknownFields.equals(other.unknownFields)) return false;
     return true;
   }
@@ -169,6 +225,10 @@ private static final long serialVersionUID = 0L;
       hash = (37 * hash) + SUCCESS_FIELD_NUMBER;
       hash = (53 * hash) + com.google.protobuf.Internal.hashBoolean(
           getSuccess());
+    }
+    if (hasSyncStatus()) {
+      hash = (37 * hash) + SYNC_STATUS_FIELD_NUMBER;
+      hash = (53 * hash) + getSyncStatus().hashCode();
     }
     hash = (29 * hash) + unknownFields.hashCode();
     memoizedHashCode = hash;
@@ -298,6 +358,7 @@ private static final long serialVersionUID = 0L;
     private void maybeForceBuilderInitialization() {
       if (com.google.protobuf.GeneratedMessageV3
               .alwaysUseFieldBuilders) {
+        getSyncStatusFieldBuilder();
       }
     }
     @java.lang.Override
@@ -305,6 +366,12 @@ private static final long serialVersionUID = 0L;
       super.clear();
       success_ = false;
       bitField0_ = (bitField0_ & ~0x00000001);
+      if (syncStatusBuilder_ == null) {
+        syncStatus_ = null;
+      } else {
+        syncStatusBuilder_.clear();
+      }
+      bitField0_ = (bitField0_ & ~0x00000002);
       return this;
     }
 
@@ -336,6 +403,14 @@ private static final long serialVersionUID = 0L;
       if (((from_bitField0_ & 0x00000001) != 0)) {
         result.success_ = success_;
         to_bitField0_ |= 0x00000001;
+      }
+      if (((from_bitField0_ & 0x00000002) != 0)) {
+        if (syncStatusBuilder_ == null) {
+          result.syncStatus_ = syncStatus_;
+        } else {
+          result.syncStatus_ = syncStatusBuilder_.build();
+        }
+        to_bitField0_ |= 0x00000002;
       }
       result.bitField0_ = to_bitField0_;
       onBuilt();
@@ -389,6 +464,9 @@ private static final long serialVersionUID = 0L;
       if (other.hasSuccess()) {
         setSuccess(other.getSuccess());
       }
+      if (other.hasSyncStatus()) {
+        mergeSyncStatus(other.getSyncStatus());
+      }
       this.mergeUnknownFields(other.unknownFields);
       onChanged();
       return this;
@@ -421,6 +499,10 @@ private static final long serialVersionUID = 0L;
 
     private boolean success_ ;
     /**
+     * <pre>
+     * TODO(gpang): remove in favor of status
+     * </pre>
+     *
      * <code>optional bool success = 1;</code>
      * @return Whether the success field is set.
      */
@@ -428,6 +510,10 @@ private static final long serialVersionUID = 0L;
       return ((bitField0_ & 0x00000001) != 0);
     }
     /**
+     * <pre>
+     * TODO(gpang): remove in favor of status
+     * </pre>
+     *
      * <code>optional bool success = 1;</code>
      * @return The success.
      */
@@ -435,6 +521,10 @@ private static final long serialVersionUID = 0L;
       return success_;
     }
     /**
+     * <pre>
+     * TODO(gpang): remove in favor of status
+     * </pre>
+     *
      * <code>optional bool success = 1;</code>
      * @param value The success to set.
      * @return This builder for chaining.
@@ -446,6 +536,10 @@ private static final long serialVersionUID = 0L;
       return this;
     }
     /**
+     * <pre>
+     * TODO(gpang): remove in favor of status
+     * </pre>
+     *
      * <code>optional bool success = 1;</code>
      * @return This builder for chaining.
      */
@@ -454,6 +548,126 @@ private static final long serialVersionUID = 0L;
       success_ = false;
       onChanged();
       return this;
+    }
+
+    private alluxio.grpc.table.SyncStatus syncStatus_;
+    private com.google.protobuf.SingleFieldBuilderV3<
+        alluxio.grpc.table.SyncStatus, alluxio.grpc.table.SyncStatus.Builder, alluxio.grpc.table.SyncStatusOrBuilder> syncStatusBuilder_;
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     * @return Whether the syncStatus field is set.
+     */
+    public boolean hasSyncStatus() {
+      return ((bitField0_ & 0x00000002) != 0);
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     * @return The syncStatus.
+     */
+    public alluxio.grpc.table.SyncStatus getSyncStatus() {
+      if (syncStatusBuilder_ == null) {
+        return syncStatus_ == null ? alluxio.grpc.table.SyncStatus.getDefaultInstance() : syncStatus_;
+      } else {
+        return syncStatusBuilder_.getMessage();
+      }
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    public Builder setSyncStatus(alluxio.grpc.table.SyncStatus value) {
+      if (syncStatusBuilder_ == null) {
+        if (value == null) {
+          throw new NullPointerException();
+        }
+        syncStatus_ = value;
+        onChanged();
+      } else {
+        syncStatusBuilder_.setMessage(value);
+      }
+      bitField0_ |= 0x00000002;
+      return this;
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    public Builder setSyncStatus(
+        alluxio.grpc.table.SyncStatus.Builder builderForValue) {
+      if (syncStatusBuilder_ == null) {
+        syncStatus_ = builderForValue.build();
+        onChanged();
+      } else {
+        syncStatusBuilder_.setMessage(builderForValue.build());
+      }
+      bitField0_ |= 0x00000002;
+      return this;
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    public Builder mergeSyncStatus(alluxio.grpc.table.SyncStatus value) {
+      if (syncStatusBuilder_ == null) {
+        if (((bitField0_ & 0x00000002) != 0) &&
+            syncStatus_ != null &&
+            syncStatus_ != alluxio.grpc.table.SyncStatus.getDefaultInstance()) {
+          syncStatus_ =
+            alluxio.grpc.table.SyncStatus.newBuilder(syncStatus_).mergeFrom(value).buildPartial();
+        } else {
+          syncStatus_ = value;
+        }
+        onChanged();
+      } else {
+        syncStatusBuilder_.mergeFrom(value);
+      }
+      bitField0_ |= 0x00000002;
+      return this;
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    public Builder clearSyncStatus() {
+      if (syncStatusBuilder_ == null) {
+        syncStatus_ = null;
+        onChanged();
+      } else {
+        syncStatusBuilder_.clear();
+      }
+      bitField0_ = (bitField0_ & ~0x00000002);
+      return this;
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    public alluxio.grpc.table.SyncStatus.Builder getSyncStatusBuilder() {
+      bitField0_ |= 0x00000002;
+      onChanged();
+      return getSyncStatusFieldBuilder().getBuilder();
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    public alluxio.grpc.table.SyncStatusOrBuilder getSyncStatusOrBuilder() {
+      if (syncStatusBuilder_ != null) {
+        return syncStatusBuilder_.getMessageOrBuilder();
+      } else {
+        return syncStatus_ == null ?
+            alluxio.grpc.table.SyncStatus.getDefaultInstance() : syncStatus_;
+      }
+    }
+    /**
+     * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+     */
+    private com.google.protobuf.SingleFieldBuilderV3<
+        alluxio.grpc.table.SyncStatus, alluxio.grpc.table.SyncStatus.Builder, alluxio.grpc.table.SyncStatusOrBuilder> 
+        getSyncStatusFieldBuilder() {
+      if (syncStatusBuilder_ == null) {
+        syncStatusBuilder_ = new com.google.protobuf.SingleFieldBuilderV3<
+            alluxio.grpc.table.SyncStatus, alluxio.grpc.table.SyncStatus.Builder, alluxio.grpc.table.SyncStatusOrBuilder>(
+                getSyncStatus(),
+                getParentForChildren(),
+                isClean());
+        syncStatus_ = null;
+      }
+      return syncStatusBuilder_;
     }
     @java.lang.Override
     public final Builder setUnknownFields(

core/transport/src/main/java/alluxio/grpc/table/AttachDatabasePResponseOrBuilder.java
@@ -8,13 +8,36 @@ public interface AttachDatabasePResponseOrBuilder extends
     com.google.protobuf.MessageOrBuilder {
 
   /**
+   * <pre>
+   * TODO(gpang): remove in favor of status
+   * </pre>
+   *
    * <code>optional bool success = 1;</code>
    * @return Whether the success field is set.
    */
   boolean hasSuccess();
   /**
+   * <pre>
+   * TODO(gpang): remove in favor of status
+   * </pre>
+   *
    * <code>optional bool success = 1;</code>
    * @return The success.
    */
   boolean getSuccess();
+
+  /**
+   * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+   * @return Whether the syncStatus field is set.
+   */
+  boolean hasSyncStatus();
+  /**
+   * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+   * @return The syncStatus.
+   */
+  alluxio.grpc.table.SyncStatus getSyncStatus();
+  /**
+   * <code>optional .alluxio.grpc.table.SyncStatus sync_status = 2;</code>
+   */
+  alluxio.grpc.table.SyncStatusOrBuilder getSyncStatusOrBuilder();
 }

commit 8bf83813d3382af526d7c5a1f1f8868fe9bb8942

Fix speedcore sync error behavior on attach
This code was not applying detachDatabase when .sync() crashed.

Fixes a regression from: #10897

pr-link: Alluxio/alluxio#10941
change-id: cid-01f908c6a21b6aed587372fb50eb1890aa317e5b

table/server/master/src/main/java/alluxio/master/table/AlluxioCatalog.java
@@ -135,6 +135,7 @@ public class AlluxioCatalog implements Journaled {
         return status;
       } catch (Exception e) {
         // Failed to connect to and sync the udb.
+        syncError = true;
         throw new IOException(String
             .format("Failed to connect underDb for Alluxio db '%s': %s", dbName,
                 e.getMessage()), e);