dc283ffaaf3f0eaa5ccc6efa9d9dfd6137d95fe7
Bradley Yoo <bradleyyoo@gmail.com>
2020-02-24 11:42:25 -0800

    Add description for Tasks in job stat
    Example:
    
    ```
    ID: 1582304046978
    Name: Compact
    Description:
    CompactConfig{input=alluxio://SpeedcorePartitionedTable-masters-1:19998/catalog/movielens/tables/ratings_part/hive/rating=1.5,
    output=alluxio://SpeedcorePartitionedTable-masters-1:19998/catalog/mov...
    Status: COMPLETED
    Task 0
    Worker: SpeedcorePartitionedTable-workers-1
    Description:
    [CompactTask{inputs=[alluxio://SpeedcorePartitionedTable-masters-1:19998/catalog/movielens/tables/ratings_part/hive/rating=1.5/000004_0],
    output=alluxio://SpeedcorePartitionedTable-masters-1:19998/...
    Status: COMPLETED
    ```
    
    pr-link: Alluxio/alluxio#10965
    change-id: cid-e960828b4748d3fdbfc21015f623b6dd41f52765

--- shell/src/main/java/alluxio/cli/job/command/StatCommand.java
+++ shell/src/main/java/alluxio/cli/job/command/StatCommand.java
@@ -107,6 +107,10 @@ public final class StatCommand extends AbstractFileSystemCommand {
             output.append("\t").append("Worker: ").append(taskInfo.getWorkerHost()).append("\n");
           }
         }
+        if (!childInfo.getDescription().isEmpty()) {
+          output.append("\t").append("Description: ").append(
+              StringUtils.abbreviate(childInfo.getDescription(), 200)).append("\n");
+        }
         output.append("\t").append("Status: ").append(childInfo.getStatus()).append("\n");
         if (childInfo.getErrorMessage() != null && !childInfo.getErrorMessage().isEmpty()) {
           output.append("\t").append("Error: ").append(childInfo.getErrorMessage()).append("\n");
