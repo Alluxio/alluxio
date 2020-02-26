e10c5b72bc031cbd7c2c39f773bb6425380b0b9b
Bradley Yoo <bradleyyoo@gmail.com>
2020-02-26 14:39:05 -0800

    Refactor CommonUtils.waitFor to use value supplier
    We have many instances in our code where we use CommonUtils.waitFor to
    wait for a certain condition to be equal to something but the last state
    of the object is not clear when there is a Timeout. This code has a new
    API that could be opted into to get such a benefit.
    
    pr-link: Alluxio/alluxio#11001
    change-id: cid-b4293b9f66e390a97806c97a530fd96f346401a9

--- core/client/fs/src/main/java/alluxio/client/file/FileSystemUtils.java
+++ core/client/fs/src/main/java/alluxio/client/file/FileSystemUtils.java
@@ -155,14 +155,14 @@ public final class FileSystemUtils {
       AlluxioException, TimeoutException, InterruptedException {
     fs.persist(uri, ScheduleAsyncPersistencePOptions
         .newBuilder().setPersistenceWaitTime(persistenceWaitTime).build());
-    CommonUtils.waitFor(String.format("%s to be persisted", uri) , () -> {
+    CommonUtils.waitForResult(String.format("%s to be persisted", uri) , () -> {
       try {
-        return fs.getStatus(uri).isPersisted();
+        return fs.getStatus(uri);
       } catch (Exception e) {
         Throwables.throwIfUnchecked(e);
         throw new RuntimeException(e);
       }
-    }, WaitForOptions.defaults().setTimeoutMs(timeoutMs)
-        .setInterval(Constants.SECOND_MS));
+    }, (status) -> status.isPersisted(),
+        WaitForOptions.defaults().setTimeoutMs(timeoutMs).setInterval(Constants.SECOND_MS));
   }
 }
