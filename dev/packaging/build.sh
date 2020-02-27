2c1d6f5f582cd81c5123868c2c4b982fe7b213ad
Zac Blanco <zac@alluxio.com>
2020-02-27 15:49:05 -0800

    build debian packages
    use ./dev/packaging/build.sh <alluxio_tarball>

---
+++ dev/packaging/build.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+# (the "License"). You may not use this work except in compliance with the License, which is
+# available at www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied, as more fully set forth in the License.
+#
+# See the NOTICE file distributed with this work for information regarding copyright ownership.
+#
+
+SCRIPT_DIR=$(cd "$( dirname "$( readlink "$0" || echo "$0" )" )"; pwd)
+
+build_docker_image() {
+    pushd "${SCRIPT_DIR}/debian"
+    docker build -t deb-builder:latest .
+    popd
+}
+
+build_debian() {
+    local alluxio_tarball="${1}"
+    if ! docker image inspect deb-builder:latest || true; then
+        build_docker_image
+    fi
+
+    local tarball="$(basename "$(realpath ${alluxio_tarball})")"
+    local version="${tarball#alluxio-}"
+    version="${version%%-bin.tar.gz}"
+
+    local package_name="alluxio_${version}-0"
+
+    cat ${alluxio_tarball} | \
+    docker run --rm -i \
+    -v "${SCRIPT_DIR}/debian":/data \
+    deb-builder:latest "${package_name}" "${version}" \
+    > "${package_name}.deb"
+}
+
+main() {
+    build_debian "${1}"
+}
+main "$@"
--- dev/packaging/debian/package.sh
+++ dev/packaging/debian/package.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
+#
+# The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+# (the "License"). You may not use this work except in compliance with the License, which is
+# available at www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied, as more fully set forth in the License.
+#
+# See the NOTICE file distributed with this work for information regarding copyright ownership.
+#
+
 set -eux
 
 main () {
     local pkg_name="${1}"
-    local control="${2}"
+    local version="${2}"
 
     tee "alluxio.tar.gz" > /dev/null
     local pkg_dir="/${pkg_name}"
+    ls /data >&2
+    ls /data/DEBIAN >&2
     mkdir -p "${pkg_dir}/DEBIAN"
+    local template="$(cat /data/DEBIAN/control)"
+    local control="$(eval "echo \"${template}\"")"
     echo "${control}" > "${pkg_dir}/DEBIAN/control"
     mkdir -p "${pkg_dir}/opt"
     tar -xzf *.tar.gz -C "${pkg_dir}/opt" && rm *.tar.gz
     mv "${pkg_dir}"/opt/alluxio* "${pkg_dir}/opt/alluxio"
-
 
     mkdir -p ${pkg_dir}/etc/profile.d/ && \
         echo "export PATH=\"${PATH}:/opt/alluxio/bin\"" >> ${pkg_dir}/etc/profile.d/10-${pkg_name}.sh
