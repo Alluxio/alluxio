af5c27f414ea24e0c7bcb37ab8b8a3029e32c96d
Zac Blanco <zac@alluxio.com>
2020-02-27 16:03:53 -0800

    allow deb->rpm conversion

--- dev/packaging/convert.sh
+++ dev/packaging/convert.sh
@@ -10,11 +10,26 @@
 # See the NOTICE file distributed with this work for information regarding copyright ownership.
 #
 
+set -eux
+
+SCRIPT_DIR=$(cd "$( dirname "$( readlink "$0" || echo "$0" )" )"; pwd)
+
+build_docker_image() {
+    pushd "${SCRIPT_DIR}/fpm"
+    docker build -t fpm:latest .
+    popd
+}
 
 main() {
-    local deb_package="$(basename "$(realpath alluxio*.deb)")"
+    local deb_pkg="${1}"
+    if ! docker image inspect deb-builder:latest || true; then
+        build_docker_image
+    fi
+
+    local deb_package="$(basename "$(realpath "${deb_pkg}")")"
+    local deb_dir="$(dirname "$(realpath "${deb_pkg}")")"
     docker run -i \
-    -v "$(realpath .)":/data \
+    -v "$(realpath "${deb_dir}")":/data \
     fpm:latest fpm --input-type deb --output-type rpm -p /data "/data/${deb_package}"
 }
 
