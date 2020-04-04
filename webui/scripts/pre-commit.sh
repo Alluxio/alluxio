018ddbc6b0b21d40c1311cdf9df487fb4098ea5a
Jason <tieujason330@gmail.com>
2020-04-04 11:57:21 -0700

    remove husky

--- webui/scripts/pre-commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $(git diff --name-only --cached | grep -c '.tsx') != 0 ]; then
-  lerna run lint-staged-tsx-files --parallel --stream
-else
-  echo 'No .tsx files to lint. Continuing with commit.'
-fi
