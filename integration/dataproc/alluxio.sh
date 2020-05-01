7b96e64fb8f42665d84cb88a5dec11b57f696310
Jason <tieujason330@gmail.com>
2020-05-01 14:36:54 -0700

    header comments

--- integration/dataproc/alluxio.sh
+++ integration/dataproc/alluxio.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#
 #    Copyright 2019 Google, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +13,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+#
 
 set -eux
 
