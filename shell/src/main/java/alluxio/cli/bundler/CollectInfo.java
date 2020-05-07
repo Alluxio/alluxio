a5231e68b29aff0f82c82089c9ef323749f688b8
jiacheliu3 <jiacheliu3@gmail.com>
2020-05-07 21:48:34 +0800

    endless styles

--- shell/src/main/java/alluxio/cli/bundler/CollectInfo.java
+++ shell/src/main/java/alluxio/cli/bundler/CollectInfo.java
@@ -46,7 +46,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
