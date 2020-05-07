2520be78203b5d5bdfcefac237a9e1602792403d
Jason <tieujason330@gmail.com>
2020-05-06 19:49:03 -0700

    license header in scripts

--- integration/packer/alluxio-presto-sandbox-docker/resources/opt/hive/conf/hive-env.sh
+++ integration/packer/alluxio-presto-sandbox-docker/resources/opt/hive/conf/hive-env.sh
@@ -1,3 +1,14 @@
+#
+# The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+# (the "License"). You may not use this work except in compliance with the License, which is
+# available at www.apache.org/licenses/LICENSE-2.0
+#
+# This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied, as more fully set forth in the License.
+#
+# See the NOTICE file distributed with this work for information regarding copyright ownership.
+#
+
 if [ "$SERVICE" = "cli" ]; then
    if [ -z "$DEBUG" ]; then
      export HADOOP_OPTS="$HADOOP_OPTS -XX:NewRatio=12 -Xms10m -XX:MaxHeapFreeRatio=40 -XX:MinHeapFreeRatio=15 -XX:+UseParNewGC -XX:-UseGCOverheadLimit"
