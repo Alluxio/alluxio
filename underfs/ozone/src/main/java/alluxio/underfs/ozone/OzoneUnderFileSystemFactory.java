86181fd1b73a82c143b92685cd0bea6dfd9b8d7d
maobaolong <307499405@qq.com>
2020-05-07 23:54:44 +0800

    Ozone as a under file system of Alluxio

---
+++ underfs/ozone/src/main/java/alluxio/underfs/ozone/OzoneUnderFileSystemFactory.java
@@ -0,0 +1,45 @@
+package alluxio.underfs.ozone;
+
+import alluxio.AlluxioURI;
+import alluxio.Constants;
+import alluxio.underfs.UnderFileSystem;
+import alluxio.underfs.UnderFileSystemConfiguration;
+import alluxio.underfs.UnderFileSystemFactory;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+
+import java.io.IOException;
+
+public class OzoneUnderFileSystemFactory implements UnderFileSystemFactory {
+  @Override
+  public UnderFileSystem create(String path, UnderFileSystemConfiguration conf) {
+    Preconditions.checkNotNull(path, "path");
+
+    if (checkOzoneCredentials(conf)) {
+      try {
+        return OzoneUnderFileSystem.createInstance(new AlluxioURI(path), conf);
+      } catch (Exception e) {
+        throw Throwables.propagate(e);
+      }
+    }
+
+    String err = "COS Credentials not available, cannot create COS Under File System.";
+    throw Throwables.propagate(new IOException(err));
+  }
+
+  @Override
+  public boolean supportsPath(String path) {
+    return path != null && path.startsWith(Constants.HEADER_OZONE);
+  }
+
+  /**
+   * @param conf optional configuration object for the UFS
+   *
+   * @return true if both access, secret and endpoint keys are present, false otherwise
+   */
+  private boolean checkOzoneCredentials(UnderFileSystemConfiguration conf) {
+    // TODO(maobaolong): impl check logic while ozone supported
+    return true;
+  }
+}
