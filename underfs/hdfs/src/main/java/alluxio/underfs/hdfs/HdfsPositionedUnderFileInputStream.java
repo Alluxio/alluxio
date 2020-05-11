e71f543923621e49514e709c5afaa07d9a4febf2
David Zhu <david@alluxio.com>
2020-05-10 22:48:33 -0700

    Improve HdfsPositionedReadUnderFileInputStream with a heuristic
    The heuristic works as follows :
    1. it starts with a pread, because a lot of times the client only does a
    single large read, and we want to benefit from pread in that case.
    2. if there are certain number of sequential reads in a row, we switch
    to buffered read mode. In this mode, preads are serviced via pread
    calls, and read calls are services by read calls. A sequential read is
    defined as a read that is within a movement limit of the previous read.
    This is to guard against workloads such as read, skip(2), read, skip(3)
    etc.
    3. any non-sequential read, we change back to pread mode.
    
    pr-link: Alluxio/alluxio#11400
    change-id: cid-482d194fd551a9e80892682a90cc5e82290e284a

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsPositionedUnderFileInputStream.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsPositionedUnderFileInputStream.java
@@ -14,21 +14,43 @@ package alluxio.underfs.hdfs;
 import alluxio.underfs.SeekableUnderFileInputStream;
 import alluxio.util.io.BufferUtils;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.PositionedReadable;
+import org.apache.hadoop.fs.Seekable;
 
 import java.io.IOException;
 
 /**
- * The input stream of HDFS as under filesystem. This input stream supports seeking but internally
- * uses the positionedRead {@link FSDataInputStream} API. This stream can be cached for reuse.
+ * The input stream of HDFS as under filesystem. This input stream has two mode of operations.
+ * Under sequential mode, it uses the read api and can take advantage of underlying stream's
+ * buffering. Under random read mode, it uses the positionedRead {@link FSDataInputStream} API.
+ * This stream can be cached for reuse.
  */
 public class HdfsPositionedUnderFileInputStream extends SeekableUnderFileInputStream {
+  // TODO(david): make these parameters configurations and add diagnostic metrics.
+  // After this many number of sequential reads (reads without large skips), it
+  // will switch to sequential read mode.
+  @VisibleForTesting
+  static final int SEQUENTIAL_READ_LIMIT = 3;
+  // This describes the number of bytes that we can move forward in a stream without
+  // switching to random read mode.
+  @VisibleForTesting
+  static final int MOVEMENT_LIMIT = 512;
+
   private long mPos;
+  // The heuristic is that if there are certain number of sequential reads in a row,
+  // we switch to buffered read mode. In this mode, preads are serviced via pread calls,
+  // and read calls are services by read calls. A sequential read is defined as a read that
+  // is within a movement limit of the previous read. This is to guard against workloads
+  // such as read, skip(2), read, skip(3) etc.
+  private int mSequentialReadCount;
 
   HdfsPositionedUnderFileInputStream(FSDataInputStream in, long pos) {
     super(in);
     mPos = pos;
+    mSequentialReadCount = 0;
   }
 
   @Override
@@ -61,22 +83,49 @@ public class HdfsPositionedUnderFileInputStream extends SeekableUnderFileInputSt
     return read(b, 0, b.length);
   }
 
+  /**
+   * If there has been a number of sequential reads in a row,
+   * we move to regular buffered reads.
+   */
   @Override
   public int read(byte[] buffer, int offset, int length) throws IOException {
-    int bytesRead = ((FSDataInputStream) in).read(mPos, buffer, offset, length);
+    int bytesRead;
+    if (isSequentialReadMode() && mPos != ((Seekable) in).getPos()) {
+      ((Seekable) in).seek(mPos);
+    }
+    if (mPos == ((Seekable) in).getPos()) {
+      // same position, use buffered reads as default
+      bytesRead = in.read(buffer, offset, length);
+    } else {
+      bytesRead = ((PositionedReadable) in).read(mPos, buffer, offset, length);
+    }
     if (bytesRead > 0) {
       mPos += bytesRead;
+      mSequentialReadCount++;
     }
     return bytesRead;
   }
 
+  private boolean isSequentialReadMode() {
+    return mSequentialReadCount >= SEQUENTIAL_READ_LIMIT;
+  }
+
   @Override
   public void seek(long position) throws IOException {
+    if (position < mPos || position - mPos > MOVEMENT_LIMIT) {
+      mSequentialReadCount = 0;
+    }
     mPos = position;
   }
 
   @Override
   public long skip(long n) throws IOException {
+    if (n <= 0) {
+      return 0;
+    }
+    if (n > MOVEMENT_LIMIT) {
+      mSequentialReadCount = 0;
+    }
     mPos += n;
     return n;
   }
