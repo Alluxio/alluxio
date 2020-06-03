21c68d7504c671eebb639683a36f247bf856f029
yuanxm5 <yuanxm5@163.com>
2020-06-03 19:39:52 +0800

    add readonly check while delete recursively

--- core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -1548,6 +1548,7 @@ public final class DefaultFileSystemMaster extends CoreMaster
             for (LockedInodePath childPath : descendants) {
               try {
                 mPermissionChecker.checkPermission(Mode.Bits.WRITE, childPath);
+                mMountTable.checkUnderWritableMountPoint(childPath.getUri());
               } catch (AccessControlException e) {
                 failedChildren.add(e.getMessage());
               }
