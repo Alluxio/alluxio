1febf34738511ac4654d3996346d7c5efeeb5460
caio <caiopava@gmail.com>
2020-06-10 16:53:03 -0300

    feat: standardize packages for dev environment

---
+++ scripts/shell/run_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ./configs/config.properties ./configs/alluxio-site.properties; \
+export ALLUXIO_JAVA_OPTS="-Dalluxio.site.conf.dir=\${alluxio.conf.dir}/,\${user.home}/.alluxio/,/etc/alluxio/,\${alluxio.home}/configs"; \
+./bin/alluxio runTests
---
+++ scripts/shell/stop_local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./bin/alluxio-stop.sh local
