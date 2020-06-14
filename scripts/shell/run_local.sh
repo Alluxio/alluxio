ff6b65621f3b6742b206be727aa69d19d8d4d3b4
LuizBuffa <53086292+LuizBuffa@users.noreply.github.com>
2020-06-14 19:34:29 -0300

    Update run_local.sh

--- scripts/shell/run_local.sh
+++ scripts/shell/run_local.sh
@@ -2,4 +2,4 @@
 
 cp ./configs/config.properties ./configs/alluxio-site.properties; \
 export ALLUXIO_JAVA_OPTS="-Dalluxio.site.conf.dir=\${alluxio.conf.dir}/,\${user.home}/.alluxio/,/etc/alluxio/,\${alluxio.home}/configs"; \
-./bin/alluxio-start.sh local SudoMount
+./bin/alluxio-start.sh local Mount
