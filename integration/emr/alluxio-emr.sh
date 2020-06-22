744691cfee93f8043177c0da4e0977541f599eb2
LuQQiu <luqiujob@gmail.com>
2020-06-22 15:51:02 -0700

    Wait for alluxio master to come up before startSync
    StartSync may fail if master doesn't come up quick
    
    pr-link: Alluxio/alluxio#11619
    change-id: cid-d390b6d46cb40065b37c449bd01fd733ae4e0eec

--- integration/emr/alluxio-emr.sh
+++ integration/emr/alluxio-emr.sh
@@ -679,11 +679,13 @@ IN
       doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a ${args} master"
       doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a job_master"
       doas alluxio "${ALLUXIO_HOME}/bin/alluxio-start.sh -a proxy"
-
       if [[ "${backup_uri}" ]]; then
         register_backup_on_shutdown "${backup_uri}"
       fi
-
+      until ${ALLUXIO_HOME}/bin/alluxio fsadmin report
+      do
+        sleep 5
+      done
       if [[ "${sync_list}" ]]; then
         IFS="${delimiter}" read -ra paths <<< "${sync_list}"
         for path in "${paths[@]}"; do
