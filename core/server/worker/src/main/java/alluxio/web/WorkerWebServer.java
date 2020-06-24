2a21eb11346203159c2a6ffce9b59db409a0d903
Bradley Yoo <bradleyyoo@gmail.com>
2020-06-23 17:04:40 -0700

    Bump jetty version from 9.2.28 to 9.4.28
    Vulnerability: https://github.com/advisories/GHSA-84q7-p226-4x5w
    
    pr-link: Alluxio/alluxio#11617
    change-id: cid-603fadbee1b591db4ac417dd7ddc105adbb25f5b

--- core/server/worker/src/main/java/alluxio/web/WorkerWebServer.java
+++ core/server/worker/src/main/java/alluxio/web/WorkerWebServer.java
@@ -30,8 +30,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
 
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.servlet.ServletException;
@@ -100,7 +100,7 @@ public final class WorkerWebServer extends WebServer {
         errorHandler.addErrorPage(404, "/");
         mServletContextHandler.setErrorHandler(errorHandler);
       }
-    } catch (MalformedURLException e) {
+    } catch (IOException e) {
       LOG.error("ERROR: resource path is malformed", e);
     }
   }
