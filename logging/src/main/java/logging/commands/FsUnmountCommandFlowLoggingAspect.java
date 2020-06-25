f7a6410c746da7e385e527345c953cb84082121c
Abner Ferreira <abner.ferreira@simbioseventures.com>
2020-06-25 20:43:17 -0300

    feat: added logging for commands: mount, unmount, mkdir

--- logging/src/main/java/logging/commands/FsUnmountCommandFlowLoggingAspect.java
+++ logging/src/main/java/logging/commands/FsUnmountCommandFlowLoggingAspect.java
@@ -1,4 +1,4 @@
-package logging.general;
+package logging.commands;
 
 import br.com.simbiose.debug_log.BaseAspect;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,16 +10,19 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Aspect
-public class GeneralFullFlowLoggingAspect extends BaseAspect {
+public class FsUnmountCommandFlowLoggingAspect extends BaseAspect {
 
-    private static final String FLOW_NAME = "GeneralFullFlow";
+    private static final String FLOW_NAME = "FsUnmountFlow";
 
-    private static final String START_METHOD = "execution(* alluxio..*main(..))";
+    private static final String START_METHOD = "execution(* alluxio.master.file.DefaultFileSystemMaster.unmount(..))";
 
     private static final String WHITE_AND_BLACK_LIST = "execution(* alluxio..*(..)) && "
-            + "!within(logging..*)";
+            + "!within(logging..*) && "
+            + "!within(alluxio.clock..*) && "
+            + "!within(alluxio.alluxio.client.metrics..*) && "
+            + "!within(alluxio.heartbeat..*)";
 
-    private static final String FINISH_METHOD = "execution(* java.lang.System.exit(..))";
+    private static final String FINISH_METHOD = "execution(* alluxio.security.authentication.onCompleted(..))";
 
     protected final Map<Long, Integer> threadIdToStep = new ConcurrentHashMap<>();
     protected final Map<Long, Long> threadIdToDebugLogId = new ConcurrentHashMap<>();
@@ -40,7 +43,7 @@ public class GeneralFullFlowLoggingAspect extends BaseAspect {
      */
     @Around(START_METHOD)
     public Object startFlux(final ProceedingJoinPoint point) throws Throwable {
-        final long threadId = -1;
+        final long threadId = Thread.currentThread().getId();
 
         threadIdToStep.put(threadId, 0);
         threadIdToDebugLogId.compute(
@@ -64,7 +67,7 @@ public class GeneralFullFlowLoggingAspect extends BaseAspect {
      */
     @Around(WHITE_AND_BLACK_LIST)
     public Object around(final ProceedingJoinPoint point) throws Throwable {
-        final long threadId = -1;
+        final long threadId = Thread.currentThread().getId();
 
         return printDebugLogForMethod(point, threadId);
     }
@@ -86,7 +89,7 @@ public class GeneralFullFlowLoggingAspect extends BaseAspect {
      */
     @Around(FINISH_METHOD)
     public Object finishFlux(final ProceedingJoinPoint point) throws Throwable {
-        final long threadId = -1;
+        final long threadId = Thread.currentThread().getId();
 
         final Object resultFromMethod = printDebugLogForMethod(point, threadId);
 
