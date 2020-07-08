dea3b052b3bcd8acb2a1d884dbef11a6034ff436
Bradley Yoo <bradleyyoo@gmail.com>
2020-07-08 10:27:31 -0700

    fix test

--- underfs/s3a/src/test/java/alluxio/underfs/s3a/S3AUnderFileSystemTest.java
+++ underfs/s3a/src/test/java/alluxio/underfs/s3a/S3AUnderFileSystemTest.java
@@ -244,6 +244,6 @@ public class S3AUnderFileSystemTest {
   public void stripPrefixIfPresent() throws Exception {
     Assert.assertEquals("", mS3UnderFileSystem.stripPrefixIfPresent("s3a://" + BUCKET_NAME));
     Assert.assertEquals("", mS3UnderFileSystem.stripPrefixIfPresent("s3a://" + BUCKET_NAME + "/"));
-    Assert.assertEquals("test", mS3UnderFileSystem.stripPrefixIfPresent("test"));
+    Assert.assertEquals("test/", mS3UnderFileSystem.stripPrefixIfPresent("test"));
   }
 }
