67ef58ae8352e5ac0098b314aab2dc618a4bdc42
Abner Eduardo Ferreira <abner.ferreira@simbioseventures.com>
2020-07-30 17:18:54 -0300

    feat: add doc for alluxio.util.OSUtils.isWindows()

--- core/base/src/main/java/alluxio/util/OSUtils.java
+++ core/base/src/main/java/alluxio/util/OSUtils.java
@@ -37,7 +37,7 @@ public final class OSUtils {
   }
 
   /**
-   * @return true if current OS is Windows
+   * @return whether the current operating system (OS) is Windows
    */
   public static boolean isWindows() {
     return SystemUtils.IS_OS_WINDOWS;
