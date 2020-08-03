dc251a5d9f447f2c7872e8f8a48f9221c4888562
Abner Eduardo Ferreira <abner.ferreira@simbioseventures.com>
2020-08-03 15:47:20 -0300

    feat: add doc for alluxio.util.network.NettyUtils.isDomainSocketSupported(WorkerNetAddress)

--- core/common/src/main/java/alluxio/util/network/NettyUtils.java
+++ core/common/src/main/java/alluxio/util/network/NettyUtils.java
@@ -120,8 +120,9 @@ public final class NettyUtils {
   }
 
   /**
-   * @param workerNetAddress the worker address
-   * @return true if the domain socket is supported by the worker
+   * @param workerNetAddress the worker network address
+   * @return whether the domain socket is supported by the worker
+   *         with the provided network address
    */
   public static boolean isDomainSocketSupported(WorkerNetAddress workerNetAddress) {
     return !workerNetAddress.getDomainSocketPath().isEmpty();
