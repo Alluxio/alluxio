3f7990b95de293f184594b8b1b8cb4cd38b9c9d2
David Zhu <david@alluxio.com>
2020-08-03 11:28:49 -0700

    Add one more mvn parameter for running test to completion
    pr-link: Alluxio/alluxio#11882
    change-id: cid-2be6a003c8bcd9fa0c2f2ec0af4488ff47ba5d6b

--- dev/jenkins/build.sh
+++ dev/jenkins/build.sh
@@ -23,7 +23,7 @@ fi
 mvn_args=""
 if [ -n "${ALLUXIO_MVN_RUNTOEND}" ]
 then
-  mvn_args+=" -fn --fail-at-end"
+  mvn_args+=" -Dmaven.test.failure.ignore=true -fn --fail-at-end"
 fi
 
 RUN_MAVEN="false"
