3e3b6a6015a96f70e71af9c451e0b4c8ee0d6d62
Zac Blanco <zac@alluxio.com>
2020-08-10 16:22:03 -0700

    Enable the JVM pause monitor by default
    In addition to turning it on by default;
    - Many new tests were added. Code coverage is 82/89 (92%)
    - Minor tweaks and improvements to the class
    
    pr-link: Alluxio/alluxio#11814
    change-id: cid-63f260768f74e4a1cbd1777d57d12c45cfce6d5c

--- core/common/src/test/java/alluxio/util/JvmPauseMonitorTest.java
+++ core/common/src/test/java/alluxio/util/JvmPauseMonitorTest.java
@@ -11,39 +11,179 @@
 
 package alluxio.util;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
 
-import alluxio.conf.InstancedConfiguration;
-import alluxio.conf.PropertyKey;
+import alluxio.TestLoggerRule;
 
-import com.google.common.collect.Lists;
-import org.junit.Ignore;
+import org.apache.log4j.Level;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
-import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public final class JvmPauseMonitorTest {
-  @Test
-  @Ignore("https://alluxio.atlassian.net/browse/ALLUXIO-3059")
-  public void monitorTest() {
-    InstancedConfiguration conf = new InstancedConfiguration(ConfigurationUtils.defaults());
-    long gcSleepInterval = conf.getMs(PropertyKey.JVM_MONITOR_SLEEP_INTERVAL_MS);
-    long warnThreshold = conf.getMs(PropertyKey.JVM_MONITOR_WARN_THRESHOLD_MS);
-    long infoThreshold = conf.getMs(PropertyKey.JVM_MONITOR_INFO_THRESHOLD_MS);
 
-    JvmPauseMonitor jvmPauseMonitor = new JvmPauseMonitor(gcSleepInterval, warnThreshold,
-        infoThreshold);
-    jvmPauseMonitor.start();
-    List<String> list = Lists.newArrayList();
-    int i = 0;
-    while (true) {
-      list.add(String.valueOf(i++));
-      if (jvmPauseMonitor.getWarnTimeExceeded() != 0) {
-        jvmPauseMonitor.stop();
-        break;
+  @Rule
+  public TestLoggerRule mLogRule = new TestLoggerRule();
+
+  @Rule
+  public ExpectedException mException = ExpectedException.none();
+
+  @Test
+  public void pauseMonitorStartStopTest() {
+    JvmPauseMonitor mon = new JvmPauseMonitor(100, 1000, 500);
+    mon.start();
+    assertTrue(mon.isStarted());
+    mon.stop();
+    assertFalse(mon.isStarted());
+  }
+
+  @Test
+  public void interruptOnStop() throws Exception {
+    JvmPauseMonitor mon = Mockito.spy(new JvmPauseMonitor(10000, 900000, 90000));
+    CyclicBarrier barrier = new CyclicBarrier(2);
+    doAnswer((Answer<Void>) invocation -> {
+      barrier.await();
+      invocation.callRealMethod();
+      return null;
+    }).when(mon).sleepMillis(any(Long.class));
+    Thread current = Thread.currentThread();
+    Thread t1 = new Thread(() -> {
+      try {
+        barrier.await();
+        current.interrupt();
+      } catch (InterruptedException | BrokenBarrierException e) {
+        fail("Failed to await on barrier");
       }
+    });
+    t1.start();
+    mon.start();
+    barrier.await();
+    mon.stop();
+    assertTrue(Thread.currentThread().isInterrupted());
+    t1.join();
+  }
+
+  @Test
+  public void testNegativeGcSleep() {
+    mException.expect(IllegalArgumentException.class);
+    new JvmPauseMonitor(-1, 100, 500);
+  }
+
+  @Test
+  public void testNegativeInfoThreshold() {
+    mException.expect(IllegalArgumentException.class);
+    new JvmPauseMonitor(100, 500, -1);
+  }
+
+  @Test
+  public void testNegativeWarnThreshold() {
+    mException.expect(IllegalArgumentException.class);
+    new JvmPauseMonitor(100, -1, 500);
+  }
+
+  @Test
+  public void testTinyWarnThreshold() {
+    mException.expect(IllegalArgumentException.class);
+    new JvmPauseMonitor(100, 50, 5000);
+  }
+
+  /**
+   * This test mocks the {@link JvmPauseMonitor#sleepMillis(long)} (long)} method to simulate a
+   * pause which logs at the INFO level.
+   */
+  @Test
+  public void testMockedInfoPause() throws Exception {
+    JvmPauseMonitor mon = Mockito.spy(new JvmPauseMonitor(100, 500, 250));
+    CyclicBarrier before = new CyclicBarrier(2);
+    doAnswer((Answer<Void>) invocation -> {
+      Thread.sleep(250);
+      invocation.callRealMethod();
+      before.await();
+      return null;
+    }).when(mon).sleepMillis(any(Long.class));
+    mon.start();
+    before.await(); // runs the monitor once
+    // wait until it reaches the barrier again
+    while (before.getNumberWaiting() < 1) {
+      Thread.sleep(20);
     }
-    assertNotEquals(jvmPauseMonitor.getWarnTimeExceeded(), 0);
-    assertNotEquals(jvmPauseMonitor.getTotalExtraTime(), 0);
+    assertEquals(1, mon.getInfoTimeExceeded());
+    assertEquals(0, mon.getWarnTimeExceeded());
+    assertEquals(250, mon.getTotalExtraTime(), 25);
+    assertTrue(mLogRule.wasLoggedWithLevel("JVM paused.*\n.*\n.*", Level.INFO));
+    assertEquals(1, mLogRule.logCount("JVM paused.*\n.*\n.*"));
+    before.await(); // runs the monitor once
+    // wait until it reaches the barrier again
+    while (before.getNumberWaiting() < 1) {
+      Thread.sleep(20);
+    }
+    assertEquals(2, mon.getInfoTimeExceeded());
+    assertEquals(0, mon.getWarnTimeExceeded());
+    assertEquals(500, mon.getTotalExtraTime(), 50);
+    assertEquals(2, mLogRule.logCount("JVM paused.*\n.*\n.*"));
+    before.await(); // runs the monitor once
+    // wait until it reaches the barrier again
+    while (before.getNumberWaiting() < 1) {
+      Thread.sleep(20);
+    }
+    assertEquals(3, mon.getInfoTimeExceeded());
+    assertEquals(0, mon.getWarnTimeExceeded());
+    assertEquals(750, mon.getTotalExtraTime(), 75);
+    assertEquals(3, mLogRule.logCount("JVM paused.*\n.*\n.*"));
+  }
+
+  /**
+   * This test mocks the {@link JvmPauseMonitor#sleepMillis(long)} (long)} method to simulate a
+   * pause which logs at the WARN level.
+   */
+  @Test
+  public void testMockedWarnPause() throws Exception {
+    JvmPauseMonitor mon = Mockito.spy(new JvmPauseMonitor(100, 200, 150));
+    CyclicBarrier before = new CyclicBarrier(2);
+    doAnswer((Answer<Void>) invocation -> {
+      Thread.sleep(250);
+      invocation.callRealMethod();
+      before.await();
+      return null;
+    }).when(mon).sleepMillis(any(Long.class));
+    mon.start();
+    before.await(); // runs the monitor once
+    // wait until it reaches the barrier again
+    while (before.getNumberWaiting() < 1) {
+      Thread.sleep(20);
+    }
+    assertEquals(1, mon.getInfoTimeExceeded());
+    assertEquals(1, mon.getWarnTimeExceeded());
+    assertEquals(250, mon.getTotalExtraTime(), 25);
+    assertTrue(mLogRule.wasLoggedWithLevel("JVM paused.*\n.*\n.*", Level.WARN));
+    assertEquals(1, mLogRule.logCount("JVM paused.*\n.*\n.*"));
+    before.await(); // runs the monitor once
+    // wait until it reaches the barrier again
+    while (before.getNumberWaiting() < 1) {
+      Thread.sleep(20);
+    }
+    assertEquals(2, mon.getInfoTimeExceeded());
+    assertEquals(2, mon.getWarnTimeExceeded());
+    assertEquals(500, mon.getTotalExtraTime(), 50);
+    assertEquals(2, mLogRule.logCount("JVM paused.*\n.*\n.*"));
+    before.await(); // runs the monitor once
+    // wait until it reaches the barrier again
+    while (before.getNumberWaiting() < 1) {
+      Thread.sleep(20);
+    }
+    assertEquals(3, mon.getInfoTimeExceeded());
+    assertEquals(3, mon.getWarnTimeExceeded());
+    assertEquals(750, mon.getTotalExtraTime(), 75);
+    assertEquals(3, mLogRule.logCount("JVM paused.*\n.*\n.*"));
   }
 }
