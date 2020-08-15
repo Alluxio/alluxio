7e8a5a630368f0b18dce0d958624138ec5b14fca
David Zhu <david@alluxio.com>
2020-08-14 22:21:39 -0700

    Test skip compile
    Since we compile everything with java 8, we want to force java 11 to use
    our java 8 compilation result. So we skip compilation stage for the java
    11 maven invocation. This also reduces testing time from 28 mins to 18
    mins.
    
    pr-link: Alluxio/alluxio#11993
    change-id: cid-59771d8a4051c318637e69a1be39eb07e6925914

--- dev/jenkins/build.sh
+++ dev/jenkins/build.sh
@@ -79,7 +79,7 @@ if [ "$RUN_MAVEN" == "true" ]; then
   # Revert back to the image default java version to run the test
   JAVA_HOME=${JAVA_HOME_BACKUP}
   PATH=${PATH_BACKUP}
-  mvn -Duser.home=/home/jenkins -T 4C test -Pdeveloper -Dskip.protoc=true  -Dmaven.javadoc.skip -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dsurefire.forkCount=${ALLUXIO_BUILD_FORKCOUNT} ${mvn_args} $@
+  mvn -Duser.home=/home/jenkins -T 4C test -Pdeveloper -Dmaven.main.skip -Dskip.protoc=true  -Dmaven.javadoc.skip -Dlicense.skip=true -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dsurefire.forkCount=${ALLUXIO_BUILD_FORKCOUNT} ${mvn_args} $@
 
   if [ -n "${ALLUXIO_SONAR_ARGS}" ]
   then
