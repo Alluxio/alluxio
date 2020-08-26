43058818ced32324ecc08f595713efe0cc94afd3
Bradley Yoo <bradleyyoo@gmail.com>
2020-08-26 13:48:40 -0700

    reduce fork count to 1

--- dev/github/run_tests.sh
+++ dev/github/run_tests.sh
@@ -81,7 +81,7 @@ if [ "$RUN_MAVEN" == "true" ]; then
   # JAVA_HOME=${JAVA_HOME_BACKUP}
   PATH=${PATH_BACKUP}
   mvn -Duser.home=/home/jenkins -T 2C test -Pdeveloper -Dmaven.main.skip -Dskip.protoc=true  -Dmaven.javadoc.skip -Dlicense.skip=true \
-  -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dsurefire.forkCount=2 ${mvn_args} $@
+  -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dsurefire.forkCount=1 ${mvn_args} $@
 
   if [ -n "${ALLUXIO_SONAR_ARGS}" ]
   then
