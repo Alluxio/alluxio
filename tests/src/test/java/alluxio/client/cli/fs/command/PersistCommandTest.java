b3fa938ea4ad304c255cbdd6c4e41851cb582889
Bradley Yoo <bradleyyoo@gmail.com>
2020-08-31 18:44:48 -0700

    checkstyle

--- tests/src/test/java/alluxio/client/cli/fs/command/PersistCommandTest.java
+++ tests/src/test/java/alluxio/client/cli/fs/command/PersistCommandTest.java
@@ -102,9 +102,9 @@ public final class PersistCommandTest extends AbstractFileSystemShellTest {
       Assert.assertEquals(mOutput.toString(), 0, ret);
       CommonUtils.waitFor("Directory and its content to be persisted", () -> {
         try {
-          return sFileSystem.getStatus(new AlluxioURI(persisted)).isPersisted() &&
-              sFileSystem.getStatus(new AlluxioURI(persisted + "/foobar1")).isPersisted() &&
-              sFileSystem.getStatus(new AlluxioURI(persisted + "/foobar2")).isPersisted();
+          return sFileSystem.getStatus(new AlluxioURI(persisted)).isPersisted()
+              && sFileSystem.getStatus(new AlluxioURI(persisted + "/foobar1")).isPersisted()
+              && sFileSystem.getStatus(new AlluxioURI(persisted + "/foobar2")).isPersisted();
         } catch (Exception e) {
           return false;
         }
