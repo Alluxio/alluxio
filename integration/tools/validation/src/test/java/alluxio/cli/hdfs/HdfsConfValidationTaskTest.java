11fb1ce2b12e52fcba04f6e2ed3e547159808b7a
David Zhu <david@alluxio.com>
2020-09-29 11:09:11 -0700

    Skip tests with a warning when the config files are not readable
    pr-link: Alluxio/alluxio#12155
    change-id: cid-617a82864a531c511db20a01cc7652dd98d1a275

--- integration/tools/validation/src/test/java/alluxio/cli/hdfs/HdfsConfValidationTaskTest.java
+++ integration/tools/validation/src/test/java/alluxio/cli/hdfs/HdfsConfValidationTaskTest.java
@@ -67,7 +67,7 @@ public class HdfsConfValidationTaskTest {
     sConf.set(PropertyKey.UNDERFS_HDFS_CONFIGURATION, hdfsSite);
     HdfsConfValidationTask task = new HdfsConfValidationTask("hdfs://namenode:9000/alluxio", sConf);
     ValidationTaskResult result = task.loadHdfsConfig();
-    assertEquals(result.getState(), ValidationUtils.State.FAILED);
+    assertEquals(result.getState(), ValidationUtils.State.SKIPPED);
     assertThat(result.getResult(), containsString("core-site.xml is not configured"));
     assertThat(result.getAdvice(), containsString("core-site.xml"));
   }
@@ -81,7 +81,7 @@ public class HdfsConfValidationTaskTest {
     sConf.set(PropertyKey.UNDERFS_HDFS_CONFIGURATION, coreSite);
     HdfsConfValidationTask task = new HdfsConfValidationTask("hdfs://namenode:9000/alluxio", sConf);
     ValidationTaskResult result = task.loadHdfsConfig();
-    assertEquals(result.getState(), ValidationUtils.State.FAILED);
+    assertEquals(result.getState(), ValidationUtils.State.SKIPPED);
     assertThat(result.getResult(), containsString("hdfs-site.xml is not configured"));
     assertThat(result.getAdvice(), containsString("hdfs-site.xml"));
   }
@@ -91,7 +91,7 @@ public class HdfsConfValidationTaskTest {
     sConf.set(PropertyKey.UNDERFS_HDFS_CONFIGURATION, "/conf/");
     HdfsConfValidationTask task = new HdfsConfValidationTask("hdfs://namenode:9000/alluxio", sConf);
     ValidationTaskResult result = task.loadHdfsConfig();
-    assertEquals(result.getState(), ValidationUtils.State.FAILED);
+    assertEquals(result.getState(), ValidationUtils.State.SKIPPED);
     assertThat(result.getResult(), containsString("hdfs-site.xml is not configured"));
     assertThat(result.getResult(), containsString("core-site.xml is not configured"));
     assertThat(result.getAdvice(), containsString("hdfs-site.xml"));
