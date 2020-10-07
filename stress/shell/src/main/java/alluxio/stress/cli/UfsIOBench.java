2b319e2be4676ef59e7bc051fdb651d93af9de5c
David Zhu <david@alluxio.com>
2020-10-07 15:36:28 -0700

    fix checkstyle

--- stress/shell/src/main/java/alluxio/stress/cli/UfsIOBench.java
+++ stress/shell/src/main/java/alluxio/stress/cli/UfsIOBench.java
@@ -29,7 +29,6 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
