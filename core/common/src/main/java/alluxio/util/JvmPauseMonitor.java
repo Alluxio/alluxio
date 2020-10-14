aed133bbc30abb4e3fb6a53127630f70817b7639
Bradley Yoo <bradleyyoo@gmail.com>
2020-10-13 17:52:27 -0700

    Revert "Wait at most 60 seconds to join the JvmMonitorThread"
    This reverts commit 3841d843483fbb885d857bc6bbdb373a1ed1a4c9.

--- core/common/src/main/java/alluxio/util/JvmPauseMonitor.java
+++ core/common/src/main/java/alluxio/util/JvmPauseMonitor.java
@@ -93,7 +93,7 @@ public class JvmPauseMonitor {
     }
     mJvmMonitorThread.interrupt();
     try {
-      mJvmMonitorThread.join(60 * 1000);
+      mJvmMonitorThread.join();
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
     }
