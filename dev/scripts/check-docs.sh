d367362d3be36578f5db731dc50b5ba26b90dfa6
Rico Chiu <Xenorith@users.noreply.github.com>
2020-10-15 16:29:49 -0700

    Add --trace to jekyll build cmd to display error
    @bf8086 this will display a stacktrace when `jekyll build` fails within
    docs check
    
    pr-link: Alluxio/alluxio#12323
    change-id: cid-f17a05f7bd88c8cbc1bda9712dd58161eb166f51

--- dev/scripts/check-docs.sh
+++ dev/scripts/check-docs.sh
@@ -20,5 +20,5 @@ SCRIPT_DIR=$(cd "$( dirname "$( readlink "$0" || echo "$0" )" )"; pwd)
 
 GOPATH="${SCRIPT_DIR}" go run "${SCRIPT_DIR}/src/alluxio.org/check-docs/main.go"
 
-cd "${SCRIPT_DIR}"/../../docs && jekyll build
+cd "${SCRIPT_DIR}"/../../docs && jekyll build --trace
 
