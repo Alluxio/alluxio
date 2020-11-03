43418ee791a67083df7ff1eb2c25a240fc7817a5
Bradley Yoo <bradleyyoo@gmail.com>
2020-11-03 11:04:49 -0800

    Improve implementation of DistributedCp and other distributed jobs
    Few Changes:
    
    1. DistributedMv is now DistributedCp + Delete (the ASYNC_THROUGH
    writeType safe guard for persisted file is now removed)
    2. DistributedCp now pre-creates all of the folders from the client
    3. DistributedCp now submits the request one file at a time.
    4. Removed CheckMigrateValid because once you submit files one at a
    time, checkMigrateValid calls getStatus on a missing file each time
    which is very expensive.
    5. The behavior of file overwriting another file used to be broken and
    is still broken in this implementation
    
    pr-link: Alluxio/alluxio#12394
    change-id: cid-f85192e2d2a6833a3a34af08bdf7743fcc62f63b

--- shell/src/main/java/alluxio/cli/fs/command/DistributedCpCommand.java
+++ shell/src/main/java/alluxio/cli/fs/command/DistributedCpCommand.java
@@ -12,17 +12,25 @@
 package alluxio.cli.fs.command;
 
 import alluxio.AlluxioURI;
-import alluxio.Constants;
 import alluxio.annotation.PublicApi;
 import alluxio.cli.CommandUtils;
+import alluxio.cli.fs.command.job.JobAttempt;
 import alluxio.client.file.FileSystemContext;
-import alluxio.client.job.JobGrpcClientUtils;
+import alluxio.client.file.URIStatus;
+import alluxio.client.job.JobMasterClient;
 import alluxio.conf.AlluxioConfiguration;
 import alluxio.conf.PropertyKey;
 import alluxio.exception.AlluxioException;
+import alluxio.exception.ExceptionMessage;
+import alluxio.exception.FileAlreadyExistsException;
+import alluxio.exception.InvalidPathException;
 import alluxio.exception.status.InvalidArgumentException;
+import alluxio.job.JobConfig;
 import alluxio.job.plan.migrate.MigrateConfig;
-import alluxio.util.CommonUtils;
+import alluxio.job.wire.JobInfo;
+import alluxio.retry.CountingRetry;
+import alluxio.retry.RetryPolicy;
+import alluxio.util.io.PathUtils;
 
 import org.apache.commons.cli.CommandLine;
 
@@ -35,7 +43,8 @@ import javax.annotation.concurrent.ThreadSafe;
  */
 @ThreadSafe
 @PublicApi
-public final class DistributedCpCommand extends AbstractFileSystemCommand {
+public class DistributedCpCommand extends AbstractDistributedJobCommand {
+  private String mWriteType;
 
   /**
    * @param fsContext the filesystem context of Alluxio
@@ -59,21 +68,81 @@ public final class DistributedCpCommand extends AbstractFileSystemCommand {
     String[] args = cl.getArgs();
     AlluxioURI srcPath = new AlluxioURI(args[0]);
     AlluxioURI dstPath = new AlluxioURI(args[1]);
-    Thread thread = CommonUtils.createProgressThread(2L * Constants.SECOND_MS, System.out);
-    thread.start();
-    try {
-      AlluxioConfiguration conf = mFsContext.getPathConf(dstPath);
-      JobGrpcClientUtils.run(new MigrateConfig(srcPath.getPath(), dstPath.getPath(),
-          conf.get(PropertyKey.USER_FILE_WRITE_TYPE_DEFAULT), true,
-          false), 1, mFsContext.getPathConf(dstPath));
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      return -1;
-    } finally {
-      thread.interrupt();
+
+    if (PathUtils.hasPrefix(dstPath.toString(), srcPath.toString())) {
+      throw new RuntimeException(ExceptionMessage.MIGRATE_CANNOT_BE_TO_SUBDIRECTORY.getMessage(
+          srcPath, dstPath));
     }
-    System.out.println("Copied " + srcPath + " to " + dstPath);
+
+    AlluxioConfiguration conf = mFsContext.getPathConf(dstPath);
+    mWriteType = conf.get(PropertyKey.USER_FILE_WRITE_TYPE_DEFAULT);
+
+    distributedCp(srcPath, dstPath);
     return 0;
+  }
+
+  private CopyJobAttempt newJob(String srcPath, String dstPath) {
+    CopyJobAttempt jobAttempt = new CopyJobAttempt(mClient,
+        new MigrateConfig(srcPath, dstPath, mWriteType, true, false),
+        new CountingRetry(3));
+
+    jobAttempt.run();
+
+    return jobAttempt;
+  }
+
+  private void distributedCp(AlluxioURI srcPath, AlluxioURI dstPath)
+      throws IOException, AlluxioException {
+    if (mFileSystem.getStatus(srcPath).isFolder()) {
+      createFolders(srcPath, dstPath);
+    }
+    copy(srcPath, dstPath);
+    // Wait remaining jobs to complete.
+    drain();
+  }
+
+  private void createFolders(AlluxioURI srcPath, AlluxioURI dstPath)
+      throws IOException, AlluxioException {
+
+    try {
+      mFileSystem.createDirectory(dstPath);
+      System.out.println("Created directory at " + dstPath.getPath());
+    } catch (FileAlreadyExistsException e) {
+      if (!mFileSystem.getStatus(dstPath).isFolder()) {
+        throw e;
+      }
+    }
+
+    for (URIStatus srcInnerStatus : mFileSystem.listStatus(srcPath)) {
+      if (srcInnerStatus.isFolder()) {
+        String dstInnerPath = computeTargetPath(srcInnerStatus.getPath(),
+            srcPath.getPath(), dstPath.getPath());
+        createFolders(new AlluxioURI(srcInnerStatus.getPath()), new AlluxioURI(dstInnerPath));
+      }
+    }
+  }
+
+  private void copy(AlluxioURI srcPath, AlluxioURI dstPath)
+      throws IOException, AlluxioException {
+
+    for (URIStatus srcInnerStatus : mFileSystem.listStatus(srcPath)) {
+      String dstInnerPath = computeTargetPath(srcInnerStatus.getPath(),
+          srcPath.getPath(), dstPath.getPath());
+      if (srcInnerStatus.isFolder()) {
+        copy(new AlluxioURI(srcInnerStatus.getPath()), new AlluxioURI(dstInnerPath));
+      } else {
+        addJob(srcInnerStatus.getPath(), dstInnerPath);
+      }
+    }
+  }
+
+  private void addJob(String srcPath, String dstPath) {
+    if (mSubmittedJobAttempts.size() >= mActiveJobs) {
+      // Wait one job to complete.
+      waitJob();
+    }
+    mSubmittedJobAttempts.add(newJob(srcPath, dstPath));
+    System.out.println("Copying " + srcPath + " to " + dstPath);
   }
 
   @Override
@@ -84,5 +153,45 @@ public final class DistributedCpCommand extends AbstractFileSystemCommand {
   @Override
   public String getDescription() {
     return "Copies a file or directory in parallel at file level.";
+  }
+
+  private static String computeTargetPath(String path, String source, String destination)
+      throws InvalidPathException {
+    String relativePath = PathUtils.subtractPaths(path, source);
+
+    return PathUtils.concatPath(destination, relativePath);
+  }
+
+  private class CopyJobAttempt extends JobAttempt {
+    private MigrateConfig mJobConfig;
+
+    CopyJobAttempt(JobMasterClient client, MigrateConfig jobConfig, RetryPolicy retryPolicy) {
+      super(client, retryPolicy);
+      mJobConfig = jobConfig;
+    }
+
+    @Override
+    protected JobConfig getJobConfig() {
+      return mJobConfig;
+    }
+
+    @Override
+    public void logFailedAttempt(JobInfo jobInfo) {
+      System.out.println(String.format("Attempt %d to copy %s to %s failed because: %s",
+          mRetryPolicy.getAttemptCount(), mJobConfig.getSource(), mJobConfig.getDestination(),
+          jobInfo.getErrorMessage()));
+    }
+
+    @Override
+    protected void logFailed() {
+      System.out.println(String.format("Failed to complete copying %s to %s after %d retries.",
+          mJobConfig.getSource(), mJobConfig.getDestination(), mRetryPolicy.getAttemptCount()));
+    }
+
+    @Override
+    public void logCompleted() {
+      System.out.println(String.format("Successfully copied %s to %s after %d attempts",
+          mJobConfig.getSource(), mJobConfig.getDestination(), mRetryPolicy.getAttemptCount()));
+    }
   }
 }
