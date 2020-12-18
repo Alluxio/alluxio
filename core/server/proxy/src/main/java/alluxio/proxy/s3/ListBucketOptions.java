2183a11f31549ebda7a729a48cabab81fe3585c5
Bradley Yoo <bradleyyoo@gmail.com>
2020-12-18 11:35:03 -0800

    Fix List Buckets to be more correct

--- core/server/proxy/src/main/java/alluxio/proxy/s3/ListBucketOptions.java
+++ core/server/proxy/src/main/java/alluxio/proxy/s3/ListBucketOptions.java
@@ -18,9 +18,10 @@ import com.google.common.base.Objects;
  * The options for list bucket operation.
  */
 public final class ListBucketOptions {
-  private String mContinuationToken = null;
+  private String mMarker = null;
   private String mMaxKeys = null;
   private String mPrefix = null;
+  private String mDelimiter = null;
 
   /**
    * Creates a default {@link ListBucketOptions}.
@@ -39,8 +40,8 @@ public final class ListBucketOptions {
   /**
    * @return the continuation token
    */
-  public String getContinuationToken() {
-    return mContinuationToken;
+  public String getMarker() {
+    return mMarker;
   }
 
   /**
@@ -58,11 +59,18 @@ public final class ListBucketOptions {
   }
 
   /**
-   * @param continuationToken the continuation token to set
+   * @return the delimiter
+   */
+  public String getDelimiter() {
+    return mDelimiter;
+  }
+
+  /**
+   * @param marker the marker to set
    * @return the updated object
    */
-  public ListBucketOptions setContinuationToken(String continuationToken) {
-    mContinuationToken = continuationToken;
+  public ListBucketOptions setMarker(String marker) {
+    mMarker = marker;
     return this;
   }
 
@@ -84,6 +92,15 @@ public final class ListBucketOptions {
     return this;
   }
 
+  /**
+   * @param delimiter the delimiter
+   * @return the updated object
+   */
+  public ListBucketOptions setDelimiter(String delimiter) {
+    mDelimiter = delimiter;
+    return this;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -93,20 +110,20 @@ public final class ListBucketOptions {
       return false;
     }
     ListBucketOptions that = (ListBucketOptions) o;
-    return Objects.equal(mContinuationToken, that.mContinuationToken)
+    return Objects.equal(mMarker, that.mMarker)
         && Objects.equal(mMaxKeys, that.mMaxKeys)
         && Objects.equal(mPrefix, that.mPrefix);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(mContinuationToken, mMaxKeys, mPrefix);
+    return Objects.hashCode(mMarker, mMaxKeys, mPrefix);
   }
 
   @Override
   public String toString() {
     return MoreObjects.toStringHelper(this)
-        .add("continuationToken", mContinuationToken)
+        .add("continuationToken", mMarker)
         .add("maxKeys", mMaxKeys)
         .add("prefix", mPrefix)
         .toString();
