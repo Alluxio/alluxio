0c72e4e63688dbee776b24139f86cf2b495150cd
Jiacheng Liu <jiacheliu3@gmail.com>
2021-02-15 17:58:39 +0800

    Support logLevel for job master/worker
    `logLevel` command currently only works for master and workers. This
    change enables the command to set log level on job master/workers.
    
    pr-link: Alluxio/alluxio#12844
    change-id: cid-77b755207b785f88a50d1d889d6cb0316c6f8cb9

--- job/server/src/main/java/alluxio/worker/AlluxioJobWorkerRestServiceHandler.java
+++ job/server/src/main/java/alluxio/worker/AlluxioJobWorkerRestServiceHandler.java
@@ -15,6 +15,7 @@ import alluxio.conf.ServerConfiguration;
 import alluxio.conf.PropertyKey;
 import alluxio.RestUtils;
 import alluxio.RuntimeConstants;
+import alluxio.util.LogUtils;
 import alluxio.web.JobWorkerWebServer;
 import alluxio.wire.AlluxioJobWorkerInfo;
 
@@ -30,6 +31,7 @@ import java.util.TreeMap;
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -46,6 +48,10 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public final class  AlluxioJobWorkerRestServiceHandler {
   public static final String SERVICE_PREFIX = "job_worker";
+  // log
+  public static final String LOG_LEVEL = "logLevel";
+  public static final String LOG_ARGUMENT_NAME = "logName";
+  public static final String LOG_ARGUMENT_LEVEL = "level";
 
   // endpoints
   public static final String GET_INFO = "info";
@@ -91,6 +97,20 @@ public final class  AlluxioJobWorkerRestServiceHandler {
               .setVersion(RuntimeConstants.VERSION);
       return result;
     }, ServerConfiguration.global());
+  }
+
+  /**
+   * @summary set the Alluxio log information
+   * @param logName the log's name
+   * @param level the log level
+   * @return the response object
+   */
+  @POST
+  @Path(LOG_LEVEL)
+  public Response logLevel(@QueryParam(LOG_ARGUMENT_NAME) final String logName,
+                           @QueryParam(LOG_ARGUMENT_LEVEL) final String level) {
+    return RestUtils.call(() -> LogUtils.setLogLevel(logName, level),
+            ServerConfiguration.global());
   }
 
   private Map<String, String> getConfigurationInternal(boolean raw) {
