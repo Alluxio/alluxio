3ef0b4d7194024c880bccee1616e014abaf2fafe
Bin Fan <fanbin103@gmail.com>
2021-02-14 18:11:26 -0800

    Support JNIFuse on macOS
    A few improvements:
    
    1. Fix https://github.com/Alluxio/alluxio/issues/12720 to support using
    JNI fuse on MacOS
    2. Fix umount issue on MacOS
    
    pr-link: Alluxio/alluxio#12836
    change-id: cid-560e0976c31ad3b9bb702f8f133ddb01ce5be472

--- integration/fuse/src/main/java/alluxio/jnifuse/struct/Statvfs.java
+++ integration/fuse/src/main/java/alluxio/jnifuse/struct/Statvfs.java
@@ -11,54 +11,25 @@
 
 package alluxio.jnifuse.struct;
 
+import jnr.ffi.Runtime;
+
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
-public class Statvfs extends Struct {
-  /* Definitions for the flag in `f_flag'. */
-  public static final int ST_RDONLY = 1; /* Mount read-only. */
-  public static final int ST_NOSUID = 2; /* Ignore suid and sgid bits. */
-  public static final int ST_NODEV = 4; /* Disallow access to device special files. */
-  public static final int ST_NOEXEC = 8; /* Disallow program execution. */
-  public static final int ST_SYNCHRONOUS = 16;/* Writes are synced at once. */
-  public static final int ST_MANDLOCK = 64; /* Allow mandatory locks on an FS. */
-  public static final int ST_WRITE = 128; /* Write on file/directory/symlink. */
-  public static final int ST_APPEND = 256; /* Append-only file. */
-  public static final int ST_IMMUTABLE = 512; /* Immutable file. */
-  public static final int ST_NOATIME = 1024; /* Do not update access times. */
-  public static final int ST_NODIRATIME = 2048;/* Do not update directory access times. */
-  public static final int ST_RELATIME = 4096; /* Update atime relative to mtime/ctime. */
+public class Statvfs extends ru.serce.jnrfuse.struct.Statvfs {
+  private final ByteBuffer buffer;
 
-  public Statvfs(ByteBuffer buffer) {
-    super(buffer);
-    f_bsize = new UnsignedLong();
-    f_frsize = new UnsignedLong();
-    f_blocks = new UnsignedLong();
-    f_bfree = new UnsignedLong();
-    f_bavail = new UnsignedLong();
-    f_files = new UnsignedLong();
-    f_ffree = new UnsignedLong();
-    f_favail = new UnsignedLong();
-    f_fsid = new UnsignedLong();
-    f_unused = null;
-    f_flag = new UnsignedLong();
-    f_namemax = new UnsignedLong();
+  public Statvfs(Runtime runtime, ByteBuffer buffer) {
+    super(runtime);
+    this.buffer = buffer;
+    // depends on the arch
+    this.buffer.order(ByteOrder.LITTLE_ENDIAN);
   }
 
-  public final UnsignedLong f_bsize;
-  public final UnsignedLong f_frsize;
-  public final UnsignedLong f_blocks;
-  public final UnsignedLong f_bfree;
-  public final UnsignedLong f_bavail;
-  public final UnsignedLong f_files;
-  public final UnsignedLong f_ffree;
-  public final UnsignedLong f_favail;
-  public final UnsignedLong f_fsid;
-  public final Signed32 f_unused;
-  public final UnsignedLong f_flag;
-  public final UnsignedLong f_namemax;
-  // __f_spare
-
-  public static Statvfs wrap(ByteBuffer buffer) {
-    return new Statvfs(buffer);
+  public static Statvfs of(ByteBuffer buffer) {
+    Runtime runtime = Runtime.getSystemRuntime();
+    Statvfs statvfs = new Statvfs(runtime, buffer);
+    statvfs.useMemory(jnr.ffi.Pointer.wrap(runtime, buffer));
+    return statvfs;
   }
 }
