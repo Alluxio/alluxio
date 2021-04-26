9dbbcf257d0b22149ec02dfba5e1f2ceead3e6b8
Eugene-Mark <eugene.ma@intel.com>
2021-04-26 19:12:10 +0800

    Fix the issue reported by checking style tool

--- core/server/worker/src/main/java/alluxio/worker/block/BlockMetadataManager.java
+++ core/server/worker/src/main/java/alluxio/worker/block/BlockMetadataManager.java
@@ -20,28 +20,30 @@ import alluxio.exception.BlockDoesNotExistException;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.InvalidWorkerStateException;
 import alluxio.exception.WorkerOutOfSpaceException;
-import alluxio.worker.block.annotator.BlockAnnotator;
-import alluxio.worker.block.annotator.BlockIterator;
-import alluxio.worker.block.annotator.DefaultBlockIterator;
+import alluxio.worker.block.allocator.Allocator;
+import alluxio.worker.block.evictor.Evictor;
 import alluxio.worker.block.meta.BlockMeta;
 import alluxio.worker.block.meta.DefaultBlockMeta;
 import alluxio.worker.block.meta.DefaultStorageTier;
 import alluxio.worker.block.meta.StorageDir;
 import alluxio.worker.block.meta.StorageTier;
 import alluxio.worker.block.meta.TempBlockMeta;
+import alluxio.worker.block.annotator.BlockIterator;
+import alluxio.worker.block.annotator.DefaultBlockIterator;
+import alluxio.worker.block.annotator.BlockAnnotator;
+
 import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.NotThreadSafe;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-
 
 /**
  * Manages the metadata of all blocks in managed space. This information is used by the
@@ -77,7 +79,7 @@ public final class BlockMetadataManager {
         mTiers.add(tier);
         mAliasToTiers.put(tier.getTierAlias(), tier);
       }
-      // Create the block iterator.
+
       if (ServerConfiguration.isSet(PropertyKey.WORKER_EVICTOR_CLASS)) {
         LOG.warn(String.format("Evictor is being emulated. Please use %s instead.",
             PropertyKey.Name.WORKER_BLOCK_ANNOTATOR_CLASS));
