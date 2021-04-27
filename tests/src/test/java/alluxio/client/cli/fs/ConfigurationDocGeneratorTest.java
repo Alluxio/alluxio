ab38fde5b45149a210989a7e4f77325b1e43c1c5
Limuzhou <mul023@ucsd.edu>
2021-04-27 14:25:02 -0700

    test fix

--- tests/src/test/java/alluxio/client/cli/fs/ConfigurationDocGeneratorTest.java
+++ tests/src/test/java/alluxio/client/cli/fs/ConfigurationDocGeneratorTest.java
@@ -120,7 +120,7 @@ public class ConfigurationDocGeneratorTest {
     PropertyKey pKey = mTestConf.getFirst();
     defaultKeys.add(pKey);
 
-    ConfigurationDocGenerator.writeCSVFile(defaultKeys, mLocation);
+    ConfigurationDocGenerator.writeCSVFile(defaultKeys, mLocation,true);
     String filePath = PathUtils.concatPath(mLocation, mTestConf.getSecond());
     Path p = Paths.get(filePath);
     assertTrue(Files.exists(p));
@@ -141,7 +141,7 @@ public class ConfigurationDocGeneratorTest {
     String description = pKey.getDescription();
     defaultKeys.add(pKey);
 
-    ConfigurationDocGenerator.writeYMLFile(defaultKeys, mLocation);
+    ConfigurationDocGenerator.writeYMLFile(defaultKeys, mLocation,true);
     String filePath = PathUtils.concatPath(mLocation, mTestConf.getSecond());
     Path p = Paths.get(filePath);
     assertTrue(Files.exists(p));
