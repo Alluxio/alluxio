8626e1b346536691ae67faa4b27a54a7695749da
Bradley Yoo <bradleyyoo@gmail.com>
2021-05-04 16:45:05 -0700

    Cancel sync job in middle of ufs calls if client cancelled
    The previous version slowed down the sync because the number of prefetch
    threads were much smaller than the number of rpc threads that were
    performing a sync in many cases.
    
    pr-link: Alluxio/alluxio#13334
    change-id: cid-224a19d976bea2a55c6674d670861dad8d3a4c1f

--- tests/src/test/java/alluxio/client/fs/LoadMetadataIntegrationTest.java
+++ tests/src/test/java/alluxio/client/fs/LoadMetadataIntegrationTest.java
@@ -86,7 +86,8 @@ public class LoadMetadataIntegrationTest extends BaseIntegrationTest {
 
   @Rule
   public LocalAlluxioClusterResource mLocalAlluxioClusterResource =
-      new LocalAlluxioClusterResource.Builder().build();
+      new LocalAlluxioClusterResource.Builder()
+          .setProperty(PropertyKey.MASTER_METADATA_SYNC_UFS_PREFETCH_POOL_SIZE, 2).build();
 
   @ClassRule
   public static UnderFileSystemFactoryRegistryRule sUnderfilesystemfactoryregistry =
