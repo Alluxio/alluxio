Alluxio/alluxio

commit d0b28d048198b23553b90b66fba44f2327367ede

Cleanup counters in local client cache
two improvements:
1. Added new counter "Client.CachePutInsufficientSpaceErrors" indicating
the failed put due to insufficient space made after eviction attempts.
2. Remove unnecessary cleanup which may artificially bump error counters
3. Remove unnecessary directory cleanup which has potential race
condition to fail put unnecessarily

pr-link: Alluxio/alluxio#12813
change-id: cid-a655bc6404cd40d0bfbc2f3ac3104adf5b1b6d4f

core/client/fs/src/main/java/alluxio/client/file/cache/store/LocalPageStore.java
@@ -23,7 +23,6 @@ import org.slf4j.LoggerFactory;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -127,13 +126,6 @@ public class LocalPageStore implements PageStore {
       throw new PageNotFoundException(p.toString());
     }
     Files.delete(p);
-    Path parent = Preconditions.checkNotNull(p.getParent(),
-        "parent of cache file should not be null");
-    try (DirectoryStream<Path> stream = Files.newDirectoryStream(parent)) {
-      if (!stream.iterator().hasNext()) {
-        Files.delete(parent);
-      }
-    }
   }
 
   private Path getFilePath(PageId pageId) {

core/client/fs/src/main/java/alluxio/client/file/cache/LocalCacheManager.java
@@ -297,7 +297,7 @@ public class LocalCacheManager implements CacheManager {
       // note that, we only evict one item a time in putAttempt. So it is possible the evicted
       // page is not large enough to cover the space needed by this page. Try again
     }
-    Metrics.PUT_EVICTION_ERRORS.inc();
+    Metrics.PUT_INSUFFICIENT_SPACE_ERRORS.inc();
     return false;
   }
 
@@ -353,15 +353,12 @@ public class LocalCacheManager implements CacheManager {
       // metalock. Evictor will be updated inside metastore.
       try (LockResource r3 = new LockResource(mMetaLock.writeLock())) {
         if (mMetaStore.hasPage(pageId)) {
-          LOG.debug("{} is already inserted by a racing thread", pageId);
-          // TODO(binfan): we should return more informative result in the future
           return PutResult.OK;
         }
         try {
           mMetaStore.removePage(victimPageInfo.getPageId());
-        } catch (Exception e) {
-          undoAddPage(pageId);
-          LOG.error("Page {} is unavailable to evict, likely due to a benign race",
+        } catch (PageNotFoundException e) {
+          LOG.warn("Page {} is unavailable to evict, likely due to a benign race",
               victimPageInfo.getPageId());
           Metrics.PUT_BENIGN_RACING_ERRORS.inc();
           return PutResult.OTHER;
@@ -383,7 +380,7 @@ public class LocalCacheManager implements CacheManager {
           // Failed to evict page, remove new page from metastore as there will not be enough space
           undoAddPage(pageId);
         }
-        LOG.error("Failed to delete page {}: {}", pageId, e);
+        LOG.error("Failed to delete page {} from pageStore", pageId, e);
         Metrics.PUT_STORE_DELETE_ERRORS.inc();
         return PutResult.OTHER;
       }
@@ -470,7 +467,7 @@ public class LocalCacheManager implements CacheManager {
         try {
           mMetaStore.removePage(pageId);
         } catch (PageNotFoundException e) {
-          LOG.error("Failed to delete page {}: {}", pageId, e);
+          LOG.error("Failed to delete page {} from metaStore: {}", pageId, e);
           Metrics.DELETE_NON_EXISTING_PAGE_ERRORS.inc();
           Metrics.DELETE_ERRORS.inc();
           return false;
@@ -589,7 +586,7 @@ public class LocalCacheManager implements CacheManager {
     try {
       mPageStore.delete(pageId);
     } catch (IOException | PageNotFoundException e) {
-      LOG.error("Failed to delete page {}: {}", pageId, e);
+      LOG.error("Failed to delete page {} from pageStore: {}", pageId, e);
       return false;
     }
     return true;
@@ -661,6 +658,9 @@ public class LocalCacheManager implements CacheManager {
     /** Errors when adding pages due to benign racing eviction. */
     private static final Counter PUT_BENIGN_RACING_ERRORS =
         MetricsSystem.counter(MetricKey.CLIENT_CACHE_PUT_BENIGN_RACING_ERRORS.getName());
+    /** Errors when adding pages due to insufficient space made after eviction. */
+    private static final Counter PUT_INSUFFICIENT_SPACE_ERRORS =
+        MetricsSystem.counter(MetricKey.CLIENT_CACHE_PUT_INSUFFICIENT_SPACE_ERRORS.getName());
     /** Errors when cache is not ready to add pages. */
     private static final Counter PUT_NOT_READY_ERRORS =
         MetricsSystem.counter(MetricKey.CLIENT_CACHE_PUT_NOT_READY_ERRORS.getName());

core/common/src/main/java/alluxio/metrics/MetricKey.java
@@ -987,6 +987,13 @@ public final class MetricKey implements Comparable<MetricKey> {
           .setMetricType(MetricType.COUNTER)
           .setIsClusterAggregated(false)
           .build();
+  public static final MetricKey CLIENT_CACHE_PUT_INSUFFICIENT_SPACE_ERRORS =
+      new Builder(Name.CLIENT_CACHE_PUT_INSUFFICIENT_SPACE_ERRORS)
+          .setDescription("Number of failures when putting cached data in the client cache due to"
+              + " insufficient space made after eviction.")
+          .setMetricType(MetricType.COUNTER)
+          .setIsClusterAggregated(false)
+          .build();
   public static final MetricKey CLIENT_CACHE_PUT_BENIGN_RACING_ERRORS =
       new Builder(Name.CLIENT_CACHE_PUT_BENIGN_RACING_ERRORS)
           .setDescription("Number of failures when adding pages due to racing eviction. This error"
@@ -1284,6 +1291,8 @@ public final class MetricKey implements Comparable<MetricKey> {
         "Client.CachePutEvictionErrors";
     public static final String CLIENT_CACHE_PUT_BENIGN_RACING_ERRORS =
         "Client.CachePutBenignRacingErrors";
+    public static final String CLIENT_CACHE_PUT_INSUFFICIENT_SPACE_ERRORS =
+        "Client.CachePutInsufficientSpaceErrors";
     public static final String CLIENT_CACHE_PUT_NOT_READY_ERRORS =
         "Client.CachePutNotReadyErrors";
     public static final String CLIENT_CACHE_PUT_STORE_DELETE_ERRORS =

commit 6ae85e6a66602698635724d8cbe5e9c65e5d0dfa

Fix a regression of uncleaned directories
Fixed a regression introduced in #12813 , where I was fixing a potential
race (benign) on creating/removing local file system directories but
leaving an unbounded amount of directories.

pr-link: Alluxio/alluxio#13344
change-id: cid-7c38ecf96192134561aac8ef2146db6232a10b44

core/client/fs/src/main/java/alluxio/client/file/cache/store/LocalPageStore.java
@@ -17,6 +17,7 @@ import alluxio.client.file.cache.PageStore;
 import alluxio.exception.PageNotFoundException;
 import alluxio.exception.status.ResourceExhaustedException;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,7 @@ import org.slf4j.LoggerFactory;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -70,8 +72,8 @@ public class LocalPageStore implements PageStore {
     Path p = getFilePath(pageId);
     try {
       if (!Files.exists(p)) {
-        Path parent = Preconditions.checkNotNull(p.getParent(),
-            "parent of cache file should not be null");
+        Path parent =
+            Preconditions.checkNotNull(p.getParent(), "parent of cache file should not be null");
         Files.createDirectories(parent);
         Files.createFile(p);
       }
@@ -93,21 +95,22 @@ public class LocalPageStore implements PageStore {
   public int get(PageId pageId, int pageOffset, int bytesToRead, byte[] buffer, int bufferOffset)
       throws IOException, PageNotFoundException {
     Preconditions.checkArgument(pageOffset >= 0, "page offset should be non-negative");
-    Preconditions.checkArgument(buffer.length >= bufferOffset, "page offset %s should be "
-        + "less or equal than buffer length %s", bufferOffset, buffer.length);
+    Preconditions.checkArgument(buffer.length >= bufferOffset,
+        "page offset %s should be " + "less or equal than buffer length %s", bufferOffset,
+        buffer.length);
     Path p = getFilePath(pageId);
     if (!Files.exists(p)) {
       throw new PageNotFoundException(p.toString());
     }
     long pageLength = p.toFile().length();
-    Preconditions.checkArgument(pageOffset <= pageLength,
-        "page offset %s exceeded page size %s", pageOffset, pageLength);
+    Preconditions.checkArgument(pageOffset <= pageLength, "page offset %s exceeded page size %s",
+        pageOffset, pageLength);
     try (RandomAccessFile localFile = new RandomAccessFile(p.toString(), "r")) {
       int bytesSkipped = localFile.skipBytes(pageOffset);
       if (pageOffset != bytesSkipped) {
         throw new IOException(
-            String.format("Failed to read page %s (%s) from offset %s: %s bytes skipped", pageId,
-                p, pageOffset, bytesSkipped));
+            String.format("Failed to read page %s (%s) from offset %s: %s bytes skipped", pageId, p,
+                pageOffset, bytesSkipped));
       }
       int bytesRead = 0;
       int bytesLeft = (int) Math.min(pageLength - pageOffset, buffer.length - bufferOffset);
@@ -131,9 +134,31 @@ public class LocalPageStore implements PageStore {
       throw new PageNotFoundException(p.toString());
     }
     Files.delete(p);
+    // Cleaning up parent directory may lead to a race condition if one thread is removing a page as
+    // well as its parent dir corresponding to the fileId, while another thread is adding
+    // a different page from the same file in the same directory.
+    // Note that, because (1) the chance of this type of racing is really low and
+    // (2) even a race happens, the only penalty is an extra cache put failure;
+    // whereas without the cleanup, there can be an unbounded amount of empty directories
+    // uncleaned which takes an unbounded amount of space possibly.
+    // We have seen the overhead goes up to a few hundred GBs due to inode storage overhead
+    // TODO(binfan): remove the coupled fileId/pagIdex encoding with storage path, so the total
+    // number of directories can be bounded.
+    Path parent =
+        Preconditions.checkNotNull(p.getParent(), "parent of cache file should not be null");
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(parent)) {
+      if (!stream.iterator().hasNext()) {
+        Files.delete(parent);
+      }
+    }
   }
 
-  private Path getFilePath(PageId pageId) {
+  /**
+   * @param pageId page Id
+   * @return the local file system path to store this page
+   */
+  @VisibleForTesting
+  public Path getFilePath(PageId pageId) {
     // TODO(feng): encode fileId with URLEncoder to escape invalid characters for file name
     return Paths.get(mRoot, Long.toString(mPageSize), getFileBucket(pageId.getFileId()),
         pageId.getFileId(), Long.toString(pageId.getPageIndex()));
@@ -196,9 +221,7 @@ public class LocalPageStore implements PageStore {
   @Override
   public Stream<PageInfo> getPages() throws IOException {
     Path rootDir = Paths.get(mRoot);
-    return Files.walk(rootDir)
-          .filter(Files::isRegularFile)
-          .map(this::getPageInfo);
+    return Files.walk(rootDir).filter(Files::isRegularFile).map(this::getPageInfo);
   }
 
   @Override

core/client/fs/src/test/java/alluxio/client/file/cache/store/LocalPageStoreTest.java
@@ -13,6 +13,8 @@ package alluxio.client.file.cache.store;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import alluxio.client.file.cache.PageId;
 import alluxio.client.file.cache.PageStore;
@@ -23,6 +25,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
@@ -70,6 +73,18 @@ public class LocalPageStoreTest {
     }
     assertEquals(10, Files.list(
         Paths.get(mOptions.getRootDir(), Long.toString(mOptions.getPageSize()))).count());
+  }
+
+  @Test
+  public void cleanFileAndDirectory() throws Exception {
+    LocalPageStore pageStore = new LocalPageStore(mOptions);
+    PageId pageId = new PageId("0", 0);
+    pageStore.put(pageId, "test".getBytes());
+    Path p = pageStore.getFilePath(pageId);
+    assertTrue(Files.exists(p));
+    pageStore.delete(pageId);
+    assertFalse(Files.exists(p));
+    assertFalse(Files.exists(p.getParent()));
   }
 
   private void helloWorldTest(PageStore store) throws Exception {