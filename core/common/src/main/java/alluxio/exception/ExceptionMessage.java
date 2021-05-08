2b462da9bc92e2cc7062991fea35903dc27c9aac
BUPTAnderson <yunlongkong@126.com>
2021-05-08 11:50:12 +0800

    Fix Alluxio/new-contributor-tasks#576

--- core/common/src/main/java/alluxio/exception/ExceptionMessage.java
+++ core/common/src/main/java/alluxio/exception/ExceptionMessage.java
@@ -62,7 +62,6 @@ public enum ExceptionMessage {
 
   // instream/outstream
   FAILED_CACHE("Failed to cache: {0}"),
-  FAILED_CREATE("Failed to create {0}"),
   FAILED_SEEK("Failed to seek to {0}"),
   FAILED_SKIP("Failed to skip {0}"),
   INSTREAM_CANNOT_SKIP("The underlying BlockInStream could not skip {0}"),
