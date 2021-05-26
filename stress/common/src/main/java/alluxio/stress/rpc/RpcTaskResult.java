b103f1337b84a7bc3814b48d698558c85d23e366
jiacheliu3 <jiacheliu3@gmail.com>
2021-05-26 10:06:33 +0800

    add rpc benchmark tool

---
+++ stress/common/src/main/java/alluxio/stress/rpc/RpcTaskResult.java
@@ -0,0 +1,147 @@
+package alluxio.stress.rpc;
+
+import alluxio.stress.BaseParameters;
+import alluxio.stress.Summary;
+import alluxio.stress.TaskResult;
+import alluxio.stress.worker.IOTaskResult;
+import alluxio.stress.worker.UfsIOParameters;
+import alluxio.util.JsonSerializable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RpcTaskResult implements TaskResult {
+  private List<Point> mPoints;
+  private List<String> mErrors;
+  private BaseParameters mBaseParameters;
+  private RpcParameters mParameters;
+
+  public RpcTaskResult() {
+    mPoints = new ArrayList<>();
+    mErrors = new ArrayList<>();
+  }
+
+  /**
+   * @return the {@link BaseParameters}
+   * */
+  public BaseParameters getBaseParameters() {
+    return mBaseParameters;
+  }
+
+  /**
+   * @param baseParameters the {@link BaseParameters} to use
+   * */
+  public void setBaseParameters(BaseParameters baseParameters) {
+    mBaseParameters = baseParameters;
+  }
+
+  /**
+   * @return the {@link UfsIOParameters}
+   * */
+  public RpcParameters getParameters() {
+    return mParameters;
+  }
+
+  /**
+   * @param parameters the {@link UfsIOParameters} to use
+   * */
+  public void setParameters(RpcParameters parameters) {
+    mParameters = parameters;
+  }
+
+  /**
+   * @param errorMsg an error msg to add
+   * */
+  public void addError(String errorMsg) {
+    mErrors.add(errorMsg);
+  }
+
+  /**
+   * @return all the error messages
+   * */
+  public List<String> getErrors() {
+    return mErrors;
+  }
+
+  /**
+   * @param errors the errors
+   * */
+  public void setErrors(List<String> errors) {
+    mErrors = errors;
+  }
+
+  public void addPoint(Point p) {
+    mPoints.add(p);
+  }
+
+  public List<Point> getPoints() {
+    return mPoints;
+  }
+
+  public void setPoints(List<Point> points) {
+    mPoints = points;
+  }
+
+  @Override
+  public Aggregator aggregator() {
+    return new Aggregator();
+  }
+
+  public void merge(RpcTaskResult r) {
+    mErrors.addAll(r.mErrors);
+    mPoints.addAll(r.mPoints);
+  }
+
+  private static final class Aggregator implements TaskResult.Aggregator<RpcTaskResult> {
+    @Override
+    public Summary aggregate(Iterable<RpcTaskResult> results) throws Exception {
+      return new RpcTaskSummary(reduceList(results));
+    }
+
+    /**
+     * Reduce a list of {@link IOTaskResult} into one.
+     *
+     * @param results a list of results to combine
+     * @return the combined result
+     * */
+    public static RpcTaskResult reduceList(Iterable<RpcTaskResult> results) {
+      RpcTaskResult aggreResult = new RpcTaskResult();
+      for (RpcTaskResult r : results) {
+        aggreResult.merge(r);
+      }
+      return aggreResult;
+    }
+  }
+
+  public static class Point implements JsonSerializable {
+    public long mDurationMs;
+
+    public Point(@JsonProperty("duration") long ms) {
+      mDurationMs = ms;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Point: {duration: %sms}", mDurationMs);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+            .add("mPoints", mPoints)
+            .add("mErrors", mErrors).toString();
+  }
+
+  public static RpcTaskResult reduceList(Iterable<RpcTaskResult> results) {
+    RpcTaskResult aggreResult = new RpcTaskResult();
+    for (RpcTaskResult r : results) {
+      aggreResult.merge(r);
+    }
+    return aggreResult;
+  }
+}
