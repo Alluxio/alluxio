a73b0aa1c7b93c85ce2fa354be76cd230d542586
maobaolong <maobaolong@139.com>
2021-06-11 08:39:46 +0800

    Address comments, update document, add regex prefix.

--- table/server/common/src/test/java/alluxio/table/common/udb/UdbConfigurationTest.java
+++ table/server/common/src/test/java/alluxio/table/common/udb/UdbConfigurationTest.java
@@ -50,17 +50,17 @@ public class UdbConfigurationTest {
   public void mountOptions() {
     testMountOptions("SCHEME" + randomString(), true);
     testMountOptions("SCHEME" + randomString(), false);
-    testMountOptions(".*", "SCHEME" + randomString(), true);
-    testMountOptions(".*", "SCHEME" + randomString(), false);
-    testMountOptions("SCHE.E.*", "SCHEME" + randomString(), true);
-    testMountOptions("SCHE.E.*", "SCHEME" + randomString(), false);
+    testMountOptions(UdbConfiguration.REGEX_PREFIX + ".*", "SCHEME" + randomString(), true);
+    testMountOptions(UdbConfiguration.REGEX_PREFIX + ".*", "SCHEME" + randomString(), false);
+    testMountOptions(UdbConfiguration.REGEX_PREFIX + "SCHE.E.*", "SCHEME" + randomString(), true);
+    testMountOptions(UdbConfiguration.REGEX_PREFIX + "SCHE.E.*", "SCHEME" + randomString(), false);
   }
 
-  private void testMountOptions(String schemeAuthority, boolean specifyTrailingSlash) {
-    testMountOptions(schemeAuthority, schemeAuthority, specifyTrailingSlash);
+  private void testMountOptions(String concreteSchemeAuthority, boolean specifyTrailingSlash) {
+    testMountOptions(concreteSchemeAuthority, concreteSchemeAuthority, specifyTrailingSlash);
   }
 
-  private void testMountOptions(String schemeAuthority, String userSchemeAuthority,
+  private void testMountOptions(String templateSchemeAuthority, String concreteSchemeAuthority,
       boolean specifyTrailingSlash) {
     Map<String, String> values = new HashMap<>();
     for (int i = 0; i < 20; i++) {
@@ -69,9 +69,9 @@ public class UdbConfigurationTest {
 
     Map<String, String> properties = new HashMap<>();
     for (Map.Entry<String, String> entry : values.entrySet()) {
-      String schemeOption = String.format("{%s}", schemeAuthority);
+      String schemeOption = String.format("{%s}", templateSchemeAuthority);
       if (specifyTrailingSlash) {
-        schemeOption = String.format("{%s/}", schemeAuthority);
+        schemeOption = String.format("{%s/}", templateSchemeAuthority);
       }
       properties.put(
           String.format("%s%s.%s", ConfigurationUtils.MOUNT_PREFIX, schemeOption, entry.getKey()),
@@ -80,8 +80,8 @@ public class UdbConfigurationTest {
     UdbConfiguration conf = new UdbConfiguration(properties);
 
     // query for mount options with and without the trailing slash
-    assertEquals(values, conf.getMountOption(userSchemeAuthority));
-    assertEquals(values, conf.getMountOption(userSchemeAuthority + "/"));
+    assertEquals(values, conf.getMountOption(concreteSchemeAuthority));
+    assertEquals(values, conf.getMountOption(concreteSchemeAuthority + "/"));
   }
 
   private String randomString() {
