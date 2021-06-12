4f7bfbb00518ca5edf05f669a8a56c4fe9134320
David Zhu <david@alluxio.com>
2021-06-12 13:04:36 -0700

    Use the correct dot

--- core/common/src/main/java/alluxio/metrics/MetricKey.java
+++ core/common/src/main/java/alluxio/metrics/MetricKey.java
@@ -343,8 +343,8 @@ public final class MetricKey implements Comparable<MetricKey> {
   public static final MetricKey MASTER_JOURNAL_SPACE_FREE_PERCENT =
       new Builder("Master.JournalFreePercent")
           .setDescription("Percentage of free space left on the journal disk(s) "
-              + "for an Alluxio master。"
-              + "This metric is only valid on Linux and when embedded journal is used. "
+              + "for an Alluxio master."
+              + "This metric is only valid on Linux and when embedded journal is used."
               + "Use this metric to monitor whether your journal is running out of disk space.")
           .setMetricType(MetricType.GAUGE)
           .build();
