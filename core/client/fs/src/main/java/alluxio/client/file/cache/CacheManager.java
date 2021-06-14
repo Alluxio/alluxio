366fbddc2b3ade016ab3c9d7463eee8e112c70a4
Zhenyu Song <sunnyszy@gmail.com>
2021-06-13 20:24:21 -0400

    Implement Shadow Cache
    Address https://github.com/Alluxio/alluxio/issues/13260.
    
    pr-link: Alluxio/alluxio#13356
    change-id: cid-719aef232d96a528cd8a56cc355b143d6328cad2

--- core/client/fs/src/main/java/alluxio/client/file/cache/CacheManager.java
+++ core/client/fs/src/main/java/alluxio/client/file/cache/CacheManager.java
@@ -14,6 +14,7 @@ package alluxio.client.file.cache;
 import alluxio.client.quota.CacheQuota;
 import alluxio.client.quota.CacheScope;
 import alluxio.conf.AlluxioConfiguration;
+import alluxio.conf.PropertyKey;
 import alluxio.metrics.MetricKey;
 import alluxio.metrics.MetricsSystem;
 import alluxio.resource.LockResource;
@@ -106,6 +107,12 @@ public interface CacheManager extends AutoCloseable {
      */
     static CacheManager create(AlluxioConfiguration conf) throws IOException {
       try {
+        boolean isShadowCacheEnabled =
+            conf.getBoolean(PropertyKey.USER_CLIENT_CACHE_SHADOW_ENABLED);
+        if (isShadowCacheEnabled) {
+          return new NoExceptionCacheManager(
+              new CacheManagerWithShadowCache(LocalCacheManager.create(conf), conf));
+        }
         return new NoExceptionCacheManager(LocalCacheManager.create(conf));
       } catch (IOException e) {
         Metrics.CREATE_ERRORS.inc();
