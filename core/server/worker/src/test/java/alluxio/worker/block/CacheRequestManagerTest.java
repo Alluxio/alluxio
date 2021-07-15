13260ca6ef458eae9c0a251d3903453b47073eb9
Jianjian <jja725@gmail.com>
2021-07-15 16:55:35 -0700

    fix unit test and shutdown threadpool for load command

--- core/server/worker/src/test/java/alluxio/worker/block/CacheRequestManagerTest.java
+++ core/server/worker/src/test/java/alluxio/worker/block/CacheRequestManagerTest.java
@@ -1,7 +1,7 @@
 /*
- * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
- * (the "License"). You may not use this work except in compliance with the License, which is
- * available at www.apache.org/licenses/LICENSE-2.0
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0 (the
+ * "License"). You may not use this work except in compliance with the License, which is available
+ * at www.apache.org/licenses/LICENSE-2.0
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied, as more fully set forth in the License.
@@ -54,7 +54,7 @@ public class CacheRequestManagerTest {
   public void submitRequestCacheBlockFromUfs() throws Exception {
     String localWorkerHostname = NetworkAddressUtils.getLocalHostName(
         (int) ServerConfiguration.getMs(PropertyKey.NETWORK_HOST_RESOLUTION_TIMEOUT_MS));
-    int port = mRandom.nextInt();
+    int port = mRandom.nextInt(5000);
     long blockId = mRandom.nextLong();
     long blockLength = mRandom.nextLong();
     Protocol.OpenUfsBlockOptions openUfsBlockOptions = Protocol.OpenUfsBlockOptions.newBuilder()
@@ -63,28 +63,27 @@ public class CacheRequestManagerTest {
         .setOpenUfsBlockOptions(openUfsBlockOptions).setSourceHost(localWorkerHostname)
         .setSourcePort(port).build();
     mCacheRequestManager.submitRequest(request);
-    verify(mBlockWorker).createBlock(Sessions.ASYNC_CACHE_WORKER_SESSION_ID, blockId, 0, "",
-        blockLength);
     verify(mBlockWorker).createUfsBlockReader(Sessions.ASYNC_CACHE_UFS_SESSION_ID, blockId, 0,
         false, openUfsBlockOptions);
   }
 
   @Test
   public void submitRequestCacheBlockFromRemoteWorker() throws Exception {
-    String remoteWorkerHostname = NetworkAddressUtils.getLocalHostName(
-        (int) ServerConfiguration.getMs(PropertyKey.NETWORK_HOST_RESOLUTION_TIMEOUT_MS) + 1);
-    int port = mRandom.nextInt();
+    String localWorkerHostname = NetworkAddressUtils.getLocalHostName(
+        (int) ServerConfiguration.getMs(PropertyKey.NETWORK_HOST_RESOLUTION_TIMEOUT_MS));
+    String fakeRemoteWorker = localWorkerHostname + "1";
+    int port = mRandom.nextInt(5000);
     long blockId = mRandom.nextLong();
     long blockLength = mRandom.nextLong();
     Protocol.OpenUfsBlockOptions openUfsBlockOptions = Protocol.OpenUfsBlockOptions.newBuilder()
         .setMaxUfsReadConcurrency(10).setUfsPath("/a").build();
     CacheRequest request = CacheRequest.newBuilder().setBlockId(blockId).setLength(blockLength)
-        .setOpenUfsBlockOptions(openUfsBlockOptions).setSourceHost(remoteWorkerHostname)
+        .setOpenUfsBlockOptions(openUfsBlockOptions).setSourceHost(fakeRemoteWorker)
         .setSourcePort(port).build();
     mCacheRequestManager.submitRequest(request);
     verify(mBlockWorker).createBlock(Sessions.ASYNC_CACHE_WORKER_SESSION_ID, blockId, 0, "",
         blockLength);
     verify(mBlockWorker).createBlockWriter(Sessions.ASYNC_CACHE_WORKER_SESSION_ID, blockId);
-    verify(mBlockWorker).commitBlock(Sessions.ASYNC_CACHE_WORKER_SESSION_ID, blockId, false);
+    // since the remoteWorker is fake, we can not verify the following data writing process
   }
 }
