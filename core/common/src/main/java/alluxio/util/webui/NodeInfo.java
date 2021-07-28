e6ab8d7562a23170878e628f1c3cb45d7b36eeaa
Yaolong Liu <lylpysz1@163.com>
2021-07-28 10:37:52 +0800

    Use String.format concat string.

--- core/common/src/main/java/alluxio/util/webui/NodeInfo.java
+++ core/common/src/main/java/alluxio/util/webui/NodeInfo.java
@@ -40,8 +40,7 @@ public final class NodeInfo implements Comparable<NodeInfo> {
    */
   public NodeInfo(WorkerInfo workerInfo) {
     if (!workerInfo.getAddress().getContainerHost().equals("")) {
-      mHost = workerInfo.getAddress().getHost() + "("
-              + workerInfo.getAddress().getContainerHost() + ")";
+      mHost = String.format("%s (%s)",workerInfo.getAddress().getHost(),workerInfo.getAddress().getContainerHost());
     } else {
       mHost = workerInfo.getAddress().getHost();
     }
