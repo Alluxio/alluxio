634bd32d1f589c91e58578f931b5abbeb8fcd348
Shawn Sun <32376495+ssz1997@users.noreply.github.com>
2021-08-06 13:41:41 -0400

    Add FUSE read stress bench
    The FUSE stress bench only supports calculating reading throughput. The
    overall design and logic is similar to the existing clientIOBench and
    related files. The first commit has the data but not the final
    calculation or the result graph.
    
    PTAL @LuQQiu, especially whether I read the files correctly. Thanks!
    
    pr-link: Alluxio/alluxio#13869
    change-id: cid-eeba0fa8490fbf0c1d8f6512b25444852b15dd70

---
+++ stress/common/src/main/java/alluxio/stress/fuse/FuseIOOperation.java
@@ -0,0 +1,64 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.stress.fuse;
+
+/**
+ * The operations for the FUSE stress tests.
+ */
+public enum FuseIOOperation {
+  /** Write operation to test the write throughput or prepare data for reading. */
+  WRITE("Write"),
+
+  /** Now only streaming reading is supported, that is, sequentially read the written files. */
+  READ("Read"),
+  // TODO(Shawn): Support other types of read
+  ;
+
+  /**
+   * @param operation the operation
+   * @return true if the operation is a read
+   */
+  public static boolean isRead(FuseIOOperation operation) {
+    switch (operation) {
+      case READ:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  private final String mName;
+
+  FuseIOOperation(String name) {
+    mName = name;
+  }
+
+  @Override
+  public String toString() {
+    return mName;
+  }
+
+  /**
+   * Creates an instance type from the string. This method is case insensitive.
+   *
+   * @param text the instance type in string
+   * @return the created instance
+   */
+  public static FuseIOOperation fromString(String text) {
+    for (FuseIOOperation type : FuseIOOperation.values()) {
+      if (type.toString().equalsIgnoreCase(text)) {
+        return type;
+      }
+    }
+    throw new IllegalArgumentException("No constant with text " + text + " found");
+  }
+}
