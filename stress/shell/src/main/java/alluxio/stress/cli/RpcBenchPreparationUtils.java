c4f45cf7858651d0c716685c2a2c9f95c124c061
Bowen Ding <bowen@alluxio.com>
2021-08-09 22:50:25 +0800

    Fix GetPinnedFileIdsBench parameters

--- stress/shell/src/main/java/alluxio/stress/cli/RpcBenchPreparationUtils.java
+++ stress/shell/src/main/java/alluxio/stress/cli/RpcBenchPreparationUtils.java
@@ -50,7 +50,7 @@ public class RpcBenchPreparationUtils {
   private static final BlockStoreLocation BLOCK_LOCATION_MEM =
       new BlockStoreLocation("MEM", 0, "MEM");
 
-  public static InstancedConfiguration sConf = InstancedConfiguration.defaults();
+  public static final InstancedConfiguration sConf = InstancedConfiguration.defaults();
 
   private RpcBenchPreparationUtils() {}
 
