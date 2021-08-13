1dcac0cb70929d4c21e5d317c3d72e44ecd765f9
Arthur Jenoudet <jenoudet@umich.edu>
2021-08-13 11:58:15 -0700

    adding new instrumentation to RaftJournalWriter

--- core/server/common/src/main/java/alluxio/master/journal/raft/RaftJournalWriter.java
+++ core/server/common/src/main/java/alluxio/master/journal/raft/RaftJournalWriter.java
@@ -17,7 +17,6 @@ import alluxio.exception.JournalClosedException;
 import alluxio.master.journal.JournalWriter;
 import alluxio.proto.journal.Journal.JournalEntry;
 import alluxio.util.FormatUtils;
-
 import com.google.common.base.Preconditions;
 import org.apache.ratis.protocol.Message;
 import org.apache.ratis.protocol.RaftClientReply;
@@ -25,13 +24,18 @@ import org.apache.ratis.util.TimeDuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.concurrent.NotThreadSafe;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicLong;
-
-import javax.annotation.concurrent.NotThreadSafe;
+import java.util.stream.Collectors;
 
 /**
  * Class for writing entries to the Raft journal. Written entries are aggregated until flush is
@@ -62,7 +66,7 @@ public class RaftJournalWriter implements JournalWriter {
    */
   public RaftJournalWriter(long nextSequenceNumberToWrite,
       LocalFirstRaftClient client) {
-    LOG.debug("Journal writer created starting at SN#{}", nextSequenceNumberToWrite);
+    LOG.info("Journal writer created starting at SN#{}", nextSequenceNumberToWrite);
     mNextSequenceNumberToWrite = new AtomicLong(nextSequenceNumberToWrite);
     mLastSubmittedSequenceNumber = new AtomicLong(-1);
     mLastCommittedSequenceNumber = new AtomicLong(-1);
@@ -77,40 +81,66 @@ public class RaftJournalWriter implements JournalWriter {
         .getBytes(PropertyKey.MASTER_EMBEDDED_JOURNAL_ENTRY_SIZE_MAX);
     mFlushBatchBytes = mEntrySizeMax / 3;
   }
+  
+  private String formatEntry(JournalEntry entry) {
+    Queue<JournalEntry> entries = new LinkedList<>(entry.getJournalEntriesList());
+    List<JournalEntry> children = new ArrayList<>();
+    while (!entries.isEmpty()) {
+      JournalEntry poll = entries.poll();
+      entries.addAll(poll.getJournalEntriesList());
+      children.add(poll);
+    }
+    return String.format("entry #%s (children entry #s: %s)", entry.getSequenceNumber(),
+            children.stream().map(JournalEntry::getSequenceNumber)
+                    .sorted(Comparator.reverseOrder()).collect(Collectors.toList()));
+  }
 
   @Override
   public void write(JournalEntry entry) throws IOException, JournalClosedException {
+    LOG.info("writing {}", formatEntry(entry));
     if (mClosed) {
-      throw new JournalClosedException("Cannot write to journal. Journal writer has been closed");
+      String s = String.format("Cannot write entry #%s to journal. Journal writer has been closed",
+              entry.getSequenceNumber());
+      LOG.info(s);
+      throw new JournalClosedException(s);
     }
     Preconditions.checkState(entry.getAllFields().size() <= 1,
         "Raft journal entries should never set multiple fields, but found %s", entry);
-    if (mCurrentJournalEntrySize.get() > mFlushBatchBytes) {
+    long l1 = mCurrentJournalEntrySize.get();
+    if (l1 > mFlushBatchBytes) {
+      LOG.info("currentJournalEntrySize={}, flushBatchBytes={}", l1, mFlushBatchBytes);
       flush();
     }
     if (mJournalEntryBuilder == null) {
       mJournalEntryBuilder = JournalEntry.newBuilder();
       mCurrentJournalEntrySize.set(0);
     }
-    LOG.trace("Writing entry {}: {}", mNextSequenceNumberToWrite, entry);
-    mJournalEntryBuilder.addJournalEntries(entry.toBuilder()
-        .setSequenceNumber(mNextSequenceNumberToWrite.getAndIncrement()).build());
+    LOG.info("Writing entry #{}: {}", mNextSequenceNumberToWrite, entry);
+    long SN = mNextSequenceNumberToWrite.getAndIncrement();
+    JournalEntry build = entry.toBuilder().setSequenceNumber(SN).build();
+    LOG.info("JournalEntry.Builder receives {}", formatEntry(build));
+    mJournalEntryBuilder.addJournalEntries(build);
     long size = entry.getSerializedSize();
     if (size > mEntrySizeMax) {
       LOG.error("Journal entry size ({}) is bigger than the max allowed size ({}) defined by {}",
           FormatUtils.getSizeFromBytes(size), FormatUtils.getSizeFromBytes(mEntrySizeMax),
           PropertyKey.MASTER_EMBEDDED_JOURNAL_ENTRY_SIZE_MAX.getName());
     }
-    mCurrentJournalEntrySize.addAndGet(size);
+    long l = mCurrentJournalEntrySize.addAndGet(size);
+    LOG.info("currentJournalEntrySize incremented by {} to {}", size, l);
   }
 
   @Override
   public void flush() throws IOException, JournalClosedException {
+    LOG.info("flushing");
     if (mClosed) {
-      throw new JournalClosedException("Cannot flush. Journal writer has been closed");
+      String s = "Cannot flush. Journal writer has been closed";
+      LOG.info(s);
+      throw new JournalClosedException(s);
     }
     if (mJournalEntryBuilder != null) {
       long flushSN = mNextSequenceNumberToWrite.get() - 1;
+      LOG.info("flushSN={}", flushSN);
       try {
         // It is ok to submit the same entries multiple times because we de-duplicate by sequence
         // number when applying them. This could happen if submit fails and we re-submit the same
@@ -118,24 +148,35 @@ public class RaftJournalWriter implements JournalWriter {
         JournalEntry entry = mJournalEntryBuilder.build();
         Message message = RaftJournalSystem.toRaftMessage(entry);
         mLastSubmittedSequenceNumber.set(flushSN);
-        LOG.trace("Flushing entry {} ({})", entry, message);
+        LOG.info("last submitted sequence number={}", mLastSubmittedSequenceNumber);
+
+        LOG.info("Flushing {} ({})", formatEntry(entry), message);
+
         RaftClientReply reply = mClient
             .sendAsync(message, TimeDuration.valueOf(mWriteTimeoutMs, TimeUnit.MILLISECONDS))
             .get(mWriteTimeoutMs, TimeUnit.MILLISECONDS);
+
         mLastCommittedSequenceNumber.set(flushSN);
+
+        LOG.info("last committed sequence number={}", mLastCommittedSequenceNumber);
         if (reply.getException() != null) {
+          LOG.info("Received exception from reply: {}", reply.getException().toString());
           throw reply.getException();
         }
       } catch (InterruptedException e) {
+        LOG.info("Received exception: {}", e.toString());
         Thread.currentThread().interrupt();
         throw new IOException(e);
       } catch (ExecutionException e) {
+        LOG.info("Received exception: {}", e.toString());
         throw new IOException(e.getCause());
       } catch (TimeoutException e) {
+        LOG.info("Received exception: {}", e.toString());
         throw new IOException(String.format(
             "Timed out after waiting %s milliseconds for journal entries to be processed",
             mWriteTimeoutMs), e);
       }
+      LOG.info("resetting journal entry builder");
       mJournalEntryBuilder = null;
     }
   }
