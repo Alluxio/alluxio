1108460ee058d817418dd43013a36c180f4b5efa
Yaolong Liu <lylpysz1@163.com>
2021-08-17 19:38:05 +0800

    modify code error

--- core/common/src/main/java/alluxio/metrics/OperationSystemGaugeSet.java
+++ core/common/src/main/java/alluxio/metrics/OperationSystemGaugeSet.java
@@ -11,7 +11,7 @@
 
 package alluxio.metrics;
 
-import com.codahale.metrics.Gauge;
+import com.codahale.metrics.CachedGauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricSet;
 import com.sun.management.OperatingSystemMXBean;
@@ -20,6 +20,7 @@ import com.sun.management.UnixOperatingSystemMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A set of counters for the os metric.
@@ -34,11 +35,36 @@ public class OperationSystemGaugeSet implements MetricSet {
   @Override
   public Map<String, Metric> getMetrics() {
     final Map<String, Metric> gauges = new HashMap<>();
-    gauges.put("os.freePhysicalMemory", (Gauge<Long>) mOsmxb::getFreePhysicalMemorySize);
-    gauges.put("os.totalPhysicalMemory", (Gauge<Long>) mOsmxb::getTotalPhysicalMemorySize);
-    gauges.put("os.cpuLoad", (Gauge<Double>) mOsmxb::getSystemCpuLoad);
-    gauges.put("os.maxFileCount", (Gauge<Long>) mUnixb::getMaxFileDescriptorCount);
-    gauges.put("os.openFileCount", (Gauge<Long>) mUnixb::getOpenFileDescriptorCount);
+    gauges.put("os.freePhysicalMemory", new CachedGauge(10, TimeUnit.MINUTES) {
+      @Override
+      protected Long loadValue() {
+        return mOsmxb.getFreePhysicalMemorySize();
+      }
+    });
+    gauges.put("os.totalPhysicalMemory", new CachedGauge<Long>(10, TimeUnit.MINUTES) {
+      @Override
+      protected Long loadValue() {
+        return mOsmxb.getTotalPhysicalMemorySize();
+      }
+    });
+    gauges.put("os.cpuLoad", new CachedGauge<Double>(10, TimeUnit.MINUTES) {
+      @Override
+      protected Double loadValue() {
+        return mOsmxb.getSystemCpuLoad();
+      }
+    });
+    gauges.put("os.maxFileCount", new CachedGauge<Long>(10, TimeUnit.MINUTES) {
+      @Override
+      protected Long loadValue() {
+        return mUnixb.getMaxFileDescriptorCount();
+      }
+    });
+    gauges.put("os.openFileCount", new CachedGauge<Long>(10, TimeUnit.MINUTES) {
+      @Override
+      protected Long loadValue() {
+        return mUnixb.getOpenFileDescriptorCount();
+      }
+    });
     return gauges;
   }
 }
