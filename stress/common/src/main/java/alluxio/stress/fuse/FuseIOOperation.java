cfb7252340a047be9a5a530a905b04ffcbb892df
ssz1997 <shawn.sun.1997@gmail.com>
2021-08-17 14:26:01 -0700

    Better format

--- stress/common/src/main/java/alluxio/stress/fuse/FuseIOOperation.java
+++ stress/common/src/main/java/alluxio/stress/fuse/FuseIOOperation.java
@@ -17,8 +17,11 @@ package alluxio.stress.fuse;
 public enum FuseIOOperation {
   /** Write operation to test the write throughput or prepare data for reading. */
   WRITE("Write"),
-  /** List the files before reading can help cache the metadata and gain more accurate reading
-   *  performance, if client-side metadata cache is enabled. */
+  
+  /**
+   * List the files before reading can help cache the metadata and gain more accurate reading
+   * performance, if client-side metadata cache is enabled.
+   */
   LIST_FILE("ListFile"),
 
   /** Now only streaming reading is supported, that is, sequentially read the written files. */
