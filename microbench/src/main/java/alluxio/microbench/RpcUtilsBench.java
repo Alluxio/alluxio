80db5b7f1372b478b2ed9f02bdea5a007c7b540e
Bowen Ding <bowen@alluxio.com>
2021-08-31 19:12:37 +0800

    Use Blackhole.consumeCpu

--- microbench/src/main/java/alluxio/microbench/RpcUtilsBench.java
+++ microbench/src/main/java/alluxio/microbench/RpcUtilsBench.java
@@ -22,9 +22,12 @@ import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.CommandLineOptionException;
+import org.openjdk.jmh.runner.options.CommandLineOptions;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.slf4j.Logger;
@@ -36,19 +39,20 @@ import java.util.concurrent.TimeUnit;
  * Benchmarks for {@link alluxio.RpcUtils}.
  */
 @Fork(value = 1, jvmArgsPrepend = "-server")
-@Warmup(iterations = 2, time = 5, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 2, time = 3, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 6, time = 3, timeUnit = TimeUnit.SECONDS)
 @BenchmarkMode(Mode.Throughput)
 public class RpcUtilsBench {
   private static final Logger LOG = LoggerFactory.getLogger(RpcUtilsBench.class);
   private static final String METHOD_NAME = "CheckAccess"; // an arbitrary rpc method name
 
-  private static int sCounter = 0;
-
   @Benchmark
   public long testCallAndReturn(BenchParams params) throws Exception {
     return callAndReturn(LOG,
-        () -> delayWithSideEffect(params.mDelay),
+        () -> {
+          Blackhole.consumeCPU(params.mDelay);
+          return params.mDelay;
+        },
         METHOD_NAME,
         true, // irrelevant as no failure should occur here
         "");
@@ -56,29 +60,21 @@ public class RpcUtilsBench {
 
   @Benchmark
   public long delayBaseline(BenchParams params) throws Exception {
-    return delayWithSideEffect(params.mDelay);
-  }
-
-  private static long delayWithSideEffect(final int delay) {
-    sCounter++;
-    for (int i = 0; i < delay; i++) {
-      // create some side effect to prevent the loop being optimized out
-      sCounter++;
-    }
-    // subtract and effectively set it back to 0
-    sCounter -= delay;
-    return sCounter;
+    Blackhole.consumeCPU(params.mDelay);
+    return params.mDelay;
   }
 
   @State(Scope.Benchmark)
   public static class BenchParams {
-    @Param({ "10000", "50000", "100000", "500000", "1000000"})
-    public int mDelay;
+    @Param({ "500", "1000", "2000", "4000", "8000", "16000"})
+    public long mDelay;
   }
 
-  public static void main(String[] args) throws RunnerException {
+  public static void main(String[] args) throws RunnerException, CommandLineOptionException {
+    Options argsCli = new CommandLineOptions(args);
     Options opts = new OptionsBuilder()
-        .include(".*.RpcUtilsBench.*")
+        .parent(argsCli)
+        .include(RpcUtilsBench.class.getName())
         .shouldDoGC(true)
         .result("results.json")
         .resultFormat(ResultFormatType.JSON)
