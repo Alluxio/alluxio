8d73eeb04ba7d0d76a71b87e5e053b787aa736b4
Göktürk Gezer <gokturk@alluxio.com>
2021-09-22 17:05:57 -0700

    Revert "Introduce deadlines for sync RPC stubs"
    This reverts commit fa5230706b429350160084268d1e925b67ce4ce9.
    
    pr-link: Alluxio/alluxio#14136
    change-id: cid-9dfd189e01fbff63eb4f41324d5fc86e27f43aff

--- core/server/worker/src/main/java/alluxio/worker/block/BlockMasterClient.java
+++ core/server/worker/src/main/java/alluxio/worker/block/BlockMasterClient.java
@@ -83,8 +83,7 @@ public class BlockMasterClient extends AbstractMasterClient {
 
   @Override
   protected void afterConnect() throws IOException {
-    mClient = BlockMasterWorkerServiceGrpc.newBlockingStub(mChannel)
-        .withDeadlineAfter(mRpcCallDeadlineDurationMs, TimeUnit.MILLISECONDS);
+    mClient = BlockMasterWorkerServiceGrpc.newBlockingStub(mChannel);
   }
 
   /**
