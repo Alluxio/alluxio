4696b5b1b3d29851cf0c7649c42e9934379d405b
xichen01 <mrchenx@126.com>
2021-09-23 22:02:50 +0800

    fix describe of metrics

--- core/common/src/main/java/alluxio/metrics/MetricKey.java
+++ core/common/src/main/java/alluxio/metrics/MetricKey.java
@@ -1158,20 +1158,20 @@ public final class MetricKey implements Comparable<MetricKey> {
   public static final MetricKey WORKER_BLOCK_READER_THREAD_ACTIVELY_COUNT =
       new Builder("Worker.BlockReaderThreadActivelyCount")
           .setDescription("The approximate number of block read "
-              + "threads that are actively executing tasks")
+              + "threads that are actively executing tasks in reader thread pool")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
   public static final MetricKey WORKER_BLOCK_READER_THREAD_CURRENT_COUNT =
       new Builder("Worker.BlockReaderThreadCurrentCount")
-          .setDescription("The approximate number of block read "
-              + "threads that are actively executing tasks")
+          .setDescription("The current number of read threads in the reader thread pool")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
-  public static final MetricKey WORKER_BLOCK_READER_THREAD_MAX_POOL_COUNT =
-      new Builder("Worker.BlockReaderThreadAllowedMaxPoolNum")
-          .setDescription("The maximum allowed number of block read threads")
+  public static final MetricKey WORKER_BLOCK_READER_THREAD_MAX_COUNT =
+      new Builder("Worker.BlockReaderThreadMaxPoolCount")
+          .setDescription("The maximum allowed number of block read "
+              + "thread in the reader thread pool")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
@@ -1182,29 +1182,29 @@ public final class MetricKey implements Comparable<MetricKey> {
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
-  public static final MetricKey WORKER_BLOCK_WRITER_THREAD_ACTIVATING_COUNT =
+  public static final MetricKey WORKER_BLOCK_WRITER_THREAD_ACTIVELY_COUNT =
       new Builder("Worker.BlockWriterThreadActivelyCount")
           .setDescription("The approximate number of block write "
-              + "threads that are actively executing tasks")
+              + "threads that are actively executing tasks in writer thread pool")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
   public static final MetricKey WORKER_BLOCK_WRITER_THREAD_CURRENT_COUNT =
       new Builder("Worker.BlockWriterThreadCurrentCount")
-          .setDescription("The approximate number of block write "
-              + "threads that are actively executing tasks")
+          .setDescription("The current number of write threads in the writer thread pool")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
-  public static final MetricKey WORKER_BLOCK_WRITER_THREAD_ALLOWED_MAX_COUNT =
-      new Builder("Worker.BlockWriterThreadAllowedMaxCount")
-          .setDescription("The maximum allowed number of block write threads")
+  public static final MetricKey WORKER_BLOCK_WRITER_THREAD_MAX_COUNT =
+      new Builder("Worker.BlockWriterThreadMaxCount")
+          .setDescription("The maximum allowed number of block write "
+              + "thread in the writer thread pool")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
   public static final MetricKey WORKER_BLOCK_WRITER_COMPLETED_TASK_COUNT =
       new Builder("Worker.BlockWriterCompleteTaskCount")
-          .setDescription("The approximate total number of block read tasks "
+          .setDescription("The approximate total number of block write tasks "
               + "that have completed execution")
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
--- core/server/worker/src/main/java/alluxio/worker/grpc/GrpcExecutors.java
+++ core/server/worker/src/main/java/alluxio/worker/grpc/GrpcExecutors.java
@@ -61,7 +61,7 @@ public final class GrpcExecutors {
         MetricKey.WORKER_BLOCK_READER_THREAD_CURRENT_COUNT.getName()),
         BLOCK_READER_THREAD_POOL_EXECUTOR::getPoolSize);
     MetricsSystem.registerGaugeIfAbsent(MetricsSystem.getMetricName(
-        MetricKey.WORKER_BLOCK_READER_THREAD_MAX_POOL_COUNT.getName()),
+        MetricKey.WORKER_BLOCK_READER_THREAD_MAX_COUNT.getName()),
         BLOCK_READER_THREAD_POOL_EXECUTOR::getMaximumPoolSize);
     MetricsSystem.registerGaugeIfAbsent(MetricsSystem.getMetricName(
         MetricKey.WORKER_BLOCK_READER_COMPLETED_TASK_COUNT.getName()),
@@ -86,13 +86,13 @@ public final class GrpcExecutors {
 
   static {
     MetricsSystem.registerGaugeIfAbsent(MetricsSystem.getMetricName(
-        MetricKey.WORKER_BLOCK_WRITER_THREAD_ACTIVATING_COUNT.getName()),
+        MetricKey.WORKER_BLOCK_WRITER_THREAD_ACTIVELY_COUNT.getName()),
         BLOCK_WRITE_THREAD_POOL_EXECUTOR::getActiveCount);
     MetricsSystem.registerGaugeIfAbsent(MetricsSystem.getMetricName(
         MetricKey.WORKER_BLOCK_WRITER_THREAD_CURRENT_COUNT.getName()),
         BLOCK_WRITE_THREAD_POOL_EXECUTOR::getPoolSize);
     MetricsSystem.registerGaugeIfAbsent(MetricsSystem.getMetricName(
-        MetricKey.WORKER_BLOCK_WRITER_THREAD_ALLOWED_MAX_COUNT.getName()),
+        MetricKey.WORKER_BLOCK_WRITER_THREAD_MAX_COUNT.getName()),
         BLOCK_WRITE_THREAD_POOL_EXECUTOR::getMaximumPoolSize);
     MetricsSystem.registerGaugeIfAbsent(MetricsSystem.getMetricName(
         MetricKey.WORKER_BLOCK_WRITER_COMPLETED_TASK_COUNT.getName()),
