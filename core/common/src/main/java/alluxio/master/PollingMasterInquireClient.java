6f4287d504423492a64185fd8cf5035f6081630e
qian0817 <qianlei666@gmail.com>
2021-09-27 23:37:55 +0800

    style: pass checkstyle.

--- core/common/src/main/java/alluxio/master/PollingMasterInquireClient.java
+++ core/common/src/main/java/alluxio/master/PollingMasterInquireClient.java
@@ -42,12 +42,10 @@ import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
 
