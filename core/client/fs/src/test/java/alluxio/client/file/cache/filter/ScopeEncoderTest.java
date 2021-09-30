0083fdb43f1dd0c2fb69dabc4eee4a797a9d8541
iluoeli <luoyili233@gmail.com>
2021-09-30 10:29:05 +0800

    Add alluxio header

--- core/client/fs/src/test/java/alluxio/client/file/cache/filter/ScopeEncoderTest.java
+++ core/client/fs/src/test/java/alluxio/client/file/cache/filter/ScopeEncoderTest.java
@@ -1,16 +1,27 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0 (the
+ * "License"). You may not use this work except in compliance with the License, which is available
+ * at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package alluxio.client.file.cache.filter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import alluxio.test.util.ConcurrencyUtils;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import alluxio.test.util.ConcurrencyUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ScopeEncoderTest {
   private static final int BITS_PER_SCOPE = 8; // 256 scopes at most
