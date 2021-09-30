be89dc874f24c571118037f57086b23753429d71
iluoeli <luoyili233@gmail.com>
2021-09-30 10:29:05 +0800

    Add alluxio header

--- core/client/fs/src/test/java/alluxio/client/file/cache/filter/SingleCuckooTableTest.java
+++ core/client/fs/src/test/java/alluxio/client/file/cache/filter/SingleCuckooTableTest.java
@@ -1,13 +1,24 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0 (the
+ * "License"). You may not use this work except in compliance with the License, which is available
+ * at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package alluxio.client.file.cache.filter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
-
-import org.junit.Test;
 
 public class SingleCuckooTableTest {
   static final int numBuckets = 16;
