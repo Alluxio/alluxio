1d5f129a254adea59aa02978b274d87da74fa17a
Jianjian <jja725@gmail.com>
2021-10-06 17:07:16 -0700

    refactoring job service bench and master bench

--- stress/common/src/main/java/alluxio/stress/GeneralParameters.java
+++ stress/common/src/main/java/alluxio/stress/GeneralParameters.java
@@ -9,53 +9,8 @@ import com.beust.jcommander.ParameterException;
 
 import java.util.EnumSet;
 
-public abstract class GeneralParameters<T extends Operation> extends Parameters {
+public abstract class GeneralParameters<T extends Enum<T>> extends Parameters {
     @Parameter(names = {"--operation"}, description =
-        "the operation to perform for the stress bench", converter = OperationConverter.class, required = true)
+        "the operation to perform for the stress bench", required = true)
     public T mOperation;
-
-    public static class OperationConverter<S extends Operation> implements IStringConverter<S> {
-        private final Class<S> clazz;
-
-        protected OperationConverter(Class<S> clazz) {
-            this.clazz = clazz;
-        }
-
-        @Override
-        public S convert(String value) {
-
-            return S.fromString(value, Class<T>);
-        }
-    }
-    public class EnumConverter<T extends Enum<T>> implements IStringConverter<T> {
-
-        private final String optionName;
-        private final Class<T> clazz;
-
-        /**
-         * Constructs a new converter.
-         * @param optionName the option name for error reporting
-         * @param clazz the enum class
-         */
-        public EnumConverter(String optionName, Class<T> clazz) {
-            this.optionName = optionName;
-            this.clazz = clazz;
-        }
-
-        @Override
-        public T convert(String value) {
-            try {
-                try {
-                    return Enum.valueOf(clazz, value);
-                } catch (IllegalArgumentException e) {
-                    return Enum.valueOf(clazz, value.toUpperCase());
-                }
-            } catch (Exception e) {
-                throw new ParameterException("Invalid value for " + optionName + " parameter. Allowed values:" +
-                    EnumSet.allOf(clazz));
-
-            }
-        }
-    }
-
 }
