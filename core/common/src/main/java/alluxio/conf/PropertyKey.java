4f870de77caaceaa5dc30229330a95d0e4eb4939
Göktürk Gezer <gokturk@alluxio.com>
2021-10-14 13:29:21 -0700

    Remove fall-back logic from journal appending
    ### What changes are proposed in this pull request?
    
    Fallback logic of (writing via remote when local fails) seem to be
    wrapped over a bug in test, and not of intention.
    Removing the logic and simplifying the code/properties little bit.
    
    pr-link: Alluxio/alluxio#14261
    change-id: cid-8bfd4f2214806248dbd34d9d93110408586d12c2

--- core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -1807,14 +1807,6 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
           .setScope(Scope.MASTER)
           .build();
-  public static final PropertyKey MASTER_EMBEDDED_JOURNAL_WRITE_LOCAL_FIRST_ENABLED =
-      new Builder(Name.MASTER_EMBEDDED_JOURNAL_WRITE_LOCAL_FIRST_ENABLED)
-          .setDefaultValue(true)
-          .setDescription("Whether the journal writer will attempt to write entry locally before "
-              + "falling back to a full remote raft client.")
-          .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
-          .setScope(Scope.MASTER)
-          .build();
   public static final PropertyKey MASTER_EMBEDDED_JOURNAL_WRITE_REMOTE_ENABLED =
       new Builder(Name.MASTER_EMBEDDED_JOURNAL_WRITE_REMOTE_ENABLED)
           .setDefaultValue(false)
