b841edc7a9a30570254f58f7f30ca5b4bdef021d
bzheng888 <bingzheng617@outlook.com>
2021-10-22 14:37:08 +0800

    Add a web ui page to show stacks

---
+++ webui/master/src/containers/pages/Stacks/stacks.tsx
@@ -0,0 +1,27 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+import React from 'react';
+import { compose } from 'redux';
+export class StacksPresenter extends React.Component {
+  public render(): JSX.Element {
+    return this.renderStacksView();
+  }
+  private renderStacksView(): JSX.Element {
+    const path = 'http://' + location.host + location.pathname;
+    return (
+      <div className="stacks-page" style={{ backgroundColor: 'white', minHeight: window.innerHeight }}>
+        <iframe style={{ width: '100%', height: window.innerHeight, overflow: 'visible' }} src={path} />
+      </div>
+    );
+  }
+}
+export default compose(StacksPresenter) as typeof React.Component;
