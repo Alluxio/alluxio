3b565f4f18ee1415c9e5f3c483cbf4bc131cd210
bzheng888 <bingzheng617@outlook.com>
2021-10-25 14:51:41 +0800

    Resolve the change request and code style

--- webui/master/src/containers/pages/Stacks/stacks.test.tsx
+++ webui/master/src/containers/pages/Stacks/stacks.test.tsx
@@ -14,7 +14,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import sinon from 'sinon';
 
-import { StacksPresenter } from './stacks';
+import { StacksPresenter } from './Stacks';
 
 configure({ adapter: new Adapter() });
 
