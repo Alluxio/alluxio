2281296931a89f84cac52fba3ceb45623b788e5b
Bowen Ding <bowen@alluxio.com>
2021-10-30 01:40:52 +0800

    Fix flaky BlockMasterMetricsTest.testSize
    ### What changes are proposed in this pull request?
    
    Fix flaky `BlockMasterMetricsTest.testSize`, e.g.
    https://github.com/Alluxio/alluxio/runs/4037547872?check_suite_focus=true
    
    ```console
    Error: 9.386 [ERROR]
    alluxio.master.block.BlockMasterMetricsTest.testSize Time elapsed: 0.002
    s <<< FAILURE!
    java.lang.AssertionError: expected:<100> but was:<0>
    at org.junit.Assert.fail(Assert.java:89)
    at org.junit.Assert.failNotEquals(Assert.java:835)
    at org.junit.Assert.assertEquals(Assert.java:120)
    at org.junit.Assert.assertEquals(Assert.java:146)
    at
    alluxio.master.block.BlockMasterMetricsTest.testSize(BlockMasterMetricsTest.java:83)
    ```
    
    ### Does this PR introduce any user facing changes?
    
    No.
    
    pr-link: Alluxio/alluxio#14339
    change-id: cid-8ad79336b51a71f6fb7f60a133db9da9327e38b7

--- core/server/master/src/test/java/alluxio/master/block/BlockMasterMetricsTest.java
+++ core/server/master/src/test/java/alluxio/master/block/BlockMasterMetricsTest.java
@@ -18,7 +18,6 @@ import alluxio.Constants;
 import alluxio.MasterStorageTierAssoc;
 import alluxio.StorageTierAssoc;
 import alluxio.master.block.DefaultBlockMaster.Metrics;
-import alluxio.master.metastore.BlockStore;
 import alluxio.metrics.MetricInfo;
 import alluxio.metrics.MetricKey;
 import alluxio.metrics.MetricsSystem;
@@ -26,10 +25,8 @@ import alluxio.metrics.MetricsSystem;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.powermock.reflect.Whitebox;
 
 /**
  * Unit tests for {@link DefaultBlockMaster.Metrics}.
@@ -76,13 +73,9 @@ public final class BlockMasterMetricsTest {
         getGauge(MetricKey.CLUSTER_CAPACITY_FREE.getName() + MetricInfo.TIER + HDD));
   }
 
-  // TODO(david): ignore until we can test this through mocking other objects
-  @Ignore
   @Test
   public void testSize() {
-    BlockStore blockStore = Mockito.mock(BlockStore.class);
-    when(blockStore.size()).thenReturn(100L);
-    Whitebox.setInternalState(mBlockMaster, "mBlockStore", blockStore);
+    when(mBlockMaster.getUniqueBlockCount()).thenReturn(100L);
     assertEquals(100L, getGauge(MetricKey.MASTER_UNIQUE_BLOCKS.getName()));
   }
 
