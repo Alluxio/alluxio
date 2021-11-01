9ce2fd3fd0dd5c29a57b0bc9135662d70f46745c
Chris Zhu <christopher.zhu.9@gmail.com>
2021-11-01 12:55:52 -0700

    Use traverseOrClose instead of traverse

--- core/server/master/src/main/java/alluxio/master/file/meta/LockedInodePath.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/LockedInodePath.java
@@ -377,7 +377,7 @@ public class LockedInodePath implements Closeable {
 
     LockedInodePath newPath =
         new LockedInodePath(mUri, this, mPathComponents, LockPattern.WRITE_EDGE, mUseTryLock);
-    newPath.traverse();
+    newPath.traverseOrClose();
     return newPath;
   }
 
