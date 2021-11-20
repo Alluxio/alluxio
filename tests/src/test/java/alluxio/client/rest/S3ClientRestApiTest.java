32b5e3fcf9e761b906c3d971d900546432541814
Shouwei <shouwei@alluxio.com>
2021-11-19 16:48:20 -0800

    Fix tests

--- tests/src/test/java/alluxio/client/rest/S3ClientRestApiTest.java
+++ tests/src/test/java/alluxio/client/rest/S3ClientRestApiTest.java
@@ -183,10 +183,10 @@ public final class S3ClientRestApiTest extends RestApiTest {
     assertEquals(6, expected.getContents().size());
     assertEquals("file0", expected.getContents().get(0).getKey());
     assertEquals("file1", expected.getContents().get(1).getKey());
-    assertEquals("folder0", expected.getContents().get(2).getKey());
+    assertEquals("folder0/", expected.getContents().get(2).getKey());
     assertEquals("folder0/file0", expected.getContents().get(3).getKey());
     assertEquals("folder0/file1", expected.getContents().get(4).getKey());
-    assertEquals("folder1", expected.getContents().get(5).getKey());
+    assertEquals("folder1/", expected.getContents().get(5).getKey());
     assertEquals(0, expected.getCommonPrefixes().size());
 
     //parameters with delimiter="/"
@@ -223,7 +223,7 @@ public final class S3ClientRestApiTest extends RestApiTest {
         TestCaseOptions.defaults().setContentType(TestCaseOptions.XML_CONTENT_TYPE)).run();
 
     assertEquals(3, expected.getContents().size());
-    assertEquals("folder0", expected.getContents().get(0).getKey());
+    assertEquals("folder0/", expected.getContents().get(0).getKey());
     assertEquals("folder0/file0", expected.getContents().get(1).getKey());
     assertEquals("folder0/file1", expected.getContents().get(2).getKey());
     assertEquals(0, expected.getCommonPrefixes().size());
@@ -244,10 +244,10 @@ public final class S3ClientRestApiTest extends RestApiTest {
 
     assertEquals(5, expected.getContents().size());
     assertEquals("file1", expected.getContents().get(0).getKey());
-    assertEquals("folder0", expected.getContents().get(1).getKey());
+    assertEquals("folder0/", expected.getContents().get(1).getKey());
     assertEquals("folder0/file0", expected.getContents().get(2).getKey());
     assertEquals("folder0/file1", expected.getContents().get(3).getKey());
-    assertEquals("folder1", expected.getContents().get(4).getKey());
+    assertEquals("folder1/", expected.getContents().get(4).getKey());
     assertEquals(0, expected.getCommonPrefixes().size());
   }
 
