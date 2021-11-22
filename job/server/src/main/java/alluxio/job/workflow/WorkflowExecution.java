3294c1acf0a4b58b3e45d77856fca6bba22cfc86
Jianjian <jja725@gmail.com>
2021-11-22 14:06:40 -0800

    fix style

--- job/server/src/main/java/alluxio/job/workflow/WorkflowExecution.java
+++ job/server/src/main/java/alluxio/job/workflow/WorkflowExecution.java
@@ -21,7 +21,6 @@ import com.codahale.metrics.Counter;
 import com.google.common.base.Preconditions;
 
 import java.util.Set;
-
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
