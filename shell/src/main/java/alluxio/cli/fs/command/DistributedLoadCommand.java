d66717495d51f4da58a916dc992cc91d0df1dfad
Jianjian <jja725@gmail.com>
2021-11-30 16:28:58 -0800

    default to not use direct cache for now

--- shell/src/main/java/alluxio/cli/fs/command/DistributedLoadCommand.java
+++ shell/src/main/java/alluxio/cli/fs/command/DistributedLoadCommand.java
@@ -196,13 +196,13 @@ public final class DistributedLoadCommand extends AbstractDistributedJobCommand 
           .argName("batch-size")
           .desc("Number of files per request")
           .build();
-  private static final Option PASSIVE_CACHE_OPTION =
+  private static final Option DIRECT_CACHE_OPTION =
       Option.builder()
-          .longOpt("passive-cache")
+          .longOpt("direct-cache")
           .required(false)
           .hasArg(false)
           .desc("Use passive-cache or direct cache request,"
-              + " turn on if you want to use the old passive cache implementation")
+              + " don't turn on if you want to use the old passive cache implementation")
           .build();
 
   /**
@@ -225,7 +225,7 @@ public final class DistributedLoadCommand extends AbstractDistributedJobCommand 
         .addOption(INDEX_FILE)
         .addOption(HOSTS_OPTION)
         .addOption(HOST_FILE_OPTION)
-        .addOption(PASSIVE_CACHE_OPTION)
+        .addOption(DIRECT_CACHE_OPTION)
         .addOption(BATCH_SIZE_OPTION);
   }
 
@@ -243,7 +243,7 @@ public final class DistributedLoadCommand extends AbstractDistributedJobCommand 
         + "[--locality-file <localityFilePath>] "
         + "[--excluded-locality <locality1>,<locality2>,...,<localityN>] "
         + "[--excluded-locality-file <localityFilePath>] "
-        + "[--passive-cache] "
+        + "[--direct-cache] "
         + "<path>";
   }
 
@@ -262,7 +262,7 @@ public final class DistributedLoadCommand extends AbstractDistributedJobCommand 
     int defaultBatchSize = conf.getInt(PropertyKey.JOB_REQUEST_BATCH_SIZE);
     int replication = FileSystemShellUtils.getIntArg(cl, REPLICATION_OPTION, DEFAULT_REPLICATION);
     int batchSize = FileSystemShellUtils.getIntArg(cl, BATCH_SIZE_OPTION, defaultBatchSize);
-    boolean directCache = !cl.hasOption(PASSIVE_CACHE_OPTION.getLongOpt());
+    boolean directCache = cl.hasOption(DIRECT_CACHE_OPTION.getLongOpt());
     Set<String> workerSet = new HashSet<>();
     Set<String> excludedWorkerSet = new HashSet<>();
     Set<String> localityIds = new HashSet<>();
