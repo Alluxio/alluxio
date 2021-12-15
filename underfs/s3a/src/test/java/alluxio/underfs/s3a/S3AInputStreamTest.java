ec262028853f82b41109e260d1b10cecc45ee310
sliu114514 <liushiqi1122@gmail.com>
2021-12-15 04:42:38 +0000

    add unit test to S3AInputStreamTest and S3AUnderFileSystemFactoryTest

--- underfs/s3a/src/test/java/alluxio/underfs/s3a/S3AInputStreamTest.java
+++ underfs/s3a/src/test/java/alluxio/underfs/s3a/S3AInputStreamTest.java
@@ -1,7 +1,24 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package alluxio.underfs.s3a;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 import alluxio.retry.CountingRetry;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
@@ -14,84 +31,82 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.*;
-
 /**
  * Unit tests for the {@link S3AInputStream}.
  */
 public class S3AInputStreamTest {
-    private static final String BUCKET_NAME = "testBucket";
-    private static final String KEY = "testKey";
-    private static final byte[] testContent = new byte[]{0x01, 0x02, 0x03 ,0x04};
-    private AmazonS3 mClient;
-    private S3ObjectInputStream mIn[];
-    private S3AInputStream testedStream;
-    private InputStream[] testInputStream;
-    private S3Object[] S3Object;
+  private static final String BUCKET_NAME = "testBucket";
+  private static final String KEY = "testKey";
+  private static final byte[] TESTCONTENT = new byte[]{0x01, 0x02, 0x03, 0x04};
+  private AmazonS3 mClient;
+  private S3ObjectInputStream[] mIn;
+  private S3AInputStream mTestS3InputStream;
+  private InputStream[] mTestInputStream;
+  private S3Object[] mS3Object;
 
-    @Before
-    public void before() throws Exception {
-        testInputStream = new InputStream[testContent.length];
-        S3Object = new S3Object[testContent.length];
-        mIn = new S3ObjectInputStream[testContent.length];
-        mClient = Mockito.mock(AmazonS3.class);
+  @Before
+  public void before() throws Exception {
+    mTestInputStream = new InputStream[TESTCONTENT.length];
+    mS3Object = new S3Object[TESTCONTENT.length];
+    mIn = new S3ObjectInputStream[TESTCONTENT.length];
+    mClient = Mockito.mock(AmazonS3.class);
 
-        for(int i = 0; i < testContent.length; i ++) {
-            final long pos = i;
-            final int finalI = i;
-            S3Object[i] = Mockito.mock(S3Object.class);
-            mIn[i] = Mockito.mock(S3ObjectInputStream.class);
-            Mockito.when(mClient.getObject(argThat(argument -> {
-                if(argument != null) {
-                    if(argument.getRange() == null)
-                        return pos == 0;
-                    else
-                        return argument.getRange()[0] == pos;
-                }
-                return false;
-            }))).thenReturn(S3Object[i]);
-            Mockito.when(S3Object[i].getObjectContent()).thenReturn(mIn[i]);
-
-            byte[] mockInput = Arrays.copyOfRange(testContent, i, testContent.length);
-            testInputStream[i] = new ByteArrayInputStream(mockInput) ;
-
-            Mockito.when(mIn[i].read()).thenAnswer(invocation -> {
-                return testInputStream[finalI].read();
-            });
-            Mockito.when(mIn[i].read(any(byte[].class), anyInt(), anyInt())).thenAnswer(invocation -> {
-                byte[] b = invocation.getArgument(0);
-                int offset = invocation.getArgument(1);
-                int length = invocation.getArgument(2);
-
-                return testInputStream[finalI].read(b, offset, length);
-            });
+    for (int i = 0; i < TESTCONTENT.length; i++) {
+      final long pos = i;
+      final int finalI = i;
+      mS3Object[i] = Mockito.mock(S3Object.class);
+      mIn[i] = Mockito.mock(S3ObjectInputStream.class);
+      Mockito.when(mClient.getObject(argThat(argument -> {
+        if (argument != null) {
+          if (argument.getRange() == null) {
+            return pos == 0;
+          }
+          else {
+            return argument.getRange()[0] == pos;
+          }
         }
-        testedStream = new S3AInputStream(BUCKET_NAME, KEY, mClient, 0, new CountingRetry(1));
-    }
+        return false;
+      }))).thenReturn(mS3Object[i]);
+      Mockito.when(mS3Object[i].getObjectContent()).thenReturn(mIn[i]);
 
-    @Test
-    public void readSingleByte() throws Exception {
-        Assert.assertEquals((byte) 0x01, testedStream.read());
-        Assert.assertEquals((byte) 0x02, testedStream.read());
-        Assert.assertEquals((byte) 0x03, testedStream.read());
-        Assert.assertEquals((byte) 0x04, testedStream.read());
-        Assert.assertEquals( -1, testedStream.read());
-    }
+      byte[] mockInput = Arrays.copyOfRange(TESTCONTENT, i, TESTCONTENT.length);
+      mTestInputStream[i] = new ByteArrayInputStream(mockInput);
 
-    @Test
-    public void skipByte() throws Exception {
-        Assert.assertEquals((byte) 0x01, testedStream.read());
-        testedStream.skip(1);
-        Assert.assertEquals((byte) 0x03, testedStream.read());
-    }
+      Mockito.when(mIn[i].read()).thenAnswer(invocation -> {
+        return mTestInputStream[finalI].read();
+      });
+      Mockito.when(mIn[i].read(any(byte[].class), anyInt(), anyInt())).thenAnswer(invocation -> {
+        byte[] b = invocation.getArgument(0);
+        int offset = invocation.getArgument(1);
+        int length = invocation.getArgument(2);
 
-    @Test
-    public void readByteArray() throws Exception {
-        byte[] bytes = new byte[4];
-        int readCount = testedStream.read(bytes, 0, 4);
-        assertEquals(4, readCount);
-        assertArrayEquals(testContent, bytes);
+        return mTestInputStream[finalI].read(b, offset, length);
+      });
     }
+    mTestS3InputStream = new S3AInputStream(BUCKET_NAME, KEY, mClient, 0, new CountingRetry(1));
+  }
+
+  @Test
+  public void readSingleByte() throws Exception {
+    Assert.assertEquals((byte) 0x01, mTestS3InputStream.read());
+    Assert.assertEquals((byte) 0x02, mTestS3InputStream.read());
+    Assert.assertEquals((byte) 0x03, mTestS3InputStream.read());
+    Assert.assertEquals((byte) 0x04, mTestS3InputStream.read());
+    Assert.assertEquals(-1, mTestS3InputStream.read());
+  }
+
+  @Test
+  public void skipByte() throws Exception {
+    Assert.assertEquals((byte) 0x01, mTestS3InputStream.read());
+    mTestS3InputStream.skip(1);
+    Assert.assertEquals((byte) 0x03, mTestS3InputStream.read());
+  }
+
+  @Test
+  public void readByteArray() throws Exception {
+    byte[] bytes = new byte[4];
+    int readCount = mTestS3InputStream.read(bytes, 0, 4);
+    assertEquals(4, readCount);
+    assertArrayEquals(TESTCONTENT, bytes);
+  }
 }
--- underfs/s3a/src/test/java/alluxio/underfs/s3a/S3AUnderFileSystemFactoryTest.java
+++ underfs/s3a/src/test/java/alluxio/underfs/s3a/S3AUnderFileSystemFactoryTest.java
@@ -19,11 +19,7 @@ import alluxio.underfs.UnderFileSystemFactory;
 import alluxio.underfs.UnderFileSystemFactoryRegistry;
 
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import java.io.IOException;
 
 /**
  * Unit tests for the {@link S3AUnderFileSystemFactory}.
@@ -48,18 +44,20 @@ public class S3AUnderFileSystemFactoryTest {
   @Test
   public void createInstanceWithNullPath() {
     AlluxioConfiguration aConf = ConfigurationTestUtils.defaults();
-    UnderFileSystemFactory factory = UnderFileSystemFactoryRegistry.find("s3a://test-bucket/path", aConf);
+    UnderFileSystemFactory factory = UnderFileSystemFactoryRegistry.find("s3a://test-bucket/path",
+            aConf);
     UnderFileSystemConfiguration conf = UnderFileSystemConfiguration.defaults(aConf);
 
     Exception e = Assert.assertThrows(NullPointerException.class, () -> factory.create(null, conf));
-    Assert.assertEquals("path",e.getMessage());
+    Assert.assertEquals("path", e.getMessage());
   }
 
   @Test
-  public void createInstanceWithPath(){
+  public void createInstanceWithPath() {
     String path = "s3a://test-bucket/path";
     AlluxioConfiguration aConf = ConfigurationTestUtils.defaults();
-    UnderFileSystemFactory factory = UnderFileSystemFactoryRegistry.find("s3a://test-bucket/path", aConf);
+    UnderFileSystemFactory factory = UnderFileSystemFactoryRegistry.find("s3a://test-bucket/path",
+            aConf);
     UnderFileSystemConfiguration conf = UnderFileSystemConfiguration.defaults(aConf);
 
     UnderFileSystem ufs = factory.create(path, conf);
@@ -67,11 +65,11 @@ public class S3AUnderFileSystemFactoryTest {
   }
 
   @Test
-  public void supportsValidPath(){
+  public void supportsValidPath() {
     String path = "s3a://test-bucket/path";
     AlluxioConfiguration aConf = ConfigurationTestUtils.defaults();
-    UnderFileSystemFactory factory = UnderFileSystemFactoryRegistry.find("s3a://test-bucket/path", aConf);
-
+    UnderFileSystemFactory factory = UnderFileSystemFactoryRegistry.find("s3a://test-bucket/path",
+            aConf);
 
     Assert.assertTrue(factory.supportsPath(path));
   }
