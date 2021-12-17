7b5203db7e05e8587e5a16fb84ab4b20741dc749
bingzheng <bingzheng617@outlook.com>
2021-12-17 09:27:35 +0800

    Make FuseShell more general
    ### What changes are proposed in this pull request?
    Add FuseCommand interface and add subcommand implementation instead of
    hard code.
    ### Why are the changes needed?
    Be able to expand better
    ### Does this PR introduce any user facing changes?
    No
    Please list the user-facing changes introduced by your change, including
    No
    
    pr-link: Alluxio/alluxio#14647
    change-id: cid-d6be5a27a951e2dd1d6ffeb89415af0e1196141e

---
+++ integration/fuse/src/main/java/alluxio/cli/command/metadatacache/SizeCommand.java
@@ -0,0 +1,50 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.cli.command.metadatacache;
+
+import alluxio.AlluxioURI;
+import alluxio.Constants;
+import alluxio.client.file.FileSystem;
+import alluxio.client.file.MetadataCachingBaseFileSystem;
+import alluxio.client.file.URIStatus;
+import alluxio.conf.AlluxioConfiguration;
+import alluxio.wire.FileInfo;
+
+public final class SizeCommand extends AbstractMetadataCacheSubCommand {
+  public SizeCommand(FileSystem fs, AlluxioConfiguration conf, String parentCommandName) {
+    super(fs, conf, parentCommandName);
+  }
+
+  @Override
+  public String getCommandName() {
+    return "size";
+  }
+
+  @Override
+  public String getUsage() {
+    return String.format("%s%s.%s.%s", Constants.DEAFULT_FUSE_MOUNT,
+        Constants.ALLUXIO_CLI_PATH, getParentCommandName(), getCommandName());
+  }
+
+  @Override
+  protected URIStatus runSubCommand(AlluxioURI path, String [] argv,
+      MetadataCachingBaseFileSystem mFileSystem) {
+    // The 'ls -l' command will show metadata cache size in the <filesize> field.
+    long size = mFileSystem.getMetadataCacheSize();
+    return new URIStatus(new FileInfo().setLength(size).setCompleted(true));
+  }
+
+  @Override
+  public String getDescription() {
+    return "Get fuse client metadata size.";
+  }
+}
