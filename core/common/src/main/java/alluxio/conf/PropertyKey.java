f7b6f5e857b3fee16490607ad7856ed78b753d9f
Lu Qiu <luqiujob@gmail.com>
2022-01-18 14:53:40 -1000

    Fix property name

--- core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -7084,7 +7084,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
     public static final String FUSE_FS_NAME = "alluxio.fuse.fs.name";
     public static final String FUSE_JNIFUSE_ENABLED = "alluxio.fuse.jnifuse.enabled";
     public static final String FUSE_PERMISSION_CHECK_ENABLED
-        = "alluxio.fuse.permission.check,enabled";
+        = "alluxio.fuse.permission.check.enabled";
     public static final String FUSE_SHARED_CACHING_READER_ENABLED
         = "alluxio.fuse.shared.caching.reader.enabled";
     public static final String FUSE_LOGGING_THRESHOLD = "alluxio.fuse.logging.threshold";
