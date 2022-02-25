ea1a5519135145c617804569b90f6b109916a4ca
xwzbupt <xwze@bupt.cn>
2022-02-26 01:41:37 +0800

    [SMALLFIX] Fix the expected Exception

--- logserver/src/test/java/alluxio/logserver/AlluxioLog4jSocketNodeTest.java
+++ logserver/src/test/java/alluxio/logserver/AlluxioLog4jSocketNodeTest.java
@@ -25,7 +25,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.Hashtable;
-import org.junit.function.ThrowingRunnable;
 
 public class AlluxioLog4jSocketNodeTest {
 
