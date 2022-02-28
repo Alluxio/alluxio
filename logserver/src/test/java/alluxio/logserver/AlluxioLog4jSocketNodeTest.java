85dbe5d57f28fa21158a7d58e8c17aa58f346f6e
xwzbupt <xwze@bupt.cn>
2022-03-01 01:09:24 +0800

    Add the loop code

--- logserver/src/test/java/alluxio/logserver/AlluxioLog4jSocketNodeTest.java
+++ logserver/src/test/java/alluxio/logserver/AlluxioLog4jSocketNodeTest.java
@@ -13,8 +13,6 @@ package alluxio.logserver;
 
 import static alluxio.logserver.AlluxioLog4jSocketNode.setAcceptList;
 
-import java.io.InvalidClassException;
-
 import org.apache.commons.io.serialization.ValidatingObjectInputStream;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -35,17 +33,69 @@ import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InvalidClassException;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 public class AlluxioLog4jSocketNodeTest {
-
   private ByteArrayOutputStream mByteArrayOutputStream;
   private ObjectOutputStream mObjectOutputStream;
-  byte[] mBuffer;
+  private byte[] mBuffer;
   private ByteArrayInputStream mByteArrayInputStream;
   private ValidatingObjectInputStream mValidatingObjectInputStream;
+
+  /**
+   * Only test the class in java.lang which can be serialized. Boolean, Byte, Character, Double,
+   * Float, Integer, Long, Short, String
+   */
+  private List<Object> createPositiveObjectList() {
+    List<Object> positiveObjectList = new ArrayList<>();
+    Logger logger = Logger.getLogger("a");
+    positiveObjectList.add(new Hashtable<>());
+    positiveObjectList.add(
+        new LoggingEvent(
+            "fqnOfCategoryClass",
+            logger,
+            Level.DEBUG,
+            "message",
+            new Throwable()));
+    positiveObjectList.add(new LocationInfo("c", "b", "c", "d"));
+    positiveObjectList.add(new ThrowableInformation(new Throwable()));
+    positiveObjectList.add(new Boolean(true));
+    positiveObjectList.add((Byte) (byte) 0x11);
+    positiveObjectList.add((Double) 0.0);
+    positiveObjectList.add((Float) 1.0F);
+    positiveObjectList.add(1);
+    positiveObjectList.add((Long) 1L);
+    positiveObjectList.add((Short) (short) 1);
+    positiveObjectList.add("string");
+    return positiveObjectList;
+  }
+
+  private List<Object> createNegativeObjectList() {
+    List<Object> positiveObjectList = new ArrayList<>();
+    Logger logger = Logger.getLogger("a");
+    positiveObjectList.add(new LogEvent(
+        "category",
+        logger,
+        Level.DEBUG,
+        "message",
+        new Throwable()));
+    positiveObjectList.add(new LogLevel("lable", 1));
+    positiveObjectList.add(new AdapterLogRecord());
+    positiveObjectList.add(new Log4JLogRecord());
+    positiveObjectList.add(new LogTableColumn("label"));
+    positiveObjectList.add(Level.DEBUG);
+    positiveObjectList.add(UtilLoggingLevel.INFO);
+    positiveObjectList.add(new Throwable());
+    positiveObjectList.add(new LogLevelFormatException("message"));
+    positiveObjectList.add(new LogTableColumnFormatException("message"));
+    positiveObjectList.add(new PropertySetterException("string"));
+    return positiveObjectList;
+  }
 
   private void createOutputStreams() throws IOException {
     mByteArrayOutputStream = new ByteArrayOutputStream();
@@ -65,226 +115,32 @@ public class AlluxioLog4jSocketNodeTest {
   }
 
   @Test
-  public void testHashtable() throws IOException, ClassNotFoundException {
-    Object numbers = new Hashtable<>();
-    System.out.println(numbers.getClass());
-    createOutputStreams();
-    mObjectOutputStream.writeObject(numbers);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Hashtable<String, Integer> numbers1 = (Hashtable) mValidatingObjectInputStream.readObject();
+  public void testPositiveObject() throws IOException, ClassNotFoundException {
+    List<Object> list = new ArrayList<>();
+    list = createPositiveObjectList();
+    for (Object object : list) {
+      createOutputStreams();
+      mObjectOutputStream.writeObject(object);
+      write2BufferAndCloseOutputStream();
+      createInputStreamsAndsetAcceptList();
+      Object numbers1 = mValidatingObjectInputStream.readObject();
+      System.out.println(numbers1.getClass());
+    }
   }
 
   @Test
-  public void testLoggingEvent() throws IOException, ClassNotFoundException {
-    Logger logger = Logger.getLogger("a");
-    LoggingEvent event = new LoggingEvent(
-        "fqnOfCategoryClass", logger, Level.DEBUG, "message", new Throwable());
-    createOutputStreams();
-    mObjectOutputStream.writeObject(event);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    LoggingEvent event1 = (LoggingEvent) mValidatingObjectInputStream.readObject();
-  }
-
-  @Test
-  public void testLocationInfo() throws IOException, ClassNotFoundException {
-    Object object = new LocationInfo("c", "b", "c", "d");
-    createOutputStreams();
-    mObjectOutputStream.writeObject(object);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    LocationInfo locationInfo1 = (LocationInfo) mValidatingObjectInputStream.readObject();
-  }
-
-  @Test
-  public void testThrowableInformation() throws IOException, ClassNotFoundException {
-    ThrowableInformation throwableInformation = new ThrowableInformation(new Throwable());
-    createOutputStreams();
-    mObjectOutputStream.writeObject(throwableInformation);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    ThrowableInformation throwableInformation1 =
-        (ThrowableInformation) mValidatingObjectInputStream.readObject();
-  }
-
-  /**
-   * Only test the class in java.lang which can be serialized. Boolean, Byte, Character, Double,
-   * Float, Integer, Long, Short, String
-   */
-  @Test
-  public void testJavaLang() throws IOException, ClassNotFoundException {
-    Boolean bool = new Boolean(true);
-    Byte byteNum = 0x11;
-    Character character = new Character('a');
-    Double doubleNum = 0.0;
-    Float floatNum = (float) 1;
-    Integer integer = 1;
-    Long longNum = (long) 1;
-    Short shortNum = 1;
-    String string = "test";
-
-    createOutputStreams();
-
-    mObjectOutputStream.writeObject(bool);
-    mObjectOutputStream.writeObject(byteNum);
-    mObjectOutputStream.writeObject(character);
-    mObjectOutputStream.writeObject(doubleNum);
-    mObjectOutputStream.writeObject(floatNum);
-    mObjectOutputStream.writeObject(integer);
-    mObjectOutputStream.writeObject(longNum);
-    mObjectOutputStream.writeObject(shortNum);
-    mObjectOutputStream.writeObject(string);
-
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-
-    Boolean bool1 = (Boolean) mValidatingObjectInputStream.readObject();
-    Byte byteNum1 = (Byte) mValidatingObjectInputStream.readObject();
-    Character character1 = (Character) mValidatingObjectInputStream.readObject();
-    Double doubleNum1 = (Double) mValidatingObjectInputStream.readObject();
-    Float floatNum1 = (Float) mValidatingObjectInputStream.readObject();
-    Integer integer1 = (Integer) mValidatingObjectInputStream.readObject();
-    Long longNum1 = (Long) mValidatingObjectInputStream.readObject();
-    Short shortNum1 = (Short) mValidatingObjectInputStream.readObject();
-    String string1 = (String) mValidatingObjectInputStream.readObject();
-  }
-
-  @Test
-  public void testLogEvent() throws IOException, ClassNotFoundException {
-    Logger logger = Logger.getLogger("a");
-    LogEvent logEvent = new LogEvent("category", logger, Level.DEBUG, "message", new Throwable());
-    createOutputStreams();
-    mObjectOutputStream.writeObject(logEvent);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      LogEvent logEvent1 = (LogEvent) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLogLevel() throws IOException, ClassNotFoundException {
-    LogLevel logLevel = new LogLevel("lable", 1);
-    createOutputStreams();
-    mObjectOutputStream.writeObject(logLevel);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      LogLevel logLevel1 = (LogLevel) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLogRecord() throws IOException {
-    AdapterLogRecord adapterLogRecord = new AdapterLogRecord();
-    createOutputStreams();
-    mObjectOutputStream.writeObject(adapterLogRecord);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      AdapterLogRecord adapterLogRecord1 = (AdapterLogRecord) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLog4JLogRecord() throws IOException, ClassNotFoundException {
-    Logger logger = Logger.getLogger("a");
-    Log4JLogRecord log4JLogRecord = new Log4JLogRecord();
-    createOutputStreams();
-    mObjectOutputStream.writeObject(log4JLogRecord);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      Log4JLogRecord log4JLogRecord1 = (Log4JLogRecord) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLogTableColumn() throws IOException, ClassNotFoundException {
-    LogTableColumn logTableColumn = new LogTableColumn("label");
-    createOutputStreams();
-    mObjectOutputStream.writeObject(logTableColumn);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      LogTableColumn logTableColumn1 = (LogTableColumn) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLevel() throws IOException, ClassNotFoundException {
-    Level level = Level.DEBUG;
-    createOutputStreams();
-    mObjectOutputStream.writeObject(level);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      Level level1 = (Level) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testUtilLoggingLevel() throws IOException, ClassNotFoundException {
-    UtilLoggingLevel utilLoggingLevel = UtilLoggingLevel.INFO;
-    createOutputStreams();
-    mObjectOutputStream.writeObject(utilLoggingLevel);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      UtilLoggingLevel utilLoggingLevel1 = (UtilLoggingLevel) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testThrowable() throws IOException, ClassNotFoundException {
-    Throwable throwable = new Throwable();
-    createOutputStreams();
-    mObjectOutputStream.writeObject(throwable);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      Throwable throwable1 = (Throwable) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLogLevelFormatException() throws IOException, ClassNotFoundException {
-    LogLevelFormatException logLevelFormatException = new LogLevelFormatException("message");
-    createOutputStreams();
-    mObjectOutputStream.writeObject(logLevelFormatException);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      LogLevelFormatException logLevelFormatException1 = (
-          LogLevelFormatException) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testLogTableColumnFormatException() throws IOException, ClassNotFoundException {
-    LogTableColumnFormatException logTableColumnFormatException = new LogTableColumnFormatException(
-        "message");
-    createOutputStreams();
-    mObjectOutputStream.writeObject(logTableColumnFormatException);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      LogTableColumnFormatException logTableColumnFormatException1 = (
-          LogTableColumnFormatException) mValidatingObjectInputStream.readObject();
-    });
-  }
-
-  @Test
-  public void testPropertySetterException() throws IOException, ClassNotFoundException {
-    PropertySetterException propertySetterException = new PropertySetterException("string");
-    createOutputStreams();
-    mObjectOutputStream.writeObject(propertySetterException);
-    write2BufferAndCloseOutputStream();
-    createInputStreamsAndsetAcceptList();
-    Assert.assertThrows(InvalidClassException.class, () -> {
-      PropertySetterException propertySetterException1 = (
-          PropertySetterException) mValidatingObjectInputStream.readObject();
-    });
+  public void testNegativeObject() throws IOException, ClassNotFoundException {
+    List<Object> list = new ArrayList<>();
+    list = createNegativeObjectList();
+    for(Object object : list){
+      System.out.println(object.getClass());
+      createOutputStreams();
+      mObjectOutputStream.writeObject(object);
+      write2BufferAndCloseOutputStream();
+      createInputStreamsAndsetAcceptList();
+      Assert.assertThrows(InvalidClassException.class, () -> {
+        Object object1 = mValidatingObjectInputStream.readObject();
+      });
+    }
   }
 }
