eac62daf7374b490fe3c3ba5ac4fcc9552439916
David Zhu <david@alluxio.com>
2022-02-28 10:39:50 -0800

    logError

--- core/server/common/src/main/java/alluxio/RpcUtils.java
+++ core/server/common/src/main/java/alluxio/RpcUtils.java
@@ -144,7 +144,7 @@ public final class RpcUtils {
         }
       }
       throw AlluxioStatusException.fromIOException(e).toGrpcStatusException();
-    } catch (RuntimeException e) {
+    } catch (RuntimeException | LinkageError e) {
       logger.error("Exit (Error): {}: {}", methodName,
           String.format(description, processObjects(logger, args)), e);
       MetricsSystem.counter(getQualifiedFailureMetricName(methodName)).inc();
