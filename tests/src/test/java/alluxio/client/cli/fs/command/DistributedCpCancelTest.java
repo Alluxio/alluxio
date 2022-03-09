7cb05b74f92f69326971f9cede0e6164f5918d79
Jianjian <jja725@gmail.com>
2022-03-09 13:33:28 -0800

    try to use sleep ufs to block open

--- tests/src/test/java/alluxio/client/cli/fs/command/DistributedCpCancelTest.java
+++ tests/src/test/java/alluxio/client/cli/fs/command/DistributedCpCancelTest.java
@@ -11,76 +11,63 @@
 
 package alluxio.client.cli.fs.command;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.AdditionalAnswers.answersWithDelay;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
 import alluxio.AlluxioURI;
 import alluxio.Constants;
 import alluxio.UnderFileSystemFactoryRegistryRule;
 import alluxio.client.cli.fs.AbstractFileSystemShellTest;
-import alluxio.client.file.FileInStream;
-import alluxio.client.file.FileSystemTestUtils;
-import alluxio.grpc.WritePType;
 import alluxio.job.plan.migrate.MigrateConfig;
 import alluxio.job.util.JobTestUtils;
 import alluxio.job.wire.Status;
-import alluxio.metrics.MetricKey;
-import alluxio.metrics.MetricsSystem;
 import alluxio.testutils.underfs.sleeping.SleepingUnderFileSystemFactory;
 import alluxio.testutils.underfs.sleeping.SleepingUnderFileSystemOptions;
-import alluxio.util.io.PathUtils;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import com.google.common.base.Joiner;
+import java.io.FileWriter;
 import com.google.common.collect.Sets;
-import com.google.common.io.Files;
-import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.internal.stubbing.answers.AnswersWithDelay;
-import org.mockito.internal.stubbing.answers.Returns;
 
 /**
  * Tests for cross-mount {@link alluxio.cli.fs.command.DistributedCpCommand}.
  */
 public final class DistributedCpCancelTest extends AbstractFileSystemShellTest {
-  private static final long SLEEP_MS = Constants.SECOND_MS * 60;
-  private static final int TEST_TIMEOUT = 15;
+  private static final long SLEEP_MS = Constants.SECOND_MS * 7;
+  private static final int TEST_TIMEOUT = 10;
+  private String mLocalUfsPath;
+
   @ClassRule
   public static UnderFileSystemFactoryRegistryRule sUnderfilesystemfactoryregistry =
       new UnderFileSystemFactoryRegistryRule(new SleepingUnderFileSystemFactory(
           new SleepingUnderFileSystemOptions()
-              .setIsDirectoryMs(SLEEP_MS)
-              .setIsFileMs(SLEEP_MS)
-              .setGetStatusMs(SLEEP_MS)
-              .setListStatusMs(SLEEP_MS)
-              .setListStatusWithOptionsMs(SLEEP_MS)
-              .setExistsMs(SLEEP_MS)
-              .setMkdirsMs(SLEEP_MS)));
+              .setOpenMs(SLEEP_MS)));
   @Rule
-  public TemporaryFolder mFolder = new TemporaryFolder();
+  public TemporaryFolder mTempFolder = new TemporaryFolder();
+  @Before
+  public void before() throws Exception {
+    mLocalUfsPath = mTempFolder.getRoot().getAbsolutePath();
+
+    sFileSystem.mount(new AlluxioURI("/mnt/"), new AlluxioURI("sleep://" + mLocalUfsPath));
+
+    new File(mLocalUfsPath + "/dir/").mkdirs();
+    FileWriter fileWriter = new FileWriter(mLocalUfsPath + "/dir/file");
+    fileWriter.write("test");
+    fileWriter.close();
+  }
+
 
   @Test
   public void testDistributedCpCancelStats() throws Exception {
-    FileSystemTestUtils.createByteFile(sFileSystem, "/testFileSource", WritePType.THROUGH, 10);
 
     long jobId = sJobMaster.run(new MigrateConfig(
-        "/testFileSource", "/testFileDest", "THROUGH", false));
-    try (MockedStatic<IOUtils> utilities = Mockito.mockStatic(IOUtils.class)) {
-      utilities.when(()->IOUtils.copyLarge(any(FileInStream.class),any())).thenAnswer(answersWithDelay(1000, new Returns(1L)));
-    }
+        "/mnt/dir/file", "/testFileDest", "THROUGH", false));
+    // wait for execution until blocked by FileInStream in = fileSystem.openFile() in migration job
+    Thread.sleep(Constants.SECOND_MS * 3);
     sJobShell.run("cancel", Long.toString(jobId));
-
     JobTestUtils
         .waitForJobStatus(sJobMaster, jobId, Sets.newHashSet(Status.CANCELED), TEST_TIMEOUT);
-    Assert.assertTrue(sFileSystem.exists(new AlluxioURI("/testFileDest")));
+    Assert.assertFalse(sFileSystem.exists(new AlluxioURI("/testFileDest")));
   }
 }
