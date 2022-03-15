5b1dad713ddbae375e5361bea4b288c9b3142a70
Jianjian <jja725@gmail.com>
2022-03-15 12:17:08 -0700

    fix style

--- job/server/src/main/java/alluxio/job/plan/migrate/MigrateDefinition.java
+++ job/server/src/main/java/alluxio/job/plan/migrate/MigrateDefinition.java
@@ -24,6 +24,7 @@ import alluxio.conf.PropertyKey;
 import alluxio.conf.ServerConfiguration;
 import alluxio.exception.ExceptionMessage;
 import alluxio.exception.FileAlreadyExistsException;
+import alluxio.exception.status.CancelledException;
 import alluxio.grpc.CreateFilePOptions;
 import alluxio.grpc.OpenFilePOptions;
 import alluxio.grpc.ReadPType;
