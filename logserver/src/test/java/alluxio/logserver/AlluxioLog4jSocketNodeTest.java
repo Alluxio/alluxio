d48e195ac8c66dbce594b6daf04d5f8ac230ff50
Bowen Ding <bowen@alluxio.com>
2022-03-29 18:11:48 +0800

    Add timeout and negative test cases

--- logserver/src/test/java/alluxio/logserver/AlluxioLog4jSocketNodeTest.java
+++ logserver/src/test/java/alluxio/logserver/AlluxioLog4jSocketNodeTest.java
@@ -12,7 +12,11 @@
 package alluxio.logserver;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
+import alluxio.Constants;
+import alluxio.conf.InstancedConfiguration;
+import alluxio.conf.PropertyKey;
 import alluxio.util.executor.ExecutorServiceFactories;
 
 import org.apache.commons.io.serialization.ValidatingObjectInputStream;
@@ -23,6 +27,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InvalidClassException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Properties;
@@ -30,19 +38,22 @@ import java.util.concurrent.ExecutorService;
 
 public class AlluxioLog4jSocketNodeTest {
   private static final Logger LOG = LoggerFactory.getLogger(AlluxioLog4jSocketNodeTest.class);
-  private static final int LOG_SERVER_PORT = 5001;
+  private static final String LOG_SERVER_HOST =
+      InstancedConfiguration.defaults().getOrDefault(PropertyKey.LOGSERVER_HOSTNAME, "localhost");
+  private static final int LOG_SERVER_PORT = Integer.parseInt(
+      InstancedConfiguration.defaults().getOrDefault(PropertyKey.LOGSERVER_PORT, "45600"));
 
   private static void setupLog4J() {
     Properties properties = new Properties();
     properties.setProperty("log4j.rootLogger", "ALL,socket");
     properties.setProperty("log4j.appender.socket", "org.apache.log4j.net.SocketAppender");
-    properties.setProperty("log4j.appender.socket.RemoteHost", "localhost");
+    properties.setProperty("log4j.appender.socket.RemoteHost", LOG_SERVER_HOST);
     properties.setProperty("log4j.appender.socket.port", Integer.toString(LOG_SERVER_PORT));
     PropertyConfigurator.configure(properties);
   }
 
   @Test
-  public void testAcceptList() throws Exception {
+  public void testAccept() throws Exception {
     ExecutorService runner =
         ExecutorServiceFactories.fixedThreadPool(getClass().getName(), 1).create();
     runner.submit(() -> {
@@ -55,30 +66,69 @@ public class AlluxioLog4jSocketNodeTest {
     });
 
     try (ServerSocket socket = new ServerSocket(LOG_SERVER_PORT)) {
-      Socket client = socket.accept();
-      try (ValidatingObjectInputStream mValidatingObjectInputStream =
-               new ValidatingObjectInputStream(client.getInputStream())) {
-        AlluxioLog4jSocketNode.setAcceptList(mValidatingObjectInputStream);
+      socket.setSoTimeout(Constants.SECOND_MS * 5);
+      try (Socket client = socket.accept()) {
+        client.setSoTimeout(Constants.SECOND_MS * 5);
+        try (ValidatingObjectInputStream mValidatingObjectInputStream =
+                 new ValidatingObjectInputStream(client.getInputStream())) {
+          AlluxioLog4jSocketNode.setAcceptList(mValidatingObjectInputStream);
 
-        LoggingEvent event = (LoggingEvent) mValidatingObjectInputStream.readObject();
-        assertEquals("trace", event.getRenderedMessage());
-        assertEquals(Level.TRACE, event.getLevel());
+          LoggingEvent event = (LoggingEvent) mValidatingObjectInputStream.readObject();
+          assertEquals("trace", event.getRenderedMessage());
+          assertEquals(Level.TRACE, event.getLevel());
 
-        event = (LoggingEvent) mValidatingObjectInputStream.readObject();
-        assertEquals("debug", event.getRenderedMessage());
-        assertEquals(Level.DEBUG, event.getLevel());
+          event = (LoggingEvent) mValidatingObjectInputStream.readObject();
+          assertEquals("debug", event.getRenderedMessage());
+          assertEquals(Level.DEBUG, event.getLevel());
 
-        event = (LoggingEvent) mValidatingObjectInputStream.readObject();
-        assertEquals("info", event.getRenderedMessage());
-        assertEquals(Level.INFO, event.getLevel());
+          event = (LoggingEvent) mValidatingObjectInputStream.readObject();
+          assertEquals("info", event.getRenderedMessage());
+          assertEquals(Level.INFO, event.getLevel());
 
-        event = (LoggingEvent) mValidatingObjectInputStream.readObject();
-        assertEquals("warn", event.getRenderedMessage());
-        assertEquals(Level.WARN, event.getLevel());
+          event = (LoggingEvent) mValidatingObjectInputStream.readObject();
+          assertEquals("warn", event.getRenderedMessage());
+          assertEquals(Level.WARN, event.getLevel());
 
-        event = (LoggingEvent) mValidatingObjectInputStream.readObject();
-        assertEquals("error", event.getRenderedMessage());
-        assertEquals(Level.ERROR, event.getLevel());
+          event = (LoggingEvent) mValidatingObjectInputStream.readObject();
+          assertEquals("error", event.getRenderedMessage());
+          assertEquals(Level.ERROR, event.getLevel());
+        }
+      }
+    }
+  }
+
+  private static class NotAllowedClass implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int mCounter;
+
+    public NotAllowedClass(int count) {
+      mCounter = count;
+    }
+  }
+
+  @Test
+  public void testReject() throws Exception {
+    ExecutorService runner =
+        ExecutorServiceFactories.fixedThreadPool(getClass().getName(), 1).create();
+    runner.submit(() -> {
+      try (Socket client = new Socket(LOG_SERVER_HOST, LOG_SERVER_PORT);
+          ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream())) {
+        oos.writeObject(new NotAllowedClass(1));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
+
+    try (ServerSocket socket = new ServerSocket(LOG_SERVER_PORT)) {
+      socket.setSoTimeout(Constants.SECOND_MS * 5);
+      try (Socket client = socket.accept()) {
+        client.setSoTimeout(Constants.SECOND_MS * 5);
+        try (ValidatingObjectInputStream mValidatingObjectInputStream =
+                 new ValidatingObjectInputStream(client.getInputStream())) {
+          AlluxioLog4jSocketNode.setAcceptList(mValidatingObjectInputStream);
+
+          assertThrows(InvalidClassException.class, mValidatingObjectInputStream::readObject);
+        }
       }
     }
   }
