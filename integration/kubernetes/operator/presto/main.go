ca4a690ff64f9a93b711e8e95cb17c718ae5ac12
Beinan Wang <soros.wang@yahoo.com>
2022-04-13 12:44:27 -0700

    Init operator project by running operator-sdk

--- integration/kubernetes/operator/presto/main.go
+++ integration/kubernetes/operator/presto/main.go
@@ -30,9 +30,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	prestov1alpha1 "github.com/Alluxio/alluxio/api/v1alpha1"
-	"github.com/Alluxio/alluxio/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -44,7 +41,6 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	utilruntime.Must(prestov1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -78,13 +74,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controllers.PrestoClusterReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "PrestoCluster")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
