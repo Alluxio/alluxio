a4b28879e3bcbae92cd28ac97d3b714824e11506
Bowen Ding <6999708+dbw9580@users.noreply.github.com>
2022-04-21 14:45:32 +0800

    merge without resolving conflicts

--- core/common/src/main/java/alluxio/metrics/MetricKey.java
+++ core/common/src/main/java/alluxio/metrics/MetricKey.java
@@ -258,6 +258,13 @@ public final class MetricKey implements Comparable<MetricKey> {
           .setDescription("Size of the absent cache")
           .setMetricType(MetricType.GAUGE)
           .build();
+  public static final MetricKey MASTER_ABSENT_PATH_CACHE_QUEUE_SIZE =
+      new Builder("Master.AbsentPathCacheQueueSize")
+          .setDescription("Alluxio maintains a cache of absent UFS paths. "
+              + "This is the number of UFS paths being processed.")
+          .setMetricType(MetricType.GAUGE)
+          .setIsClusterAggregated(false)
+          .build();
 
   // Edge cache stats
   public static final MetricKey MASTER_EDGE_CACHE_EVICTIONS =
@@ -736,6 +743,7 @@ public final class MetricKey implements Comparable<MetricKey> {
           .setMetricType(MetricType.COUNTER)
           .setIsClusterAggregated(false)
           .build();
+<<<<<<< HEAD
   public static final MetricKey MASTER_METADATA_SYNC_PREFETCH_EXECUTOR =
       new Builder("Master.MetadataSyncPrefetchExecutor")
           .setDescription(String.format("Metrics concerning the master metadata sync prefetch"
@@ -756,6 +764,23 @@ public final class MetricKey implements Comparable<MetricKey> {
           .setMetricType(MetricType.GAUGE)
           .setIsClusterAggregated(false)
           .build();
+||||||| parent of acc5dadd40 (Remove duplicate metric for absent cache)
+  public static final MetricKey MASTER_UFS_ABSENT_PATH_CACHE_SIZE =
+      new Builder("Master.UfsAbsentPathCacheSize")
+          .setDescription("Alluxio maintains a cache of absent UFS paths. "
+              + "This is the number of UFS paths cached.")
+          .setMetricType(MetricType.GAUGE)
+          .setIsClusterAggregated(false)
+          .build();
+  public static final MetricKey MASTER_UFS_ABSENT_PATH_CACHE_QUEUE_SIZE =
+      new Builder("Master.UfsAbsentPathCacheQueueSize")
+          .setDescription("Alluxio maintains a cache of absent UFS paths. "
+              + "This is the number of UFS paths being processed.")
+          .setMetricType(MetricType.GAUGE)
+          .setIsClusterAggregated(false)
+          .build();
+=======
+>>>>>>> acc5dadd40 (Remove duplicate metric for absent cache)
   public static final MetricKey MASTER_METADATA_SYNC_PREFETCH_EXECUTOR_QUEUE_SIZE =
       new Builder("Master.MetadataSyncPrefetchExecutorQueueSize")
           .setDescription("The number of queuing prefetch tasks in the metadata sync thread pool"
