f56c7e149f24dfa3197f437658c59a474abda487
shuaiwuyue <827505905@qq.com>
2022-05-25 11:27:57 +0800

    update

--- core/server/master/src/test/java/alluxio/underfs/UfsStatusCacheTest.java
+++ core/server/master/src/test/java/alluxio/underfs/UfsStatusCacheTest.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
