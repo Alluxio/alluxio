6e7cbf68e9e15e6bd67667e8602a05e7d78d4c60
Jianjian <jja725@gmail.com>
2022-05-25 18:12:53 -0700

    add runtime exception

---
+++ core/common/src/main/java/alluxio/exception/JobDoesNotExistRuntimeException.java
@@ -0,0 +1,33 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.exception;
+
+import io.grpc.Status;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+/**
+ * The exception thrown when a job definition does not exist in Alluxio.
+ */
+public class JobDoesNotExistRuntimeException extends AlluxioRuntimeException {
+  private static final long serialVersionUID = -7291730624984048562L;
+  private static final Status STATUS = Status.UNIMPLEMENTED;
+
+  /**
+   * Constructs a new exception with the specified detail message.
+   *
+   * @param message the detail message
+   */
+  public JobDoesNotExistRuntimeException(String message) {
+    super(STATUS, message);
+  }
+}
