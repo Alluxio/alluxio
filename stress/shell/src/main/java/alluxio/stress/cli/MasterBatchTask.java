80f49adeb872dede81213134f66048fc24142406
Jianjian <jja725@gmail.com>
2022-05-30 16:42:44 -0700

    fix comment

--- stress/shell/src/main/java/alluxio/stress/cli/MasterBatchTask.java
+++ stress/shell/src/main/java/alluxio/stress/cli/MasterBatchTask.java
@@ -106,7 +106,7 @@ public class MasterBatchTask extends BatchTask {
         // add all base parameters
         command.addAll(mBaseParameter.toBatchTaskArgumentString());
 
-        commandList.add(command.toArray(new String[0]));
+        commandList.add(command.toArray(new String[command.size()]));
       }
     }
     return commandList;
