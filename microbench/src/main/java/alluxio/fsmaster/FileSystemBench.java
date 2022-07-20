12b3770520ab1f72d10da1f7b81e7b5413991c45
Arthur Jenoudet <23088925+jenoudet@users.noreply.github.com>
2022-07-19 17:14:30 -0700

    creating FileSystem#getStatus microbenchmark

---
+++ microbench/src/main/java/alluxio/fsmaster/FileSystemBench.java
@@ -0,0 +1,60 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.fsmaster;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.CommandLineOptionException;
+import org.openjdk.jmh.runner.options.CommandLineOptions;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class FileSystemBench {
+  @State(Scope.Benchmark)
+  public static class FileSystem {
+    FileSystemBase mBase = new FileSystemBase();
+
+    @Setup(Level.Trial)
+    public void setup() throws Exception {
+      mBase.init();
+    }
+
+    @TearDown
+    public void tearDown() throws Exception {
+      mBase.tearDown();
+    }
+  }
+
+  @Benchmark
+  public void getStatusBench(FileSystem fs, Blackhole bh) throws Exception {
+    bh.consume(fs.mBase.getStatus());
+  }
+
+  public static void main(String[] args) throws RunnerException, CommandLineOptionException {
+    Options argsCli = new CommandLineOptions(args);
+    Options opts = new OptionsBuilder()
+        .parent(argsCli)
+        .include(FileSystemBench.class.getName())
+        .result("results.json")
+        .resultFormat(ResultFormatType.JSON)
+        .build();
+    new Runner(opts).run();
+  }
+}
