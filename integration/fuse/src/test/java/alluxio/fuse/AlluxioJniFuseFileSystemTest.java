8d0360dadad8620fee791de6f247f2bd449fe746
secfree <secfree.teng@gmail.com>
2022-07-24 23:03:57 +0800

    Fix AlluxioJniFuseFileSystemTest.read

--- integration/fuse/src/test/java/alluxio/fuse/AlluxioJniFuseFileSystemTest.java
+++ integration/fuse/src/test/java/alluxio/fuse/AlluxioJniFuseFileSystemTest.java
@@ -411,11 +411,12 @@ public class AlluxioJniFuseFileSystemTest {
     setUpOpenMock(expectedPath);
 
     FileInStream fakeInStream = mock(FileInStream.class);
-    when(fakeInStream.read(any(byte[].class),
+    mFileSystem.getStatus(expectedPath).getFileInfo().setLength(4);
+    when(fakeInStream.read(any(ByteBuffer.class),
         anyInt(), anyInt())).then((Answer<Integer>) invocationOnMock -> {
-          byte[] myDest = (byte[]) invocationOnMock.getArguments()[0];
+          ByteBuffer myDest = (ByteBuffer) invocationOnMock.getArguments()[0];
           for (byte i = 0; i < 4; i++) {
-            myDest[i] = i;
+            myDest.put(i, i);
           }
           return 4;
         });
@@ -431,7 +432,6 @@ public class AlluxioJniFuseFileSystemTest {
     mFuseFs.open("/foo/bar", mFileInfo);
 
     mFuseFs.read("/foo/bar", ptr, 4, 0, mFileInfo);
-    ptr.flip();
     final byte[] dst = new byte[4];
     ptr.get(dst, 0, 4);
     final byte[] expected = new byte[] {0, 1, 2, 3};
