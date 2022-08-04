1ada2ac8c7a840c70735f2586a0ae4496026cc4c
flaming archer <2011xuesong@gmail.com>
2022-08-04 15:42:19 +0800

    Fix master down when master change to leader
    Fix master down when master change to leader but last time failed
    because of startMasters
    
    ### What changes are proposed in this pull request?
    Fix master down when master change to leader but last time failed
    because of startMasters
    ### Why are the changes needed?
    The master change leader failed because of startMasters, but it is not
    set mRaftJournalWriter = null; The next time the master change to leader
    , it will check `Preconditions.checkState(mRaftJournalWriter == null);`,
    so the master will exit.
    
    ### Does this PR introduce any user facing changes?
    no
    
    pr-link: Alluxio/alluxio#15694
    change-id: cid-1db55a983933bc3485e4cb60da8f0fc97469894b

--- core/server/master/src/main/java/alluxio/master/FaultTolerantAlluxioMasterProcess.java
+++ core/server/master/src/main/java/alluxio/master/FaultTolerantAlluxioMasterProcess.java
@@ -166,6 +166,7 @@ final class FaultTolerantAlluxioMasterProcess extends AlluxioMasterProcess {
       startMasters(true);
     } catch (UnavailableException e) {
       LOG.warn("Error starting masters: {}", e.toString());
+      mJournalSystem.losePrimacy();
       stopMasters();
       return false;
     }
