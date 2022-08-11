8dfaaea0a908b6ce8e3d791837803f0a1368f809
LDawns <lsm@smail.nju.edu.cn>
2022-08-11 09:49:00 +0800

    fix style problems.

--- core/common/src/main/java/alluxio/util/BitsUtils.java
+++ core/common/src/main/java/alluxio/util/BitsUtils.java
@@ -11,21 +11,22 @@
 
 package alluxio.util;
 
+/**
+ * CuckooBitSet utility.
+ */
 public class BitsUtils {
+  /**
+   * Merge word b into word a with given range.
+   * @param a target word
+   * @param b source word
+   * @param fromBitIndex start bit index
+   * @param toBitIndex end bit index
+   * @return the merged value
+   */
   public static long mergeWord(long a, long b, int fromBitIndex, int toBitIndex) {
     long mask = genMask(fromBitIndex, toBitIndex);
     long v = b << fromBitIndex;
     return mergeWord(a, v, mask);
-  }
-
-  public static long getValueFromWord(long word, int fromBitIndex, int toBitIndex) {
-    return ((word & genMask(fromBitIndex, toBitIndex)) >>> fromBitIndex);
-  }
-
-  public static long genMask(int fromBitIndex, long toBitIndex) {
-    long l1 = -1L << fromBitIndex;
-    long l2 = (toBitIndex >= 64) ? (0L) : (-1L << toBitIndex);
-    return (l1 ^ l2);
   }
 
   /**
@@ -39,60 +40,25 @@ public class BitsUtils {
   }
 
   /**
-   * @param a    value to merge in non-masked bits
-   * @param b    value to merge in masked bits
-   * @param mask 1 where bits from b should be selected; 0 where from a
-   * @return result of (a & ~mask) | (b & mask)
+   * Get the value from a word with given range.
+   * @param word the word to get the value from
+   * @param fromBitIndex the start bit index
+   * @param toBitIndex the end bit index
+   * @return the value from the word
    */
-  public static long mergeWordOpt(long a, long b, long mask) {
-    return a ^ ((a ^ b) & mask);
+  public static long getValueFromWord(long word, int fromBitIndex, int toBitIndex) {
+    return ((word & genMask(fromBitIndex, toBitIndex)) >>> fromBitIndex);
   }
 
-  public static boolean hasZero4(long x) {
-    return hasZero4Internal(x) != 0;
-  }
-
-  public static boolean hasZero8(long x) {
-    return hasZero8Internal(x) != 0;
-  }
-
-  public static boolean hasZero12(long x) {
-    return hasZero12Internal(x) != 0;
-  }
-
-  public static boolean hasZero16(long x) {
-    return hasZero16Internal(x) != 0;
-  }
-
-  public static boolean hasValue4(long x, int n) {
-    return (hasZero4Internal((x) ^ (0x1111L * (n)))) != 0;
-  }
-
-  public static boolean hasValue8(long x, int n) {
-    return (hasZero8Internal((x) ^ (0x01010101L * (n)))) != 0;
-  }
-
-  public static boolean hasValue12(long x, int n) {
-    return (hasZero12Internal((x) ^ (0x001001001001L * (n)))) != 0;
-  }
-
-  public static boolean hasValue16(long x, int n) {
-    return (hasZero16Internal((x) ^ (0x0001000100010001L * (n)))) != 0;
-  }
-
-  private static long hasZero4Internal(long x) {
-    return (((x) - 0x1111L) & (~(x)) & 0x8888L);
-  }
-
-  private static long hasZero8Internal(long x) {
-    return (((x) - 0x01010101L) & (~(x)) & 0x80808080L);
-  }
-
-  private static long hasZero12Internal(long x) {
-    return (((x) - 0x001001001001L) & (~(x)) & 0x800800800800L);
-  }
-
-  private static long hasZero16Internal(long x) {
-    return (((x) - 0x0001000100010001L) & (~(x)) & 0x8000800080008000L);
+  /**
+   * Generate mask from fromBitIndex to toBitIndex.
+   * @param fromBitIndex start bit index
+   * @param toBitIndex end bit index
+   * @return mask
+   */
+  public static long genMask(int fromBitIndex, long toBitIndex) {
+    long l1 = -1L << fromBitIndex;
+    long l2 = (toBitIndex >= 64) ? (0L) : (-1L << toBitIndex);
+    return (l1 ^ l2);
   }
 }
