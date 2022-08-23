53dfd879c0d98087fd90a50d3fe541f98273c09b
maobaolong <307499405@qq.com>
2022-08-23 11:53:49 +0800

    Fix failed test

--- core/server/master/src/test/java/alluxio/master/file/FileSystemMasterTest.java
+++ core/server/master/src/test/java/alluxio/master/file/FileSystemMasterTest.java
@@ -296,7 +296,8 @@ public final class FileSystemMasterTest {
     // cannot delete root
     try {
       mFileSystemMaster.delete(ROOT_URI,
-          DeleteContext.mergeFrom(DeletePOptions.newBuilder().setRecursive(true)));
+          DeleteContext.mergeFrom(DeletePOptions.newBuilder().setRecursive(true)
+              .setDeleteMountPoint(true)));
       fail("Should not have been able to delete the root");
     } catch (InvalidPathException e) {
       assertEquals(ExceptionMessage.DELETE_ROOT_DIRECTORY.getMessage(), e.getMessage());
