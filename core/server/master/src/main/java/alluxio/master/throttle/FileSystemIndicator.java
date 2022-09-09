fd66cb436585160a2389ecbefe0ef657a12be33e
yyongycy <yong.sunny@163.com>
2022-09-09 15:57:45 +0800

    Add extra monitoring indicators part2

---
+++ core/server/master/src/main/java/alluxio/master/throttle/FileSystemIndicator.java
@@ -0,0 +1,337 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master.throttle;
+
+import alluxio.metrics.MetricsSystem;
+
+import com.google.common.base.MoreObjects;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * The filesystem indicator object.
+ */
+public class FileSystemIndicator {
+  // The observed counter names
+  private static final List<String> OBSERVED_MASTER_COUNTER = new LinkedList<>();
+  // The tbd counter name
+  private static final List<String> DUMP_MASTER_COUNTER = new LinkedList<>();
+
+  static {
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_COMPLETE_FILE_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_COMPLETED_OPERATION_RETRY_COUNT);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_CREATE_FILE_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_CREATE_DIRECTORIES_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_DELETE_PATH_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_DIRECTORIES_CREATED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILE_BLOCK_INFOS_GOT);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILE_INFOS_GOT);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_COMPLETED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_CREATED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_FREED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_PERSISTED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FREE_FILE_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_FILE_BLOCK_INFO_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_FILE_INFO_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_NEW_BLOCK_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_EVICTIONS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_HITS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_LOAD_TIMES);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_MISSES);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_ACTIVE_PATHS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_FAIL);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_NO_CHANGE);
+
+    // This is the concerns
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_OPS_COUNT);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PATHS_CANCEL);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PATHS_FAIL);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PATHS_SUCCESS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PENDING_PATHS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_CANCEL);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_FAIL);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_OPS_COUNT);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_PATHS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_RETRIES);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_SUCCESS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_SKIPPED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_SUCCESS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_TIME_MS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_MOUNT_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_NEW_BLOCKS_GOT);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_DELETED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_MOUNTED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_RENAMED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_UNMOUNTED);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_RENAME_PATH_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_SET_ACL_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_SET_ATTRIBUTE_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_UFS_STATUS_CACHE_CHILDREN_SIZE);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_UFS_STATUS_CACHE_SIZE);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_UNMOUNT_OPS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_CONFIG_HASH_IN_PROGRESS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_CONFIGURATION_IN_PROGRESS);
+    OBSERVED_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_REGISTER_WORKER_START_IN_PROGRESS);
+
+    // Following counters will be dumped to log
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_COMPLETE_FILE_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_COMPLETED_OPERATION_RETRY_COUNT);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_CREATE_DIRECTORIES_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_CREATE_DIRECTORIES_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_DELETE_PATH_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_DIRECTORIES_CREATED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILE_BLOCK_INFOS_GOT);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILE_INFOS_GOT);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_COMPLETED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_CREATED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_FREED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FILES_PERSISTED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_FREE_FILE_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_FILE_BLOCK_INFO_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_FILE_INFO_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_NEW_BLOCK_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_EVICTIONS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_HITS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_LOAD_TIMES);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_LISTING_CACHE_MISSES);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_ACTIVE_PATHS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_FAIL);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_NO_CHANGE);
+
+    // This is the concerns
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_OPS_COUNT);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PATHS_CANCEL);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PATHS_FAIL);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PATHS_SUCCESS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PENDING_PATHS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_CANCEL);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_FAIL);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_OPS_COUNT);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_PATHS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_RETRIES);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_PREFETCH_SUCCESS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_SKIPPED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_SUCCESS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_METADATA_SYNC_TIME_MS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_MOUNT_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_NEW_BLOCKS_GOT);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_DELETED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_MOUNTED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_RENAMED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_PATHS_UNMOUNTED);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_RENAME_PATH_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_SET_ACL_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_SET_ATTRIBUTE_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_UFS_STATUS_CACHE_CHILDREN_SIZE);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_UFS_STATUS_CACHE_SIZE);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_UNMOUNT_OPS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_CONFIG_HASH_IN_PROGRESS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_GET_CONFIGURATION_IN_PROGRESS);
+    DUMP_MASTER_COUNTER
+        .add(MetricsMonitorUtils.FileSystemCounterName.MASTER_REGISTER_WORKER_START_IN_PROGRESS);
+  }
+
+  private Map<String, Long> mMasterIndicators;
+  private long mPitTimeMS;
+
+  /**
+   * Filesystem indicator Constructor.
+   */
+  public FileSystemIndicator() {
+    mMasterIndicators = new HashMap<>();
+    for (String countName : OBSERVED_MASTER_COUNTER) {
+      mMasterIndicators.put(countName, MetricsSystem.METRIC_REGISTRY
+          .counter(countName).getCount());
+    }
+    mPitTimeMS = System.currentTimeMillis();
+  }
+
+  /**
+   * Filesystem indicator constructor.
+   *
+   * @param fileSystemIndicator the filesystem indicator
+   */
+  public FileSystemIndicator(FileSystemIndicator fileSystemIndicator) {
+    mMasterIndicators = new HashMap<>();
+    mMasterIndicators.putAll(fileSystemIndicator.mMasterIndicators);
+    mPitTimeMS = fileSystemIndicator.mPitTimeMS;
+  }
+
+  /**
+   * @param name the counter name
+   * @return the counter
+   */
+  public Optional<Long> getCounter(String name) {
+    if (mMasterIndicators.containsKey(name)) {
+      return Optional.of(mMasterIndicators.get(name));
+    }
+    return Optional.empty();
+  }
+
+  /**
+   * @return the point in time
+   */
+  public long getPitTimeMS() {
+    return mPitTimeMS;
+  }
+
+  /**
+   * Sets the counter.
+   *
+   * @param name the counter name
+   * @param value the counter value
+   */
+  public void setCounter(String name, long value) {
+    mMasterIndicators.put(name, value);
+  }
+
+  /**
+   * Sets the pit time.
+   *
+   * @param pitTimeMS the pit
+   */
+  public void setPitTimeMS(long pitTimeMS) {
+    mPitTimeMS = pitTimeMS;
+  }
+
+  /**
+   * Calculates the delta.
+   *
+   * @param baselineIndicators the baseline indicator object
+   */
+  public void deltaTo(FileSystemIndicator baselineIndicators) {
+    for (String name : OBSERVED_MASTER_COUNTER) {
+      mMasterIndicators.computeIfPresent(name,
+          (key, value)
+              -> { return value - baselineIndicators.mMasterIndicators.getOrDefault(key, 0L); });
+    }
+  }
+
+  /**
+   * @return the string of the indicators
+   */
+  public String toString() {
+    MoreObjects.ToStringHelper toStringHelper = MoreObjects.toStringHelper(this);
+    for (Map.Entry entry : mMasterIndicators.entrySet()) {
+      toStringHelper.add((String) (entry.getKey()), entry.getValue());
+    }
+    return toStringHelper.toString();
+  }
+}
+
---
+++ core/server/master/src/main/java/alluxio/master/throttle/ServerIndicator.java
@@ -0,0 +1,253 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master.throttle;
+
+import alluxio.conf.Configuration;
+import alluxio.conf.PropertyKey;
+import alluxio.metrics.MetricsSystem;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The server indicators.
+ */
+public class ServerIndicator {
+  private static final Logger LOG = LoggerFactory.getLogger(ServerIndicator.class);
+  private Long mHeapMax;
+  private Long mHeapUsed;
+  private Double mCpuLoad;
+
+  private long mTotalJVMPauseTimeMS;
+  private long mPITTotalJVMPauseTimeMS;
+  private long mRpcQueueSize;
+
+  private long mPitTimeMS;
+
+  /**
+   * @param heapMax the max heap
+   * @param heapUsed the used heap
+   * @param cpuLoad the cpu load
+   * @param totalJVMPauseTimeMS the total JVM pause time since previous PIT
+   * @param pitTotalJVMPauseTimeMS the pit total JVM pause time
+   * @param rpcQueueSize the pit rpc queue size
+   * @param snapshotTimeMS the pit time
+   */
+  public ServerIndicator(long heapMax, long heapUsed, double cpuLoad,
+      long totalJVMPauseTimeMS, long pitTotalJVMPauseTimeMS, long rpcQueueSize,
+      long snapshotTimeMS) {
+    mHeapMax = heapMax;
+    mHeapUsed = heapUsed;
+    mCpuLoad = cpuLoad;
+    mTotalJVMPauseTimeMS = totalJVMPauseTimeMS;
+    mPITTotalJVMPauseTimeMS = pitTotalJVMPauseTimeMS;
+    mRpcQueueSize = rpcQueueSize;
+    mPitTimeMS = snapshotTimeMS;
+  }
+
+  /**
+   * @param serverIndicator the server indicator
+   */
+  public ServerIndicator(ServerIndicator serverIndicator) {
+    this(serverIndicator, 1);
+  }
+
+  /**
+   * The scaled server indicator according to the multiple.
+   *
+   * @param serverIndicator the base server indicator
+   * @param multiple times of multiple
+   */
+  public ServerIndicator(ServerIndicator serverIndicator, int multiple) {
+    Preconditions.checkNotNull(serverIndicator, "serverIndicator");
+    mHeapMax = serverIndicator.mHeapMax;
+    mHeapUsed = serverIndicator.mHeapUsed * multiple;
+    mCpuLoad = serverIndicator.mCpuLoad * multiple;
+    mTotalJVMPauseTimeMS = serverIndicator.mTotalJVMPauseTimeMS * multiple;
+    mPITTotalJVMPauseTimeMS = serverIndicator.mPITTotalJVMPauseTimeMS;
+    mRpcQueueSize = serverIndicator.mRpcQueueSize * multiple;
+    mPitTimeMS = serverIndicator.mPitTimeMS;
+  }
+
+  /**
+   * @return current total JVM pause time
+   */
+  public static long getSystemTotalJVMPauseTime() {
+    if (Configuration.getBoolean(PropertyKey.MASTER_JVM_MONITOR_ENABLED)) {
+      return (long) (MetricsSystem.METRIC_REGISTRY
+          .gauge(MetricsMonitorUtils.ServerGaugeName.TOTAL_EXTRA_TIME, null).getValue());
+    }
+    return 0;
+  }
+
+  /**
+   * creates server indicator based on current status.
+   *
+   * @param prevTotalJVMPauseTime the previous total JVM pause time
+   * @return the server indicator
+   */
+  public static ServerIndicator createFromMetrics(long prevTotalJVMPauseTime) {
+    long pitTotalJVMPauseTime = 0;
+    long totalJVMPauseTime = 0;
+    long rpcQueueSize = 0;
+
+    if (Configuration.getBoolean(PropertyKey.MASTER_JVM_MONITOR_ENABLED)) {
+      pitTotalJVMPauseTime = (long) (MetricsSystem.METRIC_REGISTRY
+          .gauge(MetricsMonitorUtils.ServerGaugeName.TOTAL_EXTRA_TIME, null).getValue());
+      totalJVMPauseTime = pitTotalJVMPauseTime - prevTotalJVMPauseTime;
+    }
+    try {
+      rpcQueueSize = (long) (MetricsSystem.METRIC_REGISTRY
+          .gauge(MetricsMonitorUtils.ServerGaugeName.RPC_QUEUE_LENGTH, null).getValue());
+    } catch (NullPointerException e) {
+      // ignore
+    }
+
+    return new ServerIndicator((long) (MetricsSystem.METRIC_REGISTRY
+        .gauge(MetricsMonitorUtils.MemoryGaugeName.HEAP_MAX, null).getValue()),
+        (long) (MetricsSystem.METRIC_REGISTRY
+        .gauge(MetricsMonitorUtils.MemoryGaugeName.HEAP_USED, null).getValue()),
+        (double) (MetricsSystem.METRIC_REGISTRY
+            .gauge(MetricsMonitorUtils.OSGaugeName.OS_CPU_LOAD, null).getValue()),
+        totalJVMPauseTime, pitTotalJVMPauseTime, rpcQueueSize, System.currentTimeMillis());
+  }
+
+  /**
+   * Creates a threshold indicator object.
+   *
+   * @param ratioOfUsedHeap the ratio of used heap
+   * @param cpuLoad the cpu load
+   * @param totalJVMPauseTimeMS the total JVM pause time
+   * @param rpcQueueSize the rpc queue size
+   * @return  the threshold indicator object
+   */
+  public static ServerIndicator createThresholdIndicator(
+      double ratioOfUsedHeap, double cpuLoad, long totalJVMPauseTimeMS, long rpcQueueSize) {
+    long pitTotalJVMPauseTimeMS = 0;
+    long heapMax = (long) (MetricsSystem.METRIC_REGISTRY
+        .gauge(MetricsMonitorUtils.MemoryGaugeName.HEAP_MAX, null).getValue());
+    long heapUsed = (long) (ratioOfUsedHeap * heapMax);
+
+    if (Configuration.getBoolean(PropertyKey.MASTER_JVM_MONITOR_ENABLED)) {
+      pitTotalJVMPauseTimeMS = (long) (MetricsSystem.METRIC_REGISTRY
+          .gauge(MetricsMonitorUtils.ServerGaugeName.TOTAL_EXTRA_TIME, null).getValue());
+    }
+    return new ServerIndicator(heapMax,
+        heapUsed, cpuLoad, totalJVMPauseTimeMS, pitTotalJVMPauseTimeMS,
+        rpcQueueSize, System.currentTimeMillis());
+  }
+
+  /**
+   * @return the max heap
+   */
+  public Long getHeapMax() {
+    return mHeapMax;
+  }
+
+  /**
+   * @return the used heap
+   */
+  public Long getHeapUsed() {
+    return mHeapUsed;
+  }
+
+  /**
+   * @return the cpu load
+   */
+  public Double getCpuLoad() {
+    return mCpuLoad;
+  }
+
+  /**
+   * @return the total JVM pause time
+   */
+  public long getTotalJVMPauseTimeMS() {
+    return mTotalJVMPauseTimeMS;
+  }
+
+  /**
+   * @return the PIT total JVM pause time
+   */
+  public long getPITTotalJVMPauseTimeMS() {
+    return mPITTotalJVMPauseTimeMS;
+  }
+
+  /**
+   * @return the rpc queue size
+   */
+  public long getRpcQueueSize() {
+    return mRpcQueueSize;
+  }
+
+  /**
+   * @return the time to measure the indicators
+   */
+  public long getPitTimeMS() {
+    return mPitTimeMS;
+  }
+
+  /**
+   * Sets the pit time.
+   *
+   * @param pitTimeMS the pit time
+   */
+  public void setPitTimeMS(long pitTimeMS) {
+    mPitTimeMS = pitTimeMS;
+  }
+
+  /**
+   * Addition the server indicator object.
+   *
+   * @param serverIndicator the server indicator
+   */
+  public void addition(ServerIndicator serverIndicator) {
+    // This is to record the aggregated time
+    mRpcQueueSize += serverIndicator.getRpcQueueSize();
+    mCpuLoad += serverIndicator.getCpuLoad();
+    mHeapUsed += serverIndicator.getHeapUsed();
+    mTotalJVMPauseTimeMS += serverIndicator.getTotalJVMPauseTimeMS();
+  }
+
+  /**
+   * Gets the delta the server indicator based on server indicator.
+   *
+   * @param serverIndicator the base server indicator
+   */
+  public void reduction(ServerIndicator serverIndicator) {
+    mRpcQueueSize = mRpcQueueSize > serverIndicator.mRpcQueueSize
+        ? mRpcQueueSize - serverIndicator.mRpcQueueSize : 0;
+    mCpuLoad = Double.compare(mCpuLoad, serverIndicator.mCpuLoad) > 0
+        ? mCpuLoad - serverIndicator.mCpuLoad : 0;
+    mHeapUsed = mHeapUsed > serverIndicator.mHeapUsed
+        ? mHeapUsed - serverIndicator.mHeapUsed : 0;
+    mTotalJVMPauseTimeMS = mTotalJVMPauseTimeMS > serverIndicator.mTotalJVMPauseTimeMS
+        ? mTotalJVMPauseTimeMS - serverIndicator.mTotalJVMPauseTimeMS : 0;
+  }
+
+  /**
+   * @return the string object
+   */
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("heapMax", mHeapMax)
+        .add("heapUsed", mHeapUsed)
+        .add("cpuLoad", mCpuLoad)
+        .add("pitTotalJVMPauseTimeMS", mPITTotalJVMPauseTimeMS)
+        .add("totalJVMPauseTimeMS", mTotalJVMPauseTimeMS)
+        .add("rpcQueueSize", mRpcQueueSize)
+        .add("pitTimeMS", mPitTimeMS)
+        .toString();
+  }
+}
+
