025ca19d09167a06a270d9b325491ae9113f4cd8
elega <445092967@qq.com>
2022-09-30 19:17:31 +0800

    Avoid and dedup concurrent metadata sync
    ### What changes are proposed in this pull request?
    
    Update the metadata sync logic to avoid concurrent metadata sync on the
    same path #16241
    
    ### Why are the changes needed?
    
    If multiple requests attempt to sync the same directory, the metadata
    sync might happen over and over and bring huge load to UFS.
    
    ### Does this PR introduce any user facing changes?
    
    1. Larger lock scope -> previously the Inode Path Lock will be RELEASED
    of the metadata sync of the sync uri root is done and the children are
    processed by worker threads which they will acquire locks separately.
    This means that once the root sync is done, another request can come in
    and trigger another sync. With this patch, this won't be available any
    more. If you do a sync /, the whole alluxio inode tree will be blocked
    from another sync until it's done. Though this doesn't block other
    threads read the directory (e.g. getFileInfo or ListStatus, because they
    don't use the metadata sync lock.) I think this is an expected behavior.
    2. Two lock mechanisms -> which can potentially block each other. We
    acquire the metadata sync lock first and then add lock for the inode
    path tree. If the inode path is accessed by other threads and the
    metadata sync struggles to acquire the inode lock, this will blocks
    other metadata sync requests to acquire the metadata sync lock. Though I
    think this is still valid because these metadata sync threads should not
    trigger other metadata syncs before the current one is done anyways.
    3. No timeout -> I really want to set some timeout here but seems like
    in alluxio, tryLock with a timeout is barely used and our Lock
    components don't have such interface at all. We might need to keep an
    eye on any potential issues caused by a path being locked by the
    metadata lock too long.
    
    pr-link: Alluxio/alluxio#16241
    change-id: cid-cbbfec05f0ec654cfc0703c97d67b6a2e765eec7

--- core/server/master/src/main/java/alluxio/master/file/InodeSyncStream.java
+++ core/server/master/src/main/java/alluxio/master/file/InodeSyncStream.java
@@ -69,6 +69,7 @@ import alluxio.underfs.UfsManager;
 import alluxio.underfs.UfsStatus;
 import alluxio.underfs.UfsStatusCache;
 import alluxio.underfs.UnderFileSystem;
+import alluxio.util.CommonUtils;
 import alluxio.util.LogUtils;
 import alluxio.util.interfaces.Scoped;
 import alluxio.util.io.PathUtils;
@@ -262,6 +263,15 @@ public class InodeSyncStream {
       PropertyKey.MASTER_FILE_SYSTEM_MERGE_INODE_JOURNALS
   );
 
+  /** To determine whether we should only let the UFS sync happen once
+   * for the concurrent metadata sync requests syncing the same directory.
+   */
+  private final boolean mDedupConcurrentSync = Configuration.getBoolean(
+      PropertyKey.MASTER_METADATA_CONCURRENT_SYNC_DEDUP
+  );
+  private static final MetadataSyncLockManager SYNC_METADATA_LOCK_MANAGER =
+      new MetadataSyncLockManager();
+
   /**
    * Whether the caller is {@link FileSystemMaster#getFileInfo(AlluxioURI, GetStatusContext)}.
    * This is used for the {@link #mUfsSyncPathCache}.
@@ -383,10 +393,27 @@ public class InodeSyncStream {
 
   /**
    * Sync the metadata according the root path the stream was created with.
+   * [WARNING]:
+   * To avoid deadlock, please do not obtain any inode path lock before calling this method.
    *
    * @return SyncStatus object
    */
   public SyncStatus sync() throws AccessControlException, InvalidPathException {
+    long syncStartTs = CommonUtils.getCurrentMs();
+    if (!mDedupConcurrentSync) {
+      return syncInternal(syncStartTs);
+    }
+    try (MetadataSyncLockManager.MetadataSyncPathList ignored = SYNC_METADATA_LOCK_MANAGER.lockPath(
+        mRootScheme.getPath())) {
+      mRpcContext.throwIfCancelled();
+      return syncInternal(syncStartTs);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private SyncStatus syncInternal(long syncStartTs) throws
+      AccessControlException, InvalidPathException {
     // The high-level process for the syncing is:
     // 1. Given an Alluxio path, determine if it is not consistent with the corresponding UFS path.
     //     this means the UFS path does not exist, or has metadata which differs from Alluxio
@@ -402,13 +429,38 @@ public class InodeSyncStream {
       DefaultFileSystemMaster.Metrics.INODE_SYNC_STREAM_SKIPPED.inc();
       return SyncStatus.NOT_NEEDED;
     }
+    if (mDedupConcurrentSync) {
+      /*
+       * If a sync had already started after the sync initialized by the current thread
+       * which has covered the targets this thread wants to sync,
+       * then there is no need to trigger another sync.
+       * * e.g.
+       * 1. [TS=100] the sync() method is called by thread A for path /aaa
+       * 2. [TS=110] the sync() method is called by thread B for path /aaa,
+       *    thread B is blocked by the new introduced metadata sync lock
+       * 3. [TS=180] thread A finishes the metadata sync, update the SyncPathCache,
+       *    setting the last sync timestamp to 180.
+       * 4. [TS=182] thread B acquired the lock and can start sync
+       * 5. [TS=182] thread B need to check if in the past (182-110=72) unit of time,
+       *    the same path has been synced by other thread,
+       *    if yes, just skip the sync and return NOT_NEEDED.
+       * Note that this still applies if A is to sync recursively path /aaa while B is to
+       * sync path /aaa/bbb as the sync scope of A covers B's.
+       */
+      long syncIntervalToPreventConcurrentSync = Instant.now().toEpochMilli() - syncStartTs;
+      boolean shouldSync = mUfsSyncPathCache.shouldSyncPath(mRootScheme.getPath().toString(),
+          syncIntervalToPreventConcurrentSync, mIsGetFileInfo).isShouldSync();
+      if (!shouldSync) {
+        DefaultFileSystemMaster.Metrics.INODE_SYNC_STREAM_SKIPPED.inc();
+        return SyncStatus.NOT_NEEDED;
+      }
+    }
     Instant startTime = Instant.now();
     try (LockedInodePath path =
              mInodeTree.lockInodePath(mRootScheme, mRpcContext.getJournalContext())) {
       if (mAuditContext != null && mAuditContextSrcInodeFunc != null) {
         mAuditContext.setSrcInode(mAuditContextSrcInodeFunc.apply(path));
       }
-
       syncInodeMetadata(path);
       syncPathCount++;
       if (mDescendantType == DescendantType.ONE) {
