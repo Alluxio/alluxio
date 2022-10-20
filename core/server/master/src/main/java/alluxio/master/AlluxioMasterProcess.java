7a4e35240f7389efa448e6be6e8bdb8dc68ea82d
Arthur Jenoudet <23088925+jenoudet@users.noreply.github.com>
2022-10-20 10:26:17 -0700

    Fix gauges when creating a new rpc server
    When master gain/regain primacy they create a new RPC server and
    executor related metrics along with it. These metrics do not get
    reregistered when a master is promoted to leader for a second time. This
    PR fixes this problem.
    
    pr-link: Alluxio/alluxio#16257
    change-id: cid-28a9cd492347b82fb0b18baf7613c8726785e766

--- core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioMasterProcess.java
@@ -531,10 +531,13 @@ public class AlluxioMasterProcess extends MasterProcess {
     // Create an executor for Master RPC server.
     mRPCExecutor = ExecutorServiceBuilder.buildExecutorService(
         ExecutorServiceBuilder.RpcExecutorHost.MASTER);
+    MetricsSystem.removeMetrics(MetricKey.MASTER_RPC_QUEUE_LENGTH.getName());
     MetricsSystem.registerGaugeIfAbsent(MetricKey.MASTER_RPC_QUEUE_LENGTH.getName(),
         mRPCExecutor::getRpcQueueLength);
+    MetricsSystem.removeMetrics(MetricKey.MASTER_RPC_THREAD_ACTIVE_COUNT.getName());
     MetricsSystem.registerGaugeIfAbsent(MetricKey.MASTER_RPC_THREAD_ACTIVE_COUNT.getName(),
         mRPCExecutor::getActiveCount);
+    MetricsSystem.removeMetrics(MetricKey.MASTER_RPC_THREAD_CURRENT_COUNT.getName());
     MetricsSystem.registerGaugeIfAbsent(MetricKey.MASTER_RPC_THREAD_CURRENT_COUNT.getName(),
         mRPCExecutor::getPoolSize);
     // Create underlying gRPC server.
