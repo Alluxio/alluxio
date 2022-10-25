0f0f3037d1a47ce543b17fb199aa28fb04fcfeba
lucyge2022 <111789461+lucyge2022@users.noreply.github.com>
2022-10-25 15:02:19 -0700

    Implement S3 headbucket API
    Implement HeadBucket call
    
    Users using probing which shoots headbucket gets incorrectly routed to
    getbucket, it needs to have correct headbucket implentation.
    
    User will get correct headbucket behavior with correct responding time
    
    pr-link: Alluxio/alluxio#16393
    change-id: cid-5866e796785882c3f8c25c7815fc85fc05f2e282
    Conflicts:
    	core/server/proxy/src/main/java/alluxio/proxy/s3/S3ErrorCode.java

--- core/server/proxy/src/main/java/alluxio/proxy/s3/S3RestServiceHandler.java
+++ core/server/proxy/src/main/java/alluxio/proxy/s3/S3RestServiceHandler.java
@@ -251,6 +251,27 @@ public final class S3RestServiceHandler {
   }
 
   /**
+   * HeadBucket - head a bucket to check for existence.
+   * @param bucket
+   * @return the response object
+   */
+  @HEAD
+  @Path(BUCKET_PARAM)
+  public Response headBucket(
+          @PathParam("bucket") final String bucket) {
+    return S3RestUtils.call(bucket, () -> {
+      String bucketPath = S3RestUtils.parsePath(AlluxioURI.SEPARATOR + bucket);
+      final String user = getUser();
+      final FileSystem userFs = S3RestUtils.createFileSystemForUser(user, mMetaFS);
+
+      try (S3AuditContext auditContext = createAuditContext("headBucket", user, bucket, null)) {
+        S3RestUtils.checkPathIsAlluxioDirectory(userFs, bucketPath, auditContext);
+      }
+      return Response.ok().build();
+    });
+  }
+
+  /**
    * Gets a bucket and lists all the objects or bucket tags in it.
    * @param authorization header parameter authorization
    * @param bucket the bucket name
