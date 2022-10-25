fbb8c8ac73daff836a518b4b4d9c8b4160c23d70
secfree <secfree.teng@gmail.com>
2022-10-25 18:10:47 +0800

    Simplify the solution

--- core/client/fs/src/main/java/alluxio/client/block/BlockStoreClient.java
+++ core/client/fs/src/main/java/alluxio/client/block/BlockStoreClient.java
@@ -157,14 +157,6 @@ public final class BlockStoreClient {
       LOG.info("Added {} to failedWorkers for {}", dataSource, e.toString());
       failedWorkers.put(dataSource, System.currentTimeMillis());
       throw e;
-    } catch (IOException e) {
-      // Some places convert UnavailableException to IOException,
-      // for example, BlockWorkerClient$Factory.create
-      if ((e.getCause() != null) && (e.getCause() instanceof UnavailableException)) {
-        LOG.info("Added {} to failedWorkers for {}", dataSource, e.toString());
-        failedWorkers.put(dataSource, System.currentTimeMillis());
-      }
-      throw e;
     }
   }
 
