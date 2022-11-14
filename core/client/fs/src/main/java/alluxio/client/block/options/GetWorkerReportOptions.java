0549466b3c94381a0862adb9c2f562c558c04afb
yiichan <Yichuan_Sun@163.com>
2022-11-14 14:57:33 +0800

    Add BLOCK_COUNT in gRPC WorkerInfoField
    ### What changes are proposed in this pull request?
    
    Enum variant `BLOCK_COUNT` of `WorkerInfoField` in
    `GetWorkerReportOptions.java` was introduced in #14126, but its gRPC
    counterpart was not updated accordingly.
    This PR adds the missing variant, and reorders the fields of the java
    enum.
    
    ### Why are the changes needed?
    
    The field cannot be retrieved via RPC, only via internal function call,
    because gRPC will fail to serialize that java enum variant.
    
    ### Does this PR introduce any user facing changes?
    
    No.
    
    pr-link: Alluxio/alluxio#16457
    change-id: cid-e9478f69baa064003bfa5e9309963994c5298eb5

--- core/client/fs/src/main/java/alluxio/client/block/options/GetWorkerReportOptions.java
+++ core/client/fs/src/main/java/alluxio/client/block/options/GetWorkerReportOptions.java
@@ -188,7 +188,6 @@ public final class GetWorkerReportOptions implements Serializable {
    */
   public enum WorkerInfoField {
     ADDRESS,
-    BLOCK_COUNT,
     WORKER_CAPACITY_BYTES,
     WORKER_CAPACITY_BYTES_ON_TIERS,
     ID,
@@ -196,7 +195,8 @@ public final class GetWorkerReportOptions implements Serializable {
     START_TIME_MS,
     STATE,
     WORKER_USED_BYTES,
-    WORKER_USED_BYTES_ON_TIERS;
+    WORKER_USED_BYTES_ON_TIERS,
+    BLOCK_COUNT;
 
     public static final Set<WorkerInfoField> ALL = EnumSet.allOf(WorkerInfoField.class);
 
