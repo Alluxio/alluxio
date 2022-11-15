ac0d773f33ffc6654b0c2e10de6a3ae4f656a8f5
Shuaibing Zhao <zhaoshbnbjl@gmail.com>
2022-11-15 11:52:19 +0800

    rm unused import

--- core/server/master/src/main/java/alluxio/master/journal/tool/UfsJournalDumper.java
+++ core/server/master/src/main/java/alluxio/master/journal/tool/UfsJournalDumper.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
