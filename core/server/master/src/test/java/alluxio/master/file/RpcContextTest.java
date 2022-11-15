c836332519feb785093cc72d40d6a1baf93756ee
Shuaibing Zhao <zhaoshbnbjl@gmail.com>
2022-11-15 14:55:39 +0800

    rm unused import

--- core/server/master/src/test/java/alluxio/master/file/RpcContextTest.java
+++ core/server/master/src/test/java/alluxio/master/file/RpcContextTest.java
@@ -12,7 +12,6 @@
 package alluxio.master.file;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doAnswer;
