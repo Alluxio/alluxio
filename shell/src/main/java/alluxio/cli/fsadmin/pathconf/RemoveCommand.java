946d7b0c1b60a136fdbb61d5eec558a0728dfe19
maobaolong <307499405@qq.com>
2022-11-17 15:38:26 +0800

    Update shell/src/main/java/alluxio/cli/fsadmin/pathconf/RemoveCommand.java
    Co-authored-by: Bowen Ding <6999708+dbw9580@users.noreply.github.com>

--- shell/src/main/java/alluxio/cli/fsadmin/pathconf/RemoveCommand.java
+++ shell/src/main/java/alluxio/cli/fsadmin/pathconf/RemoveCommand.java
@@ -111,7 +111,8 @@ public final class RemoveCommand extends AbstractFsAdminCommand {
   @Override
   public String getUsage() {
     return String.format("%s [-R/--recursive] [--%s <key1,key2,key3>] <path>%n"
-        + "\t--%s: %s" + "\t--%s: %s",
+        + "\t--%s: %s" + "%n"
+        + "\t--%s: %s",
         getCommandName(), KEYS_OPTION_NAME,
         KEYS_OPTION_NAME, KEYS_OPTION.getDescription(),
         RECURSIVE_OPTION.getOpt(), RECURSIVE_OPTION.getDescription());
