06c2bd9f459ffc36a44ea9d556af26ec1f93c1b6
Arthur Jenoudet <23088925+jenoudet@users.noreply.github.com>
2022-11-17 17:14:11 -0800

    removing extraenous code from MasterProcess

--- core/server/master/src/main/java/alluxio/master/AlluxioSimpleMasterProcess.java
+++ core/server/master/src/main/java/alluxio/master/AlluxioSimpleMasterProcess.java
@@ -11,14 +11,12 @@
 
 package alluxio.master;
 
-import alluxio.RuntimeConstants;
 import alluxio.conf.Configuration;
 import alluxio.conf.PropertyKey;
-import alluxio.grpc.GrpcServer;
-import alluxio.grpc.GrpcServerBuilder;
 import alluxio.grpc.JournalDomain;
 import alluxio.grpc.NodeState;
 import alluxio.master.journal.JournalSystem;
+import alluxio.master.service.RpcServerSimpleService;
 import alluxio.master.service.SimpleService;
 import alluxio.master.service.web.WebServerSimpleService;
 import alluxio.util.network.NetworkAddressUtils;
@@ -28,8 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.List;
 import javax.annotation.concurrent.NotThreadSafe;
 
 /**
@@ -38,17 +34,6 @@ import javax.annotation.concurrent.NotThreadSafe;
 @NotThreadSafe
 public abstract class AlluxioSimpleMasterProcess extends MasterProcess {
   private static final Logger LOG = LoggerFactory.getLogger(AlluxioSimpleMasterProcess.class);
-
-  /**
-   * @return the master that is running on this process
-   */
-  abstract List<AbstractMaster> getAbstractMasters();
-
-  /** The connection address for the rpc server. */
-  final InetSocketAddress mRpcConnectAddress;
-
-  /** The connection address for the web service. */
-  final InetSocketAddress mWebConnectAddress;
 
   /** The master name. */
   final String mMasterName;
@@ -73,10 +58,6 @@ public abstract class AlluxioSimpleMasterProcess extends MasterProcess {
     super(journalSystem, leaderSelector, webService, rpcService);
     mMasterName = masterName;
     mJournalDomain = journalDomain;
-    mWebConnectAddress = NetworkAddressUtils.getConnectAddress(webService,
-        Configuration.global());
-    mRpcConnectAddress = NetworkAddressUtils.getConnectAddress(rpcService,
-        Configuration.global());
     if (!Configuration.isSet(hostNameKey)) {
       Configuration.set(hostNameKey,
           NetworkAddressUtils.getLocalHostName(
@@ -91,64 +72,37 @@ public abstract class AlluxioSimpleMasterProcess extends MasterProcess {
       throw new RuntimeException(String.format("Failed to create %s master", mMasterName), e);
     }
     mServices.add(WebServerSimpleService.Factory.create(mWebBindAddress, this));
-    try {
-      stopServing();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @Override
-  public InetSocketAddress getWebAddress() {
-    synchronized (mWebServerLock) {
-      if (mWebServer != null) {
-        return new InetSocketAddress(mWebServer.getBindHost(), mWebServer.getLocalPort());
-      }
-      return mWebConnectAddress;
-    }
-  }
-
-  @Override
-  public InetSocketAddress getRpcAddress() {
-    return mRpcConnectAddress;
+    mServices.add(RpcServerSimpleService.Factory.create(mRpcBindAddress, this, mRegistry));
   }
 
   @Override
   public void start() throws Exception {
+    mServices.forEach(SimpleService::start);
     mJournalSystem.start();
     // the leader selector is created in state STANDBY. Once mLeaderSelector.start is called, it
     // can transition to PRIMARY at any point.
     mLeaderSelector.start(getRpcAddress());
-    mServices.forEach(SimpleService::start);
+    startMasters(false);
 
     while (!Thread.interrupted()) {
-      if (mServingThread == null) {
-        // We are in standby mode. Nothing to do until we become the primary.
-        mLeaderSelector.waitForState(NodeState.PRIMARY);
-        LOG.info("Transitioning from standby to primary");
-        mJournalSystem.gainPrimacy();
-        stopMaster();
-        LOG.info("Secondary stopped");
-        startMaster(true);
-        mServices.forEach(SimpleService::promote);
-        mServingThread = new Thread(() -> startServing(
-                " (gained leadership)", " (lost leadership)"), "MasterServingThread");
-        mServingThread.start();
-        LOG.info("Primary started");
-      } else {
-        // We are in primary mode. Nothing to do until we become the standby.
-        mLeaderSelector.waitForState(NodeState.STANDBY);
-        LOG.info("Transitioning from primary to standby");
-        stopServing();
-        mServices.forEach(SimpleService::demote);
-        mServingThread.join();
-        mServingThread = null;
-        stopMaster();
-        mJournalSystem.losePrimacy();
-        LOG.info("Primary stopped");
-        startMaster(false);
-        LOG.info("Standby started");
-      }
+      // We are in standby mode. Nothing to do until we become the primary.
+      mLeaderSelector.waitForState(NodeState.PRIMARY);
+      LOG.info("Transitioning from standby to primary");
+      mJournalSystem.gainPrimacy();
+      stopMasters();
+      LOG.info("Secondary stopped");
+      startMasters(true);
+      mServices.forEach(SimpleService::promote);
+      LOG.info("Primary started");
+      // We are in primary mode. Nothing to do until we become the standby.
+      mLeaderSelector.waitForState(NodeState.STANDBY);
+      LOG.info("Transitioning from primary to standby");
+      mServices.forEach(SimpleService::demote);
+      stopMasters();
+      mJournalSystem.losePrimacy();
+      LOG.info("Primary stopped");
+      startMasters(false);
+      LOG.info("Standby started");
     }
   }
 
@@ -160,100 +114,26 @@ public abstract class AlluxioSimpleMasterProcess extends MasterProcess {
   @Override
   public void stop() throws Exception {
     mServices.forEach(SimpleService::stop);
-    stopRejectingServers();
-    if (isGrpcServing()) {
-      stopServing();
-    }
     mJournalSystem.stop();
-    stopMaster();
+    stopMasters();
     mLeaderSelector.stop();
   }
 
-  protected void startMaster(boolean isLeader) {
+  protected void startMasters(boolean isLeader) {
     try {
-      if (!isLeader) {
-        startRejectingServers();
-      }
-      for (AbstractMaster master : getAbstractMasters()) {
-        master.start(isLeader);
-      }
+      mRegistry.start(isLeader);
     } catch (IOException e) {
       LOG.error(e.getMessage(), e);
       throw new RuntimeException(e.getMessage(), e);
     }
   }
 
-  protected void stopMaster() {
+  protected void stopMasters() {
     try {
-      for (AbstractMaster master : getAbstractMasters()) {
-        master.stop();
-      }
+      mRegistry.stop();
     } catch (IOException e) {
       LOG.error("Failed to stop {} master", mMasterName, e);
       throw new RuntimeException(String.format("Failed to stop %s master", mMasterName), e);
-    }
-  }
-
-  protected void startServing(String startMessage, String stopMessage) {
-    LOG.info("Alluxio {} master web server version {} starting{}. webAddress={}",
-        mMasterName, RuntimeConstants.VERSION, startMessage, mWebBindAddress);
-    startServingRPCServer();
-    LOG.info(
-        "Alluxio {} master version {} started{}. bindAddress={}, connectAddress={}, webAddress={}",
-        mMasterName, RuntimeConstants.VERSION, startMessage, mRpcBindAddress,
-        mRpcConnectAddress, mWebBindAddress);
-    mGrpcServer.awaitTermination();
-    LOG.info("Alluxio {} master ended {}", mMasterName, stopMessage);
-  }
-
-  /**
-   * Starts the gRPC server. The AlluxioMaster registers the Services of registered
-   * {@link Master}s and meta services.
-   */
-  protected void startServingRPCServer() {
-    stopRejectingRpcServer();
-    try {
-      synchronized (mGrpcServerLock) {
-        LOG.info("Starting gRPC server on address:{}", mRpcBindAddress);
-        mGrpcServer = createRPCServer();
-        // Start serving.
-        mGrpcServer.start();
-        // Acquire and log bind port from newly started server.
-        InetSocketAddress listeningAddress = InetSocketAddress
-            .createUnresolved(mRpcBindAddress.getHostName(), mGrpcServer.getBindPort());
-        LOG.info("gRPC server listening on: {}", listeningAddress);
-      }
-    } catch (IOException e) {
-      LOG.error("gRPC serving failed.", e);
-      throw new RuntimeException("gRPC serving failed");
-    }
-  }
-
-  abstract GrpcServerBuilder createBaseRPCServer();
-
-  private GrpcServer createRPCServer() {
-    // Create underlying gRPC server.
-    GrpcServerBuilder builder = createBaseRPCServer();
-    // Register master services.
-    getAbstractMasters().forEach(master -> {
-      master.getServices().forEach((type, service) -> {
-        builder.addService(type, service);
-        LOG.info("registered service {}", type.name());
-      });
-    });
-    // Builds a server that is not started yet.
-    return builder.build();
-  }
-
-  protected void stopServing() {
-    synchronized (mGrpcServerLock) {
-      if (mGrpcServer != null && mGrpcServer.isServing()) {
-        LOG.info("Stopping Alluxio {} master RPC server on {} @ {}",
-            mMasterName, this, mRpcBindAddress);
-        if (!mGrpcServer.shutdown()) {
-          LOG.warn("Alluxio {} master RPC server shutdown timed out.", mMasterName);
-        }
-      }
     }
   }
 
