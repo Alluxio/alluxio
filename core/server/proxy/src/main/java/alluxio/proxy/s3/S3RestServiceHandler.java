4b83faac2eec0dbcce7b1041a1c19c79783b2c1a
Haoning Sun <msunhaoning@163.com>
2022-11-19 18:21:32 +0800

    Add Content-Range header for getObject
    ### What changes are proposed in this pull request?
    
    Add Content-Range header for getObject.
    
    ### Why are the changes needed?
    Fix #16331.
    
    ### Does this PR introduce any user facing changes?
    No.
    
    pr-link: Alluxio/alluxio#16332
    change-id: cid-bd970f0c4d1a3b307f4187668162bfb215c6f1f7

--- core/server/proxy/src/main/java/alluxio/proxy/s3/S3RestServiceHandler.java
+++ core/server/proxy/src/main/java/alluxio/proxy/s3/S3RestServiceHandler.java
@@ -1247,6 +1247,14 @@ public final class S3RestServiceHandler {
               .lastModified(new Date(status.getLastModificationTimeMs()))
               .header(S3Constants.S3_CONTENT_LENGTH_HEADER, s3Range.getLength(status.getLength()));
 
+          // Check range
+          if (s3Range.isValid()) {
+            res.status(Response.Status.PARTIAL_CONTENT)
+                .header(S3Constants.S3_ACCEPT_RANGES_HEADER, S3Constants.S3_ACCEPT_RANGES_VALUE)
+                .header(S3Constants.S3_CONTENT_RANGE_HEADER,
+                    s3Range.getRealRange(status.getLength()));
+          }
+
           // Check for the object's ETag
           String entityTag = S3RestUtils.getEntityTag(status);
           if (entityTag != null) {
