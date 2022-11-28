f637ed5084a373817a5a1bf990900eef4638e353
maobaolong <307499405@qq.com>
2022-11-28 14:38:04 +0800

    Improve the DefaultTableMaster logic

--- table/server/master/src/main/java/alluxio/master/table/DefaultTableMaster.java
+++ table/server/master/src/main/java/alluxio/master/table/DefaultTableMaster.java
@@ -24,9 +24,8 @@ import alluxio.grpc.table.Constraint;
 import alluxio.grpc.table.Database;
 import alluxio.grpc.table.Partition;
 import alluxio.grpc.table.SyncStatus;
-import alluxio.master.CoreMaster;
-import alluxio.master.CoreMasterContext;
-import alluxio.master.file.FileSystemMaster;
+import alluxio.master.AbstractMaster;
+import alluxio.master.MasterContext;
 import alluxio.master.journal.DelegatingJournaled;
 import alluxio.master.journal.JournalContext;
 import alluxio.master.journal.Journaled;
@@ -52,10 +51,10 @@ import java.util.Set;
 /**
  * This table master manages catalogs metadata information.
  */
-public class DefaultTableMaster extends CoreMaster
+public class DefaultTableMaster extends AbstractMaster
     implements TableMaster, DelegatingJournaled {
   private static final Logger LOG = LoggerFactory.getLogger(DefaultTableMaster.class);
-  private static final Set<Class<? extends Server>> DEPS = ImmutableSet.of(FileSystemMaster.class);
+  private static final Set<Class<? extends Server>> DEPS = ImmutableSet.of();
   public static final String DEFAULT_TRANSFORMATION = "file.count.max=100";
 
   private final AlluxioCatalog mCatalog;
@@ -68,7 +67,7 @@ public class DefaultTableMaster extends CoreMaster
    * @param context core master context
    * @param jobMasterClient the job master client for transformation
    */
-  public DefaultTableMaster(CoreMasterContext context, JobMasterClient jobMasterClient) {
+  public DefaultTableMaster(MasterContext context, JobMasterClient jobMasterClient) {
     super(context, new SystemClock(),
         ExecutorServiceFactories.cachedThreadPool(Constants.TABLE_MASTER_NAME));
     mCatalog = new AlluxioCatalog();
