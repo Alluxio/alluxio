beb1ed282a10f0aee48484ae13c5444599c6091d
lucyge2022 <111789461+lucyge2022@users.noreply.github.com>
2022-11-30 15:41:47 -0800

    cp without resolving conflicts

--- core/client/fs/src/main/java/alluxio/client/block/stream/UnderFileSystemFileOutStream.java
+++ core/client/fs/src/main/java/alluxio/client/block/stream/UnderFileSystemFileOutStream.java
@@ -14,7 +14,11 @@ package alluxio.client.block.stream;
 import alluxio.client.file.FileSystemContext;
 import alluxio.client.file.options.OutStreamOptions;
 import alluxio.grpc.RequestType;
+import alluxio.metrics.MetricKey;
+import alluxio.metrics.MetricsSystem;
 import alluxio.wire.WorkerNetAddress;
+
+import com.codahale.metrics.Timer;
 
 import java.io.IOException;
 import javax.annotation.concurrent.NotThreadSafe;
@@ -48,5 +52,13 @@ public class UnderFileSystemFileOutStream extends BlockOutStream {
    */
   protected UnderFileSystemFileOutStream(DataWriter dataWriter, WorkerNetAddress address) {
     super(dataWriter, Long.MAX_VALUE, address);
+  }
+
+  @Override
+  public void close() throws IOException {
+    try (Timer.Context ctx = MetricsSystem
+            .uniformTimer(MetricKey.CLOSE_UFS_OUTSTREAM_LATENCY.getName()).time()) {
+      super.close();
+    }
   }
 }
