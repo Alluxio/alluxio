065ad9281914d0b7d6e50ff8e8c3a67f63e2d93b
LuQQiu <luqiujob@gmail.com>
2022-12-07 11:57:04 -0600

    Support FUSE UFS seek
    ### What changes are proposed in this pull request?
    
    Support seek in S3AInputStream. Support Seek in UfsFileInStream.
    
    ### Why are the changes needed?
    
    Fuse is seek() heavy. Every read will first seek() and then read().
    Fuse.seek() can seek to any position and is a real random read.
    
    ### Does this PR introduce any user facing changes?
    
    Supports FUSE UFS random read operations. e.g. FIO random read
    
    pr-link: Alluxio/alluxio#16585
    change-id: cid-ed698f5c1ae361dcfc7e69fe20f65cff19c28f13

--- underfs/s3a/src/main/java/alluxio/underfs/s3a/S3AUnderFileSystem.java
+++ underfs/s3a/src/main/java/alluxio/underfs/s3a/S3AUnderFileSystem.java
@@ -678,6 +678,11 @@ public class S3AUnderFileSystem extends ObjectUnderFileSystem {
   @Override
   protected InputStream openObject(String key, OpenOptions options,
       RetryPolicy retryPolicy) {
-    return new S3AInputStream(mBucketName, key, mClient, options.getOffset(), retryPolicy);
+    return S3AInputStream.create(mBucketName, key, mClient, options.getOffset(), retryPolicy);
+  }
+
+  @Override
+  public boolean isSeekable() {
+    return true;
   }
 }
