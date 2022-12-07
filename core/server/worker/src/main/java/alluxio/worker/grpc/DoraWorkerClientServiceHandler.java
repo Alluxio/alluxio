14ae025c4fec82df05529e1e0392a1e33b205d5d
JiamingMai <jiamingmai@163.com>
2022-12-07 12:01:29 +0800

    add get file status method for dora

--- core/server/worker/src/main/java/alluxio/worker/grpc/DoraWorkerClientServiceHandler.java
+++ core/server/worker/src/main/java/alluxio/worker/grpc/DoraWorkerClientServiceHandler.java
@@ -15,9 +15,18 @@ import alluxio.annotation.SuppressFBWarnings;
 import alluxio.conf.Configuration;
 import alluxio.conf.PropertyKey;
 import alluxio.grpc.BlockWorkerGrpc;
+import alluxio.grpc.FileInfo;
+import alluxio.grpc.GetStatusPRequest;
+import alluxio.grpc.GetStatusPResponse;
+import alluxio.grpc.PAcl;
 import alluxio.grpc.ReadRequest;
 import alluxio.grpc.ReadResponse;
 import alluxio.grpc.ReadResponseMarshaller;
+import alluxio.grpc.TtlAction;
+import alluxio.underfs.UfsFileStatus;
+import alluxio.underfs.UfsManager;
+import alluxio.underfs.UnderFileSystem;
+import alluxio.underfs.UnderFileSystemConfiguration;
 import alluxio.worker.DoraWorker;
 import alluxio.worker.Worker;
 import alluxio.worker.WorkerProcess;
@@ -26,7 +35,10 @@ import com.google.common.collect.ImmutableMap;
 import io.grpc.MethodDescriptor;
 import io.grpc.stub.CallStreamObserver;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
@@ -35,6 +47,8 @@ import java.util.Map;
  */
 @SuppressFBWarnings("BC_UNCONFIRMED_CAST")
 public class DoraWorkerClientServiceHandler extends BlockWorkerGrpc.BlockWorkerImplBase {
+
+  private static final Logger LOG = LoggerFactory.getLogger(DoraWorkerClientServiceHandler.class);
 
   private static final boolean ZERO_COPY_ENABLED =
       Configuration.getBoolean(PropertyKey.WORKER_NETWORK_ZEROCOPY_ENABLED);
@@ -77,5 +91,42 @@ public class DoraWorkerClientServiceHandler extends BlockWorkerGrpc.BlockWorkerI
         mWorker, callStreamObserver);
     callStreamObserver.setOnReadyHandler(readHandler::onReady);
     return readHandler;
+  }
+
+  @Override
+  public void getStatus(GetStatusPRequest request,
+                        StreamObserver<GetStatusPResponse> responseObserver) {
+    try {
+      String ufsFilePath = request.getPath();
+      UnderFileSystem ufs = UnderFileSystem.Factory.create(request.getPath(),
+          UnderFileSystemConfiguration.defaults(Configuration.global()));
+      UfsFileStatus status = ufs.getFileStatus(request.getPath());
+      if (status == null) {
+        LOG.info(String.format("Unable to get status for under file system path %s. ",
+            ufsFilePath));
+        throw new IOException(String.format("Unable to get status for under file system path %s. ",
+            ufsFilePath));
+      }
+      GetStatusPResponse mResponse = GetStatusPResponse.newBuilder()
+          .setFileInfo(
+              FileInfo.newBuilder()
+                  .setName(status.getName())
+                  .setPath(ufsFilePath)
+                  .setUfsPath(ufsFilePath)
+                  .setLength(status.getContentLength())
+                  .setBlockSizeBytes(status.getBlockSize())
+                  .setMode(status.getMode())
+                  .setFolder(status.isDirectory())
+                  .setLastModificationTimeMs(status.getLastModifiedTime())
+                  .setOwner(status.getOwner())
+                  .setGroup(status.getGroup())
+                  .build()
+          ).build();
+      responseObserver.onNext(mResponse);
+      responseObserver.onCompleted();
+    } catch (IOException e) {
+      LOG.error(String.format("Failed to get status of %s: ", request.getPath()), e);
+      responseObserver.onError(e);
+    }
   }
 }
