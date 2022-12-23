ce65d54da1871234e58df6d742748f5dc4ce10cf
maobaolong <307499405@qq.com>
2022-12-23 09:25:28 +0800

    Fix style issue

--- core/server/master/src/main/java/alluxio/master/file/meta/InodeTreePersistentState.java
+++ core/server/master/src/main/java/alluxio/master/file/meta/InodeTreePersistentState.java
@@ -130,8 +130,7 @@ public class InodeTreePersistentState implements Journaled {
             return root.get();
           }
           throw new ConcurrentException("root was not initialized.", new RuntimeException());
-        }
-  };
+        }};
 
   /**
    * @param inodeStore file store which holds inode metadata
