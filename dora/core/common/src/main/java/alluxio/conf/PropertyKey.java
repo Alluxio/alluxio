f5ef9b089290152a95fd5c5a5e6efefe71b06851
elega <445092967@qq.com>
2023-01-04 06:21:44 +0800

    Add grpc reflection
    ### What changes are proposed in this pull request?
    
    Add grpc reflection to make the grpc tools such as grpcui and grpcurl
    send request to alluxio server easier.
    
    ### Why are the changes needed?
    
    
    ![image](https://user-images.githubusercontent.com/6771554/209753002-027c3bcc-88b1-4bf5-830a-f7abb1447c9c.png)
    
    ### Does this PR introduce any user facing changes?
    
    N/A
    
    pr-link: Alluxio/alluxio#16679
    change-id: cid-6d2d3e021036a22213fcb3358caa571bb87ca054

--- dora/core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ dora/core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -596,6 +596,17 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .setScope(Scope.SERVER)
           .setIsHidden(true)
           .build();
+  public static final PropertyKey GRPC_REFLECTION_ENABLED =
+      booleanBuilder(Name.GRPC_REFLECTION_ENABLED)
+          .setDefaultValue(false)
+          .setDescription("If true, grpc reflection will be enabled on alluxio grpc servers, "
+              + "including masters, workers, job masters and job workers. "
+              + " This makes grpc tools such as grpcurl or grpcui can send grpc requests to "
+              + "the master server easier without knowing the protobufs. "
+              + "This is a debug option.")
+          .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
+          .setScope(Scope.ALL)
+          .build();
   public static final PropertyKey HOME =
       stringBuilder(Name.HOME)
           .setDefaultValue("/opt/alluxio")
@@ -7589,6 +7600,8 @@ public final class PropertyKey implements Comparable<PropertyKey> {
     public static final String CONF_VALIDATION_ENABLED = "alluxio.conf.validation.enabled";
     public static final String DEBUG = "alluxio.debug";
     public static final String EXTENSIONS_DIR = "alluxio.extensions.dir";
+    public static final String GRPC_REFLECTION_ENABLED =
+        "alluxio.grpc.reflection.enabled";
     public static final String HOME = "alluxio.home";
     public static final String LEAK_DETECTOR_LEVEL = "alluxio.leak.detector.level";
     public static final String LEAK_DETECTOR_EXIT_ON_LEAK = "alluxio.leak.detector.exit.on.leak";
