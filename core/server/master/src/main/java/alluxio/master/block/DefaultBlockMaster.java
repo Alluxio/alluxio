501bd3583dab4a8dc7771b11b7a510cae685d778
jiacheliu3 <jiacheliu3@gmail.com>
2023-01-03 10:44:46 +0800

    add more comments

--- core/server/master/src/main/java/alluxio/master/block/DefaultBlockMaster.java
+++ core/server/master/src/main/java/alluxio/master/block/DefaultBlockMaster.java
@@ -1575,6 +1575,8 @@ public class DefaultBlockMaster extends CoreMaster implements BlockMaster {
           }
         }
       }
+      // If it has been too long before a worker can successfully register,
+      // forget this worker because it is probably gone
       for (MasterWorkerInfo worker : mTempWorkers) {
         try (LockResource r = worker.lockWorkerMeta(
             EnumSet.of(WorkerMetaLockSection.BLOCKS), false)) {
