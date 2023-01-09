f4f42498b1b550e7e691350d6ebb7fedca0da9c7
voddle <dengxinyu0105@gmail.com>
2023-01-09 18:09:43 +0800

    still wokring on UT

--- core/server/worker/src/test/java/alluxio/worker/page/PagedBlockStoreCommitBlockTest.java
+++ core/server/worker/src/test/java/alluxio/worker/page/PagedBlockStoreCommitBlockTest.java
@@ -1,12 +1,18 @@
 package alluxio.worker.page;
 
 import alluxio.AlluxioTestDirectory;
+import alluxio.Constants;
 import alluxio.client.file.cache.CacheManager;
 import alluxio.client.file.cache.CacheManagerOptions;
 import alluxio.client.file.cache.PageId;
+import alluxio.client.file.cache.evictor.CacheEvictorOptions;
+import alluxio.client.file.cache.evictor.FIFOCacheEvictor;
 import alluxio.client.file.cache.store.PageStoreDir;
+import alluxio.client.file.cache.store.PageStoreOptions;
+import alluxio.client.file.cache.store.PageStoreType;
 import alluxio.conf.AlluxioConfiguration;
 import alluxio.conf.Configuration;
+import alluxio.conf.InstancedConfiguration;
 import alluxio.conf.PropertyKey;
 import alluxio.master.NoopUfsManager;
 import alluxio.underfs.UfsManager;
@@ -15,11 +21,17 @@ import alluxio.worker.block.AbstractBlockStoreEventListener;
 import alluxio.worker.block.BlockMasterClientPool;
 import alluxio.worker.block.BlockStoreEventListener;
 import alluxio.worker.block.BlockStoreLocation;
+import com.google.common.collect.ImmutableList;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
+
+import java.nio.file.Path;
 
 import static org.junit.Assert.assertEquals;
 
@@ -33,24 +45,48 @@ public class PagedBlockStoreCommitBlockTest {
     BlockMasterClientPool blockMasterClientPool;
     AtomicReference<Long> workerId;
 
+    private static final int DIR_INDEX = 0;
+    private static final long blockId = 2L;
+
+    private PagedBlockStoreDir mDir;
+
+    @Rule
+    public TemporaryFolder mTempFolder = new TemporaryFolder();
+
     @Before
     public void setup() throws Exception {
         CacheManager cacheManager;
         List<PageStoreDir> pageStoreDirs;
+
+        Path mDirPath = mTempFolder.newFolder().toPath();
+        System.out.println(mDirPath);
+        InstancedConfiguration dirConf = Configuration.modifiableGlobal();
+        dirConf.set(PropertyKey.WORKER_PAGE_STORE_DIRS, ImmutableList.of(mDirPath));
+        dirConf.set(PropertyKey.WORKER_PAGE_STORE_SIZES, ImmutableList.of(Constants.MB));
+        dirConf.set(PropertyKey.WORKER_PAGE_STORE_TYPE, PageStoreType.LOCAL);
+        PageStoreDir pageStoreDir =
+                PageStoreDir.createPageStoreDir(
+                        new CacheEvictorOptions().setEvictorClass(FIFOCacheEvictor.class),
+                        PageStoreOptions.createForWorkerPageStore(dirConf).get(DIR_INDEX));
+        mDir = new PagedBlockStoreDir(pageStoreDir, DIR_INDEX);
+
         try{
             ufs = new NoopUfsManager();
             conf = Configuration.global();
             blockMasterClientPool = new BlockMasterClientPool();
             workerId = new AtomicReference<>(-1L);
-            pageStoreDirs = PageStoreDir.createPageStoreDirs(cacheManagerOptions);
+            cacheManagerOptions = CacheManagerOptions.createForWorker(conf);
+            // pageStoreDirs = PageStoreDir.createPageStoreDirs(cacheManagerOptions);
+            pageStoreDirs = new ArrayList<PageStoreDir>();
+            pageStoreDirs.add(pageStoreDir);
             dirs = PagedBlockStoreDir.fromPageStoreDirs(pageStoreDirs);
             pageMetaStore = new PagedBlockMetaStore(dirs);
             cacheManager = CacheManager.Factory.create(conf, cacheManagerOptions, pageMetaStore);
-            cacheManagerOptions = CacheManagerOptions.createForWorker(conf);
             pagedBlockStore = new PagedBlockStore(cacheManager, ufs, blockMasterClientPool, workerId, pageMetaStore, cacheManagerOptions.getPageSize());
         } catch (Exception e) {
-
+            System.out.println(e);
         }
+        System.out.println("Setup finished");
     }
 
     @Test
@@ -68,8 +104,14 @@ public class PagedBlockStoreCommitBlockTest {
                 assertEquals(dirs.get(0).getLocation(), location);
             }
         };
+        PagedBlockStoreDir dir =
+                (PagedBlockStoreDir) pageMetaStore.allocate(BlockPageId.tempFileIdOf(blockId), 0);
+
+        dir.putTempFile(BlockPageId.tempFileIdOf(blockId));
+        PagedTempBlockMeta blockMeta = new PagedTempBlockMeta(blockId, dir);
+        pageMetaStore.addTempBlock(blockMeta);
 
         pagedBlockStore.registerBlockStoreEventListener(listener);
-        pagedBlockStore.commitBlock(1, 1, true);
+        pagedBlockStore.commitBlock(1L, 2L, false);
     }
 }
