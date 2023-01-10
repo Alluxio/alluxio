ce0acbf6674e7b172074f845da0eecdcd2b32324
voddle <dengxinyu0105@gmail.com>
2023-01-10 16:10:31 +0800

    mock commitToMaster now

--- core/server/worker/src/test/java/alluxio/worker/page/PagedBlockStoreCommitBlockTest.java
+++ core/server/worker/src/test/java/alluxio/worker/page/PagedBlockStoreCommitBlockTest.java
@@ -1,10 +1,10 @@
 package alluxio.worker.page;
 
-import alluxio.AlluxioTestDirectory;
+
+import static org.mockito.Mockito.*;
 import alluxio.Constants;
 import alluxio.client.file.cache.CacheManager;
 import alluxio.client.file.cache.CacheManagerOptions;
-import alluxio.client.file.cache.PageId;
 import alluxio.client.file.cache.evictor.CacheEvictorOptions;
 import alluxio.client.file.cache.evictor.FIFOCacheEvictor;
 import alluxio.client.file.cache.store.PageStoreDir;
@@ -14,20 +14,25 @@ import alluxio.conf.AlluxioConfiguration;
 import alluxio.conf.Configuration;
 import alluxio.conf.InstancedConfiguration;
 import alluxio.conf.PropertyKey;
+import alluxio.exception.ExceptionMessage;
+import alluxio.exception.runtime.AlluxioRuntimeException;
+import alluxio.grpc.ErrorType;
 import alluxio.master.NoopUfsManager;
 import alluxio.underfs.UfsManager;
 //import alluxio.worker.BlockStoreBase;
-import alluxio.worker.block.AbstractBlockStoreEventListener;
-import alluxio.worker.block.BlockMasterClientPool;
-import alluxio.worker.block.BlockStoreEventListener;
-import alluxio.worker.block.BlockStoreLocation;
+import alluxio.worker.block.*;
+import alluxio.worker.block.io.BlockWriter;
 import com.google.common.collect.ImmutableList;
+import io.grpc.Status;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -45,8 +50,14 @@ public class PagedBlockStoreCommitBlockTest {
     BlockMasterClientPool blockMasterClientPool;
     AtomicReference<Long> workerId;
 
+    Boolean mCommitMaster = true;
+
     private static final int DIR_INDEX = 0;
     private static final long blockId = 2L;
+
+    public int mPageSize = 2;
+
+    private static final int offset = 0;
 
     private PagedBlockStoreDir mDir;
 
@@ -57,9 +68,9 @@ public class PagedBlockStoreCommitBlockTest {
     public void setup() throws Exception {
         CacheManager cacheManager;
         List<PageStoreDir> pageStoreDirs;
+        InstancedConfiguration mConf = Configuration.copyGlobal();
 
         Path mDirPath = mTempFolder.newFolder().toPath();
-        System.out.println(mDirPath);
         InstancedConfiguration dirConf = Configuration.modifiableGlobal();
         dirConf.set(PropertyKey.WORKER_PAGE_STORE_DIRS, ImmutableList.of(mDirPath));
         dirConf.set(PropertyKey.WORKER_PAGE_STORE_SIZES, ImmutableList.of(Constants.MB));
@@ -73,20 +84,41 @@ public class PagedBlockStoreCommitBlockTest {
         try{
             ufs = new NoopUfsManager();
             conf = Configuration.global();
-            blockMasterClientPool = new BlockMasterClientPool();
+            mConf.set(PropertyKey.WORKER_PAGE_STORE_PAGE_SIZE, mPageSize);
+            mConf.set(PropertyKey.WORKER_PAGE_STORE_DIRS, ImmutableList.of(mDirPath));
+            blockMasterClientPool = mock(BlockMasterClientPool.class);
+            BlockMasterClient mockedBlockMasterClient = mock(BlockMasterClient.class);
+            when(blockMasterClientPool.acquire()).thenReturn(mockedBlockMasterClient);
+            doNothing().when(blockMasterClientPool).release(any());
+            doAnswer((i) -> {
+                // when testing blockId == 3L, commit to Master should fail, otherwise it will do nothing and success.
+                if ((Long)i.getArgument(4) == 3L) {
+                    throw new AlluxioRuntimeException(Status.UNAVAILABLE, ExceptionMessage.FAILED_COMMIT_BLOCK_TO_MASTER.getMessage((Long)i.getArgument(5)), new IOException(), ErrorType.Internal, false);
+                }
+                return null;
+            }).when(mockedBlockMasterClient).commitBlock(anyLong(), anyLong(), anyString(), anyString(), anyLong(), anyLong());
             workerId = new AtomicReference<>(-1L);
-            cacheManagerOptions = CacheManagerOptions.createForWorker(conf);
+            cacheManagerOptions = CacheManagerOptions.createForWorker(mConf);
             // pageStoreDirs = PageStoreDir.createPageStoreDirs(cacheManagerOptions);
             pageStoreDirs = new ArrayList<PageStoreDir>();
             pageStoreDirs.add(pageStoreDir);
             dirs = PagedBlockStoreDir.fromPageStoreDirs(pageStoreDirs);
             pageMetaStore = new PagedBlockMetaStore(dirs);
             cacheManager = CacheManager.Factory.create(conf, cacheManagerOptions, pageMetaStore);
-            pagedBlockStore = new PagedBlockStore(cacheManager, ufs, blockMasterClientPool, workerId, pageMetaStore, cacheManagerOptions.getPageSize());
+            // pagedBlockStore = new PagedBlockStore(cacheManager, ufs, blockMasterClientPool, workerId, pageMetaStore, cacheManagerOptions// .getPageSize()) {
+            //     @Override
+            //     public void commitBlockToMaster(PagedBlockMeta blockMeta) {
+            //         if (mCommitMaster) {
+            //             // do nothing, 'onCommitToMaster will tell'
+            //         } else {
+            //             throw new AlluxioRuntimeException(Status.UNAVAILABLE, ExceptionMessage.FAILED_COMMIT_BLOCK_TO_MASTER.getMessage// (blockId), new IOException(), ErrorType.Internal, false);
+            //         }
+            //     }
+            // };
+            pagedBlockStore = new PagedBlockStore(cacheManager, ufs, blockMasterClientPool, workerId, pageMetaStore, cacheManagerOptions .getPageSize());
         } catch (Exception e) {
             System.out.println(e);
         }
-        System.out.println("Setup finished");
     }
 
     @Test
@@ -94,24 +126,34 @@ public class PagedBlockStoreCommitBlockTest {
         BlockStoreEventListener listener = new AbstractBlockStoreEventListener() {
             @Override
             public void onCommitBlockToLocal(long blockId, BlockStoreLocation location) {
-                assertEquals(1, blockId);
-                assertEquals(dirs.get(0).getLocation(), location);
+                assertEquals(2L, blockId);
+                // assertEquals(dirs.get(0).getLocation(), location);
             }
 
             @Override
             public void onCommitBlockToMaster(long blockId, BlockStoreLocation location) {
-                assertEquals(1, blockId);
-                assertEquals(dirs.get(0).getLocation(), location);
+                assertEquals(2L, blockId);
+                // assertEquals(dirs.get(0).getLocation(), location);
             }
         };
+        System.out.println("finding null pointer " + pagedBlockStore);
         PagedBlockStoreDir dir =
-                (PagedBlockStoreDir) pageMetaStore.allocate(BlockPageId.tempFileIdOf(blockId), 0);
+                (PagedBlockStoreDir) pageMetaStore.allocate(BlockPageId.tempFileIdOf(blockId), 1);
 
         dir.putTempFile(BlockPageId.tempFileIdOf(blockId));
         PagedTempBlockMeta blockMeta = new PagedTempBlockMeta(blockId, dir);
-        pageMetaStore.addTempBlock(blockMeta);
+        // pageMetaStore.addTempBlock(blockMeta);
+        pagedBlockStore.createBlock(1L, blockId, offset, new CreateBlockOptions(null, null, 64));
+        byte[] data = new byte[64];
+        Arrays.fill(data, (byte) 1);
+        ByteBuffer buf = ByteBuffer.wrap(data);
+        try (BlockWriter writer = pagedBlockStore.createBlockWriter(1L, blockId)) {
+            writer.append(buf);
+        } catch (Exception e) {
+            System.out.println("writer failed");
+        }
 
         pagedBlockStore.registerBlockStoreEventListener(listener);
-        pagedBlockStore.commitBlock(1L, 2L, false);
+        pagedBlockStore.commitBlock(1L, blockId, false);
     }
 }
