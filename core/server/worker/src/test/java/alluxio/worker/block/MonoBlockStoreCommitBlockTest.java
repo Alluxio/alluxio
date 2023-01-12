c9146214bf9aa574b38b3fff016fcaa57116f93f
voddle <dengxinyu0105@gmail.com>
2023-01-12 16:57:48 +0800

    implement bowen's comments on MonoTest

--- core/server/worker/src/test/java/alluxio/worker/block/MonoBlockStoreCommitBlockTest.java
+++ core/server/worker/src/test/java/alluxio/worker/block/MonoBlockStoreCommitBlockTest.java
@@ -3,13 +3,17 @@ package alluxio.worker.block;
 import alluxio.exception.status.AlluxioStatusException;
 import alluxio.underfs.UfsManager;
 import alluxio.worker.block.io.BlockWriter;
+import alluxio.worker.block.meta.StorageDir;
 import alluxio.worker.block.meta.TempBlockMeta;
 import io.grpc.Status;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.mockito.Spy;
 
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
@@ -30,11 +34,15 @@ public class MonoBlockStoreCommitBlockTest {
     BlockMasterClient mockedBlockMasterClient;
     BlockMetadataManager mBlockMetadataManager;
     BlockLockManager mBlockLockManager;
-    @Spy
     TieredBlockStore mTieredBlockStore;
+    private static final String FIRST_TIER_ALIAS = TieredBlockStoreTestUtils.TIER_ALIAS[0];
+    private StorageDir mTestDir1;
+    /** Rule to create a new temporary folder during each test. */
+    @Rule
+    public TemporaryFolder mTestFolder = new TemporaryFolder();
 
-    Long sessoinId = 1L;
-    Long blockId = 2L;
+    final Long sessionId = 1L;
+    final Long blockId = 2L;
     int FIRST_TIER = 1;
     // Maybe location should be asserted as well.
     BlockStoreEventListener listener0 = new AbstractBlockStoreEventListener() {
@@ -48,49 +56,33 @@ public class MonoBlockStoreCommitBlockTest {
             assertEquals(2L, blockId);
         }
     };
-    BlockStoreEventListener listener = spy(listener0);
+    BlockStoreEventListener mListener = spy(listener0);
 
     @Before
-    public void setup() {
+    public void setup() throws Exception {
+        File tempFolder = mTestFolder.newFolder();
+        TieredBlockStoreTestUtils.setupDefaultConf(tempFolder.getAbsolutePath());
+
         mockedBlockMasterClientPool = mock(BlockMasterClientPool.class);
         mockedBlockMasterClient = mock(BlockMasterClient.class);
         when(mockedBlockMasterClientPool.acquire()).thenReturn(mockedBlockMasterClient);
         doNothing().when(mockedBlockMasterClientPool).release(any());
         mBlockLockManager = new BlockLockManager();
         mBlockMetadataManager = BlockMetadataManager.createBlockMetadataManager();
-    }
 
-    @After
-    public void tearDown() {
-        try {
-            mMonoBlockStore.removeBlock(sessoinId, blockId);
-        } catch (Exception e) {
-
-        }
+        mTestDir1 = mBlockMetadataManager.getTier(FIRST_TIER_ALIAS).getDir(0);
     }
 
     @Test
     public void CommitLocalandCommitMasterBothSuccess() {
         mTieredBlockStore = new TieredBlockStore(mBlockMetadataManager, mBlockLockManager);
 
-        mMonoBlockStore = new MonoBlockStore(mTieredBlockStore, mockedBlockMasterClientPool, mock(UfsManager.class), new AtomicReference<>(1L));
+        prepareBlockStore();
 
-        mMonoBlockStore.createBlock(sessoinId, blockId, FIRST_TIER, new CreateBlockOptions(null, null, 64));
-        byte[] data = new byte[64];
-        Arrays.fill(data, (byte) 1);
-        ByteBuffer buf = ByteBuffer.wrap(data);
-        try (BlockWriter writer = mMonoBlockStore.createBlockWriter(1L, blockId)) {
-            // LocalCacheManager line 107 🤔
-            Thread.sleep(1000);
-            writer.append(buf);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        mMonoBlockStore.registerBlockStoreEventListener(listener);
-        mMonoBlockStore.commitBlock(sessoinId, blockId, false);
+        mMonoBlockStore.commitBlock(sessionId, blockId, false);
 
-        verify(listener).onCommitBlockToLocal(anyLong(), any(BlockStoreLocation.class));
-        verify(listener).onCommitBlockToMaster(anyLong(), any(BlockStoreLocation.class));
+        verify(mListener).onCommitBlockToLocal(anyLong(), any(BlockStoreLocation.class));
+        verify(mListener).onCommitBlockToMaster(anyLong(), any(BlockStoreLocation.class));
     }
 
     @Test
@@ -104,26 +96,14 @@ public class MonoBlockStoreCommitBlockTest {
         }
         mTieredBlockStore = new TieredBlockStore(mBlockMetadataManager, mBlockLockManager);
 
-        mMonoBlockStore = new MonoBlockStore(mTieredBlockStore, mockedBlockMasterClientPool, mock(UfsManager.class), new AtomicReference<>(1L));
+        prepareBlockStore();
 
-        mMonoBlockStore.createBlock(sessoinId, blockId, FIRST_TIER, new CreateBlockOptions(null, null, 64));
-        byte[] data = new byte[64];
-        Arrays.fill(data, (byte) 1);
-        ByteBuffer buf = ByteBuffer.wrap(data);
-        try (BlockWriter writer = mMonoBlockStore.createBlockWriter(1L, blockId)) {
-            // LocalCacheManager line 107 🤔
-            Thread.sleep(1000);
-            writer.append(buf);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        mMonoBlockStore.registerBlockStoreEventListener(listener);
         assertThrows(RuntimeException.class, () -> {
-            mMonoBlockStore.commitBlock(sessoinId, blockId, false);
+            mMonoBlockStore.commitBlock(sessionId, blockId, false);
         });
 
-        verify(listener).onCommitBlockToLocal(anyLong(), any(BlockStoreLocation.class));
-        verify(listener, never()).onCommitBlockToMaster(anyLong(), any(BlockStoreLocation.class));
+        verify(mListener).onCommitBlockToLocal(anyLong(), any(BlockStoreLocation.class));
+        verify(mListener, never()).onCommitBlockToMaster(anyLong(), any(BlockStoreLocation.class));
     }
 
 
@@ -134,26 +114,34 @@ public class MonoBlockStoreCommitBlockTest {
             throw new RuntimeException();
         }).when(mTieredBlockStore).commitBlockInternal(anyLong(), anyLong(), anyBoolean());
 
+        prepareBlockStore();
+
+        assertThrows(RuntimeException.class, () -> {
+            mMonoBlockStore.commitBlock(sessionId, blockId, false);
+        });
+
+        verify(mListener, never()).onCommitBlockToLocal(anyLong(), any(BlockStoreLocation.class));
+        verify(mListener, never()).onCommitBlockToMaster(anyLong(), any(BlockStoreLocation.class));
+    }
+
+    public void prepareBlockStore() {
         mMonoBlockStore = new MonoBlockStore(mTieredBlockStore, mockedBlockMasterClientPool, mock(UfsManager.class), new AtomicReference<>(1L));
 
-        mMonoBlockStore.createBlock(sessoinId, blockId, FIRST_TIER, new CreateBlockOptions(null, null, 64));
+        try {
+            TieredBlockStoreTestUtils.createTempBlock(sessionId, blockId, 64, mTestDir1);
+        } catch (Exception e) {
+            System.out.println("createTempBlockExceptiona: " + e);
+        }
+
         byte[] data = new byte[64];
         Arrays.fill(data, (byte) 1);
         ByteBuffer buf = ByteBuffer.wrap(data);
-        try (BlockWriter writer = mMonoBlockStore.createBlockWriter(1L, blockId)) {
-            // LocalCacheManager line 107 🤔
-            Thread.sleep(1000);
+        try (BlockWriter writer = mMonoBlockStore.createBlockWriter(sessionId, blockId)) {
             writer.append(buf);
         } catch (Exception e) {
             System.out.println(e);
         }
-        mMonoBlockStore.registerBlockStoreEventListener(listener);
-        assertThrows(RuntimeException.class, () -> {
-            mMonoBlockStore.commitBlock(sessoinId, blockId, false);
-        });
-
-        verify(listener, never()).onCommitBlockToLocal(anyLong(), any(BlockStoreLocation.class));
-        verify(listener, never()).onCommitBlockToMaster(anyLong(), any(BlockStoreLocation.class));
+        mMonoBlockStore.registerBlockStoreEventListener(mListener);
     }
 
 }
