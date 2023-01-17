6fc070180928e1696722b50ced7b9ed19f886752
voddle <dengxinyu0105@gmail.com>
2023-01-18 00:23:44 +0800

    Split BlockStoreEventListener.onCommitBlock()
    ### What changes are proposed in this pull request?
    
    `listener.onCommitBlock()` cannot reflect the state difference between
    commitBlockToLocal and commitBlockToMaster, so some times when
    commitBlockToMaster failed the `listener.onCommitBlock()` will still be
    called. This PR separated the `listener.onCommitBlock()` to
    `onCommitBlockToLocal()` and `onCommitBlockToMaster()`, and added
    corresponding UT to `PagedBlockStore` and `MonoBlockStore`.
    
    ### Why are the changes needed?
    
    Please clarify why the changes are needed. For instance,
    1. `listener.onCommitBlock()` cannot reflect the state difference
    between commitBlockToLocal and commitBlockToMaster, so some times when
    commitBlockToMaster failed the `listener.onCommitBlock()` will still be
    called. The separation of the `listener.onCommitBlock()` can eliminate
    this inconsistency.
    
    ### Does this PR introduce any user facing changes?
    
    Please list the user-facing changes introduced by your change, including
    No user-facing changes
    
    previous conversation is
    [here](https://github.com/voddle/alluxio/pull/1) since I select
    incorrect base repo the first time I create this PR...
    
    pr-link: Alluxio/alluxio#16777
    change-id: cid-9c367b0cab5dd5347eda873bc3c8ba189626cc75

--- core/server/worker/src/main/java/alluxio/worker/block/MonoBlockStore.java
+++ core/server/worker/src/main/java/alluxio/worker/block/MonoBlockStore.java
@@ -53,6 +53,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -70,6 +71,10 @@ public class MonoBlockStore implements BlockStore {
   private final UnderFileSystemBlockStore mUnderFileSystemBlockStore;
   private final BlockMasterClientPool mBlockMasterClientPool;
   private final AtomicReference<Long> mWorkerId;
+
+  private final List<BlockStoreEventListener> mBlockStoreEventListeners =
+          new CopyOnWriteArrayList<>();
+
   private final ScheduledExecutorService mDelayer =
       new ScheduledThreadPoolExecutor(1, ThreadFactoryUtils.build("LoadTimeOut", true));
 
@@ -118,6 +123,11 @@ public class MonoBlockStore implements BlockStore {
       blockMasterClient.commitBlock(mWorkerId.get(),
           mLocalBlockStore.getBlockStoreMeta().getUsedBytesOnTiers().get(loc.tierAlias()),
           loc.tierAlias(), loc.mediumType(), blockId, meta.getBlockSize());
+      for (BlockStoreEventListener listener : mBlockStoreEventListeners) {
+        synchronized (listener) {
+          listener.onCommitBlockToMaster(blockId, loc);
+        }
+      }
     } catch (AlluxioStatusException e) {
       throw AlluxioRuntimeException.from(e);
     } finally {
@@ -266,6 +276,8 @@ public class MonoBlockStore implements BlockStore {
 
   @Override
   public void registerBlockStoreEventListener(BlockStoreEventListener listener) {
+    LOG.debug("registerBlockStoreEventListener: listener={}", listener);
+    mBlockStoreEventListeners.add(listener);
     mLocalBlockStore.registerBlockStoreEventListener(listener);
   }
 
