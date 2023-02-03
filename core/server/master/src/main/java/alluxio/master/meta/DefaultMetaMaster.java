Alluxio/alluxio

commit 608c04f3415ca2e4031b447cc74b4fb97748beb5

Support update config
Fix #13513

bin/alluxio fsadmin updateConfig alluxio.master.worker.timeout=4min


![image](https://user-images.githubusercontent.com/17329931/120227035-01afda80-c27b-11eb-93e1-337e42645d95.png)



![image](https://user-images.githubusercontent.com/17329931/120226990-ec3ab080-c27a-11eb-8154-59d13851baab.png)



Now support dynamic update properties have been tested are:

- alluxio.master.unsafe.direct.persist.object.enabled
- alluxio.master.worker.timeout
- alluxio.master.audit.logging.enabled
- alluxio.master.ufs.managed.blocking.enabled
- alluxio.master.metastore.inode.inherit.owner.and.group

pr-link: Alluxio/alluxio#13514
change-id: cid-58e9469b8db6f962adf700067ff4c86d714bcadd

core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
@@ -606,6 +606,34 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
     mPathProperties.resetState();
   }
 
+  @Override
+  public Map<String, Boolean> updateConfiguration(Map<String, String> propertiesMap) {
+    Map<String, Boolean> result = new HashMap<>();
+    int successCount = 0;
+    for (Map.Entry<String, String> entry : propertiesMap.entrySet()) {
+      try {
+        PropertyKey key = PropertyKey.fromString(entry.getKey());
+        if (ServerConfiguration.getBoolean(PropertyKey.CONF_DYNAMIC_UPDATE_ENABLED)
+            && key.isDynamic()) {
+          String oldValue = ServerConfiguration.get(key);
+          ServerConfiguration.set(key, entry.getValue(), Source.RUNTIME);
+          result.put(entry.getKey(), true);
+          successCount++;
+          LOG.info("Property {} has been updated to \"{}\" from \"{}\"",
+              key.getName(), entry.getValue(), oldValue);
+        } else {
+          LOG.debug("Update a non-dynamic property {} is not allowed", key.getName());
+          result.put(entry.getKey(), false);
+        }
+      } catch (Exception e) {
+        result.put(entry.getKey(), false);
+        LOG.error("Failed to update property {} to {}", entry.getKey(), entry.getValue(), e);
+      }
+    }
+    LOG.debug("Update {} properties, succeed {}.", propertiesMap.size(), successCount);
+    return result;
+  }
+
   /**
    * Lost master periodic check.
    */

core/client/fs/src/main/java/alluxio/client/meta/MetaMasterConfigClient.java
@@ -75,4 +75,13 @@ public interface MetaMasterConfigClient extends Client {
    * @param path the path
    */
   void removePathConfiguration(AlluxioURI path) throws IOException;
+
+  /**
+   * Updates properties.
+   *
+   * @param propertiesMap the properties map to be updated
+   * @return the update properties status map
+   */
+  Map<PropertyKey, Boolean> updateConfiguration(
+      Map<PropertyKey, String> propertiesMap) throws IOException;
 }

core/client/fs/src/main/java/alluxio/client/meta/RetryHandlingMetaMasterConfigClient.java
@@ -21,6 +21,7 @@ import alluxio.grpc.MetaMasterConfigurationServiceGrpc;
 import alluxio.grpc.RemovePathConfigurationPRequest;
 import alluxio.grpc.ServiceType;
 import alluxio.grpc.SetPathConfigurationPRequest;
+import alluxio.grpc.UpdateConfigurationPRequest;
 import alluxio.master.MasterClientContext;
 import alluxio.wire.ConfigHash;
 import alluxio.wire.Configuration;
@@ -117,5 +118,23 @@ public class RetryHandlingMetaMasterConfigClient extends AbstractMasterClient
   public void removePathConfiguration(AlluxioURI path) throws IOException {
     retryRPC(() -> mClient.removePathConfiguration(RemovePathConfigurationPRequest.newBuilder()
         .setPath(path.getPath()).build()), RPC_LOG, "removePathConfiguration", "path=%s", path);
+  }
+
+  @Override
+  public Map<PropertyKey, Boolean> updateConfiguration(
+      Map<PropertyKey, String> propertiesMap) throws IOException {
+    Map<PropertyKey, Boolean> resultMap = new HashMap<>();
+    Map<String, String> inputMap = new HashMap<>();
+    propertiesMap.forEach((k, v) -> inputMap.put(k.getName(), v));
+    retryRPC(
+        () -> mClient.updateConfiguration(
+            UpdateConfigurationPRequest.newBuilder()
+                .putAllProperties(inputMap)
+                .build()),
+        RPC_LOG, "updateConfiguration", "propertiesMap=%s", propertiesMap)
+        .getStatusMap().forEach((k, v) -> {
+          resultMap.put(PropertyKey.getOrBuildCustom(k), v);
+        });
+    return resultMap;
   }
 }

core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -124,6 +124,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
     private ConsistencyCheckLevel mConsistencyCheckLevel = ConsistencyCheckLevel.IGNORE;
     private Scope mScope = Scope.ALL;
     private DisplayType mDisplayType = DisplayType.DEFAULT;
+    private boolean mIsDynamic = true;
 
     /**
      * @param name name of the property
@@ -250,6 +251,15 @@ public final class PropertyKey implements Comparable<PropertyKey> {
     }
 
     /**
+     * @param dynamic whether the property could be updated dynamically
+     * @return the updated builder instance
+     */
+    public Builder setIsDynamic(boolean dynamic) {
+      mIsDynamic = dynamic;
+      return this;
+    }
+
+    /**
      * Creates and registers the property key.
      *
      * @return the created property key instance
@@ -277,7 +287,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
 
       PropertyKey key = new PropertyKey(mName, mDescription, defaultSupplier, mAlias,
           mIgnoredSiteProperty, mIsHidden, mConsistencyCheckLevel, mScope, mDisplayType,
-          mIsBuiltIn);
+          mIsBuiltIn, mIsDynamic);
       return key;
     }
 
@@ -296,6 +306,13 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .setDefaultValue(String.format("${%s}/conf", Name.HOME))
           .setDescription("The directory containing files used to configure Alluxio.")
           .setIgnoredSiteProperty(true)
+          .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
+          .setScope(Scope.ALL)
+          .build();
+  public static final PropertyKey CONF_DYNAMIC_UPDATE_ENABLED =
+      new Builder(Name.CONF_DYNAMIC_UPDATE_ENABLED)
+          .setDefaultValue(false)
+          .setDescription("Whether to support dynamic update property.")
           .setConsistencyCheckLevel(ConsistencyCheckLevel.WARN)
           .setScope(Scope.ALL)
           .build();
@@ -4823,6 +4840,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
               + "authentication is enabled. Server trusts whoever the client claims to be.")
           .setConsistencyCheckLevel(ConsistencyCheckLevel.ENFORCE)
           .setScope(Scope.ALL)
+          .setIsDynamic(false)
           .build();
   public static final PropertyKey SECURITY_AUTHORIZATION_PERMISSION_ENABLED =
       new Builder(Name.SECURITY_AUTHORIZATION_PERMISSION_ENABLED)
@@ -4830,6 +4848,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .setDescription("Whether to enable access control based on file permission.")
           .setConsistencyCheckLevel(ConsistencyCheckLevel.ENFORCE)
           .setScope(Scope.ALL)
+          .setIsDynamic(false)
           .build();
   public static final PropertyKey SECURITY_AUTHORIZATION_PERMISSION_SUPERGROUP =
       new Builder(Name.SECURITY_AUTHORIZATION_PERMISSION_SUPERGROUP)
@@ -5247,6 +5266,8 @@ public final class PropertyKey implements Comparable<PropertyKey> {
   @ThreadSafe
   public static final class Name {
     public static final String CONF_DIR = "alluxio.conf.dir";
+    public static final String CONF_DYNAMIC_UPDATE_ENABLED =
+        "alluxio.conf.dynamic.update.enabled";
     public static final String CONF_VALIDATION_ENABLED = "alluxio.conf.validation.enabled";
     public static final String DEBUG = "alluxio.debug";
     public static final String EXTENSIONS_DIR = "alluxio.extensions.dir";
@@ -6598,6 +6619,9 @@ public final class PropertyKey implements Comparable<PropertyKey> {
   /** The displayType which indicates how the property value should be displayed. **/
   private final DisplayType mDisplayType;
 
+  /** Whether the property could be updated dynamically. */
+  private final boolean mDynamic;
+
   /**
    * @param name String of this property
    * @param description String description of this property key
@@ -6614,7 +6638,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
   private PropertyKey(String name, String description, DefaultSupplier defaultSupplier,
       String[] aliases, boolean ignoredSiteProperty, boolean isHidden,
       ConsistencyCheckLevel consistencyCheckLevel, Scope scope, DisplayType displayType,
-      boolean isBuiltIn) {
+      boolean isBuiltIn, boolean dynamic) {
     mName = Preconditions.checkNotNull(name, "name");
     // TODO(binfan): null check after we add description for each property key
     mDescription = Strings.isNullOrEmpty(description) ? "N/A" : description;
@@ -6626,6 +6650,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
     mScope = scope;
     mDisplayType = displayType;
     mIsBuiltIn = isBuiltIn;
+    mDynamic = dynamic;
   }
 
   /**
@@ -6633,7 +6658,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
    */
   private PropertyKey(String name) {
     this(name, null, new DefaultSupplier(() -> null, "null"), null, false, false,
-        ConsistencyCheckLevel.IGNORE, Scope.ALL, DisplayType.DEFAULT, true);
+        ConsistencyCheckLevel.IGNORE, Scope.ALL, DisplayType.DEFAULT, true, true);
   }
 
   /**
@@ -6781,6 +6806,13 @@ public final class PropertyKey implements Comparable<PropertyKey> {
    */
   public boolean isBuiltIn() {
     return mIsBuiltIn;
+  }
+
+  /**
+   * @return true if this property can be updated dynamically during runtime
+   */
+  public boolean isDynamic() {
+    return mDynamic;
   }
 
   /**

core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -4559,9 +4559,14 @@ public final class DefaultFileSystemMaster extends CoreMaster
    */
   private FileSystemMasterAuditContext createAuditContext(String command, AlluxioURI srcPath,
       @Nullable AlluxioURI dstPath, @Nullable Inode srcInode) {
+    // Audit log may be enabled during runtime
+    AsyncUserAccessAuditLogWriter auditLogWriter = null;
+    if (ServerConfiguration.getBoolean(PropertyKey.MASTER_AUDIT_LOGGING_ENABLED)) {
+      auditLogWriter = mAsyncAuditLogWriter;
+    }
     FileSystemMasterAuditContext auditContext =
-        new FileSystemMasterAuditContext(mAsyncAuditLogWriter);
-    if (mAsyncAuditLogWriter != null) {
+        new FileSystemMasterAuditContext(auditLogWriter);
+    if (auditLogWriter != null) {
       String user = null;
       String ugi = "";
       try {

core/server/master/src/main/java/alluxio/master/meta/MetaMaster.java
@@ -164,4 +164,10 @@ public interface MetaMaster extends BackupOps, Master {
    * @return the hostname of the master that did the checkpoint
    */
   String checkpoint() throws IOException;
+
+  /**
+   * @param propertiesMap properties to update
+   * @return the update properties status map
+   */
+  Map<String, Boolean> updateConfiguration(Map<String, String> propertiesMap);
 }

core/server/master/src/main/java/alluxio/master/meta/MetaMasterConfigurationServiceHandler.java
@@ -22,6 +22,8 @@ import alluxio.grpc.RemovePathConfigurationPRequest;
 import alluxio.grpc.RemovePathConfigurationPResponse;
 import alluxio.grpc.SetPathConfigurationPRequest;
 import alluxio.grpc.SetPathConfigurationPResponse;
+import alluxio.grpc.UpdateConfigurationPRequest;
+import alluxio.grpc.UpdateConfigurationPResponse;
 import alluxio.wire.ConfigHash;
 
 import io.grpc.stub.StreamObserver;
@@ -126,5 +128,18 @@ public final class MetaMasterConfigurationServiceHandler
         }
         return RemovePathConfigurationPResponse.getDefaultInstance();
       }, "removePathConfiguration", "request=%s", responseObserver, request);
+  }
+
+  @Override
+  public void updateConfiguration(
+      UpdateConfigurationPRequest request,
+      StreamObserver<UpdateConfigurationPResponse> responseObserver) {
+    RpcUtils.call(LOG, () -> {
+      Map<String, Boolean> result =
+          mMetaMaster.updateConfiguration(request.getPropertiesMap());
+      return UpdateConfigurationPResponse.newBuilder()
+          .putAllStatus(result)
+          .build();
+    }, "updateConfiguration", "request=%s", responseObserver, request);
   }
 }

core/transport/src/main/proto/grpc/meta_master.proto
@@ -190,25 +190,27 @@ message GetConfigHashPResponse {
   * to query cluster configuration.
   */
 service MetaMasterConfigurationService {
-    /**
-     * Returns a list of Alluxio runtime configuration information.
-     */
-    rpc GetConfiguration (GetConfigurationPOptions) returns (GetConfigurationPResponse);
+  /**
+   * Returns a list of Alluxio runtime configuration information.
+   */
+  rpc GetConfiguration (GetConfigurationPOptions) returns (GetConfigurationPResponse);
 
-    /**
-     * Sets a property for a path.
-     */
-    rpc SetPathConfiguration (SetPathConfigurationPRequest) returns (SetPathConfigurationPResponse);
+  /**
+   * Sets a property for a path.
+   */
+  rpc SetPathConfiguration (SetPathConfigurationPRequest) returns (SetPathConfigurationPResponse);
 
-    /**
-     * Removes properties for a path, if the keys are empty, it means remove all properties.
-     */
-    rpc RemovePathConfiguration (RemovePathConfigurationPRequest) returns (RemovePathConfigurationPResponse);
+  /**
+   * Removes properties for a path, if the keys are empty, it means remove all properties.
+   */
+  rpc RemovePathConfiguration (RemovePathConfigurationPRequest) returns (RemovePathConfigurationPResponse);
 
-    /**
-     * Returns the hashes of cluster and path level configurations.
-     */
-    rpc GetConfigHash (GetConfigHashPOptions) returns (GetConfigHashPResponse);
+  /**
+   * Returns the hashes of cluster and path level configurations.
+   */
+  rpc GetConfigHash (GetConfigHashPOptions) returns (GetConfigHashPResponse);
+
+  rpc UpdateConfiguration(UpdateConfigurationPRequest) returns (UpdateConfigurationPResponse);
 }
 
 message GetMasterIdPOptions {}
@@ -242,6 +244,14 @@ message MasterHeartbeatPRequest {
 }
 message MasterHeartbeatPResponse {
   optional MetaCommand command = 1;
+}
+
+message UpdateConfigurationPRequest {
+  map<string, string> properties = 1;
+}
+
+message UpdateConfigurationPResponse {
+  map<string, bool> status = 1;
 }
 
 /**

commit 407cf1521b8612319df0ca80e5fa8716918c9505

Fix updated config not notifying config registry
Add update notify to master

This issue introduced by #13514

pr-link: Alluxio/alluxio#16764
change-id: cid-36f2010233c3af95f28cf9cb7ac0a0a1963c86fe

core/server/master/src/main/java/alluxio/master/meta/DefaultMetaMaster.java
@@ -20,6 +20,7 @@ import alluxio.collections.IndexedSet;
 import alluxio.conf.Configuration;
 import alluxio.conf.ConfigurationValueOptions;
 import alluxio.conf.PropertyKey;
+import alluxio.conf.ReconfigurableRegistry;
 import alluxio.conf.Source;
 import alluxio.exception.AlluxioException;
 import alluxio.exception.status.NotFoundException;
@@ -662,6 +663,9 @@ public final class DefaultMetaMaster extends CoreMaster implements MetaMaster {
       }
     }
     LOG.debug("Update {} properties, succeed {}.", propertiesMap.size(), successCount);
+    if (successCount > 0) {
+      ReconfigurableRegistry.update();
+    }
     return result;
   }
 