6ca7d1533479d9c745a214a8239d60c2fda42f4a
Chunxu Tang <8018679+ChunxuTang@users.noreply.github.com>
2023-02-02 16:22:34 -0800

    Add cache TTL enforcement
    ### What changes are proposed in this pull request?
    
    Co-Author-By: Chen Liang <chliang@uber.com>
    
    This change adds a scheduled thread in LocalCacheManager, to
    periodically check the page create time, and delete pages that have
    passed the configurable time threshold.
    
    ### Why are the changes needed?
    
    Certain use cases require the cached data to be removed beyond certain
    longevity. This is mainly for compliance considerations.
    
    ### Does this PR introduce any user facing changes?
    
    No
    
    pr-link: Alluxio/alluxio#16843
    change-id: cid-49289cc488680f44b39386a5f364d6d7449cd11a

--- core/client/fs/src/main/java/alluxio/client/file/cache/LocalCacheManager.java
+++ core/client/fs/src/main/java/alluxio/client/file/cache/LocalCacheManager.java
@@ -14,6 +14,8 @@ package alluxio.client.file.cache;
 import static alluxio.client.file.cache.CacheManager.State.NOT_IN_USE;
 import static alluxio.client.file.cache.CacheManager.State.READ_ONLY;
 import static alluxio.client.file.cache.CacheManager.State.READ_WRITE;
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import alluxio.client.file.CacheContext;
 import alluxio.client.file.cache.store.ByteArrayTargetBuffer;
@@ -44,6 +46,7 @@ import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -89,6 +92,8 @@ public class LocalCacheManager implements CacheManager {
   private final Optional<ExecutorService> mInitService;
   /** Executor service for execute the async cache tasks. */
   private final Optional<ExecutorService> mAsyncCacheExecutor;
+  /** Executor service for execute the cache ttl check tasks. */
+  private final Optional<ScheduledExecutorService> mTtlEnforcerExecutor;
   private final ConcurrentHashSet<PageId> mPendingRequests;
   /** State of this cache. */
   private final AtomicReference<CacheManager.State> mState = new AtomicReference<>();
@@ -142,6 +147,21 @@ public class LocalCacheManager implements CacheManager {
     mInitService =
         options.isAsyncRestoreEnabled() ? Optional.of(Executors.newSingleThreadExecutor()) :
             Optional.empty();
+    if (options.isTtlEnabled()) {
+      mTtlEnforcerExecutor = Optional.of(newScheduledThreadPool(1));
+      mTtlEnforcerExecutor.get().scheduleAtFixedRate(() ->
+          LocalCacheManager.this.invalidate(pageInfo -> {
+            try {
+              return System.currentTimeMillis() - pageInfo.getCreatedTimestamp()
+                  >= options.getTtlThresholdSeconds() * 1000;
+            } catch (Exception ex) {
+              // In case of any exception, do not invalidate the cache
+              return false;
+            }
+          }), 0, options.getTtlCheckIntervalSeconds(), SECONDS);
+    } else {
+      mTtlEnforcerExecutor = Optional.empty();
+    }
     Metrics.registerGauges(mCacheSize, mPageMetaStore);
     mState.set(READ_ONLY);
     Metrics.STATE.inc();
@@ -664,6 +684,7 @@ public class LocalCacheManager implements CacheManager {
     mPageMetaStore.reset();
     mInitService.ifPresent(ExecutorService::shutdownNow);
     mAsyncCacheExecutor.ifPresent(ExecutorService::shutdownNow);
+    mTtlEnforcerExecutor.ifPresent(ExecutorService::shutdownNow);
   }
 
   /**
