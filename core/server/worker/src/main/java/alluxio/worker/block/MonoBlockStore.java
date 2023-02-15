60d51564167dbc05047d9435f0825f20ed047c0a
tian bao <2011xuesong@gmail.com>
2023-02-16 02:55:46 +0800

    Fix Worker.ActiveClients is negative when load from ufs
    ### What changes are proposed in this pull request?
    
    Fix Worker.ActiveClients is negative when load from ufs
    ### Why are the changes needed?
    
    Please clarify why the changes are needed. For instance,
    createUfsBlockReader will invoke closeUfsBlock then invoke commitBlock
    then this metric decrease.
    But forget to increase firstly . So it need to increase firstly.
    
    ### Does this PR introduce any user facing changes?
    
    Please list the user-facing changes introduced by your change, including
    no.
    
    pr-link: Alluxio/alluxio#16784
    change-id: cid-13d0875dc42336cc612dfed121bba8572164ed60

--- core/server/worker/src/main/java/alluxio/worker/block/MonoBlockStore.java
+++ core/server/worker/src/main/java/alluxio/worker/block/MonoBlockStore.java
@@ -33,6 +33,7 @@ import alluxio.retry.ExponentialBackoffRetry;
 import alluxio.retry.RetryUtils;
 import alluxio.underfs.UfsManager;
 import alluxio.util.ThreadFactoryUtils;
+import alluxio.worker.block.DefaultBlockWorker.Metrics;
 import alluxio.worker.block.io.BlockReader;
 import alluxio.worker.block.io.BlockWriter;
 import alluxio.worker.block.io.DelegatingBlockReader;
@@ -161,6 +162,7 @@ public class MonoBlockStore implements BlockStore {
     Optional<? extends BlockMeta> blockMeta = mLocalBlockStore.getVolatileBlockMeta(blockId);
     if (blockMeta.isPresent()) {
       reader = mLocalBlockStore.createBlockReader(sessionId, blockId, offset);
+      DefaultBlockWorker.Metrics.WORKER_ACTIVE_CLIENTS.inc();
     } else {
       boolean checkUfs = options != null && (options.hasUfsPath() || options.getBlockInUfsTier());
       if (!checkUfs) {
@@ -169,7 +171,6 @@ public class MonoBlockStore implements BlockStore {
       // When the block does not exist in Alluxio but exists in UFS, try to open the UFS block.
       reader = createUfsBlockReader(sessionId, blockId, offset, positionShort, options);
     }
-    DefaultBlockWorker.Metrics.WORKER_ACTIVE_CLIENTS.inc();
     return reader;
   }
 
@@ -181,7 +182,10 @@ public class MonoBlockStore implements BlockStore {
     try {
       BlockReader reader = mUnderFileSystemBlockStore.createBlockReader(sessionId, blockId, offset,
           positionShort, options);
-      return new DelegatingBlockReader(reader, () -> closeUfsBlock(sessionId, blockId));
+      BlockReader blockReader = new DelegatingBlockReader(reader,
+          () -> closeUfsBlock(sessionId, blockId));
+      Metrics.WORKER_ACTIVE_CLIENTS.inc();
+      return blockReader;
     } catch (Exception e) {
       try {
         closeUfsBlock(sessionId, blockId);
