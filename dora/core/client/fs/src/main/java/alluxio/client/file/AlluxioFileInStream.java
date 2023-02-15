0b20044e73f6bac68e93ed45cdc754aca7d14ec5
qian0817 <qianlei666@gmail.com>
2023-02-15 17:20:24 +0800

    Reduce useless async cache request when read from remote worker
    ### What changes are proposed in this pull request?
    
    reduce useless async cache request when read from remote worker
    
    ### Why are the changes needed?
    
    It is useless to trigger async cache when the remote worker has already
    cached block.
    
    ### Does this PR introduce any user facing changes?
    
    No user facing changes.
    
    pr-link: Alluxio/alluxio#16313
    change-id: cid-ad4f236cf143346085b59b24122a3dc6870281e7

--- dora/core/client/fs/src/main/java/alluxio/client/file/AlluxioFileInStream.java
+++ dora/core/client/fs/src/main/java/alluxio/client/file/AlluxioFileInStream.java
@@ -55,6 +55,7 @@ import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import javax.annotation.concurrent.NotThreadSafe;
 
 /**
@@ -528,7 +529,13 @@ public class AlluxioFileInStream extends FileInStream {
         if (mPassiveCachingEnabled && mContext.hasNodeLocalWorker()) {
           // send request to local worker
           worker = mContext.getNodeLocalWorker();
-        } else { // send request to data source
+        } else {
+          if (blockInfo.getLocations().stream()
+              .anyMatch(it -> Objects.equals(it.getWorkerAddress(), dataSource))) {
+            mLastBlockIdCached = blockId;
+            return false;
+          }
+          // send request to data source
           worker = dataSource;
         }
         try (CloseableResource<BlockWorkerClient> blockWorker =
