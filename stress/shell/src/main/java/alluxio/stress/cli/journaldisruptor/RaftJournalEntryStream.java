05196370287d427a7592183096e567fab014289d
voddle <dengxinyu0105@gmail.com>
2023-02-16 13:58:12 +0800

    rename journalTool to raft journal entry stream

--- stress/shell/src/main/java/alluxio/stress/cli/journaldisruptor/RaftJournalEntryStream.java
+++ stress/shell/src/main/java/alluxio/stress/cli/journaldisruptor/RaftJournalEntryStream.java
@@ -1,11 +1,10 @@
 package alluxio.stress.cli.journaldisruptor;
 
 import alluxio.master.journal.JournalEntryStreamReader;
-import alluxio.proto.journal.Journal;
 import alluxio.master.journal.raft.RaftJournalSystem;
 import alluxio.master.journal.raft.RaftJournalUtils;
+import alluxio.proto.journal.Journal;
 
-// import com.google.protobuf.Any;
 import org.apache.ratis.proto.RaftProtos;
 import org.apache.ratis.server.RaftServerConfigKeys;
 import org.apache.ratis.server.raftlog.segmented.LogSegmentPath;
@@ -19,6 +18,9 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * the raft version of the EntryStream.
+ */
 public class RaftJournalEntryStream extends EntryStream {
 
   private List<LogSegmentPath> mPaths;
@@ -29,19 +31,26 @@ public class RaftJournalEntryStream extends EntryStream {
   private JournalEntryStreamReader mReader;
   private byte[] mBuffer = new byte[4096];
 
-  private RaftProtos.LogEntryProto proto;
-  private Journal.JournalEntry entry;
-  private List<Journal.JournalEntry> list = new ArrayList<>();
-  private int index = 0;
+  private RaftProtos.LogEntryProto mNextProto;
+  private Journal.JournalEntry mEntry;
+  private List<Journal.JournalEntry> mList = new ArrayList<>();
+  private int mIndex = 0;
 
+  /**
+   * init RaftJournalEntryStream.
+   * @param master
+   * @param start
+   * @param end
+   * @param inputDir
+   */
   public RaftJournalEntryStream(String master, long start, long end, String inputDir) {
     super(master, start, end, inputDir);
     try (
         RaftStorage storage = StorageImplUtils.newRaftStorage(getJournalDir(),
-        RaftServerConfigKeys.Log.CorruptionPolicy.getDefault(),
-        RaftStorage.StartupOption.RECOVER,
-        RaftServerConfigKeys.STORAGE_FREE_SPACE_MIN_DEFAULT.getSize())
-        ) {
+            RaftServerConfigKeys.Log.CorruptionPolicy.getDefault(),
+            RaftStorage.StartupOption.RECOVER,
+            RaftServerConfigKeys.STORAGE_FREE_SPACE_MIN_DEFAULT.getSize())
+      ) {
       storage.initialize();
       System.out.printf("storage is: %s", storage);
       mPaths = LogSegmentPath.getLogSegmentPaths(storage);
@@ -53,37 +62,44 @@ public class RaftJournalEntryStream extends EntryStream {
 
   /**
    * the nextEntry function of EntryStream
-   * Because in Raft Journal one proto may contain several Alluxio entries, we declare a List to record current entry list. And every time when trying to return next entry the Stream will check whether current entry list has been finished. If not read one entry, if yes read next proto till met the proto that contains the Alluxio entry.
+   * Because in Raft Journal one proto may contain several Alluxio entries, we
+   * declare a List to record current entry list.
+   * And every time when trying to return next entry the Stream will
+   * check whether current entry list has been finished.
+   * If not read one entry,
+   * if yes read next proto till met the proto that contains the Alluxio entry.
    *
    * @returny next JournalEntry
    */
   @Override
   public Journal.JournalEntry nextEntry() {
-    if (index < list.size()) {
-      return list.get(index++);
+    if (mIndex < mList.size()) {
+      return mList.get(mIndex++);
     }
-    while ((proto = nextProto()) != null && !proto.hasStateMachineLogEntry()) {}
-    if (proto == null) {
+    while ((mNextProto = nextProto()) != null && !mNextProto.hasStateMachineLogEntry()) {}
+    if (mNextProto == null) {
       // no proto, stream comes to the end
       return null;
     }
     try {
-      entry = Journal.JournalEntry.parseFrom(proto.getStateMachineLogEntry().getLogData().asReadOnlyByteBuffer());
+      mEntry = Journal.JournalEntry.parseFrom(
+          mNextProto.getStateMachineLogEntry().getLogData().asReadOnlyByteBuffer());
     } catch (Exception e) {
       System.out.println("failed to parse proto to entry: " + e);
     }
-    list = entry.getJournalEntriesList();
-    index = 0;
+    mList = mEntry.getJournalEntriesList();
+    mIndex = 0;
     return nextEntry();
   }
 
   /**
    * since the non-log entry proto needs to be process/print by the journal tool,
-   * here cannot integrate the process proto
+   * here cannot integrate the process proto.
    *
    * non Alluxio entry was generated and wrote by RaftJournalWriter, no need to worry about
    *
-   * Here check SegmentedRaftLogInputStream first, if null read the path of next journal file and open the stream of it, till all paths were used
+   * Here check SegmentedRaftLogInputStream first,
+   * if null read the path of next journal file and open the stream of it, till all paths were used
    *
    * the type of the proto was judged by the caller
    * @return all LogEntryProto
@@ -96,7 +112,9 @@ public class RaftJournalEntryStream extends EntryStream {
       }
       LogSegmentPath path = mPaths.get(mCurrentPathIndex);
       try {
-        mIn = new SegmentedRaftLogInputStream(path.getPath().toFile(), path.getStartEnd().getStartIndex(), path.getStartEnd().getEndIndex(), path.getStartEnd().isOpen());
+        mIn = new SegmentedRaftLogInputStream(path.getPath().toFile(),
+            path.getStartEnd().getStartIndex(),
+            path.getStartEnd().getEndIndex(), path.getStartEnd().isOpen());
         mCurrentPathIndex += 1;
       } catch (Exception e) {
         // LOG sth, but how to init log with destination...
