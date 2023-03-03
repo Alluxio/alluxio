b725cc66ee8b45a60e55adaf1dde8b9e6548d1f8
Jiacheng Liu <jiacheliu3@gmail.com>
2023-03-03 13:51:37 +0800

    Improve state lock tracking and report on error
    ### What changes are proposed in this pull request?
    
    1. Change tracking threads to keeping thread read lock holder count
    2. Log jstack on state lock error so we can further check who are
    holding the state lock
    
    ### Why are the changes needed?
    
    Make debugging state lock issues slightly easier.
    
    ### Does this PR introduce any user facing changes?
    
    A chunky jstack output will appear in the master.log, which may look
    intimidating at first glance. This is good because I want people to
    notice and send the output to us.
    
    pr-link: Alluxio/alluxio#16984
    change-id: cid-6c88ff9cf694cb0aee3fd09359d01f377dd017d6

--- dora/core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
+++ dora/core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -5390,7 +5390,7 @@ public class DefaultFileSystemMaster extends CoreMaster
   }
 
   @Override
-  public List<String> getStateLockSharedWaitersAndHolders() {
+  public Collection<String> getStateLockSharedWaitersAndHolders() {
     return mMasterContext.getStateLockManager().getSharedWaitersAndHolders();
   }
 
