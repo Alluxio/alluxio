e612edf7328dfb6630eb61022c6052fd33c25234
lucyge2022 <111789461+lucyge2022@users.noreply.github.com>
2023-03-13 15:21:01 -0700

    Fix setTtl workflow and ttlbucket bugs
    ### What changes are proposed in this pull request?
    
    1)setttl cmd on a big directory unnecessary setattr recursively, while
    dir ttl already supercedes children ttl. there's absolutely no reason to
    save ttl on children node individually.
    2)ttlbucket unnecessarily saves Inode obj on heap unboundedly, where it
    could and should only save inodeid and load inode from inodestore to
    double check expiry during inodettlchecker processing.
    3)fix race condition on ttlbucketlists from ttlchecker and insertion
    from foreground.
    4)fix case where any expiration of inodes failed during ttlchecker it
    will never be retried again.
    
    ### Why are the changes needed?
    
    Reduce heavy GC when setttl on a huge folder / bug fixes as indicated
    above for ttl
    
    ### Does this PR introduce any user facing changes?
    
    yes. So now setTtl command on a dir will not set ttl attributes for all
    its children.
    
    pr-link: Alluxio/alluxio#16933
    change-id: cid-36ec524b2178220886cccb7a82de119bdcc8203f

--- dora/tests/src/test/java/alluxio/client/fs/TtlIntegrationTest.java
+++ dora/tests/src/test/java/alluxio/client/fs/TtlIntegrationTest.java
@@ -20,10 +20,13 @@ import alluxio.AlluxioURI;
 import alluxio.Constants;
 import alluxio.client.file.FileOutStream;
 import alluxio.client.file.FileSystem;
+import alluxio.client.file.URIStatus;
 import alluxio.conf.PropertyKey;
+import alluxio.grpc.CreateDirectoryPOptions;
 import alluxio.grpc.CreateFilePOptions;
 import alluxio.grpc.FileSystemMasterCommonPOptions;
 import alluxio.grpc.LoadMetadataPType;
+import alluxio.grpc.SetAttributePOptions;
 import alluxio.grpc.TtlAction;
 import alluxio.grpc.WritePType;
 import alluxio.heartbeat.HeartbeatContext;
@@ -41,6 +44,7 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Integration tests for handling file TTLs (times to live).
@@ -196,6 +200,56 @@ public class TtlIntegrationTest extends BaseIntegrationTest {
       } else {
         assertTrue(mFileSystem.exists(files[i]));
       }
+    }
+  }
+
+  /**
+   * Tests that ttl on a directory will be enforced on all its children regarless
+   * of their ttl.
+   * @throws Exception
+   */
+  @Test
+  public void expireADirectory() throws Exception {
+    int numFiles = 100;
+    AlluxioURI[] files = new AlluxioURI[numFiles];
+    String directoryName = "dir1";
+    mFileSystem.createDirectory(new AlluxioURI("/" + directoryName),
+        CreateDirectoryPOptions.newBuilder().setWriteType(WritePType.CACHE_THROUGH).build());
+    String fileNamePrefix = "fileDelete";
+    for (int i = 0; i < numFiles; i++) {
+      files[i] = new AlluxioURI("/" + directoryName + "/" + fileNamePrefix + i);
+      // Only the even-index files should expire.
+      long ttl = i % 2 == 0 ? TTL_INTERVAL_MS * 2000 : TTL_INTERVAL_MS * 1000;
+      mOutStream = mFileSystem.createFile(files[i],
+          CreateFilePOptions.newBuilder().setWriteType(WritePType.CACHE_THROUGH)
+              .setCommonOptions(FileSystemMasterCommonPOptions.newBuilder().setTtl(ttl)
+                  .setTtlAction(TtlAction.DELETE)).build());
+      mOutStream.write(mBuffer, 0, 10);
+      mOutStream.close();
+    }
+    // Set much smaller ttl on directory.
+    SetAttributePOptions setTTlOptions = SetAttributePOptions.newBuilder().setRecursive(false)
+        .setCommonOptions(FileSystemMasterCommonPOptions.newBuilder()
+            .setTtl(TTL_INTERVAL_MS).setTtlAction(TtlAction.DELETE).build())
+        .build();
+    mFileSystem.setAttribute(new AlluxioURI("/" + directoryName), setTTlOptions);
+    // Individual children file's ttl should not be changed.
+    Random random = new Random();
+    int fileNum = random.nextInt(numFiles);
+    URIStatus anyFileStatus = mFileSystem.getStatus(new AlluxioURI("/" + directoryName
+        + "/" + fileNamePrefix + fileNum));
+    assert (anyFileStatus.getFileInfo().getTtl()
+        == (fileNum % 2 == 0 ? TTL_INTERVAL_MS * 2000 : TTL_INTERVAL_MS * 1000));
+
+    CommonUtils.sleepMs(4 * TTL_INTERVAL_MS);
+    HeartbeatScheduler.execute(HeartbeatContext.MASTER_TTL_CHECK);
+    /* Even though children have longer ttl, but parents' ttl overrides all.
+    No Children should exist now. */
+    for (int i = 0; i < numFiles; i++) {
+      assertFalse(mFileSystem.exists(files[i]));
+      String fileName = directoryName + "/" + fileNamePrefix + i;
+      // Check Ufs file existence
+      assertFalse(Arrays.stream(mUfs.list()).anyMatch(s -> s.equals(fileName)));
     }
   }
 }
