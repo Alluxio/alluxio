8e0e9c0fdf7c9a757281a049bf5282f1f8176120
voddle <dengxinyu0105@gmail.com>
2023-03-14 17:30:44 +0800

    fake printThreadInfo

--- core/common/src/main/java/alluxio/util/ThreadUtils.java
+++ core/common/src/main/java/alluxio/util/ThreadUtils.java
@@ -120,42 +120,48 @@ public final class ThreadUtils {
    * @param stream the stream to
    * @param title  a string title for the stack trace
    */
-  public static synchronized void printThreadInfo(PrintStream stream,
-      String title) {
-    final int STACK_DEPTH = 20;
-    boolean contention = THREAD_BEAN.isThreadContentionMonitoringEnabled();
-    long[] threadIds = THREAD_BEAN.getAllThreadIds();
-    stream.println("Process Thread Dump: " + title);
-    stream.println(threadIds.length + " active threads");
-    for (long tid : threadIds) {
-      ThreadInfo info = THREAD_BEAN.getThreadInfo(tid, STACK_DEPTH);
-      if (info == null) {
-        stream.println("  Inactive");
-        continue;
-      }
-      stream.println("Thread "
-          + getTaskName(info.getThreadId(), info.getThreadName()) + ":");
-      Thread.State state = info.getThreadState();
-      stream.println("  State: " + state);
-      stream.println("  Blocked count: " + info.getBlockedCount());
-      stream.println("  Waited count: " + info.getWaitedCount());
-      if (contention) {
-        stream.println("  Blocked time: " + info.getBlockedTime());
-        stream.println("  Waited time: " + info.getWaitedTime());
-      }
-      if (state == Thread.State.WAITING) {
-        stream.println("  Waiting on " + info.getLockName());
-      } else if (state == Thread.State.BLOCKED) {
-        stream.println("  Blocked on " + info.getLockName());
-        stream.println("  Blocked by "
-            + getTaskName(info.getLockOwnerId(), info.getLockOwnerName()));
-      }
-      stream.println("  Stack:");
-      for (StackTraceElement frame : info.getStackTrace()) {
-        stream.println("    " + frame.toString());
-      }
+  // public static synchronized void printThreadInfo(PrintStream stream,
+  //     String title) {
+  //   final int STACK_DEPTH = 20;
+  //   boolean contention = THREAD_BEAN.isThreadContentionMonitoringEnabled();
+  //   long[] threadIds = THREAD_BEAN.getAllThreadIds();
+  //   stream.println("Process Thread Dump: " + title);
+  //   stream.println(threadIds.length + " active threads");
+  //   for (long tid : threadIds) {
+  //     ThreadInfo info = THREAD_BEAN.getThreadInfo(tid, STACK_DEPTH);
+  //     if (info == null) {
+  //       stream.println("  Inactive");
+  //       continue;
+  //     }
+  //     stream.println("Thread "
+  //         + getTaskName(info.getThreadId(), info.getThreadName()) + ":");
+  //     Thread.State state = info.getThreadState();
+  //     stream.println("  State: " + state);
+  //     stream.println("  Blocked count: " + info.getBlockedCount());
+  //     stream.println("  Waited count: " + info.getWaitedCount());
+  //     if (contention) {
+  //       stream.println("  Blocked time: " + info.getBlockedTime());
+  //       stream.println("  Waited time: " + info.getWaitedTime());
+  //     }
+  //     if (state == Thread.State.WAITING) {
+  //       stream.println("  Waiting on " + info.getLockName());
+  //     } else if (state == Thread.State.BLOCKED) {
+  //       stream.println("  Blocked on " + info.getLockName());
+  //       stream.println("  Blocked by "
+  //           + getTaskName(info.getLockOwnerId(), info.getLockOwnerName()));
+  //     }
+  //     stream.println("  Stack:");
+  //     for (StackTraceElement frame : info.getStackTrace()) {
+  //       stream.println("    " + frame.toString());
+  //     }
+  //   }
+  //   stream.flush();
+  // }
+
+  public static synchronized void printThreadInfo(PrintStream stream, String title) {
+    for (ThreadInfo ti: THREAD_BEAN.dumpAllThreads(true, true)) {
+      stream.print(ti.toString());
     }
-    stream.flush();
   }
 
   /**
