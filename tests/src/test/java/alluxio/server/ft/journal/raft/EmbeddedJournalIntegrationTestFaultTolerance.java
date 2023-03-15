Alluxio/alluxio

commit 9f91f95e81d7aa1d1ea4b2b2e3ede92a645fac55

Use async journals for block id journal entries in metadata sync
### What changes are proposed in this pull request?

Currently when metadata is created during metadata sync, when creating
journal entries for block ids, they will be journaled synchronously. All
other metadata updates are journaled asynchronously then flushed as a
group.

This PR makes the block id journal entries also be flushed
asynchronously using the same journal context.

Additionally it add higher concurrency defaults on some of the caches
used by metadata sync which reduces the locking overhead.

### Does this PR introduce any user facing changes?

No

pr-link: Alluxio/alluxio#16529
change-id: cid-470942e367abc32a9557f07241ef7b99439a5d82

core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -1020,7 +1020,7 @@ public class DefaultFileSystemMaster extends CoreMaster
       mBlockMaster.removeBlocks(fileInfo.getBlockIds(), true);
       // Commit all the file blocks (without locations) so the metadata for the block exists.
       commitBlockInfosForFile(
-          fileInfo.getBlockIds(), fileInfo.getLength(), fileInfo.getBlockSizeBytes());
+          fileInfo.getBlockIds(), fileInfo.getLength(), fileInfo.getBlockSizeBytes(), null);
       // Reset file-block-info list with the new list.
       try {
         fileInfo.setFileBlockInfos(getFileBlockInfoListInternal(inodePath));
@@ -1761,7 +1761,8 @@ public class DefaultFileSystemMaster extends CoreMaster
 
     if (inode.isPersisted()) {
       // Commit all the file blocks (without locations) so the metadata for the block exists.
-      commitBlockInfosForFile(entry.getSetBlocksList(), length, inode.getBlockSizeBytes());
+      commitBlockInfosForFile(entry.getSetBlocksList(), length, inode.getBlockSizeBytes(),
+          rpcContext.getJournalContext());
       // The path exists in UFS, so it is no longer absent
       mUfsAbsentPathCache.processExisting(inodePath.getUri());
     }
@@ -1807,13 +1808,18 @@ public class DefaultFileSystemMaster extends CoreMaster
    * @param blockIds the list of block ids
    * @param fileLength length of the file in bytes
    * @param blockSize the block size in bytes
+   * @param context the journal context, if null a new context will be created
    */
-  private void commitBlockInfosForFile(List<Long> blockIds, long fileLength, long blockSize)
-      throws UnavailableException {
+  private void commitBlockInfosForFile(List<Long> blockIds, long fileLength, long blockSize,
+      @Nullable JournalContext context) throws UnavailableException {
     long currLength = fileLength;
     for (long blockId : blockIds) {
       long currentBlockSize = Math.min(currLength, blockSize);
-      mBlockMaster.commitBlockInUFS(blockId, currentBlockSize);
+      if (context != null) {
+        mBlockMaster.commitBlockInUFS(blockId, currentBlockSize, context);
+      } else {
+        mBlockMaster.commitBlockInUFS(blockId, currentBlockSize);
+      }
       currLength -= currentBlockSize;
     }
   }

core/server/master/src/main/java/alluxio/master/block/BlockMaster.java
@@ -26,6 +26,7 @@ import alluxio.grpc.StorageList;
 import alluxio.grpc.WorkerLostStorageInfo;
 import alluxio.master.Master;
 import alluxio.master.block.meta.MasterWorkerInfo;
+import alluxio.master.journal.JournalContext;
 import alluxio.metrics.Metric;
 import alluxio.proto.meta.Block;
 import alluxio.wire.Address;
@@ -169,7 +170,20 @@ public interface BlockMaster extends Master, ContainerIdGenerable {
    * @param blockId the id of the block to commit
    * @param length the length of the block
    */
-  void commitBlockInUFS(long blockId, long length) throws UnavailableException;
+  default void commitBlockInUFS(long blockId, long length) throws UnavailableException {
+    try (JournalContext journalContext = createJournalContext()) {
+      commitBlockInUFS(blockId, length, journalContext);
+    }
+  }
+
+  /**
+   * Marks a block as committed, but without a worker location. This means the block is only in ufs.
+   * Append any created journal entries to the included context.
+   * @param blockId the id of the block to commit
+   * @param length the length of the block
+   * @param context the journal context
+   */
+  void commitBlockInUFS(long blockId, long length, JournalContext context);
 
   /**
    * @param blockId the block id to get information for

core/server/master/src/main/java/alluxio/master/block/DefaultBlockMaster.java
@@ -947,10 +947,9 @@ public class DefaultBlockMaster extends CoreMaster implements BlockMaster {
   }
 
   @Override
-  public void commitBlockInUFS(long blockId, long length) throws UnavailableException {
+  public void commitBlockInUFS(long blockId, long length, JournalContext journalContext) {
     LOG.debug("Commit block in ufs. blockId: {}, length: {}", blockId, length);
-    try (JournalContext journalContext = createJournalContext();
-         LockResource r = lockBlock(blockId)) {
+    try (LockResource r = lockBlock(blockId)) {
       if (mBlockMetaStore.getBlock(blockId).isPresent()) {
         // Block metadata already exists, so do not need to create a new one.
         return;

core/server/master/src/main/java/alluxio/master/file/meta/AsyncUfsAbsentPathCache.java
@@ -82,7 +82,8 @@ public class AsyncUfsAbsentPathCache implements UfsAbsentPathCache {
     mMountTable = mountTable;
     mClock = clock;
     mCurrentPaths = new ConcurrentHashMap<>(8, 0.95f, 8);
-    mCache = CacheBuilder.newBuilder().maximumSize(MAX_PATHS).recordStats().build();
+    mCache = CacheBuilder.newBuilder().maximumSize(MAX_PATHS).concurrencyLevel(Configuration.getInt(
+        PropertyKey.MASTER_UFS_PATH_CACHE_THREADS)).recordStats().build();
     /* Number of threads for the async pool. */
 
     mPool = new ThreadPoolExecutor(numThreads, numThreads, THREAD_KEEP_ALIVE_SECONDS,

core/server/master/src/main/java/alluxio/master/file/meta/UfsSyncPathCache.java
@@ -90,7 +90,8 @@ public class UfsSyncPathCache {
   @VisibleForTesting
   UfsSyncPathCache(Clock clock, @Nullable BiConsumer<String, SyncState> onRemoval) {
     mClock = Preconditions.checkNotNull(clock);
-    mItems = CacheBuilder.newBuilder()
+    mItems = CacheBuilder.newBuilder().concurrencyLevel(
+            Configuration.getInt(PropertyKey.MASTER_UFS_PATH_CACHE_THREADS))
         .removalListener(
             (removal) -> {
               if (removal.wasEvicted() && removal.getKey() != null && removal.getValue() != null) {

commit 6bbe71358328a3e1557e9a22360db1891a6a4f61

Fix metadata sync journalling when using UFS journal
This fixes an issue caused by
https://github.com/Alluxio/alluxio/pull/16529

That PR reduced the number of journal flushes during metadata sync using
embedded journal, but was not compatible with ufs journal as in ufs
journal each master has a different journal file.

This PR disables that change when using ufs journal.

pr-link: Alluxio/alluxio#17032
change-id: cid-20836bc9b5d64aff37e6f6f6e6ef3ec5f0e73617

core/server/master/src/main/java/alluxio/master/file/DefaultFileSystemMaster.java
@@ -118,6 +118,7 @@ import alluxio.master.journal.Journaled;
 import alluxio.master.journal.JournaledGroup;
 import alluxio.master.journal.NoopJournalContext;
 import alluxio.master.journal.checkpoint.CheckpointName;
+import alluxio.master.journal.ufs.UfsJournalSystem;
 import alluxio.master.metastore.DelegatingReadOnlyInodeStore;
 import alluxio.master.metastore.InodeStore;
 import alluxio.master.metastore.ReadOnlyInodeStore;
@@ -1834,7 +1835,10 @@ public class DefaultFileSystemMaster extends CoreMaster
     long currLength = fileLength;
     for (long blockId : blockIds) {
       long currentBlockSize = Math.min(currLength, blockSize);
-      if (context != null) {
+      // if we are not using the UFS journal system, we can use the same journal context
+      // for the block info so that we do not have to create a new journal
+      // context and flush again
+      if (context != null && !(mJournalSystem instanceof UfsJournalSystem)) {
         mBlockMaster.commitBlockInUFS(blockId, currentBlockSize, context);
       } else {
         mBlockMaster.commitBlockInUFS(blockId, currentBlockSize);

minicluster/src/main/java/alluxio/multi/process/PortCoordination.java
@@ -39,6 +39,7 @@ public class PortCoordination {
   ));
   // for EmbeddedJournalIntegrationTestFaultTolerance
   public static final List<ReservedPort> EMBEDDED_JOURNAL_FAILOVER = allocate(3, 0);
+  public static final List<ReservedPort> EMBEDDED_JOURNAL_FAILOVER_METADATA_SYNC = allocate(3, 1);
   public static final List<ReservedPort> EMBEDDED_JOURNAL_SNAPSHOT_MASTER = allocate(3, 0);
   public static final List<ReservedPort> EMBEDDED_JOURNAL_SNAPSHOT_FOLLOWER = allocate(3, 0);
   public static final List<ReservedPort> EMBEDDED_JOURNAL_SNAPSHOT_TRANSFER_LOAD = allocate(3, 0);

tests/src/test/java/alluxio/server/ft/FileSystemMasterFaultToleranceIntegrationTest.java
@@ -11,11 +11,13 @@
 
 package alluxio.server.ft;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import alluxio.AlluxioURI;
 import alluxio.AuthenticatedUserRule;
 import alluxio.Constants;
+import alluxio.client.file.FileSystem;
 import alluxio.conf.Configuration;
 import alluxio.conf.PropertyKey;
 import alluxio.exception.FileAlreadyCompletedException;
@@ -25,6 +27,7 @@ import alluxio.grpc.CompleteFilePOptions;
 import alluxio.grpc.CreateFilePOptions;
 import alluxio.grpc.DeletePOptions;
 import alluxio.grpc.FileSystemMasterCommonPOptions;
+import alluxio.grpc.MountPOptions;
 import alluxio.grpc.RenamePOptions;
 import alluxio.master.MultiMasterLocalAlluxioCluster;
 import alluxio.master.file.FileSystemMaster;
@@ -35,19 +38,25 @@ import alluxio.master.file.contexts.DeleteContext;
 import alluxio.master.file.contexts.RenameContext;
 import alluxio.testutils.BaseIntegrationTest;
 import alluxio.testutils.IntegrationTestUtils;
+import alluxio.util.io.PathUtils;
 import alluxio.wire.FileInfo;
 import alluxio.wire.OperationId;
 
 import com.google.common.collect.ImmutableMap;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.rules.TestName;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.FileWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
@@ -59,6 +68,9 @@ public final class FileSystemMasterFaultToleranceIntegrationTest extends BaseInt
   private static final String TEST_USER = "test";
 
   private MultiMasterLocalAlluxioCluster mMultiMasterLocalAlluxioCluster;
+
+  @Rule
+  public TemporaryFolder mFolder = new TemporaryFolder();
 
   @Rule
   public TestName mTestName = new TestName();
@@ -84,7 +96,7 @@ public final class FileSystemMasterFaultToleranceIntegrationTest extends BaseInt
 
   @Before
   public final void before() throws Exception {
-    mMultiMasterLocalAlluxioCluster = new MultiMasterLocalAlluxioCluster(2, 0);
+    mMultiMasterLocalAlluxioCluster = new MultiMasterLocalAlluxioCluster(2, 1);
     mMultiMasterLocalAlluxioCluster.initConfiguration(
         IntegrationTestUtils.getTestName(getClass().getSimpleName(), mTestName.getMethodName()));
     Configuration.set(PropertyKey.USER_RPC_RETRY_MAX_DURATION, "60sec");
@@ -103,6 +115,43 @@ public final class FileSystemMasterFaultToleranceIntegrationTest extends BaseInt
   public final void after() throws Exception {
     mMultiMasterLocalAlluxioCluster.stop();
     Configuration.reloadProperties();
+  }
+
+  @Test
+  public void syncMetadataUFSFailOver() throws Exception {
+    String ufsPath = mFolder.newFolder().getAbsoluteFile().toString();
+    String ufsUri = "file://" + ufsPath;
+    MountPOptions options = MountPOptions.newBuilder().build();
+    FileSystem client = mMultiMasterLocalAlluxioCluster.getClient();
+    AlluxioURI mountPath = new AlluxioURI("/mnt1");
+    client.mount(mountPath, new AlluxioURI(ufsUri), options);
+
+    // create files outside alluxio
+    String fileName = "someFile";
+    String contents = "contents";
+    for (int i = 0; i < 100; i++) {
+      try (FileWriter fw = new FileWriter(Paths.get(PathUtils.concatPath(
+          ufsPath, fileName + i)).toString())) {
+        fw.write(contents + i);
+      }
+    }
+    for (int i = 0; i < 100; i++) {
+      // sync it with metadata sync
+      assertEquals(contents + i, IOUtils.toString(client.openFile(
+          mountPath.join(fileName + i)), Charset.defaultCharset()));
+    }
+
+    // Promote standby to be a leader and reset test state.
+    mMultiMasterLocalAlluxioCluster.stopLeader();
+    mMultiMasterLocalAlluxioCluster.waitForNewMaster(CLUSTER_WAIT_TIMEOUT_MS);
+    mMultiMasterLocalAlluxioCluster.waitForWorkersRegistered(CLUSTER_WAIT_TIMEOUT_MS);
+
+    // read the files again
+    client = mMultiMasterLocalAlluxioCluster.getClient();
+    for (int i = 0; i < 100; i++) {
+      assertEquals(contents + i, IOUtils.toString(client.openFile(
+          mountPath.join(fileName + i)), Charset.defaultCharset()));
+    }
   }
 
   @Test

tests/src/test/java/alluxio/server/ft/journal/raft/EmbeddedJournalIntegrationTestFaultTolerance.java
@@ -21,6 +21,7 @@ import alluxio.client.file.FileSystem;
 import alluxio.conf.PropertyKey;
 import alluxio.exception.FileAlreadyExistsException;
 import alluxio.exception.FileDoesNotExistException;
+import alluxio.grpc.MountPOptions;
 import alluxio.master.journal.JournalType;
 import alluxio.master.journal.raft.RaftJournalSystem;
 import alluxio.master.journal.raft.RaftJournalUtils;
@@ -28,17 +29,23 @@ import alluxio.multi.process.MultiProcessCluster;
 import alluxio.multi.process.PortCoordination;
 import alluxio.util.CommonUtils;
 import alluxio.util.WaitForOptions;
+import alluxio.util.io.PathUtils;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.ratis.server.RaftServerConfigKeys;
 import org.apache.ratis.server.storage.RaftStorage;
 import org.apache.ratis.server.storage.StorageImplUtils;
 import org.apache.ratis.statemachine.impl.SimpleStateMachineStorage;
 import org.apache.ratis.statemachine.impl.SingleFileSnapshotInfo;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -55,6 +62,9 @@ public class EmbeddedJournalIntegrationTestFaultTolerance
   private static final int RESTART_TIMEOUT_MS = 6 * Constants.MINUTE_MS;
   private static final int NUM_MASTERS = 3;
   private static final int NUM_WORKERS = 0;
+
+  @Rule
+  public TemporaryFolder mFolder = new TemporaryFolder();
 
   @Test
   public void failover() throws Exception {
@@ -75,6 +85,58 @@ public class EmbeddedJournalIntegrationTestFaultTolerance
     mCluster.waitForAndKillPrimaryMaster(MASTER_INDEX_WAIT_TIME);
     assertTrue(fs.exists(testDir));
     mCluster.notifySuccess();
+  }
+
+  @Test
+  public void syncMetadataEJFailOver() throws Exception {
+    mCluster = MultiProcessCluster.newBuilder(
+        PortCoordination.EMBEDDED_JOURNAL_FAILOVER_METADATA_SYNC)
+        .setClusterName("EmbeddedJournalFaultTolerance_syncMetadataFailOver")
+        .setNumMasters(NUM_MASTERS)
+        .setNumWorkers(1)
+        .addProperty(PropertyKey.MASTER_JOURNAL_TYPE, JournalType.EMBEDDED)
+        .addProperty(PropertyKey.MASTER_JOURNAL_FLUSH_TIMEOUT_MS, "5min")
+        .addProperty(PropertyKey.MASTER_JOURNAL_CHECKPOINT_PERIOD_ENTRIES, 1000)
+        .addProperty(PropertyKey.MASTER_JOURNAL_LOG_SIZE_BYTES_MAX, "50KB")
+        .addProperty(PropertyKey.MASTER_EMBEDDED_JOURNAL_MIN_ELECTION_TIMEOUT, "3s")
+        .addProperty(PropertyKey.MASTER_EMBEDDED_JOURNAL_MAX_ELECTION_TIMEOUT, "6s")
+        .addProperty(PropertyKey.MASTER_STANDBY_HEARTBEAT_INTERVAL, "5s")
+        .build();
+    mCluster.start();
+    mCluster.waitForAllNodesRegistered(30_000);
+    String ufsPath = mFolder.newFolder().getAbsoluteFile().toString();
+    String ufsUri = "file://" + ufsPath;
+    MountPOptions options = MountPOptions.newBuilder().build();
+    FileSystem client = mCluster.getFileSystemClient();
+    AlluxioURI mountPath = new AlluxioURI("/mnt1");
+    client.mount(mountPath, new AlluxioURI(ufsUri), options);
+
+    // create files outside alluxio
+    String fileName = "someFile";
+    String contents = "contents";
+    for (int i = 0; i < 100; i++) {
+      try (FileWriter fw = new FileWriter(Paths.get(
+          PathUtils.concatPath(ufsPath, fileName + i)).toString())) {
+        fw.write(contents + i);
+      }
+    }
+    // sync then with metadata sync
+    for (int i = 0; i < 100; i++) {
+      assertEquals(contents + i, IOUtils.toString(client.openFile(
+          mountPath.join(fileName + i)), Charset.defaultCharset()));
+    }
+
+    // restart the cluster
+    mCluster.stopMasters();
+    mCluster.startMasters();
+    mCluster.waitForAllNodesRegistered(30_000);
+
+    // read the files again
+    client = mCluster.getFileSystemClient();
+    for (int i = 0; i < 100; i++) {
+      assertEquals(contents + i, IOUtils.toString(client.openFile(
+          mountPath.join(fileName + i)), Charset.defaultCharset()));
+    }
   }
 
   @Test