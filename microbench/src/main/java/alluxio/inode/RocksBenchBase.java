0615305c719a4bd02e2484c7fd248973a021f0d2
jiacheliu3 <jiacheliu3@gmail.com>
2023-03-22 16:39:57 +0800

    mumble over a usage only used by the test

--- microbench/src/main/java/alluxio/inode/RocksBenchBase.java
+++ microbench/src/main/java/alluxio/inode/RocksBenchBase.java
@@ -43,10 +43,11 @@ public class RocksBenchBase {
   static final String SER_NO_ALLOC_READ = "serNoAllocRead";
   static final String NO_SER_NO_ALLOC_READ = "noSerNoAllocRead";
 
-  // TODO(jiacheng): close these objects properly
   private final RocksInodeStore mRocksInodeStore;
+  // RocksDB resources managed by the RocksInodeStore, no need to close manually
   private final RocksDB mDB;
   private final AtomicReference<ColumnFamilyHandle> mInodesColumn;
+  // Created and managed in this class
   private final WriteOptions mDisableWAL;
 
   RocksBenchBase(String confType) throws IOException {
@@ -74,6 +75,7 @@ public class RocksBenchBase {
   void after() {
     mRocksInodeStore.clear();
     mRocksInodeStore.close();
+    mDisableWAL.close();
   }
 
   long getInodeReadId(long total, long nxt, long min, int threadCount, int threadId) {
