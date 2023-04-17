651c76893a33c8d736e542a80f4332e05780a67e
maobaolong <307499405@qq.com>
2023-04-17 17:13:28 +0800

    Fix findbugs issue
    Fix build issue
    
    Refactor native library loader

--- integration/jnifuse/fs/src/main/java/alluxio/jnifuse/LibFuse.java
+++ integration/jnifuse/fs/src/main/java/alluxio/jnifuse/LibFuse.java
@@ -16,49 +16,49 @@ import alluxio.jnifuse.utils.NativeLibraryLoader;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class LibFuse {
-
-  private enum LibraryState {
-    NOT_LOADED,
-    LOADING,
-    LOADED
-  }
-
-  private static AtomicReference<LibraryState> libraryLoaded =
-      new AtomicReference<>(LibraryState.NOT_LOADED);
 
   public native int fuse_main_real(AbstractFuseFileSystem fs, int argc, String[] argv);
 
   public native ByteBuffer fuse_get_context();
 
+  private static LibfuseVersion LIB_FUSE_VERSION;
+
   public static void loadLibrary(LibfuseVersion version) {
-    if (libraryLoaded.get() == LibraryState.LOADED) {
-      return;
+    String tmpDir = System.getenv("JNIFUSE_SHAREDLIB_DIR");
+    String libName = getJniLibName(version);
+    try {
+      NativeLibraryLoader.getInstance().loadLibrary(libName, tmpDir);
+      LIB_FUSE_VERSION = version;
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to load the jni-fuse shared library"
+          + e);
     }
 
-    if (libraryLoaded.compareAndSet(LibraryState.NOT_LOADED,
-        LibraryState.LOADING)) {
-      String tmpDir = System.getenv("JNIFUSE_SHAREDLIB_DIR");
-      try {
-        NativeLibraryLoader.getInstance().loadLibrary(version, tmpDir);
-      } catch (IOException e) {
-        libraryLoaded.set(LibraryState.NOT_LOADED);
-        throw new RuntimeException("Unable to load the jni-fuse shared library"
-            + e);
-      }
-
-      libraryLoaded.set(LibraryState.LOADED);
-      return;
-    }
-
-    while (libraryLoaded.get() == LibraryState.LOADING) {
+    while (NativeLibraryLoader.getLoadState() == NativeLibraryLoader.LoadState.LOADING) {
       try {
         Thread.sleep(10);
       } catch (InterruptedException e) {
         //ignore
       }
     }
+  }
+
+  private static String getJniLibName(
+      final LibfuseVersion version) {
+    switch (version) {
+      case VERSION_2:
+        return "jnifuse";
+      case VERSION_3:
+        return "jnifuse3";
+      default:
+        // should not fall here
+        throw new RuntimeException(String.format("Unsupported libfuse version %d", version));
+    }
+  }
+
+  public static LibfuseVersion getLibFuseVersion() {
+    return LIB_FUSE_VERSION;
   }
 }
