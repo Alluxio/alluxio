aa2ccc1b3f78a51e52f56bf87a10a74b56854795
falin luo <65234479+MEILIDEKCL@users.noreply.github.com>
2023-05-12 10:46:01 +0800

    Update InstancedConfigurationTest.java
    add ut for MASTER_TTL_CHECKER_INTERVAL_MS

--- core/common/src/test/java/alluxio/conf/InstancedConfigurationTest.java
+++ core/common/src/test/java/alluxio/conf/InstancedConfigurationTest.java
@@ -675,6 +675,12 @@ public class InstancedConfigurationTest {
   public void shortMasterHeartBeatTimeout() {
     mConfiguration.set(PropertyKey.MASTER_STANDBY_HEARTBEAT_INTERVAL, "5min");
     mConfiguration.set(PropertyKey.MASTER_HEARTBEAT_TIMEOUT, "4min");
+    mThrown.expect(IllegalStateException.class);
+    mConfiguration.validate();
+  }
+  
+  @Test
+  public void setMasterTTLIntervalZero() {
     mConfiguration.set(PropertyKey.MASTER_TTL_CHECKER_INTERVAL_MS, "0");
     mThrown.expect(IllegalStateException.class);
     mConfiguration.validate();
