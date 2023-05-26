9d2f9cfc3ad94b9e8cc3a9db0a97f171c13c4699
maobaolong <307499405@qq.com>
2023-05-27 06:03:20 +0800

    Cleanup HADOOP1 support
    As some people are confused about why we encountered some build error while running alluxio tests.
    
    I will suggest them to remove the HADOOP1 related code to pass the build.
    			pr-link: Alluxio/alluxio#17469
    			change-id: cid-1dde372d4a0aa0604b44ddbbe90762293f31c20c

--- underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileOutputStream.java
+++ underfs/hdfs/src/main/java/alluxio/underfs/hdfs/HdfsUnderFileOutputStream.java
@@ -58,13 +58,9 @@ public class HdfsUnderFileOutputStream extends OutputStream implements ContentHa
   @Override
   public void flush() throws IOException {
     // TODO(calvin): This functionality should be restricted to select output streams.
-    //#ifdef HADOOP1
-    mOut.sync();
-    //#else
     // Note that, hsync() flushes out the data in client's user buffer all the way to the disk
     // device which may result in much slower performance than sync().
     mOut.hsync();
-    //#endif
   }
 
   @Override
