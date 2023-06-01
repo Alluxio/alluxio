147e9a2602ea193f344f5bdf4d319ef17fa9836c
JiamingMai <jiamingmai@163.com>
2023-06-01 19:30:09 +0800

    fix FuseFileSystemDataTest by enabling netty

--- dora/core/client/fs/src/main/java/alluxio/client/file/DoraFileOutStream.java
+++ dora/core/client/fs/src/main/java/alluxio/client/file/DoraFileOutStream.java
@@ -144,20 +144,6 @@ public class DoraFileOutStream extends FileOutStream {
         //Ignore
       }
 
-      try {
-        if (mAlluxioStorageType.isStore()) {
-          if (mCanceled) {
-            mNettyDataWriter.cancel();
-          } else {
-            // Note, this is a workaround to prevent commit(blockN-1) and write(blockN)
-            // race, in worse case, this may result in commit(blockN-1) completes earlier than
-            // write(blockN), and blockN evicts the committed blockN-1 and causing file lost.
-          }
-        }
-      } catch (Exception e) {
-        //Ignore
-      }
-
       CompleteFilePOptions options = CompleteFilePOptions.newBuilder()
           .setUfsLength(mNettyDataWriter.pos())
           .setCommonOptions(FileSystemMasterCommonPOptions.newBuilder().build())
