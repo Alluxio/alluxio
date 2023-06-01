026a81a771d370d778e3f62d3b8c200655200514
Jason <tieujason330@gmail.com>
2023-06-01 14:18:06 -0700

    make docker.yml path configurable

--- dev/scripts/src/alluxio.org/build/cmd/docker.go
+++ dev/scripts/src/alluxio.org/build/cmd/docker.go
@@ -28,7 +28,7 @@ import (
 )
 
 const (
-	dockerYmlFile = "src/alluxio.org/build/docker.yml"
+	defaultDockerYmlFilePath = "src/alluxio.org/build/docker.yml"
 
 	tempAlluxioTarballName        = "alluxio-tmp.tar.gz"
 	tempAlluxioTarballPlaceholder = "${ALLUXIO_TEMP_TARBALL}"
@@ -48,41 +48,42 @@ type DockerImage struct {
 type dockerBuildOpts struct {
 	*buildOpts
 
-	image       string
-	tarballPath string
-}
-
-var (
-	dockerImages = map[string]*DockerImage{}
-)
-
-func init() {
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(stacktrace.Propagate(err, "error getting current working directory"))
-	}
-	dockerYmlPath := filepath.Join(wd, dockerYmlFile)
-	content, err := ioutil.ReadFile(dockerYmlPath)
-	if err != nil {
-		panic(stacktrace.Propagate(err, "error reading file at %v", dockerYmlPath))
-	}
-	if err := yaml.Unmarshal(content, &dockerImages); err != nil {
-		panic(stacktrace.Propagate(err, "error unmarshalling docker images from:\n%v", string(content)))
-	}
+	dockerImages  map[string]*DockerImage
+	dockerYmlFile string
+	image         string
+	tarballPath   string
 }
 
 func DockerF(args []string) error {
 	cmd := flag.NewFlagSet(Docker, flag.ExitOnError)
 	opts := &dockerBuildOpts{}
 	// docker flags
-	cmd.StringVar(&opts.image, "image", "", fmt.Sprintf("Choose the docker image to build. See available images at %v", dockerYmlFile))
+	cmd.StringVar(&opts.dockerYmlFile, "dockerYmlFile", defaultDockerYmlFilePath, "Path to docker.yml file")
+	cmd.StringVar(&opts.image, "image", "", "Choose the docker image to build. See available images in docker.yml")
 	cmd.StringVar(&opts.tarballPath, "tarballPath", "", "Set to use existing tarball for building docker image")
 	// parse flags
 	tOpts, err := parseTarballFlags(cmd, args)
 	if err != nil {
 		return stacktrace.Propagate(err, "error parsing build flags")
 	}
-	image, ok := dockerImages[opts.image]
+
+	// parse available docker images in docker.yml
+	{
+		wd, err := os.Getwd()
+		if err != nil {
+			return stacktrace.Propagate(err, "error getting current working directory")
+		}
+		dockerYmlPath := filepath.Join(wd, opts.dockerYmlFile)
+		content, err := ioutil.ReadFile(dockerYmlPath)
+		if err != nil {
+			return stacktrace.Propagate(err, "error reading file at %v", dockerYmlPath)
+		}
+		if err := yaml.Unmarshal(content, &opts.dockerImages); err != nil {
+			return stacktrace.Propagate(err, "error unmarshalling docker images from:\n%v", string(content))
+		}
+	}
+
+	image, ok := opts.dockerImages[opts.image]
 	if !ok {
 		return stacktrace.NewError("must provide valid 'image' arg")
 	}
@@ -95,7 +96,7 @@ func DockerF(args []string) error {
 		artifact, err := artifact.NewArtifact(
 			artifact.DockerArtifact,
 			opts.outputDir,
-			strings.ReplaceAll(dockerImages[opts.image].TargetName, versionPlaceholder, alluxioVersion),
+			strings.ReplaceAll(opts.dockerImages[opts.image].TargetName, versionPlaceholder, alluxioVersion),
 			alluxioVersion,
 			map[string]string{"image": opts.image})
 		if err != nil {
@@ -122,24 +123,28 @@ func DockerF(args []string) error {
 	}
 	defer os.RemoveAll(filepath.Join(dockerWs, tempAlluxioTarballName))
 
-	if err := image.build(alluxioVersion, opts.outputDir, true); err != nil {
+	if err := image.build(opts, alluxioVersion, true); err != nil {
 		return stacktrace.Propagate(err, "error building image %v", opts.image)
 	}
 
 	return nil
 }
 
-func (i *DockerImage) build(alluxioVersion, outputDir string, save bool) error {
+func (i *DockerImage) build(opts *dockerBuildOpts, alluxioVersion string, save bool) error {
 	dockerWs := filepath.Join(findRepoRoot(), i.BuildDir)
 	if i.Dependency != "" {
-		if err := dockerImages[i.Dependency].build(alluxioVersion, outputDir, false); err != nil {
+		dep, ok := opts.dockerImages[i.Dependency]
+		if !ok {
+			return stacktrace.NewError("%v not found in list of docker images", i.Dependency)
+		}
+		if err := dep.build(opts, alluxioVersion, false); err != nil {
 			return stacktrace.Propagate(err, "error building dep %v", i.Dependency)
 		}
 	}
 	i.Tag = strings.ReplaceAll(i.Tag, versionPlaceholder, alluxioVersion)
 	i.TargetName = strings.ReplaceAll(i.TargetName, versionPlaceholder, alluxioVersion)
 	i.outputTarball = fmt.Sprintf("%v/%v",
-		outputDir, strings.ReplaceAll(i.TargetName, versionPlaceholder, alluxioVersion))
+		opts.outputDir, strings.ReplaceAll(i.TargetName, versionPlaceholder, alluxioVersion))
 	var buildArgs []string
 	for _, a := range i.BuildArgs {
 		buildArgs = append(buildArgs, fmt.Sprintf("--build-arg %v",
