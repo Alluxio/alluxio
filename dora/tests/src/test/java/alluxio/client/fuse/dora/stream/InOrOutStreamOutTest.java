043116716ba107c05fdba0bc35db0ffb222517c3
wangyuyang <wyy87935@163.com>
2023-06-12 15:52:16 +0800

    Support permission check in delete, rename and createDirectory

--- dora/tests/src/test/java/alluxio/client/fuse/dora/stream/InOrOutStreamOutTest.java
+++ dora/tests/src/test/java/alluxio/client/fuse/dora/stream/InOrOutStreamOutTest.java
@@ -13,6 +13,7 @@ package alluxio.client.fuse.dora.stream;
 
 import alluxio.AlluxioURI;
 import alluxio.client.file.URIStatus;
+import alluxio.exception.FileDoesNotExistException;
 import alluxio.exception.runtime.UnimplementedRuntimeException;
 import alluxio.fuse.file.FuseFileStream;
 import alluxio.grpc.CreateDirectoryPOptions;
@@ -50,7 +51,7 @@ public class InOrOutStreamOutTest extends OutStreamTest {
     }
   }
 
-  @Test (expected = FileNotFoundException.class)
+  @Test (expected = FileDoesNotExistException.class)
   public void createEmpty() throws Exception {
     AlluxioURI alluxioURI = getTestFileUri();
     mFileSystem.createDirectory(alluxioURI.getParent(),
