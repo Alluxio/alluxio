eec62e7526e08f4601ed6e16f2abbdf03f17c407
JiamingMai <jiamingmai@163.com>
2023-06-14 16:16:49 +0800

    add HttpObjectAggregator for POST requests

--- dora/core/server/worker/src/main/java/alluxio/worker/http/HttpServerInitializer.java
+++ dora/core/server/worker/src/main/java/alluxio/worker/http/HttpServerInitializer.java
@@ -17,6 +17,7 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.compression.CompressionOptions;
 import io.netty.handler.codec.http.HttpContentCompressor;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
 
@@ -40,6 +41,7 @@ public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
   public void initChannel(SocketChannel ch) {
     ChannelPipeline p = ch.pipeline();
     p.addLast(new HttpServerCodec());
+    p.addLast(new HttpObjectAggregator(1024 * 10));
     p.addLast(new HttpContentCompressor((CompressionOptions[]) null));
     p.addLast(new HttpServerExpectContinueHandler());
     p.addLast(new HttpServerHandler(mPagedService));
