fb63adcb1e396c0aeccf63a1ec779d79ea501a96
jiacheliu3 <jiacheliu3@gmail.com>
2023-06-25 17:04:18 +0800

    checkstyle

--- dora/tests/src/test/java/alluxio/testutils/IntegrationTestUtils.java
+++ dora/tests/src/test/java/alluxio/testutils/IntegrationTestUtils.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 /**
