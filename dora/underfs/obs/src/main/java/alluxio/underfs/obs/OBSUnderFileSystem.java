e154243234a8c3ab32bd9052f921e14030515115
Zihao Zhao <zihao.zhao@alluxio.com>
2023-07-05 18:07:36 +0800

    Support position reader of OBS
    ### What changes are proposed in this pull request?
    
    Support position reader of OBS.
    
    ### Why are the changes needed?
    
    Alluxio couldn't read data in OBS. I have implemented OBSPositionReader and now it can `cat` files in OBS.
    
    Here is the screenshot about OBS's position reader.
    
    <img width="812" alt="截屏2023-06-27 22 15 24" src="https://github.com/Alluxio/alluxio/assets/57146148/c70f93be-b8d8-4ddb-b8ba-91003e7ccf9b">
    
    ### Does this PR introduce any user facing changes?
    
    No
    
    
    
    
    			pr-link: Alluxio/alluxio#17686
    			change-id: cid-0d1c75c997b3e98257119a248e2fda73240d4481

--- dora/underfs/obs/src/main/java/alluxio/underfs/obs/OBSUnderFileSystem.java
+++ dora/underfs/obs/src/main/java/alluxio/underfs/obs/OBSUnderFileSystem.java
@@ -15,7 +15,6 @@ import alluxio.AlluxioURI;
 import alluxio.Constants;
 import alluxio.PositionReader;
 import alluxio.conf.PropertyKey;
-import alluxio.exception.runtime.UnimplementedRuntimeException;
 import alluxio.retry.RetryPolicy;
 import alluxio.underfs.ObjectUnderFileSystem;
 import alluxio.underfs.UfsFileStatus;
@@ -160,6 +159,11 @@ public class OBSUnderFileSystem extends ObjectUnderFileSystem {
   @Override
   public String getUnderFSType() {
     return "obs";
+  }
+
+  @Override
+  public PositionReader openPositionRead(String path, long fileLength) {
+    return new OBSPositionReader(mClient, mBucketName, stripPrefixIfPresent(path), fileLength);
   }
 
   // No ACL integration currently, no-op
@@ -401,11 +405,6 @@ public class OBSUnderFileSystem extends ObjectUnderFileSystem {
       LOG.warn("Failed to get Object {}", pathKey, e);
       return false;
     }
-  }
-
-  @Override
-  public PositionReader openPositionRead(String path, long fileLength) {
-    throw new UnimplementedRuntimeException("Position read is not implemented");
   }
 
   // No ACL integration currently, returns default empty value
