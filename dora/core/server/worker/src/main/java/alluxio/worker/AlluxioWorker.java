29939b0306385835a883a57c0a1f38a8235a97a7
yyongycy <yong.sunny@163.com>
2023-07-31 10:07:30 +0800

    Remove the dependency of meta master

--- dora/core/server/worker/src/main/java/alluxio/worker/AlluxioWorker.java
+++ dora/core/server/worker/src/main/java/alluxio/worker/AlluxioWorker.java
@@ -14,18 +14,12 @@ package alluxio.worker;
 import alluxio.ProcessUtils;
 import alluxio.RuntimeConstants;
 import alluxio.conf.Configuration;
-import alluxio.grpc.Scope;
-import alluxio.master.MasterInquireClient;
-import alluxio.retry.RetryUtils;
-import alluxio.security.user.ServerUserState;
 import alluxio.util.CommonUtils;
 import alluxio.util.ConfigurationUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
