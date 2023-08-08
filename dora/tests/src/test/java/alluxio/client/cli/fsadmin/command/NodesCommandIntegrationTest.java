0d5062586baceaf0591ed8135f8268e516a7df2d
Lucy Ge <lucy.ge@alluxio.com>
2023-08-07 20:57:13 -0700

    checkstyle fixes

--- dora/tests/src/test/java/alluxio/client/cli/fsadmin/command/NodesCommandIntegrationTest.java
+++ dora/tests/src/test/java/alluxio/client/cli/fsadmin/command/NodesCommandIntegrationTest.java
@@ -4,16 +4,14 @@ import alluxio.client.cli.fsadmin.AbstractFsAdminShellTest;
 
 import org.junit.Assert;
 import org.junit.Test;
-import java.io.FileNotFoundException;
 
 public class NodesCommandIntegrationTest extends AbstractFsAdminShellTest {
   /* This test relies on the support of running master-less
    * alluxio mini cluster to start with for StaticMembershipManager
    * and EtcdMembershipManager. Currrently only add basic tests.
    */
-  
   @Test
-  public void testNoopMemberManager() throws FileNotFoundException {
+  public void testNoopMemberManager() {
     int ret = mFsAdminShell.run("nodes", "status");
     Assert.assertEquals(0, ret);
   }
