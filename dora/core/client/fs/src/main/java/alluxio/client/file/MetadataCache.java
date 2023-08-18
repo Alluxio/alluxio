b06b8d32903834383b40b43470dffa6b5966cfd6
YichuanSun <Yichuan_Sun@163.com>
2023-08-18 17:18:14 +0800

    add MetadataCache.invalidate(UfsUrl) method.

--- dora/core/client/fs/src/main/java/alluxio/client/file/MetadataCache.java
+++ dora/core/client/fs/src/main/java/alluxio/client/file/MetadataCache.java
@@ -12,6 +12,7 @@
 package alluxio.client.file;
 
 import alluxio.AlluxioURI;
+import alluxio.uri.UfsUrl;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.cache.Cache;
@@ -183,6 +184,11 @@ public final class MetadataCache {
    */
   public void invalidate(String path) {
     mCache.invalidate(path);
+  }
+
+  // TODO(Tony Sun): is it correct?
+  public void invalidate(UfsUrl ufsPath) {
+    mCache.invalidate(ufsPath.getFullPath());
   }
 
   /**
