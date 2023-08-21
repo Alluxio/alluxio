d385269d5e70501015ae9cf567afb39a93d34a5c
twalluxio <tongyu.wu@alluxio.com>
2023-08-21 18:01:12 +0800

    2nd edition of CopyDir command, use rsync

--- cli/src/alluxio.org/cli/cmd/initiate/copy_dir.go
+++ cli/src/alluxio.org/cli/cmd/initiate/copy_dir.go
@@ -13,20 +13,16 @@ package initiate
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
+	"os/exec"
+	"os/user"
 	"path/filepath"
-	"strconv"
 	"strings"
-	"sync"
-
-	"github.com/palantir/stacktrace"
-	"github.com/spf13/cobra"
-	"golang.org/x/crypto/ssh"
 
 	"alluxio.org/cli/processes"
 	"alluxio.org/log"
+	"github.com/palantir/stacktrace"
+	"github.com/spf13/cobra"
 )
 
 var CopyDir = &CopyDirCommand{}
@@ -39,147 +35,67 @@ func (c *CopyDirCommand) ToCommand() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		Short: "Copy a path to all master/worker nodes.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// prepare client config, ssh port info
-			config, port, err := processes.PrepareCommand()
-			if err != nil {
-				return stacktrace.Propagate(err, "prepare command failed")
-			}
-
 			// get list of masters or workers, or both
 			hosts, err := processes.GetHostnames([]string{processes.HostGroupMasters, processes.HostGroupWorkers})
 			if err != nil {
 				return stacktrace.Propagate(err, "cannot read host names")
 			}
 
+			// get name of current host, avoid rsync itself
 			myHost, err := os.Hostname()
 			if err != nil {
 				return stacktrace.Propagate(err, "cannot read local host name")
 			}
 
+			// determine the absolute directory to copy
 			absolutePath, err := filepath.Abs(args[0])
 			if err != nil {
-				return stacktrace.Propagate(err, "Invalid path to copy")
+				return stacktrace.Propagate(err, "cannot find absolute path")
 			}
 
-			var wg sync.WaitGroup
-			errs := make(chan error, len(hosts))
+			// hypothesize all hosts use the same username
+			cu, err := user.Current()
+			if err != nil {
+				return stacktrace.Propagate(err, "cannot find current user")
+			}
+
+			var errs []error
 			for _, host := range hosts {
-				if host != myHost {
-					wg.Add(1)
-					go func(host string) {
-						defer wg.Done()
-						err := copyDirectory(absolutePath, host, config, port, errs)
-						errs <- err
-					}(host)
+				if host == myHost {
+					continue
+				}
+				log.Logger.Infof("RSYNCing directory %s to %s", args[0], host)
+
+				// src path needs to end with a slash, but dst path needs to end without the slash
+				var srcDir, dstDir string
+				if strings.HasSuffix(absolutePath, "/") {
+					srcDir = absolutePath
+					dstDir = strings.TrimRight(absolutePath, "/")
+				} else {
+					srcDir = absolutePath + "/"
+					dstDir = absolutePath
+				}
+
+				cmd := exec.Command("rsync", "-az", srcDir,
+					fmt.Sprintf("%s@%s:%s", cu.Username, host, dstDir))
+				log.Logger.Infof("Command: %s", cmd)
+				if err := cmd.Run(); err != nil {
+					log.Logger.Error("Error: %s", err)
+					errs = append(errs, err)
 				}
 			}
 
-			go func() {
-				wg.Wait()
-				close(errs)
-			}()
-
-			var errMsgs []string
-			for err := range errs {
-				errMsgs = append(errMsgs, err.Error())
-			}
-			if len(errMsgs) > 0 {
+			if len(errs) > 0 {
+				var errMsgs []string
+				for _, err := range errs {
+					errMsgs = append(errMsgs, err.Error())
+				}
 				return stacktrace.NewError("At least one error encountered:\n%v", strings.Join(errMsgs, "\n"))
 			}
-			log.Logger.Infof("CopyDir successful on nodes: %s", hosts)
+
+			log.Logger.Infof("Copying path %s successful on nodes: %s", args[0], hosts)
 			return nil
 		},
 	}
 	return cmd
-}
-
-func copyDirectory(path, host string, config *ssh.ClientConfig, port int, errs chan<- error) error {
-	log.Logger.Infof("Path: %s", path)
-	conn, err := ssh.Dial("tcp", host+":"+strconv.Itoa(port), config)
-	if err != nil {
-		return err
-	}
-	defer func(conn *ssh.Client) {
-		if err := conn.Close(); err != nil {
-			log.Logger.Warnf("connection to %s closed, error: %s", host, err)
-		} else {
-			log.Logger.Debugf("connection to %s closed", host)
-		}
-	}(conn)
-
-	session, err := conn.NewSession()
-	if err != nil {
-		return stacktrace.Propagate(err, "cannot create session at %v.", host)
-	}
-	defer func(session *ssh.Session) {
-		if err := session.Close(); err != nil && err != io.EOF {
-			log.Logger.Warnf("session at %s closed, error: %s", host, err)
-		} else {
-			log.Logger.Debugf("session at %s closed", host)
-		}
-	}(session)
-
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-
-	entries, err := ioutil.ReadDir(path)
-	if err != nil {
-		return err
-	}
-
-	for _, entry := range entries {
-		filePath := filepath.Join(path, entry.Name())
-		if entry.IsDir() {
-			if err := copyDirectory(filePath, host, config, port, errs); err != nil {
-				return err
-			}
-		} else {
-			go func() {
-				err := copyFile(path, session)
-				errs <- err
-			}()
-		}
-	}
-
-	return nil
-}
-
-func copyFile(path string, session *ssh.Session) error {
-	localFile, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer func(localFile *os.File) {
-		err := localFile.Close()
-		if err != nil {
-			log.Logger.Warnf("Local file %s closed with error: %v", path, err)
-		} else {
-			log.Logger.Debugf("Local file %s closed.", path)
-		}
-	}(localFile)
-
-	remoteFile, err := session.StdinPipe()
-	if err != nil {
-		return err
-	}
-
-	errChan := make(chan error)
-	go func() {
-		defer func(remoteFile io.WriteCloser) {
-			err := remoteFile.Close()
-			if err != nil {
-				log.Logger.Warnf("Remote file %s closed with error: %v", path, err)
-			} else {
-				log.Logger.Debugf("Remote file %s closed.", path)
-			}
-		}(remoteFile)
-		_, err := io.Copy(remoteFile, localFile)
-		errChan <- err
-	}()
-
-	if err := session.Run(fmt.Sprintf("cat > %s", path)); err != nil {
-		return err
-	}
-
-	return <-errChan
 }
