316a73c7006914d9840603821980d62377289262
Huang Hua <huanghua78@msn.com>
2023-08-25 17:43:41 +0800

    choose a prefered worker from worker list
    Signed-off-by: Huang Hua <huanghua78@msn.com>

--- dora/core/common/src/main/java/alluxio/conf/PropertyKey.java
+++ dora/core/common/src/main/java/alluxio/conf/PropertyKey.java
@@ -5165,7 +5165,7 @@ public final class PropertyKey implements Comparable<PropertyKey> {
           .build();
   public static final PropertyKey USER_FILE_REPLICATION_MIN =
       intBuilder(Name.USER_FILE_REPLICATION_MIN)
-          .setDefaultValue(0)
+          .setDefaultValue(1)
           .setDescription("The target min replication level of a file in Alluxio space.")
           .setScope(Scope.CLIENT)
           .build();
