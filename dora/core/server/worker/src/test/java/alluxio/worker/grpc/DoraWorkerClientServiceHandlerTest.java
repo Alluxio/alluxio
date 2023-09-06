0dd9ac813ae7bdb2d5e605c22eaa8660c95389e3
thu-david <cwd22@mails.tsinghua.edu.cn>
2023-09-06 13:42:17 +0800

    add license

--- dora/core/server/worker/src/test/java/alluxio/worker/grpc/DoraWorkerClientServiceHandlerTest.java
+++ dora/core/server/worker/src/test/java/alluxio/worker/grpc/DoraWorkerClientServiceHandlerTest.java
@@ -1,3 +1,14 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
 package alluxio.worker.grpc;
 
 import static org.junit.Assert.assertEquals;
@@ -102,8 +113,8 @@ public class DoraWorkerClientServiceHandlerTest {
     mRequest = ListStatusPRequest.newBuilder().setOptions(
             alluxio.grpc.ListStatusPOptions.newBuilder().setRecursive(true).build())
         .setPath(nonExistentPath).build();
-    TestStreamObserver finalResponseObserver = responseObserver;
-    assertThrows(RuntimeException.class, () -> mServiceHandler.listStatus(mRequest, finalResponseObserver));
+    assertThrows(RuntimeException.class, () -> mServiceHandler.listStatus(mRequest,
+        responseObserver));
   }
 
   @Test
