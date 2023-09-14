3bbb93cd4002c85686aa87fbe3b806627c47d325
voddle <dengxinyu0105@gmail.com>
2023-09-14 17:36:45 +0800

    refactor

--- dora/core/server/worker/src/test/java/alluxio/worker/dora/DoraMetaManagerTest.java
+++ dora/core/server/worker/src/test/java/alluxio/worker/dora/DoraMetaManagerTest.java
@@ -9,7 +9,7 @@
  * See the NOTICE file distributed with this work for information regarding copyright ownership.
  */
 
-package alluxio.worker.netty;
+package alluxio.worker.dora;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
---
+++ dora/core/server/worker/src/test/java/alluxio/worker/netty/UfsFileWriteHandlerTest.java
@@ -0,0 +1,2 @@
+package alluxio.worker.netty;public class UfsFileWriteHandlerTest {
+}
