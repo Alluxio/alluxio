9205036b7773f84a962eab6b8c8ee6fa48483dca
Bowen Ding <6999708+dbw9580@users.noreply.github.com>
2023-09-26 01:49:16 +0800

    add rust toolchain to docker image and enabled integration tests

--- dev/github/run_tests.sh
+++ dev/github/run_tests.sh
@@ -38,7 +38,7 @@ PATH_BACKUP=${PATH}
 JAVA_HOME=/usr/local/openjdk-8
 PATH=$JAVA_HOME/bin:$PATH
 mvn -Duser.home=/home/jenkins -T 4C clean install -Dfindbugs.skip -Dcheckstyle.skip -DskipTests -Dmaven.javadoc.skip \
--Dlicense.skip -Dsort.skip ${mvn_args}
+-Dlicense.skip -Dsort.skip -Pnative-components ${mvn_args}
 
 # Set things up so that the current user has a real name and can authenticate.
 myuid=$(id -u)
@@ -56,4 +56,5 @@ fi
 
 # Run tests
 mvn -Duser.home=/home/jenkins test -Dmaven.main.skip -Dskip.protoc=true -Dmaven.javadoc.skip -Dlicense.skip=true \
--Dcheckstyle.skip=true -Dfindbugs.skip=true -Dsort.skip -Dsurefire.forkCount=${ALLUXIO_FORK_COUNT} ${mvn_args}
+-Dcheckstyle.skip=true -Dfindbugs.skip=true -Dsort.skip -Pnative-components -Dsurefire.forkCount=${ALLUXIO_FORK_COUNT} \
+${mvn_args}
