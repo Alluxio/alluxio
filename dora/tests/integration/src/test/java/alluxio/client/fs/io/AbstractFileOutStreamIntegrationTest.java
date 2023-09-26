dcdff966c0e2270d46c5554f3a629b307c5c4f0c
YichuanSun <Yichuan_Sun@163.com>
2023-09-26 14:53:18 +0800

    replace mPath with mRoot.

--- dora/tests/integration/src/test/java/alluxio/client/fs/io/AbstractFileOutStreamIntegrationTest.java
+++ dora/tests/integration/src/test/java/alluxio/client/fs/io/AbstractFileOutStreamIntegrationTest.java
@@ -34,8 +34,6 @@ import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Abstract classes for all integration tests of {@link FileOutStream}.
@@ -54,7 +52,7 @@ public abstract class AbstractFileOutStreamIntegrationTest extends BaseIntegrati
 
   protected FileSystem mFileSystem = null;
 
-  protected Path mPath;
+  protected String mRoot;
 
   @Rule
   public TemporaryFolder mTemporaryFolder = new TemporaryFolder();
@@ -62,8 +60,7 @@ public abstract class AbstractFileOutStreamIntegrationTest extends BaseIntegrati
   @Before
   public void before() throws Exception {
     mFileSystem = mLocalAlluxioClusterResource.get().getClient();
-    String localUfsRoot = mTemporaryFolder.getRoot().getAbsolutePath();
-    mPath = Paths.get(localUfsRoot, "testFile");
+    mRoot = mTemporaryFolder.getRoot().getAbsolutePath();
   }
 
   @After
