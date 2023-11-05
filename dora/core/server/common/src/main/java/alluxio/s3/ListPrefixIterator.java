d03df3ed245d8b9a6f51d22b778a1a111a2caf2e
humengyu <hellohumengyu@gmail.com>
2023-11-05 21:05:14 +0800

    Optimize listing objects for s3 api

---
+++ dora/core/server/common/src/main/java/alluxio/s3/ListPrefixIterator.java
@@ -0,0 +1,112 @@
+package alluxio.s3;
+
+import alluxio.AlluxioURI;
+import alluxio.client.file.URIStatus;
+import alluxio.exception.AlluxioException;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import javax.annotation.Nullable;
+
+/**
+ * An iterator for listing a given path, and its working mode is lazy loading.
+ */
+public class ListPrefixIterator implements Iterator<URIStatus> {
+
+  private static final URIStatus[] EMPTY_ARRAY = new URIStatus[0];
+
+  private final AlluxioURI mPath;
+  private final ChildrenSupplier mChildrenSupplier;
+  private final String mPrefix;
+  private final Deque<URIStatus> mStack;
+  private URIStatus[] mChildren;
+  private int mIndex;
+
+  /**
+   *
+   * @param path the path to list status
+   * @param childrenSupplier the method is used to list status
+   * @param prefix the prefix to filter
+   */
+  public ListPrefixIterator(AlluxioURI path, ChildrenSupplier childrenSupplier,
+      @Nullable String prefix) {
+    mPath = path;
+    mChildrenSupplier = childrenSupplier;
+    mPrefix = prefix == null ? "" : prefix;
+    mStack = new ArrayDeque<>();
+    mChildren = listSortedChildren(mPath);
+  }
+
+  @Override
+  public boolean hasNext() {
+    return !mStack.isEmpty() || mIndex < mChildren.length;
+  }
+
+  @Override
+  public URIStatus next() {
+    if (mIndex < mChildren.length) {
+      URIStatus status = mChildren[mIndex];
+      if (status.isFolder()) {
+        for (int i = mChildren.length - 1; i > mIndex; i--) {
+          mStack.push(mChildren[i]);
+        }
+        mIndex = 0;
+        mChildren = listSortedChildren(new AlluxioURI(status.getPath()));
+      } else {
+        mIndex++;
+      }
+      return status;
+    }
+    URIStatus status = mStack.pop();
+    mIndex = 0;
+    mChildren = EMPTY_ARRAY;
+    if (status.isFolder()) {
+      mChildren = listSortedChildren(new AlluxioURI(status.getPath()));
+    }
+    return status;
+  }
+
+  private URIStatus[] listSortedChildren(AlluxioURI parent) {
+    try {
+      return mChildrenSupplier.getChildren(parent).stream()
+          .filter(status -> status.getPath().startsWith(mPrefix))
+          .sorted(Comparator.comparing(URIStatus::getPath))
+          .toArray(URIStatus[]::new);
+    } catch (IOException | AlluxioException e) {
+      throw new ListPrefixException(e);
+    }
+  }
+
+  /**
+   * A function interface for listing status.
+   */
+  public interface ChildrenSupplier {
+
+    /**
+     *
+     * @param path
+     * @return Children of given parent path
+     * @throws IOException
+     * @throws AlluxioException
+     */
+    List<URIStatus> getChildren(AlluxioURI path) throws IOException, AlluxioException;
+  }
+
+  /**
+   * An RuntimeException to wrap Exception.
+   */
+  public static class ListPrefixException extends RuntimeException {
+
+    /**
+     *
+     * @param cause
+     */
+    public ListPrefixException(Exception cause) {
+      super(cause);
+    }
+  }
+}
---
+++ dora/core/server/proxy/src/test/java/alluxio/proxy/s3/ListPrefixIteratorTest.java
@@ -0,0 +1,129 @@
+package alluxio.proxy.s3;
+
+import alluxio.AlluxioURI;
+import alluxio.client.file.URIStatus;
+import alluxio.s3.ListPrefixIterator;
+import alluxio.s3.ListPrefixIterator.ChildrenSupplier;
+import alluxio.wire.FileInfo;
+import com.google.common.collect.Streams;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ListPrefixIteratorTest {
+
+  @Rule
+  public final TemporaryFolder mTestFolder = new TemporaryFolder();
+
+  private final Random mRandom = new Random();
+  private final char[] mChars = new char[]{'a', 'b', 'c'};
+  private final ChildrenSupplier mLocalFileChildrenSupplier = alluxioURI -> {
+    File[] files = new File(alluxioURI.getPath()).listFiles();
+    if (files == null) {
+      return Collections.emptyList();
+    } else {
+      return Arrays.stream(files).map(this::fromFile).collect(Collectors.toList());
+    }
+  };
+
+  private String getRandomFileName() {
+    int length = mRandom.nextInt(3) + 1;
+    StringBuilder name = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      name.append(mChars[mRandom.nextInt(mChars.length)]);
+    }
+    return name.toString();
+  }
+
+  private String getRandomFilePath(int depths) {
+    return IntStream.range(0, depths)
+        .mapToObj(i -> getRandomFileName())
+        .collect(Collectors.joining("/"));
+  }
+
+  public List<String> createRandomTmpFile(String root) throws IOException {
+    // clear
+    mTestFolder.delete();
+    // create 100 dirs
+    int depths = mRandom.nextInt(3) + 1;
+    Set<String> files = new HashSet<>();
+    for (int i = 0; i < 100; i++) {
+      files.add(getRandomFilePath(depths));
+    }
+    return files.stream().sorted(Comparator.naturalOrder()).map(s -> {
+          try {
+            return mTestFolder.newFolder(root + File.separator + s).getPath();
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        })
+        .collect(Collectors.toList());
+  }
+
+  @Test
+  public void testUriStatusIteratorWithoutPrefix() throws IOException {
+    for (int i = 0; i < 100; i++) {
+      String rootDir = "root" + i;
+      createRandomTmpFile(rootDir);
+      AlluxioURI path = new AlluxioURI(mTestFolder.getRoot().getPath() + File.separator + rootDir);
+      ListPrefixIterator listPrefixIterator = new ListPrefixIterator(path, mLocalFileChildrenSupplier,
+          null);
+      List<String> actual = Streams.stream(listPrefixIterator).map(URIStatus::getPath)
+          .collect(Collectors.toList());
+      List<String> expect = listLocalDir(
+          new AlluxioURI(mTestFolder.getRoot().getPath() + File.separator + rootDir));
+      Assert.assertEquals(expect, actual);
+    }
+  }
+
+  @Test
+  public void testUriStatusIteratorWithPrefix() throws IOException {
+    for (int i = 0; i < 100; i++) {
+      String rootDir = "root" + i;
+      createRandomTmpFile(rootDir);
+      AlluxioURI path = new AlluxioURI(mTestFolder.getRoot().getPath() + File.separator + rootDir);
+      List<String> local = listLocalDir(
+          new AlluxioURI(mTestFolder.getRoot().getPath() + File.separator + rootDir));
+      // test 100 times: random select prefix
+      for (int j = 0; j < 100; j++) {
+        String prefix = local.get(mRandom.nextInt(local.size()));
+        ListPrefixIterator listPrefixIterator = new ListPrefixIterator(path,
+            mLocalFileChildrenSupplier, prefix);
+        List<String> actual = Streams.stream(listPrefixIterator).map(URIStatus::getPath)
+            .collect(Collectors.toList());
+        List<String> expect = local.stream().filter(s -> s.startsWith(prefix))
+            .collect(Collectors.toList());
+        Assert.assertEquals(expect, actual);
+      }
+    }
+  }
+
+  private URIStatus fromFile(File file) {
+    return new URIStatus(new FileInfo().setPath(file.getPath()).setFolder(file.isDirectory()));
+  }
+
+  private List<String> listLocalDir(AlluxioURI path) throws IOException {
+    return Files.walk(new File(path.getPath()).toPath(),
+            FileVisitOption.FOLLOW_LINKS)
+        .map(Path::toString)
+        .sorted(Comparator.naturalOrder())
+        .skip(1)
+        .collect(Collectors.toList());
+  }
+
+}
