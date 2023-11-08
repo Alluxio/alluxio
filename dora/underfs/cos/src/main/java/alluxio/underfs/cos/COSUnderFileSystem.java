78c457cc8d0aaa6da1ee5fe23c76318a2d8fc44d
wangyuyang <wyy87935@163.com>
2023-11-08 19:47:05 +0800

    fix the NPE in COSUnderFileSystem

--- dora/underfs/cos/src/main/java/alluxio/underfs/cos/COSUnderFileSystem.java
+++ dora/underfs/cos/src/main/java/alluxio/underfs/cos/COSUnderFileSystem.java
@@ -54,6 +54,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -356,8 +357,9 @@ public class COSUnderFileSystem extends ObjectUnderFileSystem {
       if (meta == null) {
         return null;
       }
+      Date lastModifiedDate = meta.getLastModified();
       return new ObjectStatus(key, meta.getETag(), meta.getContentLength(),
-          meta.getLastModified().getTime());
+          lastModifiedDate != null ? lastModifiedDate.getTime() : null);
     } catch (CosClientException e) {
       return null;
     }
