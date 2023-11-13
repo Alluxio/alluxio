cb75d1bc589d41c6ad419ae6bdc8cd418a80c4a4
wangyuyang <wyy87935@163.com>
2023-11-13 16:47:34 +0800

    Fix the issue when mount an empty oss bucket

--- dora/underfs/oss/src/main/java/alluxio/underfs/oss/OSSUnderFileSystem.java
+++ dora/underfs/oss/src/main/java/alluxio/underfs/oss/OSSUnderFileSystem.java
@@ -395,6 +395,10 @@ public class OSSUnderFileSystem extends ObjectUnderFileSystem {
   @Override
   protected ObjectStatus getObjectStatus(String key) {
     try {
+      if (isRoot(key)) {
+        // return a virtual root object
+        return new ObjectStatus(key, null, 0, null);
+      }
       ObjectMetadata meta = mClient.getObjectMetadata(mBucketName, key);
       if (meta == null) {
         return null;
