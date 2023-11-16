709f2ad59682d80b1b4822b3e0f588acd096e35b
pkuweblab <pkuweblab@163.com>
2023-11-16 14:55:59 +0800

    Fix the NPE in COSUnderFileSystem

--- underfs/cos/src/main/java/alluxio/underfs/cos/COSUnderFileSystem.java
+++ underfs/cos/src/main/java/alluxio/underfs/cos/COSUnderFileSystem.java
@@ -42,6 +42,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.concurrent.ThreadSafe;
@@ -285,8 +286,9 @@ public class COSUnderFileSystem extends ObjectUnderFileSystem {
       if (meta == null) {
         return null;
       }
+      Date lastModifiedDate = meta.getLastModified();
       return new ObjectStatus(key, meta.getETag(), meta.getContentLength(),
-          meta.getLastModified().getTime());
+              lastModifiedDate != null ? lastModifiedDate.getTime() : null);
     } catch (CosClientException e) {
       return null;
     }
