90a25e2c79e99073ec6ca5467a90f1e9cf91b845
Bowen Ding <6999708+dbw9580@users.noreply.github.com>
2023-11-21 19:20:44 +0800

    ignore test: list sort by access time

--- dora/tests/integration/src/test/java/alluxio/client/cli/fs/command/DoraLsCommandIntegrationTest.java
+++ dora/tests/integration/src/test/java/alluxio/client/cli/fs/command/DoraLsCommandIntegrationTest.java
@@ -15,12 +15,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import alluxio.Constants;
+import alluxio.annotation.dora.DoraTestTodoItem;
 import alluxio.client.cli.fs.AbstractDoraFileSystemShellTest;
 import alluxio.conf.PropertyKey;
 import alluxio.exception.AlluxioException;
 import alluxio.grpc.WritePType;
 import alluxio.util.io.BufferUtils;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -134,6 +136,9 @@ public class DoraLsCommandIntegrationTest extends AbstractDoraFileSystemShellTes
     );
   }
 
+  @DoraTestTodoItem(owner = "Hua", action = DoraTestTodoItem.Action.REMOVE,
+      comment = "currently access time is not recorded and updated")
+  @Ignore
   @Test
   public void testLsWithSortByAccessTime() throws IOException, AlluxioException {
     String oldFile = "/testRoot/oldFile";
@@ -180,7 +185,7 @@ public class DoraLsCommandIntegrationTest extends AbstractDoraFileSystemShellTes
   }
 
   @Test
-  public void testLsWithSortByLastModificationTimeTime() throws IOException, AlluxioException {
+  public void testLsWithSortByLastModificationTime() throws IOException, AlluxioException {
     String oldFile = "/testRoot/oldFile";
     String newFile = "/testRoot/newFile";
     createByteFileInAlluxio(oldFile, BufferUtils.getIncreasingByteArray(Constants.MB),
