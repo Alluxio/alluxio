6bbe3e8726bd3431f90033418576926285a41761
Bowen Ding <6999708+dbw9580@users.noreply.github.com>
2023-12-06 11:09:54 +0800

    add copy constructors to WorkerInfo and WorkerNetAddress

--- dora/core/common/src/test/java/alluxio/wire/WorkerNetAddressTest.java
+++ dora/core/common/src/test/java/alluxio/wire/WorkerNetAddressTest.java
@@ -11,13 +11,18 @@
 
 package alluxio.wire;
 
+import static org.junit.Assert.assertNotEquals;
+
 import alluxio.grpc.GrpcUtils;
 import alluxio.util.CommonUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Defaults;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Random;
 
 public class WorkerNetAddressTest {
@@ -36,6 +41,34 @@ public class WorkerNetAddressTest {
     WorkerNetAddress workerNetAddress = createRandom();
     WorkerNetAddress other = GrpcUtils.fromProto(GrpcUtils.toProto(workerNetAddress));
     checkEquality(workerNetAddress, other);
+  }
+
+  @Test
+  public void copyConstructor() throws IllegalAccessException {
+    WorkerNetAddress original = new WorkerNetAddress()
+        .setHost("host")
+        .setContainerHost("container")
+        .setRpcPort(1)
+        .setDataPort(1)
+        .setNettyDataPort(1)
+        .setSecureRpcPort(1)
+        .setWebPort(1)
+        .setDomainSocketPath("path");
+    WorkerNetAddress copied = new WorkerNetAddress(original);
+    // copied instance should contain exactly the same content
+    checkEquality(original, copied);
+    // mutate any non-final field in the copy,
+    // and the change should not be reflected in the original
+    for (Field field : WorkerNetAddress.class.getDeclaredFields()) {
+      int fieldModifiers = field.getModifiers();
+      if (Modifier.isStatic(fieldModifiers) || Modifier.isFinal(fieldModifiers)) {
+        continue;
+      }
+      field.setAccessible(true);
+      // set fields in the copy to their default value
+      field.set(copied, Defaults.defaultValue(field.getType()));
+      assertNotEquals(field.getName(), field.get(original), field.get(copied));
+    }
   }
 
   public void checkEquality(WorkerNetAddress a, WorkerNetAddress b) {
