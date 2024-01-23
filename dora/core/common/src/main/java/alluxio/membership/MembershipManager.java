849f3a679d0d0e988cc412de26b1cf7c7929c29b
Jianjian <jja725@gmail.com>
2024-01-23 15:26:19 -0800

    add etcd membership manager only include active workers

--- dora/core/common/src/main/java/alluxio/membership/MembershipManager.java
+++ dora/core/common/src/main/java/alluxio/membership/MembershipManager.java
@@ -125,6 +125,8 @@ public interface MembershipManager extends AutoCloseable {
           return EtcdMembershipManager.create(conf);
         case MASTER:
           return MasterMembershipManager.create();
+        case SERVICE_REGISTRY:
+          return ServiceRegistryMembershipManager.create(conf);
         default:
           throw new IllegalStateException("Unrecognized Membership Type");
       }
